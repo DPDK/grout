4250f50a9f0f38f7ff3ad8242595a197a47fddb4
Robin Jarry <rjarry@redhat.com>
2023-11-07 16:08:13 +0100

    Add base meson & ci files
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

---
+++ core/main.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023 Christophe Fontaine */
+/* Copyright (c) 2023 Robin Jarry */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("coucou\n");
+	return 0;
+}
