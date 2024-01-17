79561f34f040f8d8148176fe55a0bf551332a86e
Robin Jarry <rjarry@redhat.com>
2024-01-17 21:58:40 +0100

    lib: fix memory leaks
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- modules/infra/lib/port.c
+++ modules/infra/lib/port.c
@@ -15,19 +15,21 @@
 int br_infra_port_add(const struct br_client *c, const char *devargs, uint16_t *port_id) {
 	struct br_infra_port_add_resp *resp = NULL;
 	struct br_infra_port_add_req req;
+	int ret = -1;
 
 	memset(&req, 0, sizeof(req));
 	memccpy(req.devargs, devargs, 0, sizeof(req.devargs));
 
 	if (send_recv(c, BR_INFRA_PORT_ADD, sizeof(req), &req, (void **)&resp) < 0)
-		return -1;
+		goto out;
 
 	if (port_id != NULL)
 		*port_id = resp->port_id;
 
+	ret = 0;
+out:
 	free(resp);
-
-	return 0;
+	return ret;
 }
 
 int br_infra_port_del(const struct br_client *c, uint16_t port_id) {
@@ -39,41 +41,48 @@ int br_infra_port_del(const struct br_client *c, uint16_t port_id) {
 int br_infra_port_get(const struct br_client *c, uint16_t port_id, struct br_infra_port *port) {
 	struct br_infra_port_get_req req = {port_id};
 	struct br_infra_port_get_resp *resp = NULL;
+	int ret = -1;
 
 	if (port == NULL) {
 		errno = EINVAL;
-		return -1;
+		goto out;
 	}
 
 	if (send_recv(c, BR_INFRA_PORT_GET, sizeof(req), &req, (void **)&resp) < 0)
-		return -1;
+		goto out;
 
 	memcpy(port, &resp->port, sizeof(*port));
 
-	return 0;
+	ret = 0;
+out:
+	free(resp);
+	return ret;
 }
 
 int br_infra_port_list(const struct br_client *c, size_t *n_ports, struct br_infra_port **ports) {
 	struct br_infra_port_list_resp *resp = NULL;
+	int ret = -1;
 
-	if (ports == NULL) {
+	if (n_ports == NULL || ports == NULL) {
 		errno = EINVAL;
-		return -1;
+		goto out;
 	}
 
 	if (send_recv(c, BR_INFRA_PORT_LIST, 0, NULL, (void **)&resp) < 0)
-		return -1;
+		goto out;
 
 	*n_ports = resp->n_ports;
 	*ports = calloc(resp->n_ports, sizeof(struct br_infra_port));
 	if (*ports == NULL) {
 		errno = ENOMEM;
-		return -1;
+		goto out;
 	}
 	memcpy(*ports, &resp->ports, resp->n_ports * sizeof(struct br_infra_port));
-	free(resp);
 
-	return 0;
+	ret = 0;
+out:
+	free(resp);
+	return ret;
 }
 
 int br_infra_port_set(const struct br_client *c, uint16_t port_id, uint16_t n_rxq, uint16_t burst) {
