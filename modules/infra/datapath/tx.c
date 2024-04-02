DPDK/grout

commit bd55dc4c138756ac5f2f629a183e55695c0ddcbb

tx: bulk packets
Each mbuf has a private data section containing the output port_id (as
determined in ipv4_rewrite for example).

Try to transmit as many packets as possible per call to
rte_eth_tx_burst. This brings a drastic performance increase:

Before     eth_tx -> 307.8 cycles/pkt
After      eth_tx -> 27.9 cycles/pkt

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/datapath/tx.c
@@ -14,34 +14,72 @@
 #include <rte_graph_worker.h>
 #include <rte_malloc.h>
 
+#include <stdint.h>
+
 #define TX_ERROR 0
+#define NO_PORT 1
 
 struct tx_ctx {
 	uint16_t txq_ids[RTE_MAX_ETHPORTS];
 };
 
+static inline uint16_t tx_burst(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	uint16_t port_id,
+	struct rte_mbuf **mbufs,
+	uint16_t n
+) {
+	const struct tx_ctx *ctx = node->ctx_ptr;
+	uint16_t txq_id, tx_ok;
+
+	txq_id = ctx->txq_ids[port_id];
+	tx_ok = rte_eth_tx_burst(port_id, txq_id, mbufs, n);
+	if (tx_ok < n)
+		rte_node_enqueue(graph, node, TX_ERROR, (void **)&mbufs[tx_ok], n - tx_ok);
+
+	return tx_ok;
+}
+
 static uint16_t
 tx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
-	const struct tx_ctx *ctx = node->ctx_ptr;
-	uint16_t c, count = 0;
+	uint16_t port_id, i, count, burst_start;
 
-	for (uint16_t i = 0; i < nb_objs; i++) {
+	port_id = UINT16_MAX;
+	burst_start = 0;
+	count = 0;
+
+	for (i = 0; i < nb_objs; i++) {
 		struct rte_mbuf *mbuf = objs[i];
 		struct tx_mbuf_priv *priv;
-		uint16_t port_id, txq_id;
 
-		if ((priv = tx_mbuf_priv(mbuf)) == NULL)
-			goto drop;
-
-		port_id = priv->port_id;
-		txq_id = ctx->txq_ids[port_id];
-
-		c = rte_eth_tx_burst(port_id, txq_id, &mbuf, 1);
-		count += c;
-		if (c == 1)
+		if ((priv = tx_mbuf_priv(mbuf)) == NULL) {
+			rte_node_enqueue(graph, node, NO_PORT, (void **)&mbuf, 1);
 			continue;
-drop:
-		rte_node_enqueue(graph, node, TX_ERROR, (void **)&mbuf, 1);
+		}
+		if (priv->port_id != port_id) {
+			burst_start = i;
+			if (burst_start != i) {
+				count += tx_burst(
+					graph,
+					node,
+					port_id,
+					(struct rte_mbuf **)&objs[burst_start],
+					i - burst_start
+				);
+			}
+			port_id = priv->port_id;
+		}
+	}
+
+	if (burst_start != i) {
+		count += tx_burst(
+			graph,
+			node,
+			port_id,
+			(struct rte_mbuf **)&objs[burst_start],
+			i - burst_start
+		);
 	}
 
 	return count;
@@ -96,9 +134,10 @@ static struct rte_node_register tx_node_base = {
 	.init = tx_init,
 	.fini = tx_fini,
 
-	.nb_edges = 1,
+	.nb_edges = 2,
 	.next_nodes = {
 		[TX_ERROR] = "eth_tx_error",
+		[NO_PORT] = "eth_tx_no_port",
 	},
 };
 
@@ -109,3 +148,4 @@ static struct br_node_info info = {
 BR_NODE_REGISTER(info);
 
 BR_DROP_REGISTER(eth_tx_error);
+BR_DROP_REGISTER(eth_tx_no_port);

commit 4182255a46613ca6bd0456014cda926cb0cd1700

tx: fix burst algorithm
Fix a typo. burst_start should only be updated *after* transmitting the
packets. Not before...

Fixes: bd55dc4c1387 ("tx: bulk packets")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/tx.c
@@ -58,7 +58,6 @@ tx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t
 			continue;
 		}
 		if (priv->port_id != port_id) {
-			burst_start = i;
 			if (burst_start != i) {
 				count += tx_burst(
 					graph,
@@ -67,6 +66,7 @@ tx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t
 					(void *)&objs[burst_start],
 					i - burst_start
 				);
+				burst_start = i;
 			}
 			port_id = priv->port_id;
 		}