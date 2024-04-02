DPDK/grout

commit 3cb1ab2eb410810ae85f5c774eb9a102d4a60ace

net: allow parsing ip/prefix without masking
Signed-off-by: Robin Jarry <rjarry@redhat.com>

main/include/br_net_types.h
@@ -46,7 +46,7 @@ struct ip4_net {
 	uint8_t prefixlen;
 };
 
-static inline int br_ip4_net_parse(const char *s, struct ip4_net *net) {
+static inline int br_ip4_net_parse(const char *s, struct ip4_net *net, bool zero_mask) {
 	char *addr = NULL;
 	int ret = -1;
 
@@ -62,8 +62,10 @@ static inline int br_ip4_net_parse(const char *s, struct ip4_net *net) {
 		errno = EINVAL;
 		goto out;
 	}
-	// mask non network bits to zero
-	net->addr &= htonl((uint32_t)(UINT64_MAX << (32 - net->prefixlen)));
+	if (zero_mask) {
+		// mask non network bits to zero
+		net->addr &= htonl((uint32_t)(UINT64_MAX << (32 - net->prefixlen)));
+	}
 	ret = 0;
 out:
 	free(addr);

modules/ip4/cli/route.c
@@ -15,7 +15,7 @@ static cmd_status_t route4_add(const struct br_client *c, const struct ec_pnode 
 	struct ip4_net dest;
 	ip4_addr_t next_hop;
 
-	if (br_ip4_net_parse(arg_str(p, "DEST"), &dest) < 0)
+	if (br_ip4_net_parse(arg_str(p, "DEST"), &dest, true) < 0)
 		return CMD_ERROR;
 	if (inet_pton(AF_INET, arg_str(p, "NH"), &next_hop) != 1) {
 		errno = EINVAL;
@@ -31,7 +31,7 @@ static cmd_status_t route4_add(const struct br_client *c, const struct ec_pnode 
 static cmd_status_t route4_del(const struct br_client *c, const struct ec_pnode *p) {
 	struct ip4_net dest;
 
-	if (br_ip4_net_parse(arg_str(p, "DEST"), &dest) < 0)
+	if (br_ip4_net_parse(arg_str(p, "DEST"), &dest, true) < 0)
 		return CMD_ERROR;
 
 	if (br_ip4_route_del(c, &dest, true) < 0)

commit db8c57132830280abea5c8a4fc8b9ca12de39fae

net: add missing include
Fixes: 3cb1ab2eb410 ("net: allow parsing ip/prefix without masking")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

main/include/br_net_types.h
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>