DPDK/grout

commit 3e23311b40147228eced29d3f91a89a23c16f834

arp: use a tail queue for held packets
Append packets at the tail of the queue and when the nexthop becomes
reachable, send packets starting from the head to preserve ordering.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/datapath/arp_output_request.c
@@ -44,15 +44,15 @@ static inline hold_status_t hold_packet(struct nexthop *nh, struct rte_mbuf *mbu
 		data->ether_type = rte_cpu_to_be_16(RTE_ETHER_TYPE_IPV4);
 		data->iface = iface_from_id(nh->iface_id);
 		status = OK_TO_SEND;
-	} else if (nh->n_held_pkts < IP4_NH_MAX_HELD_PKTS) {
-		// TODO: Implement this as a tail queue to preserve ordering.
-		if (nh->held_pkts == NULL) {
-			nh->held_pkts = mbuf;
+	} else if (nh->held_pkts.num < IP4_NH_MAX_HELD_PKTS) {
+		queue_mbuf_data(mbuf)->next = NULL;
+		if (nh->held_pkts.head == NULL) {
+			nh->held_pkts.head = mbuf;
 		} else {
-			br_mbuf_priv(mbuf)->next = nh->held_pkts;
-			nh->held_pkts = mbuf;
+			queue_mbuf_data(nh->held_pkts.tail)->next = mbuf;
+			nh->held_pkts.tail = mbuf;
 		}
-		nh->n_held_pkts++;
+		nh->held_pkts.num++;
 		status = HELD;
 	} else {
 		status = HOLD_QUEUE_FULL;

modules/infra/datapath/br_mbuf.h
@@ -13,26 +13,19 @@
 #include <stdalign.h>
 #include <stdint.h>
 
-#define BR_MBUF_PRIV_MAX_SIZE 24
+#define BR_MBUF_PRIV_MAX_SIZE 32
 
-struct br_mbuf_priv {
-	struct rte_mbuf *next;
-	uint8_t data[BR_MBUF_PRIV_MAX_SIZE];
-};
-
-static_assert(sizeof(struct br_mbuf_priv) <= MEMBER_SIZE(struct rte_mbuf, dynfield1));
+static_assert(BR_MBUF_PRIV_MAX_SIZE <= MEMBER_SIZE(struct rte_mbuf, dynfield1));
 
 extern int br_mdyn_offset;
-
-static inline struct br_mbuf_priv *br_mbuf_priv(struct rte_mbuf *m) {
-	return RTE_MBUF_DYNFIELD(m, br_mdyn_offset, struct br_mbuf_priv *);
-}
 
 #define BR_MBUF_PRIV_DATA_TYPE(type_name, fields)                                                  \
 	struct type_name fields;                                                                   \
 	static inline struct type_name *type_name(struct rte_mbuf *m) {                            \
 		static_assert(sizeof(struct type_name) <= BR_MBUF_PRIV_MAX_SIZE);                  \
-		return (struct type_name *)br_mbuf_priv(m)->data;                                  \
+		return RTE_MBUF_DYNFIELD(m, br_mdyn_offset, struct type_name *);                   \
 	}
+
+BR_MBUF_PRIV_DATA_TYPE(queue_mbuf_data, { struct rte_mbuf *next; });
 
 #endif

modules/infra/datapath/mbuf.c
@@ -12,8 +12,8 @@
 
 static const struct rte_mbuf_dynfield dyn_desc = {
 	.name = "br_priv",
-	.size = sizeof(struct br_mbuf_priv),
-	.align = alignof(struct br_mbuf_priv),
+	.size = BR_MBUF_PRIV_MAX_SIZE,
+	.align = alignof(void *),
 };
 
 int br_mdyn_offset = -1;

modules/ip/control/br_ip4_control.h
@@ -14,6 +14,7 @@
 #include <rte_spinlock.h>
 
 #include <stdint.h>
+#include <sys/queue.h>
 
 struct __rte_cache_aligned nexthop {
 	br_ip4_nh_flags_t flags;
@@ -26,8 +27,10 @@ struct __rte_cache_aligned nexthop {
 	uint32_t ref_count;
 	rte_spinlock_t lock;
 	// packets waiting for ARP resolution
-	uint32_t n_held_pkts;
-	struct rte_mbuf *held_pkts; // linked list using br_mbuf_priv->next
+	struct {
+		struct rte_mbuf *head, *tail;
+		uint32_t num;
+	} held_pkts;
 };
 
 #define IP4_NH_MAX_HELD_PKTS 8192

modules/ip/datapath/arp_input.c
@@ -8,6 +8,7 @@
 #include <br_ip4_control.h>
 #include <br_log.h>
 #include <br_mbuf.h>
+#include <br_queue.h>
 
 #include <rte_arp.h>
 #include <rte_byteorder.h>
@@ -33,8 +34,7 @@ static inline void update_nexthop(
 	uint16_t iface_id,
 	const struct rte_arp_hdr *arp
 ) {
-	struct br_mbuf_priv *priv;
-	struct rte_mbuf *m;
+	struct rte_mbuf *m, *next;
 
 	// Static next hops never need updating.
 	if (nh->flags & BR_IP4_NH_F_STATIC)
@@ -50,16 +50,14 @@ static inline void update_nexthop(
 	rte_ether_addr_copy(&arp->arp_data.arp_sha, &nh->lladdr);
 
 	// Flush all held packets.
-	m = nh->held_pkts;
+	m = nh->held_pkts.head;
 	while (m != NULL) {
+		next = queue_mbuf_data(m)->next;
+		ip_output_mbuf_data(m)->nh = nh;
 		rte_node_enqueue_x1(graph, node, IP_OUTPUT, m);
-		// TODO: Implement this as a tail queue to preserve ordering.
-		priv = br_mbuf_priv(m);
-		m = priv->next;
-		priv->next = NULL;
+		m = next;
 	}
-	nh->held_pkts = NULL;
-	nh->n_held_pkts = 0;
+	memset(&nh->held_pkts, 0, sizeof(nh->held_pkts));
 
 	rte_spinlock_unlock(&nh->lock);
 }

modules/ip/datapath/ip_output.c
@@ -43,11 +43,7 @@ output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint
 		mbuf = objs[i];
 		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
 
-		nh = ip_output_mbuf_data(mbuf)->nh; // from ip_input
-		if (nh == NULL) {
-			const struct iface *iface = eth_input_mbuf_data(mbuf)->iface;
-			nh = ip4_route_lookup(iface->vrf_id, ip->dst_addr); // from arp_input
-		}
+		nh = ip_output_mbuf_data(mbuf)->nh;
 		if (nh == NULL) {
 			next = NO_ROUTE;
 			goto next;

commit a6b3321aef0885f600297af0e726e12f9e98c927

arp: fix segfault when appending second packet to hold queue
Avoid accessing a NULL pointer when appending the second packet to the
hold queue.

Always set the tail of the hold queue to the last packet.

Fixes: 3e23311b4014 ("arp: use a tail queue for held packets")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/datapath/arp_output_request.c
@@ -45,12 +45,11 @@ static inline hold_status_t hold_packet(struct nexthop *nh, struct rte_mbuf *mbu
 		status = OK_TO_SEND;
 	} else if (nh->held_pkts.num < IP4_NH_MAX_HELD_PKTS) {
 		queue_mbuf_data(mbuf)->next = NULL;
-		if (nh->held_pkts.head == NULL) {
+		if (nh->held_pkts.head == NULL)
 			nh->held_pkts.head = mbuf;
-		} else {
+		else
 			queue_mbuf_data(nh->held_pkts.tail)->next = mbuf;
-			nh->held_pkts.tail = mbuf;
-		}
+		nh->held_pkts.tail = mbuf;
 		nh->held_pkts.num++;
 		status = HELD;
 	} else {