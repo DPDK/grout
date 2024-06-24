DPDK/grout

commit 4d476986da37b8d9575ab6f1f0f41ff4c665eaf2

treewide: rename the project to grout
Signed-off-by: Robin Jarry <rjarry@redhat.com>

cli/interact.c
@@ -5,7 +5,7 @@
 #include "interact.h"
 #include "log.h"
 
-#include <br_cli.h>
+#include <gr_cli.h>
 
 #include <ecoli.h>
 
@@ -13,7 +13,7 @@
 #include <signal.h>
 #include <wordexp.h>
 
-#define __PROMPT "br#"
+#define __PROMPT "grout#"
 #define PROMPT __PROMPT " "
 #define DELIM "\x1e"
 #define COLOR_PROMPT DELIM CYAN_SGR DELIM __PROMPT DELIM RESET_SGR DELIM " "
@@ -61,14 +61,14 @@ const char *__lsan_default_options(void) {
 }
 #endif
 
-int interact(const struct br_api_client *client, struct ec_node *cmdlist) {
+int interact(const struct gr_api_client *client, struct ec_node *cmdlist) {
 	int flags = EC_EDITLINE_DEFAULT_SIGHANDLER;
 	struct ec_editline *edit = NULL;
 	struct ec_node *shlex = NULL;
 	char *line = NULL;
 	int ret = -1;
 
-	if ((edit = ec_editline("br-cli", stdin, stdout, stderr, flags)) == NULL) {
+	if ((edit = ec_editline("grcli", stdin, stdout, stderr, flags)) == NULL) {
 		errorf("ec_editline: %s", strerror(errno));
 		goto end;
 	}

commit 570efb469174677e7b4468fbae7d75c08eda4dd6

cli: rename history file
This was forgotten.

Fixes: 4d476986da37 ("treewide: rename the project to grout")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

cli/interact.c
@@ -82,7 +82,7 @@ int interact(const struct gr_api_client *client, struct ec_node *cmdlist) {
 	}
 
 	wordexp_t w;
-	if (wordexp("~/.brcli_history", &w, 0) == 0 && w.we_wordc == 1)
+	if (wordexp("~/.grcli_history", &w, 0) == 0 && w.we_wordc == 1)
 		ec_editline_set_history(edit, 256, w.we_wordv[0]);
 	wordfree(&w);
 