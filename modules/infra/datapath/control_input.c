DPDK/grout

commit a610da9bdaa9a955a6cb2a85c09ff7eaa1412c78

infra: add generic ctrlplane to dataplane node
Introduce a generic mechanism to post messages from the control plane to
the dataplane. This will avoid multiplication of source nodes, ring,
etc.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/control_input.c
@@ -1,0 +1,122 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include "gr_control_input.h"
+
+#include <gr_eth_input.h>
+#include <gr_eth_output.h>
+#include <gr_graph.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+
+#include <rte_ether.h>
+#include <rte_graph_worker.h>
+#include <rte_malloc.h>
+#include <rte_version.h>
+
+enum {
+	UNKNOWN_CONTROL_INPUT_TYPE,
+	EDGE_COUNT,
+};
+
+struct gr_control_input_msg {
+	uint16_t type;
+	void *data;
+};
+
+static struct rte_ring *control_input_ring;
+static struct rte_mempool *control_input_pool;
+
+static rte_edge_t control_input_edges[1 << 8] = {UNKNOWN_CONTROL_INPUT_TYPE};
+
+void gr_control_input_add_handler(control_input_t type, const char *node_name) {
+	rte_edge_t edge = gr_node_attach_parent("control_input", node_name);
+	if (edge == RTE_EDGE_ID_INVALID)
+		ABORT("gr_node_attach_parent(control_input, lldp_output) failed");
+	if (control_input_edges[type] != UNKNOWN_CONTROL_INPUT_TYPE) {
+		ABORT("control_input: type=0x%04x -> already set to edge %u", type, edge);
+	}
+	control_input_edges[type] = edge;
+	LOG(DEBUG, "control_input: type=0x%04x -> edge %u", type, edge);
+}
+
+int post_to_stack(control_input_t type, void *data) {
+	struct gr_control_input_msg msg = {.type = type, .data = data};
+	int ret;
+
+	ret = rte_ring_enqueue_elem(control_input_ring, &msg, sizeof(msg));
+	if (ret < 0)
+		return errno_set(-ret);
+
+	return 0;
+}
+
+static uint16_t
+control_input_process(struct rte_graph *graph, struct rte_node *node, void **, uint16_t) {
+	struct gr_control_input_msg msg[RTE_GRAPH_BURST_SIZE];
+	struct rte_mbuf *mbuf;
+	uint16_t n;
+
+	n = rte_ring_dequeue_burst_elem(
+		control_input_ring,
+		msg,
+		sizeof(struct gr_control_input_msg),
+		RTE_GRAPH_BURST_SIZE,
+		NULL
+	);
+
+	for (unsigned i = 0; i < n; i++) {
+		mbuf = rte_pktmbuf_alloc(control_input_pool);
+		if (mbuf) {
+			control_input_mbuf_data(mbuf)->data = msg[i].data;
+			rte_node_enqueue_x1(graph, node, control_input_edges[msg[i].type], mbuf);
+		}
+	}
+
+	return n;
+}
+
+static void control_input_register(void) {
+	control_input_ring = rte_ring_create(
+		"control_input",
+		RTE_GRAPH_BURST_SIZE * 4,
+		SOCKET_ID_ANY,
+		RING_F_MP_RTS_ENQ | RING_F_MC_RTS_DEQ
+	);
+	if (control_input_ring == NULL)
+		ABORT("rte_ring_create(arp_output_request): %s", rte_strerror(rte_errno));
+
+	control_input_pool = rte_pktmbuf_pool_create(
+		"control_input",
+		RTE_GRAPH_BURST_SIZE * 4,
+		256, // cache_size
+		GR_MBUF_PRIV_MAX_SIZE, // priv_size
+		RTE_MBUF_DEFAULT_BUF_SIZE,
+		SOCKET_ID_ANY
+	);
+	if (control_input_pool == NULL)
+		ABORT("rte_pktmbuf_pool_create(control_): %s", rte_strerror(rte_errno));
+}
+
+static void control_input_unregister(void) {
+	rte_ring_free(control_input_ring);
+	rte_mempool_free(control_input_pool);
+}
+
+static struct rte_node_register control_input_node = {
+	.flags = RTE_NODE_SOURCE_F,
+	.name = "control_input",
+	.process = control_input_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {[UNKNOWN_CONTROL_INPUT_TYPE] = "control_input_unknown_type"},
+};
+
+static struct gr_node_info info = {
+	.node = &control_input_node,
+	.register_callback = control_input_register,
+	.unregister_callback = control_input_unregister,
+};
+
+GR_NODE_REGISTER(info);
+
+GR_DROP_REGISTER(control_input_unknown_type);

modules/infra/datapath/gr_control_input.h
@@ -1,0 +1,21 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#ifndef _GR_CONTROL_INPUT_PATH
+#define _GR_CONTROL_INPUT_PATH
+
+#include "gr_mbuf.h"
+
+#include <rte_graph.h>
+
+typedef enum {
+	CONTROL_INPUT_UNKNOWN = 0,
+	CONTROL_INPUT_ARP_REQUEST
+} control_input_t;
+
+GR_MBUF_PRIV_DATA_TYPE(control_input_mbuf_data, { void *data; });
+
+void gr_control_input_add_handler(control_input_t type, const char *node_name);
+int post_to_stack(control_input_t type, void *data);
+
+#endif

modules/infra/datapath/meson.build
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 Robin Jarry
 
 src += files(
+  'control_input.c',
   'drop.c',
   'eth_input.c',
   'eth_output.c',

commit 4afd23fc6460baad84ccfc812aad35891a9b2f5f

infra: fix control_input ring
Use correct initialiser for the control input ring.

Fixes: a610da9bdaa9 ("infra: add generic ctrlplane to dataplane node")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/datapath/control_input.c
@@ -75,8 +75,9 @@ control_input_process(struct rte_graph *graph, struct rte_node *node, void **, u
 }
 
 static void control_input_register(void) {
-	control_input_ring = rte_ring_create(
+	control_input_ring = rte_ring_create_elem(
 		"control_input",
+		sizeof(struct gr_control_input_msg),
 		RTE_GRAPH_BURST_SIZE * 4,
 		SOCKET_ID_ANY,
 		RING_F_MP_RTS_ENQ | RING_F_MC_RTS_DEQ