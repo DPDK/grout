DPDK/grout

commit 8beb4d93a61a75d47f76a58b7ed218fb0ff94346

infra: destroy ports after logical interfaces
Release ports and their associated resources only after all other
interface types are freed.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/iface.c
@@ -222,13 +222,27 @@ static void iface_init(struct event_base *) {
 }
 
 static void iface_fini(struct event_base *) {
-	for (uint16_t ifid = 0; ifid < MAX_IFACES; ifid++) {
-		struct iface *iface = ifaces[ifid];
+	struct iface *iface;
+	uint16_t ifid;
+
+	// Destroy all virtual interface first before removing DPDK ports.
+	for (ifid = 0; ifid < MAX_IFACES; ifid++) {
+		iface = ifaces[ifid];
+		if (iface != NULL || iface->type_id != GR_IFACE_TYPE_PORT) {
+			if (iface_destroy(ifid) < 0)
+				LOG(ERR, "iface_destroy: %s", strerror(errno));
+			ifaces[ifid] = NULL;
+		}
+	}
+	// Finally, destroy DPDK ports.
+	for (ifid = 0; ifid < MAX_IFACES; ifid++) {
+		iface = ifaces[ifid];
 		if (iface == NULL)
 			continue;
 		if (iface_destroy(ifid) < 0)
 			LOG(ERR, "iface_destroy: %s", strerror(errno));
 	}
+
 	rte_free(ifaces);
 	ifaces = NULL;
 }

commit 67cb00fa785870f9378220052cb7b1da7f0f2b00

infra: fix null ptr access
Fix null ptr access

Fixes: 8beb4d93a61a ("infra: destroy ports after logical interfaces")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/iface.c
@@ -228,7 +228,7 @@ static void iface_fini(struct event_base *) {
 	// Destroy all virtual interface first before removing DPDK ports.
 	for (ifid = 0; ifid < MAX_IFACES; ifid++) {
 		iface = ifaces[ifid];
-		if (iface != NULL || iface->type_id != GR_IFACE_TYPE_PORT) {
+		if (iface != NULL && iface->type_id != GR_IFACE_TYPE_PORT) {
 			if (iface_destroy(ifid) < 0)
 				LOG(ERR, "iface_destroy: %s", strerror(errno));
 			ifaces[ifid] = NULL;