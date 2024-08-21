DPDK/grout

commit 816af072e6bea8166fa23b95434d8e913ccba423

treewide: rationalize error values
Always return -errno on error.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

api/string.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Robin Jarry
 
+#include "gr_errno.h"
 #include "gr_string.h"
 
 #include <errno.h>
@@ -15,16 +16,12 @@ char *astrcat(char *buf, const char *fmt, ...) {
 	va_list ap;
 	int n;
 
-	if (fmt == NULL) {
-		errno = EINVAL;
-		goto out;
-	}
+	if (fmt == NULL)
+		return errno_set_null(EINVAL);
 
 	va_start(ap, fmt);
-	if ((n = vasprintf(&ret, fmt, ap)) < 0) {
-		ret = NULL;
-		goto out;
-	}
+	if ((n = vasprintf(&ret, fmt, ap)) < 0)
+		return errno_set_null(errno);
 	va_end(ap);
 
 	if (buf != NULL) {
@@ -32,8 +29,7 @@ char *astrcat(char *buf, const char *fmt, ...) {
 		char *tmp = malloc(buf_len + n + 1);
 		if (tmp == NULL) {
 			free(ret);
-			ret = NULL;
-			goto out;
+			return errno_set_null(ENOMEM);
 		}
 		memcpy(tmp, buf, buf_len);
 		memcpy(tmp + buf_len, ret, n + 1);
@@ -41,25 +37,21 @@ char *astrcat(char *buf, const char *fmt, ...) {
 		free(ret);
 		ret = tmp;
 	}
-out:
-	return ret;
+
+	return 0;
 }
 
 int utf8_check(const char *buf, size_t maxlen) {
 	mbstate_t mb;
 	size_t len;
 
-	if (strlen(buf) >= maxlen) {
-		errno = ENAMETOOLONG;
-		return -1;
-	}
+	if (strlen(buf) >= maxlen)
+		return errno_set(ENAMETOOLONG);
 
 	memset(&mb, 0, sizeof(mb));
 	len = mbsrtowcs(NULL, &buf, 0, &mb);
-	if (len == (size_t)-1) {
-		errno = EILSEQ;
-		return -1;
-	}
+	if (len == (size_t)-1)
+		return errno_set(EILSEQ);
 
 	return 0;
 }

api/gr_api_client_impl.h
@@ -114,7 +114,7 @@ int gr_api_client_send_recv(
 err:
 	free(req);
 	free(payload);
-	return -1;
+	return -errno;
 }
 
 #endif

api/gr_errno.h
@@ -1,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#ifndef _GR_API_ERRNO
+#define _GR_API_ERRNO
+
+#include <errno.h>
+#include <stddef.h>
+
+static inline int errno_set(int errnum) {
+	errno = errnum;
+	return -errnum;
+}
+
+static inline void *errno_set_null(int errnum) {
+	errno = errnum;
+	return NULL;
+}
+
+#endif

api/gr_net_types.h
@@ -4,6 +4,8 @@
 #ifndef _GR_NET_TYPES
 #define _GR_NET_TYPES
 
+#include "gr_errno.h"
+
 #include <arpa/inet.h>
 #include <endian.h>
 #include <errno.h>
@@ -74,7 +76,8 @@ static inline int ip4_net_format(const struct ip4_net *net, char *buf, size_t le
 	int n;
 
 	if ((tmp = inet_ntop(AF_INET, &net->ip, buf, len)) == NULL)
-		return -1;
+		return errno_set(EINVAL);
+
 	n = strlen(tmp);
 	return snprintf(buf + n, len - n, "/%u", net->prefixlen);
 }

cli/ec_node_dyn.c
@@ -96,7 +96,7 @@ static int ec_node_dyn_complete(
 	if (client == NULL)
 		client = connect_client(comp);
 	if (client == NULL)
-		return -1;
+		return -errno;
 
 	return priv->cb(client, node, comp, ec_strvec_val(strvec, 0), priv->cb_arg);
 }

cli/ecoli.c
@@ -84,80 +84,60 @@ end:
 
 const char *arg_str(const struct ec_pnode *p, const char *id) {
 	const struct ec_pnode *n = ec_pnode_find(p, id);
-	if (n == NULL) {
-		errno = ENOENT;
-		return NULL;
-	}
+	if (n == NULL)
+		return errno_set_null(ENOENT);
+
 	const struct ec_strvec *v = ec_pnode_get_strvec(n);
-	if (v == NULL || ec_strvec_len(v) != 1) {
-		errno = EIO;
-		return NULL;
-	}
+	if (v == NULL || ec_strvec_len(v) != 1)
+		return errno_set_null(EIO);
+
 	return ec_strvec_val(v, 0);
 }
 
 int arg_i64(const struct ec_pnode *p, const char *id, int64_t *val) {
 	const struct ec_pnode *n = ec_pnode_find(p, id);
-	if (n == NULL) {
-		errno = ENOENT;
-		goto err;
-	}
+	if (n == NULL)
+		return errno_set(ENOENT);
+
 	const struct ec_strvec *v = ec_pnode_get_strvec(n);
-	if (v == NULL || ec_strvec_len(v) != 1) {
-		errno = EFAULT;
-		goto err;
-	}
+	if (v == NULL || ec_strvec_len(v) != 1)
+		return errno_set(EFAULT);
+
 	const char *str = ec_strvec_val(v, 0);
-	if (ec_node_int_getval(ec_pnode_get_node(n), str, val) < 0) {
-		if (errno == 0)
-			errno = EINVAL;
-		goto err;
-	}
+	if (ec_node_int_getval(ec_pnode_get_node(n), str, val) < 0)
+		return errno_set(EINVAL);
+
 	return 0;
-err:
-	return -1;
 }
 
 int arg_u64(const struct ec_pnode *p, const char *id, uint64_t *val) {
 	const struct ec_pnode *n = ec_pnode_find(p, id);
-	if (n == NULL) {
-		errno = ENOENT;
-		goto err;
-	}
+	if (n == NULL)
+		return errno_set(ENOENT);
+
 	const struct ec_strvec *v = ec_pnode_get_strvec(n);
-	if (v == NULL || ec_strvec_len(v) != 1) {
-		errno = EFAULT;
-		goto err;
-	}
+	if (v == NULL || ec_strvec_len(v) != 1)
+		return errno_set(EFAULT);
+
 	const char *str = ec_strvec_val(v, 0);
-	if (ec_node_uint_getval(ec_pnode_get_node(n), str, val) < 0) {
-		if (errno == 0)
-			errno = EINVAL;
-		goto err;
-	}
+	if (ec_node_uint_getval(ec_pnode_get_node(n), str, val) < 0)
+		return errno_set(EINVAL);
+
 	return 0;
-err:
-	return -1;
 }
 
 int arg_eth_addr(const struct ec_pnode *p, const char *id, struct rte_ether_addr *val) {
 	const struct ec_pnode *n = ec_pnode_find(p, id);
-	if (n == NULL) {
-		errno = ENOENT;
-		goto err;
-	}
-	const struct ec_strvec *v = ec_pnode_get_strvec(n);
-	if (v == NULL || ec_strvec_len(v) != 1) {
-		errno = EFAULT;
-		goto err;
-	}
-	const char *str = ec_strvec_val(v, 0);
+	if (n == NULL)
+		return errno_set(ENOENT);
 
-	if (rte_ether_unformat_addr(str, val) < 0) {
-		errno = EINVAL;
-		goto err;
-	}
+	const struct ec_strvec *v = ec_pnode_get_strvec(n);
+	if (v == NULL || ec_strvec_len(v) != 1)
+		return errno_set(EFAULT);
+
+	const char *str = ec_strvec_val(v, 0);
+	if (rte_ether_unformat_addr(str, val) < 0)
+		return errno_set(EINVAL);
+
 	return 0;
-err:
-	return -1;
 }

cli/exec.c
@@ -45,7 +45,7 @@ static cmd_cb_t *find_cmd_callback(struct ec_pnode *parsed) {
 			return cb;
 	}
 
-	return NULL;
+	return errno_set_null(EOPNOTSUPP);
 }
 
 static exec_status_t exec_strvec(

cli/gr_cli.h
@@ -5,6 +5,7 @@
 #define _GR_CLI
 
 #include <gr_api.h>
+#include <gr_errno.h>
 #include <gr_net_types.h>
 
 #include <ecoli.h>

commit c9fbea9261b0e9b8f0d9ea0e23b5e7e36f0d38b6

string: fix astrcat return value
This was accidentally modified.

Fixes: 816af072e6be ("treewide: rationalize error values")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

api/string.c
@@ -38,7 +38,7 @@ char *astrcat(char *buf, const char *fmt, ...) {
 		ret = tmp;
 	}
 
-	return 0;
+	return ret;
 }
 
 int utf8_check(const char *buf, size_t maxlen) {