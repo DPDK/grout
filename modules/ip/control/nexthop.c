DPDK/grout

commit c545ef9e7bed0dfd12e3afb09f453a46dbdd24ed

route: store nexthop pointers directly
Instead of storing nexthops in a flat array and using a hash table to
find free slots in the array, use a mempool to manage free nexthops and
directly store the nexthop object pointers as the nexthop "id" in the
fib structure.

Update the rte_fib options to use 8 bytes for nexthop IDs. The MSB of
the nexthop IDs is used to store metadata about the routing table. On
all standard 64 bit architectures, userspace address mappings use at
most 47 bits out of 64, leaving at least 17 bits of headroom filled with
zeroes.

For clarity add inline functions to convert between pointers and ids.

This makes the whole routing/nexthop API much leaner. Update all code
accordingly.

Update ip4_route_insert() to call ip4_nexthop_decref in the case of
failure.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/control/nexthop.c
@@ -10,71 +10,58 @@
 #include <gr_log.h>
 #include <gr_net_types.h>
 #include <gr_queue.h>
+#include <gr_stb_ds.h>
 
 #include <event2/event.h>
 #include <rte_errno.h>
-#include <rte_ethdev.h>
-#include <rte_hash.h>
-#include <rte_malloc.h>
+#include <rte_ether.h>
+#include <rte_mempool.h>
 
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/queue.h>
 
-static struct nexthop *nh_array;
-static struct rte_hash *nh_hash;
+static struct rte_mempool *nh_pool;
 
-struct nexthop_key {
+struct nexthop *ip4_nexthop_new(uint16_t vrf_id, uint16_t iface_id, ip4_addr_t ip) {
+	struct nexthop *nh;
+	void *data;
+	int ret;
+
+	if ((ret = rte_mempool_get(nh_pool, &data)) < 0)
+		return errno_set_null(-ret);
+
+	nh = data;
+	nh->vrf_id = vrf_id;
+	nh->iface_id = iface_id;
+	nh->ip = ip;
+
+	return nh;
+}
+
+struct lookup_filter {
+	uint16_t vrf_id;
 	ip4_addr_t ip;
-	// XXX: Using uint16_t to hold vrf_id causes the compiler to add 2 bytes
-	// padding at the end of the structure. When the structure is
-	// initialized on the stack, the padding bytes have undetermined
-	// contents.
-	//
-	// This structure is used to compute a hash key. In order to get
-	// deterministic results, use uint32_t to store the vrf_id so that the
-	// compiler does not insert any padding.
-	uint32_t vrf_id;
+	struct nexthop *nh;
 };
 
-struct nexthop *ip4_nexthop_get(uint32_t idx) {
-	return &nh_array[idx];
+static void nh_lookup_cb(struct rte_mempool *, void *opaque, void *obj, unsigned) {
+	struct lookup_filter *filter = opaque;
+	struct nexthop *nh = obj;
+	if (filter->nh == NULL && nh->ref_count > 0 && nh->ip == filter->ip
+	    && nh->vrf_id == filter->vrf_id)
+		filter->nh = nh;
 }
 
-int ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh) {
-	struct nexthop_key key = {ip, vrf_id};
-	int32_t nh_idx;
-
-	if ((nh_idx = rte_hash_lookup(nh_hash, &key)) < 0)
-		return errno_set(-nh_idx);
-
-	*idx = nh_idx;
-	*nh = &nh_array[nh_idx];
-
-	return 0;
-}
-
-int ip4_nexthop_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh) {
-	struct nexthop_key key = {ip, vrf_id};
-	int32_t nh_idx = rte_hash_add_key(nh_hash, &key);
-
-	if (nh_idx < 0)
-		return errno_set(-nh_idx);
-
-	nh_array[nh_idx].vrf_id = vrf_id;
-	nh_array[nh_idx].ip = ip;
-
-	*idx = nh_idx;
-	*nh = &nh_array[nh_idx];
-
-	return 0;
+struct nexthop *ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip) {
+	struct lookup_filter filter = {.vrf_id = vrf_id, .ip = ip};
+	rte_mempool_obj_iter(nh_pool, nh_lookup_cb, &filter);
+	return filter.nh ?: errno_set_null(ENOENT);
 }
 
 void ip4_nexthop_decref(struct nexthop *nh) {
 	if (nh->ref_count <= 1) {
-		struct nexthop_key key = {nh->ip, nh->vrf_id};
-
 		rte_spinlock_lock(&nh->lock);
 		// Flush all held packets.
 		struct rte_mbuf *m = nh->held_pkts_head;
@@ -84,9 +71,8 @@ void ip4_nexthop_decref(struct nexthop *nh) {
 			m = next;
 		}
 		rte_spinlock_unlock(&nh->lock);
-
-		rte_hash_del_key(nh_hash, &key);
 		memset(nh, 0, sizeof(*nh));
+		rte_mempool_put(nh_pool, nh);
 	} else {
 		nh->ref_count--;
 	}
@@ -99,7 +85,6 @@ void ip4_nexthop_incref(struct nexthop *nh) {
 static struct api_out nh4_add(const void *request, void **response) {
 	const struct gr_ip4_nh_add_req *req = request;
 	struct nexthop *nh;
-	uint32_t nh_idx;
 	int ret;
 
 	(void)response;
@@ -111,20 +96,19 @@ static struct api_out nh4_add(const void *request, void **response) {
 	if (iface_from_id(req->nh.iface_id) == NULL)
 		return api_out(errno, 0);
 
-	if (ip4_nexthop_lookup(req->nh.vrf_id, req->nh.host, &nh_idx, &nh) == 0) {
+	if ((nh = ip4_nexthop_lookup(req->nh.vrf_id, req->nh.host)) != NULL) {
 		if (req->exist_ok && req->nh.iface_id == nh->iface_id
-		    && rte_is_same_ether_addr(&req->nh.mac, (void *)&nh->lladdr))
+		    && rte_is_same_ether_addr(&req->nh.mac, &nh->lladdr))
 			return api_out(0, 0);
 		return api_out(EEXIST, 0);
 	}
 
-	if ((ret = ip4_nexthop_add(req->nh.vrf_id, req->nh.host, &nh_idx, &nh)) < 0)
-		return api_out(-ret, 0);
+	if ((nh = ip4_nexthop_new(req->nh.vrf_id, req->nh.iface_id, req->nh.host)) == NULL)
+		return api_out(errno, 0);
 
-	nh->iface_id = req->nh.iface_id;
-	memcpy(&nh->lladdr, (void *)&req->nh.mac, sizeof(nh->lladdr));
+	rte_ether_addr_copy(&req->nh.mac, &nh->lladdr);
 	nh->flags = GR_IP4_NH_F_STATIC | GR_IP4_NH_F_REACHABLE;
-	ret = ip4_route_insert(nh->vrf_id, nh->ip, 32, nh_idx, nh);
+	ret = ip4_route_insert(nh->vrf_id, nh->ip, 32, nh);
 
 	return api_out(-ret, 0);
 }
@@ -132,14 +116,13 @@ static struct api_out nh4_add(const void *request, void **response) {
 static struct api_out nh4_del(const void *request, void **response) {
 	const struct gr_ip4_nh_del_req *req = request;
 	struct nexthop *nh;
-	uint32_t idx;
 
 	(void)response;
 
 	if (req->vrf_id >= IP4_MAX_VRFS)
 		return api_out(EOVERFLOW, 0);
 
-	if (ip4_nexthop_lookup(req->vrf_id, req->host, &idx, &nh) < 0) {
+	if ((nh = ip4_nexthop_lookup(req->vrf_id, req->host)) == NULL) {
 		if (errno == ENOENT && req->missing_ok)
 			return api_out(0, 0);
 		return api_out(errno, 0);
@@ -155,136 +138,128 @@ static struct api_out nh4_del(const void *request, void **response) {
 	return api_out(0, 0);
 }
 
+struct list_context {
+	uint16_t vrf_id;
+	struct gr_ip4_nh *nh;
+};
+
+static void nh_list_cb(struct rte_mempool *, void *opaque, void *obj, unsigned) {
+	struct list_context *ctx = opaque;
+	struct nexthop *nh = obj;
+	struct gr_ip4_nh api_nh;
+
+	if (nh->ref_count == 0 || (nh->vrf_id != ctx->vrf_id && ctx->vrf_id != UINT16_MAX))
+		return;
+
+	api_nh.host = nh->ip;
+	api_nh.iface_id = nh->iface_id;
+	api_nh.vrf_id = nh->vrf_id;
+	rte_ether_addr_copy(&nh->lladdr, &api_nh.mac);
+	api_nh.flags = nh->flags;
+	if (nh->last_reply > 0)
+		api_nh.age = (rte_get_tsc_cycles() - nh->last_reply) / rte_get_tsc_hz();
+	else
+		api_nh.age = 0;
+	api_nh.held_pkts = nh->held_pkts_num;
+	arrpush(ctx->nh, api_nh);
+}
+
 static struct api_out nh4_list(const void *request, void **response) {
 	const struct gr_ip4_nh_list_req *req = request;
+	struct list_context ctx = {.vrf_id = req->vrf_id, .nh = NULL};
 	struct gr_ip4_nh_list_resp *resp = NULL;
-	struct gr_ip4_nh *api_nh;
-	struct nexthop *nh;
-	uint32_t num, iter;
-	const void *key;
-	int32_t idx;
-	void *data;
 	size_t len;
 
-	num = 0;
-	iter = 0;
-	while ((idx = rte_hash_iterate(nh_hash, &key, &data, &iter)) >= 0) {
-		nh = ip4_nexthop_get(idx);
-		if (nh->vrf_id == req->vrf_id || req->vrf_id == UINT16_MAX)
-			num++;
-	}
+	rte_mempool_obj_iter(nh_pool, nh_list_cb, &ctx);
 
-	len = sizeof(*resp) + num * sizeof(struct gr_ip4_nh);
-	if ((resp = calloc(len, 1)) == NULL)
+	len = sizeof(*resp) + arrlen(ctx.nh) * sizeof(*ctx.nh);
+	if ((resp = calloc(len, 1)) == NULL) {
+		arrfree(ctx.nh);
 		return api_out(ENOMEM, 0);
-
-	iter = 0;
-	while ((idx = rte_hash_iterate(nh_hash, &key, &data, &iter)) >= 0) {
-		nh = ip4_nexthop_get(idx);
-		if (nh->vrf_id != req->vrf_id && req->vrf_id != UINT16_MAX)
-			continue;
-		api_nh = &resp->nhs[resp->n_nhs++];
-		api_nh->host = nh->ip;
-		api_nh->iface_id = nh->iface_id;
-		api_nh->vrf_id = nh->vrf_id;
-		memcpy(&api_nh->mac, &nh->lladdr, sizeof(api_nh->mac));
-		api_nh->flags = nh->flags;
-		if (nh->last_reply > 0)
-			api_nh->age = (rte_get_tsc_cycles() - nh->last_reply) / rte_get_tsc_hz();
-		api_nh->held_pkts = nh->held_pkts_num;
 	}
 
+	resp->n_nhs = arrlen(ctx.nh);
+	memcpy(&resp->nhs, ctx.nh, arrlen(ctx.nh) * sizeof(*ctx.nh));
+	arrfree(ctx.nh);
 	*response = resp;
 
 	return api_out(0, len);
 }
 
-static void nexthop_gc(evutil_socket_t, short, void *) {
+static void nh_gc_cb(struct rte_mempool *, void *, void *obj, unsigned) {
 	uint64_t now = rte_get_tsc_cycles();
 	uint64_t reply_age, request_age;
 	unsigned probes, max_probes;
 	char buf[INET_ADDRSTRLEN];
-	struct nexthop *nh;
-	const void *key;
-	uint32_t iter;
-	int32_t idx;
-	void *data;
+	struct nexthop *nh = obj;
 
 	max_probes = IP4_NH_UCAST_PROBES + IP4_NH_BCAST_PROBES;
-	iter = 0;
 
-	while ((idx = rte_hash_iterate(nh_hash, &key, &data, &iter)) >= 0) {
-		nh = ip4_nexthop_get(idx);
+	if (nh->ref_count == 0 || nh->flags & GR_IP4_NH_F_STATIC)
+		return;
 
-		if (nh->flags & GR_IP4_NH_F_STATIC)
-			continue;
+	reply_age = (now - nh->last_reply) / rte_get_tsc_hz();
+	request_age = (now - nh->last_request) / rte_get_tsc_hz();
+	probes = nh->ucast_probes + nh->bcast_probes;
 
-		reply_age = (now - nh->last_reply) / rte_get_tsc_hz();
-		request_age = (now - nh->last_request) / rte_get_tsc_hz();
-		probes = nh->ucast_probes + nh->bcast_probes;
-
-		if (nh->flags & (GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE) && request_age > probes) {
-			if (probes >= max_probes && !(nh->flags & GR_IP4_NH_F_GATEWAY)) {
-				inet_ntop(AF_INET, &nh->ip, buf, sizeof(buf));
-				LOG(DEBUG,
-				    "%s vrf=%u failed_probes=%u held_pkts=%u: %s -> failed",
-				    buf,
-				    nh->vrf_id,
-				    probes,
-				    nh->held_pkts_num,
-				    gr_ip4_nh_f_name(
-					    nh->flags & (GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE)
-				    ));
-				nh->flags &= ~(GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE);
-				nh->flags |= GR_IP4_NH_F_FAILED;
-			} else {
-				if (arp_output_request_solicit(nh) < 0)
-					LOG(ERR, "arp_output_request_solicit: %s", strerror(errno));
-			}
-		} else if (nh->flags & GR_IP4_NH_F_REACHABLE
-			   && reply_age > IP4_NH_LIFETIME_REACHABLE) {
-			nh->flags &= ~GR_IP4_NH_F_REACHABLE;
-			nh->flags |= GR_IP4_NH_F_STALE;
-		} else if (nh->flags & GR_IP4_NH_F_FAILED
-			   && request_age > IP4_NH_LIFETIME_UNREACHABLE) {
+	if (nh->flags & (GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE) && request_age > probes) {
+		if (probes >= max_probes && !(nh->flags & GR_IP4_NH_F_GATEWAY)) {
 			inet_ntop(AF_INET, &nh->ip, buf, sizeof(buf));
 			LOG(DEBUG,
-			    "%s vrf=%u failed_probes=%u held_pkts=%u: failed -> <destroy>",
+			    "%s vrf=%u failed_probes=%u held_pkts=%u: %s -> failed",
 			    buf,
 			    nh->vrf_id,
 			    probes,
-			    nh->held_pkts_num);
-
-			// this also does ip4_nexthop_decref(), freeing the next hop
-			// and buffered packets.
-			if (ip4_route_delete(nh->vrf_id, nh->ip, 32) < 0)
-				LOG(ERR, "ip4_route_delete: %s", strerror(errno));
+			    nh->held_pkts_num,
+			    gr_ip4_nh_f_name(nh->flags & (GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE))
+			);
+			nh->flags &= ~(GR_IP4_NH_F_PENDING | GR_IP4_NH_F_STALE);
+			nh->flags |= GR_IP4_NH_F_FAILED;
+		} else {
+			if (arp_output_request_solicit(nh) < 0)
+				LOG(ERR, "arp_output_request_solicit: %s", strerror(errno));
 		}
+	} else if (nh->flags & GR_IP4_NH_F_REACHABLE && reply_age > IP4_NH_LIFETIME_REACHABLE) {
+		nh->flags &= ~GR_IP4_NH_F_REACHABLE;
+		nh->flags |= GR_IP4_NH_F_STALE;
+	} else if (nh->flags & GR_IP4_NH_F_FAILED && request_age > IP4_NH_LIFETIME_UNREACHABLE) {
+		inet_ntop(AF_INET, &nh->ip, buf, sizeof(buf));
+		LOG(DEBUG,
+		    "%s vrf=%u failed_probes=%u held_pkts=%u: failed -> <destroy>",
+		    buf,
+		    nh->vrf_id,
+		    probes,
+		    nh->held_pkts_num);
+
+		// this also does ip4_nexthop_decref(), freeing the next hop
+		// and buffered packets.
+		if (ip4_route_delete(nh->vrf_id, nh->ip, 32) < 0)
+			LOG(ERR, "ip4_route_delete: %s", strerror(errno));
 	}
+}
+
+static void nexthop_gc(evutil_socket_t, short, void *) {
+	rte_mempool_obj_iter(nh_pool, nh_gc_cb, NULL);
 }
 
 static struct event *nh_gc_timer;
 
 static void nh4_init(struct event_base *ev_base) {
-	struct rte_hash_parameters params = {
-		.name = "ip4_nh",
-		.entries = IP4_MAX_NEXT_HOPS,
-		.key_len = sizeof(struct nexthop_key),
-		.extra_flag = RTE_HASH_EXTRA_FLAGS_RW_CONCURRENCY_LF
-			| RTE_HASH_EXTRA_FLAGS_TRANS_MEM_SUPPORT,
-	};
-	nh_hash = rte_hash_create(&params);
-	if (nh_hash == NULL)
-		ABORT("rte_hash_create: %s", rte_strerror(rte_errno));
-
-	nh_array = rte_calloc(
-		"nh4_array",
-		rte_hash_max_key_id(nh_hash) + 1,
+	nh_pool = rte_mempool_create(
+		"ip4_nh", // name
+		rte_align32pow2(IP4_MAX_NEXT_HOPS) - 1,
 		sizeof(struct nexthop),
-		RTE_CACHE_LINE_SIZE
+		0, // cache size
+		0, // priv size
+		NULL, // mp_init
+		NULL, // mp_init_arg
+		NULL, // obj_init
+		NULL, // obj_init_arg
+		SOCKET_ID_ANY,
+		0 // flags
 	);
-	if (nh_array == NULL)
-		ABORT("rte_calloc(nh4_array) failed");
+	if (nh_pool == NULL)
+		ABORT("rte_mempool_create(ip4_nh) failed");
 
 	nh_gc_timer = event_new(ev_base, -1, EV_PERSIST | EV_FINALIZE, nexthop_gc, NULL);
 	if (nh_gc_timer == NULL)
@@ -297,10 +272,8 @@ static void nh4_init(struct event_base *ev_base) {
 static void nh4_fini(struct event_base *) {
 	event_free(nh_gc_timer);
 	nh_gc_timer = NULL;
-	rte_hash_free(nh_hash);
-	nh_hash = NULL;
-	rte_free(nh_array);
-	nh_array = NULL;
+	rte_mempool_free(nh_pool);
+	nh_pool = NULL;
 }
 
 static struct gr_api_handler nh4_add_handler = {

modules/ip/control/address.c
@@ -66,7 +66,6 @@ static struct api_out addr_add(const void *request, void **response) {
 	const struct iface *iface;
 	unsigned addr_index;
 	struct nexthop *nh;
-	uint32_t nh_idx;
 	int ret;
 
 	(void)response;
@@ -87,30 +86,29 @@ static struct api_out addr_add(const void *request, void **response) {
 	if (ifaddrs->count == IP4_HOPLIST_MAX_SIZE)
 		return api_out(ENOSPC, 0);
 
-	if (ip4_nexthop_lookup(iface->vrf_id, req->addr.addr.ip, &nh_idx, &nh) == 0)
+	if (ip4_nexthop_lookup(iface->vrf_id, req->addr.addr.ip) != NULL)
 		return api_out(EADDRINUSE, 0);
 
-	if ((ret = ip4_nexthop_add(iface->vrf_id, req->addr.addr.ip, &nh_idx, &nh)) < 0)
-		return api_out(-ret, 0);
+	if ((nh = ip4_nexthop_new(iface->vrf_id, iface->id, req->addr.addr.ip)) == NULL)
+		return api_out(errno, 0);
 
-	nh->iface_id = req->addr.iface_id;
 	nh->prefixlen = req->addr.addr.prefixlen;
 	nh->flags = GR_IP4_NH_F_LOCAL | GR_IP4_NH_F_LINK | GR_IP4_NH_F_REACHABLE
 		| GR_IP4_NH_F_STATIC;
 
 	if (iface_get_eth_addr(iface->id, &nh->lladdr) < 0)
-		if (errno != EOPNOTSUPP)
+		if (errno != EOPNOTSUPP) {
+			ip4_nexthop_decref(nh);
 			return api_out(errno, 0);
+		}
 
-	ret = ip4_route_insert(iface->vrf_id, nh->ip, nh->prefixlen, nh_idx, nh);
-	if (ret == 0) {
-		ifaddrs->nh[addr_index] = nh;
-		ifaddrs->count++;
-	} else {
-		ip4_nexthop_decref(nh);
-	}
+	if ((ret = ip4_route_insert(iface->vrf_id, nh->ip, nh->prefixlen, nh)) < 0)
+		return api_out(-ret, 0);
 
-	return api_out(-ret, 0);
+	ifaddrs->nh[addr_index] = nh;
+	ifaddrs->count++;
+
+	return api_out(0, 0);
 }
 
 static struct api_out addr_del(const void *request, void **response) {

modules/ip/control/gr_ip4_control.h
@@ -22,7 +22,7 @@ struct __rte_cache_aligned nexthop {
 	uint16_t iface_id;
 	ip4_addr_t ip;
 	uint64_t last_request, last_reply;
-	uint32_t ref_count;
+	uint32_t ref_count; // number of routes referencing this nexthop
 	uint8_t prefixlen;
 	uint8_t ucast_probes : 4, bcast_probes : 4;
 	rte_spinlock_t lock;
@@ -55,17 +55,16 @@ struct hoplist {
 #define IP4_MAX_ROUTES (1 << 16)
 #define IP4_MAX_VRFS 256
 
-struct nexthop *ip4_nexthop_get(uint32_t idx);
-int ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh);
-int ip4_nexthop_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh);
+struct nexthop *ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip);
+struct nexthop *ip4_nexthop_new(uint16_t vrf_id, uint16_t iface_id, ip4_addr_t ip);
 void ip4_nexthop_incref(struct nexthop *);
 void ip4_nexthop_decref(struct nexthop *);
 
-int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, uint32_t nh_idx, struct nexthop *);
+int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct nexthop *);
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip);
 struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
-void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh);
+void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *);
 
 // get the default address for a given interface
 struct nexthop *ip4_addr_get_preferred(uint16_t iface_id, ip4_addr_t dst);

modules/ip/control/route.c
@@ -3,6 +3,7 @@
 
 #include <gr_api.h>
 #include <gr_control.h>
+#include <gr_infra.h>
 #include <gr_ip4.h>
 #include <gr_ip4_control.h>
 #include <gr_log.h>
@@ -10,14 +11,10 @@
 #include <gr_queue.h>
 
 #include <event2/event.h>
-#include <rte_bitmap.h>
 #include <rte_build_config.h>
 #include <rte_ethdev.h>
 #include <rte_fib.h>
-#include <rte_hash.h>
 #include <rte_malloc.h>
-#include <rte_memory.h>
-#include <rte_rcu_qsbr.h>
 #include <rte_rib.h>
 
 #include <arpa/inet.h>
@@ -36,7 +33,7 @@ static struct rte_fib_conf fib_conf = {
 	.max_routes = IP4_MAX_ROUTES,
 	.rib_ext_sz = 0,
 	.dir24_8 = {
-		.nh_sz = RTE_FIB_DIR24_8_4B,
+		.nh_sz = RTE_FIB_DIR24_8_8B,
 		.num_tbl8 = 1 << 15,
 	},
 };
@@ -75,19 +72,42 @@ static struct rte_fib *get_or_create_fib(uint16_t vrf_id) {
 	return fib;
 }
 
+static inline uintptr_t nh_ptr_to_id(struct nexthop *nh) {
+	uintptr_t id = (uintptr_t)nh;
+
+	// rte_fib stores the nexthop ID on 8 bytes minus one bit which is used
+	// to store metadata about the routing table.
+	//
+	// Address mappings in userspace are guaranteed on x86_64 and aarch64
+	// to use at most 47 bits, leaving at least 17 bits of headroom filled
+	// with zeroes.
+	//
+	// rte_fib_add already checks that the nexthop value does not exceed the
+	// maximum allowed value. For clarity, we explicitly fail if the MSB is
+	// not zero.
+	if (id & GR_BIT64(63))
+		ABORT("MSB is not 0, martian architecture?");
+
+	return id;
+}
+
+static inline struct nexthop *nh_id_to_ptr(uintptr_t id) {
+	return (struct nexthop *)id;
+}
+
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip) {
 	uint32_t host_order_ip = rte_be_to_cpu_32(ip);
 	struct rte_fib *fib = get_fib(vrf_id);
-	uint64_t nh_idx;
+	uintptr_t nh_id;
 
 	if (fib == NULL)
 		return NULL;
 
-	rte_fib_lookup_bulk(fib, &host_order_ip, &nh_idx, 1);
-	if (nh_idx == BLACKHOLE)
+	rte_fib_lookup_bulk(fib, &host_order_ip, &nh_id, 1);
+	if (nh_id == BLACKHOLE)
 		return errno_set_null(EHOSTUNREACH);
 
-	return ip4_nexthop_get(nh_idx);
+	return nh_id_to_ptr(nh_id);
 }
 
 struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
@@ -95,7 +115,7 @@ struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t p
 	struct rte_fib *fib = get_fib(vrf_id);
 	struct rte_rib_node *rn;
 	struct rte_rib *rib;
-	uint64_t nh_idx;
+	uintptr_t nh_id;
 
 	if (fib == NULL)
 		return NULL;
@@ -105,33 +125,32 @@ struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t p
 	if (rn == NULL)
 		return errno_set_null(ENETUNREACH);
 
-	rte_rib_get_nh(rn, &nh_idx);
-	return ip4_nexthop_get(nh_idx);
+	rte_rib_get_nh(rn, &nh_id);
+	return nh_id_to_ptr(nh_id);
 }
 
-int ip4_route_insert(
-	uint16_t vrf_id,
-	ip4_addr_t ip,
-	uint8_t prefixlen,
-	uint32_t nh_idx,
-	struct nexthop *nh
-) {
+int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct nexthop *nh) {
 	struct rte_fib *fib = get_or_create_fib(vrf_id);
 	uint32_t host_order_ip = rte_be_to_cpu_32(ip);
 	int ret;
 
-	if (fib == NULL)
-		return -errno;
-
-	if (ip4_route_lookup_exact(vrf_id, ip, prefixlen) != NULL)
-		return errno_set(EEXIST);
-
-	if ((ret = rte_fib_add(fib, host_order_ip, prefixlen, nh_idx)) < 0)
-		return ret;
-
 	ip4_nexthop_incref(nh);
 
+	if (fib == NULL) {
+		ret = -errno;
+		goto fail;
+	}
+	if (ip4_route_lookup_exact(vrf_id, ip, prefixlen) != NULL) {
+		ret = -EEXIST;
+		goto fail;
+	}
+	if ((ret = rte_fib_add(fib, host_order_ip, prefixlen, nh_ptr_to_id(nh))) < 0)
+		goto fail;
+
 	return 0;
+fail:
+	ip4_nexthop_decref(nh);
+	return errno_set(-ret);
 }
 
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
@@ -157,9 +176,9 @@ int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen) {
 
 static struct api_out route4_add(const void *request, void **response) {
 	const struct gr_ip4_route_add_req *req = request;
+	uint32_t host_order_ip;
 	struct rte_fib *fib;
 	struct nexthop *nh;
-	uint32_t nh_idx;
 	int ret;
 
 	(void)response;
@@ -177,12 +196,16 @@ static struct api_out route4_add(const void *request, void **response) {
 	if ((fib = get_or_create_fib(req->vrf_id)) == NULL)
 		return api_out(errno, 0);
 
-	if (ip4_nexthop_lookup(req->vrf_id, req->nh, &nh_idx, &nh) < 0)
-		if (ip4_nexthop_add(req->vrf_id, req->nh, &nh_idx, &nh) < 0)
+	if ((nh = ip4_nexthop_lookup(req->vrf_id, req->nh)) == NULL)
+		if ((nh = ip4_nexthop_new(req->vrf_id, GR_IFACE_ID_UNDEF, req->nh)) == NULL)
 			return api_out(errno, 0);
 
-	if ((ret = rte_fib_add(fib, ntohl(req->dest.ip), req->dest.prefixlen, nh_idx)) < 0)
+	host_order_ip = ntohl(req->dest.ip);
+
+	if ((ret = rte_fib_add(fib, host_order_ip, req->dest.prefixlen, nh_ptr_to_id(nh))) < 0) {
+		ip4_nexthop_decref(nh);
 		return api_out(-ret, 0);
+	}
 
 	ip4_nexthop_incref(nh);
 	nh->flags |= GR_IP4_NH_F_GATEWAY;
@@ -225,7 +248,7 @@ static struct api_out route4_get(const void *request, void **response) {
 
 	resp->nh.host = nh->ip;
 	resp->nh.iface_id = nh->iface_id;
-	memcpy(&resp->nh.mac, &nh->lladdr, sizeof(resp->nh.mac));
+	rte_ether_addr_copy(&nh->lladdr, &resp->nh.mac);
 	resp->nh.flags = nh->flags;
 
 	*response = resp;
@@ -240,9 +263,8 @@ static struct api_out route4_list(const void *request, void **response) {
 	struct rte_rib_node *rn = NULL;
 	struct gr_ip4_route *r;
 	struct rte_rib *rib;
-	struct nexthop *nh;
 	size_t num, len;
-	uint64_t nh_idx;
+	uintptr_t nh_id;
 	uint32_t ip;
 
 	(void)request;
@@ -265,21 +287,19 @@ static struct api_out route4_list(const void *request, void **response) {
 
 	while ((rn = rte_rib_get_nxt(rib, 0, 0, rn, RTE_RIB_GET_NXT_ALL)) != NULL) {
 		r = &resp->routes[resp->n_routes++];
-		rte_rib_get_nh(rn, &nh_idx);
-		nh = ip4_nexthop_get(nh_idx);
+		rte_rib_get_nh(rn, &nh_id);
 		rte_rib_get_ip(rn, &ip);
 		rte_rib_get_depth(rn, &r->dest.prefixlen);
 		r->dest.ip = htonl(ip);
-		r->nh = nh->ip;
+		r->nh = nh_id_to_ptr(nh_id)->ip;
 	}
 	// FIXME: remove this when rte_rib_get_nxt returns a default route, if any is configured
 	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL) {
 		r = &resp->routes[resp->n_routes++];
-		rte_rib_get_nh(rn, &nh_idx);
-		nh = ip4_nexthop_get(nh_idx);
+		rte_rib_get_nh(rn, &nh_id);
 		r->dest.ip = 0;
 		r->dest.prefixlen = 0;
-		r->nh = nh->ip;
+		r->nh = nh_id_to_ptr(nh_id)->ip;
 	}
 	*response = resp;
 
@@ -308,7 +328,7 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 	struct rte_rib_node *rn = NULL;
 	struct rte_rib *rib;
 	ip4_addr_t local_ip;
-	uint64_t nh_idx;
+	uintptr_t nh_id;
 	ip4_addr_t ip;
 
 	local_ip = nh->ip;
@@ -316,8 +336,8 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 
 	rib = rte_fib_get_rib(get_fib(vrf_id));
 	while ((rn = rte_rib_get_nxt(rib, 0, 0, rn, RTE_RIB_GET_NXT_ALL)) != NULL) {
-		rte_rib_get_nh(rn, &nh_idx);
-		nh = ip4_nexthop_get(nh_idx);
+		rte_rib_get_nh(rn, &nh_id);
+		nh = nh_id_to_ptr(nh_id);
 
 		if (nh && ip4_addr_same_subnet(nh->ip, local_ip, local_prefixlen)) {
 			rte_rib_get_ip(rn, &ip);
@@ -336,8 +356,8 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 	}
 
 	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL || true) {
-		rte_rib_get_nh(rn, &nh_idx);
-		nh = ip4_nexthop_get(nh_idx);
+		rte_rib_get_nh(rn, &nh_id);
+		nh = nh_id_to_ptr(nh_id);
 
 		if (nh && ip4_addr_same_subnet(nh->ip, local_ip, local_prefixlen)) {
 			rte_rib_get_ip(rn, &ip);

modules/ip/datapath/arp_input.c
@@ -73,7 +73,6 @@ arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 	struct rte_mbuf *mbuf;
 	rte_edge_t next;
 	ip4_addr_t sip;
-	uint32_t idx;
 	uint64_t now;
 
 	now = rte_get_tsc_cycles();
@@ -114,11 +113,14 @@ arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			// Request/reply to our address but no next hop entry exists.
 			// Create a new next hop and its associated /32 route to allow
 			// faster lookups for next packets.
-			if (ip4_nexthop_add(iface->vrf_id, sip, &idx, &remote) < 0) {
+			if ((remote = ip4_nexthop_new(iface->vrf_id, iface->id, sip)) == NULL) {
 				next = ERROR;
 				goto next;
 			}
-			ip4_route_insert(iface->vrf_id, sip, 32, idx, remote);
+			if (ip4_route_insert(iface->vrf_id, sip, 32, remote) < 0) {
+				next = ERROR;
+				goto next;
+			}
 			update_nexthop(graph, node, remote, now, iface->id, arp);
 		} else {
 			next = DROP;

modules/ip/datapath/arp_output_request.c
@@ -31,10 +31,8 @@ int arp_output_request_solicit(struct nexthop *nh) {
 	int ret;
 	if (nh == NULL)
 		return errno_set(EINVAL);
-	ip4_nexthop_incref(nh);
 	ret = post_to_stack(arp_solicit, nh);
 	if (ret < 0) {
-		ip4_nexthop_decref(nh);
 		return errno_set(-ret);
 	}
 	return 0;
@@ -97,7 +95,6 @@ static uint16_t arp_output_request_process(
 		eth_data->ether_type = RTE_BE16(RTE_ETHER_TYPE_ARP);
 		eth_data->iface = iface_from_id(nh->iface_id);
 
-		ip4_nexthop_decref(nh);
 		next = OUTPUT;
 		sent++;
 next:

modules/ip/datapath/ip_output.c
@@ -80,7 +80,6 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 	struct nexthop *nh;
 	uint16_t i, sent;
 	rte_edge_t next;
-	uint32_t idx;
 
 	sent = 0;
 
@@ -109,13 +108,18 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			// We currently do not have an explicit entry for this destination IP.
 			// Create a new next hop and its associated /32 route so that next
 			// packets take it in priority with a single route lookup.
-			struct nexthop *remote;
-			if (ip4_nexthop_add(nh->vrf_id, ip->dst_addr, &idx, &remote) < 0) {
+			struct nexthop *remote = ip4_nexthop_new(
+				nh->vrf_id, nh->iface_id, ip->dst_addr
+			);
+			if (remote == NULL) {
 				next = ERROR;
 				goto next;
 			}
-			ip4_route_insert(nh->vrf_id, ip->dst_addr, 32, idx, remote);
-			remote->iface_id = nh->iface_id;
+			if (ip4_route_insert(nh->vrf_id, ip->dst_addr, 32, remote) < 0) {
+				next = ERROR;
+				goto next;
+			}
+
 			ip_output_mbuf_data(mbuf)->nh = remote;
 			nh = remote;
 		}

commit 9de2cd2fc5951f909a61b3dd662c7de0c2b770bb

nexthop: fix crash when listing 0 nexthops
When no nexthops are configured, the ctx.nh array is NULL. Calling
memcpy() on a NULL pointer results in undefined behaviour and sometimes
invalid memory access. Only call memcpy() if there is at least one
nexthop to return.

Fixes: c545ef9e7bed ("route: store nexthop pointers directly")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/control/nexthop.c
@@ -179,7 +179,8 @@ static struct api_out nh4_list(const void *request, void **response) {
 	}
 
 	resp->n_nhs = arrlen(ctx.nh);
-	memcpy(&resp->nhs, ctx.nh, arrlen(ctx.nh) * sizeof(*ctx.nh));
+	if (ctx.nh != NULL)
+		memcpy(&resp->nhs, ctx.nh, arrlen(ctx.nh) * sizeof(*ctx.nh));
 	arrfree(ctx.nh);
 	*response = resp;
 