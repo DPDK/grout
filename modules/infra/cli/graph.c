DPDK/grout

commit d7bae0ed79b0bdae025ef06b966308437068c74e

graph: add more control commands
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/cli/graph.c
@@ -1,0 +1,112 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <br_cli.h>
+#include <br_client.h>
+#include <br_infra.h>
+#include <br_infra_msg.h>
+#include <br_infra_types.h>
+#include <br_net_types.h>
+
+#include <ecoli.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+static cmd_status_t graph_dump(const struct br_client *c, const struct ec_pnode *p) {
+	char *dot = NULL;
+	size_t len = 0;
+
+	(void)p;
+
+	if (br_infra_graph_dump(c, &len, &dot) < 0)
+		return CMD_ERROR;
+
+	fwrite(dot, 1, len, stdout);
+	free(dot);
+
+	return CMD_SUCCESS;
+}
+
+static int stats_order(const void *sa, const void *sb) {
+	const struct br_infra_graph_stat *a = sa;
+	const struct br_infra_graph_stat *b = sb;
+	if (a->cycles == b->cycles)
+		return 0;
+	if (a->cycles > b->cycles)
+		return -1;
+	return 1;
+}
+
+static cmd_status_t graph_stats(const struct br_client *c, const struct ec_pnode *p) {
+	struct br_infra_graph_stat *stats = NULL;
+	size_t n_stats = 0;
+
+	(void)p;
+
+	if (br_infra_graph_stats(c, &n_stats, &stats) < 0)
+		return CMD_ERROR;
+
+	qsort(stats, n_stats, sizeof(*stats), stats_order);
+
+	printf("%-32s  %14s  %16s  %10s  %18s  %10s\n",
+	       "NODE",
+	       "CALLS",
+	       "PACKETS",
+	       "PKTS/CALL",
+	       "CYCLES",
+	       "CYCLES/PKT");
+
+	for (size_t i = 0; i < n_stats; i++) {
+		struct br_infra_graph_stat *s = &stats[i];
+		double pkt_call = 0, cycles_pkt = 0;
+
+		if (s->calls != 0)
+			pkt_call = ((double)s->objects) / ((double)s->calls);
+		if (s->objects != 0)
+			cycles_pkt = ((double)s->cycles) / ((double)s->objects);
+
+		printf("%-32s  %14lu  %16lu  %10.01f  %18lu  %10.01f\n",
+		       s->node,
+		       s->calls,
+		       s->objects,
+		       pkt_call,
+		       s->cycles,
+		       cycles_pkt);
+	}
+
+	free(stats);
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	struct ec_node *node = NULL;
+
+	node = CLI_COMMAND_CONTEXT(
+		"graph",
+		"Get information about the packet processing graph.",
+		CLI_COMMAND("dump", graph_dump, "Dump the graph in DOT format."),
+		CLI_COMMAND("stats", graph_stats, "Print graph nodes statistics.")
+	);
+	if (node == NULL)
+		goto fail;
+
+	if (ec_node_or_add(root, node) < 0)
+		goto fail;
+
+	return 0;
+
+fail:
+	ec_node_free(node);
+	return -1;
+}
+
+static struct br_cli_context ctx = {
+	.name = "graph",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/infra/control/graph.c
@@ -243,6 +243,117 @@ int worker_graph_reload_all(void) {
 	return 0;
 }
 
+static struct api_out graph_dump(const void *request, void **response) {
+	struct br_infra_graph_dump_resp *resp;
+	size_t buf_len = 0, resp_len = 0;
+	const char *graph_name;
+	struct worker *worker;
+	FILE *stream = NULL;
+	char *buf = NULL;
+	int ret = 0;
+
+	(void)request;
+
+	LIST_FOREACH (worker, &workers, next) {
+		for (int i = 0; i < 2; i++) {
+			if (worker->graph[i] != NULL) {
+				graph_name = worker->graph[i]->name;
+				goto found;
+			}
+		}
+	}
+	return api_out(ENODEV, 0);
+found:
+	if ((stream = open_memstream(&buf, &buf_len)) == NULL)
+		return api_out(errno, 0);
+
+	if ((ret = rte_graph_export(graph_name, stream)) < 0)
+		goto end;
+	fflush(stream);
+	resp_len = sizeof(*resp) + buf_len + 1;
+	if ((resp = calloc(1, resp_len)) == NULL) {
+		ret = -ENOMEM;
+		resp_len = 0;
+		goto end;
+	}
+
+	resp->len = buf_len + 1;
+	memccpy(resp->dot, buf, 0, buf_len + 1);
+	*response = resp;
+end:
+	fclose(stream);
+	free(buf);
+	return api_out(-ret, resp_len);
+}
+
+struct stat_value {
+	uint64_t objects;
+	uint64_t calls;
+	uint64_t cycles;
+};
+
+struct stat_entry {
+	char *key;
+	struct stat_value value;
+};
+
+static struct api_out graph_stats(const void *request, void **response) {
+	struct br_infra_graph_stats_resp *resp;
+	struct stat_entry *smap = NULL;
+	struct worker *worker;
+	size_t len;
+	int ret;
+
+	(void)request;
+
+	sh_new_arena(smap);
+
+	LIST_FOREACH (worker, &workers, next) {
+		const struct worker_stats *w_stats = atomic_load(&worker->stats);
+		if (w_stats == NULL)
+			continue;
+		for (unsigned i = 0; i < w_stats->n_stats; i++) {
+			const struct worker_stat *s = &w_stats->stats[i];
+			const char *name = rte_node_id_to_name(s->node_id);
+			struct stat_entry *e = shgetp_null(smap, name);
+			if (e != NULL) {
+				e->value.objects += s->objs;
+				e->value.calls += s->calls;
+				e->value.cycles += s->cycles;
+			} else {
+				struct stat_value value = {
+					.objects = s->objs,
+					.calls = s->calls,
+					.cycles = s->cycles,
+				};
+				shput(smap, name, value);
+			}
+		}
+	}
+
+	len = sizeof(*resp) + shlenu(smap) * sizeof(*resp->stats);
+	if ((resp = calloc(1, len)) == NULL) {
+		ret = ENOMEM;
+		len = 0;
+		goto end;
+	}
+
+	for (unsigned i = 0; i < shlenu(smap); i++) {
+		struct br_infra_graph_stat *s = &resp->stats[i];
+		struct stat_entry *e = &smap[i];
+		memccpy(s->node, e->key, 0, sizeof(s->node));
+		s->objects = e->value.objects;
+		s->calls = e->value.calls;
+		s->cycles = e->value.cycles;
+	}
+	resp->n_stats = shlenu(smap);
+	*response = resp;
+	ret = 0;
+end:
+	shfree(smap);
+	return api_out(ret, len);
+}
+
 static void graph_init(void) {
 	struct rte_node_register *reg;
 	struct br_node_info *info;
@@ -293,6 +404,17 @@ static void graph_fini(void) {
 	node_names = NULL;
 }
 
+static struct br_api_handler graph_dump_handler = {
+	.name = "graph dump",
+	.request_type = BR_INFRA_GRAPH_DUMP,
+	.callback = graph_dump,
+};
+static struct br_api_handler graph_stats_handler = {
+	.name = "graph stats",
+	.request_type = BR_INFRA_GRAPH_STATS,
+	.callback = graph_stats,
+};
+
 static struct br_module graph_module = {
 	.name = "graph",
 	.init = graph_init,
@@ -301,5 +423,7 @@ static struct br_module graph_module = {
 };
 
 RTE_INIT(control_graph_init) {
+	br_register_api_handler(&graph_dump_handler);
+	br_register_api_handler(&graph_stats_handler);
 	br_register_module(&graph_module);
 }

modules/infra/lib/graph.c
@@ -1,0 +1,67 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include "br_infra.h"
+
+#include <br_api.h>
+#include <br_client.h>
+#include <br_client_priv.h>
+#include <br_infra_msg.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int br_infra_graph_dump(const struct br_client *c, size_t *len, char **dot) {
+	struct br_infra_graph_dump_resp *resp = NULL;
+	int ret = -1;
+
+	if (len == NULL || dot == NULL) {
+		errno = EINVAL;
+		goto out;
+	}
+	if (send_recv(c, BR_INFRA_GRAPH_DUMP, 0, NULL, (void **)&resp) < 0)
+		goto out;
+
+	*dot = calloc(1, resp->len);
+	if (*dot == NULL) {
+		errno = ENOMEM;
+		goto out;
+	}
+	*len = resp->len;
+	memcpy(*dot, resp->dot, resp->len);
+	ret = 0;
+out:
+	free(resp);
+	return ret;
+}
+
+int br_infra_graph_stats(
+	const struct br_client *c,
+	size_t *n_stats,
+	struct br_infra_graph_stat **stats
+) {
+	struct br_infra_graph_stats_resp *resp = NULL;
+	int ret = -1;
+
+	if (n_stats == NULL || stats == NULL) {
+		errno = EINVAL;
+		goto out;
+	}
+
+	if (send_recv(c, BR_INFRA_GRAPH_STATS, 0, NULL, (void **)&resp) < 0)
+		goto out;
+
+	*stats = calloc(resp->n_stats, sizeof(*resp->stats));
+	if (*stats == NULL) {
+		errno = ENOMEM;
+		goto out;
+	}
+
+	*n_stats = resp->n_stats;
+	memcpy(*stats, resp->stats, resp->n_stats * sizeof(*resp->stats));
+	ret = 0;
+out:
+	free(resp);
+	return ret;
+}

modules/infra/cli/meson.build
@@ -2,8 +2,9 @@
 # Copyright (c) 2023 Robin Jarry
 
 cli_src += files(
+  'ec_node_devargs.c',
+  'graph.c',
   'port.c',
   'rxq.c',
   'stats.c',
-  'ec_node_devargs.c',
 )

modules/infra/datapath/main_loop.c
@@ -39,6 +39,8 @@ static int node_stats_callback(
 	s = &w_stats->stats[w_stats->__n++];
 	s->node_id = stats->id;
 	s->objs = stats->objs;
+	s->calls = stats->calls;
+	s->cycles = stats->cycles;
 
 	return 0;
 }

modules/infra/include/br_infra_msg.h
@@ -113,4 +113,24 @@ struct br_infra_stats_get_resp {
 
 // struct br_infra_stats_reset_resp { };
 
+// graph
+
+#define BR_INFRA_GRAPH_DUMP REQUEST_TYPE(BR_INFRA_MODULE, 0x0030)
+
+// struct br_infra_graph_dump_req { };
+
+struct br_infra_graph_dump_resp {
+	uint32_t len;
+	char dot[/* len */];
+};
+
+#define BR_INFRA_GRAPH_STATS REQUEST_TYPE(BR_INFRA_MODULE, 0x0031)
+
+// struct br_infra_graph_stats_req { };
+
+struct br_infra_graph_stats_resp {
+	uint16_t n_stats;
+	struct br_infra_graph_stat stats[/* n_stats */];
+};
+
 #endif

modules/infra/include/br_infra_types.h
@@ -34,4 +34,11 @@ struct br_infra_stat {
 	uint64_t value;
 };
 
+struct br_infra_graph_stat {
+	char node[64];
+	uint64_t objects;
+	uint64_t calls;
+	uint64_t cycles;
+};
+
 #endif

modules/infra/include/br_worker.h
@@ -24,6 +24,8 @@ struct queue_map {
 struct worker_stat {
 	rte_node_t node_id;
 	uint64_t objs;
+	uint64_t calls;
+	uint64_t cycles;
 };
 
 struct worker_stats {

commit 5a973c1fcb5c338e205db8ed4e77183c7d03e1e4

cli: strip trailing nul byte from graph dot export
grout returns a nul terminated buffer. Strip the nul byte before writing
to stdout.

Fixes: d7bae0ed79b0 ("graph: add more control commands")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/cli/graph.c
@@ -21,7 +21,8 @@ static cmd_status_t graph_dump(const struct gr_api_client *c, const struct ec_pn
 		return CMD_ERROR;
 
 	resp = resp_ptr;
-	fwrite(resp->dot, 1, resp->len, stdout);
+	// strip the trailing NUL byte
+	fwrite(resp->dot, 1, resp->len - 1, stdout);
 	free(resp_ptr);
 
 	return CMD_SUCCESS;