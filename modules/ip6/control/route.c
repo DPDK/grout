DPDK/grout

commit 0fc1cd222b8ec3b125cdfb168a09a240cc0debf0

modules: add static ipv6 forwarding support
This is a first draft of IPv6 forwarding support. It only supports
static next hops.

This patch addresses part of the following RFC:

RFC 8200: Internet Protocol, Version 6 (IPv6) Specification
	https://www.rfc-editor.org/rfc/rfc8200

Closes: https://github.com/rjarry/grout/issues/8
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/route.c
@@ -1,0 +1,408 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <gr_api.h>
+#include <gr_control.h>
+#include <gr_infra.h>
+#include <gr_ip6.h>
+#include <gr_ip6_control.h>
+#include <gr_log.h>
+#include <gr_net_types.h>
+#include <gr_queue.h>
+
+#include <event2/event.h>
+#include <rte_build_config.h>
+#include <rte_ethdev.h>
+#include <rte_fib6.h>
+#include <rte_malloc.h>
+#include <rte_rib6.h>
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/queue.h>
+
+static struct rte_fib6 **vrf_fibs;
+#define BLACKHOLE (IP6_MAX_NEXT_HOPS + 1)
+
+static struct rte_fib6_conf fib6_conf = {
+	.type = RTE_FIB6_TRIE,
+	.default_nh = BLACKHOLE,
+	.max_routes = IP6_MAX_ROUTES,
+	.rib_ext_sz = 0,
+	.trie = {
+		.nh_sz = RTE_FIB6_TRIE_8B,
+		.num_tbl8 = 1 << 15,
+	},
+};
+
+static struct rte_fib6 *get_fib6(uint16_t vrf_id) {
+	struct rte_fib6 *fib;
+
+	if (vrf_id >= IP6_MAX_VRFS)
+		return errno_set_null(EOVERFLOW);
+
+	fib = vrf_fibs[vrf_id];
+	if (fib == NULL)
+		return errno_set_null(ENONET);
+
+	return fib;
+}
+
+static struct rte_fib6 *get_or_create_fib6(uint16_t vrf_id) {
+	struct rte_fib6 *fib;
+
+	if (vrf_id >= IP6_MAX_VRFS)
+		return errno_set_null(EOVERFLOW);
+
+	fib = vrf_fibs[vrf_id];
+	if (fib == NULL) {
+		char name[64];
+
+		snprintf(name, sizeof(name), "ip6_vrf_%u", vrf_id);
+		fib = rte_fib6_create(name, SOCKET_ID_ANY, &fib6_conf);
+		if (fib == NULL)
+			return errno_set_null(rte_errno);
+
+		vrf_fibs[vrf_id] = fib;
+	}
+
+	return fib;
+}
+
+static inline uintptr_t nh_ptr_to_id(struct nexthop6 *nh) {
+	uintptr_t id = (uintptr_t)nh;
+
+	// rte_fib6 stores the nexthop ID on 8 bytes minus one bit which is used
+	// to store metadata about the routing table.
+	//
+	// Address mappings in userspace are guaranteed on x86_64 and aarch64
+	// to use at most 47 bits, leaving at least 17 bits of headroom filled
+	// with zeroes.
+	//
+	// rte_fib6_add already checks that the nexthop value does not exceed the
+	// maximum allowed value. For clarity, we explicitly fail if the MSB is
+	// not zero.
+	if (id & GR_BIT64(63))
+		ABORT("MSB is not 0, martian architecture?");
+
+	return id;
+}
+
+static inline struct nexthop6 *nh_id_to_ptr(uintptr_t id) {
+	return (struct nexthop6 *)id;
+}
+
+struct nexthop6 *ip6_route_lookup(uint16_t vrf_id, const struct rte_ipv6_addr *ip) {
+	struct rte_fib6 *fib6 = get_fib6(vrf_id);
+	uintptr_t nh_id;
+
+	if (fib6 == NULL)
+		return NULL;
+
+	rte_fib6_lookup_bulk(fib6, ip, &nh_id, 1);
+	if (nh_id == BLACKHOLE)
+		return errno_set_null(EHOSTUNREACH);
+
+	return nh_id_to_ptr(nh_id);
+}
+
+struct nexthop6 *
+ip6_route_lookup_exact(uint16_t vrf_id, const struct rte_ipv6_addr *ip, uint8_t prefixlen) {
+	struct rte_fib6 *fib = get_fib6(vrf_id);
+	struct rte_rib6_node *rn;
+	struct rte_rib6 *rib6;
+	uintptr_t nh_id;
+
+	if (fib == NULL)
+		return NULL;
+
+	rib6 = rte_fib6_get_rib(fib);
+	rn = rte_rib6_lookup_exact(rib6, ip, prefixlen);
+	if (rn == NULL)
+		return errno_set_null(ENETUNREACH);
+
+	rte_rib6_get_nh(rn, &nh_id);
+	return nh_id_to_ptr(nh_id);
+}
+
+int ip6_route_insert(
+	uint16_t vrf_id,
+	const struct rte_ipv6_addr *ip,
+	uint8_t prefixlen,
+	struct nexthop6 *nh
+) {
+	struct rte_fib6 *fib = get_or_create_fib6(vrf_id);
+	int ret;
+
+	ip6_nexthop_incref(nh);
+
+	if (fib == NULL) {
+		ret = -errno;
+		goto fail;
+	}
+	if (ip6_route_lookup_exact(vrf_id, ip, prefixlen) != NULL) {
+		ret = -EEXIST;
+		goto fail;
+	}
+	if ((ret = rte_fib6_add(fib, ip, prefixlen, nh_ptr_to_id(nh))) < 0)
+		goto fail;
+
+	return 0;
+fail:
+	ip6_nexthop_decref(nh);
+	return errno_set(-ret);
+}
+
+int ip6_route_delete(uint16_t vrf_id, const struct rte_ipv6_addr *ip, uint8_t prefixlen) {
+	struct rte_fib6 *fib = get_fib6(vrf_id);
+	struct nexthop6 *nh;
+	int ret;
+
+	if (fib == NULL)
+		return -errno;
+
+	nh = ip6_route_lookup_exact(vrf_id, ip, prefixlen);
+	if (nh == NULL)
+		return errno_set(ENOENT);
+
+	if ((ret = rte_fib6_delete(fib, ip, prefixlen)) < 0)
+		return errno_set(-ret);
+
+	ip6_nexthop_decref(nh);
+
+	return 0;
+}
+
+static struct api_out route6_add(const void *request, void **response) {
+	const struct gr_ip6_route_add_req *req = request;
+	struct rte_fib6 *fib6;
+	struct nexthop6 *nh;
+	int ret;
+
+	(void)response;
+
+	nh = ip6_route_lookup_exact(req->vrf_id, &req->dest.ip, req->dest.prefixlen);
+	if (nh != NULL) {
+		if (rte_ipv6_addr_eq(&req->nh, &nh->ip) && req->exist_ok)
+			return api_out(0, 0);
+		return api_out(EEXIST, 0);
+	}
+
+	if (ip6_route_lookup(req->vrf_id, &req->nh) == NULL)
+		return api_out(EHOSTUNREACH, 0);
+
+	if ((fib6 = get_or_create_fib6(req->vrf_id)) == NULL)
+		return api_out(errno, 0);
+
+	if ((nh = ip6_nexthop_lookup(req->vrf_id, &req->nh)) == NULL)
+		if ((nh = ip6_nexthop_new(req->vrf_id, GR_IFACE_ID_UNDEF, &req->nh)) == NULL)
+			return api_out(errno, 0);
+
+	if ((ret = rte_fib6_add(fib6, &req->dest.ip, req->dest.prefixlen, nh_ptr_to_id(nh))) < 0) {
+		ip6_nexthop_decref(nh);
+		return api_out(-ret, 0);
+	}
+
+	ip6_nexthop_incref(nh);
+	nh->flags |= GR_IP6_NH_F_GATEWAY;
+
+	return api_out(0, 0);
+}
+
+static struct api_out route6_del(const void *request, void **response) {
+	const struct gr_ip6_route_del_req *req = request;
+	struct nexthop6 *nh;
+
+	(void)response;
+
+	if ((nh = ip6_route_lookup_exact(req->vrf_id, &req->dest.ip, req->dest.prefixlen))
+	    == NULL) {
+		if (req->missing_ok)
+			return api_out(0, 0);
+		return api_out(ENOENT, 0);
+	}
+
+	if (!(nh->flags & GR_IP6_NH_F_GATEWAY))
+		return api_out(EBUSY, 0);
+
+	if (ip6_route_delete(req->vrf_id, &req->dest.ip, req->dest.prefixlen) < 0)
+		return api_out(errno, 0);
+
+	return api_out(0, 0);
+}
+
+static struct api_out route6_get(const void *request, void **response) {
+	const struct gr_ip6_route_get_req *req = request;
+	struct gr_ip6_route_get_resp *resp = NULL;
+	struct nexthop6 *nh = NULL;
+
+	nh = ip6_route_lookup(req->vrf_id, &req->dest);
+	if (nh == NULL)
+		return api_out(ENETUNREACH, 0);
+
+	if ((resp = calloc(1, sizeof(*resp))) == NULL)
+		return api_out(ENOMEM, 0);
+
+	resp->nh.host = nh->ip;
+	resp->nh.iface_id = nh->iface_id;
+	rte_ether_addr_copy(&nh->lladdr, &resp->nh.mac);
+	resp->nh.flags = nh->flags;
+
+	*response = resp;
+
+	return api_out(0, sizeof(*resp));
+}
+
+static struct api_out route6_list(const void *request, void **response) {
+	const struct gr_ip6_route_list_req *req = request;
+	struct gr_ip6_route_list_resp *resp = NULL;
+	struct rte_fib6 *fib = get_fib6(req->vrf_id);
+	struct rte_rib6_node *rn = NULL;
+	struct gr_ip6_route *r;
+	struct rte_rib6 *rib;
+	size_t num, len;
+	uintptr_t nh_id;
+
+	(void)request;
+
+	if (fib == NULL)
+		return api_out(errno, 0);
+
+	rib = rte_fib6_get_rib(fib);
+
+	num = 0;
+	while ((rn = rte_rib6_get_nxt(rib, 0, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL)
+		num++;
+	// FIXME: remove this when rte_rib6_get_nxt returns a default route, if any is configured
+	if (rte_rib6_lookup_exact(rib, 0, 0) != NULL)
+		num++;
+
+	len = sizeof(*resp) + num * sizeof(struct gr_ip6_route);
+	if ((resp = calloc(1, len)) == NULL)
+		return api_out(ENOMEM, 0);
+
+	while ((rn = rte_rib6_get_nxt(rib, 0, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL) {
+		r = &resp->routes[resp->n_routes++];
+		rte_rib6_get_nh(rn, &nh_id);
+		rte_rib6_get_ip(rn, &r->dest.ip);
+		rte_rib6_get_depth(rn, &r->dest.prefixlen);
+		rte_ipv6_addr_cpy(&r->nh, &nh_id_to_ptr(nh_id)->ip);
+	}
+	// FIXME: remove this when rte_rib6_get_nxt returns a default route, if any is configured
+	if ((rn = rte_rib6_lookup_exact(rib, 0, 0)) != NULL) {
+		r = &resp->routes[resp->n_routes++];
+		rte_rib6_get_nh(rn, &nh_id);
+		memset(&r->dest, 0, sizeof(r->dest));
+		rte_ipv6_addr_cpy(&r->nh, &nh_id_to_ptr(nh_id)->ip);
+	}
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static void route6_init(struct event_base *) {
+	vrf_fibs = rte_calloc(
+		__func__, IP6_MAX_VRFS, sizeof(struct rte_fib6 *), RTE_CACHE_LINE_SIZE
+	);
+	if (vrf_fibs == NULL)
+		ABORT("rte_calloc(vrf_fib6s): %s", rte_strerror(rte_errno));
+}
+
+static void route6_fini(struct event_base *) {
+	for (uint16_t vrf_id = 0; vrf_id < IP6_MAX_VRFS; vrf_id++) {
+		rte_fib6_free(vrf_fibs[vrf_id]);
+		vrf_fibs[vrf_id] = NULL;
+	}
+	rte_free(vrf_fibs);
+	vrf_fibs = NULL;
+}
+
+void ip6_route_cleanup(struct nexthop6 *nh) {
+	struct rte_ipv6_addr local_ip, ip;
+	struct rte_rib6_node *rn = NULL;
+	uint8_t depth, local_depth;
+	struct rte_rib6 *rib;
+	uintptr_t nh_id;
+
+	local_ip = nh->ip;
+	local_depth = nh->prefixlen;
+
+	rib = rte_fib6_get_rib(get_fib6(nh->vrf_id));
+	while ((rn = rte_rib6_get_nxt(rib, 0, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL) {
+		rte_rib6_get_nh(rn, &nh_id);
+		nh = nh_id_to_ptr(nh_id);
+
+		if (nh && rte_ipv6_addr_eq_prefix(&nh->ip, &local_ip, local_depth)) {
+			rte_rib6_get_ip(rn, &ip);
+			rte_rib6_get_depth(rn, &depth);
+
+			LOG(DEBUG,
+			    "delete " IPV6_ADDR_FMT "/%d via " IPV6_ADDR_FMT,
+			    IPV6_ADDR_SPLIT(&ip),
+			    depth,
+			    IPV6_ADDR_SPLIT(&nh->ip));
+
+			ip6_route_delete(nh->vrf_id, &ip, depth);
+			ip6_route_delete(nh->vrf_id, &ip, RTE_IPV6_MAX_DEPTH);
+		}
+	}
+
+	if ((rn = rte_rib6_lookup_exact(rib, 0, 0)) != NULL) {
+		rte_rib6_get_nh(rn, &nh_id);
+		nh = nh_id_to_ptr(nh_id);
+
+		if (nh && rte_ipv6_addr_eq_prefix(&nh->ip, &local_ip, local_depth)) {
+			rte_rib6_get_ip(rn, &ip);
+			rte_rib6_get_depth(rn, &depth);
+
+			LOG(DEBUG,
+			    "delete " IPV6_ADDR_FMT "/%d via " IPV6_ADDR_FMT,
+			    IPV6_ADDR_SPLIT(&ip),
+			    depth,
+			    IPV6_ADDR_SPLIT(&nh->ip));
+
+			ip6_route_delete(nh->vrf_id, &nh->ip, nh->prefixlen);
+			ip6_route_delete(nh->vrf_id, &nh->ip, RTE_IPV6_MAX_DEPTH);
+		}
+	}
+
+	ip6_route_delete(nh->vrf_id, &local_ip, RTE_IPV6_MAX_DEPTH);
+}
+
+static struct gr_api_handler route6_add_handler = {
+	.name = "ipv6 route add",
+	.request_type = GR_IP6_ROUTE_ADD,
+	.callback = route6_add,
+};
+static struct gr_api_handler route6_del_handler = {
+	.name = "ipv6 route del",
+	.request_type = GR_IP6_ROUTE_DEL,
+	.callback = route6_del,
+};
+static struct gr_api_handler route6_get_handler = {
+	.name = "ipv6 route get",
+	.request_type = GR_IP6_ROUTE_GET,
+	.callback = route6_get,
+};
+static struct gr_api_handler route6_list_handler = {
+	.name = "ipv6 route list",
+	.request_type = GR_IP6_ROUTE_LIST,
+	.callback = route6_list,
+};
+
+static struct gr_module route6_module = {
+	.name = "ipv6 route",
+	.init = route6_init,
+	.fini = route6_fini,
+	.fini_prio = 10000,
+};
+
+RTE_INIT(control_ip_init) {
+	gr_register_api_handler(&route6_add_handler);
+	gr_register_api_handler(&route6_del_handler);
+	gr_register_api_handler(&route6_get_handler);
+	gr_register_api_handler(&route6_list_handler);
+	gr_register_module(&route6_module);
+}

modules/ip6/cli/route.c
@@ -1,0 +1,172 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include "ip.h"
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_cli_iface.h>
+#include <gr_ip6.h>
+#include <gr_net_types.h>
+#include <gr_table.h>
+
+#include <ecoli.h>
+#include <libsmartcols.h>
+
+#include <errno.h>
+
+static cmd_status_t route6_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_route_add_req req = {.exist_ok = true};
+
+	if (ip6_net_parse(arg_str(p, "DEST"), &req.dest, true) < 0)
+		return CMD_ERROR;
+	if (inet_pton(AF_INET6, arg_str(p, "NH"), &req.nh) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ROUTE_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t route6_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_route_del_req req = {.missing_ok = true};
+
+	if (ip6_net_parse(arg_str(p, "DEST"), &req.dest, true) < 0)
+		return CMD_ERROR;
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ROUTE_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t route6_list(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct libscols_table *table = scols_new_table();
+	const struct gr_ip6_route_list_resp *resp;
+	struct gr_ip6_route_list_req req = {0};
+	char dest[BUFSIZ], nh[BUFSIZ];
+	void *resp_ptr = NULL;
+
+	(void)p;
+
+	if (table == NULL)
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ROUTE_LIST, sizeof(req), &req, &resp_ptr) < 0) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	resp = resp_ptr;
+	scols_table_new_column(table, "DESTINATION", 0, 0);
+	scols_table_new_column(table, "NEXT_HOP", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (size_t i = 0; i < resp->n_routes; i++) {
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		const struct gr_ip6_route *route = &resp->routes[i];
+		ip6_net_format(&route->dest, dest, sizeof(dest));
+		inet_ntop(AF_INET6, &route->nh, nh, sizeof(nh));
+		scols_line_set_data(line, 0, dest);
+		scols_line_set_data(line, 1, nh);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t route6_get(const struct gr_api_client *c, const struct ec_pnode *p) {
+	const struct gr_ip6_route_get_resp *resp;
+	struct gr_ip6_route_get_req req = {0};
+	struct gr_iface iface;
+	void *resp_ptr = NULL;
+	char buf[BUFSIZ];
+	const char *dest = arg_str(p, "DEST");
+
+	if (dest == NULL) {
+		if (errno == ENOENT)
+			return route6_list(c, p);
+		return CMD_ERROR;
+	}
+	if (inet_pton(AF_INET6, dest, &req.dest) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ROUTE_GET, sizeof(req), &req, &resp_ptr) < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+	inet_ntop(AF_INET6, &resp->nh.host, buf, sizeof(buf));
+	printf("%s via %s lladdr " ETH_ADDR_FMT, dest, buf, ETH_ADDR_SPLIT(&resp->nh.mac));
+	if (iface_from_id(c, resp->nh.iface_id, &iface) == 0)
+		printf("iface %s", iface.name);
+	else
+		printf("iface %u", resp->nh.iface_id);
+	printf("\n");
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		IP6_ADD_CTX(root),
+		"route DEST via NH [vrf VRF]",
+		route6_add,
+		"Add a new route.",
+		with_help("IPv6 destination prefix.", ec_node_re("DEST", IPV6_NET_RE)),
+		with_help("IPv6 next hop address.", ec_node_re("NH", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP6_DEL_CTX(root),
+		"route DEST [vrf VRF]",
+		route6_del,
+		"Delete a route.",
+		with_help("IPv6 destination prefix.", ec_node_re("DEST", IPV6_NET_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP6_SHOW_CTX(root),
+		"route [(destination DEST),(vrf VRF)]",
+		route6_get,
+		"Show IPv6 routes.",
+		with_help("IPv6 destination address.", ec_node_re("DEST", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "ipv6 route",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/ip6/api/gr_ip6.h
@@ -1,0 +1,181 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#ifndef _GR_IP6_MSG
+#define _GR_IP6_MSG
+
+#include <gr_api.h>
+#include <gr_bitops.h>
+#include <gr_net_types.h>
+
+#include <rte_ether.h>
+#include <rte_ip6.h>
+
+#include <stdint.h>
+
+struct gr_ip6_ifaddr {
+	uint16_t iface_id;
+	struct ip6_net addr;
+};
+
+#define GR_IP6_NH_F_PENDING GR_BIT16(0) // NDP probe sent
+#define GR_IP6_NH_F_REACHABLE GR_BIT16(1) // NDP reply received
+#define GR_IP6_NH_F_STALE GR_BIT16(2) // Reachable lifetime expired, need NDP refresh
+#define GR_IP6_NH_F_FAILED GR_BIT16(3) // All NDP probes sent without reply
+#define GR_IP6_NH_F_STATIC GR_BIT16(4) // Configured by user
+#define GR_IP6_NH_F_LOCAL GR_BIT16(5) // Local address
+#define GR_IP6_NH_F_GATEWAY GR_BIT16(6) // Gateway route
+#define GR_IP6_NH_F_LINK GR_BIT16(7) // Connected link route
+#define GR_IP6_NH_F_MCAST GR_BIT16(8) // Multicast address
+typedef uint16_t gr_ip6_nh_flags_t;
+
+static inline const char *gr_ip6_nh_f_name(const gr_ip6_nh_flags_t flag) {
+	switch (flag) {
+	case GR_IP6_NH_F_PENDING:
+		return "pending";
+	case GR_IP6_NH_F_REACHABLE:
+		return "reachable";
+	case GR_IP6_NH_F_STALE:
+		return "stale";
+	case GR_IP6_NH_F_FAILED:
+		return "failed";
+	case GR_IP6_NH_F_STATIC:
+		return "static";
+	case GR_IP6_NH_F_LOCAL:
+		return "local";
+	case GR_IP6_NH_F_GATEWAY:
+		return "gateway";
+	case GR_IP6_NH_F_LINK:
+		return "link";
+	case GR_IP6_NH_F_MCAST:
+		return "multicast";
+	}
+	return "";
+}
+
+#define GR_VRF_ID_ALL UINT16_MAX
+
+struct gr_ip6_nh {
+	struct rte_ipv6_addr host;
+	struct rte_ether_addr mac;
+	uint16_t vrf_id;
+	uint16_t iface_id;
+	gr_ip6_nh_flags_t flags;
+};
+
+struct gr_ip6_route {
+	struct ip6_net dest;
+	struct rte_ipv6_addr nh;
+};
+
+#define GR_IP6_MODULE 0xfeed
+
+// next hops ///////////////////////////////////////////////////////////////////
+
+#define GR_IP6_NH_ADD REQUEST_TYPE(GR_IP6_MODULE, 0x0001)
+
+struct gr_ip6_nh_add_req {
+	struct gr_ip6_nh nh;
+	uint8_t exist_ok;
+};
+
+// struct gr_ip6_nh_add_resp { };
+
+#define GR_IP6_NH_DEL REQUEST_TYPE(GR_IP6_MODULE, 0x0002)
+
+struct gr_ip6_nh_del_req {
+	uint16_t vrf_id;
+	struct rte_ipv6_addr host;
+	uint8_t missing_ok;
+};
+
+// struct gr_ip6_nh_del_resp { };
+
+#define GR_IP6_NH_LIST REQUEST_TYPE(GR_IP6_MODULE, 0x0003)
+
+struct gr_ip6_nh_list_req {
+	uint16_t vrf_id;
+};
+
+struct gr_ip6_nh_list_resp {
+	uint16_t n_nhs;
+	struct gr_ip6_nh nhs[/* n_nhs */];
+};
+
+// routes //////////////////////////////////////////////////////////////////////
+
+#define GR_IP6_ROUTE_ADD REQUEST_TYPE(GR_IP6_MODULE, 0x0010)
+
+struct gr_ip6_route_add_req {
+	uint16_t vrf_id;
+	struct ip6_net dest;
+	struct rte_ipv6_addr nh;
+	uint8_t exist_ok;
+};
+
+// struct gr_ip6_route_add_resp { };
+
+#define GR_IP6_ROUTE_DEL REQUEST_TYPE(GR_IP6_MODULE, 0x0011)
+
+struct gr_ip6_route_del_req {
+	uint16_t vrf_id;
+	struct ip6_net dest;
+	uint8_t missing_ok;
+};
+
+// struct gr_ip6_route_del_resp { };
+
+#define GR_IP6_ROUTE_GET REQUEST_TYPE(GR_IP6_MODULE, 0x0012)
+
+struct gr_ip6_route_get_req {
+	uint16_t vrf_id;
+	struct rte_ipv6_addr dest;
+};
+
+struct gr_ip6_route_get_resp {
+	struct gr_ip6_nh nh;
+};
+
+#define GR_IP6_ROUTE_LIST REQUEST_TYPE(GR_IP6_MODULE, 0x0013)
+
+struct gr_ip6_route_list_req {
+	uint16_t vrf_id;
+};
+
+struct gr_ip6_route_list_resp {
+	uint16_t n_routes;
+	struct gr_ip6_route routes[/* n_routes */];
+};
+
+// addresses ///////////////////////////////////////////////////////////////////
+
+#define GR_IP6_ADDR_ADD REQUEST_TYPE(GR_IP6_MODULE, 0x0021)
+
+struct gr_ip6_addr_add_req {
+	struct gr_ip6_ifaddr addr;
+	uint8_t exist_ok;
+};
+
+// struct gr_ip6_addr_add_resp { };
+
+#define GR_IP6_ADDR_DEL REQUEST_TYPE(GR_IP6_MODULE, 0x0022)
+
+struct gr_ip6_addr_del_req {
+	struct gr_ip6_ifaddr addr;
+	uint8_t missing_ok;
+};
+
+// struct gr_ip6_addr_del_resp { };
+
+#define GR_IP6_ADDR_LIST REQUEST_TYPE(GR_IP6_MODULE, 0x0023)
+
+struct gr_ip6_addr_list_req {
+	uint16_t vrf_id;
+};
+
+struct gr_ip6_addr_list_resp {
+	uint16_t n_addrs;
+	struct gr_ip6_ifaddr addrs[/* n_addrs */];
+};
+
+#endif

modules/ip6/api/meson.build
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Robin Jarry
+
+api_headers += files('gr_ip6.h')
+inc += include_directories('.')
+cli_inc += include_directories('.')

modules/ip6/cli/address.c
@@ -1,0 +1,141 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include "ip.h"
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_cli_iface.h>
+#include <gr_ip6.h>
+#include <gr_net_types.h>
+#include <gr_table.h>
+
+#include <ecoli.h>
+#include <libsmartcols.h>
+
+#include <stdint.h>
+
+static cmd_status_t addr_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_addr_add_req req = {.exist_ok = true};
+	struct gr_iface iface;
+
+	if (ip6_net_parse(arg_str(p, "IP6_NET"), &req.addr.addr, false) < 0)
+		return CMD_ERROR;
+	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
+		return CMD_ERROR;
+	req.addr.iface_id = iface.id;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ADDR_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t addr_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_addr_del_req req = {.missing_ok = true};
+	struct gr_iface iface;
+
+	if (ip6_net_parse(arg_str(p, "IP6_NET"), &req.addr.addr, false) < 0)
+		return CMD_ERROR;
+	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
+		return CMD_ERROR;
+	req.addr.iface_id = iface.id;
+
+	if (gr_api_client_send_recv(c, GR_IP6_ADDR_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t addr_list(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct libscols_table *table = scols_new_table();
+	const struct gr_ip6_addr_list_resp *resp;
+	struct gr_ip6_addr_list_req req = {0};
+	struct gr_iface iface;
+
+	void *resp_ptr = NULL;
+	char buf[BUFSIZ];
+
+	(void)p;
+
+	if (table == NULL)
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	if (gr_api_client_send_recv(c, GR_IP6_ADDR_LIST, sizeof(req), &req, &resp_ptr) < 0) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	resp = resp_ptr;
+
+	scols_table_new_column(table, "IFACE", 0, 0);
+	scols_table_new_column(table, "ADDRESS", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (size_t i = 0; i < resp->n_addrs; i++) {
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		const struct gr_ip6_ifaddr *addr = &resp->addrs[i];
+		ip6_net_format(&addr->addr, buf, sizeof(buf));
+		if (iface_from_id(c, addr->iface_id, &iface) == 0)
+			scols_line_sprintf(line, 0, "%s", iface.name);
+		else
+			scols_line_sprintf(line, 0, "%u", addr->iface_id);
+		scols_line_sprintf(line, 1, "%s", buf);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		IP6_ADD_CTX(root),
+		"address IP6_NET iface IFACE",
+		addr_add,
+		"Add an IPv6 address to an interface.",
+		with_help("IPv6 address with prefix length.", ec_node_re("IP6_NET", IPV6_NET_RE)),
+		with_help("Interface name.", ec_node_dyn("IFACE", complete_iface_names, NULL))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP6_DEL_CTX(root),
+		"address IP6_NET iface IFACE",
+		addr_del,
+		"Remove an IPv6 address from an interface.",
+		with_help("IPv6 address with prefix length.", ec_node_re("IP6_NET", IPV6_NET_RE)),
+		with_help("Interface name.", ec_node_dyn("IFACE", complete_iface_names, NULL))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP6_SHOW_CTX(root),
+		"address [vrf VRF]",
+		addr_list,
+		"Display all IPv6 addresses.",
+		with_help("L3 addressing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "ipv6 address",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/ip6/cli/ip.h
@@ -1,0 +1,13 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#ifndef _GR_CLI_IP
+#define _GR_CLI_IP
+
+#include <gr_cli.h>
+
+#define IP6_ADD_CTX(root) CLI_CONTEXT(root, CTX_ADD, CTX_ARG("ip6", "Create IPv6 stack elements."))
+#define IP6_DEL_CTX(root) CLI_CONTEXT(root, CTX_DEL, CTX_ARG("ip6", "Delete IPv6 stack elements."))
+#define IP6_SHOW_CTX(root) CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("ip6", "Show IPv6 stack details."))
+
+#endif

modules/ip6/cli/meson.build
@@ -1,0 +1,8 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 Robin Jarry
+
+cli_src += files(
+  'address.c',
+  'nexthop.c',
+  'route.c',
+)

commit fbe3926fbfaaf9b42f35506a6bdc29bc480fb540

ip6: fix show route
rte_rib6_get_nxt() needs an ipv6 prefix in the form of a pointer to
a masked rte_ipv6_addr. 0 is seen as NULL and both rte_rib6_lookup_exact
and rte_rib6_get_nxt return an error which is silently ignored.

Pass a proper zero address along with a 0 prefix length which means
"all" the IPv6 addressing space.

Fixes: 0fc1cd222b8e ("modules: add static ipv6 forwarding support")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/route.c
@@ -260,6 +260,7 @@ static struct api_out route6_list(const void *request, void **response) {
 	struct gr_ip6_route_list_resp *resp = NULL;
 	struct rte_fib6 *fib = get_fib6(req->vrf_id);
 	struct rte_rib6_node *rn = NULL;
+	struct rte_ipv6_addr zero = {0};
 	struct gr_ip6_route *r;
 	struct rte_rib6 *rib;
 	size_t num, len;
@@ -273,17 +274,17 @@ static struct api_out route6_list(const void *request, void **response) {
 	rib = rte_fib6_get_rib(fib);
 
 	num = 0;
-	while ((rn = rte_rib6_get_nxt(rib, 0, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL)
+	while ((rn = rte_rib6_get_nxt(rib, &zero, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL)
 		num++;
 	// FIXME: remove this when rte_rib6_get_nxt returns a default route, if any is configured
-	if (rte_rib6_lookup_exact(rib, 0, 0) != NULL)
+	if (rte_rib6_lookup_exact(rib, &zero, 0) != NULL)
 		num++;
 
 	len = sizeof(*resp) + num * sizeof(struct gr_ip6_route);
 	if ((resp = calloc(1, len)) == NULL)
 		return api_out(ENOMEM, 0);
 
-	while ((rn = rte_rib6_get_nxt(rib, 0, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL) {
+	while ((rn = rte_rib6_get_nxt(rib, &zero, 0, rn, RTE_RIB6_GET_NXT_ALL)) != NULL) {
 		r = &resp->routes[resp->n_routes++];
 		rte_rib6_get_nh(rn, &nh_id);
 		rte_rib6_get_ip(rn, &r->dest.ip);
@@ -291,7 +292,7 @@ static struct api_out route6_list(const void *request, void **response) {
 		rte_ipv6_addr_cpy(&r->nh, &nh_id_to_ptr(nh_id)->ip);
 	}
 	// FIXME: remove this when rte_rib6_get_nxt returns a default route, if any is configured
-	if ((rn = rte_rib6_lookup_exact(rib, 0, 0)) != NULL) {
+	if ((rn = rte_rib6_lookup_exact(rib, &zero, 0)) != NULL) {
 		r = &resp->routes[resp->n_routes++];
 		rte_rib6_get_nh(rn, &nh_id);
 		memset(&r->dest, 0, sizeof(r->dest));

smoke/_init.sh
@@ -33,7 +33,9 @@ cat > $tmp/cleanup <<EOF
 grcli show stats software
 grcli show interface all
 grcli show ip nexthop
+grcli show ip route
 grcli show ip6 nexthop
+grcli show ip6 route
 EOF
 
 set -x