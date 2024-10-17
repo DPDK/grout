DPDK/grout

commit a0f9d6699605fc0b5e058696a425a8a9f743dcc3

iface: forbid duplicate names on creation
The name is checked for duplicates on `set interface` but not when
creating a new interface. Return an explicit error when trying to create
an interface with a name that is already taken.

Fixes: a188078583cf ("infra: replace port with interface abstraction")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

modules/infra/control/iface.c
@@ -77,6 +77,12 @@ struct iface *iface_create(
 		goto fail;
 	if (utf8_check(name, GR_IFACE_NAME_SIZE) < 0)
 		goto fail;
+	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
+		if (strcmp(name, iface->name) == 0) {
+			errno = EEXIST;
+			goto fail;
+		}
+	}
 
 	iface = rte_zmalloc(__func__, sizeof(*iface) + type->info_size, RTE_CACHE_LINE_SIZE);
 	if (iface == NULL) {

commit e60caf71b02f3d53e8e568085285919a27ed3fce

iface: avoid use after free on duplicate name
When an interface name is already taken, we return an error but the
iface pointer is set the already existing interface. Before returning
from iface_create, that pointer is freed without removing the interface
from the system.

When listing interfaces or accessing anything related to interfaces
after that causes an invalid memory access.

Reset iface to NULL before returning an error to avoid that.

Fixes: a0f9d6699605 ("iface: forbid duplicate names on creation")
Reported-by: Christophe Fontaine <cfontain@redhat.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/iface.c
@@ -79,6 +79,7 @@ struct iface *iface_create(
 		goto fail;
 	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
 		if (strcmp(name, iface->name) == 0) {
+			iface = NULL;
 			errno = EEXIST;
 			goto fail;
 		}