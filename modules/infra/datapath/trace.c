ec6a3552f7a61178b35f425e16eb91a69042196c
Robin Jarry <robin@jarry.cc>
2024-10-23 11:33:26 +0200

    trace: add ip proto names and numeric values
    For better readability, print the most common IP protocol names and
    values after the ttl. Add a dedicated function to format protocol values
    to a string. The names come from /etc/protocols.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>
    Acked-by: David Marchand <david.marchand@redhat.com>

--- modules/infra/datapath/trace.c
+++ modules/infra/datapath/trace.c
@@ -53,6 +53,55 @@ int eth_type_format(char *buf, size_t len, rte_be16_t type) {
 	return snprintf(buf, len, "0x%04x", rte_be_to_cpu_16(type));
 }
 
+static inline const char *ip_proto_str(uint8_t proto) {
+	switch (proto) {
+	case IPPROTO_HOPOPTS:
+		return "HOPOPT";
+	case IPPROTO_ICMP:
+		return "ICMP";
+	case IPPROTO_IGMP:
+		return "IGMP";
+	case IPPROTO_IPIP:
+		return "IPIP";
+	case IPPROTO_TCP:
+		return "TCP";
+	case IPPROTO_UDP:
+		return "UDP";
+	case IPPROTO_IPV6:
+		return "IPv6";
+	case IPPROTO_ROUTING:
+		return "RouteOpts";
+	case IPPROTO_FRAGMENT:
+		return "FragOpts";
+	case IPPROTO_GRE:
+		return "GRE";
+	case IPPROTO_ESP:
+		return "ESP";
+	case IPPROTO_AH:
+		return "AH";
+	case IPPROTO_MTP:
+		return "MTP";
+	case IPPROTO_ICMPV6:
+		return "ICMPv6";
+	case IPPROTO_NONE:
+		return "NoNext";
+	case IPPROTO_DSTOPTS:
+		return "DstOpts";
+	case IPPROTO_SCTP:
+		return "SCTP";
+	case IPPROTO_RAW:
+		return "Raw";
+	}
+	return NULL;
+}
+
+static int ip_proto_format(char *buf, size_t len, uint8_t proto) {
+	const char *str = ip_proto_str(proto);
+	if (str)
+		return snprintf(buf, len, "%s(%d)", str, proto);
+	return snprintf(buf, len, "%d", proto);
+}
+
 static ssize_t trace_icmp6(
 	char *buf,
 	const size_t len,
@@ -84,24 +133,35 @@ int trace_arp_format(char *buf, size_t len, const struct rte_arp_hdr *arp, size_
 }
 
 int trace_ip_format(char *buf, size_t len, const struct rte_ipv4_hdr *ip, size_t /*data_len*/) {
-	return snprintf(
-		buf,
+	size_t n = 0;
+	SAFE_BUF(
+		snprintf,
 		len,
-		IP4_ADDR_FMT " > " IP4_ADDR_FMT " ttl=%hhu",
+		IP4_ADDR_FMT " > " IP4_ADDR_FMT " ttl=%hhu proto=",
 		IP4_ADDR_SPLIT(&ip->src_addr),
 		IP4_ADDR_SPLIT(&ip->dst_addr),
 		ip->time_to_live
 	);
+	SAFE_BUF(ip_proto_format, len, ip->next_proto_id);
+	return n;
+err:
+	return -1;
 }
 
 int trace_ip6_format(char *buf, size_t len, const struct rte_ipv6_hdr *ip6, size_t /*data_len*/) {
 	char src[INET6_ADDRSTRLEN];
 	char dst[INET6_ADDRSTRLEN];
+	size_t n = 0;
 
 	inet_ntop(AF_INET6, &ip6->src_addr, src, sizeof(src));
 	inet_ntop(AF_INET6, &ip6->dst_addr, dst, sizeof(dst));
 
-	return snprintf(buf, len, "%s > %s ttl=%hhu", src, dst, ip6->hop_limits);
+	SAFE_BUF(snprintf, len, "%s > %s ttl=%hhu proto=", src, dst, ip6->hop_limits);
+	SAFE_BUF(ip_proto_format, len, ip6->proto);
+
+	return n;
+err:
+	return -1;
 }
 
 void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *iface) {
@@ -179,9 +239,6 @@ ipv4:
 		}
 		case IPPROTO_IPIP:
 			goto ipv4;
-		default:
-			SAFE_BUF(snprintf, sizeof(buf), " proto=%hhu", ip->next_proto_id);
-			break;
 		}
 
 		break;
@@ -219,9 +276,6 @@ ipv4:
 		switch (proto) {
 		case IPPROTO_ICMPV6:
 			SAFE_BUF(trace_icmp6, sizeof(buf), m, &offset, payload_len);
-			break;
-		default:
-			SAFE_BUF(snprintf, sizeof(buf), " nh=%hhu", proto);
 			break;
 		}
 
