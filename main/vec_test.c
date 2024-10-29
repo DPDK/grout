2a1a0e0404cc29d3a1e8c7755bdd6a6d34e8776d
Robin Jarry <robin@jarry.cc>
2024-10-29 15:12:19 +0100

    main: implement basic dynamic array utilities
    We currently depend on stb_ds.h for certain non-critical code paths.
    This lib is not available in any distribution and basically would
    require grout to vendor it. Since it provides a lot more features that
    we actually need. Implement a stripped down version of it. Do not try to
    implement any of the hash map features. We do not need them.
    
    Add basic unit tests.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>
    Acked-by: Christophe Fontaine <cfontain@redhat.com>

---
+++ main/vec_test.c
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <gr_cmocka.h>
+#include <gr_vec.h>
+
+static void int_vec(void **) {
+	int *vec = NULL;
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	for (int i = 0; i < 5; i++)
+		gr_vec_add(vec, i);
+
+	assert_int_equal(gr_vec_len(vec), 5);
+
+	for (int i = 0; i < 5; i++)
+		assert_int_equal(vec[i], i);
+
+	gr_vec_insert(vec, 1, 42);
+	assert_int_equal(gr_vec_len(vec), 6);
+	assert_int_equal(vec[0], 0);
+	assert_int_equal(vec[1], 42);
+	assert_int_equal(vec[2], 1);
+	assert_int_equal(vec[3], 2);
+	assert_int_equal(vec[4], 3);
+	assert_int_equal(vec[5], 4);
+
+	gr_vec_del_swap(vec, 3);
+	assert_int_equal(gr_vec_len(vec), 5);
+
+	assert_int_equal(gr_vec_pop(vec), 3);
+	assert_int_equal(gr_vec_pop(vec), 4);
+	assert_int_equal(gr_vec_pop(vec), 1);
+	assert_int_equal(gr_vec_pop(vec), 42);
+	assert_int_equal(gr_vec_pop(vec), 0);
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	gr_vec_free(vec);
+}
+
+static void str_vec(void **) {
+	const char **vec = NULL;
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	gr_vec_add(vec, "foo");
+	gr_vec_add(vec, "bar");
+	gr_vec_add(vec, "baz");
+
+	assert_int_equal(gr_vec_len(vec), 3);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "bar");
+	assert_string_equal(vec[2], "baz");
+
+	gr_vec_del(vec, 1);
+
+	assert_int_equal(gr_vec_len(vec), 2);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "baz");
+
+	gr_vec_insert(vec, 2, "bar");
+	assert_int_equal(gr_vec_len(vec), 3);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "baz");
+	assert_string_equal(vec[2], "bar");
+
+	gr_vec_del_swap(vec, 0);
+	assert_int_equal(gr_vec_len(vec), 2);
+	assert_string_equal(vec[0], "bar");
+	assert_string_equal(vec[1], "baz");
+
+	gr_vec_free(vec);
+}
+
+int main(void) {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(int_vec),
+		cmocka_unit_test(str_vec),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
