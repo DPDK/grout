DPDK/grout

commit bc04bf4e328c3179b44b72e9d4ee7f39788b4fdc

infra: add packet tracing framework
Introduce a way to attach per-node packet tracing to mbufs. Nodes can
append a new trace item using gr_mbuf_trace_add(). Trace items contain
the node id, processing CPU id, a time stamp and an opaque buffer to
store tracing data.

The buffer returned by gr_mbuf_trace_add() should contain stateless data
(e.g. no pointers) to avoid keeping references on objects that may
become invalid in the future.

Allocate trace items from a mempool and link them together using a tail
queue. Store the head of the tail queue in the mbuf private data. To
avoid any overlap with existing private data, shift them after the tail
queue head and update functions to access private data accordingly.

Source nodes must check if they should begin tracing a particular mbuf
by checking if the GR_IFACE_F_PACKET_TRACE flag is set on the receiving
interface. Other nodes can then rely on gr_mbuf_is_traced() to determine
if a given mbuf already contains trace items from previous nodes and
add their own contribution to the list.

Sink nodes should add their trace items and detach them from the mbuf
with gr_mbuf_trace_finish() before the mbuf is freed. This function will
enqueue the trace item tail queue into a ring waiting for control plane
to flush it.

If no items are available or if the ring is full, gr_mbuf_trace_add()
and gr_mbuf_trace_finish() will dequeue oldest traces from the ring to
make more available.

Here is a simplified representation of the trace ring after successive
calls of gr_mbuf_trace_finish(). Packets in parentheses are only there
for context. Mbufs are not stored in the ring.

  -------------------------------------------------------------------
  head                                            tail
  -------------------------------------------------------------------
  (packet 1)      (packet 2)      (packet 3)      (packet 4)

  trace node1     trace node1     trace node1     trace node1

modules/infra/datapath/trace.c
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Robin Jarry
 
+#include <gr_graph.h>
 #include <gr_icmp6.h>
 #include <gr_log.h>
+#include <gr_mbuf.h>
+#include <gr_module.h>
 #include <gr_net_types.h>
 #include <gr_trace.h>
 
@@ -10,11 +13,14 @@
 #include <rte_byteorder.h>
 #include <rte_errno.h>
 #include <rte_ether.h>
+#include <rte_graph.h>
 #include <rte_graph_worker.h>
 #include <rte_icmp.h>
 #include <rte_ip.h>
 #include <rte_ip6.h>
 #include <rte_mbuf.h>
+#include <rte_mempool.h>
+#include <rte_ring.h>
 
 static ssize_t trace_icmp6(
 	char *buf,
@@ -338,4 +344,150 @@ static ssize_t trace_icmp6(
 	return n;
 err:
 	return -1;
+}
+
+#define PACKET_COUNT_MAX RTE_GRAPH_BURST_SIZE
+
+static struct rte_mempool *trace_pool;
+static struct rte_ring *traced_packets;
+
+static void free_trace(struct gr_trace_item *t) {
+	// free the whole chain of trace items
+	while (t != NULL) {
+		struct gr_trace_item *next = STAILQ_NEXT(t, next);
+		rte_mempool_put(trace_pool, t);
+		t = next;
+	}
+}
+
+void *gr_mbuf_trace_add(struct rte_mbuf *m, struct rte_node *node, size_t data_len) {
+	struct gr_trace_head *traces = gr_mbuf_traces(m);
+	struct gr_trace_item *trace;
+	void *data;
+
+	// XXX: should we always abort even if -DNDEBUG is defined?
+	assert(data_len <= GR_TRACE_ITEM_MAX_LEN);
+
+	while (rte_mempool_get(trace_pool, &data) < 0) {
+		void *oldest = NULL;
+		rte_ring_dequeue(traced_packets, &oldest);
+		free_trace(oldest);
+	}
+
+	trace = data;
+	trace->node_id = node->id;
+	trace->len = data_len;
+
+	if (STAILQ_EMPTY(traces)) {
+		clock_gettime(CLOCK_REALTIME_COARSE, &trace->ts);
+		trace->cpu_id = rte_lcore_id();
+		STAILQ_INSERT_HEAD(traces, trace, next);
+	} else {
+		STAILQ_INSERT_TAIL(traces, trace, next);
+	}
+
+	return trace->data;
+}
+
+void gr_mbuf_trace_finish(struct rte_mbuf *m) {
+	struct gr_trace_head *traces = gr_mbuf_traces(m);
+	struct gr_trace_item *trace = STAILQ_FIRST(traces);
+
+	if (trace == NULL)
+		return;
+
+	while (rte_ring_enqueue(traced_packets, trace) == -ENOBUFS) {
+		void *oldest = NULL;
+		rte_ring_dequeue(traced_packets, &oldest);
+		free_trace(oldest);
+	}
+
+	// Reset trace head to NULL to remove all references to the trace items.
+	// This is also to ensure that reusing this mbuf will find traces disabled.
+	STAILQ_INIT(traces);
+}
+
+int gr_trace_dump(char *buf, size_t len) {
+	const struct gr_node_info *info;
+	struct gr_trace_item *t, *head;
+	struct tm tm;
+	void *data;
+	int n = 0;
+
+	if (rte_ring_dequeue(traced_packets, &data) == 0) {
+		t = data;
+		head = t;
+
+		gmtime_r(&t->ts.tv_sec, &tm);
+		n += strftime(buf + n, len - n, "--------- %H:%M:%S.", &tm);
+		SAFE_BUF(snprintf, len, "%09lu", t->ts.tv_nsec);
+		SAFE_BUF(snprintf, len, " cpu %u ---------\n", t->cpu_id);
+
+		while (t) {
+			SAFE_BUF(snprintf, len, "%s:", rte_node_id_to_name(t->node_id));
+			if ((info = gr_node_info_get(t->node_id)) != NULL && info->trace_format) {
+				SAFE_BUF(snprintf, len, " ");
+				n += info->trace_format(buf + n, len - n, t->data, t->len);
+			}
+			SAFE_BUF(snprintf, len, "\n");
+
+			t = STAILQ_NEXT(t, next);
+		}
+		free_trace(head);
+		// add empty line to separate packets
+		SAFE_BUF(snprintf, len, "\n");
+	}
+
+	return n;
+err:
+	return -1;
+}
+
+void gr_trace_clear(void) {
+	void *trace;
+	while (rte_ring_dequeue(traced_packets, &trace) == 0)
+		free_trace(trace);
+}
+
+static void trace_init(struct event_base *) {
+	trace_pool = rte_mempool_create(
+		"trace_items", // name
+		rte_align32pow2(PACKET_COUNT_MAX * 128) - 1,
+		sizeof(struct gr_trace_item),
+		0, // cache size
+		0, // priv size
+		NULL, // mp_init
+		NULL, // mp_init_arg
+		NULL, // obj_init
+		NULL, // obj_init_arg
+		SOCKET_ID_ANY,
+		0 // flags
+	);
+	if (trace_pool == NULL)
+		ABORT("rte_mempool_create(trace_items) failed");
+	traced_packets = rte_ring_create(
+		"traced_packets",
+		PACKET_COUNT_MAX,
+		SOCKET_ID_ANY,
+		RING_F_MP_RTS_ENQ | RING_F_SC_DEQ // flags
+	);
+
+	if (traced_packets == NULL)
+		ABORT("rte_ring_create(traced_packets) failed");
+}
+
+static void trace_fini(struct event_base *) {
+	gr_trace_clear();
+	rte_mempool_free(trace_pool);
+	rte_ring_free(traced_packets);
+}
+
+static struct gr_module trace_module = {
+	.name = "trace",
+	.init = trace_init,
+	.fini = trace_fini,
+};
+
+RTE_INIT(trace_constructor) {
+	gr_register_module(&trace_module);
 }

modules/infra/api/gr_infra.h
@@ -25,6 +25,7 @@
 #define GR_IFACE_F_UP GR_BIT16(0)
 #define GR_IFACE_F_PROMISC GR_BIT16(1)
 #define GR_IFACE_F_ALLMULTI GR_BIT16(2)
+#define GR_IFACE_F_PACKET_TRACE GR_BIT16(3)
 // Interface state flags
 #define GR_IFACE_S_RUNNING GR_BIT16(0)
 

modules/infra/control/gr_graph.h
@@ -4,6 +4,8 @@
 #ifndef _GR_INFRA_GRAPH
 #define _GR_INFRA_GRAPH
 
+#include <gr_trace.h>
+
 #include <rte_common.h>
 #include <rte_graph.h>
 
@@ -23,6 +25,7 @@ struct gr_node_info {
 	struct rte_node_register *node;
 	gr_node_register_cb_t register_callback;
 	gr_node_register_cb_t unregister_callback;
+	gr_trace_format_cb_t trace_format;
 	STAILQ_ENTRY(gr_node_info) next;
 };
 

modules/infra/datapath/gr_mbuf.h
@@ -5,17 +5,62 @@
 #define _GR_MBUF
 
 #include <rte_build_config.h>
+#include <rte_graph.h>
 #include <rte_mbuf.h>
+
+#include <sys/queue.h>
+
+#define GR_TRACE_ITEM_MAX_LEN 256
+
+struct gr_trace_item {
+	STAILQ_ENTRY(gr_trace_item) next;
+	struct timespec ts;
+	unsigned cpu_id;
+	rte_node_t node_id;
+	uint8_t len;
+	uint8_t data[GR_TRACE_ITEM_MAX_LEN];
+};
+
+STAILQ_HEAD(gr_trace_head, gr_trace_item);
 
 #define GR_MBUF_PRIV_MAX_SIZE RTE_CACHE_LINE_MIN_SIZE
 
 #define GR_MBUF_PRIV_DATA_TYPE(type_name, fields)                                                  \
 	struct type_name fields;                                                                   \
+	struct __##type_name {                                                                     \
+		struct gr_trace_head traces;                                                       \
+		struct type_name data;                                                             \
+	};                                                                                         \
 	static inline struct type_name *type_name(struct rte_mbuf *m) {                            \
-		static_assert(sizeof(struct type_name) <= GR_MBUF_PRIV_MAX_SIZE);                  \
-		return rte_mbuf_to_priv(m);                                                        \
+		static_assert(sizeof(struct __##type_name) <= GR_MBUF_PRIV_MAX_SIZE);              \
+		struct __##type_name *priv = rte_mbuf_to_priv(m);                                  \
+		return &priv->data;                                                                \
 	}
 
 GR_MBUF_PRIV_DATA_TYPE(queue_mbuf_data, { struct rte_mbuf *next; });
+
+// Get the head of trace items from an mbuf.
+static inline struct gr_trace_head *gr_mbuf_traces(struct rte_mbuf *m) {
+	return rte_mbuf_to_priv(m);
+}
+
+// Return true the mbuf already contains trace items.
+static inline bool gr_mbuf_is_traced(struct rte_mbuf *m) {
+	return !STAILQ_EMPTY(gr_mbuf_traces(m));
+}
+
+// Append a trace item to an mbuf.
+//
+// If the mbuf didn't contain any traces, store it as the first one and record
+// the current time into it.
+//
+// This cannot fail. If there are no free trace items available, the trace
+// buffer will be emptied starting from the oldest until one can be returned.
+//
+// Returns a pointer to a gr_trace_item.data buffer.
+void *gr_mbuf_trace_add(struct rte_mbuf *m, struct rte_node *node, size_t data_len);
+
+// Detach the trace items from an mbuf and store them in the trace buffer.
+void gr_mbuf_trace_finish(struct rte_mbuf *m);
 
 #endif

modules/infra/datapath/gr_trace.h
@@ -5,6 +5,7 @@
 #ifndef _GR_INFRA_PACKET_TRACE
 #define _GR_INFRA_PACKET_TRACE
 
+#include <rte_graph.h>
 #include <rte_mbuf.h>
 
 // Call a function writing on a buffer called 'buf'.
@@ -26,5 +27,16 @@
 
 // Write a log message with detailed packet information.
 void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *iface);
+
+// Callback associated with each node that will be invoked by gr_trace_dump
+// to format each individual trace items.
+typedef int (*gr_trace_format_cb_t)(char *buf, size_t buf_len, const void *data, size_t data_len);
+
+// Format the buffered trace items and empty the buffer.
+// Return the number of bytes written to buffer or a negative value on error.
+int gr_trace_dump(char *buf, size_t buf_len);
+
+// Empty the trace buffer.
+void gr_trace_clear(void);
 
 #endif

commit 5611a164cc8d2f00b6928dd1cff816c7c76090d8

trace: fix dead lock on control plane thread
The packet trace ring is initialized assuming that only the control
plane thread can dequeue traces (RING_F_SC_DEQ for single consumer).
Unfortunately, the data plane threads can also dequeue traces from the
ring when it gets full and they need to free items to allocate new
packet traces. This can cause random dead locks of the control plane
thread when the data plan continuously enqueues and dequeues items.

Change the ring flags to handle multi consumers as well.

Fixes: bc04bf4e328c ("infra: add packet tracing framework")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/datapath/trace.c
@@ -593,7 +593,7 @@ static void trace_init(struct event_base *) {
 		"traced_packets",
 		PACKET_COUNT_MAX,
 		SOCKET_ID_ANY,
-		RING_F_MP_RTS_ENQ | RING_F_SC_DEQ // flags
+		RING_F_MP_RTS_ENQ | RING_F_MC_RTS_DEQ
 	);
 
 	if (traced_packets == NULL)
@@ -601,9 +601,8 @@ static void trace_init(struct event_base *) {
 }
 
 static void trace_fini(struct event_base *) {
-	gr_trace_clear();
-	rte_mempool_free(trace_pool);
 	rte_ring_free(traced_packets);
+	rte_mempool_free(trace_pool);
 }
 
 static struct gr_module trace_module = {