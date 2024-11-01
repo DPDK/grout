DPDK/grout

commit a188078583cfbd2a81489e7bbac6d55d38c7cb4b

infra: replace port with interface abstraction
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/cli/iface.c
@@ -1,0 +1,324 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2023 Robin Jarry
+
+#include "br_cli_iface.h"
+
+#include <br_api.h>
+#include <br_cli.h>
+#include <br_infra.h>
+#include <br_net_types.h>
+#include <br_string.h>
+#include <br_table.h>
+
+#include <ecoli.h>
+#include <libsmartcols.h>
+
+#include <errno.h>
+#include <sys/queue.h>
+
+static STAILQ_HEAD(, cli_iface_type) types = STAILQ_HEAD_INITIALIZER(types);
+
+void register_iface_type(struct cli_iface_type *type) {
+	STAILQ_INSERT_TAIL(&types, type, next);
+}
+
+const struct cli_iface_type *type_from_name(const char *name) {
+	const struct cli_iface_type *type;
+
+	if (name == NULL) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	STAILQ_FOREACH (type, &types, next) {
+		if (strcmp(type->name, name) == 0)
+			return type;
+	}
+	errno = ENODEV;
+	return NULL;
+}
+
+int complete_iface_types(
+	const struct br_api_client *c,
+	const struct ec_node *node,
+	struct ec_comp *comp,
+	const char *arg,
+	void *cb_arg
+) {
+	const struct cli_iface_type *type;
+
+	(void)c;
+	(void)cb_arg;
+
+	STAILQ_FOREACH (type, &types, next) {
+		if (!ec_str_startswith(type->name, arg))
+			continue;
+		if (!ec_comp_add_item(comp, node, EC_COMP_FULL, arg, type->name))
+			return -1;
+	}
+	return 0;
+}
+const struct cli_iface_type *type_from_id(uint16_t type_id) {
+	const struct cli_iface_type *type;
+
+	STAILQ_FOREACH (type, &types, next) {
+		if (type->type_id == type_id)
+			return type;
+	}
+	errno = ENODEV;
+	return NULL;
+}
+
+int complete_iface_names(
+	const struct br_api_client *c,
+	const struct ec_node *node,
+	struct ec_comp *comp,
+	const char *arg,
+	void *cb_arg
+) {
+	struct br_infra_iface_list_req req = {.type = (uintptr_t)cb_arg};
+	const struct br_infra_iface_list_resp *resp;
+	void *resp_ptr = NULL;
+	int ret = -1;
+
+	if (br_api_client_send_recv(c, BR_INFRA_IFACE_LIST, sizeof(req), &req, &resp_ptr) < 0)
+		goto fail;
+
+	resp = resp_ptr;
+
+	for (uint16_t i = 0; i < resp->n_ifaces; i++) {
+		const struct br_iface *iface = &resp->ifaces[i];
+		if (!ec_str_startswith(iface->name, arg))
+			continue;
+		if (!ec_comp_add_item(comp, node, EC_COMP_FULL, arg, iface->name))
+			goto fail;
+	}
+
+	ret = 0;
+fail:
+	free(resp_ptr);
+	return ret;
+}
+
+int iface_from_name(const struct br_api_client *c, const char *name, struct br_iface *iface) {
+	struct br_infra_iface_list_req req = {.type = BR_IFACE_TYPE_UNDEF};
+	const struct br_infra_iface_list_resp *resp;
+	void *resp_ptr = NULL;
+	int ret = -1;
+
+	if (name == NULL) {
+		errno = EINVAL;
+		goto out;
+	}
+
+	if (br_api_client_send_recv(c, BR_INFRA_IFACE_LIST, sizeof(req), &req, &resp_ptr) < 0)
+		goto out;
+
+	resp = resp_ptr;
+	for (uint16_t i = 0; i < resp->n_ifaces; i++) {
+		const struct br_iface *iter = &resp->ifaces[i];
+		if (strcmp(iter->name, name) == 0) {
+			memcpy(iface, iter, sizeof(*iface));
+			ret = 0;
+			goto out;
+		}
+	}
+
+	errno = ENODEV;
+out:
+	free(resp_ptr);
+	return ret;
+}
+
+int iface_from_id(const struct br_api_client *c, uint16_t iface_id, struct br_iface *iface) {
+	struct br_infra_iface_get_req req = {.iface_id = iface_id};
+	const struct br_infra_iface_get_resp *resp;
+	void *resp_ptr = NULL;
+
+	if (br_api_client_send_recv(c, BR_INFRA_IFACE_GET, sizeof(req), &req, &resp_ptr) < 0)
+		return -1;
+
+	resp = resp_ptr;
+	memcpy(iface, &resp->iface, sizeof(*iface));
+	free(resp_ptr);
+
+	return 0;
+}
+
+static cmd_status_t iface_del(const struct br_api_client *c, const struct ec_pnode *p) {
+	struct br_infra_iface_del_req req;
+	struct br_iface iface;
+
+	if (iface_from_name(c, arg_str(p, "NAME"), &iface) < 0)
+		return CMD_ERROR;
+
+	req.iface_id = iface.id;
+
+	if (br_api_client_send_recv(c, BR_INFRA_IFACE_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static int iface_order(const void *ia, const void *ib) {
+	const struct br_iface *a = ia;
+	const struct br_iface *b = ib;
+	return a->id - b->id;
+}
+
+static cmd_status_t iface_list(const struct br_api_client *c, const struct ec_pnode *p) {
+	struct libscols_table *table = scols_new_table();
+	struct br_infra_iface_list_resp *resp;
+	struct br_infra_iface_list_req req;
+	const struct cli_iface_type *type;
+	void *resp_ptr = NULL;
+
+	if (table == NULL)
+		return CMD_ERROR;
+
+	type = type_from_name(arg_str(p, "TYPE"));
+	if (type == NULL)
+		req.type = BR_IFACE_TYPE_UNDEF;
+	else
+		req.type = type->type_id;
+
+	if (br_api_client_send_recv(c, BR_INFRA_IFACE_LIST, sizeof(req), &req, &resp_ptr) < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+	qsort(resp->ifaces, resp->n_ifaces, sizeof(*resp->ifaces), iface_order);
+
+	scols_table_new_column(table, "NAME", 0, 0);
+	scols_table_new_column(table, "ID", 0, 0);
+	scols_table_new_column(table, "FLAGS", 0, 0);
+	scols_table_new_column(table, "TYPE", 0, 0);
+	scols_table_new_column(table, "INFO", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (size_t i = 0; i < resp->n_ifaces; i++) {
+		const struct br_iface *iface = &resp->ifaces[i];
+		const struct cli_iface_type *type = type_from_id(iface->type);
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		char buf[BUFSIZ];
+		size_t n = 0;
+
+		// name
+		scols_line_set_data(line, 0, iface->name);
+
+		// id
+		scols_line_sprintf(line, 1, "%u", iface->id);
+
+		// flags
+		if (iface->flags & BR_IFACE_F_UP)
+			n += snprintf(buf + n, sizeof(buf) - n, "up");
+		else
+			n += snprintf(buf + n, sizeof(buf) - n, "down");
+		if (iface->state & BR_IFACE_S_RUNNING)
+			n += snprintf(buf + n, sizeof(buf) - n, " running");
+		if (iface->flags & BR_IFACE_F_PROMISC)
+			n += snprintf(buf + n, sizeof(buf) - n, " promisc");
+		if (iface->flags & BR_IFACE_F_ALLMULTI)
+			n += snprintf(buf + n, sizeof(buf) - n, " allmulti");
+		scols_line_set_data(line, 2, buf);
+
+		// type
+		if (type == NULL) {
+			scols_line_sprintf(line, 3, "%u", iface->type);
+			// info
+			scols_line_set_data(line, 4, "");
+		} else {
+			scols_line_set_data(line, 3, type->name);
+			// info
+			type->list_info(iface, buf, sizeof(buf));
+			scols_line_set_data(line, 4, buf);
+		}
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t iface_show(const struct br_api_client *c, const struct ec_pnode *p) {
+	const struct cli_iface_type *type;
+	struct br_iface iface;
+
+	if (arg_str(p, "all") != NULL || arg_str(p, "TYPE") != NULL)
+		return iface_list(c, p);
+
+	if (iface_from_name(c, arg_str(p, "NAME"), &iface) < 0)
+		return CMD_ERROR;
+
+	type = type_from_id(iface.type);
+
+	printf("name: %s\n", iface.name);
+	printf("id: %u\n", iface.id);
+	printf("flags: ");
+	if (iface.flags & BR_IFACE_F_UP)
+		printf("up");
+	else
+		printf("down");
+	if (iface.state & BR_IFACE_S_RUNNING)
+		printf(" running");
+	if (iface.flags & BR_IFACE_F_PROMISC)
+		printf(" promisc");
+	if (iface.flags & BR_IFACE_F_ALLMULTI)
+		printf(" allmulti");
+	printf("\n");
+	printf("mtu: %u\n", iface.mtu);
+
+	if (type == NULL) {
+		printf("type: %u\n", iface.type);
+	} else {
+		printf("type: %s\n", type->name);
+		type->show(&iface);
+	}
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_DEL, CTX_ARG("interface", "Delete interfaces.")),
+		"NAME",
+		iface_del,
+		"Delete an existing interface.",
+		with_help(
+			"Interface name.",
+			ec_node_dyn("NAME", complete_iface_names, INT2PTR(BR_IFACE_TYPE_UNDEF))
+		)
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("interface", "Display interface details.")),
+		"all|(name NAME)|(type TYPE)",
+		iface_show,
+		"Show interface details.",
+		with_help("Show all interfaces.", ec_node_str("all", "all")),
+		with_help(
+			"Show only this interface.",
+			ec_node_dyn("NAME", complete_iface_names, INT2PTR(BR_IFACE_TYPE_UNDEF))
+		),
+		with_help(
+			"Show only this type of interface.",
+			ec_node_dyn("TYPE", complete_iface_types, NULL)
+		)
+	);
+
+	return 0;
+}
+
+static struct br_cli_context ctx = {
+	.name = "infra iface",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/infra/api/iface.c
@@ -1,0 +1,213 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include "br_infra.h"
+
+#include <br_api.h>
+#include <br_control.h>
+#include <br_iface.h>
+#include <br_log.h>
+#include <br_port.h>
+#include <br_queue.h>
+#include <br_worker.h>
+
+#include <rte_ethdev.h>
+#include <rte_ether.h>
+
+static void port_from_api(void *info, const struct br_iface *iface) {
+	const struct br_iface_info_port *api = (const struct br_iface_info_port *)iface->info;
+	struct iface_info_port *port = info;
+
+	memccpy(port->devargs, api->devargs, 0, sizeof(port->devargs));
+	memcpy(&port->mac, &api->mac, sizeof(port->mac));
+	port->n_rxq = api->n_rxq;
+	port->n_txq = api->n_txq;
+	port->rxq_size = api->rxq_size;
+	port->txq_size = api->txq_size;
+}
+
+static void port_to_api(void *info, const struct iface *iface) {
+	const struct iface_info_port *port = (const struct iface_info_port *)iface->info;
+	struct br_iface_info_port *api = info;
+
+	memccpy(api->devargs, port->devargs, 0, sizeof(api->devargs));
+	memcpy(&api->mac, &port->mac, sizeof(api->mac));
+	api->n_rxq = port->n_rxq;
+	api->n_txq = port->n_txq;
+	api->rxq_size = port->rxq_size;
+	api->txq_size = port->txq_size;
+}
+
+// TODO: make this modular
+static struct {
+	size_t info_size;
+	void (*from_api)(void *, const struct br_iface *);
+	void (*to_api)(void *, const struct iface *);
+} types[] = {
+	[BR_IFACE_TYPE_PORT] = {
+		.info_size = sizeof(struct iface_info_port),
+		.from_api = port_from_api,
+		.to_api = port_to_api,
+	},
+};
+
+static void *info_from_api(const struct br_iface *iface) {
+	void *info;
+
+	if (iface->type == BR_IFACE_TYPE_UNDEF || iface->type > ARRAY_DIM(types)) {
+		errno = EOPNOTSUPP;
+		return NULL;
+	}
+	info = calloc(1, types[iface->type].info_size);
+	if (info == NULL)
+		return NULL;
+
+	types[iface->type].from_api(info, iface);
+
+	return info;
+}
+
+static void iface_to_api(struct br_iface *to, const struct iface *from) {
+	to->id = from->id;
+	to->type = from->type_id;
+	to->flags = from->flags;
+	to->state = from->state;
+	to->mtu = from->mtu;
+	memccpy(to->name, from->name, 0, sizeof(to->name));
+	types[from->type_id].to_api(to->info, from);
+}
+
+static struct api_out iface_add(const void *request, void **response) {
+	const struct br_infra_iface_add_req *req = request;
+	void *info = info_from_api(&req->iface);
+	struct br_infra_iface_add_resp *resp;
+	struct iface *iface;
+
+	if (info == NULL)
+		return api_out(errno, 0);
+
+	iface = iface_create(
+		req->iface.type, req->iface.flags, req->iface.mtu, req->iface.name, info
+	);
+	free(info);
+	if (iface == NULL)
+		return api_out(errno, 0);
+
+	if ((resp = malloc(sizeof(*resp))) == NULL) {
+		iface_destroy(iface->id);
+		return api_out(ENOMEM, 0);
+	}
+
+	resp->iface_id = iface->id;
+	*response = resp;
+
+	return api_out(0, sizeof(*resp));
+}
+
+static struct api_out iface_del(const void *request, void **response) {
+	const struct br_infra_iface_del_req *req = request;
+	int ret;
+
+	(void)response;
+
+	ret = iface_destroy(req->iface_id);
+
+	return api_out(-ret, 0);
+}
+
+static struct api_out iface_get(const void *request, void **response) {
+	const struct br_infra_iface_get_req *req = request;
+	struct br_infra_iface_get_resp *resp = NULL;
+	struct iface *iface;
+
+	if ((iface = iface_from_id(req->iface_id)) == NULL)
+		return api_out(ENODEV, 0);
+
+	if ((resp = malloc(sizeof(*resp))) == NULL)
+		return api_out(ENOMEM, 0);
+
+	iface_to_api(&resp->iface, iface);
+	*response = resp;
+
+	return api_out(0, sizeof(*resp));
+}
+
+static struct api_out iface_list(const void *request, void **response) {
+	const struct br_infra_iface_list_req *req = request;
+	struct br_infra_iface_list_resp *resp = NULL;
+	const struct iface *iface = NULL;
+	uint16_t n_ifaces;
+	size_t len;
+
+	n_ifaces = ifaces_count(req->type);
+
+	len = sizeof(*resp) + n_ifaces * sizeof(struct br_iface);
+	if ((resp = calloc(1, len)) == NULL)
+		return api_out(ENOMEM, 0);
+
+	while ((iface = iface_next(req->type, iface)) != NULL)
+		iface_to_api(&resp->ifaces[resp->n_ifaces++], iface);
+
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static struct api_out iface_set(const void *request, void **response) {
+	const struct br_infra_iface_set_req *req = request;
+	void *info = info_from_api(&req->iface);
+	int ret;
+
+	(void)response;
+
+	if (info == NULL)
+		return api_out(errno, 0);
+
+	ret = iface_reconfig(
+		req->iface.id,
+		req->set_attrs,
+		req->iface.flags,
+		req->iface.mtu,
+		req->iface.name,
+		info
+	);
+	free(info);
+	if (ret < 0)
+		return api_out(errno, 0);
+
+	return api_out(0, 0);
+}
+
+static struct br_api_handler iface_add_handler = {
+	.name = "iface add",
+	.request_type = BR_INFRA_IFACE_ADD,
+	.callback = iface_add,
+};
+static struct br_api_handler iface_del_handler = {
+	.name = "iface del",
+	.request_type = BR_INFRA_IFACE_DEL,
+	.callback = iface_del,
+};
+static struct br_api_handler iface_get_handler = {
+	.name = "iface get",
+	.request_type = BR_INFRA_IFACE_GET,
+	.callback = iface_get,
+};
+static struct br_api_handler iface_list_handler = {
+	.name = "iface list",
+	.request_type = BR_INFRA_IFACE_LIST,
+	.callback = iface_list,
+};
+static struct br_api_handler iface_set_handler = {
+	.name = "iface set",
+	.request_type = BR_INFRA_IFACE_SET,
+	.callback = iface_set,
+};
+
+RTE_INIT(infra_api_init) {
+	br_register_api_handler(&iface_add_handler);
+	br_register_api_handler(&iface_del_handler);
+	br_register_api_handler(&iface_get_handler);
+	br_register_api_handler(&iface_list_handler);
+	br_register_api_handler(&iface_set_handler);
+}

modules/infra/control/iface.c
@@ -1,0 +1,210 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include "br_iface.h"
+
+#include <br_control.h>
+#include <br_log.h>
+#include <br_macro.h>
+#include <br_string.h>
+
+#include <rte_malloc.h>
+
+#include <errno.h>
+#include <string.h>
+#include <sys/queue.h>
+#include <wchar.h>
+
+static STAILQ_HEAD(, iface_type) types = STAILQ_HEAD_INITIALIZER(types);
+
+struct iface_type *iface_type_get(uint16_t type_id) {
+	struct iface_type *t;
+	STAILQ_FOREACH (t, &types, next)
+		if (t->id == type_id)
+			return t;
+	errno = ENODEV;
+	return NULL;
+}
+
+void iface_type_register(struct iface_type *type) {
+	if (iface_type_get(type->id) != NULL)
+		ABORT("duplicate iface type id: %u", type->id);
+	STAILQ_INSERT_TAIL(&types, type, next);
+}
+
+static struct iface **ifaces;
+
+static int next_ifid(uint16_t *ifid) {
+	for (uint16_t i = 0; i < MAX_IFACES; i++) {
+		if (ifaces[i] == NULL) {
+			*ifid = i;
+			return 0;
+		}
+	}
+	errno = ENOSPC;
+	return -1;
+}
+
+struct iface *
+iface_create(uint16_t type_id, uint32_t flags, uint16_t mtu, const char *name, void *info) {
+	struct iface_type *type = iface_type_get(type_id);
+	struct iface *iface = NULL;
+	uint16_t ifid;
+
+	if (type == NULL)
+		goto fail;
+	if (utf8_check(name, MEMBER_SIZE(struct iface, name)) < 0)
+		goto fail;
+
+	iface = rte_zmalloc(__func__, sizeof(*iface) + type->info_size, RTE_CACHE_LINE_SIZE);
+	if (iface == NULL) {
+		errno = ENOMEM;
+		goto fail;
+	}
+	if (next_ifid(&ifid) < 0)
+		goto fail;
+
+	iface->id = ifid;
+	iface->type_id = type_id;
+	iface->flags = flags;
+	iface->mtu = mtu;
+	memccpy(iface->name, name, 0, sizeof(iface->name));
+	memcpy(iface->info, info, type->info_size);
+
+	if (type->init(iface) < 0)
+		goto fail;
+
+	ifaces[ifid] = iface;
+
+	return iface;
+fail:
+	rte_free(iface);
+	return NULL;
+}
+
+int iface_reconfig(
+	uint16_t ifid,
+	uint64_t set_attrs,
+	uint32_t flags,
+	uint16_t mtu,
+	const char *name,
+	void *info
+) {
+	struct iface_type *type;
+	struct iface *iface;
+
+	if (set_attrs == 0)
+		return errno_set(EINVAL);
+	if ((iface = iface_from_id(ifid)) == NULL)
+		return -1;
+
+	if (set_attrs & IFACE_SET_NAME) {
+		if (utf8_check(name, MEMBER_SIZE(struct iface, name)) < 0)
+			return -1;
+		memccpy(iface->name, name, 0, sizeof(iface->name));
+	}
+	if (set_attrs & IFACE_SET_FLAGS)
+		iface->flags = flags;
+	if (set_attrs & IFACE_SET_MTU)
+		iface->mtu = mtu;
+
+	type = iface_type_get(iface->type_id);
+	return type->reconfig(iface, set_attrs, info);
+}
+
+uint16_t ifaces_count(uint16_t type_id) {
+	uint16_t count = 0;
+
+	for (uint16_t ifid = 0; ifid < MAX_IFACES; ifid++) {
+		struct iface *iface = ifaces[ifid];
+		if (iface != NULL && (type_id == IFACE_TYPE_UNDEF || iface->type_id == type_id))
+			count++;
+	}
+
+	return count;
+}
+
+struct iface *iface_next(uint16_t type_id, const struct iface *prev) {
+	uint16_t start_id;
+
+	if (prev == NULL)
+		start_id = 0;
+	else
+		start_id = prev->id + 1;
+
+	for (uint16_t ifid = start_id; ifid < MAX_IFACES; ifid++) {
+		struct iface *iface = ifaces[ifid];
+		if (iface != NULL && (type_id == IFACE_TYPE_UNDEF || iface->type_id == type_id))
+			return iface;
+	}
+
+	return NULL;
+}
+
+struct iface *iface_from_id(uint16_t ifid) {
+	struct iface *iface = NULL;
+	if (ifid < MAX_IFACES)
+		iface = ifaces[ifid];
+	if (iface == NULL)
+		errno = ENODEV;
+	return iface;
+}
+
+int iface_get_eth_addr(uint16_t ifid, struct rte_ether_addr *mac) {
+	struct iface *iface = iface_from_id(ifid);
+	struct iface_type *type;
+
+	if (iface == NULL)
+		return -1;
+
+	type = iface_type_get(iface->type_id);
+	if (type->get_eth_addr == NULL)
+		return errno_set(EOPNOTSUPP);
+
+	return type->get_eth_addr(iface, mac);
+}
+
+int iface_destroy(uint16_t ifid) {
+	struct iface *iface = iface_from_id(ifid);
+	struct iface_type *type;
+	int ret;
+
+	if (iface == NULL)
+		return -1;
+
+	ifaces[ifid] = NULL;
+	type = iface_type_get(iface->type_id);
+	ret = type->fini(iface);
+	rte_free(iface);
+
+	return ret;
+}
+
+static void iface_init(void) {
+	ifaces = rte_calloc(__func__, MAX_IFACES, sizeof(struct iface *), RTE_CACHE_LINE_SIZE);
+	if (ifaces == NULL)
+		ABORT("rte_zmalloc(ifaces)");
+}
+
+static void iface_fini(void) {
+	for (uint16_t ifid = 0; ifid < MAX_IFACES; ifid++) {
+		struct iface *iface = ifaces[ifid];
+		if (iface == NULL)
+			continue;
+		if (iface_destroy(ifid) < 0)
+			LOG(ERR, "iface_destroy: %s", strerror(errno));
+	}
+	rte_free(ifaces);
+	ifaces = NULL;
+}
+
+static struct br_module iface_module = {
+	.name = "iface",
+	.init = iface_init,
+	.fini = iface_fini,
+	.fini_prio = 1000,
+};
+
+RTE_INIT(iface_constructor) {
+	br_register_module(&iface_module);
+}

modules/infra/api/br_infra.h
@@ -1,20 +1,55 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2023 Robin Jarry
 
-#ifndef _BR_INFRA_MSG
-#define _BR_INFRA_MSG
+#ifndef _BR_INFRA_API
+#define _BR_INFRA_API
 
 #include <br_api.h>
 #include <br_bitops.h>
+#include <br_macro.h>
 #include <br_net_types.h>
 
+#include <assert.h>
 #include <sched.h>
 #include <stdint.h>
 #include <sys/types.h>
 
-struct br_infra_port {
-	uint16_t index;
-	char device[128];
+// Value for br_iface.type
+#define BR_IFACE_TYPE_UNDEF 0x0000
+#define BR_IFACE_TYPE_PORT 0x0001
+
+// Interface configure flags
+#define BR_IFACE_F_UP BR_BIT16(0)
+#define BR_IFACE_F_PROMISC BR_BIT16(1)
+#define BR_IFACE_F_ALLMULTI BR_BIT16(2)
+// Interface state flags
+#define BR_IFACE_S_RUNNING BR_BIT16(0)
+
+// Interface reconfig attributes
+#define BR_IFACE_SET_FLAGS BR_BIT64(0)
+#define BR_IFACE_SET_MTU BR_BIT64(1)
+#define BR_IFACE_SET_NAME BR_BIT64(2)
+
+// Generic struct for all network interfaces.
+struct br_iface {
+	uint16_t id; // Interface unique index.
+	uint16_t type; // Interface type. Uses values from BR_IFACE_TYPE_*.
+	uint16_t flags; // Interface flags. Bit mask of BR_IFACE_F_*.
+	uint16_t state; // Interface state. Bit mask of BR_IFACE_S_*.
+	uint16_t mtu; // Maximum transmission unit size (incl. headers).
+	char name[64]; // Interface name (utf-8 encoded, nul terminated).
+	uint8_t info[256]; // Type specific interface info.
+};
+
+// Port reconfig attributes
+#define BR_PORT_SET_N_RXQS BR_BIT64(32)
+#define BR_PORT_SET_N_TXQS BR_BIT64(33)
+#define BR_PORT_SET_Q_SIZE BR_BIT64(34)
+#define BR_PORT_SET_MAC BR_BIT64(35)
+
+// Info for BR_IFACE_TYPE_PORT interfaces
+struct br_iface_info_port {
+	char devargs[64];
 	uint16_t n_rxq;
 	uint16_t n_txq;
 	uint16_t rxq_size;
@@ -22,8 +57,10 @@ struct br_infra_port {
 	struct eth_addr mac;
 };
 
-struct br_infra_rxq {
-	uint16_t port_id;
+static_assert(sizeof(struct br_iface_info_port) <= MEMBER_SIZE(struct br_iface, info));
+
+struct br_port_rxq_map {
+	uint16_t iface_id;
 	uint16_t rxq_id;
 	uint16_t cpu_id;
 	uint16_t enabled;
@@ -38,73 +75,70 @@ struct br_infra_stat {
 
 #define BR_INFRA_MODULE 0xacdc
 
-// ports ///////////////////////////////////////////////////////////////////////
-#define BR_INFRA_PORT_ADD REQUEST_TYPE(BR_INFRA_MODULE, 0x0001)
+// ifaces ///////////////////////////////////////////////////////////////////////
+#define BR_INFRA_IFACE_ADD REQUEST_TYPE(BR_INFRA_MODULE, 0x0001)
 
-struct br_infra_port_add_req {
-	char devargs[128];
+struct br_infra_iface_add_req {
+	// iface id is ignored and should be zeroed
+	struct br_iface iface;
 };
 
-struct br_infra_port_add_resp {
-	uint16_t port_id;
+struct br_infra_iface_add_resp {
+	uint16_t iface_id;
 };
 
-#define BR_INFRA_PORT_DEL REQUEST_TYPE(BR_INFRA_MODULE, 0x0002)
+#define BR_INFRA_IFACE_DEL REQUEST_TYPE(BR_INFRA_MODULE, 0x0002)
 
-struct br_infra_port_del_req {
-	uint16_t port_id;
+struct br_infra_iface_del_req {
+	uint16_t iface_id;
 };
 
-// struct br_infra_port_del_resp { };
+// struct br_infra_iface_del_resp { };
 
-#define BR_INFRA_PORT_GET REQUEST_TYPE(BR_INFRA_MODULE, 0x0003)
+#define BR_INFRA_IFACE_GET REQUEST_TYPE(BR_INFRA_MODULE, 0x0003)
 
-struct br_infra_port_get_req {
-	uint16_t port_id;
+struct br_infra_iface_get_req {
+	uint16_t iface_id;
 };
 
-struct br_infra_port_get_resp {
-	struct br_infra_port port;
+struct br_infra_iface_get_resp {
+	struct br_iface iface;
 };
 
-#define BR_INFRA_PORT_LIST REQUEST_TYPE(BR_INFRA_MODULE, 0x0004)
+#define BR_INFRA_IFACE_LIST REQUEST_TYPE(BR_INFRA_MODULE, 0x0004)
 
-// struct br_infra_port_list_req { };
-
-struct br_infra_port_list_resp {
-	uint16_t n_ports;
-	struct br_infra_port ports[/* n_ports */];
+struct br_infra_iface_list_req {
+	uint16_t type; // use BR_IFACE_TYPE_UNDEF for all
 };
 
-#define BR_INFRA_PORT_SET REQUEST_TYPE(BR_INFRA_MODULE, 0x0005)
-
-#define BR_INFRA_PORT_N_RXQ BR_BIT16(0)
-#define BR_INFRA_PORT_Q_SIZE BR_BIT16(1)
-typedef uint16_t br_infra_port_attr_t;
-
-struct br_infra_port_set_req {
-	uint16_t port_id;
-	br_infra_port_attr_t set_attrs;
-	uint16_t n_rxq;
-	uint16_t q_size;
+struct br_infra_iface_list_resp {
+	uint16_t n_ifaces;
+	struct br_iface ifaces[/* n_ifaces */];
 };
 
-// struct br_infra_port_set_resp { };
+#define BR_INFRA_IFACE_SET REQUEST_TYPE(BR_INFRA_MODULE, 0x0005)
 
-// port rxqs ///////////////////////////////////////////////////////////////////
+struct br_infra_iface_set_req {
+	struct br_iface iface;
+	uint64_t set_attrs;
+};
+
+// struct br_infra_iface_set_resp { };
+
+// iface rxqs ///////////////////////////////////////////////////////////////////
 #define BR_INFRA_RXQ_LIST REQUEST_TYPE(BR_INFRA_MODULE, 0x0010)
 
 // struct br_infra_rxq_list_req { };
 
 struct br_infra_rxq_list_resp {
 	uint16_t n_rxqs;
-	struct br_infra_rxq rxqs[/* n_rxq */];
+	struct br_port_rxq_map rxqs[/* n_rxq */];
 };
 
 #define BR_INFRA_RXQ_SET REQUEST_TYPE(BR_INFRA_MODULE, 0x0011)
 
 struct br_infra_rxq_set_req {
-	uint16_t port_id;
+	uint16_t iface_id;
 	uint16_t rxq_id;
 	uint16_t cpu_id;
 };

modules/infra/api/meson.build
@@ -3,7 +3,7 @@
 
 src += files(
   'graph.c',
-  'port.c',
+  'iface.c',
   'rxq.c',
   'stats.c',
 )

modules/infra/api/port.c
@@ -1,190 +1,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2024 Robin Jarry
-
-#include "br_infra.h"
-
-#include <br_api.h>
-#include <br_control.h>
-#include <br_infra.h>
-#include <br_log.h>
-#include <br_port.h>
-#include <br_queue.h>
-#include <br_worker.h>
-
-#include <rte_ethdev.h>
-#include <rte_ether.h>
-
-static int fill_port_info(struct port *e, struct br_infra_port *port) {
-	struct rte_eth_dev_info info;
-	struct rte_ether_addr mac;
-	int ret;
-
-	memset(port, 0, sizeof(*port));
-	port->index = e->port_id;
-
-	if ((ret = rte_eth_dev_info_get(e->port_id, &info)) < 0)
-		return ret;
-	if ((ret = rte_eth_macaddr_get(e->port_id, &mac)) < 0)
-		return ret;
-
-	port->n_rxq = info.nb_rx_queues;
-	port->n_txq = info.nb_tx_queues;
-	port->rxq_size = e->rxq_size;
-	port->txq_size = e->txq_size;
-
-	memccpy(port->device, rte_dev_name(info.device), 0, sizeof(port->device));
-	memcpy(port->mac.bytes, mac.addr_bytes, sizeof(port->mac.bytes));
-
-	return 0;
-}
-
-static struct api_out port_add(const void *request, void **response) {
-	const struct br_infra_port_add_req *req = request;
-	struct br_infra_port_add_resp *resp;
-	int ret;
-
-	ret = port_create(req->devargs);
-	if (ret < 0)
-		return api_out(-ret, 0);
-
-	if ((resp = malloc(sizeof(*resp))) == NULL) {
-		port_destroy(ret);
-		return api_out(ENOMEM, 0);
-	}
-
-	resp->port_id = ret;
-	*response = resp;
-
-	return api_out(0, sizeof(*resp));
-}
-
-static struct api_out port_del(const void *request, void **response) {
-	const struct br_infra_port_del_req *req = request;
-	int ret;
-
-	(void)response;
-
-	ret = port_destroy(req->port_id);
-
-	return api_out(-ret, 0);
-}
-
-static struct api_out port_get(const void *request, void **response) {
-	const struct br_infra_port_get_req *req = request;
-	struct br_infra_port_get_resp *resp = NULL;
-	struct port *port;
-	int ret;
-
-	if ((port = find_port(req->port_id)) == NULL)
-		return api_out(ENODEV, 0);
-
-	if ((resp = malloc(sizeof(*resp))) == NULL)
-		return api_out(ENOMEM, 0);
-
-	if ((ret = fill_port_info(port, &resp->port)) < 0) {
-		free(resp);
-		return api_out(-ret, 0);
-	}
-	*response = resp;
-
-	return api_out(0, sizeof(*resp));
-}
-
-static struct api_out port_list(const void *request, void **response) {
-	struct br_infra_port_list_resp *resp = NULL;
-	uint16_t n_ports = 0;
-	struct port *port;
-	size_t len;
-	int ret;
-
-	(void)request;
-
-	STAILQ_FOREACH (port, &ports, next)
-		n_ports++;
-
-	len = sizeof(*resp) + n_ports * sizeof(struct br_infra_port);
-	if ((resp = malloc(len)) == NULL)
-		return api_out(ENOMEM, 0);
-
-	memset(resp, 0, len);
-
-	n_ports = 0;
-	STAILQ_FOREACH (port, &ports, next) {
-		struct br_infra_port *p = &resp->ports[n_ports];
-		if ((ret = fill_port_info(port, p)) < 0) {
-			free(resp);
-			return api_out(-ret, 0);
-		}
-		n_ports++;
-	}
-
-	resp->n_ports = n_ports;
-
-	*response = resp;
-
-	return api_out(0, len);
-}
-
-static struct api_out port_set(const void *request, void **response) {
-	const struct br_infra_port_set_req *req = request;
-	bool reconfig = false;
-	struct port *port;
-
-	int ret;
-
-	(void)response;
-
-	if (req->set_attrs == 0)
-		return api_out(EINVAL, 0);
-
-	if ((port = find_port(req->port_id)) == NULL)
-		return api_out(ENODEV, 0);
-
-	if (req->set_attrs & BR_INFRA_PORT_N_RXQ) {
-		port->n_rxq = req->n_rxq;
-		reconfig = true;
-	}
-	if (req->set_attrs & BR_INFRA_PORT_Q_SIZE) {
-		port->rxq_size = req->q_size;
-		port->txq_size = req->q_size;
-		reconfig = true;
-	}
-	if (reconfig && (ret = port_reconfig(port)) < 0)
-		return api_out(-ret, 0);
-
-	return api_out(0, 0);
-}
-
-static struct br_api_handler port_add_handler = {
-	.name = "port add",
-	.request_type = BR_INFRA_PORT_ADD,
-	.callback = port_add,
-};
-static struct br_api_handler port_del_handler = {
-	.name = "port del",
-	.request_type = BR_INFRA_PORT_DEL,
-	.callback = port_del,
-};
-static struct br_api_handler port_get_handler = {
-	.name = "port get",
-	.request_type = BR_INFRA_PORT_GET,
-	.callback = port_get,
-};
-static struct br_api_handler port_list_handler = {
-	.name = "port list",
-	.request_type = BR_INFRA_PORT_LIST,
-	.callback = port_list,
-};
-static struct br_api_handler port_set_handler = {
-	.name = "port set",
-	.request_type = BR_INFRA_PORT_SET,
-	.callback = port_set,
-};
-
-RTE_INIT(infra_api_init) {
-	br_register_api_handler(&port_add_handler);
-	br_register_api_handler(&port_del_handler);
-	br_register_api_handler(&port_get_handler);
-	br_register_api_handler(&port_list_handler);
-	br_register_api_handler(&port_set_handler);
-}

commit 1b5d267dcb822efffe6f1dd212cd3748649c2cbc

cli: fix ignored errors on init
Make sure not to ignore any libecoli schema initialization errors.

Fixes: a188078583cf ("infra: replace port with interface abstraction")
Fixes: 0a399004797e ("ip: add ping and traceroute commands")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/cli/iface.c
@@ -385,7 +385,7 @@ static int ctx_init(struct ec_node *root) {
 		)
 	);
 
-	return 0;
+	return ret;
 }
 
 static struct gr_cli_context ctx = {

modules/ip/cli/icmp.c
@@ -196,6 +196,8 @@ static int ctx_init(struct ec_node *root) {
 		with_help("Number of packets to send.", ec_node_uint("COUNT", 1, UINT16_MAX, 10)),
 		with_help("Delay in ms between icmp echo.", ec_node_uint("DELAY", 0, 10000, 10))
 	);
+	if (ret < 0)
+		return ret;
 
 	ret = CLI_COMMAND(
 		CLI_CONTEXT(root, CTX_ARG("traceroute", "Discover IPv4 intermediate gateways.")),