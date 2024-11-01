DPDK/grout

commit f371a5e987df296c0c62cfd3506262692960c2f9

port: use interrupt thread to monitor link status and speed
The link status behind eth ports may change at any moment. The only way
to get reliable information about the link status and speed is to rely
on driver notifications via the link status check interrupt thread
(enabled by default in DPDK).

Configure all ports with intr_conf.lsc=1 if supported by the driver.

Register an LSC ethdev callback which only serves as a proxy to trigger
a libevent callback. That way, the link status information is checked
and updated from the main lcore only. libevent needs to be initialized
with pthread support to allow other threads to trigger events.

For DPDK drivers that do not support link status change events, the link
status and speed will be updated once per second via a libevent timer on
the same callback.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/port.c
@@ -3,6 +3,8 @@
 
 #include "worker_priv.h"
 
+#include <gr.h>
+#include <gr_control.h>
 #include <gr_iface.h>
 #include <gr_infra.h>
 #include <gr_log.h>
@@ -27,33 +29,19 @@
 
 #define ETHER_FRAME_GAP 20
 
-uint32_t port_get_rxq_buffer_us(uint16_t port_id, uint16_t rxq_id) {
+static int queue_buffer_us(uint32_t link_speed, uint16_t queue_size) {
 	uint32_t frame_size, pkts_per_us;
-	struct rte_eth_rxq_info qinfo;
-	struct rte_eth_link link;
-	int ret;
-
-	if ((ret = rte_eth_link_get_nowait(port_id, &link)) < 0)
-		return 0;
-	switch (link.link_speed) {
-	case RTE_ETH_SPEED_NUM_NONE:
-	case RTE_ETH_SPEED_NUM_UNKNOWN:
-		return 0;
-	}
-
-	if (rte_eth_rx_queue_info_get(port_id, rxq_id, &qinfo) < 0)
-		return 0;
 
 	// minimum ethernet frame size on the wire
 	frame_size = (RTE_ETHER_MIN_LEN + ETHER_FRAME_GAP) * 8;
 
 	// reported speed by driver is in megabit/s and we need a result in micro seconds.
 	// we can use link_speed without any conversion: megabit/s is equivalent to bit/us
-	pkts_per_us = link.link_speed / frame_size;
+	pkts_per_us = link_speed / frame_size;
 	if (pkts_per_us == 0)
 		return 0;
 
-	return qinfo.nb_desc / pkts_per_us;
+	return queue_size / pkts_per_us;
 }
 
 static uint16_t get_rxq_size(struct iface_info_port *p, const struct rte_eth_dev_info *info) {
@@ -184,6 +172,9 @@ static int port_configure(struct iface_info_port *p) {
 	else
 		conf.rxmode.mq_mode = RTE_ETH_MQ_RX_RSS;
 	conf.rxmode.offloads &= info.rx_offload_capa;
+	if (info.dev_flags != NULL && *info.dev_flags & RTE_ETH_DEV_INTR_LSC) {
+		conf.intr_conf.lsc = 1;
+	}
 
 	if ((ret = rte_eth_dev_configure(p->port_id, p->n_rxq, p->n_txq, &conf)) < 0)
 		return errno_log(-ret, "rte_eth_dev_configure");
@@ -276,14 +267,6 @@ int iface_port_reconfig(
 		}
 		if (ret < 0)
 			errno_log(-ret, "rte_eth_dev_set_link_{up,down}");
-
-		struct rte_eth_link link;
-		if (rte_eth_link_get(p->port_id, &link) == 0) {
-			if (link.link_status == RTE_ETH_LINK_UP)
-				iface->state |= GR_IFACE_S_RUNNING;
-			else
-				iface->state &= ~GR_IFACE_S_RUNNING;
-		}
 	}
 
 	if ((set_attrs & GR_IFACE_SET_MTU) && mtu != 0) {
@@ -626,6 +609,94 @@ static void port_to_api(void *info, const struct iface *iface) {
 	}
 }
 
+static struct event *link_event;
+
+static void link_event_cb(evutil_socket_t, short, void *) {
+	unsigned max_sleep_us, rx_buffer_us;
+	struct rte_eth_rxq_info qinfo;
+	struct rte_eth_link link;
+	struct queue_map *qmap;
+	struct worker *worker;
+	const struct iface *i;
+	struct iface *iface;
+
+	STAILQ_FOREACH (worker, &workers, next) {
+		if (gr_args()->poll_mode)
+			max_sleep_us = 0;
+		else
+			max_sleep_us = 1000; // unreasonably long maximum (1ms)
+
+		arrforeach (qmap, worker->rxqs) {
+			i = port_ifaces[qmap->port_id];
+			if (i == NULL)
+				continue;
+			iface = iface_from_id(i->id);
+			if (iface == NULL)
+				continue;
+
+			if (rte_eth_link_get_nowait(qmap->port_id, &link) < 0) {
+				LOG(INFO, "%s: link status down", iface->name);
+				iface->state &= ~GR_IFACE_S_RUNNING;
+				continue;
+			}
+			if (link.link_status == RTE_ETH_LINK_UP) {
+				if (!(iface->state & GR_IFACE_S_RUNNING)) {
+					LOG(INFO, "%s: link status up", iface->name);
+					iface->state |= GR_IFACE_S_RUNNING;
+				}
+			} else {
+				if (iface->state & GR_IFACE_S_RUNNING) {
+					LOG(INFO, "%s: link status down", iface->name);
+					iface->state &= ~GR_IFACE_S_RUNNING;
+				}
+				continue;
+			}
+			if (gr_args()->poll_mode)
+				continue;
+
+			switch (link.link_speed) {
+			case RTE_ETH_SPEED_NUM_NONE:
+			case RTE_ETH_SPEED_NUM_UNKNOWN:
+				continue;
+			}
+			if (rte_eth_rx_queue_info_get(qmap->port_id, qmap->queue_id, &qinfo) < 0)
+				continue;
+
+			rx_buffer_us = queue_buffer_us(link.link_speed, qinfo.nb_desc);
+			if (rx_buffer_us < max_sleep_us)
+				max_sleep_us = rx_buffer_us;
+		}
+		if (atomic_exchange(&worker->max_sleep_us, max_sleep_us) != max_sleep_us)
+			LOG(INFO, "[CPU %u] worker max sleep %uus", worker->cpu_id, max_sleep_us);
+	}
+}
+
+static int lsc_port_cb(uint16_t, enum rte_eth_event_type, void *, void *) {
+	// This callback may be executed from any dataplane or DPDK thread.
+	// In order to serialize the update of port status, propagate the callback
+	// event to the event loop running in the main lcore.
+	event_active(link_event, 0, 0);
+	return 0;
+}
+
+static void port_init(struct event_base *base) {
+	link_event = event_new(base, -1, EV_PERSIST | EV_FINALIZE, link_event_cb, NULL);
+	if (link_event == NULL)
+		ABORT("event_new() failed");
+	// Not all drivers support triggering link status change events.
+	// Ensure the link_event is triggered at least once every second.
+	struct timeval tv = {.tv_sec = 1};
+	if (event_add(link_event, &tv) < 0)
+		ABORT("event_add() failed");
+	rte_eth_dev_callback_register(RTE_ETH_ALL, RTE_ETH_EVENT_INTR_LSC, lsc_port_cb, NULL);
+}
+
+static void port_fini(struct event_base *) {
+	rte_eth_dev_callback_unregister(RTE_ETH_ALL, RTE_ETH_EVENT_INTR_LSC, lsc_port_cb, NULL);
+	event_free(link_event);
+	link_event = NULL;
+}
+
 static struct iface_type iface_type_port = {
 	.id = GR_IFACE_TYPE_PORT,
 	.name = "port",
@@ -639,6 +710,12 @@ static struct iface_type iface_type_port = {
 	.to_api = port_to_api,
 };
 
+static struct gr_module port_module = {
+	.init = port_init,
+	.fini = port_fini,
+};
+
 RTE_INIT(port_constructor) {
 	iface_type_register(&iface_type_port);
+	gr_register_module(&port_module);
 }

main/main.c
@@ -13,6 +13,7 @@
 #include <gr_macro.h>
 
 #include <event2/event.h>
+#include <event2/thread.h>
 #include <rte_eal.h>
 #include <rte_log.h>
 #include <rte_mempool.h>
@@ -361,7 +362,11 @@ int main(int argc, char **argv) {
 		perror("setlocale(LC_CTYPE, C.UTF-8)");
 		goto end;
 	}
-
+	if (evthread_use_pthreads() < 0) {
+		errno = ENOSYS;
+		perror("evthread_use_pthreads");
+		goto end;
+	}
 	if (parse_args(argc, argv) < 0)
 		goto end;
 

meson.build
@@ -45,7 +45,8 @@ dpdk_dep = dependency(
   ],
 )
 
-event_dep = dependency('libevent')
+ev_core_dep = dependency('libevent_core')
+ev_thread_dep = dependency('libevent_pthreads')
 numa_dep = dependency('numa')
 stb_dep = dependency('stb', fallback: ['stb', 'stb_dep'])
 ecoli_dep = dependency('libecoli', fallback: ['ecoli', 'libecoli_dep'])
@@ -70,7 +71,7 @@ subdir('cli')
 grout_exe = executable(
   'grout', src,
   include_directories: inc,
-  dependencies: [dpdk_dep, event_dep, numa_dep, stb_dep],
+  dependencies: [dpdk_dep, ev_core_dep, ev_thread_dep, numa_dep, stb_dep],
   install: true,
   install_dir: 'sbin',
 )
@@ -92,7 +93,7 @@ foreach t : tests
     'include_directories': inc + cli_inc,
     'c_args': ['-coverage'],
     'link_args': t['link_args'] + ['-lgcov'],
-    'dependencies': [dpdk_dep, event_dep, numa_dep, ecoli_dep, cmocka_dep, stb_dep],
+    'dependencies': [dpdk_dep, ev_core_dep, ev_thread_dep, numa_dep, ecoli_dep, cmocka_dep, stb_dep],
   }
   test(name, executable(name, kwargs: t), suite: 'unit')
 endforeach

modules/infra/api/graph.c
@@ -26,8 +26,8 @@ static struct api_out graph_dump(const void *request, void **response) {
 
 	STAILQ_FOREACH (worker, &workers, next) {
 		for (int i = 0; i < 2; i++) {
-			if (worker->config[i].graph != NULL) {
-				graph_name = worker->config[i].graph->name;
+			if (worker->graph[i] != NULL) {
+				graph_name = worker->graph[i]->name;
 				goto found;
 			}
 		}

modules/infra/control/gr_worker.h
@@ -41,10 +41,8 @@ struct worker {
 	atomic_uint next_config; // dataplane: ro, ctlplane: rw
 	atomic_uint cur_config; // dataplane: wo, ctlplane: ro
 	// synced with thread_fence
-	struct {
-		struct rte_graph *graph;
-		uint32_t max_sleep_us;
-	} config[2]; // dataplane: ro, ctlplane: rw
+	struct rte_graph *graph[2]; // dataplane: ro, ctlplane: rw
+	atomic_uint max_sleep_us; // dataplane: ro, ctlplane: rw
 
 	atomic_bool stats_reset; // dataplane: rw, ctlplane: rw
 	// dataplane: wo, ctlplane: ro, may be NULL

modules/infra/control/graph.c
@@ -3,7 +3,6 @@
 
 #include "graph_priv.h"
 
-#include <gr.h>
 #include <gr_control.h>
 #include <gr_datapath.h>
 #include <gr_graph.h>
@@ -122,17 +121,16 @@ static void node_data_reset(const char *graph) {
 void worker_graph_free(struct worker *worker) {
 	int ret;
 	for (int i = 0; i < 2; i++) {
-		if (worker->config[i].graph != NULL) {
-			node_data_reset(worker->config[i].graph->name);
-			if ((ret = rte_graph_destroy(worker->config[i].graph->id)) < 0)
+		if (worker->graph[i] != NULL) {
+			node_data_reset(worker->graph[i]->name);
+			if ((ret = rte_graph_destroy(worker->graph[i]->id)) < 0)
 				LOG(ERR, "rte_graph_destroy: %s", rte_strerror(-ret));
-			worker->config[i].graph = NULL;
+			worker->graph[i] = NULL;
 		}
 	}
 }
 
 static int worker_graph_new(struct worker *worker, uint8_t index) {
-	uint32_t max_sleep_us, rx_buffer_us;
 	struct rx_node_queues *rx = NULL;
 	struct tx_node_queues *tx = NULL;
 	char name[RTE_GRAPH_NAMESIZE];
@@ -148,7 +146,7 @@ static int worker_graph_new(struct worker *worker, uint8_t index) {
 			n_rxqs++;
 	}
 	if (n_rxqs == 0) {
-		worker->config[index].graph = NULL;
+		worker->graph[index] = NULL;
 		return 0;
 	}
 
@@ -164,10 +162,6 @@ static int worker_graph_new(struct worker *worker, uint8_t index) {
 		goto err;
 	}
 	n_rxqs = 0;
-	if (gr_args()->poll_mode)
-		max_sleep_us = 0;
-	else
-		max_sleep_us = 1000; // unreasonably long maximum (1ms)
 	arrforeach (qmap, worker->rxqs) {
 		if (!qmap->enabled)
 			continue;
@@ -178,13 +172,6 @@ static int worker_graph_new(struct worker *worker, uint8_t index) {
 		    qmap->queue_id);
 		rx->queues[n_rxqs].port_id = qmap->port_id;
 		rx->queues[n_rxqs].rxq_id = qmap->queue_id;
-		if (!gr_args()->poll_mode) {
-			// divide buffer size by two to take into account
-			// the time to wakeup from sleep
-			rx_buffer_us = port_get_rxq_buffer_us(qmap->port_id, qmap->queue_id) / 2;
-			if (rx_buffer_us < max_sleep_us)
-				max_sleep_us = rx_buffer_us;
-		}
 		n_rxqs++;
 	}
 	rx->n_queues = n_rxqs;
@@ -233,8 +220,7 @@ static int worker_graph_new(struct worker *worker, uint8_t index) {
 		ret = -rte_errno;
 		goto err;
 	}
-	worker->config[index].graph = rte_graph_lookup(name);
-	worker->config[index].max_sleep_us = max_sleep_us;
+	worker->graph[index] = rte_graph_lookup(name);
 
 	return 0;
 err:
@@ -263,11 +249,11 @@ int worker_graph_reload_all(void) {
 		// free old config
 		next = !next;
 
-		if (worker->config[next].graph != NULL) {
-			node_data_reset(worker->config[next].graph->name);
-			if ((ret = rte_graph_destroy(worker->config[next].graph->id)) < 0)
+		if (worker->graph[next] != NULL) {
+			node_data_reset(worker->graph[next]->name);
+			if ((ret = rte_graph_destroy(worker->graph[next]->id)) < 0)
 				errno_log(-ret, "rte_graph_destroy");
-			worker->config[next].graph = NULL;
+			worker->graph[next] = NULL;
 		}
 	}
 

modules/infra/control/worker_test.c
@@ -3,6 +3,7 @@
 
 #include "worker_priv.h"
 
+#include <gr.h>
 #include <gr_api.h>
 #include <gr_cmocka.h>
 #include <gr_control.h>
@@ -30,6 +31,15 @@ void iface_type_register(struct iface_type *) { }
 void iface_event_notify(iface_event_t, struct iface *) { }
 mock_func(struct rte_mempool *, gr_pktmbuf_pool_get(int8_t, uint32_t));
 void gr_pktmbuf_pool_release(struct rte_mempool *, uint32_t) { }
+
+struct gr_args args;
+const struct gr_args *gr_args(void) {
+	return &args;
+}
+
+struct iface *iface_from_id(uint16_t ifid) {
+	return ifid < ARRAY_DIM(ifaces) ? ifaces[ifid] : NULL;
+}
 
 struct iface *iface_next(uint16_t type_id, const struct iface *prev) {
 	uint16_t ifid;

modules/infra/datapath/main_loop.c
@@ -159,8 +159,7 @@ reconfig:
 		goto shutdown;
 
 	cur = atomic_load_explicit(&w->next_config, memory_order_acquire);
-	graph = w->config[cur].graph;
-	max_sleep_us = w->config[cur].max_sleep_us;
+	graph = w->graph[cur];
 	atomic_store_explicit(&w->cur_config, cur, memory_order_release);
 
 	if (graph == NULL) {
@@ -173,8 +172,6 @@ reconfig:
 	atomic_store(&w->stats, ctx.w_stats);
 
 	gr_modules_dp_init();
-
-	log(INFO, "reconfigured max_sleep=%uus", max_sleep_us);
 
 	loop = 0;
 	sleep = 0;
@@ -192,6 +189,7 @@ reconfig:
 			rte_graph_cluster_stats_get(ctx.stats, false);
 			timestamp_tmp = rte_rdtsc();
 			cycles = timestamp_tmp - timestamp;
+			max_sleep_us = atomic_load_explicit(&w->max_sleep_us, memory_order_relaxed);
 			if (ctx.last_count == 0 && max_sleep_us > 0) {
 				sleep = sleep == max_sleep_us ? sleep : (sleep + 1);
 				usleep(sleep);

commit 8fa2566006944d4c78f7ad3a90169aef1a23b52d

infra: add missing port module name
The port module didn't have any name defined. Add one. Ensure that we
will get an early crash if we have a new module that reproduces the
issue.

Fixes: f371a5e987df ("port: use interrupt thread to monitor link status and speed")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

main/module.c
@@ -44,6 +44,8 @@ const struct gr_api_handler *lookup_api_handler(const struct gr_api_request *req
 static STAILQ_HEAD(, gr_module) modules = STAILQ_HEAD_INITIALIZER(modules);
 
 void gr_register_module(struct gr_module *mod) {
+	if (mod->name == NULL)
+		ABORT("module with no name: %p", mod);
 	STAILQ_INSERT_TAIL(&modules, mod, entries);
 }
 

modules/infra/control/port.c
@@ -721,6 +721,7 @@ static struct iface_type iface_type_port = {
 };
 
 static struct gr_module port_module = {
+	.name = "iface port",
 	.init = port_init,
 	.fini = port_fini,
 };