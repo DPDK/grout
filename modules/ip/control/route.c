DPDK/grout

commit 4b9e28692fa78bb2f7b9ed8d4a17ded7e34ab801

ip: clean resources on interface deletion
On interface deletion, delete associated routes. specify module
destructor priorities to ensure resources are not freed when accessing
them.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/control/route.c
@@ -301,6 +301,64 @@ static void route4_fini(struct event_base *) {
 	vrf_fibs = NULL;
 }
 
+void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
+	struct rte_rib_node *rn = NULL;
+	struct rte_rib *rib;
+	ip4_addr_t local_ip;
+	uint8_t prefixlen;
+	uint64_t nh_idx;
+	ip4_addr_t ip;
+	uint32_t mask;
+
+	local_ip = nh->ip;
+	mask = rte_be_to_cpu_32(~(0xffffffff >> nh->prefixlen));
+
+	rib = rte_fib_get_rib(get_fib(vrf_id));
+	while ((rn = rte_rib_get_nxt(rib, 0, 0, rn, RTE_RIB_GET_NXT_ALL)) != NULL) {
+		rte_rib_get_nh(rn, &nh_idx);
+		nh = ip4_nexthop_get(nh_idx);
+
+		if (nh && (nh->ip & mask) == (local_ip & mask)) {
+			rte_rib_get_ip(rn, &ip);
+			rte_rib_get_depth(rn, &prefixlen);
+			ip = rte_be_to_cpu_32(ip);
+
+			LOG(DEBUG,
+			    "%s ip4_route_delete " IP4_ADDR_FMT "/%d via " IP4_ADDR_FMT,
+			    __func__,
+			    IP4_ADDR_SPLIT(&ip),
+			    prefixlen,
+			    IP4_ADDR_SPLIT(&nh->ip));
+
+			ip4_route_delete(vrf_id, ip, prefixlen);
+			ip4_route_delete(vrf_id, ip, 32);
+		}
+	}
+
+	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL || true) {
+		rte_rib_get_nh(rn, &nh_idx);
+		nh = ip4_nexthop_get(nh_idx);
+
+		if (nh && (nh->ip & mask) == (local_ip & mask)) {
+			rte_rib_get_ip(rn, &ip);
+			rte_rib_get_depth(rn, &prefixlen);
+			ip = rte_be_to_cpu_32(ip);
+
+			LOG(DEBUG,
+			    "%s ip4_route_delete " IP4_ADDR_FMT "/%d via " IP4_ADDR_FMT,
+			    __func__,
+			    IP4_ADDR_SPLIT(&ip),
+			    prefixlen,
+			    IP4_ADDR_SPLIT(&nh->ip));
+
+			ip4_route_delete(vrf_id, nh->ip, nh->prefixlen);
+			ip4_route_delete(vrf_id, nh->ip, 32);
+		}
+	}
+
+	ip4_route_delete(vrf_id, local_ip, 32);
+}
+
 static struct gr_api_handler route4_add_handler = {
 	.name = "ipv4 route add",
 	.request_type = GR_IP4_ROUTE_ADD,

modules/ip/control/address.c
@@ -142,6 +142,19 @@ static struct api_out addr_list(const void *request, void **response) {
 	return api_out(0, len);
 }
 
+static void iface_event_handler(iface_event_t event, struct iface *iface) {
+	struct nexthop *nh;
+
+	if (event != IFACE_EVENT_PRE_REMOVE)
+		return;
+
+	nh = ip4_addr_get(iface->id);
+	if (nh)
+		ip4_route_cleanup(iface->vrf_id, nh);
+
+	addrs[iface->id] = NULL;
+}
+
 static void addr_init(struct event_base *) {
 	addrs = rte_calloc(__func__, MAX_IFACES, sizeof(struct nexthop *), RTE_CACHE_LINE_SIZE);
 	if (addrs == NULL)
@@ -172,6 +185,11 @@ static struct gr_module addr_module = {
 	.name = "ipv4 address",
 	.init = addr_init,
 	.fini = addr_fini,
+	.fini_prio = 2000,
+};
+
+static struct iface_event_handler iface_event_address_handler = {
+	.callback = iface_event_handler,
 };
 
 RTE_INIT(address_constructor) {
@@ -179,4 +197,5 @@ RTE_INIT(address_constructor) {
 	gr_register_api_handler(&addr_del_handler);
 	gr_register_api_handler(&addr_list_handler);
 	gr_register_module(&addr_module);
+	iface_event_register_handler(&iface_event_address_handler);
 }

modules/ip/control/gr_ip4_control.h
@@ -58,7 +58,7 @@ int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, uint32_t
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip);
 struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
+void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh);
 
 struct nexthop *ip4_addr_get(uint16_t iface_id);
-
 #endif

modules/ip/control/nexthop.c
@@ -323,6 +323,7 @@ static struct gr_module nh4_module = {
 	.name = "ipv4 nexthop",
 	.init = nh4_init,
 	.fini = nh4_fini,
+	.fini_prio = 20000,
 };
 
 RTE_INIT(control_ip_init) {

commit a63b070f966319ecdb40d49b905b363eecb52157

route: fix null pointer dereference on cleanup
The nh pointer is reused in ip{4,6}_route_cleanup() and may be set to
NULL at some point. Accessing it at the end may result to a NULL pointer
dereference.

Move the deletion of the /32 or /128 route entry at the beginning to
ensure that nh is valid.

Fixes: 4b9e28692fa7 ("ip: clean resources on interface deletion")
Fixes: 0fc1cd222b8e ("modules: add static ipv6 forwarding support")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/control/route.c
@@ -325,6 +325,7 @@ void ip4_route_cleanup(struct nexthop *nh) {
 	uintptr_t nh_id;
 	ip4_addr_t ip;
 
+	ip4_route_delete(nh->vrf_id, nh->ip, 32);
 	local_ip = nh->ip;
 	local_prefixlen = nh->prefixlen;
 
@@ -368,8 +369,6 @@ void ip4_route_cleanup(struct nexthop *nh) {
 			ip4_route_delete(nh->vrf_id, nh->ip, 32);
 		}
 	}
-
-	ip4_route_delete(nh->vrf_id, local_ip, 32);
 }
 
 static struct gr_api_handler route4_add_handler = {

modules/ip6/control/route.c
@@ -321,6 +321,7 @@ void ip6_route_cleanup(struct nexthop6 *nh) {
 	struct rte_rib6 *rib;
 	uintptr_t nh_id;
 
+	ip6_route_delete(nh->vrf_id, &nh->ip, RTE_IPV6_MAX_DEPTH);
 	local_ip = nh->ip;
 	local_depth = nh->prefixlen;
 
@@ -362,8 +363,6 @@ void ip6_route_cleanup(struct nexthop6 *nh) {
 			ip6_route_delete(nh->vrf_id, &nh->ip, RTE_IPV6_MAX_DEPTH);
 		}
 	}
-
-	ip6_route_delete(nh->vrf_id, &local_ip, RTE_IPV6_MAX_DEPTH);
 }
 
 static struct gr_api_handler route6_add_handler = {