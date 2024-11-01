DPDK/grout

commit 0cbe6f12502471017ed4c2761cbfe2915d485d3e

icmp: allow sending packets from control plane
Expose an API to inject ICMP packets from the control plane.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/datapath/icmp_local_send.c
@@ -1,0 +1,134 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr_control_input.h>
+#include <gr_control_output.h>
+#include <gr_datapath.h>
+#include <gr_graph.h>
+#include <gr_ip4_control.h>
+#include <gr_ip4_datapath.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+
+#include <rte_graph_worker.h>
+#include <rte_icmp.h>
+#include <rte_ip.h>
+
+#include <netinet/in.h>
+#include <stdatomic.h>
+#include <time.h>
+
+enum {
+	OUTPUT = 0,
+	EDGE_COUNT,
+};
+
+struct ctl_to_stack {
+	ip4_addr_t dst;
+	ip4_addr_t src;
+	uint16_t vrf_id;
+	uint16_t ident;
+	uint16_t seq_num;
+	uint8_t ttl;
+};
+
+static control_input_t ip4_icmp_request;
+
+int icmp_local_send(
+	uint16_t vrf_id,
+	ip4_addr_t dst,
+	struct nexthop *gw,
+	uint16_t ident,
+	uint16_t seq_num,
+	uint8_t ttl
+) {
+	struct ctl_to_stack *msg;
+	struct nexthop *local;
+	int ret;
+
+	if ((msg = calloc(1, sizeof(struct ctl_to_stack))) == NULL)
+		return errno_set(ENOMEM);
+
+	msg->seq_num = seq_num;
+	msg->vrf_id = vrf_id;
+	msg->ident = ident;
+	msg->ttl = ttl;
+	msg->dst = dst;
+
+	if ((local = ip4_addr_get_preferred(gw->iface_id, gw->ip)) == NULL)
+		return -errno;
+
+	msg->src = local->ip;
+
+	if ((ret = post_to_stack(ip4_icmp_request, msg)) < 0) {
+		free(msg);
+		return ret;
+	}
+
+	return 0;
+}
+
+static uint16_t icmp_local_send_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t n_objs
+) {
+	struct ip_local_mbuf_data *data;
+	struct rte_icmp_hdr *icmp;
+	struct ctl_to_stack *msg;
+	struct rte_mbuf *mbuf;
+	clock_t *payload;
+	rte_edge_t next;
+
+	for (unsigned i = 0; i < n_objs; i++) {
+		mbuf = objs[i];
+		msg = control_input_mbuf_data(mbuf)->data;
+		icmp = (struct rte_icmp_hdr *)
+			rte_pktmbuf_append(mbuf, sizeof(*icmp) + sizeof(clock_t));
+
+		payload = rte_pktmbuf_mtod_offset(mbuf, clock_t *, sizeof(*icmp));
+		*payload = clock();
+
+		// Build ICMP packet
+		icmp->icmp_type = RTE_ICMP_TYPE_ECHO_REQUEST;
+		icmp->icmp_code = 0;
+		icmp->icmp_seq_nb = rte_cpu_to_be_16(msg->seq_num);
+		icmp->icmp_ident = rte_cpu_to_be_16(msg->ident);
+
+		data = ip_local_mbuf_data(mbuf);
+		data->proto = IPPROTO_ICMP;
+		data->len = sizeof(*icmp) + sizeof(clock_t);
+		data->dst = msg->dst;
+		data->src = msg->src;
+		data->vrf_id = msg->vrf_id;
+		data->ttl = msg->ttl;
+
+		next = OUTPUT;
+
+		rte_node_enqueue_x1(graph, node, next, mbuf);
+		free(msg);
+	}
+
+	return n_objs;
+}
+
+static void icmp_local_send_register(void) {
+	ip4_icmp_request = gr_control_input_register_handler("icmp_local_send");
+}
+
+static struct rte_node_register icmp_local_send_node = {
+	.name = "icmp_local_send",
+	.process = icmp_local_send_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[OUTPUT] = "icmp_output",
+	},
+};
+
+static struct gr_node_info icmp_local_send_info = {
+	.node = &icmp_local_send_node,
+	.register_callback = icmp_local_send_register,
+};
+
+GR_NODE_REGISTER(icmp_local_send_info);

modules/ip/datapath/gr_ip4_datapath.h
@@ -56,6 +56,15 @@ static inline void ip_set_fields(struct rte_ipv4_hdr *ip, struct ip_local_mbuf_d
 	ip->hdr_checksum = rte_ipv4_cksum(ip);
 }
 
+int icmp_local_send(
+	uint16_t vrf_id,
+	ip4_addr_t dst,
+	struct nexthop *gw,
+	uint16_t ident,
+	uint16_t seq_num,
+	uint8_t ttl
+);
+
 void icmp_input_register_callback(uint8_t icmp_type, control_output_cb_t cb);
 
 #endif

modules/ip/datapath/meson.build
@@ -6,6 +6,7 @@ src += files(
   'arp_output_reply.c',
   'arp_output_request.c',
   'icmp_input.c',
+  'icmp_local_send.c',
   'icmp_output.c',
   'ip_error.c',
   'ip_forward.c',

commit 8f00aed836e617dcfacc7b7cc8de50c158429984

ping: fix memory leak when no local address configured
When no suitable local address can be found on the outgoing interface,
the allocated msg memory is leaked. Free the memory before returning an
error.

Fixes: 0cbe6f125024 ("icmp: allow sending packets from control plane")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/datapath/icmp_local_send.c
@@ -55,8 +55,10 @@ int icmp_local_send(
 	msg->ttl = ttl;
 	msg->dst = dst;
 
-	if ((local = ip4_addr_get_preferred(gw->iface_id, gw->ip)) == NULL)
+	if ((local = ip4_addr_get_preferred(gw->iface_id, gw->ip)) == NULL) {
+		free(msg);
 		return -errno;
+	}
 
 	msg->src = local->ip;
 