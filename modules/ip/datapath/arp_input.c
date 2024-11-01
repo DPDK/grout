DPDK/grout

commit bfae0e36a7b4a2adbce33d8a5d44845c3f8a4336

ip: fix unresolved arp management
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/datapath/arp_input.c
@@ -106,14 +106,15 @@ arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		iface = eth_input_mbuf_data(mbuf)->iface;
 		local = ip4_addr_get(iface->id);
 		sip = arp->arp_data.arp_sip;
+		remote = ip4_route_lookup(iface->vrf_id, sip);
 
-		if (ip4_nexthop_lookup(iface->vrf_id, sip, &idx, &remote) >= 0) {
+		if (remote != NULL && remote->ip == sip) {
 			update_nexthop(graph, node, remote, now, iface->id, arp);
 		} else if (local != NULL && local->ip == arp->arp_data.arp_tip) {
 			// Request/reply to our address but no next hop entry exists.
 			// Create a new next hop and its associated /32 route to allow
 			// faster lookups for next packets.
-			if (ip4_nexthop_lookup_add(iface->vrf_id, sip, &idx, &remote) < 0) {
+			if (ip4_nexthop_add(iface->vrf_id, sip, &idx, &remote) < 0) {
 				next = ERROR;
 				goto next;
 			}

modules/ip/control/address.c
@@ -58,7 +58,7 @@ static struct api_out addr_add(const void *request, void **response) {
 	if (ip4_nexthop_lookup(iface->vrf_id, req->addr.addr.ip, &nh_idx, &nh) == 0)
 		return api_out(EADDRINUSE, 0);
 
-	if ((ret = ip4_nexthop_lookup_add(iface->vrf_id, req->addr.addr.ip, &nh_idx, &nh)) < 0)
+	if ((ret = ip4_nexthop_add(iface->vrf_id, req->addr.addr.ip, &nh_idx, &nh)) < 0)
 		return api_out(-ret, 0);
 
 	nh->iface_id = req->addr.iface_id;

modules/ip/control/br_ip4_control.h
@@ -38,7 +38,7 @@ struct __rte_cache_aligned nexthop {
 
 struct nexthop *ip4_nexthop_get(uint32_t idx);
 int ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh);
-int ip4_nexthop_lookup_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh);
+int ip4_nexthop_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh);
 void ip4_nexthop_incref(struct nexthop *);
 void ip4_nexthop_decref(struct nexthop *);
 

modules/ip/control/nexthop.c
@@ -45,17 +45,19 @@ int ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nex
 	return 0;
 }
 
-int ip4_nexthop_lookup_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh) {
-	if (ip4_nexthop_lookup(vrf_id, ip, idx, nh) < 0) {
-		struct nexthop_key key = {vrf_id, ip};
-		int32_t nh_idx = rte_hash_add_key(nh_hash, &key);
-		if (nh_idx < 0)
-			return errno_set(-nh_idx);
-		nh_array[nh_idx].vrf_id = vrf_id;
-		nh_array[nh_idx].ip = ip;
-		*idx = nh_idx;
-		*nh = &nh_array[nh_idx];
-	}
+int ip4_nexthop_add(uint16_t vrf_id, ip4_addr_t ip, uint32_t *idx, struct nexthop **nh) {
+	struct nexthop_key key = {vrf_id, ip};
+	int32_t nh_idx = rte_hash_add_key(nh_hash, &key);
+
+	if (nh_idx < 0)
+		return errno_set(-nh_idx);
+
+	nh_array[nh_idx].vrf_id = vrf_id;
+	nh_array[nh_idx].ip = ip;
+
+	*idx = nh_idx;
+	*nh = &nh_array[nh_idx];
+
 	return 0;
 }
 
@@ -93,7 +95,7 @@ static struct api_out nh4_add(const void *request, void **response) {
 		return api_out(EEXIST, 0);
 	}
 
-	if ((ret = ip4_nexthop_lookup_add(req->nh.vrf_id, req->nh.host, &nh_idx, &nh)) < 0)
+	if ((ret = ip4_nexthop_add(req->nh.vrf_id, req->nh.host, &nh_idx, &nh)) < 0)
 		return api_out(-ret, 0);
 
 	nh->iface_id = req->nh.iface_id;

modules/ip/control/route.c
@@ -176,7 +176,7 @@ static struct api_out route4_add(const void *request, void **response) {
 	if ((fib = get_or_create_fib(req->vrf_id)) == NULL)
 		return api_out(errno, 0);
 
-	if (ip4_nexthop_lookup_add(req->vrf_id, req->nh, &nh_idx, &nh) < 0)
+	if (ip4_nexthop_add(req->vrf_id, req->nh, &nh_idx, &nh) < 0)
 		return api_out(errno, 0);
 
 	if ((ret = rte_fib_add(fib, ntohl(req->dest.ip), req->dest.prefixlen, nh_idx)) < 0) {

modules/ip/datapath/ip_output.c
@@ -56,13 +56,14 @@ output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint
 			// Create a new next hop and its associated /32 route so that next
 			// packets take it in priority with a single route lookup.
 			struct nexthop *remote;
-			if (ip4_nexthop_lookup_add(nh->vrf_id, ip->dst_addr, &idx, &remote) < 0) {
+			if (ip4_nexthop_add(nh->vrf_id, ip->dst_addr, &idx, &remote) < 0) {
 				next = ERROR;
 				goto next;
 			}
 			ip4_route_insert(nh->vrf_id, ip->dst_addr, 32, idx, remote);
 			remote->iface_id = nh->iface_id;
-			ip_output_mbuf_data(mbuf)->nh = nh;
+			ip_output_mbuf_data(mbuf)->nh = remote;
+			nh = remote;
 		}
 		if (!(nh->flags & BR_IP4_NH_F_REACHABLE)) {
 			next = ARP_REQUEST;

commit 2e2ab432a54ad83da0cc6fa7584443eaf0352140

arp: fix nexthop lookup
When we have a prefix route associated with a local interface, route
lookup may return a local nexthop.  Returned nexthop IP doesn't
correspond to the arp request/reply ip address.  This causes the ARP
packet to be dropped without updating any next hop.

Do not look for a route, but for a nexthop matching the IP address.

Fixes: bfae0e36a7b4 ("ip: fix unresolved arp management")
Closes: https://github.com/DPDK/grout/issues/89
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <robin@jarry.cc>

modules/ip/datapath/arp_input.c
@@ -109,7 +109,7 @@ arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		sip = arp->arp_data.arp_sip;
 		iface = eth_input_mbuf_data(mbuf)->iface;
 		local = ip4_addr_get_preferred(iface->id, sip);
-		remote = ip4_route_lookup(iface->vrf_id, sip);
+		remote = ip4_nexthop_lookup(iface->vrf_id, sip);
 
 		if (remote != NULL && remote->ip == sip) {
 			update_nexthop(graph, node, remote, now, iface->id, arp);