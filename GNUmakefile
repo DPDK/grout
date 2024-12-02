DPDK/grout

commit 763725c614cc34443bb68a80cef8a8be5425039c

deb,rpm: fix error when no tag is found in local repo
When doing a shallow clone, we don't pull all objects and don't have the
version tags. When that happens, git describe fails:

  $ git describe --long --abbrev=8 --dirty
  fatal: No names found, cannot describe anything.

Fallback to reading the version from meson.build and suffix it with the
current timestamp and the current hostname.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

GNUmakefile
@@ -45,10 +45,16 @@ meson_opts += $(MESON_EXTRA_OPTS)
 $(BUILDDIR)/build.ninja:
 	meson setup $(BUILDDIR) $(meson_opts)
 
-debversion = $(shell git describe --long --abbrev=8 --dirty | sed 's/^v//;s/-/+/')
+empty :=
+space := $(empty) $(empty)
+version := $(shell { git describe --long --abbrev=8 --dirty 2>/dev/null || \
+	sed -En 's/.* \|\| echo (v[0-9\.]+)\>.*/\1-'`date +%Y%m%d%H%M%S`.`hostname`'/p' meson.build; } | \
+	sed 's/^v//;s/-/ /')
+debversion = $(subst $(space),+,$(version))
 
 .PHONY: deb
 deb:
+	$Q rm -f debian/changelog
 	dch --create --package grout --newversion '$(debversion)' -M Development snapshot.
 	dpkg-buildpackage -b
 	$Q arch=`dpkg-architecture -qDEB_HOST_ARCH` && \
@@ -57,8 +63,8 @@ deb:
 			$${name}_$$arch.deb || exit; \
 	done
 
-rpmversion = $(shell git describe --long --abbrev=8 --dirty | sed -E 's/^v//;s/-.+//')
-rpmrelease = $(shell git describe --long --abbrev=8 --dirty | sed -E 's/^v[0-9\.]+-//;s/-/./g').$(shell sed -nE 's/PLATFORM_ID="platform:(.*)"/\1/p' /etc/os-release)
+rpmversion = $(firstword $(version))
+rpmrelease = $(subst -,.,$(lastword $(version))).$(shell sed -nE 's/PLATFORM_ID="platform:(.*)"/\1/p' /etc/os-release)
 
 .PHONY: rpm
 rpm:

commit 53a55e71638af2ce8aff1349086e1956878d3db4

deb,rpm: fix build error when no tag is found
When no tags are found in the current repository (or when not building
from a git repo), git describe --long --abbrev=8 --dirty will return an
error and the obscure sed command will be executed instead to determine
the current version.

When hostname is not installed, it leads to an even more obscure error:

 /bin/sh: line 1: hostname: command not found
 rpmbuild -bb --build-in-place -D 'version 0.2' -D 'release 20241202102621..el9' rpm/grout.spec
 error: line 14: Illegal sequence ".." in: Release: 20241202102621..el9
 make: *** [GNUmakefile:72: rpm] Error 1

Replace the hostname value with a hard-coded "local" string. The machine
where this build took place does not matter.

Fixes: 763725c614cc ("deb,rpm: fix error when no tag is found in local repo")
Signed-off-by: Robin Jarry <robin@jarry.cc>

GNUmakefile
@@ -48,7 +48,7 @@ $(BUILDDIR)/build.ninja:
 empty :=
 space := $(empty) $(empty)
 version := $(shell { git describe --long --abbrev=8 --dirty 2>/dev/null || \
-	sed -En 's/.* \|\| echo (v[0-9\.]+)\>.*/\1-'`date +%Y%m%d%H%M%S`.`hostname`'/p' meson.build; } | \
+	sed -En 's/.* \|\| echo (v[0-9\.]+)\>.*/\1-'`date +%Y%m%d%H%M%S.local`'/p' meson.build; } | \
 	sed 's/^v//;s/-/ /')
 debversion = $(subst $(space),+,$(version))
 