f7ec1d46d4d17b2570d8cce074383e18342ad63a
Christophe Fontaine <cfontain@redhat.com>
2024-12-02 11:47:07 +0000

    infra: forward iface events as notifications
    Forward all interface events as client notifications.
    Implement the counter part in CLI to decode and print the message.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/infra/cli/notifications.c
+++ modules/infra/cli/notifications.c
@@ -8,15 +8,44 @@
 
 static cmd_status_t notifications_dump(const struct gr_api_client *c, const struct ec_pnode *) {
 	struct gr_api_notification *n;
+	struct gr_infra_iface_get_resp *p;
+
 	if (gr_api_client_enable_notifications(c) < 0)
 		return CMD_ERROR;
 
 	while (gr_api_client_recv_notification(c, &n) == 0) {
 		switch (n->type) {
+		case IFACE_EVENT_POST_ADD:
+			if (n->payload_len == sizeof(*p)) {
+				p = (struct gr_infra_iface_get_resp *)&n[1];
+				printf("Iface added: %s\n", p->iface.name);
+			};
+			break;
+		case IFACE_EVENT_PRE_REMOVE:
+			if (n->payload_len == sizeof(*p)) {
+				p = (struct gr_infra_iface_get_resp *)&n[1];
+				printf("Iface deleted: %s\n", p->iface.name);
+			}
+			break;
+		case IFACE_EVENT_STATUS_UP:
+			if (n->payload_len == sizeof(*p)) {
+				p = (struct gr_infra_iface_get_resp *)&n[1];
+				printf("Iface status UP: %s\n", p->iface.name);
+			}
+			break;
+		case IFACE_EVENT_STATUS_DOWN:
+			if (n->payload_len == sizeof(*p)) {
+				p = (struct gr_infra_iface_get_resp *)&n[1];
+				printf("Iface status DOWN: %s\n", p->iface.name);
+			}
+			break;
+		case IFACE_EVENT_POST_RECONFIG:
+			break;
 		default:
 			printf("Unknown notification 0x%x received\n", n->type);
 			break;
-		};
+		}
+
 		free(n);
 	}
 	return 0;
