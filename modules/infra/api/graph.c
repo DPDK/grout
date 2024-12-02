DPDK/grout

commit e526b1a5eb288a3a7f74c5f3bfeccfd9572f20ed

cli: strip trailing nul byte from graph dot export
grout returns a nul terminated buffer. Strip the nul byte before writing
to stdout.

Fixes: d7bae0ed79b0 ("graph: add more control commands")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/cli/graph.c
@@ -21,7 +21,8 @@ static cmd_status_t graph_dump(const struct gr_api_client *c, const struct ec_pn
 		return CMD_ERROR;
 
 	resp = resp_ptr;
-	fwrite(resp->dot, 1, resp->len, stdout);
+	// strip the trailing NUL byte
+	fwrite(resp->dot, 1, resp->len - 1, stdout);
 	free(resp_ptr);
 
 	return CMD_SUCCESS;

commit c2326d1c96edb19edb7428ffe5506c71a9601aa8

graph/dump: remove trailing nul byte from the output
It is pointless to send a nul byte and then to ignore it in the CLI.

Fixes: e526b1a5eb28 ("cli: strip trailing nul byte from graph dot export")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/api/graph.c
@@ -37,15 +37,15 @@ found:
 	if ((ret = rte_graph_export(graph_name, stream)) < 0)
 		goto end;
 	fflush(stream);
-	resp_len = sizeof(*resp) + buf_len + 1;
+	resp_len = sizeof(*resp) + buf_len;
 	if ((resp = calloc(1, resp_len)) == NULL) {
 		ret = -ENOMEM;
 		resp_len = 0;
 		goto end;
 	}
 
-	resp->len = buf_len + 1;
-	memccpy(resp->dot, buf, 0, buf_len + 1);
+	resp->len = buf_len;
+	memccpy(resp->dot, buf, 0, buf_len);
 	*response = resp;
 end:
 	fclose(stream);

modules/infra/cli/graph.c
@@ -19,8 +19,7 @@ static cmd_status_t graph_dump(const struct gr_api_client *c, const struct ec_pn
 		return CMD_ERROR;
 
 	resp = resp_ptr;
-	// strip the trailing NUL byte
-	fwrite(resp->dot, 1, resp->len - 1, stdout);
+	fwrite(resp->dot, 1, resp->len, stdout);
 	free(resp_ptr);
 
 	return CMD_SUCCESS;