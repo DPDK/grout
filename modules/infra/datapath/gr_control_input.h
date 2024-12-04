bb3940fc013537884b256a3829da31a4451aff63
Christophe Fontaine <cfontain@redhat.com>
2024-12-04 15:28:09 +0000

    infra: extend control input to inject rte_mbufs
    With "control_input", the control plane is able to inject a pointer to
    the dataplane ; in order to meet the expectation that all nodes will
    receive a rte_mbuf* array, we allocate the mbuf in the source node
    "control_input_process".
    
    Yet, there are some cases where we want to allocate and fill a rte_mbuf
    from the control plane thread, to avoid a copy later in the dataplane
    thread.
    In order to differentiate if the node expects an abstract pointer or a
    mbuf, modify the register function to accomodate both requirements,
    Update all users of "gr_control_input_register_handler".
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/infra/datapath/gr_control_input.h
+++ modules/infra/datapath/gr_control_input.h
@@ -10,7 +10,7 @@ GR_MBUF_PRIV_DATA_TYPE(control_input_mbuf_data, { void *data; });
 
 typedef uint8_t control_input_t;
 
-control_input_t gr_control_input_register_handler(const char *node_name);
+control_input_t gr_control_input_register_handler(const char *node_name, bool data_is_mbuf);
 
 int post_to_stack(control_input_t type, void *data);
 
