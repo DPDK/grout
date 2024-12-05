257669c97752bc65d67540cf74e2669246e75d7f
Robin Jarry <robin@jarry.cc>
2024-12-05 23:14:39 +0100

    smoke: do not try to delete loopback interfaces
    Deleting loopback interfaces is not possible. Avoid that.
    
    Fixes: 5c91e81e743b ("infra: forbid manual deletion of loopback iface")
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- smoke/_init.sh
+++ smoke/_init.sh
@@ -15,13 +15,13 @@ cleanup() {
 
 	# delete all non-port interfaces first
 	grcli show interface all |
-	grep -Ev -e ^NAME -e '\<port[[:space:]]+devargs=' |
+	grep -Ev -e ^NAME -e '\<port[[:space:]]+devargs=' -e '\<loopback\>' |
 	while read -r name _; do
 		grcli del interface "$name"
 	done
 	# then delete all ports
 	grcli show interface all |
-	grep -v ^NAME |
+	grep -ve ^NAME -e '\<loopback\>' |
 	while read -r name _; do
 		grcli del interface "$name"
 	done
