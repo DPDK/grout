99b20b27600fd4e15e2a5ca15b619991e53c6738
Robin Jarry <robin@jarry.cc>
2024-12-09 12:18:44 +0100

    ip_output: handle unresolved nexthops in control plane
    Do not modify nexthop objects in the data plane. It requires acquiring
    locks and also modifying the routing table. It can lead to races between
    multiple worker threads.
    
    Instead, when a nexthop is unreachable in ip_output, send the mbuf to
    the control plane thread and do whatever is required there:
    
    * Possibly create a new nexthop for prefix routes and create a new /32
      route that points to it.
    * Hold the packet to the nexthop waiting for ARP resolution.
    * Send an ARP request probe (if one hasn't already been sent earlier).
    
    Change all nexthop references to const in the datapath to ensure they
    cannot be modified.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>
    Acked-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip/datapath/ip_output.c
+++ modules/ip/datapath/ip_output.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Robin Jarry
 
+#include <gr_control_output.h>
 #include <gr_datapath.h>
 #include <gr_eth.h>
 #include <gr_graph.h>
@@ -21,9 +22,9 @@
 
 enum {
 	ETH_OUTPUT = 0,
+	HOLD,
 	NO_ROUTE,
 	ERROR,
-	QUEUE_FULL,
 	EDGE_COUNT,
 };
 
@@ -38,46 +39,13 @@ void ip_output_register_interface(uint16_t iface_type_id, const char *next_node)
 	edges[iface_type_id] = gr_node_attach_parent("ip_output", next_node);
 }
 
-typedef enum {
-	OK_TO_SEND,
-	HELD,
-	HOLD_QUEUE_FULL,
-} hold_status_t;
-
-static inline hold_status_t maybe_hold_packet(struct nexthop *nh, struct rte_mbuf *mbuf) {
-	hold_status_t status;
-
-	if (nh->flags & GR_NH_F_REACHABLE) {
-		status = OK_TO_SEND;
-	} else if (nh->held_pkts_num < NH_MAX_HELD_PKTS) {
-		queue_mbuf_data(mbuf)->next = NULL;
-		rte_spinlock_lock(&nh->lock);
-		if (nh->held_pkts_head == NULL)
-			nh->held_pkts_head = mbuf;
-		else
-			queue_mbuf_data(nh->held_pkts_tail)->next = mbuf;
-		nh->held_pkts_tail = mbuf;
-		nh->held_pkts_num++;
-		rte_spinlock_unlock(&nh->lock);
-		if (!(nh->flags & GR_NH_F_PENDING)) {
-			arp_output_request_solicit(nh);
-			nh->flags |= GR_NH_F_PENDING;
-		}
-		status = HELD;
-	} else {
-		status = HOLD_QUEUE_FULL;
-	}
-
-	return status;
-}
-
 static uint16_t
 ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_output_mbuf_data *eth_data;
 	const struct iface *iface;
+	const struct nexthop *nh;
 	struct rte_ipv4_hdr *ip;
 	struct rte_mbuf *mbuf;
-	struct nexthop *nh;
 	uint16_t i, sent;
 	rte_edge_t edge;
 
@@ -105,45 +73,18 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		if (edge != ETH_OUTPUT)
 			goto next;
 
-		if (nh->flags & GR_NH_F_LINK && ip->dst_addr != nh->ipv4) {
-			// The resolved next hop is associated with a "connected" route.
-			// We currently do not have an explicit entry for this destination IP.
-			// Create a new next hop and its associated /32 route so that next
-			// packets take it in priority with a single route lookup.
-			struct nexthop *remote = ip4_nexthop_lookup(nh->vrf_id, ip->dst_addr);
-
-			if (remote == NULL)
-				remote = ip4_nexthop_new(nh->vrf_id, nh->iface_id, ip->dst_addr);
-			else if (remote->flags & GR_NH_F_GATEWAY && remote->iface_id == 0)
-				remote->iface_id = nh->iface_id;
-
-			if (remote == NULL) {
-				edge = ERROR;
-				goto next;
-			}
-
-			assert(remote->iface_id == nh->iface_id);
-			if (ip4_route_insert(nh->vrf_id, ip->dst_addr, 32, remote) < 0) {
-				edge = ERROR;
-				goto next;
-			}
-
-			ip_output_mbuf_data(mbuf)->nh = remote;
-			nh = remote;
-		}
-
-		switch (maybe_hold_packet(nh, mbuf)) {
-		case HELD:
-			// The packet was stored in the next hop hold queue to be flushed upon
-			// reception of an ARP request or reply from the destination IP.
-			continue;
-		case HOLD_QUEUE_FULL:
+		if (!(nh->flags & GR_NH_F_REACHABLE)
+		    || (nh->flags & GR_NH_F_LINK && ip->dst_addr != nh->ipv4)) {
+			// The nexthop needs ARP resolution or it is associated with
+			// a "connected" route (i.e. matching an address/prefix on
+			// a local interface).
 			//
-			edge = QUEUE_FULL;
+			// In the later case, a new nexthop must be created along with
+			// its internal /32 route.
+			//
+			// In both case, the packet must be sent to control plane.
+			edge = HOLD;
 			goto next;
-		case OK_TO_SEND:
-			// Next hop is reachable.
-			break;
 		}
 
 		// Prepare ethernet layer info.
@@ -168,9 +109,9 @@ static struct rte_node_register output_node = {
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
 		[ETH_OUTPUT] = "eth_output",
+		[HOLD] = "ip_hold",
 		[NO_ROUTE] = "ip_error_dest_unreach",
 		[ERROR] = "ip_output_error",
-		[QUEUE_FULL] = "arp_queue_full",
 	},
 };
 
@@ -182,4 +123,3 @@ static struct gr_node_info info = {
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ip_output_error);
-GR_DROP_REGISTER(arp_queue_full);
