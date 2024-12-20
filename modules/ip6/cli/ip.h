8ed8b9d594c96ea53c378e5659c67576095f0b99
Christophe Fontaine <cfontain@redhat.com>
2024-12-20 07:38:35 +0000

    ip6: router advertisement configuration
    Add configuration knobs for RA messages.
    Instead of a hardcoded periodic message, allow a per-interface
    configuration.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip6/cli/ip.h
+++ modules/ip6/cli/ip.h
@@ -9,5 +9,8 @@
 #define IP6_ADD_CTX(root) CLI_CONTEXT(root, CTX_ADD, CTX_ARG("ip6", "Create IPv6 stack elements."))
 #define IP6_DEL_CTX(root) CLI_CONTEXT(root, CTX_DEL, CTX_ARG("ip6", "Delete IPv6 stack elements."))
 #define IP6_SHOW_CTX(root) CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("ip6", "Show IPv6 stack details."))
+#define IP6_SET_CTX(root) CLI_CONTEXT(root, CTX_SET, CTX_ARG("ip6", "Set IPv6 stack elements."))
+#define IP6_CLEAR_CTX(root)                                                                        \
+	CLI_CONTEXT(root, CTX_CLEAR, CTX_ARG("ip6", "Clear IPv6 stack elements."))
 
 #endif
--- modules/ip6/control/router_advert.c
+++ modules/ip6/control/router_advert.c
@@ -5,7 +5,6 @@
 #include <gr_control_input.h>
 #include <gr_iface.h>
 #include <gr_ip6.h>
-#include <gr_ip6_control.h>
 #include <gr_ip6_datapath.h>
 #include <gr_log.h>
 #include <gr_mempool.h>
@@ -19,33 +18,111 @@
 #include <rte_mbuf.h>
 #include <rte_mempool.h>
 
+#define RA_DEFAULT_INTERVAL 600
+#define RA_DEFAULT_LIFETIME 1800
+
 struct ra_ctx {
 	struct rte_mempool *mp;
-	struct event *timer;
 };
 
 static struct ra_ctx ra_ctx;
 static control_input_t ra_output;
+static struct event_base *ev_base;
+
+struct ra_iface_conf {
+	struct event *timer;
+	bool enabled;
+	uint16_t interval;
+	uint16_t lifetime;
+};
+
+static struct ra_iface_conf ra_conf[MAX_IFACES];
 
 static struct api_out iface_ra_set(const void *request, void ** /*response*/) {
 	const struct gr_ip6_ra_set_req *req = request;
+
 	if (iface_from_id(req->iface_id) == NULL)
 		return api_out(errno, 0);
 
+	if (req->set_interval)
+		ra_conf[req->iface_id].interval = req->interval;
+	if (req->set_lifetime)
+		ra_conf[req->iface_id].lifetime = req->lifetime;
+
+	event_add(
+		ra_conf[req->iface_id].timer,
+		&(struct timeval) {.tv_sec = ra_conf[req->iface_id].interval}
+	);
+	event_active(ra_conf[req->iface_id].timer, 0, 0);
 	return api_out(0, 0);
 }
 
 static struct api_out iface_ra_clear(const void *request, void ** /*response*/) {
 	const struct gr_ip6_ra_clear_req *req = request;
+
 	if (iface_from_id(req->iface_id) == NULL)
 		return api_out(errno, 0);
+
+	event_del(ra_conf[req->iface_id].timer);
+	ra_conf[req->iface_id].interval = RA_DEFAULT_INTERVAL;
+	ra_conf[req->iface_id].lifetime = RA_DEFAULT_LIFETIME;
 
 	return api_out(0, 0);
 }
 
+static struct api_out iface_ra_show(const void *request, void **response) {
+	const struct gr_ip6_ra_show_req *req = request;
+	struct gr_ip6_ra_show_resp *resp;
+	uint16_t iface_id, n_ras;
+	struct hoplist *addrs;
+	bool show_all = false;
+	size_t len;
+
+	if (req->iface_id == 0)
+		show_all = true;
+	else if (iface_from_id(req->iface_id) == NULL)
+		return api_out(errno, 0);
+
+	n_ras = 0;
+	for (iface_id = 0; iface_id < MAX_IFACES; iface_id++) {
+		addrs = ip6_addr_get_all(iface_id);
+		if (addrs == NULL || gr_vec_len(addrs) == 0)
+			continue;
+		if (show_all == false && iface_id != req->iface_id)
+			continue;
+		n_ras++;
+	}
+
+	len = sizeof(*resp) + n_ras * sizeof(struct gr_ip6_ra_conf);
+	resp = calloc(1, sizeof(*resp) + n_ras * sizeof(struct gr_ip6_ra_conf));
+	if (!resp)
+		return api_out(ENOMEM, 0);
+	resp->n_ras = n_ras;
+	n_ras = 0;
+	for (uint16_t iface_id = 0; iface_id < MAX_IFACES; iface_id++) {
+		addrs = ip6_addr_get_all(iface_id);
+		if (addrs == NULL || gr_vec_len(addrs) == 0)
+			continue;
+		if (show_all == false && iface_id != req->iface_id)
+			continue;
+
+		resp->ras[n_ras].iface_id = iface_id;
+		resp->ras[n_ras].enabled = event_pending(
+			ra_conf[iface_id].timer, EV_TIMEOUT | EV_READ | EV_WRITE | EV_SIGNAL, 0
+		);
+		resp->ras[n_ras].interval = ra_conf[iface_id].interval;
+		resp->ras[n_ras].lifetime = ra_conf[iface_id].lifetime;
+		n_ras++;
+	}
+
+	*response = resp;
+	return api_out(0, len);
+}
+
 void ndp_router_sollicit_input_cb(struct rte_mbuf *m) {
+	uint16_t iface_id = mbuf_data(m)->iface->id;
 	rte_pktmbuf_free(m);
-	event_active(ra_ctx.timer, 0, 0);
+	event_active(ra_conf[iface_id].timer, 0, 0);
 }
 
 static void build_ra_packet(struct rte_mbuf *m, struct rte_ipv6_addr *srcv6) {
@@ -72,7 +149,7 @@ static void build_ra_packet(struct rte_mbuf *m, struct rte_ipv6_addr *srcv6) {
 	ra->cur_hoplim = IP6_DEFAULT_HOP_LIMIT; // Default TTL for this network
 	ra->managed_addr = 0; // DHCPv6 is available
 	ra->other_config = 0; // DNS available, ...
-	ra->lifetime = RTE_BE16(0); // Not a default router
+	ra->lifetime = rte_cpu_to_be_16(ra_conf[iface_id].lifetime);
 	ra->reachable_time = RTE_BE16(0);
 	ra->retrans_timer = RTE_BE16(0);
 
@@ -92,53 +169,42 @@ static void build_ra_packet(struct rte_mbuf *m, struct rte_ipv6_addr *srcv6) {
 	icmp6->cksum = rte_ipv6_udptcp_cksum(ip, icmp6);
 }
 
-static void send_ra_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
-	struct iface *iface = NULL;
+static void send_ra_cb(evutil_socket_t, short /*what*/, void *priv) {
+	struct iface *iface = priv;
+	struct hoplist *hl;
+	struct nexthop *nh;
 	struct rte_mbuf *m;
 
-	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
-		struct hoplist *hl = ip6_addr_get_all(iface->id);
-		if (hl == NULL)
-			continue;
-		for (unsigned i = 0; i < gr_vec_len(hl); i++) {
-			struct nexthop *nh = hl->nh[i];
-			struct rte_ipv6_addr ip = nh->ipv6;
-			if (nh->family != AF_INET6)
-				continue;
-			if (!rte_ipv6_addr_is_linklocal(&ip))
-				continue;
+	if ((hl = ip6_addr_get_all(iface->id)) == NULL)
+		return;
 
-			if ((m = rte_pktmbuf_alloc(ra_ctx.mp)) == NULL) {
-				LOG(ERR, "rte_pktmbuf_alloc");
-				return;
-			}
-			mbuf_data(m)->iface = iface;
-			build_ra_packet(m, &ip);
-			post_to_stack(ra_output, m);
+	gr_vec_foreach (nh, hl->nh) {
+		struct rte_ipv6_addr ip = nh->ipv6;
+		if (nh->family != AF_INET6)
+			continue;
+		if (!rte_ipv6_addr_is_linklocal(&ip))
+			continue;
+		if ((m = rte_pktmbuf_alloc(ra_ctx.mp)) == NULL) {
+			LOG(ERR, "rte_pktmbuf_alloc");
+			return;
 		}
+		mbuf_data(m)->iface = iface;
+		build_ra_packet(m, &ip);
+		post_to_stack(ra_output, m);
 	}
 }
 
-static void ra_init(struct event_base *ev_base) {
+static void ra_init(struct event_base *base) {
+	ev_base = base;
 	ra_output = gr_control_input_register_handler("ip6_output", true);
 	ra_ctx.mp = gr_pktmbuf_pool_get(SOCKET_ID_ANY, 512);
 	if (ra_ctx.mp == NULL) {
 		ABORT("gr_pktmbuf_pool_get ENOMEM");
 	}
-
-	ra_ctx.timer = event_new(ev_base, -1, EV_PERSIST, send_ra_cb, NULL);
-	if (ra_ctx.timer == NULL) {
-		ABORT("event_new() failed");
-	}
-
-	if (event_add(ra_ctx.timer, &(struct timeval) {.tv_sec = 600}) < 0) {
-		ABORT("event_add() failed");
-	}
 }
 
 static void ra_fini(struct event_base * /*ev_base*/) {
 	gr_pktmbuf_pool_release(ra_ctx.mp, 512);
-	event_free(ra_ctx.timer);
 }
 
 static struct gr_module ra_module = {
@@ -160,8 +226,32 @@ static struct gr_api_handler ra_clear_handler = {
 	.callback = iface_ra_clear,
 };
 
+static struct gr_api_handler ra_show_handler = {
+	.name = "show interface ra",
+	.request_type = GR_IP6_IFACE_RA_SHOW,
+	.callback = iface_ra_show,
+};
+
+static void iface_event_handler(iface_event_t event, struct iface *iface) {
+	if (event == IFACE_EVENT_POST_ADD) {
+		ra_conf[iface->id].interval = RA_DEFAULT_INTERVAL;
+		ra_conf[iface->id].lifetime = RA_DEFAULT_LIFETIME;
+
+		ra_conf[iface->id].timer = event_new(ev_base, -1, EV_PERSIST, send_ra_cb, iface);
+	} else if (event == IFACE_EVENT_PRE_REMOVE) {
+		event_free(ra_conf[iface->id].timer);
+		ra_conf[iface->id].timer = NULL;
+	}
+}
+
+static struct iface_event_handler iface_event_address_handler = {
+	.callback = iface_event_handler,
+};
+
 RTE_INIT(router_advertisement_init) {
 	gr_register_module(&ra_module);
 	gr_register_api_handler(&ra_set_handler);
 	gr_register_api_handler(&ra_clear_handler);
+	gr_register_api_handler(&ra_show_handler);
+	iface_event_register_handler(&iface_event_address_handler);
 }
