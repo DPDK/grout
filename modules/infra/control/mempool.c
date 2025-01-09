DPDK/grout

commit 189ae9d7a5b91cb365b38365b3675eca68c9bb14

infra: add wrapper to create mempools
Instead of creating dedicated mempools for each port and feature,
use a wrapper to allocate a reduced number of mempools.

This will avoid corner cases where the mempool for control input (eg
512) is depleted because the mbuf aren't released by the tx_burst function
immediatly.
These elements are sent back to the mempool only when a threshold is reached,
and this depends on the queue size.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/mempool.c
@@ -1,0 +1,123 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include "gr_mempool.h"
+
+#include <gr_log.h>
+#include <gr_mbuf.h>
+
+struct mempool_tracker {
+	struct rte_mempool *mp;
+	uint32_t reserved;
+};
+
+#define MAX_MEMPOOL_PER_NUMA 32
+#define MEMPOOL_DEFAULT_SIZE (1 << 16) - 1
+
+static int mt_sort(const void *p1, const void *p2) {
+	const struct mempool_tracker *mt1 = p1;
+	const struct mempool_tracker *mt2 = p2;
+
+	if (mt1->mp == mt2->mp)
+		return 0;
+
+	if (mt1->mp && mt2->mp)
+		return (mt2->mp->size - mt2->reserved) - (mt1->mp->size - mt1->reserved);
+
+	return mt1->mp ? -1 : 1;
+}
+
+// 1 mempool tracker for each numa + SOCKET_ID_ANY
+#define MT_COUNT RTE_MAX_NUMA_NODES + 1
+static struct mempool_tracker trackers[MT_COUNT][MAX_MEMPOOL_PER_NUMA];
+static uint32_t mempool_default_size = MEMPOOL_DEFAULT_SIZE;
+
+struct rte_mempool *gr_pktmbuf_pool_get(int8_t socket_id, uint32_t count) {
+	char mp_name[RTE_MEMPOOL_NAMESIZE];
+	struct rte_mempool *mp = NULL;
+	uint32_t alloc_size;
+
+	if (socket_id < SOCKET_ID_ANY || socket_id >= RTE_MAX_NUMA_NODES)
+		return errno_set_null(EINVAL);
+
+	for (int i = 0; i < MAX_MEMPOOL_PER_NUMA; i++) {
+		unsigned mt_index = socket_id == SOCKET_ID_ANY ? 0 : socket_id + 1;
+		struct mempool_tracker *mt = &trackers[mt_index][i];
+		if (mt->mp == NULL) {
+			alloc_size = mempool_default_size;
+			if (count > mempool_default_size / 4) {
+				alloc_size = count * 2;
+				alloc_size = rte_align32pow2(alloc_size) - 1;
+				// For future mempools, increase default size;
+				mempool_default_size = alloc_size;
+			}
+			sprintf(mp_name, "mbuf_%d:%d", socket_id, i);
+			LOG(DEBUG,
+			    "allocate mempool %s reserved %u (size %u)",
+			    mp_name,
+			    count,
+			    alloc_size);
+			mt->mp = rte_pktmbuf_pool_create(
+				mp_name,
+				alloc_size,
+				RTE_MEMPOOL_CACHE_MAX_SIZE,
+				GR_MBUF_PRIV_MAX_SIZE,
+				RTE_MBUF_DEFAULT_BUF_SIZE,
+				socket_id
+			);
+			if (mt->mp == NULL)
+				return errno_set_null(rte_errno);
+			mt->reserved = count;
+			mp = mt->mp;
+			break;
+		} else if ((count + mt->reserved) <= mt->mp->size) {
+			LOG(DEBUG,
+			    "reuse mempool %s reserved %u -> %u (size %u)",
+			    mt->mp->name,
+			    mt->reserved,
+			    mt->reserved + count,
+			    mt->mp->size);
+			mt->reserved += count;
+			mp = mt->mp;
+			break;
+		}
+	}
+
+	for (int s = 0; s < MT_COUNT; s++) {
+		struct mempool_tracker *mt = trackers[s];
+		qsort(mt, MAX_MEMPOOL_PER_NUMA, sizeof(*mt), mt_sort);
+	}
+
+	return mp;
+}
+
+void gr_pktmbuf_pool_release(struct rte_mempool *mp, uint32_t count) {
+	if (mp == NULL)
+		return;
+
+	for (int s = 0; s < MT_COUNT; s++)
+		for (int i = 0; i < MAX_MEMPOOL_PER_NUMA; i++) {
+			struct mempool_tracker *mt = &trackers[s][i];
+			if (mt->mp == mp) {
+				LOG(DEBUG,
+				    "release mempool %s reserved %u -> %u (size %u)",
+				    mt->mp->name,
+				    mt->reserved,
+				    mt->reserved - count,
+				    mt->mp->size);
+				mt->reserved -= count;
+				if (mt->reserved <= 0) {
+					LOG(DEBUG, "free mempool %s", mt->mp->name);
+					rte_mempool_free(mp);
+					mt->mp = NULL;
+					mt->reserved = 0;
+				}
+				break;
+			}
+		}
+
+	for (int s = 0; s < MT_COUNT; s++) {
+		struct mempool_tracker *mt = trackers[s];
+		qsort(mt, MAX_MEMPOOL_PER_NUMA, sizeof(*mt), mt_sort);
+	}
+}

modules/infra/control/gr_mempool.h
@@ -1,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#ifndef _GR_MEMPOOL
+#define _GR_MEMPOOL
+
+#include <rte_mempool.h>
+
+struct rte_mempool *gr_pktmbuf_pool_get(int8_t socket_id, uint32_t count);
+void gr_pktmbuf_pool_release(struct rte_mempool *mp, uint32_t count);
+
+#endif

modules/infra/control/meson.build
@@ -3,6 +3,7 @@
 
 src += files(
   'iface.c',
+  'mempool.c',
   'port.c',
   'worker.c',
   'graph.c',

modules/infra/control/worker_test.c
@@ -7,6 +7,7 @@
 #include <gr_cmocka.h>
 #include <gr_control.h>
 #include <gr_infra.h>
+#include <gr_mempool.h>
 #include <gr_port.h>
 #include <gr_queue.h>
 #include <gr_stb_ds.h>
@@ -27,6 +28,8 @@ void gr_register_api_handler(struct gr_api_handler *) { }
 void gr_register_module(struct gr_module *) { }
 void iface_type_register(struct iface_type *) { }
 void iface_event_notify(iface_event_t, struct iface *) { }
+mock_func(struct rte_mempool *, gr_pktmbuf_pool_get(int8_t, uint32_t));
+void gr_pktmbuf_pool_release(struct rte_mempool *, uint32_t) { }
 
 struct iface *iface_next(uint16_t type_id, const struct iface *prev) {
 	uint16_t ifid;
@@ -183,6 +186,7 @@ static void common_mocks(void) {
 	will_return_maybe(__wrap_rte_get_main_lcore, 0);
 	will_return_maybe(__wrap_rte_mempool_free, 0);
 	will_return_maybe(__wrap_rte_pktmbuf_pool_create, 1);
+	will_return_maybe(gr_pktmbuf_pool_get, 1);
 }
 
 static void rxq_assign_main_lcore(void **) {

commit b36ed192a2a903711a1517ef6199b3e47cf9e10f

mempool: fix build error on aarch64
Fix the following error when building on aarch64:

../modules/infra/control/mempool.c: In function ‘gr_pktmbuf_pool_get’:
../modules/infra/control/mempool.c:87:17: error: implicit declaration
                of function ‘qsort’ [-Wimplicit-function-declaration]
   87 |                 qsort(mt, MAX_MEMPOOL_PER_NUMA, sizeof(*mt), mt_sort);
      |                 ^~~~~

Add the missing include of stdlib.h

Fixes: 189ae9d7a5b9 ("infra: add wrapper to create mempools")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/mempool.c
@@ -5,6 +5,8 @@
 #include <gr_mbuf.h>
 #include <gr_mempool.h>
 
+#include <stdlib.h>
+
 struct mempool_tracker {
 	struct rte_mempool *mp;
 	uint32_t reserved;