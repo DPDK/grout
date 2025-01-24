DPDK/grout

commit 8de4c1818e511ffa29c39b6454204254f9def8b2

trace: improve icmp header formatting
Also format ICMP errors. Do not print ICMP identifiers and sequence
numbers for non echo packets.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: David Marchand <david.marchand@redhat.com>

modules/infra/datapath/trace.c
@@ -170,24 +170,53 @@ int trace_icmp_format(
 	const struct rte_icmp_hdr *icmp,
 	size_t /*data_len*/
 ) {
-	size_t n = 0;
-	if (icmp->icmp_type == RTE_ICMP_TYPE_ECHO_REQUEST && icmp->icmp_code == 0) {
-		SAFE_BUF(snprintf, len, " echo request");
-	} else if (icmp->icmp_type == RTE_ICMP_TYPE_ECHO_REPLY && icmp->icmp_code == 0) {
-		SAFE_BUF(snprintf, len, " echo reply");
-	} else {
-		SAFE_BUF(snprintf, len, " type=%hhu code=%hhu", icmp->icmp_type, icmp->icmp_code);
+	switch (icmp->icmp_type) {
+	case RTE_ICMP_TYPE_ECHO_REQUEST:
+		if (icmp->icmp_code == 0)
+			return snprintf(
+				buf,
+				len,
+				"echo request id=%u seq=%u",
+				rte_be_to_cpu_16(icmp->icmp_ident),
+				rte_be_to_cpu_16(icmp->icmp_seq_nb)
+			);
+		break;
+	case RTE_ICMP_TYPE_ECHO_REPLY:
+		if (icmp->icmp_code == 0)
+			return snprintf(
+				buf,
+				len,
+				"echo reply id=%u seq=%u",
+				rte_be_to_cpu_16(icmp->icmp_ident),
+				rte_be_to_cpu_16(icmp->icmp_seq_nb)
+			);
+		break;
+	case RTE_ICMP_TYPE_DEST_UNREACHABLE:
+		switch (icmp->icmp_code) {
+		case RTE_ICMP_CODE_UNREACH_NET:
+			return snprintf(buf, len, "network unreachable");
+		case RTE_ICMP_CODE_UNREACH_HOST:
+			return snprintf(buf, len, "host unreachable");
+		case RTE_ICMP_CODE_UNREACH_PROTO:
+			return snprintf(buf, len, "protocol unreachable");
+		case RTE_ICMP_CODE_UNREACH_PORT:
+			return snprintf(buf, len, "port unreachable");
+		case RTE_ICMP_CODE_UNREACH_FRAG:
+			return snprintf(buf, len, "fragmentation needed and DF set");
+		case RTE_ICMP_CODE_UNREACH_SRC:
+			return snprintf(buf, len, "source route failed");
+		}
+		return snprintf(buf, len, "destination unreachable code=%hhu", icmp->icmp_code);
+	case RTE_ICMP_TYPE_TTL_EXCEEDED:
+		switch (icmp->icmp_code) {
+		case RTE_ICMP_CODE_TTL_EXCEEDED:
+			return snprintf(buf, len, "ttl exceeded in transit");
+		case RTE_ICMP_CODE_TTL_FRAG:
+			return snprintf(buf, len, "fragment reassembly time exceeded");
+		}
+		return snprintf(buf, len, "time exceeded code=%hhu", icmp->icmp_code);
 	}
-	SAFE_BUF(
-		snprintf,
-		len,
-		" id=%u seq=%u",
-		rte_be_to_cpu_16(icmp->icmp_ident),
-		rte_be_to_cpu_16(icmp->icmp_seq_nb)
-	);
-	return n;
-err:
-	return -1;
+	return snprintf(buf, len, "type=%hhu code=%hhu", icmp->icmp_type, icmp->icmp_code);
 }
 
 void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *iface) {

commit 05e76292ce96f15446033bf362957c414151c989

trace: add missing space after ICMP header name
Before:

... / ICMPecho request id=37781 seq=2, (pkt_len=98)
... / ICMPecho request id=37781 seq=2, (pkt_len=98)
... / ICMPecho reply id=37781 seq=2, (pkt_len=98)
... / ICMPecho reply id=37781 seq=2, (pkt_len=98)

After:

... / ICMP echo request id=37781 seq=2, (pkt_len=98)
... / ICMP echo request id=37781 seq=2, (pkt_len=98)
... / ICMP echo reply id=37781 seq=2, (pkt_len=98)
... / ICMP echo reply id=37781 seq=2, (pkt_len=98)

Fixes: 8de4c1818e51 ("trace: improve icmp header formatting")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/datapath/trace.c
@@ -352,7 +352,7 @@ ipv4:
 		case IPPROTO_ICMP: {
 			const struct rte_icmp_hdr *icmp;
 			icmp = rte_pktmbuf_mtod_offset(m, const struct rte_icmp_hdr *, offset);
-			SAFE_BUF(snprintf, sizeof(buf), " / ICMP");
+			SAFE_BUF(snprintf, sizeof(buf), " / ICMP ");
 			SAFE_BUF(trace_icmp_format, sizeof(buf), icmp, sizeof(*icmp));
 			break;
 		}