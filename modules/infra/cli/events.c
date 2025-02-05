e388ba0c9c9438a9d9cbac7d636c4b9c5b7bedb6
Robin Jarry <robin@jarry.cc>
2025-02-05 09:56:06 +0100

    events: cleanup cli code
    Prefix all events with > to better separate them from the rest of the
    text in the CLI output. Use all lowercase text for consistency. Simplify
    CLI command registration.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- modules/infra/cli/events.c
+++ modules/infra/cli/events.c
@@ -24,33 +24,33 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IFACE_EVENT_POST_ADD:
 			assert(e->payload_len == sizeof(*p));
 			p = PAYLOAD(e);
-			printf("Iface added: %s\n", p->iface.name);
+			printf("> iface add: %s\n", p->iface.name);
 			break;
 		case IFACE_EVENT_PRE_REMOVE:
 			assert(e->payload_len == sizeof(*p));
 			p = PAYLOAD(e);
-			printf("Iface deleted: %s\n", p->iface.name);
+			printf("> iface del: %s\n", p->iface.name);
 			break;
 		case IFACE_EVENT_STATUS_UP:
 			assert(e->payload_len == sizeof(*p));
 			p = PAYLOAD(e);
-			printf("Iface status UP: %s\n", p->iface.name);
+			printf("> iface up: %s\n", p->iface.name);
 			break;
 		case IFACE_EVENT_STATUS_DOWN:
 			assert(e->payload_len == sizeof(*p));
 			p = PAYLOAD(e);
-			printf("Iface status DOWN: %s\n", p->iface.name);
+			printf("> iface down: %s\n", p->iface.name);
 			break;
 		case IFACE_EVENT_POST_RECONFIG:
 			assert(e->payload_len == sizeof(*p));
 			p = PAYLOAD(e);
-			printf("Iface reconfigured: %s\n", p->iface.name);
+			printf("> iface reconf: %s\n", p->iface.name);
 			break;
 		case IP_EVENT_ADDR_ADD:
 		case IP6_EVENT_ADDR_ADD:
 			assert(e->payload_len == sizeof(*nh));
 			nh = PAYLOAD(e);
-			printf("IP address add: iface[%d] " ADDR_F "\n",
+			printf("> addr add: iface[%d] " ADDR_F "\n",
 			       nh->iface_id,
 			       ADDR_W(nh->family),
 			       &nh->addr);
@@ -59,7 +59,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IP6_EVENT_ADDR_DEL:
 			assert(e->payload_len == sizeof(*nh));
 			nh = PAYLOAD(e);
-			printf("IP address del: iface[%d] " ADDR_F "\n",
+			printf("> addr del: iface[%d] " ADDR_F "\n",
 			       nh->iface_id,
 			       ADDR_W(nh->family),
 			       &nh->addr);
@@ -67,7 +67,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IP_EVENT_ROUTE_ADD:
 			assert(e->payload_len == sizeof(*r4));
 			r4 = PAYLOAD(e);
-			printf("IP route add: %4p/%d via %4p\n",
+			printf("> route add: %4p/%d via %4p\n",
 			       &r4->dest.ip,
 			       r4->dest.prefixlen,
 			       &r4->nh);
@@ -75,7 +75,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IP_EVENT_ROUTE_DEL:
 			assert(e->payload_len == sizeof(*r4));
 			r4 = PAYLOAD(e);
-			printf("IP route del: %4p/%d via %4p\n",
+			printf("> route del: %4p/%d via %4p\n",
 			       &r4->dest.ip,
 			       r4->dest.prefixlen,
 			       &r4->nh);
@@ -83,7 +83,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IP6_EVENT_ROUTE_ADD:
 			assert(e->payload_len == sizeof(*r6));
 			r6 = PAYLOAD(e);
-			printf("IP route add: %6p/%d via %6p\n",
+			printf("> route add: %6p/%d via %6p\n",
 			       &r6->dest.ip,
 			       r6->dest.prefixlen,
 			       &r6->nh);
@@ -91,7 +91,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case IP6_EVENT_ROUTE_DEL:
 			assert(e->payload_len == sizeof(*r6));
 			r6 = PAYLOAD(e);
-			printf("IP route del: %6p/%d via %6p\n",
+			printf("> route del: %6p/%d via %6p\n",
 			       &r6->dest.ip,
 			       r6->dest.prefixlen,
 			       &r6->nh);
@@ -99,7 +99,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case NEXTHOP_EVENT_NEW:
 			assert(e->payload_len == sizeof(*nh));
 			nh = PAYLOAD(e);
-			printf("NH new: iface %d vrf %d " ADDR_F " " ETH_F "\n",
+			printf("> nh new: iface %d vrf %d " ADDR_F " " ETH_F "\n",
 			       nh->iface_id,
 			       nh->vrf_id,
 			       ADDR_W(nh->family),
@@ -109,7 +109,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case NEXTHOP_EVENT_DELETE:
 			assert(e->payload_len == sizeof(*nh));
 			nh = PAYLOAD(e);
-			printf("NH del: iface %d vrf %d " ADDR_F " " ETH_F "\n",
+			printf("> nh del: iface %d vrf %d " ADDR_F " " ETH_F "\n",
 			       nh->iface_id,
 			       nh->vrf_id,
 			       ADDR_W(nh->family),
@@ -119,7 +119,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 		case NEXTHOP_EVENT_UPDATE:
 			assert(e->payload_len == sizeof(*nh));
 			nh = PAYLOAD(e);
-			printf("NH update: iface %d vrf %d " ADDR_F " " ETH_F "\n",
+			printf("> nh update: iface %d vrf %d " ADDR_F " " ETH_F "\n",
 			       nh->iface_id,
 			       nh->vrf_id,
 			       ADDR_W(nh->family),
@@ -127,7 +127,7 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 			       &nh->mac);
 			break;
 		default:
-			printf("Unknown event 0x%x received\n", e->ev_type);
+			printf("> unknown event 0x%08x\n", e->ev_type);
 			break;
 		}
 		free(e);
@@ -139,19 +139,12 @@ static cmd_status_t events_show(const struct gr_api_client *c, const struct ec_p
 }
 
 static int ctx_init(struct ec_node *root) {
-	int ret;
-
-	ret = CLI_COMMAND(
+	return CLI_COMMAND(
 		CLI_CONTEXT(root, CTX_SHOW),
 		"events",
 		events_show,
 		"Subscribe to all events and dump them in real time"
 	);
-
-	if (ret < 0)
-		return ret;
-
-	return 0;
 }
 
 static struct gr_cli_context ctx = {
