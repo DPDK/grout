DPDK/grout

commit 2a656ae330f0b5e5a5fab1d882288c51ede1715a

deb: generate on ubuntu 24.04
Debian stable (bookworm) only has GCC 12. GCC 13 is not available in the
backports repositories.

Ubuntu 24.04 has GCC 13 which is now the minimum version required by
grout. Change the DEB build environment accordingly.

Signed-off-by: Robin Jarry <robin@jarry.cc>

.github/workflows/publish.yml
@@ -22,13 +22,12 @@ jobs:
     permissions:
       actions: write
     runs-on: ubuntu-24.04
-    container: "debian:stable"
     steps:
       - name: install system dependencies
         run: |
           set -xe
-          apt-get update -qy
-          NEEDRESTART_MODE=l apt-get install -qy --no-install-recommends \
+          sudo apt-get update -qy
+          sudo apt-get install -qy --no-install-recommends \
             git build-essential meson ninja-build pkgconf go-md2man python3-pyelftools \
             libcmocka-dev libedit-dev libevent-dev libnuma-dev \
             libsmartcols-dev libarchive-dev libibverbs-dev \

commit 8ee921947fdf7de74c334e655d14d8ab766445df

deb: handle new debug package format
Generating .deb packages on ubuntu implies generating automatic debug
packages with a different format that is not yet available in debian
stable.

Adjust code accordingly.

Fixes: 2a656ae330f0 ("deb: generate on ubuntu 24.04")
Link: https://wiki.debian.org/AutomaticDebugPackages
Link: https://wiki.ubuntu.com/AptElfDebugSymbols
Signed-off-by: Robin Jarry <robin@jarry.cc>

.github/workflows/publish.yml
@@ -41,7 +41,9 @@ jobs:
       - uses: actions/upload-artifact@v4
         with:
           name: deb-packages
-          path: grout*.deb
+          path: |
+            grout*.deb
+            grout*.ddeb
           retention-days: 5
 
   rpm:
@@ -99,4 +101,4 @@ jobs:
             grout-debugsource.x86_64.rpm
             grout_amd64.deb
             grout-dev_all.deb
-            grout-dbgsym_amd64.deb
+            grout-dbgsym_amd64.ddeb

GNUmakefile
@@ -74,10 +74,8 @@ deb:
 	dpkg-buildpackage -b
 	$Q arch=`dpkg-architecture -qDEB_HOST_ARCH` && \
 	mv -vf ../grout-dev_$(debversion)_all.deb grout-dev_all.deb && \
-	for name in grout grout-dbgsym; do \
-		mv -vf ../$${name}_$(debversion)_$$arch.deb \
-			$${name}_$$arch.deb || exit; \
-	done
+	mv -vf ../grout_$(debversion)_$$arch.deb grout_$$arch.deb && \
+	mv -vf ../grout-dbgsym_$(debversion)_$$arch.ddeb grout-dbgsym_$$arch.ddeb
 
 rpmversion = $(firstword $(version))
 rpmrelease = $(subst -,.,$(lastword $(version))).$(shell sed -nE 's/PLATFORM_ID="platform:(.*)"/\1/p' /etc/os-release)