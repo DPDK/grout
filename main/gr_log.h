903f06a70a3afbecd5f8e607834e43b932e5383c
Christophe Fontaine <cfontain@redhat.com>
2025-02-19 11:55:18 +0000

    clang15: fix compilation
    __builtin_strchr is available only with clang 16.
    Reuse the existing macro RTE_LOG_CHECK_NO_NEWLINE to build with clang 15
    available with RHEL 9.2.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- main/gr_log.h
+++ main/gr_log.h
@@ -16,9 +16,7 @@ extern int gr_rte_log_type;
 
 #define LOG(level, fmt, ...)                                                                       \
 	do {                                                                                       \
-		static_assert(                                                                     \
-			!__builtin_strchr(fmt, '\n'), "This log format string contains a \\n"      \
-		);                                                                                 \
+		RTE_LOG_CHECK_NO_NEWLINE(fmt);                                                     \
 		RTE_LOG(level, GROUT, "%s: " fmt "\n", __func__ __VA_OPT__(, ) __VA_ARGS__);       \
 	} while (0)
 
