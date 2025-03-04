ed23a64373e75f5a422ed67f33f71c646ab9e8a0
Christophe Fontaine <cfontain@redhat.com>
2025-03-04 21:13:30 +0000

    ip6: fix ip6 address deletion
    We couldn't add a previously added IPv6 on an interface, either through
    the command line, or for link local ip when we add/delete/add an interface.
    
    In rib6_cleanup, call rib6_delete with the prefixlen instead of the max
    prefix.
    
    Add these checks in the existing test.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- smoke/ip6_add_del_test.sh
+++ smoke/ip6_add_del_test.sh
@@ -7,8 +7,12 @@
 p0=${run_id}0
 
 grcli add interface port $p0 devargs net_tap0,iface=$p0 mac f0:0d:ac:dc:00:00
+grcli del interface $p0
+grcli add interface port $p0 devargs net_tap0,iface=$p0 mac f0:0d:ac:dc:00:00
 
 grcli add ip6 address 2001::1/64 iface $p0
 grcli show ip6 address
 grcli del ip6 address 2001::1/64 iface $p0
+grcli show ip6 address
+grcli add ip6 address 2001::1/64 iface $p0
 grcli show ip6 address
