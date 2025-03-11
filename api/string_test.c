9db378ca584800ac9d49171792838c04814abef4
Robin Jarry <robin@jarry.cc>
2025-03-11 16:25:13 +0100

    api: add cpuset_parse in gr_string.h
    Add a function to parse a list of CPUs to a cpu_set_t object. Add unit
    tests to ensure it works as expected.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- api/string_test.c
+++ api/string_test.c
@@ -10,7 +10,6 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/random.h>
 
 #define assert_errno_equal(call, errnum)                                                           \
 	do {                                                                                       \
@@ -68,9 +67,61 @@ static void format(void **) {
 	assert_string_equal(buf, "0-1023");
 }
 
+static void _assert_cpuset_equal(const char *s, const cpu_set_t *set, ...) {
+	cpu_set_t expected, x, missing, extraneous;
+	char buf[256];
+	va_list ap;
+
+	CPU_ZERO(&expected);
+	va_start(ap, set);
+	for (int cpu = va_arg(ap, int); cpu != -1; cpu = va_arg(ap, int))
+		CPU_SET(cpu, &expected);
+	va_end(ap);
+
+	CPU_XOR(&x, set, &expected);
+
+	CPU_AND(&missing, &x, &expected);
+	if (CPU_COUNT(&missing) > 0) {
+		cpuset_format(buf, sizeof(buf), &missing);
+		fail_msg("cpuset_parse(\"%s\") missing cpus \"%s\"", s, buf);
+	}
+
+	CPU_AND(&extraneous, &x, set);
+	if (CPU_COUNT(&extraneous) > 0) {
+		cpuset_format(buf, sizeof(buf), &extraneous);
+		fail_msg("cpuset_parse(\"%s\") extraneous cpus \"%s\"", s, buf);
+	}
+}
+
+#define assert_cpuset_equal(s, set, ...) _assert_cpuset_equal(s, set, __VA_ARGS__, -1)
+
+static void parse(void **) {
+	cpu_set_t set;
+
+	assert_errno_equal(cpuset_parse(&set, NULL), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, ""), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "foobaz"), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "--"), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "2-,2"), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "(╯°□°)╯︵ ʇnoɹƃ"), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "\xba\xd0\xca\xca"), EINVAL);
+	assert_errno_equal(cpuset_parse(&set, "21351"), EOVERFLOW);
+	assert_errno_equal(cpuset_parse(&set, "10-5"), ERANGE);
+
+	assert_errno_equal(cpuset_parse(&set, "5-10"), 0);
+	assert_cpuset_equal("5-10", &set, 5, 6, 7, 8, 9, 10);
+
+	assert_errno_equal(cpuset_parse(&set, "1,8,99-101"), 0);
+	assert_cpuset_equal("1,8,99-101", &set, 1, 8, 99, 100, 101);
+
+	assert_errno_equal(cpuset_parse(&set, "40-44,1,4-8,1,1"), 0);
+	assert_cpuset_equal("40-44,1,1,1,4-8", &set, 1, 4, 5, 6, 7, 8, 40, 41, 42, 43, 44);
+}
+
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(format),
+		cmocka_unit_test(parse),
 	};
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
