DPDK/grout

commit d221e115343efc34a3ae48ce2cfd4a3130b43c9a

infra: rework iface creation reconfig apis
Interface creation and reconfig apis are taking many parameters.
Simplify the api by using a gr_iface struct element pointer instead.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/api/iface.c
@@ -30,14 +30,7 @@ static struct api_out iface_add(const void *request, void **response) {
 	struct gr_infra_iface_add_resp *resp;
 	struct iface *iface;
 
-	iface = iface_create(
-		req->iface.type,
-		req->iface.flags,
-		req->iface.mtu,
-		req->iface.vrf_id,
-		req->iface.name,
-		req->iface.info
-	);
+	iface = iface_create(&req->iface, req->iface.info);
 	if (iface == NULL)
 		return api_out(errno, 0);
 
@@ -112,15 +105,7 @@ static struct api_out iface_set(const void *request, void ** /*response*/) {
 	const struct gr_infra_iface_set_req *req = request;
 	int ret;
 
-	ret = iface_reconfig(
-		req->iface.id,
-		req->set_attrs,
-		req->iface.flags,
-		req->iface.mtu,
-		req->iface.vrf_id,
-		req->iface.name,
-		req->iface.info
-	);
+	ret = iface_reconfig(req->iface.id, req->set_attrs, &req->iface, NULL);
 	if (ret < 0)
 		return api_out(errno, 0);
 

modules/infra/control/iface.c
@@ -51,24 +51,17 @@ static int next_ifid(uint16_t *ifid) {
 	return errno_set(ENOSPC);
 }
 
-struct iface *iface_create(
-	gr_iface_type_t type_id,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
-	const char *name,
-	const void *api_info
-) {
-	struct iface_type *type = iface_type_get(type_id);
+struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
+	struct iface_type *type = iface_type_get(conf->type);
 	struct iface *iface = NULL;
 	uint16_t ifid;
 
 	if (type == NULL)
 		goto fail;
-	if (utf8_check(name, GR_IFACE_NAME_SIZE) < 0)
+	if (utf8_check(conf->name, GR_IFACE_NAME_SIZE) < 0)
 		goto fail;
 	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
-		if (strcmp(name, iface->name) == 0) {
+		if (strcmp(conf->name, iface->name) == 0) {
 			iface = NULL;
 			errno = EEXIST;
 			goto fail;
@@ -84,12 +77,12 @@ struct iface *iface_create(
 		goto fail;
 
 	iface->id = ifid;
-	iface->type_id = type_id;
-	iface->flags = flags;
-	iface->mtu = mtu;
-	iface->vrf_id = vrf_id;
+	iface->type_id = conf->type;
+	iface->flags = conf->flags;
+	iface->mtu = conf->mtu;
+	iface->vrf_id = conf->vrf_id;
 	// this is only accessed by the API, no need to copy the name to DPDK memory (hugepages)
-	iface->name = strndup(name, GR_IFACE_NAME_SIZE);
+	iface->name = strndup(conf->name, GR_IFACE_NAME_SIZE);
 	if (iface->name == NULL)
 		goto fail;
 
@@ -111,10 +104,7 @@ fail:
 int iface_reconfig(
 	uint16_t ifid,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
-	const char *name,
+	const struct gr_iface *conf,
 	const void *api_info
 ) {
 	struct iface_type *type;
@@ -125,15 +115,15 @@ int iface_reconfig(
 	if ((iface = iface_from_id(ifid)) == NULL)
 		return -errno;
 	if (set_attrs & GR_IFACE_SET_NAME) {
-		if (utf8_check(name, GR_IFACE_NAME_SIZE) < 0)
+		if (utf8_check(conf->name, GR_IFACE_NAME_SIZE) < 0)
 			return -errno;
 
 		const struct iface *i = NULL;
 		while ((i = iface_next(GR_IFACE_TYPE_UNDEF, i)) != NULL)
-			if (i != iface && strcmp(name, i->name) == 0)
+			if (i != iface && strcmp(conf->name, i->name) == 0)
 				return errno_set(EEXIST);
 
-		char *new_name = strndup(name, GR_IFACE_NAME_SIZE);
+		char *new_name = strndup(conf->name, GR_IFACE_NAME_SIZE);
 		if (new_name == NULL)
 			return errno_set(ENOMEM);
 		free(iface->name);
@@ -142,7 +132,7 @@ int iface_reconfig(
 
 	type = iface_type_get(iface->type_id);
 	assert(type != NULL);
-	return type->reconfig(iface, set_attrs, flags, mtu, vrf_id, api_info);
+	return type->reconfig(iface, set_attrs, conf, api_info);
 }
 
 uint16_t ifaces_count(gr_iface_type_t type_id) {

modules/infra/control/gr_iface.h
@@ -31,9 +31,7 @@ typedef int (*iface_init_t)(struct iface *, const void *api_info);
 typedef int (*iface_reconfig_t)(
 	struct iface *,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
+	const struct gr_iface *,
 	const void *api_info
 );
 typedef int (*iface_fini_t)(struct iface *);
@@ -57,21 +55,11 @@ struct iface_type {
 
 void iface_type_register(struct iface_type *);
 struct iface_type *iface_type_get(gr_iface_type_t type_id);
-struct iface *iface_create(
-	gr_iface_type_t type_id,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
-	const char *name,
-	const void *api_info
-);
+struct iface *iface_create(const struct gr_iface *conf, const void *api_info);
 int iface_reconfig(
 	uint16_t ifid,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
-	const char *name,
+	const struct gr_iface *conf,
 	const void *api_info
 );
 int iface_destroy(uint16_t ifid);

modules/infra/control/gr_port.h
@@ -47,9 +47,7 @@ uint32_t port_get_rxq_buffer_us(uint16_t port_id, uint16_t rxq_id);
 int iface_port_reconfig(
 	struct iface *iface,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
+	const struct gr_iface *conf,
 	const void *api_info
 );
 const struct iface *port_get_iface(uint16_t port_id);

modules/infra/control/loopback.c
@@ -153,9 +153,9 @@ err:
 }
 
 struct iface *iface_loopback_create(uint16_t vrf_id) {
-	char ifname[64];
-	snprintf(ifname, sizeof(ifname), "gr-loop%d", vrf_id);
-	return iface_create(GR_IFACE_TYPE_LOOPBACK, 0, 1500, vrf_id, ifname, NULL);
+	struct gr_iface conf = {.type = GR_IFACE_TYPE_LOOPBACK, .mtu = 1500, .vrf_id = vrf_id};
+	snprintf(conf.name, sizeof(conf.name), "gr-loop%d", vrf_id);
+	return iface_create(&conf, NULL);
 }
 
 int iface_loopback_delete(uint16_t vrf_id) {

modules/infra/control/port.c
@@ -208,9 +208,7 @@ static int port_configure(struct iface_info_port *p) {
 int iface_port_reconfig(
 	struct iface *iface,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
+	const struct gr_iface *conf,
 	const void *api_info
 ) {
 	struct iface_info_port *p = (struct iface_info_port *)iface->info;
@@ -243,7 +241,7 @@ int iface_port_reconfig(
 		return ret;
 
 	if (set_attrs & GR_IFACE_SET_FLAGS) {
-		if (flags & GR_IFACE_F_PROMISC)
+		if (conf->flags & GR_IFACE_F_PROMISC)
 			ret = rte_eth_promiscuous_enable(p->port_id);
 		else
 			ret = rte_eth_promiscuous_disable(p->port_id);
@@ -254,7 +252,7 @@ int iface_port_reconfig(
 		else
 			iface->flags &= ~GR_IFACE_F_PROMISC;
 
-		if (flags & GR_IFACE_F_ALLMULTI)
+		if (conf->flags & GR_IFACE_F_ALLMULTI)
 			ret = rte_eth_allmulticast_enable(p->port_id);
 		else
 			ret = rte_eth_allmulticast_disable(p->port_id);
@@ -265,7 +263,7 @@ int iface_port_reconfig(
 		else
 			iface->flags &= ~GR_IFACE_F_ALLMULTI;
 
-		if (flags & GR_IFACE_F_UP) {
+		if (conf->flags & GR_IFACE_F_UP) {
 			ret = rte_eth_dev_set_link_up(p->port_id);
 			iface->flags |= GR_IFACE_F_UP;
 			gr_event_push(IFACE_EVENT_STATUS_UP, iface);
@@ -278,17 +276,17 @@ int iface_port_reconfig(
 			errno_log(-ret, "rte_eth_dev_set_link_{up,down}");
 	}
 
-	if ((set_attrs & GR_IFACE_SET_MTU) && mtu != 0) {
-		if ((ret = rte_eth_dev_set_mtu(p->port_id, mtu)) < 0)
+	if ((set_attrs & GR_IFACE_SET_MTU) && conf->mtu != 0) {
+		if ((ret = rte_eth_dev_set_mtu(p->port_id, conf->mtu)) < 0)
 			return errno_log(-ret, "rte_eth_dev_set_mtu");
-		iface->mtu = mtu;
+		iface->mtu = conf->mtu;
 	} else {
 		if ((ret = rte_eth_dev_get_mtu(p->port_id, &iface->mtu)) < 0)
 			return errno_log(-ret, "rte_eth_dev_get_mtu");
 	}
 
 	if (set_attrs & GR_IFACE_SET_VRF)
-		iface->vrf_id = vrf_id;
+		iface->vrf_id = conf->vrf_id;
 
 	if ((set_attrs & GR_PORT_SET_MAC) && !rte_is_zero_ether_addr(&api->mac)) {
 		struct rte_ether_addr mac = api->mac;
@@ -354,14 +352,10 @@ static int iface_port_fini(struct iface *iface) {
 		struct iface *iface = NULL;
 		while ((iface = iface_next(GR_IFACE_TYPE_PORT, iface)) != NULL) {
 			struct iface_info_port p = {.n_txq = 0};
-			ret = iface_port_reconfig(
-				iface,
-				GR_PORT_SET_N_TXQS,
-				iface->flags,
-				iface->mtu,
-				iface->vrf_id,
-				&p
-			);
+			struct gr_iface conf = {
+				.flags = iface->flags, .mtu = iface->mtu, .vrf_id = iface->vrf_id
+			};
+			ret = iface_port_reconfig(iface, GR_PORT_SET_N_TXQS, &conf, &p);
 			if (ret < 0)
 				goto out;
 		}
@@ -375,6 +369,7 @@ static int iface_port_init(struct iface *iface, const void *api_info) {
 	const struct gr_iface_info_port *api = api_info;
 	uint16_t port_id = RTE_MAX_ETHPORTS;
 	struct rte_dev_iterator iterator;
+	struct gr_iface conf;
 	int ret;
 
 	RTE_ETH_FOREACH_MATCHING_DEV(port_id, api->devargs, &iterator) {
@@ -399,9 +394,10 @@ static int iface_port_init(struct iface *iface, const void *api_info) {
 		goto fail;
 	}
 
-	ret = iface_port_reconfig(
-		iface, IFACE_SET_ALL, iface->flags, iface->mtu, iface->vrf_id, api_info
-	);
+	conf.flags = iface->flags;
+	conf.mtu = iface->mtu;
+	conf.vrf_id = iface->vrf_id;
+	ret = iface_port_reconfig(iface, IFACE_SET_ALL, &conf, api_info);
 	if (ret < 0) {
 		iface_port_fini(iface);
 		errno = -ret;

modules/infra/control/vlan.c
@@ -50,9 +50,7 @@ static int get_parent_port_id(uint16_t parent_id, uint16_t *port_id) {
 static int iface_vlan_reconfig(
 	struct iface *iface,
 	uint64_t set_attrs,
-	gr_iface_flags_t flags,
-	uint16_t mtu,
-	uint16_t vrf_id,
+	const struct gr_iface *conf,
 	const void *api_info
 ) {
 	struct iface_info_vlan *cur = (struct iface_info_vlan *)iface->info;
@@ -124,11 +122,11 @@ static int iface_vlan_reconfig(
 	}
 
 	if (set_attrs & GR_IFACE_SET_FLAGS)
-		iface->flags = flags;
+		iface->flags = conf->flags;
 	if (set_attrs & GR_IFACE_SET_MTU)
-		iface->mtu = mtu ? mtu : iface_from_id(cur->parent_id)->mtu;
+		iface->mtu = conf->mtu ? conf->mtu : iface_from_id(cur->parent_id)->mtu;
 	if (set_attrs & GR_IFACE_SET_VRF)
-		iface->vrf_id = vrf_id;
+		iface->vrf_id = conf->vrf_id;
 
 	gr_event_push(IFACE_EVENT_POST_RECONFIG, iface);
 
@@ -178,11 +176,12 @@ static int iface_vlan_fini(struct iface *iface) {
 }
 
 static int iface_vlan_init(struct iface *iface, const void *api_info) {
+	const struct gr_iface conf = {
+		.flags = iface->flags, .mtu = iface->mtu, .vrf_id = iface->vrf_id
+	};
 	int ret;
 
-	ret = iface_vlan_reconfig(
-		iface, IFACE_SET_ALL, iface->flags, iface->mtu, iface->vrf_id, api_info
-	);
+	ret = iface_vlan_reconfig(iface, IFACE_SET_ALL, &conf, api_info);
 	if (ret < 0) {
 		iface_vlan_fini(iface);
 		errno = -ret;

modules/infra/control/worker.c
@@ -284,14 +284,10 @@ move:
 		struct iface *iface = NULL;
 
 		while ((iface = iface_next(GR_IFACE_TYPE_PORT, iface)) != NULL) {
-			ret = iface_port_reconfig(
-				iface,
-				GR_PORT_SET_N_TXQS,
-				iface->flags,
-				iface->mtu,
-				iface->vrf_id,
-				&p
-			);
+			struct gr_iface conf = {
+				.flags = iface->flags, .mtu = iface->mtu, .vrf_id = iface->vrf_id
+			};
+			ret = iface_port_reconfig(iface, GR_PORT_SET_N_TXQS, &conf, &p);
 			if (ret < 0)
 				return ret;
 		}

commit 79e9d0071a832f5c9e3915dd61633bb3f4c0b976

infra: fix crash when changing port configuration
When changing any interface type specific configuration, a NULL pointer
dereferences happens. Example when changing the number of RXQs of
a port:

 grout> set interface port p0 rxqs 2

 ==566931==ERROR: AddressSanitizer: SEGV on unknown address
 ==566931==The signal is caused by a READ memory access.
 ==566931==Hint: address points to the zero page.
     #0 0x455ad5 in iface_port_reconfig ../modules/infra/control/port.c:166
     #1 0x42fbcd in iface_reconfig ../modules/infra/control/iface.c:136
     #2 0x40ee3e in iface_set ../modules/infra/api/iface.c:109
     #3 0x406549 in read_cb ../main/api.c:287

 Thread 1 "grout" hit Breakpoint 2, iface_port_reconfig
  (iface=0x7ff7f39f7f00, set_attrs=4294967296, conf=0x513000008e00,
  api_info=0x0) at ../modules/infra/control/port.c:154
 154             struct iface_info_port *p = (struct iface_info_port *)iface->info;
 (gdb) p api_info
 $1 = (const void *) 0x0

The same error may happen when changing the configuration of a vlan or
ipip interface.

Make sure to pass the type specific information down to iface_reconfig
as it is done for iface_create.

Update the config smoke test to ensure it works.

Fixes: d221e115343e ("infra: rework iface creation reconfig apis")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/api/iface.c
@@ -106,7 +106,7 @@ static struct api_out iface_set(const void *request, void ** /*response*/) {
 	const struct gr_infra_iface_set_req *req = request;
 	int ret;
 
-	ret = iface_reconfig(req->iface.id, req->set_attrs, &req->iface, NULL);
+	ret = iface_reconfig(req->iface.id, req->set_attrs, &req->iface, req->iface.info);
 	if (ret < 0)
 		return api_out(errno, 0);
 

smoke/config_test.sh
@@ -12,6 +12,8 @@ grcli add ip route 0.0.0.0/0 via 10.0.0.2
 grcli add ip6 address 2345::1/24 iface p0
 grcli add ip6 address 2346::1/24 iface p1
 grcli add ip6 route ::/0 via 2345::2
+grcli set interface port p0 rxqs 2
+grcli set interface port p1 rxqs 2
 grcli show interface
 grcli show ip route
 grcli show ip nexthop