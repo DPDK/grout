DPDK/grout

commit 61d060f4cdbae73c58335be319f5eb0245c2d715

arp: handle nexthop updates in control plane
Do not modify nexthop objects in the data plane. It requires acquiring
locks and also modifying the routing table. It can lead to races between
multiple worker threads.

Instead, when receiving a valid packet in arp_input_request and
arp_input_reply, make a copy of it
and send the copy to the control plane thread and do whatever is
required there:

* Possibly create a new nexthop for prefix routes and create a new /32
  route that points to it.
* Update the remote nexthop Ethernet address with the information
  provided in the ARP packet.
* If packets are present in the nexthop hold queue, flush it and send
  them to ip_output.

The original mbuf is kept and sent along the graph in the same way as
before (e.g. if it is an ARP request, send a reply).

Change all nexthop references to const in the datapath to ensure they
cannot be modified.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/datapath/arp_output_request.c
@@ -31,6 +31,15 @@ static control_input_t arp_solicit;
 int arp_output_request_solicit(struct nexthop *nh) {
 	if (nh == NULL)
 		return errno_set(EINVAL);
+
+	// This function is called by the control plane main thread.
+	// It is OK to modify the nexthop here.
+	nh->last_request = rte_get_tsc_cycles();
+	if (nh->ucast_probes < NH_UCAST_PROBES)
+		nh->ucast_probes++;
+	else
+		nh->bcast_probes++;
+
 	return post_to_stack(arp_solicit, nh);
 }
 
@@ -41,19 +50,17 @@ static uint16_t arp_output_request_process(
 	uint16_t n_objs
 ) {
 	struct eth_output_mbuf_data *eth_data;
-	struct nexthop *local, *nh;
+	const struct nexthop *local, *nh;
 	struct rte_arp_hdr *arp;
 	struct rte_mbuf *mbuf;
 	rte_edge_t edge;
 	uint16_t sent;
-	uint64_t now;
 
-	now = rte_get_tsc_cycles();
 	sent = 0;
 
 	for (unsigned i = 0; i < n_objs; i++) {
 		mbuf = objs[i];
-		nh = (struct nexthop *)control_input_mbuf_data(mbuf)->data;
+		nh = control_input_mbuf_data(mbuf)->data;
 		local = ip4_addr_get_preferred(nh->iface_id, nh->ipv4);
 
 		if (local == NULL) {
@@ -89,14 +96,10 @@ static uint16_t arp_output_request_process(
 
 		// Prepare ethernet layer info.
 		eth_data = eth_output_mbuf_data(mbuf);
-		if (nh->ucast_probes < NH_UCAST_PROBES) {
+		if (nh->ucast_probes <= NH_UCAST_PROBES)
 			eth_data->dst = arp->arp_data.arp_tha;
-			nh->ucast_probes++;
-		} else {
+		else
 			memset(&eth_data->dst, 0xff, sizeof(eth_data->dst));
-			nh->bcast_probes++;
-		}
-		nh->last_request = now;
 		eth_data->ether_type = RTE_BE16(RTE_ETHER_TYPE_ARP);
 		eth_data->iface = iface_from_id(nh->iface_id);
 

modules/ip/control/gr_ip4_control.h
@@ -24,6 +24,7 @@ struct nexthop *ip4_nexthop_lookup(uint16_t vrf_id, ip4_addr_t ip);
 struct nexthop *ip4_nexthop_new(uint16_t vrf_id, uint16_t iface_id, ip4_addr_t ip);
 
 void ip4_nexthop_unreachable_cb(struct rte_mbuf *m);
+void arp_probe_input_cb(struct rte_mbuf *m);
 
 int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct nexthop *);
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);

modules/ip/control/nexthop.c
@@ -14,6 +14,7 @@
 #include <gr_vec.h>
 
 #include <event2/event.h>
+#include <rte_arp.h>
 #include <rte_errno.h>
 #include <rte_ether.h>
 #include <rte_mempool.h>
@@ -103,6 +104,69 @@ void ip4_nexthop_unreachable_cb(struct rte_mbuf *m) {
 	} else {
 		LOG(DEBUG, IP4_F " hold queue full", &dst);
 	}
+free:
+	rte_pktmbuf_free(m);
+}
+
+void arp_probe_input_cb(struct rte_mbuf *m) {
+	const struct iface *iface;
+	struct rte_arp_hdr *arp;
+	struct rte_mbuf *held;
+	struct nexthop *nh;
+	ip4_addr_t sip;
+
+	arp = rte_pktmbuf_mtod(m, struct rte_arp_hdr *);
+	iface = mbuf_data(m)->iface;
+
+	sip = arp->arp_data.arp_sip;
+	nh = ip4_nexthop_lookup(iface->vrf_id, sip);
+	if (nh == NULL) {
+		// We don't have an entry for the ARP request sender address yet.
+		//
+		// Create one now. If the sender has requested our mac address,
+		// they will certainly contact us soon and it will save us an
+		// ARP request.
+		if ((nh = ip4_nexthop_new(iface->vrf_id, iface->id, sip)) == NULL) {
+			LOG(ERR, "ip4_nexthop_new: %s", strerror(errno));
+			goto free;
+		}
+		// Add an internal /32 route to reference the newly created nexthop.
+		if (ip4_route_insert(iface->vrf_id, sip, 32, nh) < 0) {
+			LOG(ERR, "ip4_nexthop_insert: %s", strerror(errno));
+			goto free;
+		}
+	}
+
+	// static next hops never need updating
+	if (nh->flags & GR_NH_F_STATIC)
+		goto free;
+
+	// Refresh all fields.
+	nh->last_reply = rte_get_tsc_cycles();
+	nh->iface_id = iface->id;
+	nh->flags |= GR_NH_F_REACHABLE;
+	nh->flags &= ~(GR_NH_F_STALE | GR_NH_F_PENDING | GR_NH_F_FAILED);
+	nh->ucast_probes = 0;
+	nh->bcast_probes = 0;
+	nh->lladdr = arp->arp_data.arp_sha;
+
+	// Flush all held packets.
+	held = nh->held_pkts_head;
+	while (held != NULL) {
+		struct ip_output_mbuf_data *o;
+		struct rte_mbuf *next;
+
+		next = queue_mbuf_data(held)->next;
+		o = ip_output_mbuf_data(held);
+		o->nh = nh;
+		o->iface = NULL;
+		post_to_stack(ip_output_node, held);
+		held = next;
+	}
+	nh->held_pkts_head = NULL;
+	nh->held_pkts_tail = NULL;
+	nh->held_pkts_num = 0;
+
 free:
 	rte_pktmbuf_free(m);
 }

modules/ip/datapath/arp_input.c
@@ -24,7 +24,7 @@ enum {
 
 static uint16_t
 arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
-	struct rte_arp_hdr *arp;
+	struct rte_arp_hdr *arp, *t;
 	struct rte_mbuf *mbuf;
 	rte_edge_t edge;
 
@@ -54,7 +54,7 @@ arp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		}
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
-			struct rte_arp_hdr *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
+			t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
 			*t = *arp;
 		}
 		rte_node_enqueue_x1(graph, node, edge, mbuf);

modules/ip/datapath/arp_input_reply.c
@@ -15,53 +15,10 @@
 #include <rte_graph_worker.h>
 
 enum {
-	IP_OUTPUT = 0,
+	CONTROL = 0,
 	DROP,
 	EDGE_COUNT,
 };
-
-// Declaration in gr_ip4_datapath.h. This function is shared with arp_input_request.
-void arp_update_nexthop(
-	struct rte_graph *graph,
-	struct rte_node *node,
-	struct nexthop *nh,
-	const struct iface *iface,
-	const struct rte_ether_addr *mac
-) {
-	struct ip_output_mbuf_data *o;
-	struct rte_mbuf *m, *next;
-
-	// Static next hops never need updating.
-	if (nh->flags & GR_NH_F_STATIC)
-		return;
-
-	rte_spinlock_lock(&nh->lock);
-
-	// Refresh all fields.
-	nh->last_reply = rte_get_tsc_cycles();
-	nh->iface_id = iface->id;
-	nh->flags |= GR_NH_F_REACHABLE;
-	nh->flags &= ~(GR_NH_F_STALE | GR_NH_F_PENDING | GR_NH_F_FAILED);
-	nh->ucast_probes = 0;
-	nh->bcast_probes = 0;
-	nh->lladdr = *mac;
-
-	// Flush all held packets.
-	m = nh->held_pkts_head;
-	while (m != NULL) {
-		next = queue_mbuf_data(m)->next;
-		o = ip_output_mbuf_data(m);
-		o->nh = nh;
-		o->iface = NULL;
-		rte_node_enqueue_x1(graph, node, IP_OUTPUT, m);
-		m = next;
-	}
-	nh->held_pkts_head = NULL;
-	nh->held_pkts_tail = NULL;
-	nh->held_pkts_num = 0;
-
-	rte_spinlock_unlock(&nh->lock);
-}
 
 static uint16_t arp_input_reply_process(
 	struct rte_graph *graph,
@@ -85,10 +42,10 @@ static uint16_t arp_input_reply_process(
 			gr_mbuf_trace_add(mbuf, node, 0);
 
 		if (remote != NULL) {
-			arp_update_nexthop(graph, node, remote, iface, &arp->arp_data.arp_sha);
-			if (gr_mbuf_is_traced(mbuf))
-				gr_mbuf_trace_finish(mbuf);
-			rte_pktmbuf_free(mbuf);
+			struct control_output_mbuf_data *d = control_output_mbuf_data(mbuf);
+			d->callback = arp_probe_input_cb;
+			d->iface = iface;
+			rte_node_enqueue_x1(graph, node, CONTROL, mbuf);
 		} else {
 			rte_node_enqueue_x1(graph, node, DROP, mbuf);
 		}
@@ -104,7 +61,7 @@ static struct rte_node_register node = {
 
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
-		[IP_OUTPUT] = "ip_output",
+		[CONTROL] = "control_output",
 		[DROP] = "arp_input_reply_drop",
 	},
 };

modules/ip/datapath/arp_input_request.c
@@ -15,7 +15,7 @@
 #include <rte_graph_worker.h>
 
 enum {
-	IP_OUTPUT = 0,
+	CONTROL = 0,
 	REPLY,
 	DROP,
 	ERROR,
@@ -28,13 +28,13 @@ static uint16_t arp_input_request_process(
 	void **objs,
 	uint16_t nb_objs
 ) {
-	struct nexthop *remote, *local;
+	struct control_output_mbuf_data *ctrl_data;
 	struct arp_reply_mbuf_data *d;
+	struct rte_mbuf *mbuf, *copy;
 	const struct iface *iface;
 	struct rte_arp_hdr *arp;
-	struct rte_mbuf *mbuf;
+	struct nexthop *local;
 	rte_edge_t edge;
-	ip4_addr_t sip;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
@@ -48,25 +48,17 @@ static uint16_t arp_input_request_process(
 			goto next;
 		}
 
-		sip = arp->arp_data.arp_sip;
-		remote = ip4_nexthop_lookup(iface->vrf_id, sip);
-		if (remote == NULL) {
-			// We don't have an entry for the ARP request sender address yet.
-			//
-			// Create one now. If the sender has requested our mac address,
-			// they will certainly contact us soon and it will save us an
-			// ARP request.
-			if ((remote = ip4_nexthop_new(iface->vrf_id, iface->id, sip)) == NULL) {
-				edge = ERROR;
-				goto next;
-			}
-			// Add an internal /32 route to reference the newly created nexthop.
-			if (ip4_route_insert(iface->vrf_id, sip, 32, remote) < 0) {
-				edge = ERROR;
-				goto next;
-			}
+		copy = rte_pktmbuf_copy(mbuf, mbuf->pool, 0, UINT32_MAX);
+		if (copy == NULL) {
+			edge = ERROR;
+			goto next;
 		}
-		arp_update_nexthop(graph, node, remote, iface, &arp->arp_data.arp_sha);
+		if (gr_mbuf_is_traced(mbuf))
+			gr_mbuf_trace_add(copy, node, 0);
+		ctrl_data = control_output_mbuf_data(copy);
+		ctrl_data->callback = arp_probe_input_cb;
+		ctrl_data->iface = iface;
+		rte_node_enqueue_x1(graph, node, CONTROL, copy);
 
 		edge = REPLY;
 		d = arp_reply_mbuf_data(mbuf);
@@ -87,7 +79,7 @@ static struct rte_node_register node = {
 
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
-		[IP_OUTPUT] = "ip_output",
+		[CONTROL] = "control_output",
 		[REPLY] = "arp_output_reply",
 		[DROP] = "arp_input_request_drop",
 		[ERROR] = "arp_input_request_error",

commit 9266401ac5811b0d0e1a9014a3a95f5fc69ac1d2

nexthop: fix mcast/bcast solicit
After a nexthop expired, and when the peer doesn't respond to
unicast probes, grout never switched back to broadcast (ip) or
multicast (ip6) probes.

Instead of checking on the ucast probe counter, check the bcast probe
counter.

Fixes: 61d060f4cdba ("arp: handle nexthop updates in control plane")
Fixes: 35a067714239 ("ndp: handle nexthop updates in control plane")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip/datapath/arp_output_request.c
@@ -96,7 +96,7 @@ static uint16_t arp_output_request_process(
 
 		// Prepare ethernet layer info.
 		eth_data = eth_output_mbuf_data(mbuf);
-		if (nh->ucast_probes <= NH_UCAST_PROBES)
+		if (nh->bcast_probes == 0)
 			eth_data->dst = arp->arp_data.arp_tha;
 		else
 			memset(&eth_data->dst, 0xff, sizeof(eth_data->dst));

modules/ip6/datapath/ndp_ns_output.c
@@ -93,7 +93,7 @@ static uint16_t ndp_ns_output_process(
 		d = ip6_local_mbuf_data(mbuf);
 		d->iface = iface_from_id(local->iface_id);
 		d->src = local->ipv6;
-		if (nh->last_reply != 0 && nh->ucast_probes <= NH_UCAST_PROBES)
+		if (nh->last_reply != 0 && nh->bcast_probes == 0)
 			d->dst = nh->ipv6;
 		else
 			rte_ipv6_solnode_from_addr(&d->dst, &nh->ipv6);