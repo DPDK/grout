DPDK/grout

commit fe3e408851ceb929771a4eb9f984623334f1a102

route: rework route cleanup routine
Remove the extraneous vrf_id argument.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/control/nexthop.c
@@ -234,8 +234,7 @@ static void nh_gc_cb(struct rte_mempool *, void *, void *obj, unsigned) {
 
 		// this also does ip4_nexthop_decref(), freeing the next hop
 		// and buffered packets.
-		if (ip4_route_delete(nh->vrf_id, nh->ip, 32) < 0)
-			LOG(ERR, "ip4_route_delete: %s", strerror(errno));
+		ip4_route_cleanup(nh);
 	}
 }
 

modules/ip/control/address.c
@@ -134,13 +134,11 @@ static struct api_out addr_del(const void *request, void **response) {
 	if ((nh->flags & (GR_IP4_NH_F_LOCAL | GR_IP4_NH_F_LINK)) || nh->ref_count > 1)
 		return api_out(EBUSY, 0);
 
-	ip4_route_cleanup(nh->vrf_id, nh);
+	ip4_route_cleanup(nh);
 
 	// shift the remaining addresses
-	for (; i < addrs->count; i++) {
-		if (i + 1 < addrs->count)
-			addrs->nh[i] = addrs->nh[i + 1];
-	}
+	for (; i < addrs->count - 1; i++)
+		addrs->nh[i] = addrs->nh[i + 1];
 	addrs->count--;
 
 	return api_out(0, 0);
@@ -194,7 +192,7 @@ static void iface_event_handler(iface_event_t event, struct iface *iface) {
 		return;
 
 	for (unsigned i = 0; i < ifaddrs->count; i++)
-		ip4_route_cleanup(iface->vrf_id, ifaddrs->nh[i]);
+		ip4_route_cleanup(ifaddrs->nh[i]);
 
 	memset(ifaddrs, 0, sizeof(*ifaddrs));
 }

modules/ip/control/gr_ip4_control.h
@@ -64,7 +64,7 @@ int ip4_route_insert(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen, struct n
 int ip4_route_delete(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
 struct nexthop *ip4_route_lookup(uint16_t vrf_id, ip4_addr_t ip);
 struct nexthop *ip4_route_lookup_exact(uint16_t vrf_id, ip4_addr_t ip, uint8_t prefixlen);
-void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *);
+void ip4_route_cleanup(struct nexthop *);
 
 // get the default address for a given interface
 struct nexthop *ip4_addr_get_preferred(uint16_t iface_id, ip4_addr_t dst);

modules/ip/control/route.c
@@ -323,7 +323,7 @@ static void route4_fini(struct event_base *) {
 	vrf_fibs = NULL;
 }
 
-void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
+void ip4_route_cleanup(struct nexthop *nh) {
 	uint8_t prefixlen, local_prefixlen;
 	struct rte_rib_node *rn = NULL;
 	struct rte_rib *rib;
@@ -334,7 +334,7 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 	local_ip = nh->ip;
 	local_prefixlen = nh->prefixlen;
 
-	rib = rte_fib_get_rib(get_fib(vrf_id));
+	rib = rte_fib_get_rib(get_fib(nh->vrf_id));
 	while ((rn = rte_rib_get_nxt(rib, 0, 0, rn, RTE_RIB_GET_NXT_ALL)) != NULL) {
 		rte_rib_get_nh(rn, &nh_id);
 		nh = nh_id_to_ptr(nh_id);
@@ -350,12 +350,12 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 			    prefixlen,
 			    IP4_ADDR_SPLIT(&nh->ip));
 
-			ip4_route_delete(vrf_id, ip, prefixlen);
-			ip4_route_delete(vrf_id, ip, 32);
+			ip4_route_delete(nh->vrf_id, ip, prefixlen);
+			ip4_route_delete(nh->vrf_id, ip, 32);
 		}
 	}
 
-	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL || true) {
+	if ((rn = rte_rib_lookup_exact(rib, 0, 0)) != NULL) {
 		rte_rib_get_nh(rn, &nh_id);
 		nh = nh_id_to_ptr(nh_id);
 
@@ -370,12 +370,12 @@ void ip4_route_cleanup(uint16_t vrf_id, struct nexthop *nh) {
 			    prefixlen,
 			    IP4_ADDR_SPLIT(&nh->ip));
 
-			ip4_route_delete(vrf_id, nh->ip, nh->prefixlen);
-			ip4_route_delete(vrf_id, nh->ip, 32);
+			ip4_route_delete(nh->vrf_id, nh->ip, nh->prefixlen);
+			ip4_route_delete(nh->vrf_id, nh->ip, 32);
 		}
 	}
 
-	ip4_route_delete(vrf_id, local_ip, 32);
+	ip4_route_delete(nh->vrf_id, local_ip, 32);
 }
 
 static struct gr_api_handler route4_add_handler = {

commit fadc48a987430abe2babe489b945ad0ea2d1d8d9

nexthop: fix disappearing addresses and routes after expiration
After a fixed period of time (20 minutes), grout will mark non-static
nexthops that were resolved earlier (with ARP or NDP) as STALE and will
try to probe them to refresh their mac addresses.

After 6 probes have been sent (3 unicast using the previously known mac
address of the nexthop, and 3 broadcast/multicast) without any reply
from the nexthop, it is marked as FAILED.

After 1 minute in the FAILED state, it is "freed" using the callback
specified in the nexthop_ops per family (at the moment, only IPv4 and
IPv6).

The current callbacks are rib4_cleanup and rib6_cleanup which are very
aggressive. They effectively delete everything related to the nexthop.
Including any local address, routes, etc.

Replace these callbacks with simpler ones which only delete /32 and /128
routes and decrement the reference counter on the nexthop. If the
nexthop still has routes referencing it (for example, it is used as
a gateway for a route), scrub its flags and mac address fields so that
it can reused later.

Fixes: fe3e408851ce ("route: rework route cleanup routine")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/control/nexthop.c
@@ -263,6 +263,14 @@ static struct api_out nh4_list(const void *request, void **response) {
 	return api_out(0, len);
 }
 
+static void nh4_free(struct nexthop *nh) {
+	rib4_delete(nh->vrf_id, nh->ipv4, 32);
+	if (nh->ref_count > 0) {
+		nh->flags &= ~(GR_NH_F_REACHABLE | GR_NH_F_PENDING | GR_NH_F_FAILED);
+		memset(&nh->mac, 0, sizeof(nh->mac));
+	}
+}
+
 static void nh4_init(struct event_base *) {
 	ip_output_node = gr_control_input_register_handler("ip_output", true);
 	arp_output_reply_node = gr_control_input_register_handler("arp_output_reply", true);
@@ -291,7 +299,7 @@ static struct gr_module nh4_module = {
 
 static struct nexthop_ops nh_ops = {
 	.solicit = arp_output_request_solicit,
-	.free = rib4_cleanup,
+	.free = nh4_free,
 };
 
 RTE_INIT(control_ip_init) {

modules/ip6/control/nexthop.c
@@ -291,6 +291,14 @@ static struct api_out nh6_list(const void *request, void **response) {
 	return api_out(0, len);
 }
 
+static void nh6_free(struct nexthop *nh) {
+	rib6_delete(nh->vrf_id, nh->iface_id, &nh->ipv6, RTE_IPV6_MAX_DEPTH);
+	if (nh->ref_count > 0) {
+		nh->flags &= ~(GR_NH_F_REACHABLE | GR_NH_F_PENDING | GR_NH_F_FAILED);
+		memset(&nh->mac, 0, sizeof(nh->mac));
+	}
+}
+
 static void nh6_init(struct event_base *) {
 	ip6_output_node = gr_control_input_register_handler("ip6_output", true);
 	ndp_na_output_node = gr_control_input_register_handler("ndp_na_output", true);
@@ -319,7 +327,7 @@ static struct gr_module nh6_module = {
 
 static struct nexthop_ops nh_ops = {
 	.solicit = nh6_solicit,
-	.free = rib6_cleanup,
+	.free = nh6_free,
 };
 
 RTE_INIT(control_ip_init) {