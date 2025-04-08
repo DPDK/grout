1b4549d1037c8a35bc488ae9bcf9a8458cf43e80
Robin Jarry <robin@jarry.cc>
2025-04-08 14:02:13 +0200

    cli: use scols_line_sprintf if available
    Since libsmartcols 2.41, scols_line_sprintf is available and does not
    need to be implemented by grout. Add the required meson code to avoid
    building our local version when the symbol is available in the library.
    
    Link: https://github.com/util-linux/util-linux/commit/f0d0817c37931b504a7
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- cli/gr_table.h
+++ cli/gr_table.h
@@ -4,9 +4,13 @@
 #ifndef _GR_TABLE
 #define _GR_TABLE
 
+#ifndef NEED_SCOLS_LINE_SPRINTF
+#include <libsmartcols.h>
+#else
 struct libscols_line;
 
 int scols_line_sprintf(struct libscols_line *, int column, const char *fmt, ...)
 	__attribute__((format(printf, 3, 4)));
+#endif
 
 #endif
