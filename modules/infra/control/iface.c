f7a52f5181915cf5b4ad3f6befbc82c7a6cfe066
Maxime Leroy <maxime@leroys.fr>
2025-04-25 16:20:24 +0200

    infra: send iface down notification before deleting iface
    To support FRR, interfaces created or deleted in Grout must be synchronized
    with FRR. FRR listens for interface notifications from Grout to keep its
    internal state consistent.
    
    When FRR receives an IFACE_EVENT_PRE_REMOVE notification, the dplane plugin
    in FRR attempts to remove the corresponding interface from memory. However,
    Zebra fails to delete it if the interface is still marked as "up".
    
    In the Linux kernel, when an interface is deleted, a netlink message is
    first sent to indicate that the interface is down. Grout should follow the
    same behavior.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>

--- modules/infra/control/iface.c
+++ modules/infra/control/iface.c
@@ -244,6 +244,11 @@ int iface_destroy(uint16_t ifid) {
 	if (gr_vec_len(iface->subinterfaces) != 0)
 		return errno_set(EBUSY);
 
+	/* interface is still up, send status down */
+	if (iface->flags & GR_IFACE_F_UP) {
+		iface->flags &= ~GR_IFACE_F_UP;
+		gr_event_push(IFACE_EVENT_STATUS_DOWN, iface);
+	}
 	gr_event_push(IFACE_EVENT_PRE_REMOVE, iface);
 
 	ifaces[ifid] = NULL;
