DPDK/grout

commit 5a25e184cb1d367f43a02c4650d1918422e0a5ef

port: expose link_speed to the api
Add a new link_speed field to gr_iface_info_port.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/api/gr_infra.h
@@ -90,6 +90,7 @@ struct __gr_iface_info_port_base {
 	uint16_t n_txq;
 	uint16_t rxq_size;
 	uint16_t txq_size;
+	uint16_t link_speed; //!< Physical link speed in Megabit/sec.
 	struct rte_ether_addr mac;
 };
 

modules/infra/cli/port.c
@@ -21,6 +21,10 @@ static void port_show(const struct gr_api_client *c, const struct gr_iface *ifac
 	printf("devargs: %s\n", port->devargs);
 	printf("driver:  %s\n", port->driver_name);
 	printf("mac: " ETH_F "\n", &port->mac);
+	if (port->link_speed == UINT16_MAX)
+		printf("speed: unknown\n");
+	else
+		printf("speed: %u Mb/s\n", port->link_speed);
 	printf("n_rxq: %u\n", port->n_rxq);
 	printf("n_txq: %u\n", port->n_txq);
 	printf("rxq_size: %u\n", port->rxq_size);

modules/infra/control/port.c
@@ -568,6 +568,7 @@ static struct event *link_event;
 static void link_event_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
 	unsigned max_sleep_us, rx_buffer_us;
 	struct rte_eth_rxq_info qinfo;
+	struct iface_info_port *port;
 	struct rte_eth_link link;
 	struct queue_map *qmap;
 	struct worker *worker;
@@ -588,10 +589,14 @@ static void link_event_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
 			if (iface == NULL)
 				continue;
 
+			port = (struct iface_info_port *)iface->info;
+
 			if (rte_eth_link_get_nowait(qmap->port_id, &link) < 0) {
 				LOG(WARNING, "rte_eth_link_get_nowait: %s", strerror(rte_errno));
 				continue;
 			}
+			port->link_speed = (uint16_t)link.link_speed;
+
 			if (link.link_status == RTE_ETH_LINK_UP) {
 				if (!(iface->state & GR_IFACE_S_RUNNING)) {
 					LOG(INFO, "%s: link status up", iface->name);

commit 7fd96431031824f1d25aaf8fbc1920ba987c2447

port: avoid truncating link speed values above 64gb/s
Ports with higher speeds (100gb/s and 400gb/s) are not represented in
the API. The field is only a 16bit wide integer. These values are
truncated.

Change the field to use 32 bits as it is in the DPDK API.

Fixes: 5a25e184cb1d ("port: expose link_speed to the api")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/api/gr_infra.h
@@ -90,7 +90,7 @@ struct __gr_iface_info_port_base {
 	uint16_t n_txq;
 	uint16_t rxq_size;
 	uint16_t txq_size;
-	uint16_t link_speed; //!< Physical link speed in Megabit/sec.
+	uint32_t link_speed; //!< Physical link speed in Megabit/sec.
 	struct rte_ether_addr mac;
 };
 

modules/infra/cli/port.c
@@ -21,7 +21,7 @@ static void port_show(const struct gr_api_client *c, const struct gr_iface *ifac
 	printf("devargs: %s\n", port->devargs);
 	printf("driver:  %s\n", port->driver_name);
 	printf("mac: " ETH_F "\n", &port->mac);
-	if (port->link_speed == UINT16_MAX)
+	if (port->link_speed == UINT32_MAX)
 		printf("speed: unknown\n");
 	else
 		printf("speed: %u Mb/s\n", port->link_speed);

modules/infra/control/port.c
@@ -605,7 +605,7 @@ static void link_event_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
 				LOG(WARNING, "rte_eth_link_get_nowait: %s", strerror(rte_errno));
 				continue;
 			}
-			port->link_speed = (uint16_t)link.link_speed;
+			port->link_speed = link.link_speed;
 
 			if (link.link_status == RTE_ETH_LINK_UP) {
 				if (!(iface->state & GR_IFACE_S_RUNNING)) {