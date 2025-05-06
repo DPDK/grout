DPDK/grout

commit 9ca93361aea1f345b83df793e35711ed94fe1b9c

port: allow attaching already probed devices
At startup, we pass -a 0000:00:00.0 to rte_eal_init() to disable
automatic PCI probing. However, this only affects the PCI bus devices.
Other buses (e.g. FSMLC) are not affected by this argument and are
always probed in rte_eal_init().

When configuring a port with such a device, we get an error from the API
saying that the port is already attached to another interface:

	grout# add interface port dpni2 devargs fslmc:dpni.2
	error: File exists

Instead of iterating over the DPDK probed devices, look at grout ports
to determine whether a port matching this devargs value is already
configured.

If we cannot find one, check if the device has already been probed and
only probe it if it was not.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/port.c
@@ -326,21 +326,31 @@ static int iface_port_init(struct iface *iface, const void *api_info) {
 	const struct gr_iface_info_port *api = api_info;
 	uint16_t port_id = RTE_MAX_ETHPORTS;
 	struct rte_dev_iterator iterator;
+	const struct iface *i;
 	struct gr_iface conf;
 	int ret;
 
-	RTE_ETH_FOREACH_MATCHING_DEV(port_id, api->devargs, &iterator) {
-		rte_eth_iterator_cleanup(&iterator);
-		return errno_set(EEXIST);
+	i = NULL;
+	while ((i = iface_next(GR_IFACE_TYPE_PORT, i)) != NULL) {
+		const struct iface_info_port *p = (const struct iface_info_port *)i->info;
+		if (strncmp(p->devargs, api->devargs, sizeof(api->devargs)) == 0)
+			return errno_set(EEXIST);
 	}
-
-	if ((ret = rte_dev_probe(api->devargs)) < 0)
-		return errno_set(-ret);
 
 	RTE_ETH_FOREACH_MATCHING_DEV(port_id, api->devargs, &iterator) {
 		rte_eth_iterator_cleanup(&iterator);
 		break;
 	}
+
+	if (!rte_eth_dev_is_valid_port(port_id)) {
+		if ((ret = rte_dev_probe(api->devargs)) < 0)
+			return errno_set(-ret);
+		RTE_ETH_FOREACH_MATCHING_DEV(port_id, api->devargs, &iterator) {
+			rte_eth_iterator_cleanup(&iterator);
+			break;
+		}
+	}
+
 	if (!rte_eth_dev_is_valid_port(port_id))
 		return errno_set(EIDRM);
 

commit 759fd5e9b8623394dcdbd18971b09f8a880e9bb7

dpaa2: do not close devices
The dpaa2 devices do not support close/re-open with the current DPDK.
Let's work around this limitation for the time being.

From the design of the DPDK's fslmc bus, it seems to be the only
option for the time being.

Fix:
 grout# add interface port dpni.2 devargs fslmc:dpni.2 up mode l3
 Created interface 1
 grout# del interface dpni.2
 grout# add interface port dpni.2 devargs fslmc:dpni.2 up mode l3
 error: command failed: Identifier removed

Fixes: 9ca93361aea1 ("port: allow attaching already probed device")
Signed-off-by: Vincent Jardin <vjardin@free.fr>

modules/infra/control/port.c
@@ -307,10 +307,15 @@ static int iface_port_fini(struct iface *iface) {
 		LOG(ERR, "rte_eth_dev_info_get: %s", rte_strerror(-ret));
 	if ((ret = rte_eth_dev_stop(port->port_id)) < 0)
 		LOG(ERR, "rte_eth_dev_stop: %s", rte_strerror(-ret));
+	// XXX DPDK bus/fslmc VFIO constraint for dpaa2
+	if (strcmp(info.driver_name, "net_dpaa2") == 0)
+		goto fini;
 	if ((ret = rte_eth_dev_close(port->port_id)) < 0)
 		LOG(ERR, "rte_eth_dev_close: %s", rte_strerror(-ret));
 	if (info.device != NULL && (ret = rte_dev_remove(info.device)) < 0)
 		LOG(ERR, "rte_dev_remove: %s", rte_strerror(-ret));
+
+fini:
 	if (port->pool != NULL) {
 		gr_pktmbuf_pool_release(port->pool, port->pool_size);
 		port->pool = NULL;