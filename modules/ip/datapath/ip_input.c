DPDK/grout

commit 2e7d5f6f1aefbe8f219c0897e447543e877c4a9d

ip: clarify usage of mpriv in ip_input_process
The eth_input_mbuf_data and ip_output_input_mbuf_data pointers are
initialized at the same time, which may suggest that both structures can be
accessed concurrently. However, this is not the case, as
GR_MBUF_PRIV_DATA_TYPE defines the private type as a union.

To improve clarity, this patch ensures that these variables are defined
only when they are actually used.

Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/ip/datapath/ip_input.c
@@ -59,9 +59,6 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 		nh = NULL;
 		mbuf = objs[i];
 		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
-		e = eth_input_mbuf_data(mbuf);
-		d = ip_output_mbuf_data(mbuf);
-		iface = e->iface;
 
 		// RFC 1812 section 5.2.2 IP Header Validation
 		//
@@ -114,6 +111,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
+		e = eth_input_mbuf_data(mbuf);
 		switch (e->domain) {
 		case ETH_DOMAIN_LOOPBACK:
 		case ETH_DOMAIN_LOCAL:
@@ -131,6 +129,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
+		iface = e->iface;
 		nh = fib4_lookup(iface->vrf_id, ip->dst_addr);
 		if (nh == NULL) {
 			edge = NO_ROUTE;
@@ -151,6 +150,7 @@ next:
 			*t = *ip;
 		}
 		// Store the resolved next hop for ip_output to avoid a second route lookup.
+		d = ip_output_mbuf_data(mbuf);
 		d->nh = nh;
 		d->iface = iface;
 		rte_node_enqueue_x1(graph, node, edge, mbuf);

commit 9c562dd9c08558132e0ab57fdac7c798d40bd677

ip: fix crash when receiving broadcast packets
When receiving a broadcast or multicast ethernet frame (e.g. a DHCP
request), the ip_output_mbuf_data->iface field wrongfully is reset to
NULL.

This leads to a crash in ip_input_local when accessing iface->vrf_id:

Thread 6 "grout:w20" received signal SIGSEGV, Segmentation fault.
0x00000000004fe924 in ip_input_local_process
	(graph=0x7ff7e95f5dc0, node=0x7ff7e95fb140, objs=0x7ff7f39ad740, nb_objs=1)
	at ../modules/ip/datapath/ip_local.c:48
48                              data->vrf_id = iface->vrf_id;

Make sure that iface is always preserved to its original value.
Homogenize the code with ip6_input.

Fixes: 2e7d5f6f1aef ("ip: clarify usage of mpriv in ip_input_process")
Reported-by: Spoorthi K <spk@redhat.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/datapath/ip_input.c
@@ -41,10 +41,11 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 	uint16_t i;
 
 	for (i = 0; i < nb_objs; i++) {
-		iface = NULL;
-		nh = NULL;
 		mbuf = objs[i];
 		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
+		e = eth_input_mbuf_data(mbuf);
+		iface = e->iface;
+		nh = NULL;
 
 		// RFC 1812 section 5.2.2 IP Header Validation
 		//
@@ -97,7 +98,6 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
-		e = eth_input_mbuf_data(mbuf);
 		switch (e->domain) {
 		case ETH_DOMAIN_LOOPBACK:
 		case ETH_DOMAIN_LOCAL:
@@ -115,7 +115,6 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
-		iface = e->iface;
 		nh = fib4_lookup(iface->vrf_id, ip->dst_addr);
 		if (nh == NULL) {
 			edge = NO_ROUTE;
@@ -138,7 +137,6 @@ next:
 		// Store the resolved next hop for ip_output to avoid a second route lookup.
 		d = ip_output_mbuf_data(mbuf);
 		d->nh = nh;
-		d->iface = iface;
 		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 