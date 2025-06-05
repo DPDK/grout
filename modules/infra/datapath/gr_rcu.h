edd979753704e25d7712cc21c55326fd96b44997
Robin Jarry <robin@jarry.cc>
2025-06-05 14:42:58 +0200

    main-loop: expose rcu object
    Create an RCU object global to the application and initialize it with
    RTE_MAX_LCORE reader threads. Report quiescent state every turn of main
    loop.
    
    Expose the rte_rcu_qsbr structure pointer for any subsystem that would
    like to make use of it.
    
    It will be leveraged in the next commits to ensure the datapath workers
    do not hold a reference on objects when they are freed/unallocated by
    the control plane thread.
    
    Link: http://doc.dpdk.org/guides/prog_guide/rcu_lib.html
    Signed-off-by: Robin Jarry <robin@jarry.cc>

---
+++ modules/infra/datapath/gr_rcu.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#ifndef _GR_RCU
+#define _GR_RCU
+
+#include <rte_rcu_qsbr.h>
+
+struct rte_rcu_qsbr *gr_datapath_rcu(void);
+
+#endif
