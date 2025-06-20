c991044c2fa7974e4a542202b24cc00831763316
Christophe Fontaine <cfontain@redhat.com>
2025-06-20 12:54:31 +0000

    ip6: convert router adv/sollicit to proper nodes
    The NDP router solicitation input callback
    (ndp_router_sollicit_input_cb) is replaced by a dedicated DPDK graph
    node (ndp_router_sollicit_process).
    Packets are now sent directly to the "ndp_router_sollicit" node instead
    of using the control output callback mechanism.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip6/datapath/ndp_rs_input.c
+++ modules/ip6/datapath/ndp_rs_input.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2025 Christophe Fontaine
 
-#include <gr_control_output.h>
 #include <gr_graph.h>
 #include <gr_icmp6.h>
 #include <gr_ip6_control.h>
@@ -15,7 +14,7 @@
 #include <rte_ip6.h>
 
 enum {
-	CONTROL,
+	ROUTER_SOLICIT,
 	INVAL,
 	EDGE_COUNT,
 };
@@ -26,7 +25,6 @@ static uint16_t ndp_rs_input_process(
 	void **objs,
 	uint16_t nb_objs
 ) {
-	struct control_output_mbuf_data *co;
 	struct ip6_local_mbuf_data *d;
 	struct rte_mbuf *mbuf;
 	struct icmp6 *icmp6;
@@ -59,9 +57,7 @@ static uint16_t ndp_rs_input_process(
 		// - ICMP length (derived from the IP length) is 8 or more octets.
 		ASSERT_NDP(d->len >= 8);
 
-		next = CONTROL;
-		co = control_output_mbuf_data(mbuf);
-		co->callback = ndp_router_sollicit_input_cb;
+		next = ROUTER_SOLICIT;
 next:
 		if (gr_mbuf_is_traced(mbuf))
 			gr_mbuf_trace_add(mbuf, node, 0);
@@ -76,7 +72,7 @@ static struct rte_node_register node = {
 	.process = ndp_rs_input_process,
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
-		[CONTROL] = "control_output",
+		[ROUTER_SOLICIT] = "ndp_router_solicit",
 		[INVAL] = "ndp_rs_input_inval",
 	},
 };
