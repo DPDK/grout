DPDK/grout

commit 7ebb3719adbb99118f4904dc598af1a2282026ca

srv6: remove policy logic from dataplane
SRv6 policy (e.g. color-based selection) is a control-plane concept per
RFC 9256.

It should not be implemented in the dataplane, which only executes the
selected SID list and behavior. If needed, color-to-policy resolution
should be handled by FRR or another control plane component.

ECMP across multiple SRv6 paths should be implemented generically at the
nexthop level, not as SRv6-specific logic.

To do that, sr policy and steer are merged together to sr route.

Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/srv6/control_headend.c
@@ -11,339 +11,222 @@
 #include <gr_srv6.h>
 #include <gr_vec.h>
 
-static struct rte_hash *srv6_policy_hash;
-static struct rte_hash *srv6_steer_hash;
+static struct rte_hash *srv6_encap_hash;
 
-// policies ////////////////////////////////////////////////////////////////
+// routes ////////////////////////////////////////////////////////////////
 
-static struct srv6_policy_data *srv6_policy_get(const struct rte_ipv6_addr *bsid) {
+static struct srv6_policy_data *srv6_encap_get(const struct nexthop *nh) {
 	void *data;
 
-	if (rte_hash_lookup_data(srv6_policy_hash, bsid, &data) < 0)
+	if (nh == NULL || rte_hash_lookup_data(srv6_encap_hash, &nh, &data) < 0)
 		return NULL;
+
 	return data;
 }
 
-static struct api_out srv6_policy_add(const void *request, void ** /*response*/) {
-	const struct gr_srv6_policy_add_req *req = request;
-	struct srv6_policy_data *d;
+static int srv6_encap_data_add(
+	struct nexthop *nh,
+	gr_srv6_encap_behavior_t encap_behavior,
+	uint8_t n_seglist,
+	const struct rte_ipv6_addr *seglist,
+	struct srv6_encap_data **d
+) {
+	struct srv6_encap_data *_d;
 	int ret;
 
-	if (req->p.n_seglist > GR_SRV6_POLICY_SEGLIST_COUNT_MAX)
-		return api_out(EINVAL, 0);
+	if (srv6_encap_get(nh))
+		return -EEXIST;
 
-	d = srv6_policy_get(&req->p.bsid);
-	if (d != NULL)
-		return api_out(EEXIST, 0);
-
-	d = calloc(1, sizeof(*d) + sizeof(d->seglist[0]) * req->p.n_seglist);
+	_d = calloc(1, sizeof(*_d) + sizeof(_d->seglist[0]) * n_seglist);
 	if (d == NULL)
-		return api_out(ENOMEM, 0);
-	d->bsid = req->p.bsid;
-	d->encap = req->p.encap_behavior;
-	d->weight = req->p.weight;
-	d->n_seglist = req->p.n_seglist;
-	memcpy(d->seglist, req->p.seglist, sizeof(d->seglist[0]) * req->p.n_seglist);
+		return -ENOMEM;
 
-	ret = rte_hash_add_key_data(srv6_policy_hash, &req->p.bsid, d);
+	_d->encap = encap_behavior;
+	_d->n_seglist = n_seglist;
+	memcpy(_d->seglist, seglist, sizeof(_d->seglist[0]) * n_seglist);
+
+	ret = rte_hash_add_key_data(srv6_encap_hash, &nh, _d);
 	if (ret < 0) {
-		free(d);
-		return api_out(ENOMEM, 0);
+		free(_d);
+		return -EEXIST;
 	}
 
-	return api_out(0, 0);
+	nexthop_incref(nh);
+	*d = _d;
+	return 0;
 }
 
-static struct api_out srv6_policy_del(const void *request, void ** /*response*/) {
-	const struct gr_srv6_policy_del_req *req = request;
-	struct srv6_policy_data **dlist;
-	struct srv6_policy_data *d;
-	struct nexthop *nh;
-	size_t i;
-
-	d = srv6_policy_get(&req->bsid);
-	if (d == NULL)
-		return api_out(ENOENT, 0);
-
-	gr_vec_foreach (nh, d->nhlist) {
-		// remove this sr policy from nexthop's list of sr policy
-		dlist = srv6_steer_get(nh);
-		for (i = 0; i < gr_vec_len(dlist); i++) {
-			if (dlist[i] == d) {
-				gr_vec_del(dlist, i);
-				break;
-			}
-		}
-		// this steering rule doesn't have any output sr policy, remove it.
-		if (gr_vec_len(dlist) == 0) {
-			if (nh->type == GR_NH_SR6_IPV6)
-				rib6_delete(
-					nh->vrf_id, GR_IFACE_ID_UNDEF, &nh->ipv6, nh->prefixlen
-				);
-			else
-				rib4_delete(nh->vrf_id, nh->ipv4, nh->prefixlen);
-			rte_hash_del_key(srv6_steer_hash, &nh);
-			gr_vec_free(dlist);
-		}
-		nexthop_decref(nh);
-	}
-
-	gr_vec_free(d->nhlist);
+static int srv6_encap_data_del(struct nexthop *nh, struct srv6_encap_data *d) {
+	rte_hash_del_key(srv6_encap_hash, &nh);
+	nexthop_decref(nh);
 	free(d);
-	rte_hash_del_key(srv6_policy_hash, &req->bsid);
-
-	return api_out(0, 0);
+	return 0;
 }
 
-static struct api_out srv6_policy_list(const void * /* request */, void **response) {
-	struct srv6_policy_data **d_list = NULL;
-	struct gr_srv6_policy_list_resp *resp;
-	const struct srv6_policy_data *d;
-	ssize_t len = sizeof(*resp);
-	struct gr_srv6_policy *ad;
-	void *data_ptr, *ptr;
-	const void *key_ptr;
-	uint32_t iter, i;
-
-	iter = 0;
-	key_ptr = NULL;
-	while (rte_hash_iterate(srv6_policy_hash, &key_ptr, &data_ptr, &iter) >= 0) {
-		d = data_ptr;
-		gr_vec_add(d_list, data_ptr);
-		len += sizeof(struct gr_srv6_policy) + d->n_seglist * sizeof(d->seglist[0]);
-	}
-	if ((resp = calloc(1, len)) == NULL) {
-		gr_vec_free(d_list);
-		return api_out(ENOMEM, 0);
-	}
-
-	if (d_list == NULL) {
-		*response = resp;
-		return api_out(0, len);
-	}
-
-	resp->n_policy = gr_vec_len(d_list);
-	ptr = resp->policy;
-	for (i = 0; i < resp->n_policy; i++) {
-		d = d_list[i];
-		ad = ptr;
-		ad->bsid = d->bsid;
-		ad->weight = d->weight;
-		ad->encap_behavior = d->encap;
-		ad->n_seglist = d->n_seglist;
-		memcpy(ad->seglist, d->seglist, d->n_seglist * sizeof(d->seglist[0]));
-		ptr += sizeof(*ad) + ad->n_seglist * sizeof(ad->seglist[0]);
-	}
-	assert(ptr - (void *)resp <= len);
-	gr_vec_free(d_list);
-
-	*response = resp;
-
-	return api_out(0, len);
-}
-
-static void srv6_policy_init(void) {
-	// store srv6 policying rules, index by binding-sid
+static void srv6_encap_data_init(void) {
+	// store srv6 encap data, index by nh
 	struct rte_hash_parameters params = {
-		.name = "srv6_policy",
+		.name = "srv6_encap",
 		.entries = 4096,
-		.key_len = sizeof(struct rte_ipv6_addr),
+		.key_len = sizeof(struct nexthop *),
 		.socket_id = SOCKET_ID_ANY,
 		.extra_flag = RTE_HASH_EXTRA_FLAGS_RW_CONCURRENCY_LF
 			| RTE_HASH_EXTRA_FLAGS_TRANS_MEM_SUPPORT,
 	};
-	srv6_policy_hash = rte_hash_create(&params);
-	if (srv6_policy_hash == NULL)
-		ABORT("rte_hash_create(srv6_policy)");
+	srv6_encap_hash = rte_hash_create(&params);
+	if (srv6_encap_hash == NULL)
+		ABORT("rte_hash_create(srv6_encap)");
 }
 
-static void srv6_policy_release(void) {
-	struct srv6_policy_data *d;
+static void srv6_encap_data_release(void) {
+	//struct srv6_encap_data *d;
 	const void *key = NULL;
 	void *data = NULL;
 	uint32_t iter;
 
 	iter = 0;
-	while (rte_hash_iterate(srv6_policy_hash, &key, &data, &iter) >= 0) {
-		rte_hash_del_key(srv6_policy_hash, key);
+	while (rte_hash_iterate(srv6_encap_hash, &key, &data, &iter) >= 0) {
+		srv6_encap_data_del((struct nexthop *)key, (struct srv6_encap_data *)data);
+		/*
+		rte_hash_del_key(srv6_encap_hash, key);
 		d = data;
-		gr_vec_free(d->nhlist);
 		free(d);
+		*/
 	}
-	rte_hash_free(srv6_policy_hash);
-	srv6_policy_hash = NULL;
+	rte_hash_free(srv6_encap_hash);
+	srv6_encap_hash = NULL;
 }
 
-// steer ////////////////////////////////////////////////////////////////
-
-struct srv6_policy_data **srv6_steer_get(const struct nexthop *nh) {
+struct srv6_encap_data *srv6_encap_data_get(const struct nexthop *nh) {
 	void *data;
 
-	if (nh == NULL || rte_hash_lookup_data(srv6_steer_hash, &nh, &data) < 0)
+	if (nh == NULL || (nh->type != GR_NH_SR6_IPV6 && nh->type != GR_NH_SR6_IPV4)
+	    || rte_hash_lookup_data(srv6_encap_hash, &nh, &data) < 0)
 		return NULL;
 	return data;
 }
 
-static struct api_out srv6_steer_add(const void *request, void ** /*response*/) {
-	const struct gr_srv6_steer_add_req *req = request;
-	struct srv6_policy_data **dlist, **prev_dlist;
-	struct srv6_policy_data *d, *d_tmp;
+// srv6 route ////////////////////////////////////////////////////////////////
+static struct api_out srv6_route_add(const void *request, void ** /*response*/) {
+	const struct gr_srv6_route_add_req *req = request;
+	struct srv6_encap_data *d;
 	struct nexthop *nh;
-
-	d = srv6_policy_get(&req->bsid);
-	if (d == NULL)
-		return api_out(ENOENT, 0);
+	int ret;
 
 	// retrieve or create nexthop into rib4/rib6
-	if (req->l3.is_dest6) {
+	if (req->r.key.is_dest6) {
 		nh = rib6_lookup_exact(
-			req->l3.vrf_id,
+			req->r.key.vrf_id,
 			GR_IFACE_ID_UNDEF,
-			&req->l3.dest6.ip,
-			req->l3.dest6.prefixlen
+			&req->r.key.dest6.ip,
+			req->r.key.dest6.prefixlen
 		);
-		dlist = srv6_steer_get(nh);
-		if (dlist != NULL)
-			goto add_rule;
+
+		d = srv6_encap_data_get(nh);
+		if (d != NULL)
+			return api_out(-EEXIST, 0);
 
 		nh = nexthop_new(
-			GR_NH_SR6_IPV6, req->l3.vrf_id, GR_IFACE_ID_UNDEF, &req->l3.dest6.ip
+			GR_NH_SR6_IPV6, req->r.key.vrf_id, GR_IFACE_ID_UNDEF, &req->r.key.dest6.ip
 		);
 		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->prefixlen = req->l3.dest6.prefixlen;
 
-		if (rib6_insert(
-			    req->l3.vrf_id,
-			    GR_IFACE_ID_UNDEF,
-			    &req->l3.dest6.ip,
-			    req->l3.dest6.prefixlen,
-			    GR_RT_ORIGIN_LINK,
-			    nh
-		    )
-		    < 0)
-			return api_out(errno, 0);
-
+		nh->prefixlen = req->r.key.dest6.prefixlen;
 	} else {
-		nh = rib4_lookup_exact(req->l3.vrf_id, req->l3.dest4.ip, req->l3.dest4.prefixlen);
-		dlist = srv6_steer_get(nh);
-		if (dlist != NULL)
-			goto add_rule;
+		nh = rib4_lookup_exact(
+			req->r.key.vrf_id, req->r.key.dest4.ip, req->r.key.dest4.prefixlen
+		);
+		d = srv6_encap_data_get(nh);
+		if (d != NULL)
+			return api_out(-EEXIST, 0);
 
 		nh = nexthop_new(
-			GR_NH_SR6_IPV4, req->l3.vrf_id, GR_IFACE_ID_UNDEF, &req->l3.dest4.ip
+			GR_NH_SR6_IPV4, req->r.key.vrf_id, GR_IFACE_ID_UNDEF, &req->r.key.dest4.ip
 		);
 		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->prefixlen = req->l3.dest4.prefixlen;
-
-		if (rib4_insert(
-			    req->l3.vrf_id,
-			    req->l3.dest4.ip,
-			    req->l3.dest4.prefixlen,
-			    GR_RT_ORIGIN_LINK,
-			    nh
-		    )
-		    < 0)
-			return api_out(errno, 0);
+		nh->prefixlen = req->r.key.dest4.prefixlen;
 	}
 	nh->flags |= GR_NH_F_GATEWAY | GR_NH_F_STATIC | GR_NH_F_REACHABLE;
 
-	// new nexthop: push this first policy
-	gr_vec_add(dlist, d);
-	if (rte_hash_add_key_data(srv6_steer_hash, &nh, dlist) < 0) {
+	ret = srv6_encap_data_add(nh, req->r.encap_behavior, req->r.n_seglist, req->r.seglist, &d);
+	if (ret < 0) {
 		nexthop_decref(nh);
-		if (req->l3.is_dest6)
-			rib6_delete(
-				req->l3.vrf_id,
-				GR_IFACE_ID_UNDEF,
-				&req->l3.dest6.ip,
-				req->l3.dest6.prefixlen
-			);
-		else
-			rib4_delete(req->l3.vrf_id, req->l3.dest4.ip, req->l3.dest4.prefixlen);
-		return api_out(ENOMEM, 0);
+		return api_out(-ret, 0);
 	}
 
-	gr_vec_add(d->nhlist, nh);
-	return api_out(0, 0);
+	if (req->r.key.is_dest6)
+		ret = rib6_insert(
+			req->r.key.vrf_id,
+			GR_IFACE_ID_UNDEF,
+			&req->r.key.dest6.ip,
+			req->r.key.dest6.prefixlen,
+			GR_RT_ORIGIN_LINK,
+			nh
+		);
+	else
+		ret = rib4_insert(
+			req->r.key.vrf_id,
+			req->r.key.dest4.ip,
+			req->r.key.dest4.prefixlen,
+			GR_RT_ORIGIN_LINK,
+			nh
+		);
 
-add_rule:
-	// existing nexthop: check if not a duplicate, then add sr policy to nh
-	gr_vec_foreach (d_tmp, dlist) {
-		if (d_tmp == d)
-			return api_out(EEXIST, 0);
+	if (ret < 0) {
+		srv6_encap_data_del(nh, d);
+		return api_out(-ret, 0);
 	}
-	nexthop_incref(nh);
-	prev_dlist = dlist;
-	gr_vec_add(dlist, d);
-	if (prev_dlist != dlist)
-		rte_hash_add_key_data(srv6_steer_hash, &nh, dlist);
 
-	gr_vec_add(d->nhlist, nh);
 	return api_out(0, 0);
 }
 
-static struct api_out srv6_steer_del(const void *request, void ** /*response*/) {
-	const struct gr_srv6_steer_del_req *req = request;
-	struct srv6_policy_data **dlist;
-	struct srv6_policy_data *d;
+static struct api_out srv6_route_del(const void *request, void ** /*response*/) {
+	const struct gr_srv6_route_del_req *req = request;
+	struct srv6_encap_data *d;
 	struct nexthop *nh;
-	unsigned i;
 
-	if (req->l3.is_dest6) {
+	if (req->key.is_dest6)
 		nh = rib6_lookup_exact(
-			req->l3.vrf_id,
+			req->key.vrf_id,
 			GR_IFACE_ID_UNDEF,
-			&req->l3.dest6.ip,
-			req->l3.dest6.prefixlen
+			&req->key.dest6.ip,
+			req->key.dest6.prefixlen
 		);
-		dlist = srv6_steer_get(nh);
-		if (dlist == NULL)
-			return api_out(ENOENT, 0);
+	else
+		nh = rib4_lookup_exact(
+			req->key.vrf_id, req->key.dest4.ip, req->key.dest4.prefixlen
+		);
 
+	d = srv6_encap_data_get(nh);
+	if (d != NULL)
+		return api_out(-EEXIST, 0);
+
+	if (req->key.is_dest6)
 		rib6_delete(
-			req->l3.vrf_id,
+			req->key.vrf_id,
 			GR_IFACE_ID_UNDEF,
-			&req->l3.dest6.ip,
-			req->l3.dest6.prefixlen
+			&req->key.dest6.ip,
+			req->key.dest6.prefixlen
 		);
 
-	} else {
-		nh = rib4_lookup_exact(req->l3.vrf_id, req->l3.dest4.ip, req->l3.dest4.prefixlen);
-		dlist = srv6_steer_get(nh);
-		if (dlist == NULL)
-			return api_out(ENOENT, 0);
+	else
+		rib4_delete(req->key.vrf_id, req->key.dest4.ip, req->key.dest4.prefixlen);
 
-		rib4_delete(req->l3.vrf_id, req->l3.dest4.ip, req->l3.dest4.prefixlen);
-	}
-
-	rte_hash_del_key(srv6_steer_hash, &nh);
-	nexthop_decref(nh);
-
-	gr_vec_foreach (d, dlist) {
-		if (rte_ipv6_addr_is_unspec(&req->bsid) || rte_ipv6_addr_eq(&req->bsid, &d->bsid)) {
-			for (i = 0; i < gr_vec_len(d->nhlist); i++) {
-				if (nh == d->nhlist[i]) {
-					gr_vec_del(d->nhlist, i);
-					break;
-				}
-			}
-		}
-	}
-	gr_vec_free(dlist);
-
+	srv6_encap_data_del(nh, d);
 	return api_out(0, 0);
 }
 
-static struct api_out srv6_steer_list(const void *request, void **response) {
-	const struct gr_srv6_steer_list_req *req = request;
-	struct srv6_policy_data ***d_listlist = NULL;
+static struct api_out srv6_route_list(const void *request, void **response) {
+	const struct gr_srv6_route_list_req *req = request;
+
+	struct srv6_encap_data **d_list = NULL;
 	const struct nexthop **nh_list = NULL;
-	struct gr_srv6_steer_list_resp *resp;
-	struct srv6_policy_data **d_list;
-	ssize_t j, len = sizeof(*resp);
-	struct gr_srv6_steer_entry *e;
+	struct gr_srv6_route_list_resp *resp;
+	struct srv6_encap_data *d;
+	ssize_t len = sizeof(*resp);
+	struct gr_srv6_route *r;
 	const struct nexthop *nh;
 	void *data_ptr, *ptr;
 	const void *key_ptr;
@@ -351,21 +234,19 @@ static struct api_out srv6_steer_list(const void *request, void **response) {
 
 	iter = 0;
 	key_ptr = NULL;
-	while (rte_hash_iterate(srv6_steer_hash, &key_ptr, &data_ptr, &iter) >= 0) {
+	while (rte_hash_iterate(srv6_encap_hash, &key_ptr, &data_ptr, &iter) >= 0) {
 		nh = *(const struct nexthop **)key_ptr;
-		d_list = data_ptr;
+		d = data_ptr;
+
 		if (req->vrf_id == UINT16_MAX || req->vrf_id == nh->vrf_id) {
 			gr_vec_add(nh_list, nh);
-			gr_vec_add(d_listlist, data_ptr);
-			// clang-format off
-			len += sizeof(struct gr_srv6_steer_entry)
-				+ gr_vec_len(d_list) * sizeof(struct rte_ipv6_addr);
-			// clang-format on
+			gr_vec_add(d_list, d);
+			len += sizeof(struct gr_srv6_route) + d->n_seglist * sizeof(d->seglist[0]);
 		}
 	}
 	if ((resp = calloc(1, len)) == NULL) {
 		gr_vec_free(nh_list);
-		gr_vec_free(d_listlist);
+		gr_vec_free(d_list);
 		return api_out(ENOMEM, 0);
 	}
 
@@ -374,109 +255,64 @@ static struct api_out srv6_steer_list(const void *request, void **response) {
 		return api_out(0, len);
 	}
 
-	resp->n_steer = gr_vec_len(nh_list);
-	ptr = resp->steer;
-	for (i = 0; i < resp->n_steer; i++) {
-		e = ptr;
+	resp->n_route = gr_vec_len(nh_list);
+	ptr = resp->route;
+	for (i = 0; i < resp->n_route; i++) {
+		r = ptr;
 		nh = nh_list[i];
-		e->l3.vrf_id = nh->vrf_id;
+		d = d_list[i];
+
+		r->key.vrf_id = nh->vrf_id;
 		if (nh->type == GR_NH_SR6_IPV6) {
-			e->l3.is_dest6 = true;
-			e->l3.dest6.ip = nh->ipv6;
-			e->l3.dest6.prefixlen = nh->prefixlen;
-		} else if (nh->type == GR_NH_SR6_IPV4) {
-			e->l3.is_dest6 = false;
-			e->l3.dest4.ip = nh->ipv4;
-			e->l3.dest4.prefixlen = nh->prefixlen;
-		} else
-			abort();
-		d_list = d_listlist[i];
-		e->n_bsid = gr_vec_len(d_list);
-		for (j = 0; j < e->n_bsid; j++)
-			e->bsid[j] = d_list[j]->bsid;
-		ptr += sizeof(*e) + e->n_bsid * sizeof(e->bsid[0]);
+			r->key.is_dest6 = true;
+			r->key.dest6.ip = nh->ipv6;
+			r->key.dest6.prefixlen = nh->prefixlen;
+		} else {
+			assert(nh->type == GR_NH_SR6_IPV4);
+
+			r->key.is_dest6 = false;
+			r->key.dest4.ip = nh->ipv4;
+			r->key.dest4.prefixlen = nh->prefixlen;
+		}
+
+		r->encap_behavior = d->encap;
+		r->n_seglist = d->n_seglist;
+		memcpy(r->seglist, d->seglist, r->n_seglist * sizeof(r->seglist[0]));
+		ptr += sizeof(*r) + r->n_seglist * sizeof(r->seglist[0]);
 	}
 	assert(ptr - (void *)resp <= len);
 	gr_vec_free(nh_list);
-	gr_vec_free(d_listlist);
+	gr_vec_free(d_list);
 
 	*response = resp;
 
 	return api_out(0, len);
 }
 
-static void srv6_steer_init(void) {
-	// store srv6 steering rules, index by nh (nexthop) ptr
-	struct rte_hash_parameters params = {
-		.name = "srv6_steer",
-		.entries = 8192,
-		.key_len = sizeof(struct nexthop *),
-		.socket_id = SOCKET_ID_ANY,
-		.extra_flag = RTE_HASH_EXTRA_FLAGS_RW_CONCURRENCY_LF
-			| RTE_HASH_EXTRA_FLAGS_TRANS_MEM_SUPPORT,
-	};
-	srv6_steer_hash = rte_hash_create(&params);
-	if (srv6_steer_hash == NULL)
-		ABORT("rte_hash_create(srv6_steer)");
-}
-
-static void srv6_steer_release(void) {
-	const void *key = NULL;
-	void *data = NULL;
-	uint32_t iter;
-
-	iter = 0;
-	while (rte_hash_iterate(srv6_steer_hash, &key, &data, &iter) >= 0) {
-		rte_hash_del_key(srv6_steer_hash, key);
-		gr_vec_free(data);
-	}
-
-	rte_hash_free(srv6_steer_hash);
-	srv6_steer_hash = NULL;
-}
-
 // srv6 headend module /////////////////////////////////////////////////////
 
 static void srv6_init(struct event_base *) {
-	srv6_policy_init();
-	srv6_steer_init();
+	srv6_encap_data_init();
 }
 
 static void srv6_fini(struct event_base *) {
-	srv6_policy_release();
-	srv6_steer_release();
+	srv6_encap_data_release();
 }
 
-static struct gr_api_handler srv6_policy_add_handler = {
-	.name = "sr policy add",
-	.request_type = GR_SRV6_POLICY_ADD,
-	.callback = srv6_policy_add,
+static struct gr_api_handler srv6_route_add_handler = {
+	.name = "sr route add",
+	.request_type = GR_SRV6_ROUTE_ADD,
+	.callback = srv6_route_add,
 };
-static struct gr_api_handler srv6_policy_del_handler = {
-	.name = "sr policy del",
-	.request_type = GR_SRV6_POLICY_DEL,
-	.callback = srv6_policy_del,
+static struct gr_api_handler srv6_route_del_handler = {
+	.name = "sr route del",
+	.request_type = GR_SRV6_ROUTE_DEL,
+	.callback = srv6_route_del,
 };
-static struct gr_api_handler srv6_policy_list_handler = {
-	.name = "sr policy list",
-	.request_type = GR_SRV6_POLICY_LIST,
-	.callback = srv6_policy_list,
-};
-
-static struct gr_api_handler srv6_steer_add_handler = {
-	.name = "sr steer add",
-	.request_type = GR_SRV6_STEER_ADD,
-	.callback = srv6_steer_add,
-};
-static struct gr_api_handler srv6_steer_del_handler = {
-	.name = "sr steer del",
-	.request_type = GR_SRV6_STEER_DEL,
-	.callback = srv6_steer_del,
-};
-static struct gr_api_handler srv6_steer_list_handler = {
-	.name = "sr steer list",
-	.request_type = GR_SRV6_STEER_LIST,
-	.callback = srv6_steer_list,
+static struct gr_api_handler srv6_route_list_handler = {
+	.name = "sr route list",
+	.request_type = GR_SRV6_ROUTE_LIST,
+	.callback = srv6_route_list,
 };
 
 static struct gr_module srv6_headend_module = {
@@ -486,11 +322,8 @@ static struct gr_module srv6_headend_module = {
 };
 
 RTE_INIT(srv6_constructor) {
-	gr_register_api_handler(&srv6_policy_add_handler);
-	gr_register_api_handler(&srv6_policy_del_handler);
-	gr_register_api_handler(&srv6_policy_list_handler);
-	gr_register_api_handler(&srv6_steer_add_handler);
-	gr_register_api_handler(&srv6_steer_del_handler);
-	gr_register_api_handler(&srv6_steer_list_handler);
+	gr_register_api_handler(&srv6_route_add_handler);
+	gr_register_api_handler(&srv6_route_del_handler);
+	gr_register_api_handler(&srv6_route_list_handler);
 	gr_register_module(&srv6_headend_module);
 }

commit cdf63c04f18c393d9a51ae2caf0d253fd84cfb75

srv6: fix calloc null check in encap_data_add
Check _d, not d, after calloc to avoid crash on allocation failure.

Fixes: 7ebb3719adbb ("srv6: remove policy logic from dataplane")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/srv6/control_headend.c
@@ -38,7 +38,7 @@ static int srv6_encap_data_add(
 		return -EEXIST;
 
 	_d = calloc(1, sizeof(*_d) + sizeof(_d->seglist[0]) * n_seglist);
-	if (d == NULL)
+	if (_d == NULL)
 		return -ENOMEM;
 
 	_d->encap = encap_behavior;