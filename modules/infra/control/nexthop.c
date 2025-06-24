DPDK/grout

commit a0132720febcfa50d798849751677450bd13eb36

nexthop: introduce type operations
Allow modules to register callbacks to free the private data stored in
nexthop of a given type.

If defined, invoke the free callback when the refcount of a nexthop
reaches 0.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/nexthop.c
@@ -24,6 +24,7 @@
 static struct rte_mempool *pool;
 static struct event *ageing_timer;
 static const struct nexthop_af_ops *af_ops[256];
+static const struct nexthop_type_ops *type_ops[256];
 struct gr_nexthop_config nh_conf = {
 	.max_count = DEFAULT_MAX_COUNT,
 	.lifetime_reachable_sec = DEFAULT_LIFETIME_REACHABLE,
@@ -107,6 +108,25 @@ void nexthop_af_ops_register(addr_family_t af, const struct nexthop_af_ops *ops)
 
 const struct nexthop_af_ops *nexthop_af_ops_get(addr_family_t af) {
 	return af_ops[af];
+}
+
+void nexthop_type_ops_register(gr_nh_type_t type, const struct nexthop_type_ops *ops) {
+	switch (type) {
+	case GR_NH_T_L3:
+	case GR_NH_T_SR6:
+	case GR_NH_T_SR6_LOCAL:
+		if (ops == NULL || ops->free)
+			ABORT("invalid type ops");
+		if (type_ops[type] != NULL)
+			ABORT("duplicate type ops %hhu", type);
+		type_ops[type] = ops;
+		return;
+	}
+	ABORT("invalid nexthop type %hhu", type);
+}
+
+const struct nexthop_type_ops *nexthop_type_ops_get(gr_nh_type_t type) {
+	return type_ops[type];
 }
 
 struct nexthop *
@@ -243,6 +263,10 @@ void nexthop_decref(struct nexthop *nh) {
 			m = next;
 		}
 		gr_event_push(GR_EVENT_NEXTHOP_DELETE, nh);
+		const struct nexthop_type_ops *ops = type_ops[nh->type];
+		if (ops != NULL && ops->free != NULL)
+			ops->free(nh);
+
 		memset(nh, 0, sizeof(*nh));
 		rte_mempool_put(pool, nh);
 	} else {

modules/infra/control/gr_nh_control.h
@@ -84,4 +84,12 @@ struct nexthop_af_ops {
 void nexthop_af_ops_register(addr_family_t af, const struct nexthop_af_ops *);
 const struct nexthop_af_ops *nexthop_af_ops_get(addr_family_t af);
 
+struct nexthop_type_ops {
+	// Callback that will be invoked the nexthop refcount reaches zero.
+	void (*free)(struct nexthop *);
+};
+
+void nexthop_type_ops_register(gr_nh_type_t type, const struct nexthop_type_ops *);
+const struct nexthop_type_ops *nexthop_type_ops_get(gr_nh_type_t type);
+
 #endif

commit 3d6271a91a01fbd41c7faf81b846a4ed746b1da6

infra/nexthop: reject ops when free==NULL, not when set
Invert the check in nexthop_type_ops_register(): abort only if
ops->free is NULL.

Fixes: a0132720febc ("nexthop: introduce type operations")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/infra/control/nexthop.c
@@ -116,7 +116,7 @@ void nexthop_type_ops_register(gr_nh_type_t type, const struct nexthop_type_ops 
 	case GR_NH_T_SR6:
 	case GR_NH_T_SR6_LOCAL:
 	case GR_NH_T_DNAT:
-		if (ops == NULL || ops->free)
+		if (ops == NULL || ops->free == NULL)
 			ABORT("invalid type ops");
 		if (type_ops[type] != NULL)
 			ABORT("duplicate type ops %hhu", type);