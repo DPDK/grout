62071f39ab1bb77307a821019d25440d6d9edfbf
Robin Jarry <robin@jarry.cc>
2025-06-25 23:04:42 +0200

    rx,tx: enable multi segment to support higher mtu
    In order to support receiving packets larger than 2048 bytes, we need to
    enable multi-segment rx and tx offload in the drivers.
    
    Enable RTE_ETH_RX_OFFLOAD_SCATTER and RTE_ETH_TX_OFFLOAD_MULTI_SEGS in
    the requested offload flags when configuring DPDK ports.
    
    Update the basic IP forward test to check we are indeed receiving 5000
    bytes packets with a configured MTU of 9000 bytes.
    
    Suggested-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- smoke/ip_forward_test.sh
+++ smoke/ip_forward_test.sh
@@ -7,8 +7,8 @@
 p0=${run_id}0
 p1=${run_id}1
 
-grcli add interface port $p0 devargs net_tap0,iface=$p0 mac f0:0d:ac:dc:00:00
-grcli add interface port $p1 devargs net_tap1,iface=$p1 mac f0:0d:ac:dc:00:01
+grcli add interface port $p0 devargs net_tap0,iface=$p0 mac f0:0d:ac:dc:00:00 mtu 9000
+grcli add interface port $p1 devargs net_tap1,iface=$p1 mac f0:0d:ac:dc:00:01 mtu 9000
 grcli add ip address 172.16.0.1/24 iface $p0
 grcli add ip address 172.16.1.1/24 iface $p1
 grcli add ip route 16.0.0.0/16 via 172.16.0.2
@@ -19,7 +19,7 @@ for n in 0 1; do
 	p=$run_id$n
 	netns_add $p
 	ip link set $p netns $p
-	ip -n $p link set $p address ba:d0:ca:ca:00:0$n
+	ip -n $p link set $p address ba:d0:ca:ca:00:0$n mtu 9000
 	ip -n $p link set $p up
 	ip -n $p link set lo up
 	ip -n $p addr add 172.16.$n.2/24 dev $p
@@ -28,8 +28,8 @@ for n in 0 1; do
 	ip -n $p addr show
 done
 
-ip netns exec $p0 ping -i0.01 -c3 -n 16.1.0.1
-ip netns exec $p1 ping -i0.01 -c3 -n 16.0.0.1
+ip netns exec $p0 ping -i0.01 -c3 -n 16.1.0.1 -s 5000
+ip netns exec $p1 ping -i0.01 -c3 -n 16.0.0.1 -s 5000
 ip netns exec $p0 ping -i0.01 -c3 -n 172.16.1.2
 ip netns exec $p1 ping -i0.01 -c3 -n 172.16.0.2
 ip netns exec $p0 ping -i0.01 -c3 -n 172.16.0.1
