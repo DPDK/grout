DPDK/grout

commit 4e3ba188c6285aba14c32a7434206af633cdb989

nexthop: factorize api calls for ipv4 and ipv6
Instead of mostly duplicated code in the ip and ip6 modules, move the
management of nexthops in the infra module.

Add a new "add" operation in the nexthop_ops structure to be invoked
when creating a nexthop. Reuse the existing "free" operation when
deleting one.

Only allow creating GR_NH_IPV4 and GR_NH_IPV6 nexthops. The other types
are reserved of internal purposes in the SRv6 module.

Move CLI commands under the root "nexthop" context. All GR_NH_IPV4 and
GR_NH_IPV6 nexthops are now listed together. Adapt smoke tests
accordingly.

No functional change besides the CLI commands move.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/cli/nexthop.c
@@ -6,8 +6,10 @@
 #include <gr_cli_iface.h>
 #include <gr_net_types.h>
 #include <gr_nexthop.h>
+#include <gr_table.h>
 
 #include <ecoli.h>
+#include <libsmartcols.h>
 
 #include <stdio.h>
 #include <unistd.h>
@@ -56,6 +58,143 @@ static cmd_status_t show_config(const struct gr_api_client *c, const struct ec_p
 	return CMD_SUCCESS;
 }
 
+static cmd_status_t nh_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_nh_add_req req = {0};
+	struct gr_iface iface;
+
+	switch (arg_ip4(p, "IP", &req.nh.ipv4)) {
+	case 0:
+		req.nh.type = GR_NH_IPV4;
+		break;
+	case -EINVAL:
+		if (arg_ip6(p, "IP", &req.nh.ipv6) < 0)
+			return CMD_ERROR;
+		req.nh.type = GR_NH_IPV6;
+		break;
+	default:
+		return CMD_ERROR;
+	}
+	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
+		return CMD_ERROR;
+	req.nh.iface_id = iface.id;
+	req.nh.vrf_id = iface.vrf_id;
+
+	if (arg_eth_addr(p, "MAC", &req.nh.mac) < 0)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_NH_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t nh_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_nh_del_req req = {.missing_ok = true};
+
+	switch (arg_ip4(p, "IP", &req.nh.ipv4)) {
+	case 0:
+		req.nh.type = GR_NH_IPV4;
+		break;
+	case -EINVAL:
+		if (arg_ip6(p, "IP", &req.nh.ipv6) < 0)
+			return CMD_ERROR;
+		req.nh.type = GR_NH_IPV6;
+		break;
+	default:
+		return CMD_ERROR;
+	}
+	if (arg_u16(p, "VRF", &req.nh.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_NH_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t nh_list(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_nh_list_req req = {.vrf_id = UINT16_MAX};
+	struct libscols_table *table = scols_new_table();
+	const struct gr_nh_list_resp *resp;
+	struct gr_iface iface;
+	void *resp_ptr = NULL;
+	char buf[BUFSIZ];
+	clock_t now;
+	ssize_t n;
+
+	if (table == NULL)
+		return CMD_ERROR;
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+	if (gr_api_client_send_recv(c, GR_NH_LIST, sizeof(req), &req, &resp_ptr) < 0) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	resp = resp_ptr;
+	now = gr_clock_us();
+
+	scols_table_new_column(table, "VRF", 0, 0);
+	scols_table_new_column(table, "TYPE", 0, 0);
+	scols_table_new_column(table, "IP", 0, 0);
+	scols_table_new_column(table, "MAC", 0, 0);
+	scols_table_new_column(table, "IFACE", 0, 0);
+	scols_table_new_column(table, "QUEUE", 0, 0);
+	scols_table_new_column(table, "AGE", 0, 0);
+	scols_table_new_column(table, "STATE", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (size_t i = 0; i < resp->n_nhs; i++) {
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		const struct gr_nexthop *nh = &resp->nhs[i];
+
+		scols_line_sprintf(line, 0, "%u", nh->vrf_id);
+		scols_line_sprintf(line, 1, "%s", nh_type_name(nh));
+		scols_line_sprintf(line, 2, ADDR_F, ADDR_W(nh_af(nh)), &nh->addr);
+
+		if (nh->flags & GR_NH_F_REACHABLE) {
+			scols_line_sprintf(line, 3, ETH_F, &nh->mac);
+			if (iface_from_id(c, nh->iface_id, &iface) == 0)
+				scols_line_sprintf(line, 4, "%s", iface.name);
+			else
+				scols_line_sprintf(line, 4, "%u", nh->iface_id);
+			scols_line_sprintf(line, 5, "%u", nh->held_pkts);
+			if (nh->flags & GR_NH_F_STATIC)
+				scols_line_set_data(line, 6, "-");
+			else
+				scols_line_sprintf(
+					line, 6, "%ld", (now - nh->last_reply) / CLOCKS_PER_SEC
+				);
+		} else {
+			scols_line_set_data(line, 3, "??:??:??:??:??:??");
+			scols_line_set_data(line, 4, "?");
+			scols_line_sprintf(line, 5, "%u", nh->held_pkts);
+			scols_line_set_data(line, 6, "?");
+		}
+
+		n = 0;
+		buf[0] = '\0';
+		gr_nh_flags_foreach (f, nh->flags)
+			SAFE_BUF(snprintf, sizeof(buf), "%s ", gr_nh_flag_name(f));
+		if (n > 0)
+			buf[n - 1] = '\0';
+
+		scols_line_sprintf(line, 7, "%s", buf);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+err:
+	scols_unref_table(table);
+	free(resp_ptr);
+	return CMD_ERROR;
+}
+
 static int ctx_init(struct ec_node *root) {
 	int ret;
 
@@ -101,6 +240,37 @@ static int ctx_init(struct ec_node *root) {
 		"nexthop",
 		show_config,
 		"Show the current nexthop configuration.",
+	);
+	if (ret < 0)
+		return ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ADD),
+		"nexthop IP iface IFACE mac MAC",
+		nh_add,
+		"Add a new next hop.",
+		with_help("IPv4 address.", ec_node_re("IP", IP_ANY_RE)),
+		with_help("Ethernet address.", ec_node_re("MAC", ETH_ADDR_RE)),
+		with_help("Output interface.", ec_node_dyn("IFACE", complete_iface_names, NULL))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_DEL),
+		"nexthop IP [vrf VRF]",
+		nh_del,
+		"Delete a next hop.",
+		with_help("IPv4 address.", ec_node_re("IP", IP_ANY_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW),
+		"nexthop [vrf VRF]",
+		nh_list,
+		"List all next hops.",
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
 	);
 	if (ret < 0)
 		return ret;

modules/infra/api/nexthop.c
@@ -2,8 +2,11 @@
 // Copyright (c) 2025 Robin Jarry
 
 #include <gr_api.h>
+#include <gr_iface.h>
 #include <gr_module.h>
+#include <gr_nexthop.h>
 #include <gr_nh_control.h>
+#include <gr_vec.h>
 
 static struct api_out nh_config_get(const void * /*request*/, void **response) {
 	struct gr_infra_nh_config_get_resp *resp = malloc(sizeof(*resp));
@@ -18,24 +21,164 @@ static struct api_out nh_config_get(const void * /*request*/, void **response) {
 	return api_out(0, sizeof(*resp));
 }
 
-static struct api_out nh_config_set(const void *request, void ** /*response*/) {
-	const struct gr_infra_nh_config_set_req *req = request;
-	return api_out(-nexthop_config_set(&req->base), 0);
-}
-
-static struct gr_api_handler get_handler = {
+static struct gr_api_handler config_get_handler = {
 	.name = "nh config get",
 	.request_type = GR_INFRA_NH_CONFIG_GET,
 	.callback = nh_config_get,
 };
 
-static struct gr_api_handler set_handler = {
+static struct api_out nh_config_set(const void *request, void ** /*response*/) {
+	const struct gr_infra_nh_config_set_req *req = request;
+	return api_out(-nexthop_config_set(&req->base), 0);
+}
+
+static struct gr_api_handler config_set_handler = {
 	.name = "nh config set",
 	.request_type = GR_INFRA_NH_CONFIG_SET,
 	.callback = nh_config_set,
 };
 
-RTE_INIT(trace_init) {
-	gr_register_api_handler(&get_handler);
-	gr_register_api_handler(&set_handler);
+static struct api_out nh_add(const void *request, void ** /*response*/) {
+	const struct gr_nh_add_req *req = request;
+	const struct nexthop_ops *ops;
+	struct nexthop *nh;
+	int ret;
+
+	switch (req->nh.type) {
+	case GR_NH_IPV4:
+		if (req->nh.ipv4 == 0)
+			return api_out(EDESTADDRREQ, 0);
+		break;
+	case GR_NH_IPV6:
+		if (rte_ipv6_addr_is_unspec(&req->nh.ipv6))
+			return api_out(EDESTADDRREQ, 0);
+		break;
+	default:
+		return api_out(ENOPROTOOPT, 0);
+	}
+	if (req->nh.vrf_id >= MAX_VRFS)
+		return api_out(EOVERFLOW, 0);
+
+	if (iface_from_id(req->nh.iface_id) == NULL)
+		return api_out(errno, 0);
+
+	nh = nexthop_lookup(req->nh.type, req->nh.vrf_id, req->nh.iface_id, &req->nh.addr);
+	if (nh != NULL) {
+		if (req->exist_ok && req->nh.iface_id == nh->iface_id
+		    && rte_is_same_ether_addr(&req->nh.mac, &nh->mac))
+			return api_out(0, 0);
+		return api_out(EEXIST, 0);
+	}
+
+	nh = nexthop_new(req->nh.type, req->nh.vrf_id, req->nh.iface_id, &req->nh.addr);
+	if (nh == NULL)
+		return api_out(errno, 0);
+
+	nh->mac = req->nh.mac;
+	nh->flags = GR_NH_F_STATIC | GR_NH_F_REACHABLE;
+
+	ops = nexthop_ops_get(req->nh.type);
+	assert(ops != NULL);
+	ret = ops->add(nh);
+
+	return api_out(-ret, 0);
+}
+
+static struct gr_api_handler nh_add_handler = {
+	.name = "nexthop add",
+	.request_type = GR_NH_ADD,
+	.callback = nh_add,
+};
+
+static struct api_out nh_del(const void *request, void ** /*response*/) {
+	const struct gr_nh_del_req *req = request;
+	const struct nexthop_ops *ops;
+	struct nexthop *nh;
+
+	switch (req->nh.type) {
+	case GR_NH_IPV4:
+		if (req->nh.ipv4 == 0)
+			return api_out(EDESTADDRREQ, 0);
+		break;
+	case GR_NH_IPV6:
+		if (rte_ipv6_addr_is_unspec(&req->nh.ipv6))
+			return api_out(EDESTADDRREQ, 0);
+		break;
+	default:
+		return api_out(ENOPROTOOPT, 0);
+	}
+	if (req->nh.vrf_id >= MAX_VRFS)
+		return api_out(EOVERFLOW, 0);
+
+	nh = nexthop_lookup(req->nh.type, req->nh.vrf_id, req->nh.iface_id, &req->nh.addr);
+	if (nh == NULL) {
+		if (errno == ENOENT && req->missing_ok)
+			return api_out(0, 0);
+		return api_out(errno, 0);
+	}
+	if ((nh->flags & (GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_GATEWAY)) || nh->ref_count > 1)
+		return api_out(EBUSY, 0);
+
+	ops = nexthop_ops_get(req->nh.type);
+	assert(ops != NULL);
+	ops->free(nh);
+
+	return api_out(0, 0);
+}
+
+static struct gr_api_handler nh_del_handler = {
+	.name = "nexthop del",
+	.request_type = GR_NH_DEL,
+	.callback = nh_del,
+};
+
+struct list_context {
+	uint16_t vrf_id;
+	struct gr_nexthop *nh;
+};
+
+static void nh_list_cb(struct nexthop *nh, void *priv) {
+	struct list_context *ctx = priv;
+
+	if (nh->vrf_id != ctx->vrf_id && ctx->vrf_id != UINT16_MAX)
+		return;
+
+	gr_vec_add(ctx->nh, nh->base);
+}
+
+static struct api_out nh_list(const void *request, void **response) {
+	const struct gr_nh_list_req *req = request;
+	struct list_context ctx = {.vrf_id = req->vrf_id, .nh = NULL};
+	struct gr_nh_list_resp *resp = NULL;
+	size_t len;
+
+	nexthop_iter(nh_list_cb, &ctx);
+
+	len = sizeof(*resp) + gr_vec_len(ctx.nh) * sizeof(*ctx.nh);
+	if ((resp = calloc(1, len)) == NULL) {
+		gr_vec_free(ctx.nh);
+		return api_out(ENOMEM, 0);
+	}
+
+	resp->n_nhs = gr_vec_len(ctx.nh);
+	if (ctx.nh != NULL)
+		memcpy(resp->nhs, ctx.nh, resp->n_nhs * sizeof(resp->nhs[0]));
+	gr_vec_free(ctx.nh);
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static struct gr_api_handler nh_list_handler = {
+	.name = "nexthop list",
+	.request_type = GR_NH_LIST,
+	.callback = nh_list,
+};
+
+RTE_INIT(_init) {
+	gr_register_api_handler(&config_get_handler);
+	gr_register_api_handler(&config_set_handler);
+	gr_register_api_handler(&nh_add_handler);
+	gr_register_api_handler(&nh_del_handler);
+	gr_register_api_handler(&nh_list_handler);
 }

modules/ip/cli/nexthop.c
@@ -1,176 +1,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2024 Robin Jarry
-
-#include "ip.h"
-
-#include <gr_api.h>
-#include <gr_cli.h>
-#include <gr_cli_iface.h>
-#include <gr_clock.h>
-#include <gr_ip4.h>
-#include <gr_macro.h>
-#include <gr_net_types.h>
-#include <gr_table.h>
-
-#include <ecoli.h>
-#include <libsmartcols.h>
-
-#include <errno.h>
-#include <stdint.h>
-
-static cmd_status_t nh4_add(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip4_nh_add_req req = {0};
-	struct gr_iface iface;
-
-	if (arg_ip4(p, "IP", &req.nh.ipv4) < 0)
-		return CMD_ERROR;
-	if (arg_eth_addr(p, "MAC", &req.nh.mac) < 0)
-		return CMD_ERROR;
-	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
-		return CMD_ERROR;
-	req.nh.iface_id = iface.id;
-
-	if (gr_api_client_send_recv(c, GR_IP4_NH_ADD, sizeof(req), &req, NULL) < 0)
-		return CMD_ERROR;
-
-	return CMD_SUCCESS;
-}
-
-static cmd_status_t nh4_del(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip4_nh_del_req req = {.missing_ok = true};
-
-	if (arg_ip4(p, "IP", &req.host) < 0)
-		return CMD_ERROR;
-	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
-		return CMD_ERROR;
-
-	if (gr_api_client_send_recv(c, GR_IP4_NH_DEL, sizeof(req), &req, NULL) < 0)
-		return CMD_ERROR;
-
-	return CMD_SUCCESS;
-}
-
-static cmd_status_t nh4_list(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip4_nh_list_req req = {.vrf_id = UINT16_MAX};
-	struct libscols_table *table = scols_new_table();
-	const struct gr_ip4_nh_list_resp *resp;
-	struct gr_iface iface;
-	void *resp_ptr = NULL;
-	char buf[BUFSIZ];
-	clock_t now;
-	ssize_t n;
-
-	if (table == NULL)
-		return CMD_ERROR;
-	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT) {
-		scols_unref_table(table);
-		return CMD_ERROR;
-	}
-	if (gr_api_client_send_recv(c, GR_IP4_NH_LIST, sizeof(req), &req, &resp_ptr) < 0) {
-		scols_unref_table(table);
-		return CMD_ERROR;
-	}
-
-	resp = resp_ptr;
-	now = gr_clock_us();
-
-	scols_table_new_column(table, "VRF", 0, 0);
-	scols_table_new_column(table, "IP", 0, 0);
-	scols_table_new_column(table, "MAC", 0, 0);
-	scols_table_new_column(table, "IFACE", 0, 0);
-	scols_table_new_column(table, "QUEUE", 0, 0);
-	scols_table_new_column(table, "AGE", 0, 0);
-	scols_table_new_column(table, "STATE", 0, 0);
-	scols_table_set_column_separator(table, "  ");
-
-	for (size_t i = 0; i < resp->n_nhs; i++) {
-		struct libscols_line *line = scols_table_new_line(table, NULL);
-		const struct gr_nexthop *nh = &resp->nhs[i];
-
-		n = 0;
-		buf[0] = '\0';
-		gr_nh_flags_foreach (f, nh->flags)
-			SAFE_BUF(snprintf, sizeof(buf), "%s ", gr_nh_flag_name(f));
-		if (n > 0)
-			buf[n - 1] = '\0';
-
-		scols_line_sprintf(line, 0, "%u", nh->vrf_id);
-		scols_line_sprintf(line, 1, IP4_F, &nh->ipv4);
-		if (nh->flags & GR_NH_F_REACHABLE) {
-			scols_line_sprintf(line, 2, ETH_F, &nh->mac);
-			if (iface_from_id(c, nh->iface_id, &iface) == 0)
-				scols_line_sprintf(line, 3, "%s", iface.name);
-			else
-				scols_line_sprintf(line, 3, "%u", nh->iface_id);
-			scols_line_sprintf(line, 4, "%u", nh->held_pkts);
-			if (nh->flags & GR_NH_F_STATIC)
-				scols_line_set_data(line, 5, "-");
-			else
-				scols_line_sprintf(
-					line, 5, "%ld", (now - nh->last_reply) / CLOCKS_PER_SEC
-				);
-		} else {
-			scols_line_set_data(line, 2, "??:??:??:??:??:??");
-			scols_line_set_data(line, 3, "?");
-			scols_line_sprintf(line, 4, "%u", nh->held_pkts);
-			scols_line_set_data(line, 5, "?");
-		}
-		scols_line_sprintf(line, 6, "%s", buf);
-	}
-
-	scols_print_table(table);
-	scols_unref_table(table);
-	free(resp_ptr);
-
-	return CMD_SUCCESS;
-err:
-	scols_unref_table(table);
-	free(resp_ptr);
-	return CMD_ERROR;
-}
-
-static int ctx_init(struct ec_node *root) {
-	int ret;
-
-	ret = CLI_COMMAND(
-		IP_ADD_CTX(root),
-		"nexthop IP mac MAC iface IFACE",
-		nh4_add,
-		"Add a new next hop.",
-		with_help("IPv4 address.", ec_node_re("IP", IPV4_RE)),
-		with_help("Ethernet address.", ec_node_re("MAC", ETH_ADDR_RE)),
-		with_help("Output interface.", ec_node_dyn("IFACE", complete_iface_names, NULL))
-	);
-	if (ret < 0)
-		return ret;
-	ret = CLI_COMMAND(
-		IP_DEL_CTX(root),
-		"nexthop IP [vrf VRF]",
-		nh4_del,
-		"Delete a next hop.",
-		with_help("IPv4 address.", ec_node_re("IP", IPV4_RE)),
-		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
-	);
-	if (ret < 0)
-		return ret;
-	ret = CLI_COMMAND(
-		IP_SHOW_CTX(root),
-		"nexthop [vrf VRF]",
-		nh4_list,
-		"List all next hops.",
-		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
-	);
-	if (ret < 0)
-		return ret;
-
-	return 0;
-}
-
-static struct gr_cli_context ctx = {
-	.name = "ipv4 nexthop",
-	.init = ctx_init,
-};
-
-static void __attribute__((constructor, used)) init(void) {
-	register_context(&ctx);
-}

modules/ip/control/nexthop.c
@@ -175,92 +175,8 @@ free:
 	rte_pktmbuf_free(m);
 }
 
-static struct api_out nh4_add(const void *request, void ** /*response*/) {
-	const struct gr_ip4_nh_add_req *req = request;
-	struct nexthop *nh;
-	int ret;
-
-	if (req->nh.ipv4 == 0)
-		return api_out(EINVAL, 0);
-	if (req->nh.vrf_id >= MAX_VRFS)
-		return api_out(EOVERFLOW, 0);
-	if (iface_from_id(req->nh.iface_id) == NULL)
-		return api_out(errno, 0);
-
-	if ((nh = nh4_lookup(req->nh.vrf_id, req->nh.ipv4)) != NULL) {
-		if (req->exist_ok && req->nh.iface_id == nh->iface_id
-		    && rte_is_same_ether_addr(&req->nh.mac, &nh->mac))
-			return api_out(0, 0);
-		return api_out(EEXIST, 0);
-	}
-
-	if ((nh = nh4_new(req->nh.vrf_id, req->nh.iface_id, req->nh.ipv4)) == NULL)
-		return api_out(errno, 0);
-
-	nh->mac = req->nh.mac;
-	nh->flags = GR_NH_F_STATIC | GR_NH_F_REACHABLE;
-	ret = rib4_insert(nh->vrf_id, nh->ipv4, 32, GR_RT_ORIGIN_LINK, nh);
-
-	return api_out(-ret, 0);
-}
-
-static struct api_out nh4_del(const void *request, void ** /*response*/) {
-	const struct gr_ip4_nh_del_req *req = request;
-	struct nexthop *nh;
-
-	if (req->vrf_id >= MAX_VRFS)
-		return api_out(EOVERFLOW, 0);
-
-	if ((nh = nh4_lookup(req->vrf_id, req->host)) == NULL) {
-		if (errno == ENOENT && req->missing_ok)
-			return api_out(0, 0);
-		return api_out(errno, 0);
-	}
-	if ((nh->flags & (GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_GATEWAY)) || nh->ref_count > 1)
-		return api_out(EBUSY, 0);
-
-	// this also does ip4_nexthop_decref(), freeing the next hop
-	if (rib4_delete(req->vrf_id, req->host, 32) < 0)
-		return api_out(errno, 0);
-
-	return api_out(0, 0);
-}
-
-struct list_context {
-	uint16_t vrf_id;
-	struct gr_nexthop *nh;
-};
-
-static void nh_list_cb(struct nexthop *nh, void *priv) {
-	struct list_context *ctx = priv;
-
-	if (nh->type != GR_NH_IPV4 || (nh->vrf_id != ctx->vrf_id && ctx->vrf_id != UINT16_MAX))
-		return;
-
-	gr_vec_add(ctx->nh, nh->base);
-}
-
-static struct api_out nh4_list(const void *request, void **response) {
-	const struct gr_ip4_nh_list_req *req = request;
-	struct list_context ctx = {.vrf_id = req->vrf_id, .nh = NULL};
-	struct gr_ip4_nh_list_resp *resp = NULL;
-	size_t len;
-
-	nexthop_iter(nh_list_cb, &ctx);
-
-	len = sizeof(*resp) + gr_vec_len(ctx.nh) * sizeof(*ctx.nh);
-	if ((resp = calloc(1, len)) == NULL) {
-		gr_vec_free(ctx.nh);
-		return api_out(ENOMEM, 0);
-	}
-
-	resp->n_nhs = gr_vec_len(ctx.nh);
-	if (ctx.nh != NULL)
-		memcpy(resp->nhs, ctx.nh, resp->n_nhs * sizeof(resp->nhs[0]));
-	gr_vec_free(ctx.nh);
-	*response = resp;
-
-	return api_out(0, len);
+static int nh4_add(struct nexthop *nh) {
+	return rib4_insert(nh->vrf_id, nh->ipv4, 32, GR_RT_ORIGIN_LINK, nh);
 }
 
 static void nh4_free(struct nexthop *nh) {
@@ -276,22 +192,6 @@ static void nh4_init(struct event_base *) {
 	arp_output_reply_node = gr_control_input_register_handler("arp_output_reply", true);
 }
 
-static struct gr_api_handler nh4_add_handler = {
-	.name = "ipv4 nexthop add",
-	.request_type = GR_IP4_NH_ADD,
-	.callback = nh4_add,
-};
-static struct gr_api_handler nh4_del_handler = {
-	.name = "ipv4 nexthop del",
-	.request_type = GR_IP4_NH_DEL,
-	.callback = nh4_del,
-};
-static struct gr_api_handler nh4_list_handler = {
-	.name = "ipv4 nexthop list",
-	.request_type = GR_IP4_NH_LIST,
-	.callback = nh4_list,
-};
-
 static struct gr_module nh4_module = {
 	.name = "ipv4 nexthop",
 	.depends_on = "graph",
@@ -299,14 +199,12 @@ static struct gr_module nh4_module = {
 };
 
 static struct nexthop_ops nh_ops = {
+	.add = nh4_add,
 	.solicit = arp_output_request_solicit,
 	.free = nh4_free,
 };
 
 RTE_INIT(control_ip_init) {
-	gr_register_api_handler(&nh4_add_handler);
-	gr_register_api_handler(&nh4_del_handler);
-	gr_register_api_handler(&nh4_list_handler);
 	gr_register_module(&nh4_module);
 	nexthop_ops_register(GR_NH_IPV4, &nh_ops);
 }

modules/ip6/cli/nexthop.c
@@ -1,176 +1,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2024 Robin Jarry
-
-#include "ip.h"
-
-#include <gr_api.h>
-#include <gr_cli.h>
-#include <gr_cli_iface.h>
-#include <gr_clock.h>
-#include <gr_ip6.h>
-#include <gr_net_types.h>
-#include <gr_table.h>
-
-#include <ecoli.h>
-#include <libsmartcols.h>
-
-#include <errno.h>
-#include <stdint.h>
-
-static cmd_status_t nh6_add(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip6_nh_add_req req = {0};
-	struct gr_iface iface;
-
-	if (arg_ip6(p, "IP", &req.nh.ipv6) < 0)
-		return CMD_ERROR;
-	if (arg_eth_addr(p, "MAC", &req.nh.mac) < 0)
-		return CMD_ERROR;
-	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
-		return CMD_ERROR;
-	req.nh.iface_id = iface.id;
-
-	if (gr_api_client_send_recv(c, GR_IP6_NH_ADD, sizeof(req), &req, NULL) < 0)
-		return CMD_ERROR;
-
-	return CMD_SUCCESS;
-}
-
-static cmd_status_t nh6_del(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip6_nh_del_req req = {.missing_ok = true};
-
-	if (arg_ip6(p, "IP", &req.host) < 0)
-		return CMD_ERROR;
-	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
-		return CMD_ERROR;
-
-	if (gr_api_client_send_recv(c, GR_IP6_NH_DEL, sizeof(req), &req, NULL) < 0)
-		return CMD_ERROR;
-
-	return CMD_SUCCESS;
-}
-
-static cmd_status_t nh6_list(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_ip6_nh_list_req req = {.vrf_id = UINT16_MAX};
-	struct libscols_table *table = scols_new_table();
-	const struct gr_ip6_nh_list_resp *resp;
-	struct gr_iface iface;
-	void *resp_ptr = NULL;
-	char buf[128];
-	clock_t now;
-	ssize_t n;
-
-	if (table == NULL)
-		return CMD_ERROR;
-	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT) {
-		scols_unref_table(table);
-		return CMD_ERROR;
-	}
-	if (gr_api_client_send_recv(c, GR_IP6_NH_LIST, sizeof(req), &req, &resp_ptr) < 0) {
-		scols_unref_table(table);
-		return CMD_ERROR;
-	}
-
-	resp = resp_ptr;
-	now = gr_clock_us();
-
-	scols_table_new_column(table, "VRF", 0, 0);
-	scols_table_new_column(table, "IP", 0, 0);
-	scols_table_new_column(table, "MAC", 0, 0);
-	scols_table_new_column(table, "IFACE", 0, 0);
-	scols_table_new_column(table, "QUEUE", 0, 0);
-	scols_table_new_column(table, "AGE", 0, 0);
-	scols_table_new_column(table, "STATE", 0, 0);
-	scols_table_set_column_separator(table, "  ");
-
-	for (size_t i = 0; i < resp->n_nhs; i++) {
-		struct libscols_line *line = scols_table_new_line(table, NULL);
-		const struct gr_nexthop *nh = &resp->nhs[i];
-
-		n = 0;
-		buf[0] = '\0';
-		gr_nh_flags_foreach (f, nh->flags)
-			SAFE_BUF(snprintf, sizeof(buf), "%s ", gr_nh_flag_name(f));
-		if (n > 0)
-			buf[n - 1] = '\0';
-
-		scols_line_sprintf(line, 0, "%u", nh->vrf_id);
-		scols_line_sprintf(line, 1, IP6_F, &nh->ipv6);
-		if (nh->flags & GR_NH_F_REACHABLE) {
-			scols_line_sprintf(line, 2, ETH_F, &nh->mac);
-			if (iface_from_id(c, nh->iface_id, &iface) == 0)
-				scols_line_sprintf(line, 3, "%s", iface.name);
-			else
-				scols_line_sprintf(line, 3, "%u", nh->iface_id);
-			scols_line_sprintf(line, 4, "%u", nh->held_pkts);
-			if (nh->flags & GR_NH_F_STATIC)
-				scols_line_set_data(line, 5, "-");
-			else
-				scols_line_sprintf(
-					line, 5, "%ld", (now - nh->last_reply) / CLOCKS_PER_SEC
-				);
-		} else {
-			scols_line_set_data(line, 2, "??:??:??:??:??:??");
-			scols_line_set_data(line, 3, "?");
-			scols_line_sprintf(line, 4, "%u", nh->held_pkts);
-			scols_line_set_data(line, 5, "?");
-		}
-		scols_line_sprintf(line, 6, "%s", buf);
-	}
-
-	scols_print_table(table);
-	scols_unref_table(table);
-	free(resp_ptr);
-
-	return CMD_SUCCESS;
-err:
-	scols_unref_table(table);
-	free(resp_ptr);
-
-	return CMD_ERROR;
-}
-
-static int ctx_init(struct ec_node *root) {
-	int ret;
-
-	ret = CLI_COMMAND(
-		IP6_ADD_CTX(root),
-		"nexthop IP mac MAC iface IFACE",
-		nh6_add,
-		"Add a new next hop.",
-		with_help("IPv6 address.", ec_node_re("IP", IPV6_RE)),
-		with_help("Ethernet address.", ec_node_re("MAC", ETH_ADDR_RE)),
-		with_help("Output interface.", ec_node_dyn("IFACE", complete_iface_names, NULL))
-	);
-	if (ret < 0)
-		return ret;
-	ret = CLI_COMMAND(
-		IP6_DEL_CTX(root),
-		"nexthop IP [vrf VRF]",
-		nh6_del,
-		"Delete a next hop.",
-		with_help("IPv6 address.", ec_node_re("IP", IPV6_RE)),
-		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
-	);
-	if (ret < 0)
-		return ret;
-	ret = CLI_COMMAND(
-		IP6_SHOW_CTX(root),
-		"nexthop [vrf VRF]",
-		nh6_list,
-		"List all next hops.",
-		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
-	);
-	if (ret < 0)
-		return ret;
-
-	return 0;
-}
-
-static struct gr_cli_context ctx = {
-	.name = "ipv6 nexthop",
-	.init = ctx_init,
-};
-
-static void __attribute__((constructor, used)) init(void) {
-	register_context(&ctx);
-}

modules/ip6/control/nexthop.c
@@ -21,7 +21,6 @@
 #include <rte_errno.h>
 #include <rte_ether.h>
 #include <rte_ip6.h>
-#include <rte_mempool.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -220,95 +219,8 @@ free:
 	rte_pktmbuf_free(m);
 }
 
-static struct api_out nh6_add(const void *request, void ** /*response*/) {
-	const struct gr_ip6_nh_add_req *req = request;
-	struct nexthop *nh;
-	int ret;
-
-	if (rte_ipv6_addr_is_unspec(&req->nh.ipv6) || rte_ipv6_addr_is_mcast(&req->nh.ipv6))
-		return api_out(EINVAL, 0);
-	if (req->nh.vrf_id >= MAX_VRFS)
-		return api_out(EOVERFLOW, 0);
-	if (iface_from_id(req->nh.iface_id) == NULL)
-		return api_out(errno, 0);
-
-	if ((nh = nh6_lookup(req->nh.vrf_id, req->nh.iface_id, &req->nh.ipv6)) != NULL) {
-		if (req->exist_ok && req->nh.iface_id == nh->iface_id
-		    && rte_is_same_ether_addr(&req->nh.mac, &nh->mac))
-			return api_out(0, 0);
-		return api_out(EEXIST, 0);
-	}
-
-	if ((nh = nh6_new(req->nh.vrf_id, req->nh.iface_id, &req->nh.ipv6)) == NULL)
-		return api_out(errno, 0);
-
-	nh->mac = req->nh.mac;
-	nh->flags = GR_NH_F_STATIC | GR_NH_F_REACHABLE;
-	ret = rib6_insert(
-		nh->vrf_id, nh->iface_id, &nh->ipv6, RTE_IPV6_MAX_DEPTH, GR_RT_ORIGIN_LINK, nh
-	);
-
-	return api_out(-ret, 0);
-}
-
-static struct api_out nh6_del(const void *request, void ** /*response*/) {
-	const struct gr_ip6_nh_del_req *req = request;
-	struct nexthop *nh;
-
-	if (req->vrf_id >= MAX_VRFS)
-		return api_out(EOVERFLOW, 0);
-
-	if ((nh = nh6_lookup(req->vrf_id, GR_IFACE_ID_UNDEF, &req->host)) == NULL) {
-		if (errno == ENOENT && req->missing_ok)
-			return api_out(0, 0);
-		return api_out(errno, 0);
-	}
-	if ((nh->flags & (GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_GATEWAY)) || nh->ref_count > 1)
-		return api_out(EBUSY, 0);
-
-	// this also does nh6_decref(), freeing the next hop
-	if (rib6_delete(req->vrf_id, GR_IFACE_ID_UNDEF, &req->host, RTE_IPV6_MAX_DEPTH) < 0)
-		return api_out(errno, 0);
-
-	return api_out(0, 0);
-}
-
-struct list_context {
-	uint16_t vrf_id;
-	struct gr_nexthop *nh;
-};
-
-static void nh_list_cb(struct nexthop *nh, void *priv) {
-	struct list_context *ctx = priv;
-
-	if (nh->type != GR_NH_IPV6 || (nh->vrf_id != ctx->vrf_id && ctx->vrf_id != UINT16_MAX)
-	    || rte_ipv6_addr_is_mcast(&nh->ipv6))
-		return;
-
-	gr_vec_add(ctx->nh, nh->base);
-}
-
-static struct api_out nh6_list(const void *request, void **response) {
-	const struct gr_ip6_nh_list_req *req = request;
-	struct list_context ctx = {.vrf_id = req->vrf_id, .nh = NULL};
-	struct gr_ip6_nh_list_resp *resp = NULL;
-	size_t len;
-
-	nexthop_iter(nh_list_cb, &ctx);
-
-	len = sizeof(*resp) + gr_vec_len(ctx.nh) * sizeof(*ctx.nh);
-	if ((resp = calloc(1, len)) == NULL) {
-		gr_vec_free(ctx.nh);
-		return api_out(ENOMEM, 0);
-	}
-
-	resp->n_nhs = gr_vec_len(ctx.nh);
-	if (ctx.nh != NULL)
-		memcpy(resp->nhs, ctx.nh, resp->n_nhs * sizeof(resp->nhs[0]));
-	gr_vec_free(ctx.nh);
-	*response = resp;
-
-	return api_out(0, len);
+static int nh6_add(struct nexthop *nh) {
+	return rib6_insert(nh->vrf_id, nh->iface_id, &nh->ipv6, 128, GR_RT_ORIGIN_LINK, nh);
 }
 
 static void nh6_free(struct nexthop *nh) {
@@ -324,22 +236,6 @@ static void nh6_init(struct event_base *) {
 	ndp_na_output_node = gr_control_input_register_handler("ndp_na_output", true);
 }
 
-static struct gr_api_handler nh6_add_handler = {
-	.name = "ipv6 nexthop add",
-	.request_type = GR_IP6_NH_ADD,
-	.callback = nh6_add,
-};
-static struct gr_api_handler nh6_del_handler = {
-	.name = "ipv6 nexthop del",
-	.request_type = GR_IP6_NH_DEL,
-	.callback = nh6_del,
-};
-static struct gr_api_handler nh6_list_handler = {
-	.name = "ipv6 nexthop list",
-	.request_type = GR_IP6_NH_LIST,
-	.callback = nh6_list,
-};
-
 static struct gr_module nh6_module = {
 	.name = "ipv6 nexthop",
 	.depends_on = "graph",
@@ -347,14 +243,12 @@ static struct gr_module nh6_module = {
 };
 
 static struct nexthop_ops nh_ops = {
+	.add = nh6_add,
 	.solicit = nh6_solicit,
 	.free = nh6_free,
 };
 
 RTE_INIT(control_ip_init) {
-	gr_register_api_handler(&nh6_add_handler);
-	gr_register_api_handler(&nh6_del_handler);
-	gr_register_api_handler(&nh6_list_handler);
 	gr_register_module(&nh6_module);
 	nexthop_ops_register(GR_NH_IPV6, &nh_ops);
 }

commit a6d7aac86d100d9f1006b48dd51494d7d6c8591f

cli: set exist_ok to true when adding nexthops
This was forgotten when merging IPv4 and IPv6.

Fixes: 4e3ba188c628 ("nexthop: factorize api calls for ipv4 and ipv6")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/cli/nexthop.c
@@ -59,7 +59,7 @@ static cmd_status_t show_config(const struct gr_api_client *c, const struct ec_p
 }
 
 static cmd_status_t nh_add(const struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_nh_add_req req = {0};
+	struct gr_nh_add_req req = {.exist_ok = true};
 	struct gr_iface iface;
 
 	switch (arg_ip4(p, "IP", &req.nh.ipv4)) {