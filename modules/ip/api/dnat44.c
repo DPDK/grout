DPDK/grout

commit 93647f952012b8b875d289c187724564abbdfa11

ip: add stateless dnat44 support
Allow configuring stateless "destination NAT" addresses. This is
a convoluted term to represent the following:

                          client
                |                        ^
                | destination            |  source changed
                |    8.8.8.8             |   to 8.8.8.8
                v                        |
            +----------------------------------+
            |       iface p0    8.8.8.1/24     |
            |                                  |
            |              grout               |
            |                                  |
            |       iface p1    10.16.0.1/24   |
            +----------------------------------+
                 |                       |
          destination changed            |
             to 10.16.0.99               |
                 |                       |
                 v                       |
                    service 10.16.0.99

This enables grout for exposing virtual/floating IP addresses on one
interface without actually owning these addresses. And internally
translating the packet addresses to forward packets to another host
located behind another interface.

DNAT44 is always bi-directional and is configured from the "external" or
"public" standpoint. Packets received on the "public" interface with
a specific IP address will have their destination address changed to an
internal IP. Packets transmitted from the public interface that have
a source IP address matching the internal IP will have their source IP
changed to the "public" IP.

Internally, this feature is implemented using a nexthop for the public
facing address (the matching address) since nexthops themselves are
required to support ARP and normal communication, we only store the
address to substitute on packets in a private field. The new
GR_NH_F_DNAT flag is added for this purpose to identify nexthops which
are used for DNAT.

modules/ip/api/dnat44.c
@@ -1,0 +1,153 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#include <gr_api.h>
+#include <gr_ip4.h>
+#include <gr_ip4_datapath.h>
+#include <gr_module.h>
+#include <gr_vec.h>
+
+static struct api_out dnat44_add(const void *request, void ** /*response*/) {
+	const struct gr_dnat44_add_req *req = request;
+	struct dnat44_nh_data *data;
+	struct iface *iface;
+	struct nexthop *nh;
+	int ret;
+
+	iface = iface_from_id(req->rule.iface_id);
+	if (iface == NULL)
+		return api_out(ENODEV, 0);
+
+	nh = nh4_lookup(iface->vrf_id, req->rule.match);
+	if (nh != NULL) {
+		data = dnat44_nh_data(nh);
+		if (nh->type != GR_NH_T_DNAT || data->replace != req->rule.replace)
+			return api_out(EADDRINUSE, 0);
+		if (req->exist_ok)
+			return api_out(0, 0);
+		return api_out(EEXIST, 0);
+	}
+
+	nh = nh4_new(iface->vrf_id, iface->id, req->rule.match);
+	if (nh == NULL)
+		return api_out(ENOMEM, 0);
+
+	nh->flags = GR_NH_F_LOCAL | GR_NH_F_STATIC;
+	nh->state = GR_NH_S_REACHABLE;
+	nh->type = GR_NH_T_DNAT;
+	data = dnat44_nh_data(nh);
+	data->replace = req->rule.replace;
+	ret = rib4_insert(iface->vrf_id, req->rule.match, 32, GR_RT_ORIGIN_INTERNAL, nh);
+	if (ret < 0)
+		return api_out(-ret, 0);
+
+	ret = snat44_rule_add(iface, req->rule.replace, req->rule.match);
+	if (ret < 0)
+		goto fail;
+
+	return api_out(0, 0);
+fail:
+	rib4_delete(iface->vrf_id, req->rule.match, 32);
+	snat44_rule_del(iface, req->rule.replace);
+	return api_out(-ret, 0);
+}
+
+static struct api_out dnat44_del(const void *request, void ** /*response*/) {
+	const struct gr_dnat44_del_req *req = request;
+	struct dnat44_nh_data *data;
+	struct iface *iface;
+	struct nexthop *nh;
+
+	iface = iface_from_id(req->rule.iface_id);
+	if (iface == NULL)
+		return api_out(ENODEV, 0);
+
+	nh = nh4_lookup(iface->vrf_id, req->rule.match);
+	if (nh == NULL) {
+		if (req->missing_ok)
+			return api_out(0, 0);
+		return api_out(ENOENT, 0);
+	}
+	data = dnat44_nh_data(nh);
+	if (nh->type != GR_NH_T_DNAT || data->replace != req->rule.replace)
+		return api_out(EADDRINUSE, 0);
+
+	rib4_delete(iface->vrf_id, req->rule.match, 32);
+	snat44_rule_del(iface, req->rule.replace);
+
+	return api_out(0, 0);
+}
+
+struct dnat44_list_iterator {
+	uint16_t vrf_id;
+	struct gr_dnat44_rule *rules;
+};
+
+static void dnat44_list_iter(struct nexthop *nh, void *priv) {
+	struct dnat44_list_iterator *iter = priv;
+	struct dnat44_nh_data *data;
+
+	if (iter->vrf_id != GR_VRF_ID_ALL && nh->vrf_id != iter->vrf_id)
+		return;
+
+	if (nh->type != GR_NH_T_DNAT)
+		return;
+
+	data = dnat44_nh_data(nh);
+
+	struct gr_dnat44_rule rule = {
+		.iface_id = nh->iface_id,
+		.match = nh->ipv4,
+		.replace = data->replace,
+	};
+	gr_vec_add(iter->rules, rule);
+}
+
+static struct api_out dnat44_list(const void *request, void **response) {
+	const struct gr_dnat44_list_req *req = request;
+	struct gr_dnat44_list_resp *resp;
+	struct dnat44_list_iterator iter = {
+		.vrf_id = req->vrf_id,
+		.rules = NULL,
+	};
+	size_t len;
+
+	nexthop_iter(dnat44_list_iter, &iter);
+
+	len = sizeof(*resp) + gr_vec_len(iter.rules) * sizeof(struct gr_dnat44_rule);
+	resp = malloc(len);
+	if (resp == NULL) {
+		free(iter.rules);
+		return api_out(ENOMEM, 0);
+	}
+
+	resp->n_rules = gr_vec_len(iter.rules);
+	memcpy(resp->rules, iter.rules, gr_vec_len(iter.rules) * sizeof(struct gr_dnat44_rule));
+	gr_vec_free(iter.rules);
+
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static struct gr_api_handler add_handler = {
+	.name = "nat44 add",
+	.request_type = GR_DNAT44_ADD,
+	.callback = dnat44_add,
+};
+static struct gr_api_handler del_handler = {
+	.name = "nat44 del",
+	.request_type = GR_DNAT44_DEL,
+	.callback = dnat44_del,
+};
+static struct gr_api_handler list_handler = {
+	.name = "nat44 list",
+	.request_type = GR_DNAT44_LIST,
+	.callback = dnat44_list,
+};
+
+RTE_INIT(_init) {
+	gr_register_api_handler(&add_handler);
+	gr_register_api_handler(&del_handler);
+	gr_register_api_handler(&list_handler);
+}

modules/ip/cli/dnat44.c
@@ -1,0 +1,146 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#include "ip.h"
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_cli_iface.h>
+#include <gr_ip4.h>
+#include <gr_net_types.h>
+#include <gr_table.h>
+
+#include <ecoli.h>
+#include <libsmartcols.h>
+
+#include <stdint.h>
+
+static cmd_status_t dnat44_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_dnat44_add_req req = {.exist_ok = true};
+	struct gr_iface iface;
+
+	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
+		return CMD_ERROR;
+	req.rule.iface_id = iface.id;
+	if (arg_ip4(p, "DEST", &req.rule.match) < 0)
+		return CMD_ERROR;
+	if (arg_ip4(p, "REPLACE", &req.rule.replace) < 0)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_DNAT44_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t dnat44_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_dnat44_del_req req = {.missing_ok = true};
+	struct gr_iface iface;
+
+	if (iface_from_name(c, arg_str(p, "IFACE"), &iface) < 0)
+		return CMD_ERROR;
+	req.rule.iface_id = iface.id;
+	if (arg_ip4(p, "DEST", &req.rule.match) < 0)
+		return CMD_ERROR;
+	if (arg_ip4(p, "REPLACE", &req.rule.replace) < 0)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_DNAT44_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t dnat44_list(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct libscols_table *table = scols_new_table();
+	const struct gr_dnat44_list_resp *resp;
+	struct gr_dnat44_list_req req = {0};
+	void *resp_ptr = NULL;
+
+	if (table == NULL)
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	if (gr_api_client_send_recv(c, GR_DNAT44_LIST, sizeof(req), &req, &resp_ptr) < 0) {
+		scols_unref_table(table);
+		return CMD_ERROR;
+	}
+
+	resp = resp_ptr;
+
+	scols_table_new_column(table, "INTERFACE", 0, 0);
+	scols_table_new_column(table, "DESTINATION", 0, 0);
+	scols_table_new_column(table, "REPLACE", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (size_t i = 0; i < resp->n_rules; i++) {
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		const struct gr_dnat44_rule *rule = &resp->rules[i];
+		struct gr_iface iface;
+
+		if (iface_from_id(c, rule->iface_id, &iface) < 0)
+			scols_line_sprintf(line, 0, "%u", rule->iface_id);
+		else
+			scols_line_sprintf(line, 0, "%s", iface.name);
+
+		scols_line_sprintf(line, 1, IP4_F, &rule->match);
+		scols_line_sprintf(line, 2, IP4_F, &rule->replace);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		IP_ADD_CTX(root),
+		"dnat44 interface IFACE destination DEST replace REPLACE",
+		dnat44_add,
+		"Create a DNAT44 rule.",
+		with_help("Input interface.", ec_node_dyn("IFACE", complete_iface_names, NULL)),
+		with_help("Destination IPv4 address to match.", ec_node_re("DEST", IPV4_RE)),
+		with_help("Replace match with this IPv4 address.", ec_node_re("REPLACE", IPV4_RE))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP_DEL_CTX(root),
+		"dnat44 interface IFACE destination DEST replace REPLACE",
+		dnat44_del,
+		"Delete a DNAT44 rule.",
+		with_help("Input interface.", ec_node_dyn("IFACE", complete_iface_names, NULL)),
+		with_help("Destination IPv4 address to match.", ec_node_re("DEST", IPV4_RE)),
+		with_help("Replace match with this IPv4 address.", ec_node_re("REPLACE", IPV4_RE))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		IP_SHOW_CTX(root),
+		"dnat44 [vrf VRF]",
+		dnat44_list,
+		"Display DNAT44 rules.",
+		with_help("L3 addressing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "nat44",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/ip/datapath/dnat44.c
@@ -1,0 +1,76 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#include <gr_fib4.h>
+#include <gr_ip4_datapath.h>
+#include <gr_log.h>
+#include <gr_module.h>
+
+enum edges {
+	FORWARD = 0,
+	LOCAL,
+	NO_ROUTE,
+	EDGE_COUNT,
+};
+
+static uint16_t
+dnat44_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	struct ip_output_mbuf_data *d;
+	struct dnat44_nh_data *data;
+	struct rte_ipv4_hdr *ip;
+	struct rte_mbuf *mbuf;
+	rte_edge_t edge;
+	uint16_t i;
+
+	for (i = 0; i < nb_objs; i++) {
+		mbuf = objs[i];
+
+		d = ip_output_mbuf_data(mbuf);
+		data = dnat44_nh_data(d->nh);
+		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
+		ip->hdr_checksum = fixup_checksum(ip->hdr_checksum, ip->dst_addr, data->replace);
+		ip->dst_addr = data->replace;
+
+		d->nh = fib4_lookup(d->iface->vrf_id, ip->dst_addr);
+
+		if (d->nh == NULL)
+			edge = NO_ROUTE;
+		else if (d->nh->flags & GR_NH_F_LOCAL && ip->dst_addr == d->nh->ipv4)
+			edge = LOCAL;
+		else
+			edge = FORWARD;
+
+		if (gr_mbuf_is_traced(mbuf)) {
+			struct rte_ipv4_hdr *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
+			*t = *ip;
+		}
+		rte_node_enqueue_x1(graph, node, edge, mbuf);
+	}
+
+	return nb_objs;
+}
+
+static void dnat44_register(void) {
+	ip_input_register_nexthop_type(GR_NH_T_DNAT, "dnat44");
+}
+
+static struct rte_node_register node = {
+	.name = "dnat44",
+
+	.process = dnat44_process,
+
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[FORWARD] = "ip_forward",
+		[LOCAL] = "ip_input_local",
+		[NO_ROUTE] = "ip_error_dest_unreach",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.register_callback = dnat44_register,
+	.trace_format = (gr_trace_format_cb_t)trace_ip_format,
+};
+
+GR_NODE_REGISTER(info);

modules/infra/api/gr_infra.h
@@ -29,6 +29,7 @@ typedef enum : uint16_t {
 	GR_IFACE_F_PROMISC = GR_BIT16(1),
 	GR_IFACE_F_ALLMULTI = GR_BIT16(2),
 	GR_IFACE_F_PACKET_TRACE = GR_BIT16(3),
+	GR_IFACE_F_SNAT = GR_BIT16(4),
 } gr_iface_flags_t;
 
 // Interface state flags

modules/infra/api/gr_nexthop.h
@@ -30,6 +30,7 @@ typedef enum : uint8_t {
 	GR_NH_T_L3 = 0, // Default value
 	GR_NH_T_SR6,
 	GR_NH_T_SR6_LOCAL,
+	GR_NH_T_DNAT,
 } gr_nh_type_t;
 
 //! Nexthop structure exposed to the API.
@@ -105,6 +106,8 @@ static inline const char *gr_nh_type_name(const struct gr_nexthop *nh) {
 		return "SRv6";
 	case GR_NH_T_SR6_LOCAL:
 		return "SRv6-local";
+	case GR_NH_T_DNAT:
+		return "DNAT";
 	}
 	return "?";
 }

modules/infra/cli/iface.c
@@ -282,6 +282,8 @@ static cmd_status_t iface_list(const struct gr_api_client *c, const struct ec_pn
 			SAFE_BUF(snprintf, sizeof(buf), " allmulti");
 		if (iface->flags & GR_IFACE_F_PACKET_TRACE)
 			SAFE_BUF(snprintf, sizeof(buf), " tracing");
+		if (iface->flags & GR_IFACE_F_SNAT)
+			SAFE_BUF(snprintf, sizeof(buf), " snat");
 		scols_line_set_data(line, 2, buf);
 
 		// mode

modules/infra/control/nexthop.c
@@ -115,6 +115,7 @@ void nexthop_type_ops_register(gr_nh_type_t type, const struct nexthop_type_ops 
 	case GR_NH_T_L3:
 	case GR_NH_T_SR6:
 	case GR_NH_T_SR6_LOCAL:
+	case GR_NH_T_DNAT:
 		if (ops == NULL || ops->free)
 			ABORT("invalid type ops");
 		if (type_ops[type] != NULL)

commit 57d76349d14877d97b0183c52f37d89617087424

dnat44: use correct free function
The rules are a gr_vec. Free them with the correct function in case of
error.

Fixes: 93647f952012 ("ip: add stateless dnat44 support")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/api/dnat44.c
@@ -123,7 +123,7 @@ static struct api_out dnat44_list(const void *request, void **response) {
 	len = sizeof(*resp) + gr_vec_len(iter.rules) * sizeof(struct gr_dnat44_rule);
 	resp = malloc(len);
 	if (resp == NULL) {
-		free(iter.rules);
+		gr_vec_free(iter.rules);
 		return api_out(ENOMEM, 0);
 	}
 