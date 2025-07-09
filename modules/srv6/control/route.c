DPDK/grout

commit 3f7be3ff32cd88fd9fd7f67922b90f4abe2f6e2e

nexthop: change nexthop_new to accept a full object
Currently, when creating nexthops, we only create partial objects and
update some fields afterwards. This causes notification events to carry
incomplete objects.

Change nexthop_new to take a single struct gr_nexthop object as
argument. That way we can create entries with all fields initialized
which ensures the notification event payloads to have all proper data.

Remove nh4_new and nh6_new to avoid having too many indirections.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/srv6/control/route.c
@@ -36,7 +36,6 @@ static int srv6_encap_data_add(
 	memcpy(d->seglist, seglist, sizeof(d->seglist[0]) * n_seglist);
 
 	srv6_encap_nh_priv(nh)->d = d;
-	nh->type = GR_NH_T_SR6_OUTPUT;
 
 	return 0;
 }
@@ -50,6 +49,14 @@ static void srv6_encap_data_del(struct nexthop *nh) {
 // srv6 route ////////////////////////////////////////////////////////////////
 static struct api_out srv6_route_add(const void *request, void ** /*response*/) {
 	const struct gr_srv6_route_add_req *req = request;
+	struct gr_nexthop base = {
+		.type = GR_NH_T_SR6_OUTPUT,
+		.af = GR_AF_IP6,
+		.state = GR_NH_S_REACHABLE,
+		.flags = GR_NH_F_GATEWAY | GR_NH_F_STATIC,
+		.vrf_id = req->r.key.vrf_id,
+		.iface_id = GR_IFACE_ID_UNDEF,
+	};
 	struct nexthop *nh;
 	int ret;
 
@@ -65,13 +72,8 @@ static struct api_out srv6_route_add(const void *request, void ** /*response*/) 
 		if (nh && srv6_encap_nh_priv(nh)->d != NULL)
 			return api_out(EEXIST, 0);
 
-		nh = nexthop_new(
-			GR_AF_IP6, req->r.key.vrf_id, GR_IFACE_ID_UNDEF, &req->r.key.dest6.ip
-		);
-		if (nh == NULL)
-			return api_out(errno, 0);
-
-		nh->prefixlen = req->r.key.dest6.prefixlen;
+		base.ipv6 = req->r.key.dest6.ip;
+		base.prefixlen = req->r.key.dest6.prefixlen;
 	} else {
 		nh = rib4_lookup_exact(
 			req->r.key.vrf_id, req->r.key.dest4.ip, req->r.key.dest4.prefixlen
@@ -79,15 +81,13 @@ static struct api_out srv6_route_add(const void *request, void ** /*response*/) 
 		if (nh && srv6_encap_nh_priv(nh)->d != NULL)
 			return api_out(EEXIST, 0);
 
-		nh = nexthop_new(
-			GR_AF_IP4, req->r.key.vrf_id, GR_IFACE_ID_UNDEF, &req->r.key.dest4.ip
-		);
-		if (nh == NULL)
-			return api_out(errno, 0);
-		nh->prefixlen = req->r.key.dest4.prefixlen;
+		base.ipv4 = req->r.key.dest4.ip;
+		base.prefixlen = req->r.key.dest4.prefixlen;
 	}
-	nh->flags |= GR_NH_F_GATEWAY | GR_NH_F_STATIC;
-	nh->state = GR_NH_S_REACHABLE;
+
+	nh = nexthop_new(&base);
+	if (nh == NULL)
+		return api_out(errno, 0);
 
 	ret = srv6_encap_data_add(nh, req->r.encap_behavior, req->r.n_seglist, req->r.seglist);
 	if (ret < 0) {

modules/ip/control/route.c
@@ -227,9 +227,16 @@ static struct api_out route4_add(const void *request, void ** /*response*/) {
 	// if the route gateway is reachable via a prefix route,
 	// create a new unresolved nexthop
 	if (nh->ipv4 != req->nh) {
-		if ((nh = nh4_new(req->vrf_id, nh->iface_id, req->nh)) == NULL)
+		nh = nexthop_new(&(struct gr_nexthop) {
+			.type = GR_NH_T_L3,
+			.af = GR_AF_IP4,
+			.flags = GR_NH_F_GATEWAY,
+			.vrf_id = req->vrf_id,
+			.iface_id = nh->iface_id,
+			.ipv4 = req->nh,
+		});
+		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->flags |= GR_NH_F_GATEWAY;
 	}
 
 	// if route insert fails, the created nexthop will be freed

modules/ip6/control/route.c
@@ -248,9 +248,16 @@ static struct api_out route6_add(const void *request, void ** /*response*/) {
 	// if the route gateway is reachable via a prefix route,
 	// create a new unresolved nexthop
 	if (!rte_ipv6_addr_eq(&nh->ipv6, &req->nh)) {
-		if ((nh = nh6_new(req->vrf_id, nh->iface_id, &req->nh)) == NULL)
+		nh = nexthop_new(&(struct gr_nexthop) {
+			.type = GR_NH_T_L3,
+			.af = GR_AF_IP6,
+			.flags = GR_NH_F_GATEWAY,
+			.vrf_id = nh->vrf_id,
+			.iface_id = nh->iface_id,
+			.ipv6 = req->nh,
+		});
+		if (nh == NULL)
 			return api_out(errno, 0);
-		nh->flags |= GR_NH_F_GATEWAY;
 	}
 
 	// if route insert fails, the created nexthop will be freed

modules/infra/api/nexthop.c
@@ -41,44 +41,48 @@ static struct gr_api_handler config_set_handler = {
 static struct api_out nh_add(const void *request, void ** /*response*/) {
 	const struct gr_nh_add_req *req = request;
 	const struct nexthop_af_ops *ops;
+	struct gr_nexthop base = req->nh;
 	struct nexthop *nh;
+
 	int ret;
 
-	switch (req->nh.af) {
+	switch (base.af) {
 	case GR_AF_IP4:
-		if (req->nh.ipv4 == 0)
+		if (base.ipv4 == 0)
 			return api_out(EDESTADDRREQ, 0);
 		break;
 	case GR_AF_IP6:
-		if (rte_ipv6_addr_is_unspec(&req->nh.ipv6))
+		if (rte_ipv6_addr_is_unspec(&base.ipv6))
 			return api_out(EDESTADDRREQ, 0);
 		break;
 	default:
 		return api_out(ENOPROTOOPT, 0);
 	}
-	if (req->nh.vrf_id >= MAX_VRFS)
+	if (base.vrf_id >= MAX_VRFS)
 		return api_out(EOVERFLOW, 0);
 
-	if (iface_from_id(req->nh.iface_id) == NULL)
+	if (iface_from_id(base.iface_id) == NULL)
 		return api_out(errno, 0);
 
-	nh = nexthop_lookup(req->nh.af, req->nh.vrf_id, req->nh.iface_id, &req->nh.addr);
+	nh = nexthop_lookup(base.af, base.vrf_id, base.iface_id, &base.addr);
 	if (nh != NULL) {
-		if (req->exist_ok && req->nh.iface_id == nh->iface_id
-		    && rte_is_same_ether_addr(&req->nh.mac, &nh->mac))
+		if (req->exist_ok && base.iface_id == nh->iface_id
+		    && rte_is_same_ether_addr(&base.mac, &nh->mac))
 			return api_out(0, 0);
 		return api_out(EEXIST, 0);
 	}
 
-	nh = nexthop_new(req->nh.af, req->nh.vrf_id, req->nh.iface_id, &req->nh.addr);
+	base.type = GR_NH_T_L3;
+	base.state = GR_NH_S_NEW;
+	base.flags = 0;
+	if (!rte_is_zero_ether_addr(&base.mac)) {
+		base.state = GR_NH_S_REACHABLE;
+		base.flags = GR_NH_F_STATIC;
+	}
+
+	nh = nexthop_new(&base);
 	if (nh == NULL)
 		return api_out(errno, 0);
-
-	nh->mac = req->nh.mac;
-	if (!rte_is_zero_ether_addr(&nh->mac)) {
-		nh->flags = GR_NH_F_STATIC;
-		nh->state = GR_NH_S_REACHABLE;
-	}
 
 	ops = nexthop_af_ops_get(req->nh.af);
 	assert(ops != NULL);

modules/infra/control/gr_nh_control.h
@@ -54,7 +54,7 @@ nexthop_lookup(addr_family_t af, uint16_t vrf_id, uint16_t iface_id, const void 
 bool nexthop_equal(const struct nexthop *, const struct nexthop *);
 
 // Allocate a new nexthop from the global pool with the provided initial values.
-struct nexthop *nexthop_new(addr_family_t af, uint16_t vrf_id, uint16_t iface_id, const void *addr);
+struct nexthop *nexthop_new(const struct gr_nexthop *);
 
 // Clean all next hop related to an interface.
 void nexthop_cleanup(uint16_t iface_id);

modules/infra/control/nexthop.c
@@ -130,11 +130,27 @@ const struct nexthop_type_ops *nexthop_type_ops_get(gr_nh_type_t type) {
 	return type_ops[type];
 }
 
-struct nexthop *
-nexthop_new(addr_family_t af, uint16_t vrf_id, uint16_t iface_id, const void *addr) {
+struct nexthop *nexthop_new(const struct gr_nexthop *base) {
 	struct nexthop *nh;
 	void *data;
 	int ret;
+
+	switch (base->type) {
+	case GR_NH_T_L3:
+	case GR_NH_T_SR6_OUTPUT:
+	case GR_NH_T_SR6_LOCAL:
+	case GR_NH_T_DNAT:
+		break;
+	default:
+		ABORT("invalid nexthop type %hhu", base->type);
+	}
+	switch (base->af) {
+	case GR_AF_IP4:
+	case GR_AF_IP6:
+		break;
+	default:
+		ABORT("invalid nexthop family %hhu", base->af);
+	}
 
 	if (rte_lcore_has_role(rte_lcore_id(), ROLE_NON_EAL))
 		ABORT("nexthop created from datapath thread");
@@ -143,19 +159,7 @@ nexthop_new(addr_family_t af, uint16_t vrf_id, uint16_t iface_id, const void *ad
 		return errno_set_null(-ret);
 
 	nh = data;
-	nh->vrf_id = vrf_id;
-	nh->iface_id = iface_id;
-	nh->af = af;
-	switch (af) {
-	case GR_AF_IP4:
-		nh->ipv4 = *(ip4_addr_t *)addr;
-		break;
-	case GR_AF_IP6:
-		nh->ipv6 = *(struct rte_ipv6_addr *)addr;
-		break;
-	default:
-		ABORT("invalid nexthop family %hhu", af);
-	}
+	nh->base = *base;
 
 	gr_event_push(GR_EVENT_NEXTHOP_NEW, nh);
 

modules/ip/api/dnat44.c
@@ -28,13 +28,18 @@ static struct api_out dnat44_add(const void *request, void ** /*response*/) {
 		return api_out(EEXIST, 0);
 	}
 
-	nh = nh4_new(iface->vrf_id, iface->id, req->rule.match);
+	nh = nexthop_new(&(struct gr_nexthop) {
+		.type = GR_NH_T_DNAT,
+		.af = GR_AF_IP4,
+		.flags = GR_NH_F_LOCAL | GR_NH_F_STATIC,
+		.state = GR_NH_S_REACHABLE,
+		.vrf_id = iface->vrf_id,
+		.iface_id = iface->id,
+		.ipv4 = req->rule.match,
+	});
 	if (nh == NULL)
 		return api_out(ENOMEM, 0);
 
-	nh->flags = GR_NH_F_LOCAL | GR_NH_F_STATIC;
-	nh->state = GR_NH_S_REACHABLE;
-	nh->type = GR_NH_T_DNAT;
 	data = dnat44_nh_data(nh);
 	data->replace = req->rule.replace;
 	ret = rib4_insert(iface->vrf_id, req->rule.match, 32, GR_RT_ORIGIN_INTERNAL, nh);

modules/ip/control/address.c
@@ -80,18 +80,21 @@ static struct api_out addr_add(const void *request, void ** /*response*/) {
 	if (nh4_lookup(iface->vrf_id, req->addr.addr.ip) != NULL)
 		return api_out(EADDRINUSE, 0);
 
-	if ((nh = nh4_new(iface->vrf_id, iface->id, req->addr.addr.ip)) == NULL)
+	struct gr_nexthop base = {
+		.type = GR_NH_T_L3,
+		.af = GR_AF_IP4,
+		.flags = GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_STATIC,
+		.state = GR_NH_S_REACHABLE,
+		.vrf_id = iface->vrf_id,
+		.iface_id = iface->id,
+		.ipv4 = req->addr.addr.ip,
+		.prefixlen = req->addr.addr.prefixlen,
+	};
+	if (iface_get_eth_addr(iface->id, &base.mac) < 0 && errno != EOPNOTSUPP)
 		return api_out(errno, 0);
 
-	nh->prefixlen = req->addr.addr.prefixlen;
-	nh->flags = GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_STATIC;
-	nh->state = GR_NH_S_REACHABLE;
-
-	if (iface_get_eth_addr(iface->id, &nh->mac) < 0)
-		if (errno != EOPNOTSUPP) {
-			nexthop_decref(nh);
-			return api_out(errno, 0);
-		}
+	if ((nh = nexthop_new(&base)) == NULL)
+		return api_out(errno, 0);
 
 	if ((ret = rib4_insert(iface->vrf_id, nh->ipv4, nh->prefixlen, GR_RT_ORIGIN_LINK, nh)) < 0)
 		return api_out(-ret, 0);

commit d4935df611046a42934ecf61c3443af0bc061d20

srv6: fix nexthop family for ipv4 route
Set nexthop family to AF_INET4.

Fixes: 3f7be3ff32cd ("nexthop: change nexthop_new to accept a full object")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/srv6/control/route.c
@@ -51,7 +51,6 @@ static struct api_out srv6_route_add(const void *request, void ** /*response*/) 
 	const struct gr_srv6_route_add_req *req = request;
 	struct gr_nexthop base = {
 		.type = GR_NH_T_SR6_OUTPUT,
-		.af = GR_AF_IP6,
 		.state = GR_NH_S_REACHABLE,
 		.flags = GR_NH_F_GATEWAY | GR_NH_F_STATIC,
 		.vrf_id = req->r.key.vrf_id,
@@ -75,6 +74,7 @@ static struct api_out srv6_route_add(const void *request, void ** /*response*/) 
 
 		base.ipv6 = req->r.key.dest6.ip;
 		base.prefixlen = req->r.key.dest6.prefixlen;
+		base.af = GR_AF_IP6;
 	} else {
 		nh = rib4_lookup_exact(
 			req->r.key.vrf_id, req->r.key.dest4.ip, req->r.key.dest4.prefixlen
@@ -84,6 +84,7 @@ static struct api_out srv6_route_add(const void *request, void ** /*response*/) 
 
 		base.ipv4 = req->r.key.dest4.ip;
 		base.prefixlen = req->r.key.dest4.prefixlen;
+		base.af = GR_AF_IP4;
 	}
 
 	nh = nexthop_new(&base);