6c0b81f2482d462255b266462daa0c03ea0c051b
Maxime Leroy <maxime@leroys.fr>
2025-07-17 16:20:06 +0200

    smoke: add explicit route in vrf_forward_test
    Add some explicit route to *vrf_forward_test*, matching what
    ip_forward_test.sh already does.
    
    The next commit will clone this test for FRR: it will create the route
    from vtysh and verify that it is correctly synchronised to Grout.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>

--- smoke/vrf_forward_test.sh
+++ smoke/vrf_forward_test.sh
@@ -15,8 +15,12 @@ grcli add interface port $p2 devargs net_tap2,iface=$p2 vrf 2 mac f0:0d:ac:dc:02
 grcli add interface port $p3 devargs net_tap3,iface=$p3 vrf 2 mac f0:0d:ac:dc:02:01
 grcli add ip address 172.16.0.1/24 iface $p0
 grcli add ip address 172.16.1.1/24 iface $p1
+grcli add ip route 16.0.0.0/16 via 172.16.0.2 vrf 1
+grcli add ip route 16.1.0.0/16 via 172.16.1.2 vrf 1
 grcli add ip address 172.16.0.1/24 iface $p2
 grcli add ip address 172.16.1.1/24 iface $p3
+grcli add ip route 16.0.0.0/16 via 172.16.0.2 vrf 2
+grcli add ip route 16.1.0.0/16 via 172.16.1.2 vrf 2
 
 for n in 0 1; do
 	p=$run_id$n
@@ -25,10 +29,12 @@ for n in 0 1; do
 	ip -n $p link set $p address ba:d0:ca:ca:01:0$n
 	ip -n $p link set $p up
 	ip -n $p addr add 172.16.$((n % 2)).2/24 dev $p
+	ip -n $p addr add 16.$((n % 2)).0.1/16 dev lo
 	ip -n $p route add default via 172.16.$((n % 2)).1
 	ip -n $p addr show
 done
 ip netns exec $p0 ping -i0.01 -c3 -n 172.16.1.2
+ip netns exec $p0 ping -i0.01 -c3 -n 16.1.0.1
 
 for n in 2 3; do
 	p=$run_id$n
@@ -37,7 +43,9 @@ for n in 2 3; do
 	ip -n $p link set $p address ba:d0:ca:ca:02:0$n
 	ip -n $p link set $p up
 	ip -n $p addr add 172.16.$((n % 2)).2/24 dev $p
+	ip -n $p addr add 16.$((n % 2)).0.1/16 dev lo
 	ip -n $p route add default via 172.16.$((n % 2)).1
 	ip -n $p addr show
 done
 ip netns exec $p2 ping -i0.01 -c3 -n 172.16.1.2
+ip netns exec $p2 ping -i0.01 -c3 -n 16.1.0.1
