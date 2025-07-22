ad1b18df1101794e180134e0bb9c4abd219855c8
Christophe Fontaine <cfontain@redhat.com>
2025-07-22 14:28:08 +0000

    ip6: convert ndp_probe callback to a node
    The Neighbor Discovery Protocol (NDP) probe logic is moved from a
    callback-based control plane handler (ndp_probe_input_cb) to a dedicated
    DPDK graph node (ndp_probe).
    The obsolete callback and related handler registration code are removed
    from both headers and implementation files.
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/ip6/control/gr_ip6_control.h
+++ modules/ip6/control/gr_ip6_control.h
@@ -20,9 +20,6 @@ nh6_lookup(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *ip) {
 	return nexthop_lookup(GR_AF_IP6, vrf_id, iface_id, ip);
 }
 
-void ndp_probe_input_cb(struct rte_mbuf *m);
-void ndp_router_sollicit_input_cb(struct rte_mbuf *m);
-
 int rib6_insert(
 	uint16_t vrf_id,
 	uint16_t iface_id,
--- modules/ip6/control/nexthop.c
+++ modules/ip6/control/nexthop.c
@@ -3,8 +3,6 @@
 
 #include <gr_api.h>
 #include <gr_clock.h>
-#include <gr_control_input.h>
-#include <gr_control_output.h>
 #include <gr_icmp6.h>
 #include <gr_iface.h>
 #include <gr_ip6.h>
@@ -26,7 +24,6 @@
 #include <string.h>
 #include <sys/queue.h>
 
-static control_input_t ip6_output_node;
 enum {
 	NH6_SINK = 0,
 	NH6_IP6_OUTPUT,
@@ -166,124 +163,179 @@ GR_DROP_REGISTER(nh6_nh_alloc);
 GR_DROP_REGISTER(nh6_route_insert);
 GR_DROP_REGISTER(nh6_queue_full);
 
-static control_input_t ndp_na_output_node;
+enum {
+	SINK = 0,
+	NDP_NA_OUTPUT,
+	IP6_OUTPUT,
+	ERR_ICMP6_OPT_INVAL,
+	ERR_ICMP6_TYPE_INVAL,
+	ERR_NDP_NH_ALLOC,
+	ERR_NDP_ROUTE_INSERT,
+	NDP_EDGE_COUNT,
+};
 
-void ndp_probe_input_cb(struct rte_mbuf *m) {
-	const struct icmp6 *icmp6 = rte_pktmbuf_mtod(m, const struct icmp6 *);
+static uint16_t ndp_probe_input_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
 	const struct rte_ipv6_addr *remote, *local;
 	const struct ip6_local_mbuf_data *d;
 	const struct icmp6_neigh_solicit *ns;
 	const struct icmp6_neigh_advert *na;
 	icmp6_opt_found_t lladdr_found;
+	const struct icmp6 *icmp6;
 	const struct iface *iface;
 	struct rte_ether_addr mac;
-	struct nexthop *nh = NULL;
+	struct nexthop *nh;
+	rte_edge_t edge;
 
-	d = (const struct ip6_local_mbuf_data *)control_output_mbuf_data(m)->cb_data;
-	iface = control_output_mbuf_data(m)->iface;
+	struct rte_mbuf *m;
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		edge = SINK;
+		nh = NULL;
 
-	switch (icmp6->type) {
-	case ICMP6_TYPE_NEIGH_SOLICIT:
-		ns = PAYLOAD(icmp6);
-		local = &ns->target;
-		remote = &d->src;
-		lladdr_found = icmp6_get_opt(
-			m, sizeof(*icmp6) + sizeof(*ns), ICMP6_OPT_SRC_LLADDR, &mac
-		);
-		break;
-	case ICMP6_TYPE_NEIGH_ADVERT:
-		na = PAYLOAD(icmp6);
-		local = NULL;
-		remote = &na->target;
-		lladdr_found = icmp6_get_opt(
-			m, sizeof(*icmp6) + sizeof(*na), ICMP6_OPT_TARGET_LLADDR, &mac
-		);
-		break;
-	default:
-		goto free;
-	}
+		if (gr_mbuf_is_traced(m))
+			gr_mbuf_trace_add(m, node, 0);
 
-	if (lladdr_found == ICMP6_OPT_INVAL)
-		goto free;
+		icmp6 = rte_pktmbuf_mtod(m, const struct icmp6 *);
+		d = ip6_local_mbuf_data(m);
+		iface = d->iface;
 
-	if (!rte_ipv6_addr_is_unspec(remote) && !rte_ipv6_addr_is_mcast(remote)) {
-		nh = nh6_lookup(iface->vrf_id, iface->id, remote);
-		if (nh == NULL) {
-			// We don't have an entry for the probe sender address yet.
-			//
-			// Create one now. If the sender has requested our mac address, they
-			// will certainly contact us soon and it will save us an NDP solicitation.
-			nh = nexthop_new(&(struct gr_nexthop) {
-				.type = GR_NH_T_L3,
-				.af = GR_AF_IP6,
-				.vrf_id = iface->vrf_id,
-				.iface_id = iface->id,
-				.ipv6 = *remote,
-				.origin = GR_NH_ORIGIN_INTERNAL,
-			});
-			if (nh == NULL) {
-				LOG(ERR, "ip6_nexthop_new: %s", strerror(errno));
-				goto free;
-			}
-
-			// Add an internal /128 route to reference the newly created nexthop.
-			int ret = rib6_insert(
-				iface->vrf_id,
-				iface->id,
-				remote,
-				RTE_IPV6_MAX_DEPTH,
-				GR_NH_ORIGIN_INTERNAL,
-				nh
+		switch (icmp6->type) {
+		case ICMP6_TYPE_NEIGH_SOLICIT:
+			ns = PAYLOAD(icmp6);
+			local = &ns->target;
+			remote = &d->src;
+			lladdr_found = icmp6_get_opt(
+				m, sizeof(*icmp6) + sizeof(*ns), ICMP6_OPT_SRC_LLADDR, &mac
 			);
-			if (ret < 0) {
-				LOG(ERR, "ip6_route_insert: %s", strerror(errno));
-				goto free;
+			break;
+		case ICMP6_TYPE_NEIGH_ADVERT:
+			na = PAYLOAD(icmp6);
+			local = NULL;
+			remote = &na->target;
+			lladdr_found = icmp6_get_opt(
+				m, sizeof(*icmp6) + sizeof(*na), ICMP6_OPT_TARGET_LLADDR, &mac
+			);
+			break;
+		default:
+			edge = ERR_ICMP6_TYPE_INVAL;
+			goto next;
+		}
+
+		if (lladdr_found == ICMP6_OPT_INVAL) {
+			LOG(DEBUG, "invalid ICMP6 option %d", icmp6->type);
+			edge = ERR_ICMP6_OPT_INVAL;
+			goto next;
+		};
+
+		if (!rte_ipv6_addr_is_unspec(remote) && !rte_ipv6_addr_is_mcast(remote)) {
+			nh = nh6_lookup(iface->vrf_id, iface->id, remote);
+			if (nh == NULL) {
+				// We don't have an entry for the probe sender address yet.
+				//
+				// Create one now. If the sender has requested our mac address, they
+				// will certainly contact us soon and it will save us an NDP solicitation.
+				nh = nexthop_new(&(struct gr_nexthop) {
+					.type = GR_NH_T_L3,
+					.af = GR_AF_IP6,
+					.vrf_id = iface->vrf_id,
+					.iface_id = iface->id,
+					.ipv6 = *remote,
+					.origin = GR_NH_ORIGIN_INTERNAL,
+				});
+				if (nh == NULL) {
+					LOG(ERR, "ip6_nexthop_new: %s", strerror(errno));
+					edge = ERR_NDP_NH_ALLOC;
+					goto next;
+				}
+
+				// Add an internal /128 route to reference the newly created nexthop.
+				int ret = rib6_insert(
+					iface->vrf_id,
+					iface->id,
+					remote,
+					RTE_IPV6_MAX_DEPTH,
+					GR_NH_ORIGIN_INTERNAL,
+					nh
+				);
+				if (ret < 0) {
+					LOG(ERR, "ip6_route_insert: %s", strerror(errno));
+					edge = ERR_NDP_ROUTE_INSERT;
+					goto next;
+				}
 			}
 		}
-	}
 
-	if (nh && !(nh->flags & GR_NH_F_STATIC) && lladdr_found == ICMP6_OPT_FOUND) {
-		// Refresh all fields.
-		nh->last_reply = gr_clock_us();
-		nh->state = GR_NH_S_REACHABLE;
-		nh->ucast_probes = 0;
-		nh->bcast_probes = 0;
-		nh->mac = mac;
-	}
-
-	if (icmp6->type == ICMP6_TYPE_NEIGH_SOLICIT && local != NULL) {
-		// send a reply for our local ip
-		struct ndp_na_output_mbuf_data *d = ndp_na_output_mbuf_data(m);
-		d->local = nh6_lookup(iface->vrf_id, iface->id, local);
-		d->remote = nh;
-		d->iface = iface;
-		if (post_to_stack(ndp_na_output_node, m) < 0) {
-			LOG(ERR, "post_to_stack: %s", strerror(errno));
-			goto free;
+		if (nh && !(nh->flags & GR_NH_F_STATIC) && lladdr_found == ICMP6_OPT_FOUND) {
+			// Refresh all fields.
+			nh->last_reply = gr_clock_us();
+			nh->state = GR_NH_S_REACHABLE;
+			nh->ucast_probes = 0;
+			nh->bcast_probes = 0;
+			nh->mac = mac;
 		}
-		// prevent double free, mbuf has been re-consumed by datapath
-		m = NULL;
-	}
 
-	// Flush all held packets.
-	struct rte_mbuf *held = nh->held_pkts_head;
-	while (held != NULL) {
-		struct ip6_output_mbuf_data *o;
-		struct rte_mbuf *next;
+		if (icmp6->type == ICMP6_TYPE_NEIGH_SOLICIT && local != NULL) {
+			// send a reply for our local ip
+			struct ndp_na_output_mbuf_data *d = ndp_na_output_mbuf_data(m);
+			d->local = nh6_lookup(iface->vrf_id, iface->id, local);
+			d->remote = nh;
+			d->iface = iface;
+			edge = NDP_NA_OUTPUT;
+		}
 
-		next = queue_mbuf_data(held)->next;
-		o = ip6_output_mbuf_data(held);
-		o->nh = nh;
-		o->iface = NULL;
-		post_to_stack(ip6_output_node, held);
-		held = next;
+		// Flush all held packets.
+		struct rte_mbuf *held = nh->held_pkts_head;
+		while (held != NULL) {
+			struct ip6_output_mbuf_data *o;
+			struct rte_mbuf *next;
+
+			next = queue_mbuf_data(held)->next;
+			o = ip6_output_mbuf_data(held);
+			o->nh = nh;
+			o->iface = NULL;
+			rte_node_enqueue_x1(graph, node, IP6_OUTPUT, held);
+			held = next;
+		}
+		nh->held_pkts_head = NULL;
+		nh->held_pkts_tail = NULL;
+		nh->held_pkts = 0;
+next:
+		rte_node_enqueue_x1(graph, node, edge, m);
+		continue;
 	}
-	nh->held_pkts_head = NULL;
-	nh->held_pkts_tail = NULL;
-	nh->held_pkts = 0;
-free:
-	rte_pktmbuf_free(m);
+	return nb_objs;
 }
+
+static struct rte_node_register ndp_probe_node = {
+	.flags = GR_NODE_FLAG_CONTROL_PLANE,
+	.name = "ndp_probe",
+	.process = ndp_probe_input_process,
+	.nb_edges = NDP_EDGE_COUNT,
+	.next_nodes = {
+		[SINK] = "ctlplane_sink",
+		[IP6_OUTPUT] = "ip6_output",
+		[NDP_NA_OUTPUT] = "ndp_na_output",
+		[ERR_ICMP6_OPT_INVAL] = "ndp_icmp6_opt_inval",
+		[ERR_ICMP6_TYPE_INVAL] = "ndp_icmp6_type_inval",
+		[ERR_NDP_ROUTE_INSERT] = "ndp_route_insert",
+		[ERR_NDP_NH_ALLOC] = "ndp_nh_alloc",
+	},
+};
+
+static struct gr_node_info ndp_probe_info = {
+	.node = &ndp_probe_node,
+};
+
+GR_NODE_REGISTER(ndp_probe_info);
+GR_DROP_REGISTER(ndp_icmp6_opt_inval);
+GR_DROP_REGISTER(ndp_icmp6_type_inval);
+GR_DROP_REGISTER(ndp_route_insert);
+GR_DROP_REGISTER(ndp_nh_alloc);
 
 static int nh6_add(struct nexthop *nh) {
 	return rib6_insert(nh->vrf_id, nh->iface_id, &nh->ipv6, 128, GR_NH_ORIGIN_INTERNAL, nh);
@@ -297,15 +349,9 @@ static void nh6_del(struct nexthop *nh) {
 	}
 }
 
-static void nh6_init(struct event_base *) {
-	ip6_output_node = gr_control_input_register_handler("ip6_output", true);
-	ndp_na_output_node = gr_control_input_register_handler("ndp_na_output", true);
-}
-
 static struct gr_module nh6_module = {
 	.name = "ipv6 nexthop",
 	.depends_on = "graph",
-	.init = nh6_init,
 };
 
 static struct nexthop_af_ops nh_ops = {
