11f77931f65994d68259e78cc41fea3b4a1adf75
Christophe Fontaine <cfontain@redhat.com>
2025-08-04 16:04:11 +0000

    frr: use tunsrc api
    Now that grout exposes a tunsrc api, use it in FRR.
    
    vtysh# show running-config
    segment-routing
     srv6
      encapsulation
       source-address 3fff::1
     traffic-eng
     exit
    exit
    !
    
    grout# show sr tunsrc
    sr tunsrc addr 3fff::1
    
    Suggested-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

--- frr/if_grout.h
+++ frr/if_grout.h
@@ -10,6 +10,7 @@
 #include <zebra/zebra_dplane.h>
 
 enum zebra_dplane_result grout_add_del_address(struct zebra_dplane_ctx *ctx);
+enum zebra_dplane_result grout_set_sr_tunsrc(struct zebra_dplane_ctx *ctx);
 
 void grout_interface_addr_dplane(struct gr_nexthop *gr_nh, bool new);
 void grout_link_change(struct gr_iface *gr_if, bool new, bool startup);
