DPDK/grout

commit ebdb8babc741d295d5b3b1189ca79abe8ba2ab71

infra: avoid event API for VRF create/delete
In FRR, a VRF must be created before any interface is attached to it.

Grout uses POST_ADD events to create VRFs when interfaces reference
them. This means the VRF is only created *after* the interface is
processed, violating FRR’s requirement.

Unlike the kernel, which uses an explicit API to create VRFs, Grout
created them lazily. This patch moves VRF creation/deletion directly into
iface_create/delete to ensure correct ordering.

Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/infra/control/iface.c
@@ -17,6 +17,7 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/queue.h>
+#include <vrf_priv.h>
 #include <wchar.h>
 
 static STAILQ_HEAD(, iface_type) types = STAILQ_HEAD_INITIALIZER(types);
@@ -73,6 +74,7 @@ static int next_ifid(uint16_t *ifid) {
 struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 	struct iface_type *type = iface_type_get(conf->type);
 	struct iface *iface = NULL;
+	bool vrf_ref = false;
 	uint16_t ifid;
 
 	if (type == NULL)
@@ -91,6 +93,11 @@ struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 	if (iface == NULL) {
 		errno = ENOMEM;
 		goto fail;
+	}
+	if (conf->type != GR_IFACE_TYPE_LOOPBACK) {
+		if (vrf_incref(conf->vrf_id) < 0)
+			goto fail;
+		vrf_ref = true;
 	}
 	if (conf->type == GR_IFACE_TYPE_LOOPBACK && conf->vrf_id) {
 		ifid = conf->vrf_id;
@@ -117,8 +124,12 @@ struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 
 	return iface;
 fail:
-	if (iface != NULL)
+	if (iface != NULL) {
+		if (vrf_ref)
+			vrf_decref(iface->vrf_id);
+
 		free(iface->name);
+	}
 	rte_free(iface);
 	return NULL;
 }
@@ -131,6 +142,8 @@ int iface_reconfig(
 ) {
 	struct iface_type *type;
 	struct iface *iface;
+	uint16_t old_vrf_id;
+	int ret;
 
 	if (set_attrs == 0)
 		return errno_set(EINVAL);
@@ -154,7 +167,23 @@ int iface_reconfig(
 
 	type = iface_type_get(iface->type);
 	assert(type != NULL);
-	return type->reconfig(iface, set_attrs, conf, api_info);
+
+	if (set_attrs & GR_IFACE_SET_VRF) {
+		old_vrf_id = iface->vrf_id;
+		ret = vrf_incref(conf->vrf_id);
+		if (ret < 0)
+			return ret;
+	}
+
+	ret = type->reconfig(iface, set_attrs, conf, api_info);
+	if (set_attrs & GR_IFACE_SET_VRF) {
+		if (ret == 0)
+			vrf_decref(old_vrf_id);
+		else
+			vrf_decref(conf->vrf_id);
+	}
+
+	return ret;
 }
 
 uint16_t ifaces_count(gr_iface_type_t type_id) {
@@ -275,6 +304,8 @@ int iface_destroy(uint16_t ifid) {
 		gr_event_push(GR_EVENT_IFACE_STATUS_DOWN, iface);
 	}
 	gr_event_push(GR_EVENT_IFACE_PRE_REMOVE, iface);
+	if (iface->type != GR_IFACE_TYPE_LOOPBACK)
+		vrf_decref(iface->vrf_id);
 	nexthop_cleanup(ifid);
 
 	ifaces[ifid] = NULL;

modules/infra/control/vrf.c
@@ -9,6 +9,8 @@
 #include <gr_string.h>
 #include <gr_vec.h>
 
+#include <vrf_priv.h>
+
 struct vrf_info {
 	int ref_count;
 	struct iface *iface;
@@ -21,59 +23,36 @@ struct iface *get_vrf_iface(uint16_t vrf_id) {
 	return vrfs[vrf_id].iface;
 }
 
-static void iface_event_vrf(uint32_t event, const void *obj) {
-	const struct iface *iface = obj;
-	int ifaces_per_vrf[ARRAY_DIM(vrfs)] = {0};
+int vrf_incref(uint16_t vrf_id) {
+	if (vrf_id >= MAX_VRFS)
+		return errno_set(EINVAL);
 
-	if (iface->type == GR_IFACE_TYPE_LOOPBACK)
-		return;
-
-	switch (event) {
-	case GR_EVENT_IFACE_POST_ADD:
-		if (++vrfs[iface->vrf_id].ref_count == 1)
-			vrfs[iface->vrf_id].iface = iface_loopback_create(iface->vrf_id);
-		break;
-	case GR_EVENT_IFACE_PRE_REMOVE:
-		if (--vrfs[iface->vrf_id].ref_count == 0) {
-			iface_loopback_delete(iface->vrf_id);
-			vrfs[iface->vrf_id].iface = NULL;
+	if (vrfs[vrf_id].ref_count == 0) {
+		vrfs[vrf_id].iface = iface_loopback_create(vrf_id);
+		if (vrfs[vrf_id].iface == NULL) {
+			return errno_set(errno);
 		}
-		break;
-	case GR_EVENT_IFACE_POST_RECONFIG:
-		iface = NULL;
-		while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
-			if (iface->type == GR_IFACE_TYPE_LOOPBACK)
-				continue;
-			if (iface->mode == GR_IFACE_MODE_L3)
-				ifaces_per_vrf[iface->vrf_id]++;
-		}
-		for (unsigned i = 0; i < ARRAY_DIM(vrfs); i++) {
-			if (vrfs[i].ref_count > ifaces_per_vrf[i]) {
-				vrfs[i].ref_count = ifaces_per_vrf[i];
-				if (vrfs[i].ref_count == 0) {
-					iface_loopback_delete(i);
-					vrfs[i].iface = NULL;
-				}
-			} else if (vrfs[i].ref_count < ifaces_per_vrf[i]) {
-				vrfs[i].ref_count = ifaces_per_vrf[i];
-				if (vrfs[i].ref_count == 1)
-					vrfs[i].iface = iface_loopback_create(i);
-			}
-		}
-		break;
 	}
+	vrfs[vrf_id].ref_count++;
+
+	return 0;
 }
 
-static struct gr_event_subscription iface_event_vrf_sub = {
-	.callback = iface_event_vrf,
-	.ev_count = 3,
-	.ev_types = {
-		GR_EVENT_IFACE_POST_ADD,
-		GR_EVENT_IFACE_PRE_REMOVE,
-		GR_EVENT_IFACE_POST_RECONFIG,
-	},
-};
+void vrf_decref(uint16_t vrf_id) {
+	if (vrf_id >= MAX_VRFS)
+		return;
 
-RTE_INIT(vrf_constructor) {
-	gr_event_subscribe(&iface_event_vrf_sub);
+	if (vrfs[vrf_id].ref_count == 1) {
+		int ret;
+
+		ret = iface_loopback_delete(vrf_id);
+		if (ret < 0)
+			ABORT("loopback for vrf %u cannot be deleted %s", vrf_id, strerror(errno));
+
+		assert(ret == 0);
+		vrfs[vrf_id].iface = NULL;
+	}
+
+	vrfs[vrf_id].ref_count--;
+	return;
 }

modules/infra/control/vrf_priv.h
@@ -1,0 +1,7 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Maxime Leroy, Free Mobile
+
+#pragma once
+
+int vrf_incref(uint16_t vrfid);
+void vrf_decref(uint16_t vrfid);

commit 76da8e955dc8904e181fff56998b4872ffcf5f19

infra: fix error when loopback tun interface cannot be created
When running grout as !root or when running it in a network namespace,
the creation of the loopback tun network device may fail with obscure
errors:

  ERR: GROUT: iface_loopback_init: ioctl Operation not permitted

or:

  ERR: GROUT: iface_loopback_init: ioctl Device or resource busy

This should not prevent the creation of interfaces. It will only hinder
the linux communication (and maybe some cross VRF functionality). In
such cases, an error in the log will be enough to identify the issue.

Change vrf_incref to not return any error. Replace the aggressive ABORT
in vrf_decref with a WARNING log.

Fixes: ebdb8babc741 ("infra: avoid event API for VRF create/delete")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/iface.c
@@ -95,8 +95,7 @@ struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 		goto fail;
 	}
 	if (conf->type != GR_IFACE_TYPE_LOOPBACK) {
-		if (vrf_incref(conf->vrf_id) < 0)
-			goto fail;
+		vrf_incref(conf->vrf_id);
 		vrf_ref = true;
 	}
 	if (conf->type == GR_IFACE_TYPE_LOOPBACK && conf->vrf_id) {
@@ -170,9 +169,7 @@ int iface_reconfig(
 
 	if (set_attrs & GR_IFACE_SET_VRF) {
 		old_vrf_id = iface->vrf_id;
-		ret = vrf_incref(conf->vrf_id);
-		if (ret < 0)
-			return ret;
+		vrf_incref(conf->vrf_id);
 	}
 
 	ret = type->reconfig(iface, set_attrs, conf, api_info);

modules/infra/control/vrf.c
@@ -23,19 +23,22 @@ struct iface *get_vrf_iface(uint16_t vrf_id) {
 	return vrfs[vrf_id].iface;
 }
 
-int vrf_incref(uint16_t vrf_id) {
+void vrf_incref(uint16_t vrf_id) {
 	if (vrf_id >= MAX_VRFS)
-		return errno_set(EINVAL);
+		return;
 
 	if (vrfs[vrf_id].ref_count == 0) {
 		vrfs[vrf_id].iface = iface_loopback_create(vrf_id);
 		if (vrfs[vrf_id].iface == NULL) {
-			return errno_set(errno);
+			LOG(WARNING,
+			    "loopback for vrf %u cannot be created: %s",
+			    vrf_id,
+			    strerror(errno));
+			return;
 		}
 	}
-	vrfs[vrf_id].ref_count++;
 
-	return 0;
+	vrfs[vrf_id].ref_count++;
 }
 
 void vrf_decref(uint16_t vrf_id) {
@@ -43,16 +46,15 @@ void vrf_decref(uint16_t vrf_id) {
 		return;
 
 	if (vrfs[vrf_id].ref_count == 1) {
-		int ret;
-
-		ret = iface_loopback_delete(vrf_id);
-		if (ret < 0)
-			ABORT("loopback for vrf %u cannot be deleted %s", vrf_id, strerror(errno));
-
-		assert(ret == 0);
+		if (iface_loopback_delete(vrf_id) < 0) {
+			LOG(WARNING,
+			    "loopback for vrf %u cannot be deleted: %s",
+			    vrf_id,
+			    strerror(errno));
+			return;
+		}
 		vrfs[vrf_id].iface = NULL;
 	}
 
 	vrfs[vrf_id].ref_count--;
-	return;
 }

modules/infra/control/vrf_priv.h
@@ -3,5 +3,7 @@
 
 #pragma once
 
-int vrf_incref(uint16_t vrfid);
+#include <stdint.h>
+
+void vrf_incref(uint16_t vrfid);
 void vrf_decref(uint16_t vrfid);