DPDK/grout

commit c68f16410aa406a53f81b8105d30557ea0db49b2

mk: add convenience target to update graph.svg
The docs/graph.svg image is tested for consistency in smoke tests.
Regenerating this image is easy but requires multiple steps. Add a short
make target to automate these.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

GNUmakefile
@@ -24,6 +24,16 @@ unit-tests: $(BUILDDIR)/build.ninja
 smoke-tests: all
 	./smoke/run.sh $(BUILDDIR)
 
+.PHONY: update-graph
+update-graph: all
+	$Q set -xe; tmp=`mktemp -d`; \
+	trap "killall grout; wait; rm -rf $$tmp" EXIT; \
+	export GROUT_SOCK_PATH="$$tmp/sock"; \
+	$(BUILDDIR)/grout -t & \
+	socat FILE:/dev/null UNIX-CONNECT:$$GROUT_SOCK_PATH,retry=10 && \
+	$(BUILDDIR)/grcli add interface port p0 devargs net_null && \
+	$(BUILDDIR)/grcli show graph brief | dot -Tsvg > docs/graph.svg
+
 .PHONY: coverage
 coverage: test
 	$Q mkdir -p $(BUILDDIR)/coverage

commit 84119da4ce3982d4e0d5d339a566f017922d1f09

mk: fix leaking grout process in update-graph
The started process is "$(BUILDDIR)/grout", not "grout", hence killall
fails with the error:

	killall: grout: No such process

This leaks processes which causes obscure errors in the long run.

Use kill %1 which will properly identify the background job.

Fixes: c68f16410aa4 ("mk: add convenience target to update graph.svg")
Signed-off-by: Robin Jarry <robin@jarry.cc>

GNUmakefile
@@ -32,7 +32,7 @@ smoke-tests: all
 .PHONY: update-graph
 update-graph: all
 	$Q set -xe; tmp=`mktemp -d`; \
-	trap "killall grout; wait; rm -rf $$tmp" EXIT; \
+	trap "kill %1; wait; rm -rf $$tmp" EXIT; \
 	export GROUT_SOCK_PATH="$$tmp/sock"; \
 	$(BUILDDIR)/grout -t & \
 	socat FILE:/dev/null UNIX-CONNECT:$$GROUT_SOCK_PATH,retry=10 && \