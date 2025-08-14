DPDK/grout

commit 93647f952012b8b875d289c187724564abbdfa11

ip: add stateless dnat44 support
Allow configuring stateless "destination NAT" addresses. This is
a convoluted term to represent the following:

                          client
                |                        ^
                | destination            |  source changed
                |    8.8.8.8             |   to 8.8.8.8
                v                        |
            +----------------------------------+
            |       iface p0    8.8.8.1/24     |
            |                                  |
            |              grout               |
            |                                  |
            |       iface p1    10.16.0.1/24   |
            +----------------------------------+
                 |                       |
          destination changed            |
             to 10.16.0.99               |
                 |                       |
                 v                       |
                    service 10.16.0.99

This enables grout for exposing virtual/floating IP addresses on one
interface without actually owning these addresses. And internally
translating the packet addresses to forward packets to another host
located behind another interface.

DNAT44 is always bi-directional and is configured from the "external" or
"public" standpoint. Packets received on the "public" interface with
a specific IP address will have their destination address changed to an
internal IP. Packets transmitted from the public interface that have
a source IP address matching the internal IP will have their source IP
changed to the "public" IP.

Internally, this feature is implemented using a nexthop for the public
facing address (the matching address) since nexthops themselves are
required to support ARP and normal communication, we only store the
address to substitute on packets in a private field. The new
GR_NH_F_DNAT flag is added for this purpose to identify nexthops which
are used for DNAT.

modules/ip/datapath/ip_output.c
@@ -4,6 +4,7 @@
 #include <gr_control_output.h>
 #include <gr_datapath.h>
 #include <gr_eth.h>
+#include <gr_fib4.h>
 #include <gr_graph.h>
 #include <gr_iface.h>
 #include <gr_ip4.h>
@@ -86,6 +87,9 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		// By default, it will be eth_output unless another output node was registered.
 		edge = iface_type_edges[iface->type];
 		mbuf_data(mbuf)->iface = iface;
+
+		if (iface->flags & GR_IFACE_F_SNAT)
+			snat44_process(iface, ip);
 
 		if (edge != ETH_OUTPUT)
 			goto next;

modules/infra/api/gr_infra.h
@@ -29,6 +29,7 @@ typedef enum : uint16_t {
 	GR_IFACE_F_PROMISC = GR_BIT16(1),
 	GR_IFACE_F_ALLMULTI = GR_BIT16(2),
 	GR_IFACE_F_PACKET_TRACE = GR_BIT16(3),
+	GR_IFACE_F_SNAT = GR_BIT16(4),
 } gr_iface_flags_t;
 
 // Interface state flags

modules/infra/api/gr_nexthop.h
@@ -30,6 +30,7 @@ typedef enum : uint8_t {
 	GR_NH_T_L3 = 0, // Default value
 	GR_NH_T_SR6,
 	GR_NH_T_SR6_LOCAL,
+	GR_NH_T_DNAT,
 } gr_nh_type_t;
 
 //! Nexthop structure exposed to the API.
@@ -105,6 +106,8 @@ static inline const char *gr_nh_type_name(const struct gr_nexthop *nh) {
 		return "SRv6";
 	case GR_NH_T_SR6_LOCAL:
 		return "SRv6-local";
+	case GR_NH_T_DNAT:
+		return "DNAT";
 	}
 	return "?";
 }

modules/infra/cli/iface.c
@@ -282,6 +282,8 @@ static cmd_status_t iface_list(const struct gr_api_client *c, const struct ec_pn
 			SAFE_BUF(snprintf, sizeof(buf), " allmulti");
 		if (iface->flags & GR_IFACE_F_PACKET_TRACE)
 			SAFE_BUF(snprintf, sizeof(buf), " tracing");
+		if (iface->flags & GR_IFACE_F_SNAT)
+			SAFE_BUF(snprintf, sizeof(buf), " snat");
 		scols_line_set_data(line, 2, buf);
 
 		// mode

modules/infra/control/nexthop.c
@@ -115,6 +115,7 @@ void nexthop_type_ops_register(gr_nh_type_t type, const struct nexthop_type_ops 
 	case GR_NH_T_L3:
 	case GR_NH_T_SR6:
 	case GR_NH_T_SR6_LOCAL:
+	case GR_NH_T_DNAT:
 		if (ops == NULL || ops->free)
 			ABORT("invalid type ops");
 		if (type_ops[type] != NULL)

modules/ip/api/dnat44.c
@@ -1,0 +1,153 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#include <gr_api.h>
+#include <gr_ip4.h>
+#include <gr_ip4_datapath.h>
+#include <gr_module.h>
+#include <gr_vec.h>
+
+static struct api_out dnat44_add(const void *request, void ** /*response*/) {
+	const struct gr_dnat44_add_req *req = request;
+	struct dnat44_nh_data *data;
+	struct iface *iface;
+	struct nexthop *nh;
+	int ret;
+
+	iface = iface_from_id(req->rule.iface_id);
+	if (iface == NULL)
+		return api_out(ENODEV, 0);
+
+	nh = nh4_lookup(iface->vrf_id, req->rule.match);
+	if (nh != NULL) {
+		data = dnat44_nh_data(nh);
+		if (nh->type != GR_NH_T_DNAT || data->replace != req->rule.replace)
+			return api_out(EADDRINUSE, 0);
+		if (req->exist_ok)
+			return api_out(0, 0);
+		return api_out(EEXIST, 0);
+	}
+
+	nh = nh4_new(iface->vrf_id, iface->id, req->rule.match);
+	if (nh == NULL)
+		return api_out(ENOMEM, 0);
+
+	nh->flags = GR_NH_F_LOCAL | GR_NH_F_STATIC;
+	nh->state = GR_NH_S_REACHABLE;
+	nh->type = GR_NH_T_DNAT;
+	data = dnat44_nh_data(nh);
+	data->replace = req->rule.replace;
+	ret = rib4_insert(iface->vrf_id, req->rule.match, 32, GR_RT_ORIGIN_INTERNAL, nh);
+	if (ret < 0)
+		return api_out(-ret, 0);
+
+	ret = snat44_rule_add(iface, req->rule.replace, req->rule.match);
+	if (ret < 0)
+		goto fail;
+
+	return api_out(0, 0);
+fail:
+	rib4_delete(iface->vrf_id, req->rule.match, 32);
+	snat44_rule_del(iface, req->rule.replace);
+	return api_out(-ret, 0);
+}
+
+static struct api_out dnat44_del(const void *request, void ** /*response*/) {
+	const struct gr_dnat44_del_req *req = request;
+	struct dnat44_nh_data *data;
+	struct iface *iface;
+	struct nexthop *nh;
+
+	iface = iface_from_id(req->rule.iface_id);
+	if (iface == NULL)
+		return api_out(ENODEV, 0);
+
+	nh = nh4_lookup(iface->vrf_id, req->rule.match);
+	if (nh == NULL) {
+		if (req->missing_ok)
+			return api_out(0, 0);
+		return api_out(ENOENT, 0);
+	}
+	data = dnat44_nh_data(nh);
+	if (nh->type != GR_NH_T_DNAT || data->replace != req->rule.replace)
+		return api_out(EADDRINUSE, 0);
+
+	rib4_delete(iface->vrf_id, req->rule.match, 32);
+	snat44_rule_del(iface, req->rule.replace);
+
+	return api_out(0, 0);
+}
+
+struct dnat44_list_iterator {
+	uint16_t vrf_id;
+	struct gr_dnat44_rule *rules;
+};
+
+static void dnat44_list_iter(struct nexthop *nh, void *priv) {
+	struct dnat44_list_iterator *iter = priv;
+	struct dnat44_nh_data *data;
+
+	if (iter->vrf_id != GR_VRF_ID_ALL && nh->vrf_id != iter->vrf_id)
+		return;
+
+	if (nh->type != GR_NH_T_DNAT)
+		return;
+
+	data = dnat44_nh_data(nh);
+
+	struct gr_dnat44_rule rule = {
+		.iface_id = nh->iface_id,
+		.match = nh->ipv4,
+		.replace = data->replace,
+	};
+	gr_vec_add(iter->rules, rule);
+}
+
+static struct api_out dnat44_list(const void *request, void **response) {
+	const struct gr_dnat44_list_req *req = request;
+	struct gr_dnat44_list_resp *resp;
+	struct dnat44_list_iterator iter = {
+		.vrf_id = req->vrf_id,
+		.rules = NULL,
+	};
+	size_t len;
+
+	nexthop_iter(dnat44_list_iter, &iter);
+
+	len = sizeof(*resp) + gr_vec_len(iter.rules) * sizeof(struct gr_dnat44_rule);
+	resp = malloc(len);
+	if (resp == NULL) {
+		free(iter.rules);
+		return api_out(ENOMEM, 0);
+	}
+
+	resp->n_rules = gr_vec_len(iter.rules);
+	memcpy(resp->rules, iter.rules, gr_vec_len(iter.rules) * sizeof(struct gr_dnat44_rule));
+	gr_vec_free(iter.rules);
+
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static struct gr_api_handler add_handler = {
+	.name = "nat44 add",
+	.request_type = GR_DNAT44_ADD,
+	.callback = dnat44_add,
+};
+static struct gr_api_handler del_handler = {
+	.name = "nat44 del",
+	.request_type = GR_DNAT44_DEL,
+	.callback = dnat44_del,
+};
+static struct gr_api_handler list_handler = {
+	.name = "nat44 list",
+	.request_type = GR_DNAT44_LIST,
+	.callback = dnat44_list,
+};
+
+RTE_INIT(_init) {
+	gr_register_api_handler(&add_handler);
+	gr_register_api_handler(&del_handler);
+	gr_register_api_handler(&list_handler);
+}

modules/ip/api/gr_ip4.h
@@ -133,10 +133,49 @@ struct gr_ip4_icmp_recv_resp {
 	clock_t response_time;
 };
 
+// events //////////////////////////////////////////////////////////////////////
 typedef enum {
 	GR_EVENT_IP_ADDR_ADD = EVENT_TYPE(GR_IP4_MODULE, 0x0001),
 	GR_EVENT_IP_ADDR_DEL = EVENT_TYPE(GR_IP4_MODULE, 0x0002),
 	GR_EVENT_IP_ROUTE_ADD = EVENT_TYPE(GR_IP4_MODULE, 0x0003),
 	GR_EVENT_IP_ROUTE_DEL = EVENT_TYPE(GR_IP4_MODULE, 0x0004),
 } gr_event_ip_t;
+
+// dnat44 //////////////////////////////////////////////////////////////////////
+
+struct gr_dnat44_rule {
+	uint16_t iface_id;
+	ip4_addr_t match;
+	ip4_addr_t replace;
+};
+
+#define GR_DNAT44_ADD REQUEST_TYPE(GR_IP4_MODULE, 0x0031)
+
+struct gr_dnat44_add_req {
+	struct gr_dnat44_rule rule;
+	bool exist_ok;
+};
+
+// struct gr_dnat44_add_resp { };
+
+#define GR_DNAT44_DEL REQUEST_TYPE(GR_IP4_MODULE, 0x0032)
+
+struct gr_dnat44_del_req {
+	struct gr_dnat44_rule rule;
+	bool missing_ok;
+};
+
+// struct gr_dnat44_del_resp { };
+
+#define GR_DNAT44_LIST REQUEST_TYPE(GR_IP4_MODULE, 0x0033)
+
+struct gr_dnat44_list_req {
+	uint16_t vrf_id;
+};
+
+struct gr_dnat44_list_resp {
+	uint16_t n_rules;
+	struct gr_dnat44_rule rules[/* n_rules */];
+};
+
 #endif

commit 4f54ce098609582c6010a9142fa0ffd93fdb1c53

dnat: fix tcp/udp checksum after translation
When performing NAT on TCP/UDP packets, changing either source or
destination IP addresses require also updating the TCP/UDP checksum.

Fixes: 93647f952012 ("ip: add stateless dnat44 support")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/filter/control/snat44_static.c
@@ -8,7 +8,6 @@
 
 #include <rte_common.h>
 #include <rte_hash.h>
-#include <rte_ip4.h>
 
 static struct rte_hash *snat_hash;
 

modules/filter/datapath/dnat44_static.c
@@ -7,6 +7,9 @@
 #include <gr_module.h>
 #include <gr_nat_datapath.h>
 
+#include <rte_tcp.h>
+#include <rte_udp.h>
+
 enum edges {
 	FORWARD = 0,
 	LOCAL,
@@ -34,6 +37,29 @@ static uint16_t dnat44_static_process(
 		data = dnat44_nh_data(d->nh);
 		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
 		ip->hdr_checksum = fixup_checksum_32(ip->hdr_checksum, ip->dst_addr, data->replace);
+
+		switch (ip->next_proto_id) {
+		case IPPROTO_TCP: {
+			struct rte_tcp_hdr *tcp = rte_pktmbuf_mtod_offset(
+				mbuf, struct rte_tcp_hdr *, rte_ipv4_hdr_len(ip)
+			);
+			tcp->cksum = fixup_checksum_32(tcp->cksum, ip->dst_addr, data->replace);
+			break;
+		}
+		case IPPROTO_UDP: {
+			struct rte_udp_hdr *udp = rte_pktmbuf_mtod_offset(
+				mbuf, struct rte_udp_hdr *, rte_ipv4_hdr_len(ip)
+			);
+			if (udp->dgram_cksum != 0)
+				udp->dgram_cksum = fixup_checksum_32(
+					udp->dgram_cksum, ip->dst_addr, data->replace
+				);
+			break;
+		}
+		}
+
+		// Modify the address *after* updating the TCP/UDP checksum.
+		// We need the old address value to fixup the checksum properly.
 		ip->dst_addr = data->replace;
 
 		d->nh = fib4_lookup(d->iface->vrf_id, ip->dst_addr);

modules/filter/datapath/gr_nat_datapath.h
@@ -40,4 +40,4 @@ fixup_checksum_32(rte_be16_t old_cksum, ip4_addr_t old_addr, ip4_addr_t new_addr
 	return ~sum & 0xffff;
 }
 
-bool snat44_static_process(const struct iface *, struct rte_ipv4_hdr *);
+bool snat44_static_process(const struct iface *, struct rte_mbuf *);

modules/filter/datapath/snat44_static.c
@@ -4,13 +4,40 @@
 #include <gr_nat_control.h>
 #include <gr_nat_datapath.h>
 
-bool snat44_static_process(const struct iface *iface, struct rte_ipv4_hdr *ip) {
+#include <rte_tcp.h>
+#include <rte_udp.h>
+
+bool snat44_static_process(const struct iface *iface, struct rte_mbuf *mbuf) {
+	struct rte_ipv4_hdr *ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
 	ip4_addr_t replace;
 
 	if (!snat44_static_lookup_translation(iface->id, ip->src_addr, &replace))
 		return false;
 
 	ip->hdr_checksum = fixup_checksum_32(ip->hdr_checksum, ip->src_addr, replace);
+
+	switch (ip->next_proto_id) {
+	case IPPROTO_TCP: {
+		struct rte_tcp_hdr *tcp = rte_pktmbuf_mtod_offset(
+			mbuf, struct rte_tcp_hdr *, rte_ipv4_hdr_len(ip)
+		);
+		tcp->cksum = fixup_checksum_32(tcp->cksum, ip->dst_addr, replace);
+		break;
+	}
+	case IPPROTO_UDP: {
+		struct rte_udp_hdr *udp = rte_pktmbuf_mtod_offset(
+			mbuf, struct rte_udp_hdr *, rte_ipv4_hdr_len(ip)
+		);
+		if (udp->dgram_cksum != 0)
+			udp->dgram_cksum = fixup_checksum_32(
+				udp->dgram_cksum, ip->dst_addr, replace
+			);
+		break;
+	}
+	}
+
+	// Modify the address *after* updating the TCP/UDP checksum.
+	// We need the old address value to fixup the checksum properly.
 	ip->src_addr = replace;
 
 	return true;

modules/ip/datapath/ip_output.c
@@ -97,7 +97,7 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		mbuf_data(mbuf)->iface = iface;
 
 		if (iface->flags & GR_IFACE_F_SNAT_STATIC)
-			snat44_static_process(iface, ip);
+			snat44_static_process(iface, mbuf);
 
 		if (edge != ETH_OUTPUT)
 			goto next;