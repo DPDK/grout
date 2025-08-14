DPDK/grout

commit 2190714808ce1fbfc53733fb296b3aec1148429d

infra: add API and CLI command to obtain and clear iface stats
Display interface statistics (rx_packets, rx_bytes, tx_packets and tx_bytes)
per interface using 'show interface stats' grcli command.
With clear stats, reset the these interface statistics along with software and
hardware stats.

Sample output:
grout# show interface
<return>             Validate command.
type                 Show interface details.
stats                Show interface statistics.
name                 Show interface details.

grout# show interface stats
INTERFACE  RX_PACKETS  RX_BYTES  TX_PACKETS  TX_BYTES
p0         7           2757      2           100
gr-loop0   0           0         0           0

grout# clear stats
grout# show interface stats
INTERFACE  RX_PACKETS  RX_BYTES  TX_PACKETS  TX_BYTES
p0         0           0         0           0
gr-loop0   0           0         0           0

Signed-off-by: Spoorthi K <spk@redhat.com>

modules/infra/cli/iface.c
@@ -329,12 +329,75 @@ err:
 	return CMD_ERROR;
 }
 
+static cmd_status_t iface_stats(const struct gr_api_client *c, const struct ec_pnode * /*p*/) {
+	struct gr_infra_iface_stats_get_resp *resp = NULL;
+	struct libscols_table *table = NULL;
+	cmd_status_t status = CMD_ERROR;
+	void *resp_ptr = NULL;
+	int ret;
+
+	// Send the new API request and wait for the response
+	ret = gr_api_client_send_recv(c, GR_INFRA_IFACE_STATS_GET, 0, NULL, &resp_ptr);
+	if (ret < 0) {
+		errorf("failed to get interface stats: %s", strerror(-ret));
+		goto end;
+	}
+
+	resp = resp_ptr;
+
+	table = scols_new_table();
+	if (table == NULL) {
+		errorf("failed to create table: %s", strerror(errno));
+		goto end;
+	}
+
+	scols_table_new_column(table, "INTERFACE", 0, 0);
+	scols_table_new_column(table, "RX_PACKETS", SCOLS_FL_RIGHT, 0);
+	scols_table_new_column(table, "RX_BYTES", SCOLS_FL_RIGHT, 0);
+	scols_table_new_column(table, "TX_PACKETS", SCOLS_FL_RIGHT, 0);
+	scols_table_new_column(table, "TX_BYTES", SCOLS_FL_RIGHT, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (uint16_t i = 0; i < resp->n_stats; i++) {
+		struct libscols_line *line = scols_table_new_line(table, NULL);
+		struct gr_iface iface;
+		if (line == NULL) {
+			errorf("failed to create line: %s", strerror(errno));
+			goto end;
+		}
+
+		if (iface_from_id(c, resp->stats[i].iface_id, &iface) == 0)
+			scols_line_set_data(line, 0, iface.name);
+		else
+			scols_line_set_data(line, 0, "?");
+
+		scols_line_sprintf(line, 1, "%lu", resp->stats[i].rx_packets);
+		scols_line_sprintf(line, 2, "%lu", resp->stats[i].rx_bytes);
+		scols_line_sprintf(line, 3, "%lu", resp->stats[i].tx_packets);
+		scols_line_sprintf(line, 4, "%lu", resp->stats[i].tx_bytes);
+	}
+
+	scols_print_table(table);
+	status = CMD_SUCCESS;
+
+end:
+	if (table)
+		scols_unref_table(table);
+	free(resp_ptr);
+	return status;
+}
+
 static cmd_status_t iface_show(const struct gr_api_client *c, const struct ec_pnode *p) {
 	const struct cli_iface_type *type;
 	struct gr_iface iface;
 
-	if (arg_str(p, "NAME") == NULL || arg_str(p, "TYPE") != NULL)
+	if (arg_str(p, "stats") != NULL) {
+		return iface_stats(c, p);
+	}
+
+	if (arg_str(p, "NAME") == NULL || arg_str(p, "TYPE") != NULL) {
 		return iface_list(c, p);
+	}
 
 	if (iface_from_name(c, arg_str(p, "NAME"), &iface) < 0)
 		return CMD_ERROR;
@@ -387,7 +450,7 @@ static int ctx_init(struct ec_node *root) {
 		return ret;
 	ret = CLI_COMMAND(
 		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("interface", "Display interface details.")),
-		"[(name NAME)|(type TYPE)]",
+		"[(name NAME)|(type TYPE)|stats]",
 		iface_show,
 		"Show interface details.",
 		with_help(
@@ -397,7 +460,8 @@ static int ctx_init(struct ec_node *root) {
 		with_help(
 			"Show only this type of interface.",
 			ec_node_dyn("TYPE", complete_iface_types, NULL)
-		)
+		),
+		with_help("Show interface statistics.", ec_node_str("stats", "stats"))
 	);
 
 	return ret;

modules/infra/api/gr_infra.h
@@ -196,6 +196,23 @@ struct gr_infra_iface_set_req {
 
 // struct gr_infra_iface_set_resp { };
 
+#define GR_INFRA_IFACE_STATS_GET REQUEST_TYPE(GR_INFRA_MODULE, 0x0006)
+
+// struct gr_infra_iface_stats_get_req { };
+
+struct gr_iface_stats {
+	uint16_t iface_id;
+	uint64_t rx_packets;
+	uint64_t rx_bytes;
+	uint64_t tx_packets;
+	uint64_t tx_bytes;
+};
+
+struct gr_infra_iface_stats_get_resp {
+	uint16_t n_stats;
+	struct gr_iface_stats stats[/* n_stats */];
+};
+
 // port rxqs ///////////////////////////////////////////////////////////////////
 #define GR_INFRA_RXQ_LIST REQUEST_TYPE(GR_INFRA_MODULE, 0x0010)
 

modules/infra/api/stats.c
@@ -202,15 +202,72 @@ static struct api_out stats_reset(const void * /*request*/, void ** /*response*/
 	}
 
 	iface = NULL;
-	while ((iface = iface_next(GR_IFACE_TYPE_PORT, iface)) != NULL) {
-		struct iface_info_port *port = (struct iface_info_port *)iface->info;
-		if ((ret = rte_eth_stats_reset(port->port_id)) < 0)
-			return api_out(-ret, 0);
-		if ((ret = rte_eth_xstats_reset(port->port_id)) < 0)
-			return api_out(-ret, 0);
+
+	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
+		struct iface_stats *sw_stats = iface_get_stats(iface->id);
+		// Reset software stats for all interface types.
+		if (sw_stats != NULL) {
+			memset(sw_stats, 0, sizeof(*sw_stats));
+		}
+
+		if (iface->type == GR_IFACE_TYPE_PORT) {
+			struct iface_info_port *port = (struct iface_info_port *)iface->info;
+			if ((ret = rte_eth_stats_reset(port->port_id)) < 0)
+				return api_out(-ret, 0);
+			if ((ret = rte_eth_xstats_reset(port->port_id)) < 0)
+				return api_out(-ret, 0);
+		}
 	}
 
 	return api_out(0, 0);
+}
+
+static struct api_out iface_stats_get(const void * /*request*/, void **response) {
+	struct gr_infra_iface_stats_get_resp *resp = NULL;
+	struct gr_iface_stats *stats_vec = NULL;
+	struct iface *iface = NULL;
+	int ret = 0;
+
+	while ((iface = iface_next(GR_IFACE_TYPE_UNDEF, iface)) != NULL) {
+		struct iface_stats *sw_stats = iface_get_stats(iface->id);
+		if (sw_stats == NULL)
+			continue;
+
+		// Create a single stats object per interface
+		struct gr_iface_stats s;
+		s.iface_id = iface->id;
+		s.rx_packets = 0;
+		s.rx_bytes = 0;
+		s.tx_packets = 0;
+		s.tx_bytes = 0;
+
+		// Aggregate per-core stats
+		for (int i = 0; i < RTE_MAX_LCORE; i++) {
+			s.rx_packets += sw_stats->rx_packets[i];
+			s.rx_bytes += sw_stats->rx_bytes[i];
+			s.tx_packets += sw_stats->tx_packets[i];
+			s.tx_bytes += sw_stats->tx_bytes[i];
+		}
+		gr_vec_add(stats_vec, s);
+	}
+
+	size_t n_stats = gr_vec_len(stats_vec);
+	size_t len = sizeof(*resp) + n_stats * sizeof(struct gr_iface_stats);
+	if ((resp = calloc(1, len)) == NULL) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	resp->n_stats = n_stats;
+	memcpy(resp->stats, stats_vec, n_stats * sizeof(struct gr_iface_stats));
+
+	gr_vec_free(stats_vec);
+	*response = resp;
+	return api_out(0, len);
+err:
+	gr_vec_free(stats_vec);
+	free(resp);
+	return api_out(-ret, 0);
 }
 
 static int
@@ -417,9 +474,16 @@ static struct gr_api_handler stats_reset_handler = {
 	.callback = stats_reset,
 };
 
+static struct gr_api_handler iface_stats_get_handler = {
+	.name = "iface stats get",
+	.request_type = GR_INFRA_IFACE_STATS_GET,
+	.callback = iface_stats_get,
+};
+
 RTE_INIT(infra_stats_init) {
 	gr_register_api_handler(&stats_get_handler);
 	gr_register_api_handler(&stats_reset_handler);
+	gr_register_api_handler(&iface_stats_get_handler);
 	rte_telemetry_register_cmd(
 		"/grout/stats/graph",
 		telemetry_sw_stats_get,

commit c061ed06a2dda669209ea3b79e58a6a1eecc3666

iface: fix stats command alignments
The function signature is as follows:

struct libscols_column *
scols_table_new_column(struct libscols_table *, const char *name, double whint, int flags);

Pass the flags argument in the correct position.

Fixes: 2190714808ce ("infra: add API and CLI command to obtain and clear iface stats")
Fixes: a592db25717e ("cli: add interface rates command")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/cli/iface.c
@@ -353,12 +353,12 @@ static cmd_status_t iface_stats(const struct gr_api_client *c, const struct ec_p
 	}
 
 	scols_table_new_column(table, "INTERFACE", 0, 0);
-	scols_table_new_column(table, "RX_PACKETS", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "RX_BYTES", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "RX_DROPS", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_PACKETS", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_BYTES", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_ERRORS", SCOLS_FL_RIGHT, 0);
+	scols_table_new_column(table, "RX_PACKETS", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "RX_BYTES", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "RX_DROPS", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_PACKETS", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_BYTES", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_ERRORS", 0, SCOLS_FL_RIGHT);
 	scols_table_set_column_separator(table, "  ");
 
 	for (uint16_t i = 0; i < resp->n_stats; i++) {
@@ -418,12 +418,12 @@ static cmd_status_t iface_rates(const struct gr_api_client *c, const struct ec_p
 	}
 
 	scols_table_new_column(table, "INTERFACE", 0, 0);
-	scols_table_new_column(table, "RX_PACKETS/S", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "RX_BYTES/S", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "RX_DROPS/S", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_PACKETS/S", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_BYTES/S", SCOLS_FL_RIGHT, 0);
-	scols_table_new_column(table, "TX_ERRORS/S", SCOLS_FL_RIGHT, 0);
+	scols_table_new_column(table, "RX_PACKETS/S", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "RX_BYTES/S", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "RX_DROPS/S", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_PACKETS/S", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_BYTES/S", 0, SCOLS_FL_RIGHT);
+	scols_table_new_column(table, "TX_ERRORS/S", 0, SCOLS_FL_RIGHT);
 	scols_table_set_column_separator(table, "  ");
 
 	for (uint16_t i = 0; i < resp2->n_stats; i++) {