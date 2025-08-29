ec251eff030bfc16727376ec5be9aa8a480791bc
Robin Jarry <robin@jarry.cc>
2025-08-29 16:35:57 +0200

    ip6: avoid additional rib lookup before sending ndp request
    The nexthop has already been resolved once in ip6_input and can be passed
    to the control plane. Store it in control_output_mbuf_data->cb_data so
    that nh6_unreachable_cb can reuse it without an additional rib6_lookup.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>

--- modules/ip6/datapath/ip6_hold.c
+++ modules/ip6/datapath/ip6_hold.c
@@ -4,6 +4,7 @@
 #include <gr_control_output.h>
 #include <gr_graph.h>
 #include <gr_ip6_control.h>
+#include <gr_ip6_datapath.h>
 #include <gr_mbuf.h>
 
 #include <rte_mbuf.h>
@@ -16,12 +17,15 @@ enum {
 static uint16_t
 ip6_hold_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct control_output_mbuf_data *d;
+	const struct nexthop *nh;
 	struct rte_mbuf *mbuf;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
+		nh = ip6_output_mbuf_data(mbuf)->nh;
 		d = control_output_mbuf_data(mbuf);
 		d->callback = nh6_unreachable_cb;
+		memcpy(d->cb_data, &nh, sizeof(const struct nexthop *));
 		if (gr_mbuf_is_traced(mbuf))
 			gr_mbuf_trace_add(mbuf, node, 0);
 		rte_node_enqueue_x1(graph, node, CONTROL, mbuf);
