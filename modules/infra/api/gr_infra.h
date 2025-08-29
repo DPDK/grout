DPDK/grout

commit 599a538ea42f754a0c421fcdb8992b122b4cf779

infra: introduce interface mode
Interfaces will not always be an "L3" (routed) interface.
Introduce an enum describing the current mode, default
being "L3".

CLI is updated to reflect if the interface is in L3 mode:

grout# show interface all
NAME      ID  FLAGS       MODE      DOMAIN  TYPE  INFO
dpdk-p0   0   up running  L3        0       port  devargs=0000:05:00.1 mac=3c:fd:fe:a0:a4:c4

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/api/gr_infra.h
@@ -46,6 +46,11 @@ typedef enum {
 
 #define GR_VRF_ID_ALL UINT16_MAX
 
+typedef enum gr_iface_mode {
+	GR_IFACE_MODE_L3 = 0,
+	GR_IFACE_MODE_COUNT
+} __attribute__((mode(QI))) gr_iface_mode_t;
+
 // Generic struct for all network interfaces.
 struct gr_iface {
 	uint16_t id; // Interface unique index.
@@ -53,11 +58,17 @@ struct gr_iface {
 	gr_iface_flags_t flags; // Interface flags. Bit mask of GR_IFACE_F_*.
 	gr_iface_state_t state; // Interface state. Bit mask of GR_IFACE_S_*.
 	uint16_t mtu; // Maximum transmission unit size (incl. headers).
-	uint16_t vrf_id; // L3 addressing and routing domain
+	gr_iface_mode_t mode;
+	union {
+		uint16_t vrf_id; // L3 addressing and routing domain
+		uint16_t domain_id; // L2 xconnect peer interface id
+	};
 #define GR_IFACE_NAME_SIZE 64
 	char name[GR_IFACE_NAME_SIZE]; // Interface name (utf-8 encoded, nul terminated).
 	uint8_t info[256]; // Type specific interface info.
 };
+
+void register_interface_mode(gr_iface_mode_t mode, const char *next_node);
 
 // Port reconfig attributes
 #define GR_PORT_SET_N_RXQS GR_BIT64(32)

modules/infra/datapath/eth_input.c
@@ -143,9 +143,14 @@ static struct rte_node_register node = {
 	},
 };
 
+static void eth_input_register(void) {
+	register_interface_mode(GR_IFACE_MODE_L3, "eth_input");
+}
+
 static struct gr_node_info info = {
 	.node = &node,
 	.trace_format = eth_trace_format,
+	.register_callback = eth_input_register,
 };
 
 GR_NODE_REGISTER(info);

modules/infra/api/iface.c
@@ -21,6 +21,7 @@ static void iface_to_api(struct gr_iface *to, const struct iface *from) {
 	to->state = from->state;
 	to->mtu = from->mtu;
 	to->vrf_id = from->vrf_id;
+	to->mode = from->mode;
 	memccpy(to->name, from->name, 0, sizeof(to->name));
 	type->to_api(to->info, from);
 }

modules/infra/cli/iface.c
@@ -250,7 +250,8 @@ static cmd_status_t iface_list(const struct gr_api_client *c, const struct ec_pn
 	scols_table_new_column(table, "NAME", 0, 0);
 	scols_table_new_column(table, "ID", 0, 0);
 	scols_table_new_column(table, "FLAGS", 0, 0);
-	scols_table_new_column(table, "VRF", 0, 0);
+	scols_table_new_column(table, "MODE", 0, 0);
+	scols_table_new_column(table, "DOMAIN", 0, 0);
 	scols_table_new_column(table, "TYPE", 0, 0);
 	scols_table_new_column(table, "INFO", 0, 0);
 	scols_table_set_column_separator(table, "  ");
@@ -283,21 +284,24 @@ static cmd_status_t iface_list(const struct gr_api_client *c, const struct ec_pn
 			SAFE_BUF(snprintf, sizeof(buf), " tracing");
 		scols_line_set_data(line, 2, buf);
 
+		// mode
+		scols_line_set_data(line, 3, iface->mode == GR_IFACE_MODE_L3 ? "L3" : "UNKNOWN");
+
 		// vrf
-		scols_line_sprintf(line, 3, "%u", iface->vrf_id);
+		scols_line_sprintf(line, 4, "%u", iface->vrf_id);
 
 		if (type == NULL) {
 			// type
-			scols_line_sprintf(line, 4, "%u", iface->type);
+			scols_line_sprintf(line, 5, "%u", iface->type);
 			// info
-			scols_line_set_data(line, 5, "");
+			scols_line_set_data(line, 6, "");
 		} else {
 			// type
-			scols_line_set_data(line, 4, type->name);
+			scols_line_set_data(line, 5, type->name);
 			// info
 			buf[0] = 0;
 			type->list_info(c, iface, buf, sizeof(buf));
-			scols_line_set_data(line, 5, buf);
+			scols_line_set_data(line, 6, buf);
 		}
 	}
 

modules/infra/control/gr_iface.h
@@ -19,7 +19,11 @@ struct __rte_cache_aligned iface {
 	gr_iface_flags_t flags;
 	gr_iface_state_t state;
 	uint16_t mtu;
-	uint16_t vrf_id; // L3 addressing and routing domain
+	union {
+		uint16_t vrf_id; // L3 addressing and routing domain
+		uint16_t domain_id;
+	};
+	enum gr_iface_mode mode;
 	const struct iface **subinterfaces;
 	char *name;
 	alignas(alignof(void *)) uint8_t info[/* size depends on type */];

modules/infra/control/iface.c
@@ -76,6 +76,7 @@ struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 	if (next_ifid(&ifid) < 0)
 		goto fail;
 
+	iface->mode = GR_IFACE_MODE_L3;
 	iface->id = ifid;
 	iface->type_id = conf->type;
 	iface->flags = conf->flags;

modules/infra/control/port.c
@@ -353,7 +353,10 @@ static int iface_port_fini(struct iface *iface) {
 		while ((iface = iface_next(GR_IFACE_TYPE_PORT, iface)) != NULL) {
 			struct iface_info_port p = {.n_txq = 0};
 			struct gr_iface conf = {
-				.flags = iface->flags, .mtu = iface->mtu, .vrf_id = iface->vrf_id
+				.flags = iface->flags,
+				.mtu = iface->mtu,
+				.mode = iface->mode,
+				.vrf_id = iface->vrf_id
 			};
 			ret = iface_port_reconfig(iface, GR_PORT_SET_N_TXQS, &conf, &p);
 			if (ret < 0)
@@ -397,6 +400,7 @@ static int iface_port_init(struct iface *iface, const void *api_info) {
 	conf.flags = iface->flags;
 	conf.mtu = iface->mtu;
 	conf.vrf_id = iface->vrf_id;
+	conf.mode = iface->mode;
 	ret = iface_port_reconfig(iface, IFACE_SET_ALL, &conf, api_info);
 	if (ret < 0) {
 		iface_port_fini(iface);

commit bfcfd3f1eccb1e4d58e2a35daab8e979fcf29b31

infra: remove register_interface_mode from public api
This function should not be exposed in the API headers.

Fixes: 599a538ea42f ("infra: introduce interface mode")
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/api/gr_infra.h
@@ -77,8 +77,6 @@ struct gr_iface {
 	uint8_t info[256]; // Type specific interface info.
 };
 
-void register_interface_mode(gr_iface_mode_t mode, const char *next_node);
-
 // Port reconfig attributes
 #define GR_PORT_SET_N_RXQS GR_BIT64(32)
 #define GR_PORT_SET_N_TXQS GR_BIT64(33)

modules/infra/datapath/eth_input.c
@@ -4,6 +4,7 @@
 #include <gr_eth.h>
 #include <gr_graph.h>
 #include <gr_log.h>
+#include <gr_rxtx.h>
 #include <gr_trace.h>
 #include <gr_vlan.h>
 

modules/infra/datapath/gr_rxtx.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <gr_infra.h>
+
 #include <rte_build_config.h>
 
 #include <stddef.h>
@@ -28,3 +30,5 @@ struct rxtx_trace_data {
 };
 
 int rxtx_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);
+
+void register_interface_mode(gr_iface_mode_t mode, const char *next_node);