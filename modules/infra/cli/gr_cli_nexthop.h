163eb2ca646b0dc8fe1cf28973847eb8082ce9e7
Robin Jarry <rjarry@redhat.com>
2025-09-10 13:23:38 +0200

    cli: centralize nexthop formatting
    Consolidate nexthop formatting logic across CLI components by
    introducing a pluggable formatter system. Replace duplicate formatting
    code in route and nexthop list commands with a unified
    gr_cli_format_nexthop() function that supports type-specific formatters.
    
    For now, only L3 nexthops have a formatter registered.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

---
+++ modules/infra/cli/gr_cli_nexthop.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#pragma once
+
+#include <gr_nexthop.h>
+
+#include <stdio.h>
+#include <sys/queue.h>
+
+struct gr_cli_nexthop_formatter {
+	const char *name;
+	gr_nh_type_t type;
+	ssize_t (*format)(char *buf, size_t len, const struct gr_nexthop *);
+	STAILQ_ENTRY(gr_cli_nexthop_formatter) entries;
+};
+
+void gr_cli_nexthop_register_formatter(struct gr_cli_nexthop_formatter *);
+
+ssize_t gr_cli_format_nexthop(
+	char *buf,
+	size_t len,
+	struct gr_api_client *,
+	const struct gr_nexthop *,
+	bool with_base_info
+);
