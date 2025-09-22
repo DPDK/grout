DPDK/grout

commit 87629ec883bb97554a1f3b11eb0275cf01aafe34

cli: fix broken context initialization
Depending on the order in which files are linked in the final binary,
the interface context (modules/infra/cli/iface.c) may be loaded before
the stats context (modules/infra/cli/stats.c). When the stats context
loads, there happens to be an existing "stats" node present in the
command tree:

  show interface stats
                 ^^^^^

This causes the code in get_or_create to skip the creation of the stats
context node and use this constant str node in its place. Then, when
trying to append stats commands under this str node, we get an EINVAL
error from libecoli since the base node is not an "or" node.

Replace ec_node_find() which searches recursively for *any* node that
has the provided ID. Only consider direct children of the root node and
ensure they are of type "or".

Fixes: af3ef1185218 ("cli: rework command registration")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

cli/ecoli.c
@@ -42,10 +42,15 @@ static struct ec_node *get_or_create(struct ec_node *root, const char *name, con
 		goto fail;
 	}
 
-	// if context is already present, return the OR node directly
-	or_node = ec_node_find(root, name);
-	if (or_node != NULL)
-		return or_node;
+	for (unsigned i = 0; i < ec_node_get_children_count(root); i++) {
+		unsigned refs;
+		ec_node_get_child(root, i, &or_node, &refs);
+		if (strcmp(ec_node_id(or_node), name) == 0
+		    && strcmp(ec_node_type(or_node)->name, "or") == 0) {
+			// if context is already present, return the OR node directly
+			return or_node;
+		}
+	}
 
 	// else, create the context node
 	if ((or_node = ec_node("or", name)) == NULL)

commit c2dee5880561c5b9326c202460e1b3c7b1959855

cli: fix broken context initialization for real
While the previous commit indeed fixed the incorrect call to
ec_node_or_add(), it did introduce another stupid mistake. The "or" node
that we are looking for is not a direct descendant of the root context.
Instead, each branch of the root is made of a "seq" node that starts
with a "str" node without any ID followed by the "or" node that we are
looking for. This causes context nodes to be duplicated and breaks
completion:

    grout# ad<tab>
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    add                  Create objects in the configuration.
    grout# add <tab>
    nexthop              Create a new nexthop.
    nexthop              Create a new nexthop.
    interface            Create interfaces.
    interface            Create interfaces.
    ip                   Create IPv4 stack elements.
    ip                   Create IPv4 stack elements.
    ip6                  Create IPv6 stack elements.
    ip6                  Create IPv6 stack elements.
    interface            Create interfaces.
    policy               Create policy elements.
    policy               Create policy elements.
    sr                   Create srv6 stack elements.
    sr                   Create srv6 stack elements.

Take this into account to ensure we find something.

Fixes: 87629ec883bb ("cli: fix broken context initialization")

cli/ecoli.c
@@ -43,10 +43,19 @@ static struct ec_node *get_or_create(struct ec_node *root, const char *name, con
 	}
 
 	for (unsigned i = 0; i < ec_node_get_children_count(root); i++) {
+		struct ec_node *seq_node;
 		unsigned refs;
-		ec_node_get_child(root, i, &or_node, &refs);
-		if (strcmp(ec_node_id(or_node), name) == 0
-		    && strcmp(ec_node_type(or_node)->name, "or") == 0) {
+		if (ec_node_get_child(root, i, &seq_node, &refs) < 0)
+			continue;
+		if (strcmp(ec_node_type(seq_node)->name, "seq") != 0)
+			continue;
+		if (ec_node_get_children_count(seq_node) != 2)
+			continue;
+		if (ec_node_get_child(seq_node, 1, &or_node, &refs) < 0)
+			continue;
+		if (strcmp(ec_node_type(or_node)->name, "or") != 0)
+			continue;
+		if (strcmp(ec_node_id(or_node), name) == 0) {
 			// if context is already present, return the OR node directly
 			return or_node;
 		}