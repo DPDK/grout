DPDK/grout

commit 2a1a0e0404cc29d3a1e8c7755bdd6a6d34e8776d

main: implement basic dynamic array utilities
We currently depend on stb_ds.h for certain non-critical code paths.
This lib is not available in any distribution and basically would
require grout to vendor it. Since it provides a lot more features that
we actually need. Implement a stripped down version of it. Do not try to
implement any of the hash map features. We do not need them.

Add basic unit tests.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

main/gr_vec.h
@@ -1,0 +1,143 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#ifndef _GR_VEC
+#define _GR_VEC
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// (internal) vector header
+struct gr_vec_hdr {
+	size_t len;
+	size_t cap;
+};
+
+// (internal) get a pointer the vector header
+static inline struct gr_vec_hdr *gr_vec_hdr(void *vec) {
+	if (vec == NULL)
+		return NULL;
+	return ((struct gr_vec_hdr *)vec) - 1;
+}
+
+// get the current capacity of a vector
+static inline size_t gr_vec_cap(void *vec) {
+	if (vec == NULL)
+		return 0;
+	return gr_vec_hdr(vec)->cap;
+}
+
+// get the size of a vector
+static inline size_t gr_vec_len(void *vec) {
+	if (vec == NULL)
+		return 0;
+	return gr_vec_hdr(vec)->len;
+}
+
+// (internal) allocate memory to prepare for more items
+static inline void *gr_vec_grow(void *vec, size_t item_size, size_t add_num, size_t min_cap) {
+	size_t min_len = gr_vec_len(vec) + add_num;
+	struct gr_vec_hdr *hdr;
+
+	if (min_len > min_cap)
+		min_cap = min_len;
+	if (min_cap <= gr_vec_cap(vec))
+		return vec;
+
+	// ensure large enough capacity grow to avoid multiple realloc calls
+	if (min_cap < 2 * gr_vec_cap(vec))
+		min_cap = 2 * gr_vec_cap(vec);
+	else if (min_cap < 4)
+		min_cap = 4;
+
+	hdr = realloc(gr_vec_hdr(vec), sizeof(*hdr) + item_size * min_cap);
+	if (hdr == NULL)
+		abort();
+
+	if (vec == NULL)
+		hdr->len = 0;
+	hdr->cap = min_cap;
+
+	return hdr + 1;
+}
+
+// (internal) delete multiple items stating at a given index
+static inline void gr_vec_del_range(void *vec, size_t item_size, size_t start, size_t len) {
+	struct gr_vec_hdr *hdr = gr_vec_hdr(vec);
+	size_t end = start + len;
+
+	if (hdr == NULL || start >= hdr->len || len == 0 || item_size == 0)
+		abort();
+
+	if (end >= hdr->len) {
+		hdr->len = start;
+		return;
+	}
+
+	memmove((void *)((uintptr_t)vec + (item_size * start)),
+		(void *)((uintptr_t)vec + (item_size * end)),
+		item_size * len);
+
+	hdr->len -= len;
+}
+
+// (internal) delete multiple items stating at a given index
+static inline void *gr_vec_shift_range(void *vec, size_t item_size, size_t start, size_t len) {
+	size_t end = start + len;
+
+	if (len == 0 || item_size == 0 || start > gr_vec_len(vec))
+		abort();
+
+	vec = gr_vec_grow(vec, item_size, len, 0);
+
+	memmove((void *)((uintptr_t)vec + (item_size * end)),
+		(void *)((uintptr_t)vec + (item_size * start)),
+		item_size * (gr_vec_len(vec) - start));
+
+	gr_vec_hdr(vec)->len += len;
+
+	return vec;
+}
+
+// free a previously allocated vector
+#define gr_vec_free(v) ((v) ? free(gr_vec_hdr(v)) : (void)0, (v) = NULL)
+
+// force a vector with a specified min capacity
+#define gr_vec_cap_set(v, c) ((v) = gr_vec_grow((v), sizeof(*(v)), 0, (c)))
+
+// ensure a vector has enough capacity to add n items
+#define gr_vec_maybe_grow(v, n) ((v) = gr_vec_grow((v), sizeof(*(v)), (n), 0))
+
+// add an item at the end of a vector
+#define gr_vec_add(v, x) (gr_vec_maybe_grow(v, 1), (v)[gr_vec_hdr(v)->len++] = (x))
+
+// add an item at a specific index in a vector
+#define gr_vec_insert(v, i, x) ((v) = gr_vec_shift_range(v, sizeof(*(v)), (i), 1), (v)[i] = (x))
+
+// remove the last item from a vector and return it
+#define gr_vec_pop(v) (gr_vec_len(v) > 0 ? (void)0 : abort(), (v)[--gr_vec_hdr(v)->len])
+
+// get the last item from a vector
+#define gr_vec_last(v) (gr_vec_len(v) > 0 ? (void)0 : abort(), (v)[gr_vec_hdr(v)->len - 1])
+
+// delete an item at the specified index, shifting following items
+#define gr_vec_del(v, i) gr_vec_del_range(v, sizeof(*(v)), (i), 1)
+
+// delete multiple items stating at a given index
+#define gr_vec_del_n(v, i, n) gr_vec_del_range(vec, sizeof(*(v)), (i), (n))
+
+// replace the item at the specified index with the last item
+#define gr_vec_del_swap(v, i) ((v)[i] = gr_vec_last(v), gr_vec_hdr(v)->len--)
+
+// iterate over a vector, dereferencing each item into a local variable
+#define gr_vec_foreach(x, v)                                                                       \
+	for (size_t __i = 0, __next = 1; __next && __i < gr_vec_len(v); __next = !__next, __i++)   \
+		for (x = v[__i]; __next; __next = !__next)
+
+// iterate over a vector, referencing each item into a local pointer
+#define gr_vec_foreach_ref(p, v)                                                                   \
+	for (size_t __i = 0, __next = 1; __next && __i < gr_vec_len(v); __next = !__next, __i++)   \
+		for (p = &v[__i]; __next; __next = !__next)
+
+#endif

main/vec_test.c
@@ -1,0 +1,83 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <gr_cmocka.h>
+#include <gr_vec.h>
+
+static void int_vec(void **) {
+	int *vec = NULL;
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	for (int i = 0; i < 5; i++)
+		gr_vec_add(vec, i);
+
+	assert_int_equal(gr_vec_len(vec), 5);
+
+	for (int i = 0; i < 5; i++)
+		assert_int_equal(vec[i], i);
+
+	gr_vec_insert(vec, 1, 42);
+	assert_int_equal(gr_vec_len(vec), 6);
+	assert_int_equal(vec[0], 0);
+	assert_int_equal(vec[1], 42);
+	assert_int_equal(vec[2], 1);
+	assert_int_equal(vec[3], 2);
+	assert_int_equal(vec[4], 3);
+	assert_int_equal(vec[5], 4);
+
+	gr_vec_del_swap(vec, 3);
+	assert_int_equal(gr_vec_len(vec), 5);
+
+	assert_int_equal(gr_vec_pop(vec), 3);
+	assert_int_equal(gr_vec_pop(vec), 4);
+	assert_int_equal(gr_vec_pop(vec), 1);
+	assert_int_equal(gr_vec_pop(vec), 42);
+	assert_int_equal(gr_vec_pop(vec), 0);
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	gr_vec_free(vec);
+}
+
+static void str_vec(void **) {
+	const char **vec = NULL;
+
+	assert_int_equal(gr_vec_len(vec), 0);
+
+	gr_vec_add(vec, "foo");
+	gr_vec_add(vec, "bar");
+	gr_vec_add(vec, "baz");
+
+	assert_int_equal(gr_vec_len(vec), 3);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "bar");
+	assert_string_equal(vec[2], "baz");
+
+	gr_vec_del(vec, 1);
+
+	assert_int_equal(gr_vec_len(vec), 2);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "baz");
+
+	gr_vec_insert(vec, 2, "bar");
+	assert_int_equal(gr_vec_len(vec), 3);
+	assert_string_equal(vec[0], "foo");
+	assert_string_equal(vec[1], "baz");
+	assert_string_equal(vec[2], "bar");
+
+	gr_vec_del_swap(vec, 0);
+	assert_int_equal(gr_vec_len(vec), 2);
+	assert_string_equal(vec[0], "bar");
+	assert_string_equal(vec[1], "baz");
+
+	gr_vec_free(vec);
+}
+
+int main(void) {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(int_vec),
+		cmocka_unit_test(str_vec),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}

.clang-format
@@ -82,6 +82,8 @@ ForEachMacros:
   - TAILQ_FOREACH
   - TAILQ_FOREACH_SAFE
   - rte_graph_foreach_node
+  - gr_vec_foreach
+  - gr_vec_foreach_ref
   - stbds_arrforeach
   - arrforeach
 IfMacros:

main/meson.build
@@ -13,3 +13,10 @@ src += files(
 )
 
 inc += include_directories('.')
+
+tests += [
+  {
+    'sources': files('vec_test.c'),
+    'link_args': [],
+  },
+]

commit e75dc5f19d75ed3c7b5b5d6d014e248f2260ba53

vec: fix invalid memmove size in gr_vec_del
The size to move is from after the deleted range *until the end* of the
vector.

Update unit tests to ensure it works as expected.

Fixes: 2a1a0e0404cc ("main: implement basic dynamic array utilities")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

main/gr_vec.h
@@ -95,7 +95,7 @@ static inline void __gr_vec_del_range(void *vec, size_t item_size, uint32_t star
 
 	memmove((void *)((uintptr_t)vec + (item_size * start)),
 		(void *)((uintptr_t)vec + (item_size * end)),
-		item_size * len);
+		item_size * (gr_vec_len(vec) - end));
 
 	hdr->len -= len;
 }

main/vec_test.c
@@ -19,6 +19,16 @@ static void int_vec(void **) {
 	for (int i = 0; i < 5; i++)
 		assert_int_equal(vec[i], i);
 
+	gr_vec_del(vec, 0);
+	assert_int_equal(gr_vec_len(vec), 4);
+	for (int i = 0; i < 4; i++)
+		assert_int_equal(vec[i], i + 1);
+
+	gr_vec_insert(vec, 0, 0);
+	assert_int_equal(gr_vec_len(vec), 5);
+	for (int i = 0; i < 5; i++)
+		assert_int_equal(vec[i], i);
+
 	gr_vec_insert(vec, 1, 42);
 	assert_int_equal(gr_vec_len(vec), 6);
 	assert_int_equal(vec[0], 0);