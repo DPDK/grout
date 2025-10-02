DPDK/grout

commit 7c94f16c572e482766a9ab4c32563f6ac927dd7b

stats: allow filtering by cpu id
Add a new cpu_id field to gr_infra_stats_get_req. When it is UINT16_MAX,
aggregate statistics for all CPUs like before. Otherwise, only return
stats for this CPU/worker.

Update the CLI command with a new argument.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/api/stats.c
@@ -34,7 +34,7 @@ static struct stat *find_stat(gr_vec struct stat *stats, const char *name) {
 	return errno_set_null(ENOENT);
 }
 
-static gr_vec struct stat *graph_stats(void) {
+static gr_vec struct stat *graph_stats(uint16_t cpu_id) {
 	gr_vec struct stat *stats = NULL;
 	struct worker *worker;
 	struct stat *s;
@@ -42,6 +42,8 @@ static gr_vec struct stat *graph_stats(void) {
 	STAILQ_FOREACH (worker, &workers, next) {
 		const struct worker_stats *w_stats = atomic_load(&worker->stats);
 		if (w_stats == NULL)
+			continue;
+		if (cpu_id != UINT16_MAX && worker->cpu_id != cpu_id)
 			continue;
 		for (unsigned i = 0; i < w_stats->n_stats; i++) {
 			const struct node_stats *n = &w_stats->stats[i];
@@ -76,6 +78,9 @@ static gr_vec struct stat *graph_stats(void) {
 		}
 	}
 
+	if (stats == NULL)
+		return errno_set_null(ENODEV);
+
 	return stats;
 }
 
@@ -87,8 +92,10 @@ static struct api_out stats_get(const void *request, struct api_ctx *) {
 	struct stat *s;
 	int ret;
 
-	if (req->flags & GR_INFRA_STAT_F_SW)
-		stats = graph_stats();
+	if (req->flags & GR_INFRA_STAT_F_SW) {
+		if ((stats = graph_stats(req->cpu_id)) == NULL)
+			return api_out(errno, 0, NULL);
+	}
 
 	if (req->flags & GR_INFRA_STAT_F_HW) {
 		struct rte_eth_xstat_name *names = NULL;
@@ -284,7 +291,7 @@ err:
 
 static int
 telemetry_sw_stats_get(const char * /*cmd*/, const char * /*params*/, struct rte_tel_data *d) {
-	gr_vec struct stat *stats = graph_stats();
+	gr_vec struct stat *stats = graph_stats(UINT16_MAX);
 	struct stat *s;
 
 	rte_tel_data_start_dict(d);

modules/infra/cli/stats.c
@@ -30,7 +30,7 @@ static int stats_order_cycles(const void *sa, const void *sb) {
 }
 
 static cmd_status_t stats_get(struct gr_api_client *c, const struct ec_pnode *p) {
-	struct gr_infra_stats_get_req req = {.flags = 0};
+	struct gr_infra_stats_get_req req = {.flags = 0, .cpu_id = UINT16_MAX};
 	bool brief = arg_str(p, "brief") != NULL;
 	struct gr_infra_stats_get_resp *resp;
 	void *resp_ptr = NULL;
@@ -46,6 +46,8 @@ static cmd_status_t stats_get(struct gr_api_client *c, const struct ec_pnode *p)
 	if (pattern == NULL)
 		pattern = "*";
 	snprintf(req.pattern, sizeof(req.pattern), "%s", pattern);
+	if (arg_u16(p, "CPU", &req.cpu_id) < 0 && errno != ENOENT)
+		goto fail;
 
 	if (gr_api_client_send_recv(c, GR_INFRA_STATS_GET, sizeof(req), &req, &resp_ptr) < 0)
 		goto fail;
@@ -115,12 +117,16 @@ static int ctx_init(struct ec_node *root) {
 
 	ret = CLI_COMMAND(
 		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("stats", "Print statistics.")),
-		"(software [brief])|hardware [zero,(pattern PATTERN)]",
+		"(software [brief])|hardware [zero,(pattern PATTERN),(cpu CPU)]",
 		stats_get,
 		"Print statistics.",
 		with_help("Print software stats.", ec_node_str("software", "software")),
 		with_help("Print hardware stats.", ec_node_str("hardware", "hardware")),
 		with_help("Only print packet counts.", ec_node_str("brief", "brief")),
+		with_help(
+			"Only return stats from one CPU.",
+			ec_node_uint("CPU", 0, UINT16_MAX - 1, 10)
+		),
 		with_help("Print stats with value 0.", ec_node_str("zero", "zero")),
 		with_help("Filter by glob pattern.", ec_node("any", "PATTERN"))
 	);

modules/infra/api/gr_infra.h
@@ -235,6 +235,7 @@ typedef uint16_t gr_infra_stats_flags_t;
 
 struct gr_infra_stats_get_req {
 	gr_infra_stats_flags_t flags;
+	uint16_t cpu_id; // use UINT16_MAX for all CPUs
 	char pattern[64]; // optional glob pattern
 };
 

commit 1f780dcb7e3bf7a480e4a3c294c29ec49a6a72bc

stats: fix error when dumping stats without any ports configured
When dumping software stats and no ports are configured, no workers are
reporting any stats and we get an error:

	grout# stats show software
	error: command failed: No such device

Do not return an error when asking to dump all CPUs.

Fixes: 7c94f16c572e ("stats: allow filtering by cpu id")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/api/stats.c
@@ -73,9 +73,6 @@ static gr_vec struct gr_infra_stat *graph_stats(uint16_t cpu_id) {
 		}
 	}
 
-	if (stats == NULL)
-		return errno_set_null(ENODEV);
-
 	return stats;
 }
 
@@ -88,8 +85,9 @@ static struct api_out stats_get(const void *request, struct api_ctx *) {
 	int ret;
 
 	if (req->flags & GR_INFRA_STAT_F_SW) {
-		if ((stats = graph_stats(req->cpu_id)) == NULL)
-			return api_out(errno, 0, NULL);
+		stats = graph_stats(req->cpu_id);
+		if (stats == NULL && req->cpu_id != UINT16_MAX)
+			return api_out(ENODEV, 0, NULL);
 	}
 
 	if (req->flags & GR_INFRA_STAT_F_HW) {