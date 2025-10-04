DPDK/grout

commit 0067e940f81cf77a95dbf5140ec5a6ebfe5b48c4

stats: allow sorting by graph topological order
Include a new topo_order field in gr_infra_stat. For meta statistics
such as idle, set it to UINT64_MAX so that they are always at the end of
the chain.

For node stats, use the ordering produced by DPDK rte_graph.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/main_loop.c
@@ -103,6 +103,7 @@ static int stats_reload(const struct rte_graph *graph, struct stats_context *ctx
 		rte_graph_foreach_node (count, off, graph, node) {
 			ctx->node_to_index[node->id] = count;
 			ctx->w_stats->stats[count].node_id = node->id;
+			ctx->w_stats->stats[count].topo_order = count;
 		}
 	}
 

modules/infra/api/gr_infra.h
@@ -124,6 +124,7 @@ struct gr_port_rxq_map {
 
 struct gr_infra_stat {
 	char name[64];
+	uint64_t topo_order;
 	uint64_t objs;
 	uint64_t calls;
 	uint64_t cycles;

modules/infra/api/stats.c
@@ -51,6 +51,7 @@ static gr_vec struct gr_infra_stat *graph_stats(uint16_t cpu_id) {
 					.objs = n->objs,
 					.calls = n->calls,
 					.cycles = n->cycles,
+					.topo_order = n->topo_order,
 				};
 				memccpy(stat.name, name, 0, sizeof(stat.name));
 				gr_vec_add(stats, stat);
@@ -65,6 +66,7 @@ static gr_vec struct gr_infra_stat *graph_stats(uint16_t cpu_id) {
 				.objs = 0,
 				.calls = w_stats->n_sleeps,
 				.cycles = w_stats->sleep_cycles,
+				.topo_order = UINT64_MAX,
 			};
 			memccpy(stat.name, "idle", 0, sizeof(stat.name));
 			gr_vec_add(stats, stat);

modules/infra/cli/stats.c
@@ -40,6 +40,17 @@ static int stats_order_packets(const void *sa, const void *sb) {
 	return 1;
 }
 
+static int stats_order_topo(const void *sa, const void *sb) {
+	const struct gr_infra_stat *a = sa;
+	const struct gr_infra_stat *b = sb;
+
+	if (a->topo_order == b->topo_order)
+		return 0;
+	if (a->topo_order > b->topo_order)
+		return 1;
+	return -1;
+}
+
 static cmd_status_t stats_get(struct gr_api_client *c, const struct ec_pnode *p) {
 	struct gr_infra_stats_get_req req = {.flags = 0, .cpu_id = UINT16_MAX};
 	bool brief = arg_str(p, "brief") != NULL;
@@ -73,6 +84,8 @@ static cmd_status_t stats_get(struct gr_api_client *c, const struct ec_pnode *p)
 		sort_func = stats_order_cycles;
 	else if (strcmp(order, "packets") == 0)
 		sort_func = stats_order_packets;
+	else if (strcmp(order, "graph") == 0)
+		sort_func = stats_order_topo;
 	else if (req.flags & GR_INFRA_STAT_F_HW || brief)
 		sort_func = stats_order_name;
 	else
@@ -161,6 +174,10 @@ static int ctx_init(struct ec_node *root) {
 				with_help(
 					"Sort by decreasing number of packets.",
 					ec_node_str(EC_NO_ID, "packets")
+				),
+				with_help(
+					"Sort by graph topological order.",
+					ec_node_str(EC_NO_ID, "graph")
 				),
 				with_help(
 					"Sort by decreasing number of cycles.",

modules/infra/control/gr_worker.h
@@ -24,6 +24,7 @@ struct queue_map {
 
 struct node_stats {
 	rte_node_t node_id;
+	uint16_t topo_order;
 	uint64_t objs;
 	uint64_t calls;
 	uint64_t cycles;

commit dbc76f3b966e20340b8086494a84be3c5b4edaac

stats: fix graph topological order
rte_graph_foreach_node does not iterate in the topological graph order.
At first glance it may look like so but it does not.

Use the previously introduced topo_sort generic function to sort a list
of nodes based on an is_parent callback.

Once the nodes are properly sorted, iterate over them and initialize the
topo_order field accordingly.

Fixes: 0067e940f81c ("stats: allow sorting by graph topological order")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/main_loop.c
@@ -7,6 +7,8 @@
 #include <gr_log.h>
 #include <gr_module.h>
 #include <gr_rcu.h>
+#include <gr_sort.h>
+#include <gr_vec.h>
 #include <gr_worker.h>
 
 #include <rte_common.h>
@@ -63,6 +65,24 @@ static inline void stats_reset(struct worker_stats *stats) {
 	stats->n_loops = 0;
 }
 
+static bool node_is_parent(const void *node, const void *maybe_parent) {
+	const struct rte_node *p = maybe_parent;
+	const struct rte_node *n = node;
+
+	for (rte_edge_t edge = 0; edge < p->nb_edges; edge++) {
+		if (p->nodes[edge]->id == n->id)
+			return true;
+	}
+
+	return false;
+}
+
+static int node_name_cmp(const void *a, const void *b) {
+	const struct rte_node *na = *(const struct rte_node **)a;
+	const struct rte_node *nb = *(const struct rte_node **)b;
+	return strncmp(na->name, nb->name, sizeof(na->name));
+}
+
 static int stats_reload(const struct rte_graph *graph, struct stats_context *ctx) {
 	struct rte_graph_cluster_stats_param stats_param;
 	const char *graph_names[1];
@@ -112,14 +132,26 @@ static int stats_reload(const struct rte_graph *graph, struct stats_context *ctx
 		}
 		ctx->w_stats->n_stats = graph->nb_nodes;
 
-		struct rte_node *node;
+		gr_vec const struct rte_node **nodes = NULL;
+		const struct rte_node *node;
 		rte_graph_off_t off;
 		rte_node_t count;
-		rte_graph_foreach_node (count, off, graph, node) {
+		rte_graph_foreach_node (count, off, graph, node)
+			gr_vec_add(nodes, node);
+
+		// sort by name first to ensure stable topo_sort
+		qsort(nodes, count, sizeof(void *), node_name_cmp);
+		topo_sort((gr_vec const void **)nodes, node_is_parent, NULL);
+
+		count = 0;
+		gr_vec_foreach (node, nodes) {
 			ctx->node_to_index[node->id] = count;
 			ctx->w_stats->stats[count].node_id = node->id;
 			ctx->w_stats->stats[count].topo_order = count;
+			count++;
 		}
+
+		gr_vec_free(nodes);
 	}
 
 	return 0;