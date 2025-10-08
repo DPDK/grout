d3bab2074c2b86abf507f66792cb3aae58d89997
Robin Jarry <rjarry@redhat.com>
2025-10-08 11:06:23 +0200

    datapath: drop packets on interfaces with admin down flag
    Check GR_IFACE_F_UP flag in eth_input, eth_output, ipip_input and
    ipip_output nodes before processing packets. If the interface or its
    parent (for VLANs) is administratively down, redirect packets to the
    iface_input_admin_down drop node.
    
    This prevents traffic from flowing through interfaces that have been
    disabled via configuration.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- modules/ipip/datapath_out.c
+++ modules/ipip/datapath_out.c
@@ -23,6 +23,7 @@ enum {
 	IP_OUTPUT = 0,
 	NO_TUNNEL,
 	NO_HEADROOM,
+	IFACE_DOWN,
 	EDGE_COUNT,
 };
 
@@ -50,6 +51,10 @@ ipip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 		if (gr_mbuf_is_traced(mbuf)) {
 			struct trace_ipip_data *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
 			t->iface_id = iface->id;
+		}
+		if (!(iface->flags & GR_IFACE_F_UP)) {
+			edge = IFACE_DOWN;
+			goto next;
 		}
 		ip_data->iface = iface;
 		ipip = iface_info_ipip(iface);
@@ -98,6 +103,7 @@ static struct rte_node_register ipip_output_node = {
 		[IP_OUTPUT] = "ip_output",
 		[NO_TUNNEL] = "ipip_output_no_tunnel",
 		[NO_HEADROOM] = "error_no_headroom",
+		[IFACE_DOWN] = "iface_input_admin_down",
 	},
 };
 
