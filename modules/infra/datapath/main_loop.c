DPDK/grout

commit e535cfd5c25c055ff66b0e88dd5840cac2f1cb29

stats: avoid crash with node_id greater than 255
The node_to_index array is used to map node IDs to offsets in the
w_stats->stats array. If we have nodes with IDs greater than 255, this
results in invalid memory accesses and can cause crashes.

Dynamically allocate this array using the maximum node ID as reported by
the rte_graph API.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/main_loop.c
@@ -25,7 +25,7 @@ struct stats_context {
 	struct rte_graph_cluster_stats *stats;
 	uint64_t last_count;
 	struct worker_stats *w_stats;
-	uint8_t node_to_index[256];
+	unsigned *node_to_index;
 };
 
 static int node_stats_callback(
@@ -37,7 +37,7 @@ static int node_stats_callback(
 	struct stats_context *ctx = cookie;
 	struct node_stats *s;
 	uint64_t objs_incr;
-	uint8_t index;
+	unsigned index;
 
 	objs_incr = stats->objs - stats->prev_objs;
 	ctx->last_count += objs_incr;
@@ -95,6 +95,19 @@ static int stats_reload(const struct rte_graph *graph, struct stats_context *ctx
 		);
 		if (ctx->w_stats == NULL) {
 			LOG(ERR, "rte_zmalloc_socket: %s", rte_strerror(rte_errno));
+			return -rte_errno;
+		}
+		ctx->node_to_index = rte_calloc_socket(
+			__func__,
+			rte_node_max_count() + 1,
+			sizeof(*ctx->node_to_index),
+			RTE_CACHE_LINE_SIZE,
+			graph->socket
+		);
+		if (ctx->node_to_index == NULL) {
+			rte_free(ctx->w_stats);
+			ctx->w_stats = NULL;
+			LOG(ERR, "rte_calloc_socket: %s", rte_strerror(rte_errno));
 			return -rte_errno;
 		}
 		ctx->w_stats->n_stats = graph->nb_nodes;

commit 5e0700b7c52e5817612e4c5d5c9e4f8c6df1f049

stats: free node_to_index array when shutting down workers
The stats_context.node_to_index array is dynamically allocated using
rte_malloc(). Call rte_free() when exiting the main loop to avoid
leaking memory.

Explicitly initialize all pointers in the stats_context to avoid
ambiguities.

Fixes: e535cfd5c25c ("stats: avoid crash with node_id greater than 255")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/main_loop.c
@@ -165,7 +165,12 @@ static int stats_reload(const struct rte_graph *graph, struct stats_context *ctx
 static struct rte_rcu_qsbr *rcu;
 
 void *gr_datapath_loop(void *priv) {
-	struct stats_context ctx = {.last_count = 0};
+	struct stats_context ctx = {
+		.stats = NULL,
+		.last_count = 0,
+		.node_to_index = NULL,
+		.w_stats = NULL,
+	};
 	uint64_t timestamp, timestamp_tmp, cycles;
 	uint32_t sleep, max_sleep_us;
 	struct worker *w = priv;
@@ -271,6 +276,7 @@ shutdown:
 	if (ctx.stats)
 		rte_graph_cluster_stats_destroy(ctx.stats);
 	rte_free(ctx.w_stats);
+	rte_free(ctx.node_to_index);
 	rte_rcu_qsbr_thread_unregister(rcu, rte_lcore_id());
 	rte_thread_unregister();
 	w->lcore_id = LCORE_ID_ANY;