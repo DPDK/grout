813af553f2bd208db6a39c89ac77b3e0368078b1
Anthony Harivel <aharivel@redhat.com>
2025-10-20 11:28:47 +0200

    smoke: add IPv4 fragmentation test
    Add a smoke test that verifies IPv4 packet fragmentation behavior by
    configuring asymmetric MTUs between two TAP interfaces (1500 and 1280
    bytes) and sending packets through grout.
    
    The test validates three scenarios: normal forwarding without
    fragmentation, ICMP error generation when a large packet has the DF
    flag set, and successful fragmentation when the DF flag is not set.
    This ensures both the fragmentation logic and the DF flag handling
    work correctly.
    
    Signed-off-by: Anthony Harivel <aharivel@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

---
+++ smoke/ip_fragment_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Anthony Harivel
+# Test IPv4 fragmentation
+
+. $(dirname $0)/_init.sh
+
+p0=${run_id}0
+p1=${run_id}1
+
+grcli interface add port $p0 devargs net_tap0,iface=$p0 mac f0:0d:ac:dc:00:00
+# Set smaller MTU on p1 (egress) to force fragmentation
+grcli interface add port $p1 devargs net_tap1,iface=$p1 mac f0:0d:ac:dc:00:01 mtu 1280
+grcli address add 172.16.0.1/24 iface $p0
+grcli address add 172.16.1.1/24 iface $p1
+
+for n in 0 1; do
+	p=$run_id$n
+	netns_add $p
+	ip link set $p mtu 1500
+	ip link set $p netns $p
+	ip -n $p link set $p address ba:d0:ca:ca:00:0$n
+	ip -n $p link set $p up
+	ip -n $p link set lo up
+	ip -n $p addr add 172.16.$n.2/24 dev $p
+	ip -n $p route add default via 172.16.$n.1
+	# Clear PMTU cache to ensure kernel uses interface MTU
+	ip -n $p route flush cache
+done
+
+# Test 1: Ping with default packet size (should work without fragmentation)
+ip netns exec $p0 ping -i0.01 -c3 -n 172.16.1.2
+
+# Test 2: Large packet with DF flag set (should get ICMP fragmentation needed error)
+# Send 1260-byte packet with DF=1 (Don't Fragment)
+# Packet size: 1260 + 8 (ICMP) + 20 (IP) = 1288 bytes
+# Fits in p0 MTU (1500) but exceeds p1 MTU (1280)
+# Expected: ICMP Type 3 Code 4 (Fragmentation Needed and DF Set)
+ip netns exec $p0 ping -i0.01 -c3 -s 1260 -M do -n 172.16.1.2 && fail "ping with DF flag should have failed"
+
+# Test 3: Large packet without DF flag (should fragment and succeed)
+# Send 1260-byte packet with DF=0 (fragmentation allowed)
+# Packet size: 1260 + 8 (ICMP) + 20 (IP) = 1288 bytes
+# Fits in p0 MTU (1500) but needs fragmentation for p1 MTU (1280)
+# Expected: Packet is fragmented into 2 fragments (1276 + 32 bytes) and ping succeeds
+ip netns exec $p0 ip route flush cache
+ip netns exec $p0 ping -i0.01 -c3 -s 1260 -M dont -n 172.16.1.2
