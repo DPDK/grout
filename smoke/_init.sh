DPDK/grout

commit 6a7e9a4bffa2ef934380200274ed21e92985eb46

smoke: print stack traces on crashes
The smoke tests suite does not provide any information when grout
crashes with a signal. This makes debugging very hard, especially in CI
environments.

Capture the grout PID at startup with pgrep to be able to use it later
in the cleanup trap function. Set ulimit to allow core dumps, even when
ASAN is enabled. Configure the kernel core pattern to write core dump
files in the temporary test folder.

In the cleanup EXIT trap, save the current script exit status into
a variable to avoid it being trampled by subsequent commands. When
waiting for the grout process, inspect the returned status code to
detect if it was terminated by a signal.

If a core dump is available, use gdb to extract stack traces from all
threads. As a fallback, when no custom core pattern could be set, use
systemd-coredumpctl to gather information about the crash.

Restore the trap exit status after all cleanup has been done to
preserve the original script exit status and avoid marking failed tests
as successful.

This requires adding gdb as a dependency for running the smoke tests.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

smoke/_init.sh
@@ -18,6 +18,7 @@ else
 fi
 
 cleanup() {
+	status="$?"
 	set +e
 	sh -x $tmp/cleanup
 	# delete all non-port interfaces first
@@ -50,11 +51,38 @@ cleanup() {
 			fi
 		done
 	fi
-	rm -rf -- "$tmp"
 	if [ "$run_grout" = true ]; then
-		kill -15 %?grout
-		wait %?grout || fail "grout crashed"
+		set +x
+		kill -15 "$grout_pid"
+		wait "$grout_pid"
+		ret="$?"
+		if [ "$ret" -ne 0 ]; then
+			status="$ret"
+			if [ "$ret" -gt 128 ]; then
+				sig=$((ret - 128))
+				echo "fail: grout terminated by signal SIG$(kill -l $sig)"
+			else
+				echo "fail: grout exited with an error status $ret"
+			fi >&2
+			if [ -n "$core_pattern" ]; then
+				# core dumps written to files
+				for core in $tmp/core.*.*; do
+					[ -f "$core" ] || continue
+					gdb -c "$core" -batch \
+						-ex "info threads" \
+						-ex "thread apply all bt"
+				done
+				 # restore original core pattern
+				sysctl -w kernel.core_pattern="$core_pattern"
+			else
+				# fallback to systemd-coredump, if available
+				coredumpctl info --no-pager "$grout_pid"
+			fi
+		fi
+		set -x
 	fi
+	rm -rf -- "$tmp"
+	exit $status
 }
 
 fail() {
@@ -103,9 +131,16 @@ EOF
 set -x
 
 if [ "$run_grout" = true ]; then
+	ulimit -c unlimited
+	core_pattern=$(sysctl -n kernel.core_pattern)
+	if ! sysctl -w kernel.core_pattern="$tmp/core.%e.%p"; then
+		unset core_pattern
+	fi
+	export ASAN_OPTIONS=disable_coredump=0
 	taskset -c 0,1 grout -tvvx $grout_extra_options &
 fi
 socat FILE:/dev/null UNIX-CONNECT:$GROUT_SOCK_PATH,retry=10
+grout_pid=$(pgrep -g0 grout)
 
 case "$(basename $0)" in
 config_test.sh|graph_svg_test.sh)

.github/workflows/check.yml
@@ -66,8 +66,8 @@ jobs:
           set -xe
           sudo apt-get update -qy
           sudo apt-get install -qy --no-install-recommends \
-            make gcc ccache ninja-build meson git go-md2man libibverbs-dev \
-            libasan8 libcmocka-dev libedit-dev libarchive-dev \
+            make gcc gdb ccache ninja-build meson git go-md2man \
+            libibverbs-dev libasan8 libcmocka-dev libedit-dev libarchive-dev \
             libevent-dev libsmartcols-dev libnuma-dev python3-pyelftools \
             socat tcpdump traceroute graphviz iproute2 iputils-ping ndisc6 jq
           if echo $MESON_EXTRA_OPTS | grep -q frr=enabled ; then

commit cd56d4d636b7e965e84cb23bd1ecbba33c444b90

smoke: fix running grout in a separate terminal
When grout is started manually in a separate terminal, pgrep -g0 grout
returns nothing and exits with an error status. Indeed, there is no
grout process started as a child of the shell script.

Only check for the PID if grout was started by the script.

Fixes: 6a7e9a4bffa2 ("smoke: print stack traces on crashes")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

smoke/_init.sh
@@ -140,7 +140,9 @@ if [ "$run_grout" = true ]; then
 	taskset -c 0,1 grout -tvvx $grout_extra_options &
 fi
 socat FILE:/dev/null UNIX-CONNECT:$GROUT_SOCK_PATH,retry=10
-grout_pid=$(pgrep -g0 grout)
+if [ "$run_grout" = true ]; then
+	grout_pid=$(pgrep -g0 grout)
+fi
 
 case "$(basename $0)" in
 config_test.sh|graph_svg_test.sh)