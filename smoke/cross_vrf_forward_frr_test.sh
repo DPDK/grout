0553a52bfbc82b5836d6050a1caf9276c6b122d7
Robin Jarry <rjarry@redhat.com>
2025-10-20 19:09:51 +0200

    smoke: homogenize netns names
    Remove the "n-" prefix from network namespace names in FRR smoke tests.
    Use the port name directly as the network namespace name, consistent
    with non-FRR tests.
    
    This simplifies the test code by eliminating the need to track separate
    port and namespace names.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- smoke/cross_vrf_forward_frr_test.sh
+++ smoke/cross_vrf_forward_frr_test.sh
@@ -12,13 +12,13 @@ create_interface $p1 f0:0d:ac:dc:00:01 2
 
 for n in 0 1; do
 	p=$run_id$n
-	netns_add n-$p
-	ip link set $p netns n-$p
-	ip -n n-$p link set $p up
-	ip -n n-$p addr add 172.16.$n.2/24 dev $p
-	ip -n n-$p addr add 16.$n.0.1/16 dev lo
-	ip -n n-$p route add default via 172.16.$n.1
-	ip -n n-$p addr show
+	netns_add $p
+	ip link set $p netns $p
+	ip -n $p link set $p up
+	ip -n $p addr add 172.16.$n.2/24 dev $p
+	ip -n $p addr add 16.$n.0.1/16 dev lo
+	ip -n $p route add default via 172.16.$n.1
+	ip -n $p addr show
 done
 
 set_ip_address $p0 172.16.0.1/24
@@ -34,5 +34,5 @@ set_ip_route 16.1.0.0/16 172.16.1.2 2 2 # required for ARP resolution
 set_ip_route 16.0.0.0/16 "$(vrf_name_from_id 1)" 2 1
 set_ip_route 16.0.0.0/16 172.16.0.2 1
 
-ip netns exec n-$p0 ping -i0.01 -c3 -I 16.0.0.1 -n 16.1.0.1
-ip netns exec n-$p1 ping -i0.01 -c3 -I 16.1.0.1 -n 16.0.0.1
+ip netns exec $p0 ping -i0.01 -c3 -I 16.0.0.1 -n 16.1.0.1
+ip netns exec $p1 ping -i0.01 -c3 -I 16.1.0.1 -n 16.0.0.1
