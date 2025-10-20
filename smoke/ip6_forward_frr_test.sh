4ba8038399716d77e7f1c0366e309f03a5a3bb88
Robin Jarry <rjarry@redhat.com>
2025-10-20 19:09:51 +0200

    smoke: homogenize netns names
    Remove the "n-" prefix from network namespace names in FRR smoke tests.
    Use the port name directly as the network namespace name, consistent
    with non-FRR tests.
    
    This simplifies the test code by eliminating the need to track separate
    port and namespace names.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- smoke/ip6_forward_frr_test.sh
+++ smoke/ip6_forward_frr_test.sh
@@ -12,17 +12,17 @@ create_interface $p2 d2:f0:0c:ba:a4:12
 
 for n in 1 2; do
 	p=$run_id$n
-	netns_add n-$p
-	ip link set $p netns n-$p
-	ip -n n-$p link set $p up
-	ip -n n-$p addr add fd00:ba4:$n::2/64 dev $p
+	netns_add $p
+	ip link set $p netns $p
+	ip -n $p link set $p up
+	ip -n $p addr add fd00:ba4:$n::2/64 dev $p
 	if [[ $n -eq 1 ]]; then
-		ip -n n-$p addr add fd00:f00:$n::2/64 dev lo
+		ip -n $p addr add fd00:f00:$n::2/64 dev lo
 	else
-		ip -n n-$p addr add fd00:f00:$n::2/64 dev $p
+		ip -n $p addr add fd00:f00:$n::2/64 dev $p
 	fi
-	ip -n n-$p route add default via fd00:ba4:$n::1
-	ip -n n-$p addr show
+	ip -n $p route add default via fd00:ba4:$n::1
+	ip -n $p addr show
 done
 
 set_ip_address $p1 fd00:ba4:1::1/64
@@ -32,15 +32,15 @@ set_ip_route fd00:f00:2::/64 $p2
 
 sleep 3  # wait for DAD
 
-ip netns exec n-$p1 ping6 -i0.01 -c3 -n fe80::d2f0:cff:feba:a411
-ip netns exec n-$p2 ping6 -i0.01 -c3 -n fe80::d2f0:cff:feba:a412
-ip netns exec n-$p1 ping6 -i0.01 -c3 -n fd00:f00:2::2
-ip netns exec n-$p2 ping6 -i0.01 -c3 -n fd00:f00:1::2
-ip netns exec n-$p1 ping6 -i0.01 -c3 -n fd00:ba4:2::2
-ip netns exec n-$p2 ping6 -i0.01 -c3 -n fd00:ba4:1::2
-ip netns exec n-$p1 ping6 -i0.01 -c3 -n fd00:ba4:1::1
-ip netns exec n-$p2 ping6 -i0.01 -c3 -n fd00:ba4:2::1
-ip netns exec n-$p1 traceroute -N1 -n fd00:ba4:2::2
-ip netns exec n-$p2 traceroute -N1 -n fd00:ba4:1::2
-ip netns exec n-$p1 traceroute -N1 -n fd00:f00:2::2
-ip netns exec n-$p2 traceroute -N1 -n fd00:f00:1::2
+ip netns exec $p1 ping6 -i0.01 -c3 -n fe80::d2f0:cff:feba:a411
+ip netns exec $p2 ping6 -i0.01 -c3 -n fe80::d2f0:cff:feba:a412
+ip netns exec $p1 ping6 -i0.01 -c3 -n fd00:f00:2::2
+ip netns exec $p2 ping6 -i0.01 -c3 -n fd00:f00:1::2
+ip netns exec $p1 ping6 -i0.01 -c3 -n fd00:ba4:2::2
+ip netns exec $p2 ping6 -i0.01 -c3 -n fd00:ba4:1::2
+ip netns exec $p1 ping6 -i0.01 -c3 -n fd00:ba4:1::1
+ip netns exec $p2 ping6 -i0.01 -c3 -n fd00:ba4:2::1
+ip netns exec $p1 traceroute -N1 -n fd00:ba4:2::2
+ip netns exec $p2 traceroute -N1 -n fd00:ba4:1::2
+ip netns exec $p1 traceroute -N1 -n fd00:f00:2::2
+ip netns exec $p2 traceroute -N1 -n fd00:f00:1::2
