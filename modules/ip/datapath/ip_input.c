DPDK/grout

commit e5570d2ad6bfe85b6785371f81dd8e9aebbcdf14

policy: add stateful dynamic source nat support
Add support for "stateful" dynamic source NAT. It is a convoluted term
to represent the following:

                      public server (8.8.8.8)
                    ^                        |
  source changed    |                        | conntrack lookup
     8.8.8.1        |                        | destination changed
  + ephemeral port  |                        | to 10.16.0.99
                    |                        v + original port
                +----------------------------------+
                |       iface wan   8.8.8.1/24     |
                |                                  |
                |              grout               |
                |                                  |
                |       iface lan   10.16.0.1/24   |
                +----------------------------------+
                     |                       |
                     |                       |
                     |                       v
                        client 10.16.0.99

Basically, SNAT policies contain an output interface, a private subnet
and a public IP address to be substituted for outgoing traffic. When
a SNAT policy is configured, the GR_IFACE_F_SNAT_DYNAMIC flag is added
to the wan interface. The public IP address *must* be configured prior
to creating SNAT policies.

Every time a packet originating from a client on the lan interface side
is outgoing the wan interface (one with GR_IFACE_F_SNAT_DYNAMIC), a new
connection will be created in the conntrack module (if none exists
already). The connection will be referenced in a hash table with its
forward and reverse keys (taking the translated traffic into account).

IMPORTANT: The connections and SNAT translations *must* be created
synchronously in the datapath. With an "upcall" or "exception" mechanism
that sends packets to the control plane for conntrack creation, there is
a risk that while the connection is created and packets are in queue to

modules/ip/datapath/ip_input.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Robin Jarry
 
+#include <gr_conntrack_control.h>
 #include <gr_eth.h>
 #include <gr_fib4.h>
 #include <gr_graph.h>
@@ -19,6 +20,7 @@
 
 enum edges {
 	FORWARD = 0,
+	DNAT44_DYNAMIC,
 	OUTPUT,
 	LOCAL,
 	NO_ROUTE,
@@ -43,7 +45,6 @@ void ip_input_register_nexthop_type(gr_nh_type_t type, const char *next_node) {
 static uint16_t
 ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_input_mbuf_data *e;
-	struct ip_output_mbuf_data *d;
 	const struct iface *iface;
 	const struct nexthop *nh;
 	struct rte_ipv4_hdr *ip;
@@ -132,24 +133,39 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
+		// Store the resolved next hop for ip_output to avoid a second route lookup.
+		ip_output_mbuf_data(mbuf)->nh = nh;
+
 		edge = nh_type_edges[nh->type];
 		if (edge != FORWARD)
 			goto next;
 
 		// If the resolved next hop is local and the destination IP is ourselves,
 		// send to ip_local.
-		if (nh->flags & GR_NH_F_LOCAL && ip->dst_addr == nh->ipv4)
+		if (nh->flags & GR_NH_F_LOCAL && ip->dst_addr == nh->ipv4) {
 			edge = LOCAL;
-		else if (e->domain == ETH_DOMAIN_LOOPBACK)
+			if (iface->flags & GR_IFACE_F_SNAT_DYNAMIC) {
+				conn_flow_t flow = CONN_FLOW_REV;
+				struct conn_key key;
+				struct conn *conn;
+
+				// XXX: All returning IP fragments will go to LOCAL whether they
+				// are part of a conntrack or not. We need reassembly to fix this.
+				if (gr_conn_parse_key(iface, GR_AF_IP4, mbuf, &key)
+				    && (conn = gr_conn_lookup(&key, &flow)) != NULL) {
+					struct conn_mbuf_data *cd = conn_mbuf_data(mbuf);
+					cd->conn = conn;
+					cd->flow = flow;
+					edge = DNAT44_DYNAMIC;
+				}
+			}
+		} else if (e->domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
 			struct rte_ipv4_hdr *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
 			*t = *ip;
 		}
-		// Store the resolved next hop for ip_output to avoid a second route lookup.
-		d = ip_output_mbuf_data(mbuf);
-		d->nh = nh;
 		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 
@@ -170,6 +186,7 @@ static struct rte_node_register input_node = {
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
 		[FORWARD] = "ip_forward",
+		[DNAT44_DYNAMIC] = "dnat44_dynamic",
 		[OUTPUT] = "ip_output",
 		[LOCAL] = "ip_input_local",
 		[NO_ROUTE] = "ip_error_dest_unreach",
@@ -206,12 +223,24 @@ mock_func(uint16_t, drop_packets(struct rte_graph *, struct rte_node *, void **,
 mock_func(int, drop_format(char *, size_t, const void *, size_t));
 mock_func(int, trace_ip_format(char *, size_t, const struct rte_ipv4_hdr *, size_t));
 mock_func(void, gr_eth_input_add_type(rte_be16_t, const char *));
+mock_func(
+	bool,
+	gr_conn_parse_key(
+		const struct iface *,
+		const addr_family_t,
+		const struct rte_mbuf *,
+		struct conn_key *
+	)
+);
+mock_func(struct conn *, gr_conn_lookup(const struct conn_key *, conn_flow_t *));
 
 struct fake_mbuf {
 	struct rte_ipv4_hdr ipv4_hdr;
 	struct rte_mbuf mbuf;
 	uint8_t priv_data[GR_MBUF_PRIV_MAX_SIZE];
 };
+
+static struct iface iface;
 
 static void ipv4_init_default_mbuf(struct fake_mbuf *fake_mbuf) {
 	memset(fake_mbuf, 0, sizeof(struct fake_mbuf));
@@ -235,7 +264,8 @@ static void ipv4_init_default_mbuf(struct fake_mbuf *fake_mbuf) {
 	fake_mbuf->mbuf.ol_flags = 0;
 	fake_mbuf->mbuf.packet_type = RTE_PTYPE_L3_IPV4;
 
-	eth_input_mbuf_data(&fake_mbuf->mbuf)->domain = ETH_DOMAIN_OTHER;
+	eth_input_mbuf_data(&fake_mbuf->mbuf)->iface = &iface;
+	eth_input_mbuf_data(&fake_mbuf->mbuf)->domain = ETH_DOMAIN_LOCAL;
 }
 
 static void ip_input_invalid_mbuf_len(void **) {
@@ -299,6 +329,28 @@ static void ip_input_invalid_total_length(void **) {
 	ip_input_process(NULL, NULL, &obj, 1);
 }
 
+static void ip_input_conntrack_dnat(void **) {
+	struct fake_mbuf fake_mbuf;
+	void *obj = &fake_mbuf.mbuf;
+
+	ipv4_init_default_mbuf(&fake_mbuf);
+	fake_mbuf.ipv4_hdr.hdr_checksum = rte_ipv4_cksum(&fake_mbuf.ipv4_hdr);
+
+	struct nexthop nh = {
+		.flags = GR_NH_F_LOCAL,
+		.ipv4 = fake_mbuf.ipv4_hdr.dst_addr,
+	};
+	will_return(fib4_lookup, &nh);
+
+	iface.flags |= GR_IFACE_F_SNAT_DYNAMIC;
+	struct conn conn;
+	will_return(gr_conn_parse_key, true);
+	will_return(gr_conn_lookup, &conn);
+
+	expect_value(rte_node_enqueue_x1, next, DNAT44_DYNAMIC);
+	ip_input_process(NULL, NULL, &obj, 1);
+}
+
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(ip_input_invalid_mbuf_len),
@@ -306,6 +358,7 @@ int main(void) {
 		cmocka_unit_test(ip_input_invalid_version),
 		cmocka_unit_test(ip_input_invalid_ihl),
 		cmocka_unit_test(ip_input_invalid_total_length),
+		cmocka_unit_test(ip_input_conntrack_dnat),
 	};
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }

modules/infra/api/gr_infra.h
@@ -29,6 +29,7 @@ typedef enum : uint16_t {
 	GR_IFACE_F_ALLMULTI = GR_BIT16(2),
 	GR_IFACE_F_PACKET_TRACE = GR_BIT16(3),
 	GR_IFACE_F_SNAT_STATIC = GR_BIT16(4),
+	GR_IFACE_F_SNAT_DYNAMIC = GR_BIT16(5),
 } gr_iface_flags_t;
 
 // Interface state flags

modules/infra/cli/iface.c
@@ -284,7 +284,7 @@ static cmd_status_t iface_list(const struct gr_api_client *c, const struct ec_pn
 			SAFE_BUF(snprintf, sizeof(buf), " allmulti");
 		if (iface->flags & GR_IFACE_F_PACKET_TRACE)
 			SAFE_BUF(snprintf, sizeof(buf), " tracing");
-		if (iface->flags & GR_IFACE_F_SNAT_STATIC)
+		if (iface->flags & (GR_IFACE_F_SNAT_STATIC | GR_IFACE_F_SNAT_DYNAMIC))
 			SAFE_BUF(snprintf, sizeof(buf), " snat");
 		scols_line_set_data(line, 2, buf);
 
@@ -507,7 +507,7 @@ static cmd_status_t iface_show(const struct gr_api_client *c, const struct ec_pn
 		printf(" allmulti");
 	if (iface.flags & GR_IFACE_F_PACKET_TRACE)
 		printf(" tracing");
-	if (iface.flags & GR_IFACE_F_SNAT_STATIC)
+	if (iface.flags & (GR_IFACE_F_SNAT_STATIC | GR_IFACE_F_SNAT_DYNAMIC))
 		printf(" nat");
 	printf("\n");
 	printf("vrf: %u\n", iface.vrf_id);

modules/ip/datapath/ip_output.c
@@ -27,6 +27,7 @@ enum {
 	NO_ROUTE,
 	ERROR,
 	TOO_BIG,
+	DROP,
 	EDGE_COUNT,
 };
 
@@ -96,8 +97,14 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		edge = iface_type_edges[iface->type];
 		mbuf_data(mbuf)->iface = iface;
 
-		if (iface->flags & GR_IFACE_F_SNAT_STATIC)
-			snat44_static_process(iface, mbuf);
+		switch (snat44_process(iface, mbuf)) {
+		case NAT_VERDICT_CONTINUE:
+		case NAT_VERDICT_FINAL:
+			break;
+		case NAT_VERDICT_DROP:
+			edge = DROP;
+			break;
+		}
 
 		if (edge != ETH_OUTPUT)
 			goto next;
@@ -142,6 +149,7 @@ static struct rte_node_register output_node = {
 		[NO_ROUTE] = "ip_error_dest_unreach",
 		[ERROR] = "ip_output_error",
 		[TOO_BIG] = "ip_output_too_big",
+		[DROP] = "ip_output_drop",
 	},
 };
 
@@ -154,3 +162,4 @@ GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ip_output_error);
 GR_DROP_REGISTER(ip_output_too_big);
+GR_DROP_REGISTER(ip_output_drop);

modules/policy/api/gr_nat.h
@@ -5,6 +5,7 @@
 
 #include <gr_net_types.h>
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #define GR_NAT_MODULE 0x0bad
@@ -45,4 +46,39 @@ struct gr_dnat44_list_req {
 struct gr_dnat44_list_resp {
 	uint16_t n_policies;
 	struct gr_dnat44_policy policies[/* n_policies */];
+};
+
+// snat44 //////////////////////////////////////////////////////////////////////
+
+struct gr_snat44_policy {
+	uint16_t iface_id;
+	struct ip4_net net;
+	ip4_addr_t replace;
+};
+
+#define GR_SNAT44_ADD REQUEST_TYPE(GR_NAT_MODULE, 0x0011)
+
+struct gr_snat44_add_req {
+	struct gr_snat44_policy policy;
+	bool exist_ok;
+};
+
+// struct gr_snat44_add_resp { };
+
+#define GR_SNAT44_DEL REQUEST_TYPE(GR_NAT_MODULE, 0x0012)
+
+struct gr_snat44_del_req {
+	struct gr_snat44_policy policy;
+	bool missing_ok;
+};
+
+// struct gr_snat44_del_resp { };
+
+#define GR_SNAT44_LIST REQUEST_TYPE(GR_NAT_MODULE, 0x0013)
+
+// struct gr_snat44_list_req { };
+
+struct gr_snat44_list_resp {
+	uint16_t n_policies;
+	struct gr_snat44_policy policies[/* n_policies */];
 };

modules/policy/api/meson.build
@@ -3,6 +3,7 @@
 
 src += files(
   'dnat44.c',
+  'snat44.c',
 )
 
 api_headers += files(

modules/policy/api/snat44.c
@@ -1,0 +1,83 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Robin Jarry
+
+#include <gr_api.h>
+#include <gr_ip4_control.h>
+#include <gr_module.h>
+#include <gr_nat.h>
+#include <gr_nat_control.h>
+#include <gr_vec.h>
+
+static struct api_out snat44_add(const void *request, void ** /*response*/) {
+	const struct gr_snat44_add_req *req = request;
+	struct iface *iface;
+	int ret;
+
+	iface = iface_from_id(req->policy.iface_id);
+	if (iface == NULL)
+		return api_out(ENODEV, 0);
+
+	if (nh4_lookup(iface->vrf_id, req->policy.replace) == NULL)
+		return api_out(EADDRNOTAVAIL, 0);
+
+	ret = snat44_dynamic_policy_add(&req->policy);
+	if (ret == -EEXIST && req->exist_ok)
+		ret = 0;
+
+	return api_out(-ret, 0);
+}
+
+static struct api_out snat44_del(const void *request, void ** /*response*/) {
+	const struct gr_snat44_del_req *req = request;
+	int ret;
+
+	ret = snat44_dynamic_policy_del(&req->policy);
+	if (ret == -ENOENT && req->missing_ok)
+		ret = 0;
+
+	return api_out(-ret, 0);
+}
+
+static struct api_out snat44_list(const void * /*request*/, void **response) {
+	gr_vec struct gr_snat44_policy *policies;
+	struct gr_snat44_list_resp *resp;
+	size_t len;
+
+	policies = snat44_dynamic_policy_export();
+	len = sizeof(*resp) + gr_vec_len(policies) * sizeof(*policies);
+	resp = malloc(len);
+	if (resp == NULL) {
+		gr_vec_free(policies);
+		return api_out(ENOMEM, 0);
+	}
+
+	resp->n_policies = gr_vec_len(policies);
+	memcpy(resp->policies, policies, gr_vec_len(policies) * sizeof(*policies));
+	gr_vec_free(policies);
+
+	*response = resp;
+
+	return api_out(0, len);
+}
+
+static struct gr_api_handler add_handler = {
+	.name = "snat44 add",
+	.request_type = GR_SNAT44_ADD,
+	.callback = snat44_add,
+};
+static struct gr_api_handler del_handler = {
+	.name = "snat44 del",
+	.request_type = GR_SNAT44_DEL,
+	.callback = snat44_del,
+};
+static struct gr_api_handler list_handler = {
+	.name = "snat44 list",
+	.request_type = GR_SNAT44_LIST,
+	.callback = snat44_list,
+};
+
+RTE_INIT(_init) {
+	gr_register_api_handler(&add_handler);
+	gr_register_api_handler(&del_handler);
+	gr_register_api_handler(&list_handler);
+}

commit 912d36eec2e8c54bd20e5efd0bad10393b940dd9

ip: fix loopback input detection
In commit e5570d2ad6bf ("policy: add stateful dynamic source nat
support"), the assignment ip_output_mbuf_data(mbuf)->nh = nh was moved
earlier in the processing path. This assignment overwrites the mbuf
metadata that e points to, since eth_input_mbuf_data and
ip_output_mbuf_data share the same memory area.

As a result, accessing e->domain after this assignment reads garbage
data, causing incorrect routing decisions when checking whether a packet
should be sent to ip_output directly versus being forwarded.

Cache the domain value in a local variable before the mbuf metadata is
overwritten to ensure the correct routing decision is made.

Fixes: e5570d2ad6bf ("policy: add stateful dynamic source nat support")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Tested-by: Maxime Leroy <maxime@leroys.fr>
Acked-by: Maxime Leroy <maxime@leroys.fr>

modules/ip/datapath/ip_input.c
@@ -50,6 +50,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 	const struct nexthop *nh;
 	struct rte_ipv4_hdr *ip;
 	struct rte_mbuf *mbuf;
+	eth_domain_t domain;
 	rte_edge_t edge;
 	uint16_t i;
 
@@ -57,6 +58,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 		mbuf = objs[i];
 		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
 		e = eth_input_mbuf_data(mbuf);
+		domain = e->domain;
 		iface = e->iface;
 		nh = NULL;
 
@@ -111,7 +113,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 			goto next;
 		}
 
-		switch (e->domain) {
+		switch (domain) {
 		case ETH_DOMAIN_LOOPBACK:
 		case ETH_DOMAIN_LOCAL:
 			// Packet sent to our ethernet address.
@@ -143,7 +145,7 @@ ip_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, ui
 
 		// If the resolved next hop is local and the destination IP is ourselves,
 		// send to ip_local.
-		if (e->domain == ETH_DOMAIN_LOOPBACK)
+		if (domain == ETH_DOMAIN_LOOPBACK)
 			edge = OUTPUT;
 		else if (nh->type == GR_NH_T_L3) {
 			l3 = nexthop_info_l3(nh);