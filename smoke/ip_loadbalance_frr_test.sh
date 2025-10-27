d08da9d33474c9b1eb270a3f1cf0bfeffd0e9743
Robin Jarry <rjarry@redhat.com>
2025-10-27 15:22:28 +0100

    ecmp: fix smoke tests
    These tests were added at the same time than the rework of smoke test
    interface/netns names and were lost in translation.
    
    Fix the tests to use the same naming patterns.
    
    Fixes: 329388e22ecc ("smoke: homogenize netns names")
    Fixes: 99ea3c6bc205 ("smoke: add helper to create ports")
    Fixes: b3be6c9c3539 ("smoke: use deterministic interface names")
    Fixes: b99eb3a93a32 ("smoke: remove the need for stable mac addresses")
    Fixes: ba05a4cb538a ("smoke: always set lo up in network namespaces")
    Fixes: f08369509dad ("smoke: use different names for network namespaces")
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- smoke/ip_loadbalance_frr_test.sh
+++ smoke/ip_loadbalance_frr_test.sh
@@ -3,45 +3,34 @@
 # Copyright (c) 2025 Christophe Fontaine
 
 #
-#                         |   p1 (.1.2)
-#  [p0 ns0] --- grout --- | ns1          lo
-#                         |   p2 (.2.2)
-#
-#
+#                        |     p1 (.1.2)
+#  [p0 n0] --- grout --- | n1             lo
+#                        |     p2 (.2.2)
 #
 . $(dirname $0)/_init_frr.sh
 
-p0=${run_id}0
-p1=${run_id}1
-p2=${run_id}2
+create_interface p0
+create_interface p1
+create_interface p2
 
-create_interface $p0 f0:0d:ac:dc:00:00
-create_interface $p1 f0:0d:ac:dc:00:01
-create_interface $p2 f0:0d:ac:dc:00:02
+netns_add n0
+ip link set p0 netns n0
+ip -n n0 link set p0 up
+ip -n n0 addr add 172.16.0.2/24 dev p0
+ip -n n0 route add default via 172.16.0.1
 
-netns_add n-$p0
-	ip l set $p0 netns n-$p0
-	ip -n n-$p0 link set $p0 address ba:d0:ca:ca:00:00
-	ip -n n-$p0 link set $p0 up
-	ip -n n-$p0 link set lo up
-	ip -n n-$p0 addr add 172.16.0.2/24 dev $p0
-	ip -n n-$p0 route add default via 172.16.0.1
+netns_add n1
+ip link set p1 netns n1
+ip link set p2 netns n1
+ip -n n1 addr add 192.0.0.2/32 dev lo
+ip -n n1 link set p1 up
+ip -n n1 addr add 172.16.1.2/24 dev p1
+ip -n n1 link set p2 up
+ip -n n1 addr add 172.16.2.2/24 dev p2
 
-netns_add n-$p1
-	ip l set $p1 netns n-$p1
-	ip l set $p2 netns n-$p1
-	ip -n n-$p1 link set lo up
-	ip -n n-$p1 addr add 192.0.0.2/32 dev lo
-	ip -n n-$p1 link set $p1 address ba:d0:ca:ca:00:01
-	ip -n n-$p1 link set $p1 up
-	ip -n n-$p1 addr add 172.16.1.2/24 dev $p1
-	ip -n n-$p1 link set $p2 address ba:d0:ca:ca:00:02
-	ip -n n-$p1 link set $p2 up
-	ip -n n-$p1 addr add 172.16.2.2/24 dev $p2
-
-set_ip_address $p0 172.16.0.1/24
-set_ip_address $p1 172.16.1.1/24
-set_ip_address $p2 172.16.2.1/24
+set_ip_address p0 172.16.0.1/24
+set_ip_address p1 172.16.1.1/24
+set_ip_address p2 172.16.2.1/24
 
 set_ip_route 192.0.0.0/24 172.16.1.2
 # Can't use set_ip_route a second time
@@ -52,9 +41,9 @@ vtysh <<-EOF
 	ip route 192.0.0.0/24 172.16.2.2
 EOF
 
-ip -n n-$p1 route add default via 172.16.1.1
-ip netns exec n-$p0  ping 192.0.0.2 -i0.01 -c 3
+ip -n n1 route add default via 172.16.1.1
+ip netns exec n0 ping -i0.01 -c3 -n 192.0.0.2
 
-ip -n n-$p1 route del default
-ip -n n-$p1 route add default via 172.16.2.1
-ip netns exec n-$p0  ping 192.0.0.2 -i0.01 -c 3
+ip -n n1 route del default
+ip -n n1 route add default via 172.16.2.1
+ip netns exec n0 ping -i0.01 -c3 -n 192.0.0.2
