12859ea79171f50ed2ed6a3bbd18c0c2292b45cc
Christophe Fontaine <cfontain@redhat.com>
2025-10-29 09:04:46 +0100

    frr: add smoke test for isisd
    Using the l2 redirect capability, we're able to peer with a
    remote FRR instance router, using the following conf:
    
    ip router-id 10.0.0.1
    !
    interface p0
     ip address 10.0.0.1/24
     ip router isis test
     isis network point-to-point
    exit
    !
    router isis test
     net 49.0000.0000.0001.00
    exit
    
    fedora-linux-42# show isis neighbor
    Area test:
     System Id           Interface   L  State         Holdtime SNPA
     fedora-linux-42     p0          3  Up            28       2020.2020.2020
    
    fedora-linux-42# show ip route isis
    Codes: K - kernel route, C - connected, L - local, S - static,
           O - OSPF, I - IS-IS, B - BGP, T - Table, v - VNC,
           V - VNC-Direct, t - Table-Direct,
           > - selected route, * - FIB route, q - queued, r - rejected, b - backup
           t - trapped, o - offload failure
    
    IPv4 unicast VRF default:
    I   10.0.0.0/24 [115/20] via 10.0.0.2, p0 inactive, weight 1, 00:05:51
    I   192.0.0.0/24 [115/10] via 10.0.0.2, p0, weight 1, 00:05:51
    
    Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
    Reviewed-by: Maxime Leroy <maxime@leroys.fr>

---
+++ smoke/isis_frr_test.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Christophe Fontaine
+
+#
+#                                                       .--------------------.
+#                                                       |  netns "isis-peer" |
+#  .--------..------------.                             |       .-------.    |
+#  | zebra  ||   grout    |                             |       | isisd |    |
+#  '--------'|            |                             |       '-------'    |
+#  .-------. |      .------------.               .------------. .-------.    |
+#  | isisd | |      |     p0     |    net_tap    |    x-p0    | | zebra |    |
+#  '-------' |      |            +---------------+            | '-------'    |
+#      .----------. | 172.16.0.1 |               | 172.16.0.2 |.----------.  |
+#      | gr-loop0 | '------------'               '------------'|    lo    |  |
+#      '----------'       |                             |      |          |  |
+#           |      ping <------------------------------------> | 16.0.0.1 |  |
+#           |             |                             |      '----------'  |
+#           '-------------'                             '--------------------'
+
+
+. $(dirname $0)/_init_frr.sh
+
+create_interface p0
+set_ip_address p0 172.16.0.1/24
+
+# Configure Grout FRR instance
+vtysh <<-EOF
+configure terminal
+!
+ip router-id 172.16.0.1
+!
+interface p0
+	ip router isis smoke
+	isis network point-to-point
+exit
+!
+router isis smoke
+	net 49.0000.0000.0001.00
+	redistribute ipv4 static level-1
+	redistribute ipv4 static level-2
+exit
+!
+EOF
+
+start_frr_on_namespace isis-peer
+ip link set x-p0 netns isis-peer
+
+# Configure FRR ISIS peer router
+vtysh -N isis-peer <<-EOF
+configure terminal
+!
+ip router-id 172.16.0.2
+!
+interface lo
+	ip address 16.0.0.1/32
+exit
+!
+interface x-p0
+	ip address 172.16.0.2/24
+	ip router isis smoke
+	isis network point-to-point
+exit
+!
+router isis smoke
+	net 49.0000.0000.0002.00
+	redistribute ipv4 connected level-1
+exit
+!
+EOF
+
+# Wait for ISIS peer neighbor
+attempts=20
+while ! vtysh -c 'show isis neighbor json' | jq -e '.areas[0].circuits[0].state == "Up"' ; do
+	sleep 1
+	if [ "$attempts" -le 0 ]; then
+		fail "ISIS failed to connect to neighbor."
+	fi
+	attempts=$((attempts - 1))
+done
+
+# Wait for ISIS route exchange
+attempts=90
+while ! vtysh -c 'show ip route isis json' | jq -e '."16.0.0.1/32"' ; do
+	sleep 1
+	if [ "$attempts" -le 0 ]; then
+		fail "ISIS failed to get routes."
+	fi
+	attempts=$((attempts - 1))
+done
+
+grcli ping 16.0.0.1 count 3 delay 10
