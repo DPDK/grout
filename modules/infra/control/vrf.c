391f9fa0320451742889ea1d28a77e77c0131954
Maxime Leroy <maxime@leroys.fr>
2025-11-03 09:21:41 +0100

    infra: create VRF and default route for each loopback interface
    This commit introduces automatic VRF and route setup for local loopback
    interfaces created by Grout.
    
    When a new VRF is added in Grout, a corresponding Linux VRF interface is
    now created and linked with its dedicated loopback device.
    
    For each VRF vrf-X, a Linux VRF interface with the same name (vrf-X) is
    created with routing table ID 1000 + X.
    
    The associated loopback device gr-loopX is enslaved to this VRF.
    A default route is then added in table 1000 + X, pointing to gr-loopX,
    so that all traffic originating from the VRF is routed back through its
    local loopback.
    
    For the VRF 0 (i.e. gr-loop0), no Linux VRF interface is created;
    instead, the default route in the main table is updated to point to
    gr-loop0. This is intentional, as Grout should run inside a dedicated
    network namespace.
    
    This setup ensures correct handling of local traffic for daemons such as
    bgpd that listen on 0.0.0.0 and are not bound to a specific device.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/infra/control/vrf.c
+++ modules/infra/control/vrf.c
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Christophe Fontaine
 
+#include <gr_errno.h>
 #include <gr_event.h>
 #include <gr_iface.h>
 #include <gr_log.h>
+#include <gr_loopback.h>
 #include <gr_macro.h>
 #include <gr_module.h>
+#include <gr_netlink.h>
 #include <gr_string.h>
 #include <gr_vec.h>
 
+#include <linux/rtnetlink.h>
 #include <vrf_priv.h>
 
 struct vrf_info {
@@ -21,6 +25,96 @@ static struct vrf_info vrfs[GR_MAX_VRFS];
 
 struct iface *get_vrf_iface(uint16_t vrf_id) {
 	return vrfs[vrf_id].iface;
+}
+
+static int
+netlink_create_vrf_and_enslave(const char *vrf_name, uint32_t vrf_table, const char *loop_name) {
+	int ret;
+
+	ret = netlink_link_add_vrf(vrf_name, vrf_table);
+	if (ret < 0)
+		return ret;
+
+	ret = netlink_link_set_master(loop_name, vrf_name);
+	if (ret < 0)
+		return ret;
+
+	ret = netlink_link_set_admin_state(vrf_name, true);
+	if (ret < 0)
+		return ret;
+
+	ret = netlink_link_set_admin_state(loop_name, true);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static int netlink_delete_vrf_and_unslave(const char *vrf_name, const char *loop_name) {
+	int ret;
+
+	ret = netlink_link_set_master(loop_name, NULL);
+	if (ret < 0)
+		return ret;
+
+	ret = netlink_link_set_admin_state(loop_name, false);
+	if (ret < 0)
+		return ret;
+
+	return netlink_link_del_iface(vrf_name);
+}
+
+static uint32_t vrf_id_to_table_id(uint16_t vrf_id) {
+	if (vrf_id == 0)
+		return RT_TABLE_MAIN;
+	// Reserved values for table_id are 0, 252, 253, 254 and 255.
+	// Since we may have more than 252 VRFs, use an arbitrary value greater than 255.
+	return vrf_id + 1000;
+}
+
+static void netlink_vrf_add(const struct iface *loop_iface) {
+	uint32_t table_id = vrf_id_to_table_id(loop_iface->vrf_id);
+	const char *loop_name = loopback_get_tun_name(loop_iface);
+	int ret;
+
+	if (loop_iface->vrf_id) {
+		ret = netlink_create_vrf_and_enslave(loop_iface->name, table_id, loop_name);
+		if (ret < 0) {
+			LOG(WARNING,
+			    "create vrf %u for %s failed: %s",
+			    loop_iface->vrf_id,
+			    loop_iface->name,
+			    strerror(errno));
+			return;
+		}
+	}
+
+	ret = netlink_add_route(loop_name, table_id);
+	if (ret < 0)
+		LOG(WARNING, "add route on %s failed: %s", loop_iface->name, strerror(errno));
+}
+
+static void netlink_vrf_del(const struct iface *loop_iface) {
+	uint32_t table_id = vrf_id_to_table_id(loop_iface->vrf_id);
+	const char *loop_name = loopback_get_tun_name(loop_iface);
+	int ret;
+
+	if (loop_iface->vrf_id) {
+		ret = netlink_delete_vrf_and_unslave(loop_iface->name, loop_name);
+		if (ret < 0)
+			LOG(WARNING,
+			    "delete vrf %u for %s failed: %s",
+			    loop_iface->vrf_id,
+			    loop_iface->name,
+			    strerror(errno));
+	} else {
+		ret = netlink_del_route(loop_name, table_id);
+		if (ret < 0)
+			LOG(WARNING,
+			    "delete route on %s failed: %s",
+			    loop_iface->name,
+			    strerror(errno));
+	}
 }
 
 void vrf_incref(uint16_t vrf_id) {
@@ -36,6 +130,8 @@ void vrf_incref(uint16_t vrf_id) {
 			    strerror(errno));
 			return;
 		}
+
+		netlink_vrf_add(vrfs[vrf_id].iface);
 	}
 
 	vrfs[vrf_id].ref_count++;
@@ -46,6 +142,9 @@ void vrf_decref(uint16_t vrf_id) {
 		return;
 
 	if (vrfs[vrf_id].ref_count == 1) {
+		if (vrfs[vrf_id].iface != NULL)
+			netlink_vrf_del(vrfs[vrf_id].iface);
+
 		if (iface_loopback_delete(vrf_id) < 0) {
 			LOG(WARNING,
 			    "loopback for vrf %u cannot be deleted: %s",
