DPDK/grout

commit a1d2887396357e82a54fcb8736595b39d140a98e

port: implement interface callbacks for hardware configuration
Implement the iface_type callbacks for DPDK ethernet ports, allowing
configuration operations to be performed through the interface layer
instead of directly on port objects.

Ignore ENOSYS and EOPNOTSUPP errors from DPDK drivers when hardware
features are unavailable. Update interface flags based on actual
hardware state after configuration attempts to reflect what the driver
actually supports.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/port.c
@@ -146,6 +146,159 @@ int port_configure(struct iface_info_port *p, uint16_t n_txq_min) {
 	return 0;
 }
 
+static int port_up_down(struct iface *iface, bool up) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret;
+
+	if (up) {
+		ret = rte_eth_dev_set_link_up(p->port_id);
+		switch (ret) {
+		case 0:
+		case -ENOSYS:
+		case -EOPNOTSUPP:
+			break;
+		default:
+			return errno_log(-ret, "rte_eth_dev_set_link_up");
+		}
+		iface->flags |= GR_IFACE_F_UP;
+	} else {
+		ret = rte_eth_dev_set_link_down(p->port_id);
+		switch (ret) {
+		case 0:
+		case -ENOSYS:
+		case -EOPNOTSUPP:
+			break;
+		default:
+			return errno_log(-ret, "rte_eth_dev_set_link_down");
+		}
+		iface->flags &= ~GR_IFACE_F_UP;
+	}
+
+	return 0;
+}
+
+static int port_mac_set(struct iface *iface, const struct rte_ether_addr *mac) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret;
+
+	if (!rte_is_zero_ether_addr(mac)) {
+		struct rte_ether_addr mut_mac = *mac;
+		if ((ret = rte_eth_dev_default_mac_addr_set(p->port_id, &mut_mac)) < 0)
+			return errno_log(-ret, "rte_eth_dev_default_mac_addr_set");
+		p->mac = mut_mac;
+	} else if ((ret = rte_eth_macaddr_get(p->port_id, &p->mac)) < 0) {
+		return errno_log(-ret, "rte_eth_macaddr_get");
+	}
+
+	return 0;
+}
+
+static int port_promisc_set(struct iface *iface, bool enabled) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret;
+
+	if (enabled)
+		ret = rte_eth_promiscuous_enable(p->port_id);
+	else
+		ret = rte_eth_promiscuous_disable(p->port_id);
+
+	switch (ret) {
+	case 0:
+	case -ENOSYS:
+	case -EOPNOTSUPP:
+		break;
+	default:
+		return errno_log(-ret, "rte_eth_promiscuous_{en,dis}able");
+	}
+
+	if (rte_eth_promiscuous_get(p->port_id) == 1)
+		iface->flags |= GR_IFACE_F_PROMISC;
+	else
+		iface->flags &= ~GR_IFACE_F_PROMISC;
+
+	return 0;
+}
+
+static int port_allmulti_set(struct iface *iface, bool enabled) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret;
+
+	if (enabled)
+		ret = rte_eth_allmulticast_enable(p->port_id);
+	else
+		ret = rte_eth_allmulticast_disable(p->port_id);
+
+	switch (ret) {
+	case 0:
+	case -ENOSYS:
+	case -EOPNOTSUPP:
+		break;
+	default:
+		return errno_log(-ret, "rte_eth_allmulticast_{en,dis}able");
+	}
+
+	if (rte_eth_allmulticast_get(p->port_id) == 1)
+		iface->flags |= GR_IFACE_F_ALLMULTI;
+	else
+		iface->flags &= ~GR_IFACE_F_ALLMULTI;
+
+	return 0;
+}
+
+static int port_mtu_set(struct iface *iface, uint16_t mtu) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret;
+
+	if (mtu != 0) {
+		ret = rte_eth_dev_set_mtu(p->port_id, mtu);
+		switch (ret) {
+		case 0:
+		case -ENOSYS:
+		case -EOPNOTSUPP:
+			break;
+		default:
+			return errno_log(-ret, "rte_eth_dev_set_mtu");
+		}
+		iface->mtu = mtu;
+	} else {
+		if ((ret = rte_eth_dev_get_mtu(p->port_id, &iface->mtu)) < 0)
+			return errno_log(-ret, "rte_eth_dev_get_mtu");
+	}
+
+	gr_vec_foreach (struct iface *s, iface->subinterfaces)
+		s->mtu = iface->mtu;
+
+	return 0;
+}
+
+static int port_vlan_add(struct iface *iface, uint16_t vlan_id) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret = rte_eth_dev_vlan_filter(p->port_id, vlan_id, true);
+	switch (ret) {
+	case 0:
+	case -ENOSYS:
+	case -EOPNOTSUPP:
+		break;
+	default:
+		return errno_log(-ret, "rte_eth_dev_vlan_filter");
+	}
+	return 0;
+}
+
+static int port_vlan_del(struct iface *iface, uint16_t vlan_id) {
+	struct iface_info_port *p = (struct iface_info_port *)iface->info;
+	int ret = rte_eth_dev_vlan_filter(p->port_id, vlan_id, false);
+	switch (ret) {
+	case 0:
+	case -ENOSYS:
+	case -EOPNOTSUPP:
+		break;
+	default:
+		return errno_log(-ret, "rte_eth_dev_vlan_filter");
+	}
+	return 0;
+}
+
 static int iface_port_reconfig(
 	struct iface *iface,
 	uint64_t set_attrs,
@@ -702,6 +855,13 @@ static struct iface_type iface_type_port = {
 	.get_eth_addr = port_mac_get,
 	.add_eth_addr = port_mac_add,
 	.del_eth_addr = port_mac_del,
+	.set_eth_addr = port_mac_set,
+	.set_mtu = port_mtu_set,
+	.set_up_down = port_up_down,
+	.set_promisc = port_promisc_set,
+	.set_allmulti = port_allmulti_set,
+	.add_vlan = port_vlan_add,
+	.del_vlan = port_vlan_del,
 	.to_api = port_to_api,
 };
 

commit 039cc035190c0aa96de5714c3426faf8562889b2

port: stop port before changing MTU
Some DPDK drivers require the port to be stopped before the MTU can be
changed. Without this, rte_eth_dev_set_mtu() may fail or behave
unpredictably depending on the driver implementation.

Stop the port before calling rte_eth_dev_set_mtu() and restart it
afterward. Use RCU synchronization to ensure datapath workers observe
the started flag change before the port is actually stopped, preventing
them from attempting to receive or transmit packets during the MTU
change operation.

Fixes: a1d288739635 ("port: implement interface callbacks for hardware configuration")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/port.c
@@ -251,6 +251,10 @@ static int port_mtu_set(struct iface *iface, uint16_t mtu) {
 	int ret;
 
 	if (mtu != 0) {
+		p->started = false;
+		rte_rcu_qsbr_synchronize(gr_datapath_rcu(), RTE_QSBR_THRID_INVALID);
+		if ((ret = rte_eth_dev_stop(p->port_id)) < 0)
+			return errno_log(-ret, "rte_eth_dev_stop");
 		ret = rte_eth_dev_set_mtu(p->port_id, mtu);
 		switch (ret) {
 		case 0:
@@ -260,6 +264,9 @@ static int port_mtu_set(struct iface *iface, uint16_t mtu) {
 		default:
 			return errno_log(-ret, "rte_eth_dev_set_mtu");
 		}
+		if ((ret = rte_eth_dev_start(p->port_id)) < 0)
+			return errno_log(-ret, "rte_eth_dev_start");
+		p->started = true;
 		iface->mtu = mtu;
 	} else {
 		if ((ret = rte_eth_dev_get_mtu(p->port_id, &iface->mtu)) < 0)