DPDK/grout

commit cdfe4353139d31aca7c036a8726b38cab2873536

loopback: rebuild ip/ip6 header
We wrongly assumed that the data was still present when prepending
an ip header.
Rewrite the ip/ip6 header instead.

Fixes: e27fab5 ("ip/ip6: post tcp/udp packets to loopback interface")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/loopback.c
@@ -134,6 +134,12 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 
 	// packet sent from linux tun iface, no need to compute checksum;
 	mbuf->ol_flags = RTE_MBUF_F_RX_IP_CKSUM_GOOD;
+
+	// We can't call rte_net_get_ptype directly as we do not have an ethernet frame.
+	// An option would be to prepend/adjust every buffer, but let's set directly
+	// the information we need instead.
+	mbuf->packet_type = data[0] == 6 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
+
 	// Emulate ethernet input, required by ip(6)_input
 	e = eth_input_mbuf_data(mbuf);
 	e->iface = iface;

modules/l4/l4_redirect_loopback.c
@@ -16,6 +16,8 @@
 enum edges {
 	REDIRECT = 0,
 	NO_IFACE,
+	BAD_PROTO,
+	NO_HEADROOM,
 	EDGE_COUNT,
 };
 
@@ -25,24 +27,51 @@ static uint16_t redirect_loopback_process(
 	void **objs,
 	uint16_t nb_objs
 ) {
-	struct rte_ipv4_hdr *ip;
 	struct rte_mbuf *mbuf;
 	struct mbuf_data *d;
-	int edge = NO_IFACE;
+	rte_edge_t edge;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
+		edge = REDIRECT;
+
 		d = mbuf_data(mbuf);
 		d->iface = get_vrf_iface(d->iface->vrf_id);
-		if (d->iface)
-			edge = REDIRECT;
-		rte_pktmbuf_prepend(mbuf, sizeof(*ip));
+		if (!d->iface) {
+			edge = NO_IFACE;
+			goto next;
+		}
+
+		if (mbuf->packet_type & RTE_PTYPE_L3_IPV4) {
+			struct ip_local_mbuf_data *d = ip_local_mbuf_data(mbuf);
+			struct rte_ipv4_hdr *ip;
+			ip = (struct rte_ipv4_hdr *)rte_pktmbuf_prepend(mbuf, sizeof(*ip));
+			if (ip == NULL) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			ip_set_fields(ip, d);
+		} else if (mbuf->packet_type & RTE_PTYPE_L3_IPV6) {
+			struct ip6_local_mbuf_data *d = ip6_local_mbuf_data(mbuf);
+			struct rte_ipv6_hdr *ip;
+			ip = (struct rte_ipv6_hdr *)rte_pktmbuf_prepend(mbuf, sizeof(*ip));
+			if (ip == NULL) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			ip6_set_fields(ip, d->len, d->proto, &d->src, &d->dst);
+			ip->hop_limits = d->hop_limit;
+		} else {
+			edge = BAD_PROTO;
+		}
+
+next:
 		if (gr_mbuf_is_traced(mbuf)) {
 			gr_mbuf_trace_add(mbuf, node, 0);
 		}
+		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 
-	rte_node_enqueue(graph, node, edge, objs, nb_objs);
 	return nb_objs;
 }
 
@@ -53,6 +82,8 @@ static struct rte_node_register tcp_redirect_loopback_node = {
 	.next_nodes = {
 		[REDIRECT] = "loopback_output",
 		[NO_IFACE] = "no_loop_iface",
+		[BAD_PROTO] = "l4_bad_proto",
+		[NO_HEADROOM] = "error_no_headroom",
 	},
 };
 
@@ -63,6 +94,8 @@ static struct rte_node_register udp_redirect_loopback_node = {
 	.next_nodes = {
 		[REDIRECT] = "loopback_output",
 		[NO_IFACE] = "no_loop_iface",
+		[BAD_PROTO] = "l4_bad_proto",
+		[NO_HEADROOM] = "error_no_headroom",
 	},
 };
 
@@ -73,6 +106,8 @@ static struct rte_node_register sctp_redirect_loopback_node = {
 	.next_nodes = {
 		[REDIRECT] = "loopback_output",
 		[NO_IFACE] = "no_loop_iface",
+		[BAD_PROTO] = "l4_bad_proto",
+		[NO_HEADROOM] = "error_no_headroom",
 	},
 };
 
@@ -111,3 +146,4 @@ GR_NODE_REGISTER(info_udp_redirect);
 GR_NODE_REGISTER(info_sctp_redirect);
 
 GR_DROP_REGISTER(no_loop_iface);
+GR_DROP_REGISTER(l4_bad_proto);

commit 2c760d3f6f8c61b37ea12630fa979440bc9e1f2e

loopback: fix packet type of packet received from linux
The loopback code was checking `data[0] == 6` to decide whether a packet
was IPv6, but the IP version is stored in the upper 4 bits of the first
byte.

Use `(data[0] & 0xf0) == 0x60` to check the version field and correctly
set `mbuf->packet_type` to either `RTE_PTYPE_L3_IPV6` or `RTE_PTYPE_L3_IPV4`.

Fixes: cdfe4353139d ("loopback: rebuild ip/ip6 header")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/loopback.c
@@ -138,7 +138,7 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 	// We can't call rte_net_get_ptype directly as we do not have an ethernet frame.
 	// An option would be to prepend/adjust every buffer, but let's set directly
 	// the information we need instead.
-	mbuf->packet_type = data[0] == 6 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
+	mbuf->packet_type = (data[0] & 0xf0) == 0x60 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
 
 	// required by ip(6)_input
 	e = eth_input_mbuf_data(mbuf);