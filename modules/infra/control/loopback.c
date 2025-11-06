db93b874f23c22ca9f8b093f1b31301b2bb19c1c
Maxime Leroy <maxime@leroys.fr>
2025-11-06 09:52:07 +0100

    loopback: remove IFF_ONE_QUEUE flag to create tun device
    The IFF_ONE_QUEUE flag has had no functional effect since 2012.
    
    The tun driver always queues all packets on the device by default,
    making both modes (default and one-queue) behave the same. The flag
    is now only kept for userspace compatibility and can be safely removed.
    
    Link: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=5d097109257c
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
    Acked-by: Robin Jarry <rjarry@redhat.com>

--- modules/infra/control/loopback.c
+++ modules/infra/control/loopback.c
@@ -176,7 +176,7 @@ static int iface_loopback_init(struct iface *iface, const void * /* api_info */)
 
 	memset(&ifr, 0, sizeof(struct ifreq));
 	memccpy(ifr.ifr_name, iface->name, 0, IFNAMSIZ);
-	ifr.ifr_flags = IFF_TUN | IFF_POINTOPOINT | IFF_ONE_QUEUE;
+	ifr.ifr_flags = IFF_TUN | IFF_POINTOPOINT;
 
 	if ((ioctl_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		LOG(ERR, "socket(SOCK_DGRAM): %s", strerror(errno));
