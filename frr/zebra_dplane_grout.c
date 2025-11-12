DPDK/grout

commit 5e970e09a5891f68718630ee82b4183e567c63a8

frr: use tunsrc api
Now that grout exposes a tunsrc api, use it in FRR.

vtysh# show running-config
segment-routing
 srv6
  encapsulation
   source-address 3fff::1
 traffic-eng
 exit
exit
!

grout# show sr tunsrc
sr tunsrc addr 3fff::1

Suggested-by: Maxime Leroy <maxime@leroys.fr>
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

frr/zebra_dplane_grout.c
@@ -444,6 +444,9 @@ static enum zebra_dplane_result zd_grout_process_update(struct zebra_dplane_ctx 
 	case DPLANE_OP_NH_DELETE:
 		return grout_add_del_nexthop(ctx);
 
+	case DPLANE_OP_SRV6_ENCAP_SRCADDR_SET:
+		return grout_set_sr_tunsrc(ctx);
+
 	case DPLANE_OP_NONE:
 		return ZEBRA_DPLANE_REQUEST_SUCCESS;
 

frr/if_grout.c
@@ -15,6 +15,7 @@
 
 #include <gr_ip4.h>
 #include <gr_ip6.h>
+#include <gr_srv6.h>
 
 #include <zebra/interface.h>
 #include <zebra_dplane_grout.h>
@@ -264,6 +265,23 @@ enum zebra_dplane_result grout_add_del_address(struct zebra_dplane_ctx *ctx) {
 	}
 
 	if (grout_client_send_recv(req_type, req_len, &req, NULL) < 0)
+		return ZEBRA_DPLANE_REQUEST_FAILURE;
+
+	return ZEBRA_DPLANE_REQUEST_SUCCESS;
+}
+
+enum zebra_dplane_result grout_set_sr_tunsrc(struct zebra_dplane_ctx *ctx) {
+	const struct in6_addr *tunsrc_addr = dplane_ctx_get_srv6_encap_srcaddr(ctx);
+	struct gr_srv6_tunsrc_set_req req;
+
+	if (tunsrc_addr == NULL) {
+		if (IS_ZEBRA_DEBUG_DPLANE_GROUT)
+			zlog_debug("sr tunsrc set failed: SRv6 encap source address not set");
+		return ZEBRA_DPLANE_REQUEST_FAILURE;
+	}
+	memcpy(&req.addr, tunsrc_addr, sizeof(req.addr));
+
+	if (grout_client_send_recv(GR_SRV6_TUNSRC_SET, sizeof(req), &req, NULL) < 0)
 		return ZEBRA_DPLANE_REQUEST_FAILURE;
 
 	return ZEBRA_DPLANE_REQUEST_SUCCESS;

frr/if_grout.h
@@ -10,6 +10,7 @@
 #include <zebra/zebra_dplane.h>
 
 enum zebra_dplane_result grout_add_del_address(struct zebra_dplane_ctx *ctx);
+enum zebra_dplane_result grout_set_sr_tunsrc(struct zebra_dplane_ctx *ctx);
 
 void grout_interface_addr_dplane(struct gr_nexthop *gr_nh, bool new);
 void grout_link_change(struct gr_iface *gr_if, bool new, bool startup);

commit af3726ea693c22c2cfcac71e149764237eae29e6

frr: add missing debug string
Add missing string in the debug function 'gr_req_type_to_str'.

Fixes: 5e970e09a589 ("frr: use tunsrc api")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

frr/zebra_dplane_grout.c
@@ -315,6 +315,8 @@ static const char *gr_req_type_to_str(uint32_t e) {
 		return TOSTRING(GR_NH_ADD);
 	case GR_NH_DEL:
 		return TOSTRING(GR_NH_DEL);
+	case GR_INFRA_IFACE_GET:
+		return TOSTRING(GR_INFRA_IFACE_GET);
 	case GR_INFRA_IFACE_LIST:
 		return TOSTRING(GR_INFRA_IFACE_LIST);
 	case GR_IP4_ADDR_LIST:
@@ -327,6 +329,8 @@ static const char *gr_req_type_to_str(uint32_t e) {
 		return TOSTRING(GR_IP4_ROUTE_LIST);
 	case GR_IP6_ROUTE_LIST:
 		return TOSTRING(GR_IP6_ROUTE_LIST);
+	case GR_SRV6_TUNSRC_SET:
+		return TOSTRING(GR_SRV6_TUNSRC_SET);
 	default:
 		return "unknown";
 	}