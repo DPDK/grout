DPDK/grout

commit 4853d4d9b129dcee204a8c717d5bcc79699e6b7b

smoke: make private netns work for single tests
When running individual smoke tests, the netns isn't used. Add a stanza
at the top of _init.sh to ensure switching to the grout netns before
executing anything else.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

smoke/_init.sh
@@ -3,6 +3,12 @@
 
 set -e -o pipefail
 
+if [ -z "$(ip netns identify)" ]; then
+	set -x
+	ip netns add grout 2>/dev/null || :
+	exec ip netns exec grout "$0" "$@"
+fi
+
 : "${test_frr:=false}"
 
 if [ -n "$ZEBRA_DEBUG_DPLANE_GROUT" ]; then

smoke/run.sh
@@ -28,10 +28,6 @@ if [ -n "${builddir+x}" ] && \
 	test_frr=true
 fi
 
-ip netns del grout 2>/dev/null || :
-ip netns add grout
-trap "ip netns del grout" EXIT
-
 for script in $here/*_test.sh; do
 	name=$(basename $script)
 	case "$name" in
@@ -46,8 +42,8 @@ for script in $here/*_test.sh; do
 
 	{
 		echo "====================================================="
-		echo "+ ip netns exec grout $script $builddir"
-		if ! ip netns exec grout "$script" "$builddir"; then
+		echo "+ $script $builddir"
+		if ! "$script" "$builddir"; then
 			res=FAILED
 		fi
 		end=$(date +%s)

commit 5968e2e4c671398e9cb26c0a4cdc23f4916bd2c8

smoke: allow external debugging of network namespaces
Since recently, the smoke tests are executed in a "grout" netns which is
reused across test runs.

Smoke tests do create additional namespaces to act as routers connected
to grout.

The issue is that creating "nested" network namespaces with iproute2 is
not possible. The file created in /run/netns has a reference which is
unusable outside of the process that has created it.

 ~# ip netns
 Error: Peer netns reference is invalid.
 Error: Peer netns reference is invalid.
 grout
 ns-a
 ns-b

 ~# ip netns exec ns-b ip link show
 setting the network namespace "ns-b" failed: Invalid argument

The only way to have namespaces which are visible/accessible from
everywhere is to create them from the base namespace (the netns of PID
1).

Fixes: 4853d4d9b129 ("smoke: make private netns work for single tests")
Fixes: 4d5fa1c6b65b ("main,smoke: run in private netns")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Maxime Leroy <maxime@leroys.fr>

smoke/_init.sh
@@ -112,10 +112,10 @@ builddir=${1-}
 
 netns_add() {
 	local ns="$1"
-	ip netns add "$ns"
+	nsenter -t 1 -n -m ip netns add "$ns"
 	cat >> $tmp/cleanup <<EOF
-ip netns pids "$ns" | xargs -r kill --timeout 500 KILL
-ip netns del "$ns"
+nsenter -t 1 -n -m ip netns pids "$ns" | xargs -r kill --timeout 500 KILL
+nsenter -t 1 -n -m ip netns del "$ns"
 EOF
 	ip -n "$ns" link set lo up
 }

smoke/_init_frr.sh
@@ -266,14 +266,14 @@ EOF
 	fi
 	tailpid=$(pgrep -g0 tail | tail -n1)
 
-	ip netns add $namespace
+	nsenter -t 1 -n -m ip netns add $namespace
 	frrinit.sh start $namespace
 
 	cat >> $tmp/cleanup <<EOF
 frrinit.sh stop $namespace
 kill $tailpid
-ip netns pids $namespace | xargs -r kill --timeout 500 KILL
-ip netns del $namespace
+nsenter -t 1 -n -m ip netns pids $namespace | xargs -r kill --timeout 500 KILL
+nsenter -t 1 -n -m ip netns del $namespace
 EOF
 
 	SECONDS=0