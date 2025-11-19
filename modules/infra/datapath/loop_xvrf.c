8e31c2f550988ce67e7b87cbbbda5a41b7160c25
Maxime Leroy <maxime@leroys.fr>
2025-11-19 18:23:36 +0100

    xvrf: simplify VRF interface retrieval
    ip_output/ip6_output already performs:
        iface = iface_from_id(nh->iface_id);
        mbuf_data(mbuf)->iface = iface;
    
    xvrf_loop_process was doing this again unnecessarily. Remove the
    redundant code.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Reviewed-by: Robin Jarry <rjarry@redhat.com>

--- modules/infra/datapath/loop_xvrf.c
+++ modules/infra/datapath/loop_xvrf.c
@@ -13,7 +13,6 @@
 enum {
 	IP_INPUT,
 	IP6_INPUT,
-	INVALID_IFACE,
 	EDGE_COUNT,
 };
 
@@ -30,38 +29,28 @@ static uint16_t
 loop_xvrf_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_input_mbuf_data *eth_data;
 	struct iface_stats *stats;
-	struct iface *loop_iface;
 	struct rte_mbuf *m;
 	rte_edge_t edge;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		m = objs[i];
 
-		if (m->packet_type & RTE_PTYPE_L3_IPV4) {
+		if (m->packet_type & RTE_PTYPE_L3_IPV4)
 			edge = IP_INPUT;
-			loop_iface = iface_from_id(ip_output_mbuf_data(m)->nh->iface_id);
-		} else {
+		else
 			edge = IP6_INPUT;
-			loop_iface = iface_from_id(ip6_output_mbuf_data(m)->nh->iface_id);
-		}
-		if (loop_iface == NULL || loop_iface->type != GR_IFACE_TYPE_LOOPBACK) {
-			edge = INVALID_IFACE; // should not happens
-			goto next;
-		}
 
 		eth_data = eth_input_mbuf_data(m);
-		eth_data->iface = loop_iface;
 		eth_data->domain = ETH_DOMAIN_LOCAL;
 
 		// XXX: increment tx stats of gr-loopX on initial vrf
-		stats = iface_get_stats(rte_lcore_id(), loop_iface->id);
+		stats = iface_get_stats(rte_lcore_id(), eth_data->iface->id);
 		stats->rx_packets += 1;
 		stats->rx_bytes += rte_pktmbuf_pkt_len(m);
-next:
-		if (gr_mbuf_is_traced(m)
-		    || (loop_iface && loop_iface->flags & GR_IFACE_F_PACKET_TRACE)) {
+
+		if (gr_mbuf_is_traced(m) || (eth_data->iface->flags & GR_IFACE_F_PACKET_TRACE)) {
 			struct trace_vrf_data *t = gr_mbuf_trace_add(m, node, sizeof(*t));
-			t->vrf_id = loop_iface ? loop_iface->vrf_id : GR_VRF_ID_ALL;
+			t->vrf_id = eth_data->iface->vrf_id;
 		}
 
 		rte_node_enqueue_x1(graph, node, edge, m);
@@ -81,7 +70,6 @@ static struct rte_node_register loop_xvrf_node = {
 	.next_nodes = {
 		[IP_INPUT] = "ip_input",
 		[IP6_INPUT] = "ip6_input",
-		[INVALID_IFACE] = "loop_iface_invalid",
 	},
 };
 
@@ -92,5 +80,3 @@ static struct gr_node_info info = {
 };
 
 GR_NODE_REGISTER(info);
-
-GR_DROP_REGISTER(loop_iface_invalid);
