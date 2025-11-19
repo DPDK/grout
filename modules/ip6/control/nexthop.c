DPDK/grout

commit 9cb61fd787fa4ad9d89f669892c30ca90b2af2c2

ip6_output: handle unresolved nexthops in control plane
Do not modify nexthop objects in the data plane. It requires acquiring
locks and also modifying the routing table. It can lead to races between
multiple worker threads.

Instead, when a nexthop is unreachable in ip_output, send the mbuf to
the control plane thread and do whatever is required there:

* Possibly create a new nexthop for prefix routes and create a new /128
  route that points to it.
* Hold the packet to the nexthop waiting for NDP resolution.
* Send an NDP request probe (if one hasn't already been sent earlier).

Change all nexthop references to const in the datapath to ensure they
cannot be modified.

Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/nexthop.c
@@ -2,6 +2,8 @@
 // Copyright (c) 2024 Robin Jarry
 
 #include <gr_api.h>
+#include <gr_control_input.h>
+#include <gr_control_output.h>
 #include <gr_iface.h>
 #include <gr_ip6.h>
 #include <gr_ip6_control.h>
@@ -32,6 +34,80 @@ ip6_nexthop_new(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *
 
 struct nexthop *ip6_nexthop_lookup(uint16_t vrf_id, const struct rte_ipv6_addr *ip) {
 	return nexthop_lookup(nh_pool, vrf_id, ip);
+}
+
+static control_input_t ip6_output_node;
+
+void ip6_nexthop_unreachable_cb(struct rte_mbuf *m) {
+	struct rte_ipv6_hdr *ip = rte_pktmbuf_mtod(m, struct rte_ipv6_hdr *);
+	struct rte_ipv6_addr dst = ip->dst_addr;
+	struct nexthop *nh;
+
+	nh = ip6_route_lookup(control_output_mbuf_data(m)->iface->vrf_id, &dst);
+	if (nh == NULL)
+		goto free; // route to dst has disappeared
+
+	if (nh->flags & GR_NH_F_LINK && !rte_ipv6_addr_eq(&dst, &nh->ipv6)) {
+		// The resolved nexthop is associated with a "connected" route.
+		// We currently do not have an explicit route entry for this
+		// destination IP.
+		struct nexthop *remote = ip6_nexthop_lookup(nh->vrf_id, &ip->dst_addr);
+
+		if (remote == NULL) {
+			// No existing nexthop for this IP, create one.
+			remote = ip6_nexthop_new(nh->vrf_id, nh->iface_id, &ip->dst_addr);
+		} else if (remote->flags & GR_NH_F_GATEWAY && remote->iface_id == 0) {
+			// Gateway route with uninitialized destination.
+			// Now, we can at least know what is the output interface.
+			remote->iface_id = nh->iface_id;
+		}
+
+		if (remote == NULL) {
+			LOG(ERR, "cannot allocate nexthop: %s", strerror(errno));
+			goto free;
+		}
+		if (remote->iface_id != nh->iface_id)
+			ABORT(IP6_F " nexthop lookup gives wrong interface", &ip);
+
+		// Create an associated /128 route so that next packets take it
+		// in priority with a single route lookup.
+		if (ip6_route_insert(nh->vrf_id, &ip->dst_addr, RTE_IPV6_MAX_DEPTH, remote) < 0) {
+			LOG(ERR, "failed to insert route: %s", strerror(errno));
+			goto free;
+		}
+		nh = remote;
+	}
+
+	if (nh->flags & GR_NH_F_REACHABLE) {
+		// The nexthop may have become reachable while the packet was
+		// passed from the datapath to here. Re-send it to datapath.
+		struct ip6_output_mbuf_data *d = ip6_output_mbuf_data(m);
+		d->nh = nh;
+		if (post_to_stack(ip6_output_node, m) < 0) {
+			LOG(ERR, "post_to_stack: %s", strerror(errno));
+			goto free;
+		}
+		return;
+	}
+
+	if (nh->held_pkts_num < NH_MAX_HELD_PKTS) {
+		queue_mbuf_data(m)->next = NULL;
+		if (nh->held_pkts_head == NULL)
+			nh->held_pkts_head = m;
+		else
+			queue_mbuf_data(nh->held_pkts_tail)->next = m;
+		nh->held_pkts_tail = m;
+		nh->held_pkts_num++;
+		if (!(nh->flags & GR_NH_F_PENDING)) {
+			ip6_nexthop_solicit(nh);
+			nh->flags |= GR_NH_F_PENDING;
+		}
+		return;
+	} else {
+		LOG(DEBUG, IP4_F " hold queue full", &dst);
+	}
+free:
+	rte_pktmbuf_free(m);
 }
 
 static struct api_out nh6_add(const void *request, void ** /*response*/) {
@@ -143,6 +219,8 @@ static void nh6_init(struct event_base *ev_base) {
 	nh_pool = nh_pool_new(AF_INET6, ev_base, &opts);
 	if (nh_pool == NULL)
 		ABORT("nh_pool_new(AF_INET6) failed");
+
+	ip6_output_node = gr_control_input_register_handler("ip6_output", true);
 }
 
 static void nh6_fini(struct event_base *) {

modules/ip6/control/gr_ip6_control.h
@@ -24,6 +24,8 @@
 struct nexthop *ip6_nexthop_lookup(uint16_t vrf_id, const struct rte_ipv6_addr *);
 struct nexthop *ip6_nexthop_new(uint16_t vrf_id, uint16_t iface_id, const struct rte_ipv6_addr *);
 
+void ip6_nexthop_unreachable_cb(struct rte_mbuf *m);
+
 int ip6_route_insert(uint16_t vrf_id, const struct rte_ipv6_addr *, uint8_t prefixlen, struct nexthop *);
 int ip6_route_delete(uint16_t vrf_id, const struct rte_ipv6_addr *, uint8_t prefixlen);
 void ip6_route_cleanup(struct nexthop *);

modules/ip6/datapath/gr_ip6_datapath.h
@@ -17,7 +17,7 @@
 
 #include <stdint.h>
 
-GR_MBUF_PRIV_DATA_TYPE(ip6_output_mbuf_data, { struct nexthop *nh; });
+GR_MBUF_PRIV_DATA_TYPE(ip6_output_mbuf_data, { const struct nexthop *nh; });
 
 GR_MBUF_PRIV_DATA_TYPE(ndp_mbuf_data, {
 	struct nexthop *local;

modules/ip6/datapath/ip6_hold.c
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Robin Jarry
+
+#include <gr_control_output.h>
+#include <gr_graph.h>
+#include <gr_ip6_control.h>
+#include <gr_mbuf.h>
+
+#include <rte_graph_worker.h>
+#include <rte_mbuf.h>
+
+enum {
+	CONTROL = 0,
+	EDGE_COUNT,
+};
+
+static uint16_t
+ip6_hold_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	struct control_output_mbuf_data *d;
+	struct rte_mbuf *mbuf;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		mbuf = objs[i];
+		d = control_output_mbuf_data(mbuf);
+		d->callback = ip6_nexthop_unreachable_cb;
+		if (gr_mbuf_is_traced(mbuf))
+			gr_mbuf_trace_add(mbuf, node, 0);
+		rte_node_enqueue_x1(graph, node, CONTROL, mbuf);
+	}
+
+	return nb_objs;
+}
+
+static struct rte_node_register node = {
+	.name = "ip6_hold",
+	.process = ip6_hold_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[CONTROL] = "control_output",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+};
+
+GR_NODE_REGISTER(info);

modules/ip6/datapath/ip6_output.c
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Robin Jarry
 
-#include <gr_datapath.h>
 #include <gr_eth.h>
 #include <gr_graph.h>
 #include <gr_iface.h>
 #include <gr_ip6.h>
-#include <gr_ip6_control.h>
 #include <gr_ip6_datapath.h>
 #include <gr_log.h>
 #include <gr_mbuf.h>
@@ -21,9 +19,9 @@
 
 enum {
 	ETH_OUTPUT = 0,
+	HOLD,
 	DEST_UNREACH,
 	ERROR,
-	QUEUE_FULL,
 	EDGE_COUNT,
 };
 
@@ -38,47 +36,13 @@ void ip6_output_register_interface(uint16_t iface_type_id, const char *next_node
 	edges[iface_type_id] = gr_node_attach_parent("ip6_output", next_node);
 }
 
-typedef enum {
-	OK_TO_SEND,
-	HELD,
-	HOLD_QUEUE_FULL,
-} hold_status_t;
-
-static inline hold_status_t maybe_hold_packet(struct nexthop *nh, struct rte_mbuf *mbuf) {
-	struct rte_ipv6_hdr *ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv6_hdr *);
-	hold_status_t status;
-
-	if (nh->flags & GR_NH_F_REACHABLE || rte_ipv6_addr_is_mcast(&ip->dst_addr)) {
-		status = OK_TO_SEND;
-	} else if (nh->held_pkts_num < NH_MAX_HELD_PKTS) {
-		queue_mbuf_data(mbuf)->next = NULL;
-		rte_spinlock_lock(&nh->lock);
-		if (nh->held_pkts_head == NULL)
-			nh->held_pkts_head = mbuf;
-		else
-			queue_mbuf_data(nh->held_pkts_tail)->next = mbuf;
-		nh->held_pkts_tail = mbuf;
-		nh->held_pkts_num++;
-		rte_spinlock_unlock(&nh->lock);
-		if (!(nh->flags & GR_NH_F_PENDING)) {
-			ip6_nexthop_solicit(nh);
-			nh->flags |= GR_NH_F_PENDING;
-		}
-		status = HELD;
-	} else {
-		status = HOLD_QUEUE_FULL;
-	}
-
-	return status;
-}
-
 static uint16_t
 ip6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_output_mbuf_data *eth_data;
 	const struct iface *iface;
+	const struct nexthop *nh;
 	struct rte_ipv6_hdr *ip;
 	struct rte_mbuf *mbuf;
-	struct nexthop *nh;
 	uint16_t i, sent;
 	rte_edge_t edge;
 
@@ -105,46 +69,12 @@ ip6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 		if (edge != ETH_OUTPUT)
 			goto next;
 
-		if (nh->flags & GR_NH_F_LINK && !rte_ipv6_addr_is_mcast(&ip->dst_addr)
-		    && !rte_ipv6_addr_eq(&ip->dst_addr, &nh->ipv6)) {
-			// The resolved next hop is associated with a "connected" route.
-			// We currently do not have an explicit entry for this destination IP.
-			// Create a new next hop and its associated /128 route so that next
-			// packets take it in priority with a single route lookup.
-			struct nexthop *remote = ip6_nexthop_lookup(nh->vrf_id, &ip->dst_addr);
-
-			if (remote == NULL)
-				remote = ip6_nexthop_new(nh->vrf_id, nh->iface_id, &ip->dst_addr);
-			else if (remote->flags & GR_NH_F_GATEWAY && remote->iface_id == 0)
-				remote->iface_id = nh->iface_id;
-
-			if (remote == NULL) {
-				edge = ERROR;
-				goto next;
-			}
-
-			assert(remote->iface_id == nh->iface_id);
-			if (ip6_route_insert(nh->vrf_id, &ip->dst_addr, RTE_IPV6_MAX_DEPTH, remote)
-			    < 0) {
-				edge = ERROR;
-				goto next;
-			}
-			ip6_output_mbuf_data(mbuf)->nh = remote;
-			nh = remote;
-		}
-
-		switch (maybe_hold_packet(nh, mbuf)) {
-		case HELD:
-			// The packet was stored in the next hop hold queue to be flushed upon
-			// reception of an NDP request or reply from the destination IP.
-			continue;
-		case HOLD_QUEUE_FULL:
-			//
-			edge = QUEUE_FULL;
+		if (!rte_ipv6_addr_is_mcast(&ip->dst_addr)
+		    && (!(nh->flags & GR_NH_F_REACHABLE)
+			|| (nh->flags & GR_NH_F_LINK && !rte_ipv6_addr_eq(&ip->dst_addr, &nh->ipv6))
+		    )) {
+			edge = HOLD;
 			goto next;
-		case OK_TO_SEND:
-			// Next hop is reachable.
-			break;
 		}
 
 		// Prepare ethernet layer info.
@@ -172,9 +102,9 @@ static struct rte_node_register output_node = {
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
 		[ETH_OUTPUT] = "eth_output",
+		[HOLD] = "ip6_hold",
 		[ERROR] = "ip6_output_error",
 		[DEST_UNREACH] = "ip6_error_dest_unreach",
-		[QUEUE_FULL] = "ndp_queue_full",
 	},
 };
 
@@ -186,4 +116,3 @@ static struct gr_node_info info = {
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ip6_output_error);
-GR_DROP_REGISTER(ndp_queue_full);

modules/ip6/datapath/meson.build
@@ -6,6 +6,7 @@ src += files(
   'icmp6_output.c',
   'ip6_error.c',
   'ip6_forward.c',
+  'ip6_hold.c',
   'ip6_input.c',
   'ip6_local.c',
   'ip6_output.c',

commit c3e60ee09514de7cedc48cd008941305a2193301

ip6: fix debug message
Fix debug message using IP4_F for an IPv6 address.

Fixes: 9cb61fd787fa ("ip6_output: handle unresolved nexthops in control plane")
Reported-by: coderabbitai
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/nexthop.c
@@ -113,7 +113,7 @@ void nh6_unreachable_cb(struct rte_mbuf *m) {
 		}
 		return;
 	} else {
-		LOG(DEBUG, IP4_F " hold queue full", &dst);
+		LOG(DEBUG, IP6_F " hold queue full", dst);
 	}
 free:
 	rte_pktmbuf_free(m);