DPDK/grout

commit d0de3b2361809c2b8c368d36ffef7a6d3bc2a4c6

loopback: add loopback interface
Add loopback interface, backed by a tun device.
One (1) loopback interface will be created for each VRF, so we
do not need to add an explicit API to create/delete this interface.

Packets from a loopback interface must not be polled by any dataplane thread.
Use control_(input|output) nodes to exchange packets between the dataplane
and the control plane.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/loopback.c
@@ -1,0 +1,278 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr.h>
+#include <gr_control_input.h>
+#include <gr_control_output.h>
+#include <gr_eth.h>
+#include <gr_iface.h>
+#include <gr_infra.h>
+#include <gr_log.h>
+#include <gr_loopback.h>
+#include <gr_mempool.h>
+#include <gr_module.h>
+
+#include <event2/event.h>
+#include <rte_errno.h>
+#include <rte_malloc.h>
+
+#include <fcntl.h>
+#include <linux/if_tun.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#define TUN_TAP_DEV_PATH "/dev/net/tun"
+
+static struct rte_mempool *loopback_pool;
+static struct event_base *ev_base;
+
+struct iface_info_loopback {
+	int fd;
+	struct event *ev;
+};
+
+static void finalize_fd(struct event *ev, void * /*priv*/) {
+	int fd = event_get_fd(ev);
+	if (fd >= 0)
+		close(fd);
+}
+
+void loopback_tx(struct rte_mbuf *m) {
+	struct mbuf_data *d = mbuf_data(m);
+	struct iface_info_loopback *lo;
+	struct iovec iov[2];
+	struct tun_pi pi;
+	char *data;
+
+	lo = (struct iface_info_loopback *)d->iface->info;
+	if (rte_pktmbuf_linearize(m) == 0) {
+		data = rte_pktmbuf_mtod(m, char *);
+	} else {
+		data = rte_malloc(NULL, rte_pktmbuf_pkt_len(m), 0);
+		if (data == NULL) {
+			LOG(ERR, "rte_malloc failed %s", rte_strerror(rte_errno));
+			goto end;
+		}
+		// with a non-contiguous mbuf, rte_pktmbuf_read returns a pointer
+		// to the user provided buffer.
+		rte_pktmbuf_read(m, 0, rte_pktmbuf_pkt_len(m), data);
+	}
+	pi.flags = 0;
+	if ((data[0] & 0xf0) == 0x40)
+		pi.proto = RTE_BE16(RTE_ETHER_TYPE_IPV4);
+	else if ((data[0] & 0xf0) == 0x60)
+		pi.proto = RTE_BE16(RTE_ETHER_TYPE_IPV6);
+	else {
+		LOG(ERR, "Bad proto: 0x%x - drop packet", data[0]);
+		goto end;
+	}
+	// Do not retry even in case of  if EAGAIN || EWOULDBLOCK
+	// If the tun device queue is full, something really bad is
+	// already happening on the management plane side.
+	iov[0].iov_base = &pi;
+	iov[0].iov_len = sizeof(pi);
+	iov[1].iov_base = data;
+	iov[1].iov_len = rte_pktmbuf_pkt_len(m);
+
+	if (writev(lo->fd, iov, ARRAY_DIM(iov)) < 0) {
+		// The user messed up and removed gr-loopX
+		// release resources on our side to try to recover
+		if (errno == EBADFD) {
+			iface_destroy(d->iface->id);
+		}
+		LOG(ERR, "write to tun device failed %s", strerror(errno));
+	}
+
+end:
+	if (!rte_pktmbuf_is_contiguous(m))
+		rte_free(data);
+	rte_pktmbuf_free(m);
+}
+
+static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
+	struct eth_input_mbuf_data *e;
+	struct iface_info_loopback *lo;
+	struct iface *iface = ev_iface;
+	struct rte_mbuf *mbuf;
+	size_t read_len;
+	size_t len;
+	char *data;
+
+	lo = (struct iface_info_loopback *)iface->info;
+
+	if (reason & EV_CLOSED) {
+		// The user messed up and removed gr-loopX
+		LOG(ERR, "tun device %s deleted", iface->name);
+		iface_destroy(iface->id);
+		return;
+	}
+
+	mbuf = rte_pktmbuf_alloc(loopback_pool);
+	if (!mbuf) {
+		LOG(ERR, "rte_pktmbuf_alloc %s", rte_strerror(rte_errno));
+		goto err;
+	}
+
+	read_len = iface->mtu + sizeof(struct tun_pi);
+	if ((data = rte_pktmbuf_append(mbuf, read_len)) == NULL) {
+		LOG(ERR, "rte_pktmbuf_alloc %s", rte_strerror(rte_errno));
+		goto err;
+	}
+
+	if ((len = read(lo->fd, data, read_len)) <= 0) {
+		if (errno == EAGAIN || errno == EWOULDBLOCK)
+			return;
+		LOG(ERR, "read from tun device %s failed %s", iface->name, strerror(errno));
+		goto err;
+	}
+
+	rte_pktmbuf_trim(mbuf, read_len - len);
+
+	// packet sent from linux tun iface, no need to compute checksum;
+	mbuf->ol_flags = RTE_MBUF_F_RX_IP_CKSUM_GOOD;
+	// Emulate ethernet input, required by ip(6)_input
+	e = eth_input_mbuf_data(mbuf);
+	e->iface = iface;
+	e->eth_dst = ETH_DST_LOCAL;
+
+	post_to_stack(loopback_get_control_id(), mbuf);
+	return;
+
+err:
+	rte_pktmbuf_free(mbuf);
+}
+
+struct iface *iface_loopback_create(uint16_t vrf_id) {
+	char ifname[64];
+	snprintf(ifname, sizeof(ifname), "gr-loop%d", vrf_id);
+	return iface_create(GR_IFACE_TYPE_LOOPBACK, 0, 1500, vrf_id, ifname, NULL);
+}
+
+int iface_loopback_delete(uint16_t vrf_id) {
+	const struct iface *i = NULL;
+	while ((i = iface_next(GR_IFACE_TYPE_LOOPBACK, i)) != NULL)
+		if (i->vrf_id == vrf_id)
+			return iface_destroy(i->id);
+
+	return errno_set(ENODEV);
+}
+
+static int iface_loopback_init(struct iface *iface, const void * /* api_info */) {
+	struct iface_info_loopback *lo = (struct iface_info_loopback *)iface->info;
+	struct ifreq ifr;
+	int ioctl_sock;
+	int err_save;
+	int flags;
+
+	memset(&ifr, 0, sizeof(struct ifreq));
+	memccpy(ifr.ifr_name, iface->name, 0, IFNAMSIZ);
+	ifr.ifr_flags = IFF_TUN | IFF_POINTOPOINT | IFF_ONE_QUEUE;
+
+	if ((ioctl_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		LOG(ERR, "socket %s", strerror(errno));
+		goto err;
+	}
+
+	if ((lo->fd = open(TUN_TAP_DEV_PATH, O_RDWR)) < 0) {
+		LOG(ERR, "open %s", strerror(errno));
+		goto err;
+	}
+
+	if (ioctl(lo->fd, TUNSETIFF, &ifr) < 0) {
+		LOG(ERR, "ioctl %s", strerror(errno));
+		goto err;
+	}
+
+	flags = fcntl(lo->fd, F_GETFL);
+	if (flags == -1) {
+		LOG(ERR, "fnctl %s", strerror(errno));
+		goto err;
+	}
+
+	flags |= O_NONBLOCK;
+	if (fcntl(lo->fd, F_SETFL, flags) < 0) {
+		LOG(ERR, "fnctl %s", strerror(errno));
+		goto err;
+	}
+
+	if (ioctl(ioctl_sock, SIOCGIFFLAGS, &ifr) < 0) {
+		LOG(ERR, "ioctl %s", strerror(errno));
+		goto err;
+	}
+
+	ifr.ifr_flags |= IFF_UP;
+	if (ioctl(ioctl_sock, SIOCSIFFLAGS, &ifr) < 0) {
+		LOG(ERR, "ioctl %s", strerror(errno));
+		goto err;
+	}
+
+	iface->flags = GR_IFACE_F_UP;
+	iface->state = GR_IFACE_S_RUNNING;
+	lo->ev = event_new(
+		ev_base,
+		lo->fd,
+		EV_READ | EV_CLOSED | EV_PERSIST | EV_FINALIZE,
+		iface_loopback_poll,
+		iface
+	);
+
+	if (lo->ev == NULL || event_add(lo->ev, NULL) < 0) {
+		event_free(lo->ev);
+		goto err;
+	}
+	close(ioctl_sock);
+	return 0;
+
+err:
+	err_save = errno;
+	if (lo->fd > 0)
+		close(lo->fd);
+	if (ioctl_sock > 0)
+		close(ioctl_sock);
+	return errno_set(err_save);
+	;
+}
+
+static int iface_loopback_fini(struct iface *iface) {
+	struct iface_info_loopback *lo = (struct iface_info_loopback *)iface->info;
+	event_free_finalize(0, lo->ev, finalize_fd);
+	return 0;
+}
+
+static void loopback_module_init(struct event_base *base) {
+	loopback_pool = gr_pktmbuf_pool_get(SOCKET_ID_ANY, RTE_GRAPH_BURST_SIZE);
+	if (!loopback_pool)
+		ABORT("pktmbuf_pool returned NULL");
+	ev_base = base;
+}
+
+static void loopback_module_fini(struct event_base *) {
+	gr_pktmbuf_pool_release(loopback_pool, RTE_GRAPH_BURST_SIZE);
+}
+
+static void iface_loopback_to_api(void * /* info */, const struct iface * /* iface */) { }
+
+static struct iface_type iface_type_loopback = {
+	.id = GR_IFACE_TYPE_LOOPBACK,
+	.name = "loopback",
+	.info_size = sizeof(struct iface_info_loopback),
+	.init = iface_loopback_init,
+	.fini = iface_loopback_fini,
+	.to_api = iface_loopback_to_api,
+};
+
+static struct gr_module loopback_module = {
+	.name = "iface loopback",
+	.init = loopback_module_init,
+	.fini = loopback_module_fini,
+	.fini_prio = 1000,
+};
+
+RTE_INIT(loopback_constructor) {
+	iface_type_register(&iface_type_loopback);
+	gr_register_module(&loopback_module);
+}

modules/infra/cli/loopback.c
@@ -1,0 +1,23 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_cli_iface.h>
+#include <gr_infra.h>
+
+static void loopback_show(const struct gr_api_client *, const struct gr_iface *) { }
+
+static void
+loopback_list_info(const struct gr_api_client *, const struct gr_iface *, char *, size_t) { }
+
+static struct cli_iface_type loopback_type = {
+	.type_id = GR_IFACE_TYPE_LOOPBACK,
+	.name = "loopback",
+	.show = loopback_show,
+	.list_info = loopback_list_info,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_iface_type(&loopback_type);
+}

modules/infra/api/gr_infra.h
@@ -18,6 +18,7 @@
 #define GR_IFACE_TYPE_UNDEF 0x00
 #define GR_IFACE_TYPE_PORT 0x01
 #define GR_IFACE_TYPE_VLAN 0x02
+#define GR_IFACE_TYPE_LOOPBACK 0xFF
 #define GR_IFACE_TYPE_MAX 256
 
 // Interface configure flags

modules/infra/cli/meson.build
@@ -4,6 +4,7 @@
 cli_src += files(
   'graph.c',
   'iface.c',
+  'loopback.c',
   'port.c',
   'stats.c',
   'trace.c',

modules/infra/control/gr_iface.h
@@ -84,6 +84,10 @@ int iface_del_eth_addr(uint16_t ifid, const struct rte_ether_addr *);
 uint16_t ifaces_count(uint16_t type_id);
 struct iface *iface_next(uint16_t type_id, const struct iface *prev);
 
+struct iface *get_vrf_iface(uint16_t vrf_id);
+struct iface *iface_loopback_create(uint16_t vrf_id);
+int iface_loopback_delete(uint16_t vrf_id);
+
 #define IFACE_EVENTS                                                                               \
 	IFACE_EVENT(UNKNOWN), IFACE_EVENT(POST_ADD), IFACE_EVENT(PRE_REMOVE),                      \
 		IFACE_EVENT(POST_RECONFIG), IFACE_EVENT(STATUS_UP), IFACE_EVENT(STATUS_DOWN)

modules/infra/control/gr_loopback.h
@@ -1,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#ifndef _LOOPBACK
+#define _LOOPBACK
+
+#include <gr_control_input.h>
+
+void loopback_tx(struct rte_mbuf *m);
+control_input_t loopback_get_control_id(void);
+
+#endif

modules/infra/control/meson.build
@@ -4,6 +4,7 @@
 src += files(
   'control_output.c',
   'iface.c',
+  'loopback.c',
   'mempool.c',
   'port.c',
   'worker.c',

modules/infra/datapath/loop_input.c
@@ -1,0 +1,127 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr.h>
+#include <gr_control_input.h>
+#include <gr_graph.h>
+#include <gr_ip4_datapath.h>
+#include <gr_ip6_datapath.h>
+#include <gr_loopback.h>
+#include <gr_trace.h>
+
+#include <rte_graph_worker.h>
+
+#include <linux/if_tun.h>
+
+static control_input_t control_to_loopback_input;
+
+control_input_t loopback_get_control_id(void) {
+	return control_to_loopback_input;
+}
+
+enum {
+	IP_LOCAL,
+	IP_OUTPUT,
+	IP6_LOCAL,
+	IP6_OUTPUT,
+	IP_NO_ROUTE,
+	IP6_NO_ROUTE,
+	BAD_PROTO,
+	EDGE_COUNT,
+};
+
+static uint16_t loopback_input_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	struct rte_mbuf *mbuf;
+	struct tun_pi *pi;
+	rte_edge_t edge;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		mbuf = objs[i];
+
+		if (gr_mbuf_is_traced(mbuf)
+		    || mbuf_data(mbuf)->iface->flags & GR_IFACE_F_PACKET_TRACE) {
+			gr_mbuf_trace_add(mbuf, node, 0);
+		}
+
+		pi = rte_pktmbuf_mtod(mbuf, struct tun_pi *);
+		rte_pktmbuf_adj(mbuf, sizeof(*pi));
+
+		if (pi->proto == RTE_BE16(RTE_ETHER_TYPE_IPV4)) {
+			struct ip_output_mbuf_data *d;
+			struct rte_ipv4_hdr *ip;
+			struct nexthop *nh;
+
+			d = ip_output_mbuf_data(mbuf);
+			ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv4_hdr *);
+			nh = ip4_route_lookup(d->iface->vrf_id, ip->dst_addr);
+			if (nh == NULL) {
+				edge = IP_NO_ROUTE;
+			} else {
+				d->nh = nh;
+				// If the resolved next hop is local and the destination IP is ourselves,
+				// send to ip_local.
+				if (nh->flags & GR_IP4_NH_F_LOCAL && ip->dst_addr == nh->ip)
+					edge = IP_LOCAL;
+				else
+					edge = IP_OUTPUT;
+			}
+		} else if (pi->proto == RTE_BE16(RTE_ETHER_TYPE_IPV6)) {
+			struct ip6_output_mbuf_data *d;
+			struct rte_ipv6_hdr *ip;
+			struct nexthop6 *nh;
+
+			d = ip6_output_mbuf_data(mbuf);
+			ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv6_hdr *);
+			nh = ip6_route_lookup(d->iface->vrf_id, &ip->dst_addr);
+
+			if (nh == NULL) {
+				edge = IP6_NO_ROUTE;
+			} else {
+				d->nh = nh;
+				// If the resolved next hop is local and the destination IP is ourselves,
+				// send to ip6_local.
+				if (nh->flags & GR_IP6_NH_F_LOCAL
+				    && rte_ipv6_addr_eq(&ip->dst_addr, &nh->ip))
+					edge = IP6_LOCAL;
+				else
+					edge = IP6_OUTPUT;
+			}
+		} else {
+			edge = BAD_PROTO;
+		}
+		rte_node_enqueue_x1(graph, node, edge, mbuf);
+	}
+	return nb_objs;
+}
+
+static struct rte_node_register loopback_input_node = {
+	.name = "loopback_input",
+	.process = loopback_input_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[IP_LOCAL] = "ip_input_local",
+		[IP_OUTPUT] = "ip_output",
+		[IP6_LOCAL] = "ip6_input_local",
+		[IP6_OUTPUT] = "ip6_output",
+		[IP_NO_ROUTE] = "ip_error_dest_unreach",
+		[IP6_NO_ROUTE] = "ip6_error_dest_unreach",
+		[BAD_PROTO] = "loopback_error_bad_proto",
+	},
+};
+
+static void loopback_input_register(void) {
+	control_to_loopback_input = gr_control_input_register_handler("loopback_input", true);
+}
+
+static struct gr_node_info info = {
+	.node = &loopback_input_node,
+	.register_callback = loopback_input_register,
+};
+
+GR_NODE_REGISTER(info);
+GR_DROP_REGISTER(loopback_error_bad_proto);

commit 76465180a8fce708fcafa646110aac4f8acdb61b

loopback: fix mbuf leak
Fix mbuf leak on EAGAIN/EWOULDBLOCK in iface_loopback_poll.

Fixes: d0de3b236180 ("loopback: add loopback interface")
Reported-by: coderabbitai
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/loopback.c
@@ -141,7 +141,7 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 
 	if ((len = read(lo->fd, data, read_len)) <= 0) {
 		if (errno == EAGAIN || errno == EWOULDBLOCK)
-			return;
+			goto err;
 		LOG(ERR, "read from tun device %s failed %s", iface->name, strerror(errno));
 		goto err;
 	}