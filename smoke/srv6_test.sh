5cab28542fe9049c48b198a450ac7a6c96142922
Maxime Leroy <maxime@leroys.fr>
2025-11-21 13:17:13 +0100

    smoke/srv6: use sid format supported by FRR
    FRR currently doesn't support SRv6 SIDs with a non-zero ARG field. When a
    locator is configured with block_len 16, node_len 16 and func_len 16, the
    implicit format is:
    
      block_len 16, node_len 16, func_len 16, arg_len 0
    
    All SIDs must follow this format, which means the ARG bits must always be
    zero.
    
    Prior to FRR 10.5 there was no validation on the SID value, so invalid
    SIDs were silently accepted and the smoke SRv6 tests passed. Starting
    with 10.5 we validate the SID against the configured locator format
    before installing it in the RIB or advertising it via routing protocols,
    so the old test SIDs are now rejected. (but not in vtysh)
    
    Update the smoke SRv6 tests to use SIDs and prefixes that respect the
    configured locator format (arg_len 0), so the tests pass again with FRR
    10.5+.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Reviewed-by: Robin Jarry <rjarry@redhat.com>

--- smoke/srv6_test.sh
+++ smoke/srv6_test.sh
@@ -6,7 +6,7 @@
 
 port_add p0
 port_add p1
-grcli address add fd00:102::1/64 iface p1
+grcli address add fd00:102::1/32 iface p1
 grcli address add 192.168.61.1/24 iface p0
 
 for n in 0 1; do
@@ -17,7 +17,7 @@ for n in 0 1; do
 	ip -n $ns link set $p up
 done
 ip -n n0 addr add 192.168.61.2/24 dev x-p0
-ip -n n1 addr add fd00:102::2/64 dev x-p1
+ip -n n1 addr add fd00:102::2/32 dev x-p1
 
 sleep 3
 
@@ -28,10 +28,10 @@ sleep 3
 #
 # test case:
 #   - (1) send ipv4 ping from p0
-#   - (2) grout encap in srv6, send to sid fd00:202::2
+#   - (2) grout encap in srv6, send to sid fd00:202:200::
 #   - (3) linux x-p1 decap it
 #   - (4) reply to ping
-#   - (5) linux x-p1 reencap in srv6, send to grout sid fd00:202::100,
+#   - (5) linux x-p1 reencap in srv6, send to grout sid fd00:202:100::,
 #   - (6) grout decap it, reply back in ipv4 to p0
 #
 
@@ -43,25 +43,25 @@ ip netns exec n1 sysctl -w net.ipv6.conf.x-p1.forwarding=1
 ip -n n0 route add default via 192.168.61.1 dev x-p0
 
 # (2)
-grcli nexthop add srv6 seglist fd00:202::2 id 42
+grcli nexthop add srv6 seglist fd00:202:200:: id 42
 grcli route add 192.168.0.0/16 via id 42
-grcli route add fd00:202::/64 via fd00:102::2
+grcli route add fd00:202::/32 via fd00:102::2
 
 # (3)
-ip -n n1 -6 route add fd00:202::2 encap seg6local action End.DX4 nh4 192.168.60.1 count dev x-p1
+ip -n n1 -6 route add fd00:202:200:: encap seg6local action End.DX4 nh4 192.168.60.1 count dev x-p1
 
 # (4) 192.168.60.0/24 is our 'public' network
 ip -n n1 addr add 192.168.60.1/24 dev x-p1
 
 # (5)
-ip -n n1 route add 192.168.61.0/24 encap seg6 mode encap segs fd00:202::100 dev x-p1
-ip -n n1 -6 route add fd00:202::/64 via fd00:102::1 dev x-p1
+ip -n n1 route add 192.168.61.0/24 encap seg6 mode encap segs fd00:202:100:: dev x-p1
+ip -n n1 -6 route add fd00:202::/32 via fd00:102::1 dev x-p1
 
 # (6)
 grcli nexthop add srv6-local behavior end.dt4 id 666
-grcli route add fd00:202::100/128 via id 666
+grcli route add fd00:202:100::/48 via id 666
 
 # test
 ip netns exec n0 ping -i0.01 -c3 -n 192.168.60.1
 # check that sid is reachable
-ip netns exec n1 ping6 -i0.01 -c3 -n fd00:202::100
+ip netns exec n1 ping6 -i0.01 -c3 -n fd00:202:100::
