09b9b4e85c213d402edd5263b8734eba97a03604
Maxime Leroy <maxime@leroys.fr>
2025-11-25 17:45:48 +0100

    frr: use uint16_t consistently for grout ifindex
    Grout interface IDs (IFIDs) are defined as uint16_t, but the if_map code
    was inconsistent: some functions used int16_t while the if_map structure
    used ifindex_t.
    
    Update all Grout-side ifindex handling to use uint16_t consistently,
    matching Grout's definition.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Reviewed-by: Robin Jarry <rjarry@redhat.com>

--- frr/if_map.h
+++ frr/if_map.h
@@ -9,9 +9,9 @@
 DECLARE_MGROUP(GROUT);
 DECLARE_MTYPE(GROUT_MEM);
 
-bool add_ifindex_mapping(ifindex_t grout_ifindex, ifindex_t frr_ifindex);
-bool remove_mapping_by_grout_ifindex(ifindex_t grout_ifindex);
+bool add_ifindex_mapping(uint16_t grout_ifindex, ifindex_t frr_ifindex);
+bool remove_mapping_by_grout_ifindex(uint16_t grout_ifindex);
 
-ifindex_t ifindex_grout_to_frr(int16_t grout_ifindex);
+ifindex_t ifindex_grout_to_frr(uint16_t grout_ifindex);
 uint16_t ifindex_frr_to_grout(ifindex_t frr_ifindex);
 void init_ifindex_mappings(void);
