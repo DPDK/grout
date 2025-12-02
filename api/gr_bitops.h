9cfc97c5b47e89d8572c0aab51494f5d2f15fa11
Robin Jarry <rjarry@redhat.com>
2025-12-02 14:00:48 +0100

    api: document common headers
    Add comments to utility headers for bit operations, error codes, macros,
    network types and string helpers.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- api/gr_bitops.h
+++ api/gr_bitops.h
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 
+// Bit manipulation utilities for flags and bitmasks.
 #define GR_BIT8(n) (UINT8_C(1) << (n))
 #define GR_BIT16(n) (UINT16_C(1) << (n))
 #define GR_BIT32(n) (UINT32_C(1) << (n))
