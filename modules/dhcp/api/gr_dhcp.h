c6a483058562a94cfb32e2e91da4aa7b29ad4911
Robin Jarry <rjarry@redhat.com>
2025-12-02 14:04:27 +0100

    api: document gr_dhcp.h
    Add comments for DHCP client state machine states and API endpoints.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/dhcp/api/gr_dhcp.h
+++ modules/dhcp/api/gr_dhcp.h
@@ -9,37 +9,38 @@
 
 #include <stdint.h>
 
+// DHCP client state machine states (RFC 2131).
 typedef enum dhcp_state : uint8_t {
-	DHCP_STATE_INIT = 0,
-	DHCP_STATE_SELECTING,
-	DHCP_STATE_REQUESTING,
-	DHCP_STATE_BOUND,
-	DHCP_STATE_RENEWING,
-	DHCP_STATE_REBINDING,
+	DHCP_STATE_INIT = 0, // Initial state, no configuration.
+	DHCP_STATE_SELECTING, // Waiting for DHCPOFFER messages.
+	DHCP_STATE_REQUESTING, // Waiting for DHCPACK message.
+	DHCP_STATE_BOUND, // Lease acquired and valid.
+	DHCP_STATE_RENEWING, // Renewing lease with original server (T1 expired).
+	DHCP_STATE_REBINDING, // Rebinding with any server (T2 expired).
 } dhcp_state_t;
 
+// DHCP client status information for an interface.
 struct gr_dhcp_status {
 	uint16_t iface_id;
 	dhcp_state_t state;
 	ip4_addr_t server_ip;
 	ip4_addr_t assigned_ip;
 	uint32_t lease_time;
-	uint32_t renewal_time; // T1
-	uint32_t rebind_time; // T2
+	uint32_t renewal_time;
+	uint32_t rebind_time;
 };
 
 #define GR_DHCP_MODULE 0xd4c9
 
-// list ////////////////////////////////////////////////////////////////////////
-
+// List all active DHCP clients and their status.
 #define GR_DHCP_LIST REQUEST_TYPE(GR_DHCP_MODULE, 0x01)
 
 // struct gr_dhcp_list_req { };
 
 STREAM_RESP(struct gr_dhcp_status);
 
-// start ///////////////////////////////////////////////////////////////////////
-
+// Start DHCP client on an interface.
+// Initiates DHCP discovery to obtain IPv4 address configuration.
 #define GR_DHCP_START REQUEST_TYPE(GR_DHCP_MODULE, 0x02)
 
 struct gr_dhcp_start_req {
@@ -48,8 +49,8 @@ struct gr_dhcp_start_req {
 
 // struct gr_dhcp_start_resp { };
 
-// stop ////////////////////////////////////////////////////////////////////////
-
+// Stop DHCP client on an interface.
+// Releases the current lease and removes assigned address.
 #define GR_DHCP_STOP REQUEST_TYPE(GR_DHCP_MODULE, 0x03)
 
 struct gr_dhcp_stop_req {
