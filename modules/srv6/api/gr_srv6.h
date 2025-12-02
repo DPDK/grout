4bee2c5cb936609d40bbecbc560e9d14205a5d89
Robin Jarry <rjarry@redhat.com>
2025-12-02 14:00:48 +0100

    api: document gr_srv6.h
    Add comments for SRv6 locator and SID management endpoints.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/srv6/api/gr_srv6.h
+++ modules/srv6/api/gr_srv6.h
@@ -11,16 +11,20 @@
 
 #define GR_SRV6_MODULE 0xfeef
 
+// NB: SRv6 nexthops are managed using GR_NH_* messages from gr_infra.h
+
 // sr routes //////////////////////////////////////////////////////
 
 #define GR_SRV6_ROUTE_SEGLIST_COUNT_MAX 60
 
-// SRv6 Route Headend Behaviors Signaling (rfc8986 8.4)
+// SRv6 Route Headend Behaviors Signaling (RFC 8986 Section 8.4).
 typedef enum : uint8_t {
 	SR_H_ENCAPS,
 	SR_H_ENCAPS_RED,
 } gr_srv6_encap_behavior_t;
 
+// SRv6 output nexthop information for packet encapsulation.
+// Used with GR_NH_T_SR6_OUTPUT nexthops via GR_NH_ADD from gr_infra.h.
 struct gr_nexthop_info_srv6 {
 	gr_srv6_encap_behavior_t encap_behavior;
 	uint8_t n_seglist;
@@ -29,14 +33,17 @@ struct gr_nexthop_info_srv6 {
 
 // sr tun src //////////////////////////////////////////////////////
 
+// Set the global SRv6 tunnel source address.
 #define GR_SRV6_TUNSRC_SET REQUEST_TYPE(GR_SRV6_MODULE, 0x0005)
 struct gr_srv6_tunsrc_set_req {
 	struct rte_ipv6_addr addr;
 };
 
+// Clear the global SRv6 tunnel source address.
 #define GR_SRV6_TUNSRC_CLEAR REQUEST_TYPE(GR_SRV6_MODULE, 0x0006)
 // struct gr_srv6_tunsrc_clear_req { };
 
+// Show the current SRv6 tunnel source address.
 #define GR_SRV6_TUNSRC_SHOW REQUEST_TYPE(GR_SRV6_MODULE, 0x0007)
 // struct gr_srv6_tunsrc_show_req { };
 
@@ -46,19 +53,18 @@ struct gr_srv6_tunsrc_show_resp {
 
 // localsid (tunnel transit and exit) /////////////////////////////////
 
-//
-// https://www.iana.org/assignments/segment-routing/segment-routing.xhtml
-//
-// flavor (psp/usd) are defined alongside as flag
-//
+// SRv6 Local SID behaviors (IANA assigned values).
+// See: https://www.iana.org/assignments/segment-routing/segment-routing.xhtml
+// Flavors (PSP/USD) are defined as separate flags.
 typedef enum : uint16_t {
-	SR_BEHAVIOR_END = 0x0001,
-	SR_BEHAVIOR_END_T = 0x0009,
-	SR_BEHAVIOR_END_DT6 = 0x0012,
-	SR_BEHAVIOR_END_DT4 = 0x0013,
-	SR_BEHAVIOR_END_DT46 = 0x0014,
+	SR_BEHAVIOR_END = 0x0001, // Endpoint function.
+	SR_BEHAVIOR_END_T = 0x0009, // Endpoint function with specific table.
+	SR_BEHAVIOR_END_DT6 = 0x0012, // Decaps and IPv6 table lookup.
+	SR_BEHAVIOR_END_DT4 = 0x0013, // Decaps and IPv4 table lookup.
+	SR_BEHAVIOR_END_DT46 = 0x0014, // Decaps and IPv4/IPv6 table lookup.
 } gr_srv6_behavior_t;
 
+// Convert SRv6 behavior enum to string representation.
 static inline const char *gr_srv6_behavior_name(gr_srv6_behavior_t b) {
 	switch (b) {
 	case SR_BEHAVIOR_END:
@@ -75,11 +81,14 @@ static inline const char *gr_srv6_behavior_name(gr_srv6_behavior_t b) {
 	return "?";
 }
 
+// SRv6 flavor flags for Local SID behaviors.
 typedef enum : uint8_t {
-	GR_SR_FL_FLAVOR_PSP = GR_BIT8(0),
-	GR_SR_FL_FLAVOR_USD = GR_BIT8(1),
+	GR_SR_FL_FLAVOR_PSP = GR_BIT8(0), // Penultimate Segment Popping.
+	GR_SR_FL_FLAVOR_USD = GR_BIT8(1), // Ultimate Segment Decapsulation.
 } gr_srv6_flags_t;
 
+// SRv6 local nexthop information for Local SID processing.
+// Used with GR_NH_T_SR6_LOCAL nexthops via GR_NH_ADD from gr_infra.h.
 struct gr_nexthop_info_srv6_local {
 	uint16_t out_vrf_id;
 	gr_srv6_behavior_t behavior;
