DPDK/grout

commit 9d8684d3dff7f10c9f4ef95067ea965fc177f3c1

ip6: factorize solicited node mcast address config
In both places where iface6_addr_add() is called, we also compute
a solicited node multicast address and add it to the interface. Include
this directly in iface6_addr_add().

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/address.c
@@ -159,6 +159,7 @@ static int mcast6_addr_del(const struct iface *iface, const struct rte_ipv6_addr
 
 static int
 iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8_t prefixlen) {
+	struct rte_ipv6_addr solicited_node;
 	struct hoplist *addrs;
 	struct nexthop *nh;
 	int ret;
@@ -193,6 +194,15 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 	if ((nh = nexthop_new(&base)) == NULL)
 		return errno_set(-errno);
 
+	// join the solicited node multicast group
+	rte_ipv6_solnode_from_addr(&solicited_node, ip);
+	if (mcast6_addr_add(iface, &solicited_node) < 0) {
+		if (errno != EOPNOTSUPP && errno != EEXIST) {
+			nexthop_decref(nh);
+			return errno_set(errno);
+		}
+	}
+
 	ret = rib6_insert(iface->vrf_id, iface->id, ip, nh->prefixlen, GR_NH_ORIGIN_LINK, nh);
 	if (ret < 0)
 		return errno_set(-ret);
@@ -205,7 +215,6 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 
 static struct api_out addr6_add(const void *request, struct api_ctx *) {
 	const struct gr_ip6_addr_add_req *req = request;
-	struct rte_ipv6_addr solicited_node;
 	struct iface *iface;
 	int ret;
 
@@ -216,13 +225,6 @@ static struct api_out addr6_add(const void *request, struct api_ctx *) {
 	if ((ret = iface6_addr_add(iface, &req->addr.addr.ip, req->addr.addr.prefixlen)) < 0)
 		if (ret != -EEXIST || !req->exist_ok)
 			return api_out(-ret, 0, NULL);
-
-	// join the solicited node multicast group
-	rte_ipv6_solnode_from_addr(&solicited_node, &req->addr.addr.ip);
-	if (mcast6_addr_add(iface, &solicited_node) < 0) {
-		if (errno != EOPNOTSUPP && errno != EEXIST)
-			return api_out(errno, 0, NULL);
-	}
 
 	return api_out(0, 0, NULL);
 }
@@ -302,8 +304,8 @@ static const struct rte_ipv6_addr well_known_mcast_addrs[] = {
 };
 
 static void ip6_iface_event_handler(uint32_t event, const void *obj) {
-	struct rte_ipv6_addr link_local, solicited_node;
 	const struct iface *iface = obj;
+	struct rte_ipv6_addr link_local;
 	struct rte_ether_addr mac;
 	struct nexthop *nh;
 	unsigned i;
@@ -316,10 +318,6 @@ static void ip6_iface_event_handler(uint32_t event, const void *obj) {
 			rte_ipv6_llocal_from_ethernet(&link_local, &mac);
 			if (iface6_addr_add(iface, &link_local, 64) < 0)
 				errno_log(errno, "iface_addr_add");
-
-			rte_ipv6_solnode_from_addr(&solicited_node, &link_local);
-			if (mcast6_addr_add(iface, &solicited_node) < 0)
-				LOG(INFO, "%s: mcast_addr_add: %s", iface->name, strerror(errno));
 		}
 		for (i = 0; i < ARRAY_DIM(well_known_mcast_addrs); i++) {
 			if (mcast6_addr_add(iface, &well_known_mcast_addrs[i]) < 0)

commit 4ec877ef2633e22788f731f50817be13d593dbf5

address: fix assert error when mcast cannot be configured
When starting grout as a non-root user and configuring a dummy net_null
port, on a debug build, grout aborts with an assertion error:

+ grcli interface add port p0 devargs net_null

ERR: GROUT: iface_loopback_init: ioctl(TUNSETIFF): Operation not permitted
WARN: GROUT: vrf_incref: loopback for vrf 0 cannot be created: Operation not permitted
ERR: GROUT: cp_create: ioctl(TUNSETIFF): Operation not permitted
ERR: GROUT: cp_update: ioctl(SIOCSIFMTU) Operation not permitted
grout: ../modules/infra/control/nexthop.c:543: nexthop_decref: Assertion `nh->ref_count > 0' failed.

While the errors are expected since grout has no permission to create
nor configure tap interfaces, it shouldn't cause a crash.

The nexthop was just created with refcount = 0. Calling decref directly
is an invalid operation. Add an artificial incref before decref so that
the nexthop is freed.

Fixes: 9d8684d3dff7 ("ip6: factorize solicited node mcast address config")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/address.c
@@ -220,6 +220,7 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 	rte_ipv6_solnode_from_addr(&solicited_node, ip);
 	if (mcast6_addr_add(iface, &solicited_node) < 0) {
 		if (errno != EOPNOTSUPP && errno != EEXIST) {
+			nexthop_incref(nh);
 			nexthop_decref(nh);
 			return errno_set(errno);
 		}