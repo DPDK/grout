DPDK/grout

commit 991a8c0e315c2ac2a50d5b4813873d2d1115d635

address: fix assert error when mcast cannot be configured
When starting grout as a non-root user and configuring a dummy net_null
port, on a debug build, grout aborts with an assertion error:

+ grcli interface add port p0 devargs net_null

ERR: GROUT: iface_loopback_init: ioctl(TUNSETIFF): Operation not permitted
WARN: GROUT: vrf_incref: loopback for vrf 0 cannot be created: Operation not permitted
ERR: GROUT: cp_create: ioctl(TUNSETIFF): Operation not permitted
ERR: GROUT: cp_update: ioctl(SIOCSIFMTU) Operation not permitted
grout: ../modules/infra/control/nexthop.c:543: nexthop_decref: Assertion `nh->ref_count > 0' failed.

While the errors are expected since grout has no permission to create
nor configure tap interfaces, it shouldn't cause a crash.

The nexthop was just created with refcount = 0. Calling decref directly
is an invalid operation. Add an artificial incref before decref so that
the nexthop is freed.

Fixes: 9d8684d3dff7 ("ip6: factorize solicited node mcast address config")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/address.c
@@ -220,6 +220,7 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 	rte_ipv6_solnode_from_addr(&solicited_node, ip);
 	if (mcast6_addr_add(iface, &solicited_node) < 0) {
 		if (errno != EOPNOTSUPP && errno != EEXIST) {
+			nexthop_incref(nh);
 			nexthop_decref(nh);
 			return errno_set(errno);
 		}

commit 15ef7a4a542f27fed45fefd178807b8f50688e08

nexthop: introduce explicit destroy operation
When creating a nexthop, it has refcount=0. If we need to destroy it
before its refcount has been incremented we need to do the awkward
incref, decref combo.

This is confusing and we will surely forget why this is even there in
a couple of months.

Add an explicit nexthop_destroy() function and call it from within
nexthop_decref() and in the IPv6 address code.

Fixes: 991a8c0e315c ("address: fix assert error when mcast cannot be configured")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Maxime Leroy <maxime@leroys.fr>

modules/infra/control/gr_nh_control.h
@@ -110,8 +110,11 @@ void nexthop_routes_cleanup(struct nexthop *);
 void nexthop_incref(struct nexthop *);
 
 // Decrement the reference counter of a nexthop.
-// When the counter drops to 0, the nexthop is marked as available in the global pool.
+// When the counter drops to 0, the nexthop is destroyed and returned to the global pool.
 void nexthop_decref(struct nexthop *);
+
+// Return the nexthop to the global pool regardless of its refcount.
+void nexthop_destroy(struct nexthop *);
 
 // Callback for nh_iter().
 typedef void (*nh_iter_cb_t)(struct nexthop *nh, void *priv);

modules/infra/control/nexthop.c
@@ -539,32 +539,37 @@ void nexthop_iface_cleanup(uint16_t iface_id) {
 	nexthop_iter(nh_cleanup_interface_cb, &filter);
 }
 
+void nexthop_destroy(struct nexthop *nh) {
+	assert(nh->ref_count == 0);
+
+	nh_groups_remove_member(nh);
+	nexthop_id_put(nh);
+	rte_rcu_qsbr_synchronize(gr_datapath_rcu(), RTE_QSBR_THRID_INVALID);
+
+	// Push NEXTHOP_DELETE event after RCU sync to ensure all datapath
+	// threads have seen that this nexthop is gone. At this point, only
+	// packets already in the control output ring may still reference it.
+	// The event triggers a drain that frees those packets before we free
+	// the nexthop memory.
+	if (nh->origin != GR_NH_ORIGIN_INTERNAL)
+		gr_event_push(GR_EVENT_NEXTHOP_DELETE, nh);
+
+	assert(nh_stats.total > 0);
+	nh_stats.total--;
+	assert(nh_stats.by_type[nh->type] > 0);
+	nh_stats.by_type[nh->type]--;
+
+	const struct nexthop_type_ops *ops = type_ops[nh->type];
+	if (ops != NULL && ops->free != NULL)
+		ops->free(nh);
+	rte_mempool_put(pool, nh);
+}
+
 void nexthop_decref(struct nexthop *nh) {
 	assert(nh->ref_count > 0);
 	nh->ref_count--;
-	if (nh->ref_count == 0) {
-		nh_groups_remove_member(nh);
-		nexthop_id_put(nh);
-		rte_rcu_qsbr_synchronize(gr_datapath_rcu(), RTE_QSBR_THRID_INVALID);
-
-		// Push NEXTHOP_DELETE event after RCU sync to ensure all datapath
-		// threads have seen that this nexthop is gone. At this point, only
-		// packets already in the control output ring may still reference it.
-		// The event triggers a drain that frees those packets before we free
-		// the nexthop memory.
-		if (nh->origin != GR_NH_ORIGIN_INTERNAL)
-			gr_event_push(GR_EVENT_NEXTHOP_DELETE, nh);
-
-		assert(nh_stats.total > 0);
-		nh_stats.total--;
-		assert(nh_stats.by_type[nh->type] > 0);
-		nh_stats.by_type[nh->type]--;
-
-		const struct nexthop_type_ops *ops = type_ops[nh->type];
-		if (ops != NULL && ops->free != NULL)
-			ops->free(nh);
-		rte_mempool_put(pool, nh);
-	}
+	if (nh->ref_count == 0)
+		nexthop_destroy(nh);
 }
 
 void nexthop_incref(struct nexthop *nh) {

modules/ip6/control/address.c
@@ -220,8 +220,7 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 	rte_ipv6_solnode_from_addr(&solicited_node, ip);
 	if (mcast6_addr_add(iface, &solicited_node) < 0) {
 		if (errno != EOPNOTSUPP && errno != EEXIST) {
-			nexthop_incref(nh);
-			nexthop_decref(nh);
+			nexthop_destroy(nh);
 			return errno_set(errno);
 		}
 	}