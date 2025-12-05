DPDK/grout

commit 0edd9956e7cc8215a95ff3a99764b9c4a82f9914

trace: return packets by batches
When grout is processing traffic, traces are collected and enqueued in
the ring buffer. Since the CLI is requesting traces packet by packet in
an infinite loop, the `show trace` command may never end. Since the ring
buffer will never become empty and `show trace` will never receive an
empty response.

To avoid this, allow the API to return up to 32 packets per batch. Even
with this, the response should fit in GR_API_MAX_MSG_LEN. Also, add
a show trace count N parameter to limit the amount of traced packets
before exiting. By default dump 10 packets. Update smoke tests to dump
50 packets.

If the requested packet count is higher than GR_INFRA_PACKET_TRACE_BATCH
(32 packets), request multiple times until the server sends an empty
response or until we reached the maximum.

Fixes: bc04bf4e328c ("infra: add packet tracing framework")
Signed-off-by: Robin Jarry <robin@jarry.cc>
Acked-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/cli/trace.c
@@ -49,22 +49,37 @@ static cmd_status_t trace_del(const struct gr_api_client *c, const struct ec_pno
 	return CMD_SUCCESS;
 }
 
-static cmd_status_t trace_show(const struct gr_api_client *c, const struct ec_pnode *) {
+static cmd_status_t trace_show(const struct gr_api_client *c, const struct ec_pnode *p) {
 	const struct gr_infra_packet_trace_dump_resp *resp = NULL;
+	struct gr_infra_packet_trace_dump_req req;
+	uint16_t max_packets = 10;
 	void *resp_ptr = NULL;
-	size_t len = 0;
+	int ret;
+
+	if (arg_u16(p, "COUNT", &max_packets) < 0)
+		return CMD_ERROR;
 
 	do {
-		if (gr_api_client_send_recv(c, GR_INFRA_PACKET_TRACE_DUMP, 0, NULL, &resp_ptr) < 0)
+		req.max_packets = max_packets > GR_INFRA_PACKET_TRACE_BATCH ?
+			GR_INFRA_PACKET_TRACE_BATCH :
+			max_packets;
+		ret = gr_api_client_send_recv(
+			c, GR_INFRA_PACKET_TRACE_DUMP, sizeof(req), &req, &resp_ptr
+		);
+		if (ret < 0)
 			return CMD_ERROR;
 
 		resp = resp_ptr;
-		len = resp->len;
-		if (len > 1) {
+		if (resp->n_packets > 0)
+			max_packets -= resp->n_packets;
+		else
+			max_packets = 0;
+		if (resp->len > 1)
 			fwrite(resp->trace, 1, resp->len, stdout);
-		}
+
 		free(resp_ptr);
-	} while (len > 0);
+		resp_ptr = NULL;
+	} while (max_packets > 0);
 
 	return CMD_SUCCESS;
 }
@@ -115,7 +130,16 @@ static int ctx_init(struct ec_node *root) {
 	if (ret < 0)
 		return ret;
 
-	ret = CLI_COMMAND(CLI_CONTEXT(root, CTX_SHOW), "trace", trace_show, "Show traced packets.");
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("trace", "Show traced packets.")),
+		"[count COUNT]",
+		trace_show,
+		"Show traced packets.",
+		with_help(
+			"Maximum number of packets to show (default 10).",
+			ec_node_uint("COUNT", 1, UINT16_MAX, 10)
+		)
+	);
 	if (ret < 0)
 		return ret;
 

modules/infra/api/trace.c
@@ -46,24 +46,29 @@ static struct api_out set_trace(const void *request, void ** /*response*/) {
 	return api_out(0, 0);
 }
 
-#define TRACE_MAX_LEN (6 * 1024) // 64 nodes, 80 cols per node: ~5120 bytes.
-
-static struct api_out dump_trace(const void * /*request*/, void **response) {
+static struct api_out dump_trace(const void *request, void **response) {
+	const struct gr_infra_packet_trace_dump_req *req = request;
 	struct gr_infra_packet_trace_dump_resp *resp;
-	int len = 0;
+	int ret;
 
-	if ((resp = calloc(1, sizeof(*resp) + TRACE_MAX_LEN)) == NULL)
+	if ((resp = malloc(GR_API_MAX_MSG_LEN)) == NULL)
 		return api_out(ENOMEM, 0);
 
-	if ((len = gr_trace_dump(resp->trace, TRACE_MAX_LEN)) < 0) {
+	ret = gr_trace_dump(
+		resp->trace,
+		GR_API_MAX_MSG_LEN - sizeof(*resp),
+		req->max_packets,
+		&resp->len,
+		&resp->n_packets
+	);
+	if (ret < 0) {
 		free(resp);
-		return api_out(-len, 0);
+		return api_out(-ret, 0);
 	}
 
-	resp->len = len;
 	*response = resp;
 
-	return api_out(0, sizeof(*resp) + len);
+	return api_out(0, sizeof(*resp) + resp->len);
 }
 
 static struct api_out clear_trace(const void * /*request*/, void ** /*response*/) {

modules/infra/datapath/trace.c
@@ -513,14 +513,22 @@ void gr_mbuf_trace_finish(struct rte_mbuf *m) {
 	STAILQ_INIT(traces);
 }
 
-int gr_trace_dump(char *buf, size_t len) {
+int gr_trace_dump(
+	char *buf,
+	size_t len,
+	uint16_t max_packets,
+	uint32_t *n_bytes,
+	uint16_t *n_packets
+) {
 	struct gr_trace_item *head = NULL;
 	const struct gr_node_info *info;
+	uint32_t n = 0;
+	uint16_t p = 0;
 	struct tm tm;
-	int n = 0, s;
 	void *data;
+	int s;
 
-	if (rte_ring_dequeue(traced_packets, &data) == 0) {
+	while (rte_ring_dequeue(traced_packets, &data) == 0 && p < max_packets) {
 		struct gr_trace_item *t = data;
 		head = t;
 
@@ -547,9 +555,13 @@ int gr_trace_dump(char *buf, size_t len) {
 		head = NULL;
 		// add empty line to separate packets
 		SAFE_BUF(snprintf, len, "\n");
+		p += 1;
 	}
 
-	return n;
+	*n_bytes = n;
+	*n_packets = p;
+
+	return 0;
 err:
 	free_trace(head);
 	return errno ? -errno : errno_set(ENOBUFS);

modules/infra/api/gr_infra.h
@@ -205,6 +205,8 @@ struct gr_infra_graph_dump_resp {
 };
 
 // packet tracing //////////////////////////////////////////////////////////////
+#define GR_INFRA_PACKET_TRACE_BATCH 32
+
 #define GR_INFRA_PACKET_TRACE_CLEAR REQUEST_TYPE(GR_INFRA_MODULE, 0x0040)
 
 // struct gr_infra_trace_clear_req { };
@@ -212,9 +214,12 @@ struct gr_infra_graph_dump_resp {
 
 #define GR_INFRA_PACKET_TRACE_DUMP REQUEST_TYPE(GR_INFRA_MODULE, 0x0041)
 
-// struct gr_infra_trace_dump_req { };
+struct gr_infra_packet_trace_dump_req {
+	uint16_t max_packets;
+};
 
 struct gr_infra_packet_trace_dump_resp {
+	uint16_t n_packets;
 	uint32_t len;
 	char trace[/* len */];
 };

modules/infra/datapath/gr_trace.h
@@ -21,9 +21,16 @@ void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *if
 // to format each individual trace items.
 typedef int (*gr_trace_format_cb_t)(char *buf, size_t buf_len, const void *data, size_t data_len);
 
-// Format the buffered trace items and empty the buffer.
-// Return the number of bytes written to buffer or a negative value on error.
-int gr_trace_dump(char *buf, size_t buf_len);
+// Format the buffered trace items, emptying the trace ring of max_packets.
+// Set the number of written bytes to n_bytes and the number of dumped packets to n_packets.
+// Return 0 on success or a negative value on error.
+int gr_trace_dump(
+	char *buf,
+	size_t buf_len,
+	uint16_t max_packets,
+	uint32_t *n_bytes,
+	uint16_t *n_packets
+);
 
 // Empty the trace buffer.
 void gr_trace_clear(void);

smoke/_init.sh
@@ -41,7 +41,7 @@ grcli show ip nexthop
 grcli show ip route
 grcli show ip6 nexthop
 grcli show ip6 route
-grcli show trace
+grcli show trace count 50
 EOF
 
 set -x

commit d88666145b998dae2a6f8263e94007b075d481ce

trace: fix error when count is not specified
When running trace show without specifying the count the CLI returns an
error:

  grout# trace show
  error: command failed: No such file or directory (ENOENT)

The count argument is optional, do not fail if it was not specified.

Fixes: 0edd9956e7cc ("trace: return packets by batches")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/cli/trace.c
@@ -58,7 +58,7 @@ static cmd_status_t trace_show(struct gr_api_client *c, const struct ec_pnode *p
 	void *resp_ptr = NULL;
 	int ret;
 
-	if (arg_u16(p, "COUNT", &max_packets) < 0)
+	if (arg_u16(p, "COUNT", &max_packets) < 0 && errno != ENOENT)
 		return CMD_ERROR;
 
 	do {