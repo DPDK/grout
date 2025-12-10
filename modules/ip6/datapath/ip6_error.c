DPDK/grout

commit 7f068c47af42e2b5b663f7a22aeed2eb7e463455

ip6: add support for icmp6 errors
Return packets to the source when ttl is exceeded or when grout does not
have a route to reach the destination.

Update the IPv6 forwarding test to run traceroute.

Link: https://www.rfc-editor.org/rfc/rfc4443.html#section-3.1
Link: https://www.rfc-editor.org/rfc/rfc4443.html#section-3.3
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/datapath/ip6_error.c
@@ -1,0 +1,148 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr_datapath.h>
+#include <gr_graph.h>
+#include <gr_icmp6.h>
+#include <gr_ip6_control.h>
+#include <gr_ip6_datapath.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+
+#include <rte_common.h>
+#include <rte_graph_worker.h>
+#include <rte_ip.h>
+
+enum edges {
+	ICMP_OUTPUT = 0,
+	NO_HEADROOM,
+	NO_IP,
+	EDGE_COUNT,
+};
+
+static uint16_t
+ip6_error_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	struct icmp6_err_dest_unreach *du;
+	struct icmp6_err_ttl_exceeded *te;
+	struct ip6_local_mbuf_data *d;
+	const struct iface *iface;
+	struct rte_ipv6_hdr *ip;
+	icmp6_type_t icmp_type;
+	struct rte_mbuf *mbuf;
+	struct nexthop6 *nh;
+	struct icmp6 *icmp6;
+	rte_edge_t edge;
+
+	icmp_type = node->ctx[0];
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		mbuf = objs[i];
+
+		// Get the pointer to the start of the ipv6 header before
+		// prepending any data
+		ip = rte_pktmbuf_mtod(mbuf, struct rte_ipv6_hdr *);
+
+		// https://www.rfc-editor.org/rfc/rfc4443.html#section-3
+		// ICMPv6 error messages should contain "As much of invoking
+		// packet as possible without the ICMPv6 packet exceeding the
+		// minimum IPv6 MTU (1280)"
+		if (rte_pktmbuf_pkt_len(mbuf) > RTE_IPV6_MIN_MTU)
+			rte_pktmbuf_trim(mbuf, rte_pktmbuf_pkt_len(mbuf) - RTE_IPV6_MIN_MTU);
+
+		switch (icmp_type) {
+		case ICMP6_ERR_DEST_UNREACH:
+			du = (struct icmp6_err_dest_unreach *)
+				rte_pktmbuf_prepend(mbuf, sizeof(*du));
+			if (unlikely(du == NULL)) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			break;
+		case ICMP6_ERR_TTL_EXCEEDED:
+			te = (struct icmp6_err_ttl_exceeded *)
+				rte_pktmbuf_prepend(mbuf, sizeof(*du));
+			if (unlikely(te == NULL)) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			break;
+		default:
+			ABORT("unexpected icmp_type value %hhu", icmp_type);
+			break;
+		}
+
+		icmp6 = (struct icmp6 *)rte_pktmbuf_prepend(mbuf, sizeof(*icmp6));
+		if (unlikely(icmp6 == NULL || ip == NULL)) {
+			edge = NO_HEADROOM;
+			goto next;
+		}
+		icmp6->type = icmp_type;
+		icmp6->code = 0;
+
+		// Get the local router IP address from the input iface
+		iface = ip6_output_mbuf_data(mbuf)->input_iface;
+		if (iface == NULL) {
+			edge = NO_IP;
+			goto next;
+		}
+		if ((nh = ip6_addr_get_preferred(iface->id, &ip->src_addr)) == NULL) {
+			edge = NO_IP;
+			goto next;
+		}
+		d = ip6_local_mbuf_data(mbuf);
+		rte_ipv6_addr_cpy(&d->src, &nh->ip);
+		rte_ipv6_addr_cpy(&d->dst, &ip->src_addr);
+		d->len = rte_pktmbuf_pkt_len(mbuf);
+		d->input_iface = iface;
+		edge = ICMP_OUTPUT;
+next:
+		rte_node_enqueue_x1(graph, node, edge, mbuf);
+	}
+
+	return nb_objs;
+}
+
+static int ttl_exceeded_init(const struct rte_graph *, struct rte_node *node) {
+	node->ctx[0] = ICMP6_ERR_TTL_EXCEEDED;
+	return 0;
+}
+
+static int no_route_init(const struct rte_graph *, struct rte_node *node) {
+	node->ctx[0] = ICMP6_ERR_DEST_UNREACH;
+	return 0;
+}
+
+static struct rte_node_register dest_unreach_node = {
+	.name = "ip6_error_dest_unreach",
+	.process = ip6_error_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[ICMP_OUTPUT] = "icmp6_output",
+		[NO_HEADROOM] = "error_no_headroom",
+		[NO_IP] = "error_no_local_ip",
+	},
+	.init = no_route_init,
+};
+
+struct rte_node_register ttl_exceeded_node = {
+	.name = "ip6_error_ttl_exceeded",
+	.process = ip6_error_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[ICMP_OUTPUT] = "icmp6_output",
+		[NO_HEADROOM] = "error_no_headroom",
+		[NO_IP] = "error_no_local_ip",
+	},
+	.init = ttl_exceeded_init,
+};
+
+static struct gr_node_info dest_unreach_info = {
+	.node = &dest_unreach_node,
+};
+
+static struct gr_node_info ttl_exceeded_info = {
+	.node = &ttl_exceeded_node,
+};
+
+GR_NODE_REGISTER(dest_unreach_info);
+GR_NODE_REGISTER(ttl_exceeded_info);

modules/infra/datapath/gr_icmp6.h
@@ -15,12 +15,18 @@
 
 // ICMP6 packet types
 typedef enum {
+	ICMP6_ERR_DEST_UNREACH = UINT8_C(1),
+	ICMP6_ERR_PKT_TOO_BIG = UINT8_C(2),
+	ICMP6_ERR_TTL_EXCEEDED = UINT8_C(3),
+	ICMP6_ERR_PARAM_PROBLEM = UINT8_C(4),
+
 	ICMP6_TYPE_ECHO_REQUEST = UINT8_C(128),
 	ICMP6_TYPE_ECHO_REPLY = UINT8_C(129),
 	ICMP6_TYPE_ROUTER_SOLICIT = UINT8_C(133),
 	ICMP6_TYPE_ROUTER_ADVERT = UINT8_C(134),
 	ICMP6_TYPE_NEIGH_SOLICIT = UINT8_C(135),
 	ICMP6_TYPE_NEIGH_ADVERT = UINT8_C(136),
+
 	_ICMP6_TYPE_MAX = UINT8_C(0xff),
 } __rte_packed icmp6_type_t;
 
@@ -29,6 +35,26 @@ struct icmp6 {
 	icmp6_type_t type;
 	uint8_t code;
 	rte_be16_t cksum;
+} __rte_packed;
+
+// ICMP6_ERR_DEST_UNREACH
+struct icmp6_err_dest_unreach {
+	uint32_t __unused;
+} __rte_packed;
+
+// ICMP6_ERR_PKT_TOO_BIG
+struct icmp6_err_pkt_too_big {
+	rte_be32_t mtu;
+} __rte_packed;
+
+// ICMP6_ERR_TTL_EXCEEDED
+struct icmp6_err_ttl_exceeded {
+	uint32_t __unused;
+} __rte_packed;
+
+// ICMP6_ERR_PARAM_PROBLEM
+struct icmp6_err_param_problem {
+	rte_be32_t offset;
 } __rte_packed;
 
 // ICMP6_TYPE_ECHO_REQUEST

modules/infra/datapath/trace.c
@@ -228,6 +228,18 @@ static ssize_t trace_icmp6(
 	payload_len -= sizeof(*icmp6);
 
 	switch (icmp6->type) {
+	case ICMP6_ERR_DEST_UNREACH:
+		n += snprintf(buf + n, len - n, " destination unreachable");
+		break;
+	case ICMP6_ERR_PKT_TOO_BIG:
+		n += snprintf(buf + n, len - n, " packet too big");
+		break;
+	case ICMP6_ERR_TTL_EXCEEDED:
+		n += snprintf(buf + n, len - n, " ttl exceeded");
+		break;
+	case ICMP6_ERR_PARAM_PROBLEM:
+		n += snprintf(buf + n, len - n, " parameter problem");
+		break;
 	case ICMP6_TYPE_ECHO_REQUEST: {
 		const struct icmp6_echo_request *req = PAYLOAD(icmp6);
 		*offset += sizeof(*req);

modules/ip6/datapath/ip6_forward.c
@@ -43,7 +43,7 @@ static struct rte_node_register node = {
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
 		[OUTPUT] = "ip6_output",
-		[TTL_EXCEEDED] = "ip6_forward_ttl_exceeded",
+		[TTL_EXCEEDED] = "ip6_error_ttl_exceeded",
 	},
 };
 
@@ -52,5 +52,3 @@ static struct gr_node_info info = {
 };
 
 GR_NODE_REGISTER(info);
-
-GR_DROP_REGISTER(ip6_forward_ttl_exceeded);

modules/ip6/datapath/ip6_input.c
@@ -19,7 +19,7 @@
 enum edges {
 	FORWARD = 0,
 	LOCAL,
-	NO_ROUTE,
+	DEST_UNREACH,
 	NOT_MEMBER,
 	OTHER_HOST,
 	BAD_VERSION,
@@ -98,7 +98,7 @@ ip6_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 
 		nh = ip6_route_lookup(iface->vrf_id, &ip->dst_addr);
 		if (nh == NULL) {
-			edge = NO_ROUTE;
+			edge = DEST_UNREACH;
 			goto next;
 		}
 
@@ -131,7 +131,7 @@ static struct rte_node_register input_node = {
 	.next_nodes = {
 		[FORWARD] = "ip6_forward",
 		[LOCAL] = "ip6_input_local",
-		[NO_ROUTE] = "ip6_input_no_route",
+		[DEST_UNREACH] = "ip6_error_dest_unreach",
 		[NOT_MEMBER] = "ip6_input_not_member",
 		[OTHER_HOST] = "ip6_input_other_host",
 		[BAD_VERSION] = "ip6_input_bad_version",
@@ -147,7 +147,6 @@ static struct gr_node_info info = {
 
 GR_NODE_REGISTER(info);
 
-GR_DROP_REGISTER(ip6_input_no_route);
 GR_DROP_REGISTER(ip6_input_not_member);
 GR_DROP_REGISTER(ip6_input_other_host);
 GR_DROP_REGISTER(ip6_input_bad_version);

modules/ip6/datapath/ip6_output.c
@@ -21,7 +21,7 @@
 
 enum {
 	ETH_OUTPUT = 0,
-	NO_ROUTE,
+	DEST_UNREACH,
 	ERROR,
 	QUEUE_FULL,
 	EDGE_COUNT,
@@ -90,7 +90,7 @@ ip6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 
 		nh = ip6_output_mbuf_data(mbuf)->nh;
 		if (nh == NULL) {
-			edge = NO_ROUTE;
+			edge = DEST_UNREACH;
 			goto next;
 		}
 		iface = iface_from_id(nh->iface_id);
@@ -163,7 +163,7 @@ static struct rte_node_register output_node = {
 	.next_nodes = {
 		[ETH_OUTPUT] = "eth_output",
 		[ERROR] = "ip6_output_error",
-		[NO_ROUTE] = "ip6_output_no_route",
+		[DEST_UNREACH] = "ip6_error_dest_unreach",
 		[QUEUE_FULL] = "ndp_queue_full",
 	},
 };
@@ -175,5 +175,4 @@ static struct gr_node_info info = {
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(ip6_output_error);
-GR_DROP_REGISTER(ip6_output_no_route);
 GR_DROP_REGISTER(ndp_queue_full);

modules/ip6/datapath/meson.build
@@ -4,6 +4,7 @@
 src += files(
   'icmp6_input.c',
   'icmp6_output.c',
+  'ip6_error.c',
   'ip6_forward.c',
   'ip6_input.c',
   'ip6_local.c',

smoke/ip6_forward_test.sh
@@ -30,3 +30,5 @@ ip netns exec $p1 ping6 -i0.01 -c3 fd00:ba4:2::2
 ip netns exec $p2 ping6 -i0.01 -c3 fd00:ba4:1::2
 ip netns exec $p1 ping6 -i0.01 -c3 fd00:ba4:1::1
 ip netns exec $p2 ping6 -i0.01 -c3 fd00:ba4:2::1
+ip netns exec $p1 traceroute6 -N1 fd00:ba4:2::2
+ip netns exec $p2 traceroute6 -N1 fd00:ba4:1::2

commit fa34d54f051e2a0140a4bd5068cc78ecae919239

ip6: fix prepend size
Prepend was called with the wrong size.

Fixes: 7f068c47af42 ("ip6: add support for icmp6 errors")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/datapath/ip6_error.c
@@ -70,7 +70,7 @@ ip6_error_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		case ICMP6_ERR_TTL_EXCEEDED:
 			// clang-format off
 			te = (struct icmp6_err_ttl_exceeded *)
-				rte_pktmbuf_prepend(mbuf, sizeof(*du));
+				rte_pktmbuf_prepend(mbuf, sizeof(*te));
 			// clang-format on
 			if (unlikely(te == NULL)) {
 				edge = NO_HEADROOM;