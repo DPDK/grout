40d5e1798eba2b3aee9e33d5e6705964bebadc7b
Robin Jarry <rjarry@redhat.com>
2025-12-10 12:12:09 +0100

    ctlplane: handle interface renaming
    When a grout interface is renamed, rename its control plane counterpart.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- modules/infra/control/gr_netlink.h
+++ modules/infra/control/gr_netlink.h
@@ -9,6 +9,7 @@
 
 int netlink_link_set_admin_state(const char *ifname, bool up);
 int netlink_link_set_master(const char *ifname, const char *master_ifname);
+int netlink_link_set_name(uint32_t ifindex, const char *ifname);
 int netlink_link_add_vrf(const char *vrf_name, uint32_t table_id);
 int netlink_link_del_iface(const char *ifname);
 int netlink_add_route(const char *ifname, uint32_t table);
