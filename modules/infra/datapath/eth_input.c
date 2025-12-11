DPDK/grout

commit 236e7440516d3b7d54f7a37c87d517796d3cda9e

infra: add snap input node
SNAP (Sub Network Access Protocol) is used for STP or ISIS.
The frame handling is different: as the application opens a
raw socket, we need to forward the full frame.

As the ethertype and the snap len overlaps, the ethertype
is required to be greater than 1536 (0x600): everything below
must be considered as SNAP.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/eth_input.c
@@ -5,6 +5,7 @@
 #include <gr_graph.h>
 #include <gr_log.h>
 #include <gr_rxtx.h>
+#include <gr_snap.h>
 #include <gr_trace.h>
 #include <gr_vlan.h>
 
@@ -17,6 +18,7 @@ enum {
 	UNKNOWN_VLAN,
 	INVALID_IFACE,
 	IFACE_DOWN,
+	SNAP,
 	NB_EDGES,
 };
 
@@ -57,6 +59,11 @@ eth_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		l2_hdr_size = sizeof(*eth);
 		eth_type = eth->ether_type;
 		vlan_id = 0;
+
+		if (unlikely(rte_be_to_cpu_16(eth_type) < SNAP_MAX_LEN)) {
+			edge = SNAP;
+			goto snap;
+		}
 
 		if (m->ol_flags & RTE_MBUF_F_RX_VLAN_STRIPPED) {
 			vlan_id = m->vlan_tci & 0xfff;
@@ -120,6 +127,7 @@ next:
 			t->iface_id = eth_in->iface->id;
 		}
 		rte_pktmbuf_adj(m, l2_hdr_size);
+snap:
 		rte_node_enqueue_x1(graph, node, edge, m);
 	}
 	return nb_objs;
@@ -153,6 +161,7 @@ static struct rte_node_register node = {
 		[UNKNOWN_VLAN] = "eth_input_unknown_vlan",
 		[INVALID_IFACE] = "eth_input_invalid_iface",
 		[IFACE_DOWN] = "iface_input_admin_down",
+		[SNAP] = "snap_input",
 		// other edges are updated dynamically with gr_eth_input_add_type
 	},
 };

modules/infra/datapath/gr_snap.h
@@ -1,0 +1,27 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Christophe Fontaine
+
+#pragma once
+
+#include <gr_mbuf.h>
+
+#include <rte_byteorder.h>
+#include <rte_ether.h>
+
+#define SNAP_MAX_LEN 1536
+
+GR_MBUF_PRIV_DATA_TYPE(eth_snap_mbuf_data, {
+	struct rte_ether_addr src;
+	struct rte_ether_addr dst;
+	rte_be16_t len;
+});
+
+void gr_snap_input_add_mac_redirect(const struct rte_ether_addr *mac_addr, const char *next_node);
+
+struct snap_trace_data {
+	struct rte_ether_addr dst;
+	struct rte_ether_addr src;
+	uint16_t len;
+	uint16_t iface_id;
+};
+int snap_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);

modules/infra/datapath/meson.build
@@ -15,6 +15,7 @@ src += files(
   'port_output.c',
   'port_rx.c',
   'port_tx.c',
+  'snap_input.c',
   'trace.c',
 )
 inc += include_directories('.')

modules/infra/datapath/snap_input.c
@@ -1,0 +1,100 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Christophe Fontaine
+
+#include <gr_eth.h>
+#include <gr_snap.h>
+
+// MAC redirect static array for up to 64 MAC addresses
+#define MAX_MAC_REDIRECTS 64
+
+struct mac_redirect_entry {
+	struct rte_ether_addr mac_addr;
+	rte_edge_t edge;
+};
+
+static struct mac_redirect_entry mac_redirects[MAX_MAC_REDIRECTS];
+static unsigned int mac_redirect_count = 0;
+
+void gr_snap_input_add_mac_redirect(const struct rte_ether_addr *mac_addr, const char *next_node) {
+	rte_edge_t edge;
+
+	if (mac_redirect_count >= MAX_MAC_REDIRECTS)
+		ABORT("MAC redirect table full (max %d entries)", MAX_MAC_REDIRECTS);
+
+	for (unsigned int i = 0; i < mac_redirect_count; i++) {
+		if (rte_is_same_ether_addr(mac_addr, &mac_redirects[i].mac_addr))
+			ABORT("MAC redirect already exists: " ETH_F, mac_addr);
+	}
+
+	LOG(DEBUG, "snap_input: mac=" ETH_F " -> %s", mac_addr, next_node);
+	edge = gr_node_attach_parent("snap_input", next_node);
+	mac_redirects[mac_redirect_count].mac_addr = *mac_addr;
+	mac_redirects[mac_redirect_count].edge = edge;
+	mac_redirect_count++;
+}
+
+enum {
+	UNKNOWN_DST_MAC,
+	NB_EDGES,
+};
+
+int snap_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
+	const struct snap_trace_data *t = data;
+	const struct iface *iface = iface_from_id(t->iface_id);
+	const char *ifname = iface ? iface->name : "[deleted]";
+	size_t n = 0;
+
+	SAFE_BUF(snprintf, len, ETH_F " > " ETH_F " len=%u", &t->src, &t->dst, t->len);
+	SAFE_BUF(snprintf, len, " iface=%s", ifname);
+
+	return n;
+err:
+	return -1;
+}
+
+static uint16_t
+snap_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	struct rte_ether_hdr *eth;
+	struct rte_mbuf *m;
+	rte_edge_t edge;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		edge = UNKNOWN_DST_MAC;
+		eth = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+
+		for (uint16_t j = 0; j < mac_redirect_count; j++) {
+			if (rte_is_same_ether_addr(&eth->dst_addr, &mac_redirects[j].mac_addr)) {
+				edge = mac_redirects[j].edge;
+				break;
+			}
+		}
+
+		if (gr_mbuf_is_traced(m)) {
+			struct snap_trace_data *t = gr_mbuf_trace_add(m, node, sizeof(*t));
+			t->dst = eth->dst_addr;
+			t->src = eth->src_addr;
+			t->len = rte_be_to_cpu_16(eth->ether_type);
+			t->iface_id = mbuf_data(m)->iface->id;
+		}
+		rte_node_enqueue_x1(graph, node, edge, m);
+	}
+	return nb_objs;
+}
+
+static struct rte_node_register node = {
+	.name = "snap_input",
+	.process = snap_input_process,
+	.nb_edges = NB_EDGES,
+	.next_nodes = {
+		[UNKNOWN_DST_MAC] = "snap_input_unknown_dst_mac",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.trace_format = snap_trace_format,
+};
+
+GR_NODE_REGISTER(info);
+GR_DROP_REGISTER(snap_input_unknown_dst_mac);

commit 2964138ce13213cdf5508b2b337e4515624e5239

snap: allow snap over vlan
SNAP frames may be transmitted over vlan.
For control plane, this means we must send the frame over the proper
interface.

Example:
NOTICE: GROUT: [rx eno49v1] ea:e7:65:49:e1:d6 > 01:80:c2:00:00:14 / VLAN id=210 / SNAP / IS-IS all Level 1 IS's, (pkt_len=1514)
NOTICE: GROUT: [cp tx eno49v1] ea:e7:65:49:e1:d6 > 01:80:c2:00:00:14 / VLAN id=210 / SNAP / IS-IS all Level 1 IS's, (pkt_len=1514)

Currently, the frame is sent to the CP interface on the parent interface
instead of the VLAN sub-interface. In the eth_input node, pop the VLAN
tag and update the interface before evaluating redirection to the SNAP
node.

Fixes: 236e7440516d ("infra: add snap input node")
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/eth_input.c
@@ -60,11 +60,6 @@ eth_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		eth_type = eth->ether_type;
 		vlan_id = 0;
 
-		if (unlikely(rte_be_to_cpu_16(eth_type) < SNAP_MAX_LEN)) {
-			edge = SNAP;
-			goto snap;
-		}
-
 		if (m->ol_flags & RTE_MBUF_F_RX_VLAN_STRIPPED) {
 			vlan_id = m->vlan_tci & 0xfff;
 			m->ol_flags &= ~RTE_MBUF_F_RX_VLAN_STRIPPED;
@@ -90,6 +85,12 @@ eth_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			}
 			eth_in->iface = vlan_iface;
 		}
+
+		if (unlikely(rte_be_to_cpu_16(eth_type) < SNAP_MAX_LEN)) {
+			edge = SNAP;
+			goto snap;
+		}
+
 		edge = l2l3_edges[eth_type];
 
 		if (iface == NULL || iface->id != eth_in->iface->id) {