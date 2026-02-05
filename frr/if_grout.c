b554d1cc289ec5cbe169bfe0dd7a097d61e7500a
Maxime Leroy <maxime@leroys.fr>
2026-02-05 16:18:27 +0100

    frr: only set table_id for VRF interfaces in dplane_grout
    The table_id was set for all interfaces in VRF mode, but only VRF
    interfaces (loopbacks) have a table_id in the kernel. Align dplane_grout
    behavior with the kernel by only setting table_id when the interface
    type is ZEBRA_IF_VRF.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Reviewed-by: Robin Jarry <rjarry@redhat.com>

--- frr/if_grout.c
+++ frr/if_grout.c
@@ -110,9 +110,6 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		switch (gr_if->mode) {
 		case GR_IFACE_MODE_VRF:
 			if (gr_if->base.vrf_id != 0) {
-				dplane_ctx_set_ifp_table_id(
-					ctx, ifindex_grout_to_frr(gr_if->base.vrf_id)
-				);
 				// In Linux, vrf_id equals the interface index; in Grout we model
 				// a VRF with its gr‑vrf interface.
 				// The gr‑vrf’s ifindex is guaranteed to match vrf_id
@@ -120,8 +117,17 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 					ctx, ifindex_grout_to_frr(gr_if->base.vrf_id)
 				);
 			} else {
-				dplane_ctx_set_ifp_table_id(ctx, 0);
 				dplane_ctx_set_ifp_vrf_id(ctx, 0);
+			}
+
+			// For VRF interface, we must set the table_id
+			if (zif_type == ZEBRA_IF_VRF) {
+				if (gr_if->base.vrf_id != 0)
+					dplane_ctx_set_ifp_table_id(
+						ctx, ifindex_grout_to_frr(gr_if->base.vrf_id)
+					);
+				else
+					dplane_ctx_set_ifp_table_id(ctx, 0);
 			}
 			break;
 		case GR_IFACE_MODE_BOND:
