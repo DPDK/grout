DPDK/grout

commit d9a915ca309be7643d903e9c71837559b03bc78f

srv6: add tunsrc api
sr6_output node is slowed down by a systematic call to "addr6_get_preferred".
Mimicking linux API, we can now configure an ip address which will be
used for all packets rewrote by sr6_output.

grout# show stats software
NODE                        CALLS     PACKETS  PKTS/CALL  CYCLES/CALL  CYCLES/PKT
sr6_output                 339712    86966261      256.0      47440.7       185.3

With the new API, after configuring the tunsrc address:
grout# set sr tunsrc 3fff::1
grout# show stats software
NODE                       CALLS    PACKETS  PKTS/CALL  CYCLES/CALL    CYCLES/PKT
sr6_output              11030012  385426489       34.9       4021.7         115.1

Suggested-by: Maxime Leroy <maxime@leroys.fr>
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>

modules/srv6/cli/route.c
@@ -159,6 +159,40 @@ static cmd_status_t srv6_route_show(const struct gr_api_client *c, const struct 
 	return CMD_SUCCESS;
 }
 
+static cmd_status_t srv6_tunsrc_set(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_tunsrc_set_req req;
+
+	if (arg_ip6(p, "SRC", &req.addr) < 0)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_TUNSRC_SET, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_tunsrc_clear(const struct gr_api_client *c, const struct ec_pnode *) {
+	if (gr_api_client_send_recv(c, GR_SRV6_TUNSRC_CLEAR, 0, NULL, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_tunsrc_show(const struct gr_api_client *c, const struct ec_pnode *) {
+	struct gr_srv6_tunsrc_show_resp *resp;
+	void *resp_ptr;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_TUNSRC_SHOW, 0, NULL, &resp_ptr) < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+	printf("sr tunsrc addr " IP6_F "\n", &resp->addr);
+
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
 static int ctx_init(struct ec_node *root) {
 	int ret;
 
@@ -195,6 +229,33 @@ static int ctx_init(struct ec_node *root) {
 		"View all SR route",
 		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
 
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SET, CTX_ARG("sr", "Set srv6 stack elements.")),
+		"tunsrc SRC",
+		srv6_tunsrc_set,
+		"Set Segment Routing SRv6 source address",
+		with_help("Ipv6 address to use as source.", ec_node_re("SRC", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_CLEAR, CTX_ARG("sr", "Clear srv6 stack elements.")),
+		"tunsrc",
+		srv6_tunsrc_clear,
+		"Clear Segment Routing SRv6 source address"
+	);
+	if (ret < 0)
+		return ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("sr", "Show srv6 stack elements.")),
+		"tunsrc",
+		srv6_tunsrc_show,
+		"Show Segment Routing SRv6 source address"
 	);
 	if (ret < 0)
 		return ret;

modules/srv6/control/route.c
@@ -232,6 +232,64 @@ static struct api_out srv6_route_list(const void *request, void **response) {
 	return api_out(0, ctx.len);
 }
 
+struct nexthop *tunsrc_nh = NULL;
+
+static struct api_out srv6_tunsrc_clear(const void * /*request*/, void ** /*response*/) {
+	if (tunsrc_nh) {
+		nexthop_decref(tunsrc_nh);
+		tunsrc_nh = NULL;
+	}
+
+	return api_out(0, 0);
+}
+
+static struct api_out srv6_tunsrc_set(const void *request, void ** /*response*/) {
+	const struct gr_srv6_tunsrc_set_req *req = request;
+	struct nexthop *nh;
+
+	if (rte_ipv6_addr_is_unspec(&req->addr))
+		return srv6_tunsrc_clear(NULL, NULL);
+
+	struct gr_nexthop base = {
+		.type = GR_NH_T_L3,
+		.af = GR_AF_IP6,
+		.flags = GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_STATIC,
+		.state = GR_NH_S_REACHABLE,
+		.vrf_id = GR_VRF_ID_ALL,
+		.iface_id = GR_IFACE_ID_UNDEF,
+		.ipv6 = req->addr,
+		.prefixlen = 128,
+		.origin = GR_NH_ORIGIN_LINK,
+	};
+
+	if (tunsrc_nh)
+		nexthop_decref(tunsrc_nh);
+
+	if ((nh = nexthop_new(&base)) == NULL)
+		return api_out(-errno, 0);
+
+	tunsrc_nh = nh;
+	nexthop_incref(nh);
+
+	return api_out(0, 0);
+}
+
+static struct api_out srv6_tunsrc_show(const void * /*request*/, void **response) {
+	const struct rte_ipv6_addr unspec = RTE_IPV6_ADDR_UNSPEC;
+	struct gr_srv6_tunsrc_show_resp *resp;
+
+	if ((resp = calloc(1, sizeof(*resp))) == NULL)
+		return api_out(-ENOMEM, 0);
+
+	if (tunsrc_nh)
+		resp->addr = tunsrc_nh->ipv6;
+	else
+		resp->addr = unspec;
+
+	*response = resp;
+	return api_out(0, sizeof(*resp));
+}
+
 // srv6 headend module /////////////////////////////////////////////////////
 
 static struct gr_api_handler srv6_route_add_handler = {
@@ -249,6 +307,21 @@ static struct gr_api_handler srv6_route_list_handler = {
 	.request_type = GR_SRV6_ROUTE_LIST,
 	.callback = srv6_route_list,
 };
+static struct gr_api_handler srv6_tunsrc_set_handler = {
+	.name = "sr tunsrc set",
+	.request_type = GR_SRV6_TUNSRC_SET,
+	.callback = srv6_tunsrc_set,
+};
+static struct gr_api_handler srv6_tunsrc_clear_handler = {
+	.name = "sr tunsrc clear",
+	.request_type = GR_SRV6_TUNSRC_CLEAR,
+	.callback = srv6_tunsrc_clear,
+};
+static struct gr_api_handler srv6_tunsrc_show_handler = {
+	.name = "sr tunsrc show",
+	.request_type = GR_SRV6_TUNSRC_SHOW,
+	.callback = srv6_tunsrc_show,
+};
 
 static struct nexthop_type_ops nh_ops = {
 	.free = srv6_encap_data_del,
@@ -259,5 +332,8 @@ RTE_INIT(srv6_constructor) {
 	gr_register_api_handler(&srv6_route_add_handler);
 	gr_register_api_handler(&srv6_route_del_handler);
 	gr_register_api_handler(&srv6_route_list_handler);
+	gr_register_api_handler(&srv6_tunsrc_set_handler);
+	gr_register_api_handler(&srv6_tunsrc_clear_handler);
+	gr_register_api_handler(&srv6_tunsrc_show_handler);
 	nexthop_type_ops_register(GR_NH_T_SR6_OUTPUT, &nh_ops);
 }

modules/srv6/api/gr_srv6.h
@@ -63,6 +63,21 @@ struct gr_srv6_route_list_resp {
 	uint8_t route[/* n_route */];
 };
 
+#define GR_SRV6_TUNSRC_SET REQUEST_TYPE(GR_SRV6_MODULE, 0x0005)
+struct gr_srv6_tunsrc_set_req {
+	struct rte_ipv6_addr addr;
+};
+
+#define GR_SRV6_TUNSRC_CLEAR REQUEST_TYPE(GR_SRV6_MODULE, 0x0006)
+// struct gr_srv6_tunsrc_clear_req { };
+
+#define GR_SRV6_TUNSRC_SHOW REQUEST_TYPE(GR_SRV6_MODULE, 0x0007)
+// struct gr_srv6_tunsrc_show_req { };
+
+struct gr_srv6_tunsrc_show_resp {
+	struct rte_ipv6_addr addr;
+};
+
 // localsid (tunnel transit and exit) /////////////////////////////////
 
 //

modules/srv6/control/gr_srv6_nexthop.h
@@ -26,3 +26,9 @@ struct srv6_encap_data {
 // A pointer to it is stored in nexthop priv.
 //
 GR_NH_PRIV_DATA_TYPE(srv6_encap_nh_priv, { struct srv6_encap_data *d; });
+
+extern struct nexthop *tunsrc_nh;
+static inline const struct nexthop *
+sr_tunsrc_get(uint16_t iface_id, const struct rte_ipv6_addr *dst) {
+	return tunsrc_nh ? tunsrc_nh : addr6_get_preferred(iface_id, dst);
+}

modules/srv6/datapath/srv6_output.c
@@ -134,9 +134,7 @@ srv6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 		}
 		ip6_output_mbuf_data(m)->nh = nh;
 
-		// Use output interface ip as source address
-		// XXX is it safe from DP ? this one HAVE TO be cached, at least
-		nh = addr6_get_preferred(nh->iface_id, &nh->ipv6);
+		nh = sr_tunsrc_get(nh->iface_id, &nh->ipv6);
 		if (nh == NULL) {
 			// cannot output packet on interface that does not have ip6 addr
 			edge = NO_ROUTE;

commit 6240fd316395157c6c4bbf34029cc76282e165fc

srv6: remove orphaned VRF help node from tunsrc set command
The "srv6 tunsrc set SRC" command syntax does not accept a VRF
parameter, and the handler srv6_tunsrc_set does not parse one either.
Remove the unused VRF with_help node.

Fixes: d9a915ca309b ("srv6: add tunsrc api")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/srv6/cli/route.c
@@ -154,8 +154,7 @@ static int ctx_init(struct ec_node *root) {
 		"set SRC",
 		srv6_tunsrc_set,
 		"Set Segment Routing SRv6 source address",
-		with_help("Ipv6 address to use as source.", ec_node_re("SRC", IPV6_RE)),
-		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+		with_help("Ipv6 address to use as source.", ec_node_re("SRC", IPV6_RE))
 	);
 	if (ret < 0)
 		return ret;