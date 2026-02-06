DPDK/grout

commit 1ce0534479340817f133eae0673c7802dca5f37f

ctlplane: use netlink for carrier control
Replace TUNSETCARRIER ioctl with netlink IFLA_CARRIER attribute to
control the carrier state of TAP interfaces. This provides more
consistent interface state management through netlink.

Add IFLA_CARRIER attribute to netlink_link_set_admin_state() so that
both the admin state and carrier are set together.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/ctlplane.c
@@ -330,17 +330,6 @@ static void cp_delete(struct iface *iface) {
 		event_free_finalize(0, iface->cp_ev, finalize_fd);
 }
 
-static void cp_set_carrier(struct iface *iface) {
-#ifdef TUNSETCARRIER
-	int carrier = iface->flags & GR_IFACE_S_RUNNING ? 1 : 0;
-	if (ioctl(iface->cp_fd, TUNSETCARRIER, &carrier) < 0) {
-		LOG(ERR, "ioctl(TUNSETCARRIER): %s", strerror(errno));
-	}
-#else
-	(void)iface;
-#endif
-}
-
 static void cp_set_speed(struct iface *iface) {
 	struct ethtool_link_settings *els;
 	struct ifreq ifr = {0};
@@ -447,12 +436,10 @@ static void iface_event(uint32_t event, const void *obj) {
 		cp_delete(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_UP:
-		cp_set_speed(iface);
-		cp_set_carrier(iface);
 		netlink_link_set_admin_state(iface->name, true);
+		cp_set_speed(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_DOWN:
-		cp_set_carrier(iface);
 		netlink_link_set_admin_state(iface->name, false);
 		break;
 	}

modules/infra/control/netlink.c
@@ -49,7 +49,7 @@ again:
 }
 
 int netlink_link_set_admin_state(const char *ifname, bool up) {
-	char buf[NLMSG_SPACE(sizeof(struct ifinfomsg))];
+	char buf[NLMSG_SPACE(sizeof(struct ifinfomsg)) + NLA_SPACE(sizeof(uint8_t))];
 	struct nlmsghdr *nlh;
 	struct ifinfomsg *ifi;
 	uint32_t ifindex;
@@ -68,6 +68,8 @@ int netlink_link_set_admin_state(const char *ifname, bool up) {
 	ifi->ifi_index = ifindex;
 	ifi->ifi_change = IFF_UP;
 	ifi->ifi_flags = up ? IFF_UP : 0;
+
+	mnl_attr_put_u8(nlh, IFLA_CARRIER, up ? 1 : 0);
 
 	return netlink_send_req(nlh);
 }

commit fccf63b633592309694a24039aac6f12f2903ea3

infra: add carrier parameter to netlink_link_set_admin_state
VRF devices do not support the IFLA_CARRIER netlink attribute and
return EOPNOTSUPP when it is set. This was causing VRF interface
creation to fail.

Add a carrier parameter to let callers specify whether IFLA_CARRIER
should be set. TAP interfaces (ctlplane) use carrier=true to maintain
the intended behavior, while TUN (loopback) and VRF interfaces use
carrier=false to avoid the unsupported operation.

Fixes: 1ce053447934 ("ctlplane: use netlink for carrier control")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>

modules/infra/control/ctlplane.c
@@ -282,7 +282,7 @@ static void cp_create(struct iface *iface) {
 
 	snprintf(ifalias, IFALIASZ, "Grout control plane interface");
 	netlink_set_ifalias(iface->cp_id, ifalias);
-	netlink_link_set_admin_state(iface->cp_id, false);
+	netlink_link_set_admin_state(iface->cp_id, false, true);
 
 	iface->cp_ev = event_new(
 		ev_base,
@@ -412,11 +412,11 @@ static void iface_event(uint32_t event, const void *obj) {
 		cp_delete(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_UP:
-		netlink_link_set_admin_state(iface->cp_id, true);
+		netlink_link_set_admin_state(iface->cp_id, true, true);
 		cp_set_speed(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_DOWN:
-		netlink_link_set_admin_state(iface->cp_id, false);
+		netlink_link_set_admin_state(iface->cp_id, false, true);
 		break;
 	}
 }

modules/infra/control/gr_netlink.h
@@ -9,7 +9,7 @@
 
 #include <stdbool.h>
 
-int netlink_link_set_admin_state(uint32_t ifindex, bool up);
+int netlink_link_set_admin_state(uint32_t ifindex, bool up, bool carrier);
 int netlink_link_set_mac(uint32_t ifindex, const struct rte_ether_addr *mac);
 int netlink_link_set_master(uint32_t ifindex, uint32_t master_ifindex);
 int netlink_link_set_mtu(uint32_t ifindex, uint32_t mtu);

modules/infra/control/loopback.c
@@ -251,12 +251,12 @@ static int iface_loopback_init(struct iface *iface, const void * /* api_info */)
 		goto err;
 	}
 
-	if (netlink_link_set_admin_state(iface->cp_id, false) < 0) {
+	if (netlink_link_set_admin_state(iface->cp_id, false, false) < 0) {
 		LOG(ERR, "netlink_link_set_admin_state(false): %s", strerror(errno));
 		goto err;
 	}
 
-	if (netlink_link_set_admin_state(iface->cp_id, true) < 0) {
+	if (netlink_link_set_admin_state(iface->cp_id, true, false) < 0) {
 		LOG(ERR, "netlink_link_set_admin_state(true): %s", strerror(errno));
 		goto err;
 	}

modules/infra/control/netlink.c
@@ -50,7 +50,7 @@ again:
 	return 0;
 }
 
-int netlink_link_set_admin_state(uint32_t ifindex, bool up) {
+int netlink_link_set_admin_state(uint32_t ifindex, bool up, bool carrier) {
 	char buf[NLMSG_SPACE(sizeof(struct ifinfomsg)) + NLA_SPACE(sizeof(uint8_t))];
 	struct nlmsghdr *nlh;
 	struct ifinfomsg *ifi;
@@ -66,7 +66,8 @@ int netlink_link_set_admin_state(uint32_t ifindex, bool up) {
 	ifi->ifi_change = IFF_UP;
 	ifi->ifi_flags = up ? IFF_UP : 0;
 
-	mnl_attr_put_u8(nlh, IFLA_CARRIER, up ? 1 : 0);
+	if (carrier)
+		mnl_attr_put_u8(nlh, IFLA_CARRIER, up ? 1 : 0);
 
 	return netlink_send_req(nlh);
 }

modules/infra/control/vrf.c
@@ -50,11 +50,11 @@ static int netlink_create_vrf_and_enslave(
 	if (ret < 0)
 		return ret;
 
-	ret = netlink_link_set_admin_state(*vrf_ifindex, true);
+	ret = netlink_link_set_admin_state(*vrf_ifindex, true, false);
 	if (ret < 0)
 		return ret;
 
-	ret = netlink_link_set_admin_state(loop_ifindex, true);
+	ret = netlink_link_set_admin_state(loop_ifindex, true, false);
 	if (ret < 0)
 		return ret;
 
@@ -68,7 +68,7 @@ static int netlink_delete_vrf_and_unslave(uint32_t vrf_ifindex, uint32_t loop_if
 	if (ret < 0)
 		return ret;
 
-	ret = netlink_link_set_admin_state(loop_ifindex, false);
+	ret = netlink_link_set_admin_state(loop_ifindex, false, false);
 	if (ret < 0)
 		return ret;
 