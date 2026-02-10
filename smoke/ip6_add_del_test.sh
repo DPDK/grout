58b1fce0ef28fe80789e8860e5b82163c231f474
Robin Jarry <rjarry@redhat.com>
2026-02-10 13:29:03 +0100

    ip,ip6: flush addresses on interface mode change
    When an interface leaves VRF mode (e.g. reconfigured as cross-connect),
    any IPv4 and IPv6 addresses previously configured on it become invalid.
    Likewise, when an interface moves to a different VRF, its addresses
    belong to the old VRF and need to be removed.
    
    Subscribe to GR_EVENT_IFACE_POST_RECONFIG in both IPv4 and IPv6 address
    modules. On reconfiguration, flush all addresses when the interface is
    no longer in VRF mode or has moved to a different VRF. For IPv6, also
    reinitialize link-local and well-known multicast addresses when entering
    VRF mode or changing VRFs.
    
    Extend the IPv6 add/del smoke test to exercise VRF reassignment and
    cross-connect mode transitions.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

--- smoke/ip6_add_del_test.sh
+++ smoke/ip6_add_del_test.sh
@@ -14,3 +14,14 @@ grcli address del 2001::1/64 iface p0
 grcli address show
 grcli address add 2001::1/64 iface p0
 grcli address show
+
+grcli interface add vrf foo
+grcli interface set port p0 vrf foo
+grcli address show
+grcli nexthop show internal
+
+grcli interface add port p1 devargs net_tap1,iface=x-p1
+grcli interface set port p0 domain p1
+grcli interface set port p1 domain p0
+
+grcli nexthop show internal
