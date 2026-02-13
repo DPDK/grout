4ddcf1357b438d40b12fa9a5f3dcebc22873b6a6
Robin Jarry <rjarry@redhat.com>
2026-02-13 08:39:41 +0100

    l4: allow dynamic registration of UDP port aliases
    VXLAN uses UDP port 4789 by default but allows configuring a custom
    destination port per interface. Allow the control plane to register
    additional UDP ports at runtime as aliases for an already registered
    port, reusing the same datapath edge.
    
    Use reference counting so that multiple interfaces sharing the same
    non-default port do not interfere with each other during teardown.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Maxime Leroy <maxime@leroys.fr>

--- modules/l4/l4_input_local.c
+++ modules/l4/l4_input_local.c
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2024 Christophe Fontaine
 
-#include "gr_l4.h"
-
 #include <gr_datapath.h>
 #include <gr_graph.h>
 #include <gr_ip4_datapath.h>
 #include <gr_ip6_datapath.h>
+#include <gr_l4.h>
 #include <gr_log.h>
 #include <gr_port.h>
 #include <gr_trace.h>
@@ -20,6 +19,7 @@ enum edges {
 	EDGE_COUNT,
 };
 
+static unsigned udp_refcounts[UINT_NUM_VALUES(rte_be16_t)] = {0};
 static rte_edge_t udp_edges[UINT_NUM_VALUES(rte_be16_t)] = {MANAGEMENT};
 
 void l4_input_register_port(uint8_t proto, rte_be16_t port, const char *next_node) {
@@ -30,10 +30,38 @@ void l4_input_register_port(uint8_t proto, rte_be16_t port, const char *next_nod
 		if (udp_edges[port] != MANAGEMENT)
 			ABORT("next node already registered for udp port=%hu", p);
 		udp_edges[port] = gr_node_attach_parent("l4_input_local", next_node);
+		udp_refcounts[port]++;
 		break;
 	default:
 		ABORT("proto not supported %hhu", proto);
 	}
+}
+
+int l4_input_alias_port(uint8_t proto, rte_be16_t port, rte_be16_t alias) {
+	assert(proto == IPPROTO_UDP);
+
+	if (udp_edges[port] == MANAGEMENT)
+		return errno_set(EADDRNOTAVAIL);
+	if (udp_edges[alias] != MANAGEMENT && udp_edges[alias] != udp_edges[port])
+		return errno_set(EADDRINUSE);
+
+	udp_edges[alias] = udp_edges[port];
+	udp_refcounts[alias]++;
+
+	return 0;
+}
+
+int l4_input_unalias_port(uint8_t proto, rte_be16_t alias) {
+	assert(proto == IPPROTO_UDP);
+
+	if (udp_edges[alias] == MANAGEMENT || udp_refcounts[alias] == 0)
+		return errno_set(EADDRNOTAVAIL);
+
+	udp_refcounts[alias]--;
+	if (udp_refcounts[alias] == 0)
+		udp_edges[alias] = MANAGEMENT;
+
+	return 0;
 }
 
 static uint16_t l4_input_local_process(
