8d8122e4606e28d8160f930ee7a4ace69c0fb3d6
Robin Jarry <rjarry@redhat.com>
2026-02-15 21:57:57 +0100

    frr: use standard zebra dplane debug flag
    Replace the custom zebra_debug_dplane_grout variable and
    ZEBRA_DEBUG_DPLANE_GROUT environment variable with FRR's existing
    IS_ZEBRA_DEBUG_DPLANE_DPDK flag. This is safe because grout replaces
    the DPDK dplane provider entirely, so both flags can never be active
    at the same time. Adding a new `debug zebra dplane grout` command
    would require patching zebra itself.
    
    Debug logging is now controlled at runtime via `debug zebra dplane
    dpdk` in vtysh or frr.conf. Enable it by default in the smoke test
    frr.conf. Remove the ZEBRA_DEBUG_DPLANE_GROUT=1 export and the
    run_frr toggle that depended on it.
    
    Change early connection logs to NOTICE since they fire before
    frr.conf has been read. These are needed to detect zebra startup
    at the beginning of smoke tests.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Maxime Leroy <maxime@leroys.fr>

--- frr/log_grout.h
+++ frr/log_grout.h
@@ -6,15 +6,10 @@
 #include <lib/zlog.h>
 #include <zebra/debug.h>
 
-extern unsigned long zebra_debug_dplane_grout;
-
-#define ZEBRA_DEBUG_DPLANE_GROUT 0x01
-#define IS_ZEBRA_DEBUG_DPLANE_GROUT (zebra_debug_dplane_grout & ZEBRA_DEBUG_DPLANE_GROUT)
-
 #define gr_log(priority, fmt, ...) zlog(priority, "GROUT: " fmt, ##__VA_ARGS__)
 #define gr_log_err(fmt, ...) gr_log(LOG_ERR, fmt, ##__VA_ARGS__)
 #define gr_log_debug(fmt, ...)                                                                     \
 	do {                                                                                       \
-		if (IS_ZEBRA_DEBUG_DPLANE_GROUT)                                                   \
+		if (IS_ZEBRA_DEBUG_DPLANE_DPDK)                                                    \
 			gr_log(LOG_DEBUG, "%s: " fmt, __func__, ##__VA_ARGS__);                    \
 	} while (0)
