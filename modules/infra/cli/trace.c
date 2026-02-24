DPDK/grout

commit 22ef4775c301722e8dedf2dbdb1b9097af760e0a

logging: use a single api endpoint
No need for two separate API handlers for such a trivial functionality.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/cli/trace.c
@@ -93,15 +93,10 @@ static cmd_status_t trace_clear(struct gr_api_client *c, const struct ec_pnode *
 	return CMD_SUCCESS;
 }
 
-static cmd_status_t packet_logging_set(struct gr_api_client *c, const struct ec_pnode *) {
-	if (gr_api_client_send_recv(c, GR_INFRA_PACKET_LOG_SET, 0, NULL, NULL) < 0)
-		return CMD_ERROR;
+static cmd_status_t packet_logging_set(struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_infra_packet_log_set_req req = {.enabled = arg_str(p, "enable") != NULL};
 
-	return CMD_SUCCESS;
-}
-
-static cmd_status_t packet_logging_clear(struct gr_api_client *c, const struct ec_pnode *) {
-	if (gr_api_client_send_recv(c, GR_INFRA_PACKET_LOG_CLEAR, 0, NULL, NULL) < 0)
+	if (gr_api_client_send_recv(c, GR_INFRA_PACKET_LOG_SET, sizeof(req), &req, NULL) < 0)
 		return CMD_ERROR;
 
 	return CMD_SUCCESS;
@@ -170,7 +165,7 @@ static int ctx_init(struct ec_node *root) {
 	ret = CLI_COMMAND(
 		LOGGING_CTX(root),
 		"disable",
-		packet_logging_clear,
+		packet_logging_set,
 		"Disable logging of ingress/egress packets."
 	);
 	if (ret < 0)

modules/infra/api/trace.c
@@ -77,13 +77,9 @@ static struct api_out clear_trace(const void * /*request*/, struct api_ctx *) {
 	return api_out(0, 0, NULL);
 }
 
-static struct api_out packet_log_enable(const void * /*request */, struct api_ctx *) {
-	gr_config.log_packets = true;
-	return api_out(0, 0, NULL);
-}
-
-static struct api_out packet_log_disable(const void * /*request */, struct api_ctx *) {
-	gr_config.log_packets = false;
+static struct api_out packet_log_set(const void *request, struct api_ctx *) {
+	const struct gr_infra_packet_log_set_req *req = request;
+	gr_config.log_packets = req->enabled;
 	return api_out(0, 0, NULL);
 }
 
@@ -108,13 +104,7 @@ static struct gr_api_handler clear_trace_handler = {
 static struct gr_api_handler set_packet_log_handler = {
 	.name = "set packet logging",
 	.request_type = GR_INFRA_PACKET_LOG_SET,
-	.callback = packet_log_enable,
-};
-
-static struct gr_api_handler clear_packet_log_handler = {
-	.name = "clear packet logging",
-	.request_type = GR_INFRA_PACKET_LOG_CLEAR,
-	.callback = packet_log_disable,
+	.callback = packet_log_set,
 };
 
 static struct gr_event_subscription iface_add_sub = {
@@ -128,6 +118,5 @@ RTE_INIT(trace_init) {
 	gr_register_api_handler(&dump_trace_handler);
 	gr_register_api_handler(&clear_trace_handler);
 	gr_register_api_handler(&set_packet_log_handler);
-	gr_register_api_handler(&clear_packet_log_handler);
 	gr_event_subscribe(&iface_add_sub);
 }

modules/infra/api/gr_infra.h
@@ -358,13 +358,14 @@ struct gr_infra_packet_trace_set_req {
 
 // struct gr_infra_packet_trace_set_resp { };
 
+// Enable/disable packet ingress/egress logging.
 #define GR_INFRA_PACKET_LOG_SET REQUEST_TYPE(GR_INFRA_MODULE, 0x0043)
-// struct gr_infra_packet_log_set_req { };
-// struct gr_infra_packet_log_set_resp { };
 
-#define GR_INFRA_PACKET_LOG_CLEAR REQUEST_TYPE(GR_INFRA_MODULE, 0x0044)
-// struct gr_infra_packet_log_clear_req { };
-// struct gr_infra_packet_log_clear_resp { };
+struct gr_infra_packet_log_set_req {
+	bool enabled;
+};
+
+// struct gr_infra_packet_log_set_resp { };
 
 // cpu affinities //////////////////////////////////////////////////////////////
 #define GR_INFRA_CPU_AFFINITY_GET REQUEST_TYPE(GR_INFRA_MODULE, 0x0050)

commit 1df95f0eaaec6149907caa778879086952432e3e

cli: fix logging enable
It is not possible to enable packet logging from the CLI. Since the
"enable" and "disable" nodes do not have any ID, arg_str("enable")
always returns NULL.

Fixes: 22ef4775c301 ("logging: use a single api endpoint")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Anthony Harivel <aharivel@redhat.com>

modules/infra/cli/trace.c
@@ -155,18 +155,16 @@ static int ctx_init(struct ec_node *root) {
 
 	ret = CLI_COMMAND(
 		LOGGING_CTX(root),
-		"enable",
+		"enable|disable",
 		packet_logging_set,
-		"Enable logging of ingress/egress packets."
-	);
-	if (ret < 0)
-		return ret;
-
-	ret = CLI_COMMAND(
-		LOGGING_CTX(root),
-		"disable",
-		packet_logging_set,
-		"Disable logging of ingress/egress packets."
+		"Control logging of ingress/egress packets.",
+		with_help(
+			"Enable logging of ingress/egress packets.", ec_node_str("enable", "enable")
+		),
+		with_help(
+			"Disable logging of ingress/egress packets.",
+			ec_node_str("disable", "disable")
+		)
 	);
 	if (ret < 0)
 		return ret;