DPDK/grout

commit 770168529e5cce582018b8081d8ecae4848b7c7f

port: add dedicated port_tx functions
Move VLAN subinterface resolution from eth_output to iface_output. When
the destination interface is a VLAN, resolve its parent and store the
VLAN ID in iface_mbuf_data. Update the output interface pointer to the
parent so that downstream nodes operate on the physical port or bond.

Move VLAN header insertion from eth_output to port_tx. Add four
dedicated process functions selected at graph creation time based on
port capabilities and queue sharing:

 tx_process                - software VLAN insertion, exclusive queue
 tx_shared_process         - software VLAN insertion, shared queue (spinlock)
 tx_offload_process        - hardware VLAN offload, exclusive queue
 tx_shared_offload_process - hardware VLAN offload, shared queue (spinlock)

When RTE_ETH_TX_OFFLOAD_VLAN_INSERT is available, set RTE_MBUF_F_TX_VLAN
and vlan_tci on the mbuf and let the hardware insert the tag. Otherwise,
insert the VLAN header in software.

Add rxtx_flags_t to identify which TX path variant handled a packet.
Record these flags in TX traces instead of port/queue, which is already
visible from the node clone name.

Simplify eth_input and eth_output traces to only record the Ethernet
header. The interface and VLAN information moves to iface_output. The
eth_input trace temporarily loses this context; it will be restored
once a similar iface_input node is introduced on the RX path.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/datapath/port_rx.c
@@ -24,8 +24,17 @@ enum {
 };
 
 int rxtx_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
-	const struct port_queue *t = data;
-	return snprintf(buf, len, "port=%u queue=%u", t->port_id, t->queue_id);
+	rxtx_flags_t flags = *(const rxtx_flags_t *)data;
+	size_t n = 0;
+
+	if (flags & RXTX_F_VLAN_OFFLOAD)
+		SAFE_BUF(snprintf, len, "vlan_offload");
+	if (flags & RXTX_F_TXQ_SHARED)
+		SAFE_BUF(snprintf, len, "%sshared", n ? " " : "");
+
+	return n;
+err:
+	return -1;
 }
 
 static rte_edge_t edges[UINT_NUM_VALUES(gr_iface_mode_t)] = {IFACE_MODE_UNKNOWN};

modules/infra/control/graph.c
@@ -92,6 +92,7 @@ worker_graph_new(struct worker *worker, uint8_t index, gr_vec struct iface_info_
 	char graph_name[RTE_GRAPH_NAMESIZE];
 	char node_name[RTE_NODE_NAMESIZE];
 	gr_vec char **rx_nodes = NULL;
+	struct iface_info_port *port;
 	struct queue_map *qmap;
 	struct rte_node *node;
 	uint16_t graph_uid;
@@ -191,6 +192,7 @@ worker_graph_new(struct worker *worker, uint8_t index, gr_vec struct iface_info_
 		struct tx_node_ctx *ctx = tx_node_ctx(node);
 		ctx->txq.port_id = qmap->port_id;
 		ctx->txq.queue_id = qmap->queue_id;
+		port = find_port(ports, qmap->port_id);
 
 		// set spinlock only if multiple workers share this TX queue
 		unsigned txq_users = 0;
@@ -202,14 +204,22 @@ worker_graph_new(struct worker *worker, uint8_t index, gr_vec struct iface_info_
 			}
 		}
 		if (txq_users > 1) {
-			struct iface_info_port *port = find_port(ports, qmap->port_id);
 			ctx->lock = &port->txq_locks[qmap->queue_id];
+			if (port->tx_offloads & RTE_ETH_TX_OFFLOAD_VLAN_INSERT)
+				node->process = tx_shared_offload_process;
+			else
+				node->process = tx_shared_process;
 			LOG(WARNING,
 			    "[CPU %d] port %s txq %u shared by %u workers",
 			    worker->cpu_id,
 			    port->devargs,
 			    qmap->queue_id,
 			    txq_users);
+		} else {
+			if (port->tx_offloads & RTE_ETH_TX_OFFLOAD_VLAN_INSERT)
+				node->process = tx_offload_process;
+			else
+				node->process = tx_process;
 		}
 
 		for (rte_edge_t edge = 0; edge < gr_vec_len(tx_node_names); edge++) {

modules/infra/datapath/eth_input.c
@@ -117,12 +117,8 @@ eth_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 next:
 		if (gr_mbuf_is_traced(m)
 		    || (vlan_iface && vlan_iface->flags & GR_IFACE_F_PACKET_TRACE)) {
-			struct eth_trace_data *t = gr_mbuf_trace_add(m, node, sizeof(*t));
-			t->eth.dst_addr = eth->dst_addr;
-			t->eth.src_addr = eth->src_addr;
-			t->eth.ether_type = eth_type;
-			t->vlan_id = vlan_id;
-			t->iface_id = eth_in->iface->id;
+			struct rte_ether_hdr *t = gr_mbuf_trace_add(m, node, sizeof(*t));
+			*t = *eth;
 		}
 		rte_pktmbuf_adj(m, l2_hdr_size);
 snap:
@@ -135,18 +131,11 @@ snap:
 }
 
 int eth_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
-	const struct eth_trace_data *t = data;
-	const struct iface *iface = iface_from_id(t->iface_id);
-	const char *ifname = iface ? iface->name : "[deleted]";
+	const struct rte_ether_hdr *t = data;
 	size_t n = 0;
 
-	SAFE_BUF(snprintf, len, ETH_F " > " ETH_F " type=", &t->eth.src_addr, &t->eth.dst_addr);
-	SAFE_BUF(eth_type_format, len, t->eth.ether_type);
-
-	if (t->vlan_id != 0)
-		SAFE_BUF(snprintf, len, " vlan=%u", t->vlan_id);
-
-	SAFE_BUF(snprintf, len, " iface=%s", ifname);
+	SAFE_BUF(snprintf, len, ETH_F " > " ETH_F " type=", &t->src_addr, &t->dst_addr);
+	SAFE_BUF(eth_type_format, len, t->ether_type);
 
 	return n;
 err:

modules/infra/datapath/eth_output.c
@@ -6,9 +6,8 @@
 #include <gr_iface.h>
 #include <gr_log.h>
 #include <gr_net_types.h>
-#include <gr_port.h>
+#include <gr_rxtx.h>
 #include <gr_trace.h>
-#include <gr_vlan.h>
 
 #include <rte_ether.h>
 
@@ -18,7 +17,6 @@ enum {
 	OUTPUT = 0,
 	NO_HEADROOM,
 	NO_MAC,
-	NO_PARENT,
 	NB_EDGES,
 };
 
@@ -26,45 +24,16 @@ static uint16_t
 eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_output_mbuf_data *priv;
 	struct rte_ether_addr src_mac;
-	const struct iface *iface;
 	struct rte_ether_hdr *eth;
+	uint16_t last_iface_id;
 	struct rte_mbuf *mbuf;
-	uint16_t vlan_id;
 	rte_edge_t edge;
+
+	last_iface_id = GR_IFACE_ID_UNDEF;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
 		priv = eth_output_mbuf_data(mbuf);
-		iface = priv->iface;
-		vlan_id = 0;
-
-		if (priv->iface->type == GR_IFACE_TYPE_VLAN) {
-			struct iface_stats *stats = iface_get_stats(
-				rte_lcore_id(), priv->iface->id
-			);
-			stats->tx_packets += 1;
-			stats->tx_bytes += rte_pktmbuf_pkt_len(mbuf);
-
-			const struct iface_info_vlan *sub = iface_info_vlan(priv->iface);
-			priv->iface = iface_from_id(sub->parent_id);
-			if (priv->iface == NULL) {
-				edge = NO_PARENT;
-				goto next;
-			}
-			struct rte_vlan_hdr *vlan = gr_mbuf_prepend(mbuf, vlan);
-			if (unlikely(vlan == NULL)) {
-				edge = NO_HEADROOM;
-				goto next;
-			}
-			vlan_id = sub->vlan_id;
-			vlan->vlan_tci = rte_cpu_to_be_16(sub->vlan_id);
-			vlan->eth_proto = priv->ether_type;
-			priv->ether_type = RTE_BE16(RTE_ETHER_TYPE_VLAN);
-			src_mac = sub->mac;
-		} else if (iface_get_eth_addr(priv->iface, &src_mac) < 0) {
-			edge = NO_MAC;
-			goto next;
-		}
 
 		eth = gr_mbuf_prepend(mbuf, eth);
 		if (unlikely(eth == NULL)) {
@@ -72,18 +41,23 @@ eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 			goto next;
 		}
 		eth->dst_addr = priv->dst;
+		if (priv->iface->id != last_iface_id) {
+			if (iface_get_eth_addr(priv->iface, &src_mac) < 0) {
+				edge = NO_MAC;
+				goto next;
+			}
+			last_iface_id = priv->iface->id;
+		}
 		eth->src_addr = src_mac;
 		eth->ether_type = priv->ether_type;
 
 		edge = OUTPUT;
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
-			struct eth_trace_data *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
-			t->eth.dst_addr = priv->dst;
-			t->eth.src_addr = src_mac;
-			t->eth.ether_type = priv->ether_type;
-			t->vlan_id = vlan_id;
-			t->iface_id = iface->id;
+			struct rte_ether_hdr *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
+			t->dst_addr = priv->dst;
+			t->src_addr = src_mac;
+			t->ether_type = priv->ether_type;
 		}
 		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
@@ -101,7 +75,6 @@ static struct rte_node_register node = {
 		[OUTPUT] = "iface_output",
 		[NO_HEADROOM] = "error_no_headroom",
 		[NO_MAC] = "eth_output_no_mac",
-		[NO_PARENT] = "eth_output_vlan_no_parent",
 	},
 };
 
@@ -114,4 +87,3 @@ static struct gr_node_info info = {
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(eth_output_no_mac);
-GR_DROP_REGISTER(eth_output_vlan_no_parent);

modules/infra/datapath/gr_eth.h
@@ -28,12 +28,6 @@ GR_MBUF_PRIV_DATA_TYPE(eth_output_mbuf_data, {
 
 void gr_eth_input_add_type(rte_be16_t eth_type, const char *node_name);
 
-struct eth_trace_data {
-	struct rte_ether_hdr eth;
-	uint16_t vlan_id;
-	uint16_t iface_id;
-};
-
 int eth_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);
 
 int lacp_send_pdu(const struct iface *, const struct lacp_pdu *);

modules/infra/datapath/gr_rxtx.h
@@ -6,6 +6,7 @@
 #include <gr_control_queue.h>
 #include <gr_graph.h>
 #include <gr_infra.h>
+#include <gr_mbuf.h>
 
 #include <rte_build_config.h>
 #include <rte_graph.h>
@@ -39,6 +40,8 @@ struct port_output_edges {
 	rte_edge_t edges[RTE_MAX_ETHPORTS];
 };
 
+GR_MBUF_PRIV_DATA_TYPE(iface_mbuf_data, { uint16_t vlan_id; });
+
 int rxtx_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);
 
 void iface_input_mode_register(gr_iface_mode_t, const char *next_node);
@@ -46,6 +49,16 @@ void iface_input_mode_register(gr_iface_mode_t, const char *next_node);
 void iface_output_type_register(gr_iface_type_t, const char *next_node);
 
 void iface_cp_tx(void *obj, uintptr_t priv, const struct control_queue_drain *);
+
+typedef enum : uint16_t {
+	RXTX_F_VLAN_OFFLOAD = GR_BIT16(0),
+	RXTX_F_TXQ_SHARED = GR_BIT16(1),
+} rxtx_flags_t;
+
+uint16_t tx_offload_process(struct rte_graph *, struct rte_node *, void **, uint16_t);
+uint16_t tx_process(struct rte_graph *, struct rte_node *, void **, uint16_t);
+uint16_t tx_shared_offload_process(struct rte_graph *, struct rte_node *, void **, uint16_t);
+uint16_t tx_shared_process(struct rte_graph *, struct rte_node *, void **, uint16_t);
 
 #define IFACE_STATS_VARS(dir)                                                                      \
 	struct iface_stats *dir##_stats;                                                           \

modules/infra/datapath/iface_output.c
@@ -5,14 +5,17 @@
 #include <gr_iface.h>
 #include <gr_log.h>
 #include <gr_mbuf.h>
+#include <gr_port.h>
 #include <gr_rxtx.h>
 #include <gr_trace.h>
+#include <gr_vlan.h>
 
 #include <stdint.h>
 
 enum {
 	INVAL = 0,
 	IFACE_DOWN,
+	NO_PARENT,
 	NB_EDGES,
 };
 
@@ -28,12 +31,32 @@ void iface_output_type_register(gr_iface_type_t type, const char *next_node) {
 	iface_type_edges[type] = gr_node_attach_parent("iface_output", next_node);
 }
 
+struct iface_output_trace_data {
+	uint16_t iface_id;
+	uint16_t vlan_id;
+};
+
+static int iface_output_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
+	const struct iface_output_trace_data *t = data;
+	const struct iface *iface = iface_from_id(t->iface_id);
+	size_t n = 0;
+
+	SAFE_BUF(snprintf, len, "iface=%s", iface ? iface->name : "[deleted]");
+	if (t->vlan_id != 0)
+		SAFE_BUF(snprintf, len, " vlan=%u", t->vlan_id);
+
+	return n;
+err:
+	return -1;
+}
+
 static uint16_t iface_output_process(
 	struct rte_graph *graph,
 	struct rte_node *node,
 	void **objs,
 	uint16_t nb_objs
 ) {
+	uint16_t iface_id, vlan_id;
 	const struct iface *iface;
 	struct rte_mbuf *m;
 	rte_edge_t edge;
@@ -42,13 +65,27 @@ static uint16_t iface_output_process(
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		m = objs[i];
-
 		iface = mbuf_data(m)->iface;
-		if (iface == NULL) {
-			edge = INVAL;
-			goto next;
+		iface_id = iface->id;
+
+		if (iface->type == GR_IFACE_TYPE_VLAN) {
+			const struct iface_info_vlan *vlan = iface_info_vlan(iface);
+			vlan_id = vlan->vlan_id;
+			iface = iface_from_id(vlan->parent_id);
+		} else {
+			vlan_id = 0;
 		}
 
+		if (gr_mbuf_is_traced(m)) {
+			struct iface_output_trace_data *t = gr_mbuf_trace_add(m, node, sizeof(*t));
+			t->iface_id = iface_id;
+			t->vlan_id = vlan_id;
+		}
+
+		if (iface == NULL) {
+			edge = NO_PARENT;
+			goto next;
+		}
 		if (!(iface->flags & GR_IFACE_F_UP)) {
 			edge = IFACE_DOWN;
 			goto next;
@@ -56,25 +93,16 @@ static uint16_t iface_output_process(
 
 		IFACE_STATS_INC(tx, m, iface);
 
+		iface_mbuf_data(m)->iface = iface;
+		iface_mbuf_data(m)->vlan_id = vlan_id;
 		edge = iface_type_edges[iface->type];
-
 next:
-		if (gr_mbuf_is_traced(m)) {
-			uint16_t *iface_id = gr_mbuf_trace_add(m, node, sizeof(*iface_id));
-			*iface_id = iface ? iface->id : 0;
-		}
 		rte_node_enqueue_x1(graph, node, edge, m);
 	}
 
 	IFACE_STATS_FLUSH(tx);
 
 	return nb_objs;
-}
-
-static int iface_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
-	const uint16_t *iface_id = data;
-	const struct iface *iface = iface_from_id(*iface_id);
-	return snprintf(buf, len, "iface=%s", iface ? iface->name : "[deleted]");
 }
 
 static struct rte_node_register node = {
@@ -86,16 +114,18 @@ static struct rte_node_register node = {
 	.next_nodes = {
 		[INVAL] = "iface_output_inval_type",
 		[IFACE_DOWN] = "iface_output_admin_down",
+		[NO_PARENT] = "iface_output_vlan_no_parent",
 	},
 };
 
 static struct gr_node_info info = {
 	.node = &node,
 	.type = GR_NODE_T_L1,
-	.trace_format = iface_trace_format,
+	.trace_format = iface_output_trace_format,
 };
 
 GR_NODE_REGISTER(info);
 
 GR_DROP_REGISTER(iface_output_inval_type);
 GR_DROP_REGISTER(iface_output_admin_down);
+GR_DROP_REGISTER(iface_output_vlan_no_parent);

modules/infra/datapath/port_tx.c
@@ -20,60 +20,198 @@
 enum {
 	TX_ERROR = 0,
 	TX_DOWN,
+	NO_HEADROOM,
 	NB_EDGES,
 };
 
-static uint16_t
-tx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
-	const struct tx_node_ctx *ctx = tx_node_ctx(node);
-	struct rte_mbuf **mbufs = (struct rte_mbuf **)objs;
+static inline bool tx_begin(
+	const struct tx_node_ctx *ctx,
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
 	const struct iface_info_port *port;
 	const struct iface *iface;
-	uint16_t tx_ok;
 
-	iface = mbuf_data(mbufs[0])->iface;
+	iface = mbuf_data(objs[0])->iface;
 	port = iface_info_port(iface);
 
-	if (!(iface->flags & GR_IFACE_F_UP) || !port->started) {
+	if (!port->started) {
 		for (unsigned i = 0; i < nb_objs; i++) {
-			if (gr_mbuf_is_traced(mbufs[i])) {
+			if (gr_mbuf_is_traced(objs[i])) {
 				struct port_queue *t;
-				t = gr_mbuf_trace_add(mbufs[i], node, sizeof(*t));
+				t = gr_mbuf_trace_add(objs[i], node, sizeof(*t));
 				*t = ctx->txq;
 			}
 		}
 		rte_node_enqueue(graph, node, TX_DOWN, objs, nb_objs);
-		return 0;
+		return false;
 	}
 
 	if (unlikely(gr_config.log_packets)) {
 		for (unsigned i = 0; i < nb_objs; i++) {
-			struct rte_mbuf *m = mbufs[i];
+			struct rte_mbuf *m = objs[i];
 			const struct iface *iface = mbuf_data(m)->iface;
 			trace_log_packet(m, "tx", iface->name);
 		}
 	}
 
-	if (likely(ctx->lock == NULL)) {
-		tx_ok = rte_eth_tx_burst(ctx->txq.port_id, ctx->txq.queue_id, mbufs, nb_objs);
-	} else {
-		rte_spinlock_lock(ctx->lock);
-		tx_ok = rte_eth_tx_burst(ctx->txq.port_id, ctx->txq.queue_id, mbufs, nb_objs);
-		rte_spinlock_unlock(ctx->lock);
-	}
+	return true;
+}
 
+static inline void tx_finish(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs,
+	uint16_t tx_ok,
+	rxtx_flags_t flags
+) {
 	if (tx_ok < nb_objs)
 		rte_node_enqueue(graph, node, TX_ERROR, &objs[tx_ok], nb_objs - tx_ok);
 
 	for (unsigned i = 0; i < tx_ok; i++) {
 		// FIXME racy: we are operating on mbufs already passed to driver
-		if (gr_mbuf_is_traced(mbufs[i])) {
-			struct port_queue *t;
-			t = gr_mbuf_trace_add(mbufs[i], node, sizeof(*t));
-			*t = ctx->txq;
-			gr_mbuf_trace_finish(mbufs[i]);
+		if (gr_mbuf_is_traced(objs[i])) {
+			rxtx_flags_t *t = gr_mbuf_trace_add(objs[i], node, sizeof(*t));
+			*t = flags;
+			gr_mbuf_trace_finish(objs[i]);
 		}
 	}
+}
+
+static inline uint16_t tx_add_vlan(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs,
+	struct rte_mbuf **mbufs
+) {
+	const struct iface_mbuf_data *d;
+	struct rte_ether_hdr *eth;
+	struct rte_vlan_hdr *vlan;
+	struct rte_mbuf *m;
+	uint16_t ok = 0;
+	void *data;
+
+	for (unsigned i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		d = iface_mbuf_data(m);
+		if (d->vlan_id != 0) {
+			eth = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+			data = gr_mbuf_prepend(m, vlan);
+			if (data == NULL) {
+				rte_node_enqueue_x1(graph, node, NO_HEADROOM, m);
+				continue;
+			}
+			memmove(data, eth, sizeof(*eth) - sizeof(eth->ether_type));
+			eth = data;
+			vlan = PAYLOAD(eth);
+			vlan->vlan_tci = rte_cpu_to_be_16(d->vlan_id);
+			eth->ether_type = RTE_BE16(RTE_ETHER_TYPE_VLAN);
+		}
+		mbufs[ok++] = m;
+	}
+
+	return ok;
+}
+
+static inline void tx_offload_add_vlan(void **objs, uint16_t nb_objs) {
+	const struct iface_mbuf_data *d;
+	struct rte_mbuf *m;
+
+	for (unsigned i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		d = iface_mbuf_data(m);
+		if (d->vlan_id != 0) {
+			m->ol_flags |= RTE_MBUF_F_TX_VLAN;
+			m->vlan_tci = d->vlan_id;
+		}
+	}
+}
+
+uint16_t tx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	const struct tx_node_ctx *ctx = tx_node_ctx(node);
+	struct rte_mbuf *mbufs[RTE_GRAPH_BURST_SIZE];
+	uint16_t tx_ok;
+
+	if (unlikely(!tx_begin(ctx, graph, node, objs, nb_objs)))
+		return 0;
+
+	nb_objs = tx_add_vlan(graph, node, objs, nb_objs, mbufs);
+	if (unlikely(nb_objs == 0))
+		return 0;
+
+	tx_ok = rte_eth_tx_burst(ctx->txq.port_id, ctx->txq.queue_id, mbufs, nb_objs);
+
+	tx_finish(graph, node, (void *)mbufs, nb_objs, tx_ok, 0);
+
+	return nb_objs;
+}
+
+uint16_t
+tx_shared_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	const struct tx_node_ctx *ctx = tx_node_ctx(node);
+	struct rte_mbuf *mbufs[RTE_GRAPH_BURST_SIZE];
+	uint16_t tx_ok;
+
+	if (unlikely(!tx_begin(ctx, graph, node, objs, nb_objs)))
+		return 0;
+
+	nb_objs = tx_add_vlan(graph, node, objs, nb_objs, mbufs);
+	if (unlikely(nb_objs == 0))
+		return 0;
+
+	rte_spinlock_lock(ctx->lock);
+	tx_ok = rte_eth_tx_burst(ctx->txq.port_id, ctx->txq.queue_id, mbufs, nb_objs);
+	rte_spinlock_unlock(ctx->lock);
+
+	tx_finish(graph, node, (void *)mbufs, nb_objs, tx_ok, RXTX_F_TXQ_SHARED);
+
+	return nb_objs;
+}
+
+uint16_t tx_shared_offload_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	const struct tx_node_ctx *ctx = tx_node_ctx(node);
+	uint16_t tx_ok;
+
+	if (unlikely(!tx_begin(ctx, graph, node, objs, nb_objs)))
+		return 0;
+
+	tx_offload_add_vlan(objs, nb_objs);
+
+	rte_spinlock_lock(ctx->lock);
+	tx_ok = rte_eth_tx_burst(
+		ctx->txq.port_id, ctx->txq.queue_id, (struct rte_mbuf **)objs, nb_objs
+	);
+	rte_spinlock_unlock(ctx->lock);
+
+	tx_finish(graph, node, objs, nb_objs, tx_ok, RXTX_F_TXQ_SHARED | RXTX_F_VLAN_OFFLOAD);
+
+	return nb_objs;
+}
+
+uint16_t
+tx_offload_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	const struct tx_node_ctx *ctx = tx_node_ctx(node);
+	uint16_t tx_ok;
+
+	if (unlikely(!tx_begin(ctx, graph, node, objs, nb_objs)))
+		return 0;
+
+	tx_offload_add_vlan(objs, nb_objs);
+
+	tx_ok = rte_eth_tx_burst(
+		ctx->txq.port_id, ctx->txq.queue_id, (struct rte_mbuf **)objs, nb_objs
+	);
+
+	tx_finish(graph, node, objs, nb_objs, tx_ok, RXTX_F_VLAN_OFFLOAD);
 
 	return nb_objs;
 }
@@ -87,6 +225,7 @@ static struct rte_node_register node = {
 	.next_nodes = {
 		[TX_ERROR] = "port_tx_error",
 		[TX_DOWN] = "port_tx_down",
+		[NO_HEADROOM] = "error_no_headroom",
 	},
 };
 

commit bdade3d2423ad8330279fb183775b1f29eb66b37

trace: use iface_mbuf_data for VLAN display
Packet tracing in TX does not show the VLAN tag because it is inserted
after the trace happens. In RX, the trace was logged before populating
iface_mbuf_data, so d->vlan_id was not yet set.

Read d->vlan_id from iface_mbuf_data instead of inspecting mbuf offload
flags or parsing VLAN headers from the packet. Move the trace_log call
after the iface_mbuf_data population loop in all port_rx functions so
that the VLAN id is available when logging.

Fixes: 770168529e5c ("port: add dedicated port_tx functions")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: David Marchand <david.marchand@redhat.com>

modules/infra/datapath/port_rx.c
@@ -98,8 +98,6 @@ uint16_t rx_offload_process(struct rte_graph *graph, struct rte_node *node, void
 	if (rx == 0)
 		return 0;
 
-	trace_log(RXTX_F_VLAN_OFFLOAD, ctx->iface, node, mbufs, rx);
-
 	for (unsigned r = 0; r < rx; r++) {
 		m = mbufs[r];
 		d = iface_mbuf_data(m);
@@ -112,6 +110,8 @@ uint16_t rx_offload_process(struct rte_graph *graph, struct rte_node *node, void
 			d->vlan_id = 0;
 		}
 	}
+
+	trace_log(RXTX_F_VLAN_OFFLOAD, ctx->iface, node, mbufs, rx);
 
 	node->idx = rx;
 	rte_node_next_stream_move(graph, node, IFACE_INPUT);
@@ -134,8 +134,6 @@ uint16_t rx_process(struct rte_graph *graph, struct rte_node *node, void **, uin
 	if (rx == 0)
 		return 0;
 
-	trace_log(0, ctx->iface, node, mbufs, rx);
-
 	for (unsigned r = 0; r < rx; r++) {
 		m = mbufs[r];
 		d = iface_mbuf_data(m);
@@ -148,6 +146,8 @@ uint16_t rx_process(struct rte_graph *graph, struct rte_node *node, void **, uin
 			d->vlan_id = 0;
 		}
 	}
+
+	trace_log(0, ctx->iface, node, mbufs, rx);
 
 	node->idx = rx;
 	rte_node_next_stream_move(graph, node, IFACE_INPUT);
@@ -176,8 +176,6 @@ rx_bond_offload_process(struct rte_graph *graph, struct rte_node *node, void **,
 	if (rx == 0)
 		return 0;
 
-	trace_log(RXTX_F_VLAN_OFFLOAD | RXTX_F_BOND, ctx->iface, node, mbufs, rx);
-
 	for (unsigned r = 0; r < rx; r++) {
 		m = mbufs[r];
 		d = iface_mbuf_data(m);
@@ -195,6 +193,8 @@ rx_bond_offload_process(struct rte_graph *graph, struct rte_node *node, void **,
 				d->iface = iface;
 		}
 	}
+
+	trace_log(RXTX_F_VLAN_OFFLOAD | RXTX_F_BOND, ctx->iface, node, mbufs, rx);
 
 	node->idx = rx;
 	rte_node_next_stream_move(graph, node, IFACE_INPUT);
@@ -222,8 +222,6 @@ uint16_t rx_bond_process(struct rte_graph *graph, struct rte_node *node, void **
 	if (rx == 0)
 		return 0;
 
-	trace_log(RXTX_F_BOND, ctx->iface, node, mbufs, rx);
-
 	for (unsigned r = 0; r < rx; r++) {
 		m = mbufs[r];
 		d = iface_mbuf_data(m);
@@ -243,6 +241,8 @@ uint16_t rx_bond_process(struct rte_graph *graph, struct rte_node *node, void **
 			d->vlan_id = 0;
 		}
 	}
+
+	trace_log(RXTX_F_BOND, ctx->iface, node, mbufs, rx);
 
 	node->idx = rx;
 	rte_node_next_stream_move(graph, node, IFACE_INPUT);

modules/infra/datapath/trace.c
@@ -10,6 +10,7 @@
 #include <gr_mbuf.h>
 #include <gr_module.h>
 #include <gr_net_types.h>
+#include <gr_rxtx.h>
 #include <gr_trace.h>
 
 #include <rte_arp.h>
@@ -421,6 +422,7 @@ err:
 }
 
 void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *iface) {
+	const struct iface_mbuf_data *d = iface_mbuf_data((void *)m);
 	const struct rte_ether_hdr *eth;
 	struct rte_ether_addr src, dst;
 	rte_be16_t ether_type;
@@ -436,19 +438,8 @@ void trace_log_packet(const struct rte_mbuf *m, const char *node, const char *if
 
 	SAFE_BUF(snprintf, sizeof(buf), ETH_F " > " ETH_F, &src, &dst);
 
-	if (m->ol_flags & RTE_MBUF_F_RX_VLAN_STRIPPED) {
-		uint16_t vlan_id = m->vlan_tci & 0xfff;
-		SAFE_BUF(snprintf, sizeof(buf), " / VLAN id=%u", vlan_id);
-	} else if (ether_type == RTE_BE16(RTE_ETHER_TYPE_VLAN)) {
-		const struct rte_vlan_hdr *vlan;
-		uint16_t vlan_id;
-
-		vlan = rte_pktmbuf_mtod_offset(m, const struct rte_vlan_hdr *, offset);
-		offset += sizeof(*vlan);
-		vlan_id = rte_be_to_cpu_16(vlan->vlan_tci) & 0xfff;
-		ether_type = vlan->eth_proto;
-		SAFE_BUF(snprintf, sizeof(buf), " / VLAN id=%u", vlan_id);
-	}
+	if (d->vlan_id != 0)
+		SAFE_BUF(snprintf, sizeof(buf), " / VLAN id=%u", d->vlan_id);
 
 	// ETHERTYPE are greater than 1536 (0x600)
 	// If this is not the case, this represent the len of the SNAP frame