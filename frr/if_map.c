4472eb1f543501cdfbd651ff19a897865aea1c2b
Maxime Leroy <maxime@leroys.fr>
2026-02-25 14:28:42 +0100

    frr: protect ifindex mappings with a mutex
    The ifindex/VRF mapping hash tables are accessed from both the main
    thread (grout_sync_ifaces via grout_link_change, grout_sync_* via
    vrf_grout_to_frr lookups) and the dplane thread (zd_grout_process via
    grout_add_del_route, grout_add_del_nexthop, grout_add_del_address).
    
    FRR hash tables (DECLARE_HASH) are not thread-safe. Concurrent add,
    del or clear from one thread while the other traverses the hash can
    corrupt the internal structure or cause a use-after-free on the
    ifindex_mapping entries.
    
    This race exists from initial startup: grout_sync_ifaces populates the
    mappings on the main thread while the dplane provider may already be
    processing queued requests that perform lookups on the dplane thread.
    
    Add a pthread_mutex_t to serialize all hash table operations.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>

--- frr/if_map.c
+++ frr/if_map.c
@@ -5,6 +5,8 @@
 
 #include <gr_infra.h>
 
+#include <lib/frr_pthread.h>
+
 DEFINE_MGROUP(GROUT, "Grout plugin memory");
 DEFINE_MTYPE(GROUT, GROUT_MEM, "Grout plugin memory");
 
@@ -53,6 +55,7 @@ DECLARE_HASH(
 // Global hash table instances
 static struct grout_to_frr_head grout_to_frr_mappings = INIT_HASH(grout_to_frr_mappings);
 static struct frr_to_grout_head frr_to_grout_mappings = INIT_HASH(frr_to_grout_mappings);
+static pthread_mutex_t ifindex_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 // Add bidirectional mapping
 bool add_ifindex_mapping(uint16_t grout_ifindex, ifindex_t frr_ifindex) {
@@ -60,22 +63,23 @@ bool add_ifindex_mapping(uint16_t grout_ifindex, ifindex_t frr_ifindex) {
 	mapping->grout_ifindex = grout_ifindex;
 	mapping->frr_ifindex = frr_ifindex;
 
-	// Add to both hash tables
-	struct ifindex_mapping *existing_forward = grout_to_frr_add(
-		&grout_to_frr_mappings, mapping
-	);
-	if (existing_forward && existing_forward != mapping) {
-		XFREE(MTYPE_GROUT_MEM, mapping);
-		return false; // Duplicate grout_ifindex
-	}
+	frr_with_mutex(&ifindex_mtx) {
+		struct ifindex_mapping *existing_forward = grout_to_frr_add(
+			&grout_to_frr_mappings, mapping
+		);
+		if (existing_forward && existing_forward != mapping) {
+			XFREE(MTYPE_GROUT_MEM, mapping);
+			return false;
+		}
 
-	struct ifindex_mapping *existing_reverse = frr_to_grout_add(
-		&frr_to_grout_mappings, mapping
-	);
-	if (existing_reverse && existing_reverse != mapping) {
-		grout_to_frr_del(&grout_to_frr_mappings, mapping);
-		XFREE(MTYPE_GROUT_MEM, mapping);
-		return false; // Duplicate frr_ifindex
+		struct ifindex_mapping *existing_reverse = frr_to_grout_add(
+			&frr_to_grout_mappings, mapping
+		);
+		if (existing_reverse && existing_reverse != mapping) {
+			grout_to_frr_del(&grout_to_frr_mappings, mapping);
+			XFREE(MTYPE_GROUT_MEM, mapping);
+			return false;
+		}
 	}
 
 	return true;
@@ -83,24 +87,43 @@ bool add_ifindex_mapping(uint16_t grout_ifindex, ifindex_t frr_ifindex) {
 
 ifindex_t ifindex_grout_to_frr(uint16_t grout_ifindex) {
 	struct ifindex_mapping key = {.grout_ifindex = grout_ifindex};
-	struct ifindex_mapping *found = grout_to_frr_find(&grout_to_frr_mappings, &key);
-	return found ? found->frr_ifindex : IFINDEX_INTERNAL;
+	ifindex_t ret = IFINDEX_INTERNAL;
+
+	frr_with_mutex(&ifindex_mtx) {
+		struct ifindex_mapping *found = grout_to_frr_find(&grout_to_frr_mappings, &key);
+		if (found)
+			ret = found->frr_ifindex;
+	}
+
+	return ret;
 }
 
 uint16_t ifindex_frr_to_grout(ifindex_t frr_ifindex) {
 	struct ifindex_mapping key = {.frr_ifindex = frr_ifindex};
-	struct ifindex_mapping *found = frr_to_grout_find(&frr_to_grout_mappings, &key);
-	return found ? found->grout_ifindex : GR_IFACE_ID_UNDEF;
+	uint16_t ret = GR_IFACE_ID_UNDEF;
+
+	frr_with_mutex(&ifindex_mtx) {
+		struct ifindex_mapping *found = frr_to_grout_find(&frr_to_grout_mappings, &key);
+		if (found)
+			ret = found->grout_ifindex;
+	}
+
+	return ret;
 }
 
 bool remove_mapping_by_grout_ifindex(uint16_t grout_ifindex) {
 	struct ifindex_mapping key = {.grout_ifindex = grout_ifindex};
-	struct ifindex_mapping *found = grout_to_frr_find(&grout_to_frr_mappings, &key);
-	if (!found)
-		return false;
+	struct ifindex_mapping *found = NULL;
 
-	grout_to_frr_del(&grout_to_frr_mappings, found);
-	frr_to_grout_del(&frr_to_grout_mappings, found);
+	frr_with_mutex(&ifindex_mtx) {
+		found = grout_to_frr_find(&grout_to_frr_mappings, &key);
+		if (!found)
+			return false;
+
+		grout_to_frr_del(&grout_to_frr_mappings, found);
+		frr_to_grout_del(&frr_to_grout_mappings, found);
+	}
+
 	XFREE(MTYPE_GROUT_MEM, found);
 	return true;
 }
