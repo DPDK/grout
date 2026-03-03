d745f79b8d0717d3052e34b2bd62299d341ad295
Robin Jarry <rjarry@redhat.com>
2026-03-03 09:57:31 +0100

    cli: pipe command output through a pager
    Spawn a less(1) subprocess before each command callback and redirect
    stdout to its stdin pipe. After the callback returns, restore stdout
    and wait for the pager to exit.
    
    The pager is only started when stdout is a tty. It can be overridden
    via GROUT_PAGER or PAGER environment variables. Setting either to an
    empty string or "cat" disables paging. Default less flags are set to
    -SFRXKe so that short output is displayed without entering the pager
    at all (-F), ANSI colors are preserved (-R), and Ctrl-C works (-K).
    
    SIGPIPE is temporarily ignored while the pager is active so that
    quitting less early does not kill grcli. The pager can also be
    disabled with -p/--no-pager on the command line.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

---
+++ cli/pager.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#pragma once
+
+void pager_init(void);
+void pager_disable(void);
+void pager_start(void);
+void pager_stop(void);
