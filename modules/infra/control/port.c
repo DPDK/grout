DPDK/grout

commit 789392fe5b3e916e524fdca79ae7c484dc790e90

iface: implement mac address filtering
Add new callbacks in the iface_type structure to add/remove ethernet
addresses in the filtered list.

Currently, only the port interface type implements these callbacks.

Add mac_filter structures in the port private data to manage the list of
allowed ethernet addresses. Handle the number of references that are
held on each one.

Use that new API to manage vlan ethernet addresses.

Closes: https://github.com/rjarry/grout/issues/27
Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/port.c
@@ -425,9 +425,182 @@ const struct iface *port_get_iface(uint16_t port_id) {
 	return port_ifaces[port_id];
 }
 
-static int iface_port_get_eth_addr(const struct iface *iface, struct rte_ether_addr *mac) {
-	const struct iface_info_port *port = (const struct iface_info_port *)iface->info;
+static int port_mac_get(const struct iface *iface, struct rte_ether_addr *mac) {
+	struct iface_info_port *port = (struct iface_info_port *)iface->info;
 	rte_ether_addr_copy(&port->mac, mac);
+	return 0;
+}
+
+static int port_mac_add(struct iface *iface, const struct rte_ether_addr *mac) {
+	struct iface_info_port *port = (struct iface_info_port *)iface->info;
+	struct mac_filter *filter;
+	const char *mac_type;
+	bool multicast;
+	uint8_t i;
+	int ret;
+
+	if (mac == NULL || !(rte_is_multicast_ether_addr(mac) || rte_is_unicast_ether_addr(mac)))
+		return errno_set(EINVAL);
+
+	if (rte_is_same_ether_addr(mac, &port->mac))
+		return 0;
+
+	multicast = rte_is_multicast_ether_addr(mac);
+	if (multicast) {
+		mac_type = "multicast";
+		filter = &port->mcast_filter;
+	} else {
+		mac_type = "unicast";
+		filter = &port->ucast_filter;
+	}
+
+	for (i = 0; i < filter->count; i++) {
+		if (rte_is_same_ether_addr(&filter->mac[i], mac)) {
+			LOG(INFO,
+			    "%s: %s mac " ETH_ADDR_FMT " already filtered (refs=%u)",
+			    iface->name,
+			    mac_type,
+			    ETH_ADDR_SPLIT(mac),
+			    filter->refcnt[i]++);
+			return 0;
+		}
+	}
+	if (i == ARRAY_DIM(filter->mac))
+		return errno_log(ENOSPC, mac_type);
+
+	rte_ether_addr_copy(mac, &filter->mac[i]);
+	filter->refcnt[i] = 1;
+	filter->count++;
+
+	LOG(NOTICE,
+	    "%s: enabling %s " ETH_ADDR_FMT " mac filter",
+	    iface->name,
+	    mac_type,
+	    ETH_ADDR_SPLIT(mac));
+
+	if (filter->flags & MAC_FILTER_F_ALL)
+		return 0;
+
+	if (multicast)
+		ret = rte_eth_dev_set_mc_addr_list(port->port_id, filter->mac, filter->count);
+	else
+		ret = rte_eth_dev_mac_addr_add(port->port_id, &filter->mac[i], 0);
+
+	if (ret == -ENOSPC || ret == -EOPNOTSUPP) {
+		if (ret == -ENOSPC) {
+			filter->flags |= MAC_FILTER_F_NOSPC;
+			filter->hw_limit = filter->count - 1;
+			LOG(WARNING, "%s: %s: %s", iface->name, mac_type, rte_strerror(-ret));
+		} else {
+			filter->flags |= MAC_FILTER_F_UNSUPP;
+			LOG(NOTICE, "%s: %s: %s", iface->name, mac_type, rte_strerror(-ret));
+		}
+
+		mac_type = multicast ? "allmulti" : "promisc";
+		LOG(NOTICE, "%s: enabling %s", iface->name, mac_type);
+
+		// promisc and allmulti enable is a noop if already enabled
+		if (multicast)
+			ret = rte_eth_allmulticast_enable(port->port_id);
+		else
+			ret = rte_eth_promiscuous_enable(port->port_id);
+
+		if (ret == 0)
+			filter->flags |= MAC_FILTER_F_ALL;
+	}
+
+	if (ret < 0) {
+		filter->count--;
+		return errno_log(-ret, mac_type);
+	}
+
+	return 0;
+}
+
+static int port_mac_del(struct iface *iface, const struct rte_ether_addr *mac) {
+	struct iface_info_port *port = (struct iface_info_port *)iface->info;
+	struct mac_filter *filter;
+	const char *mac_type;
+	bool multicast;
+	uint8_t i;
+	int ret;
+
+	if (mac == NULL || !(rte_is_multicast_ether_addr(mac) || rte_is_unicast_ether_addr(mac)))
+		return errno_set(EINVAL);
+
+	if (rte_is_same_ether_addr(mac, &port->mac))
+		return 0;
+
+	multicast = rte_is_multicast_ether_addr(mac);
+	if (multicast) {
+		mac_type = "multicast";
+		filter = &port->mcast_filter;
+	} else {
+		mac_type = "unicast";
+		filter = &port->ucast_filter;
+	}
+
+	for (i = 0; i < filter->count; i++) {
+		if (rte_is_same_ether_addr(&filter->mac[i], mac))
+			goto found;
+	}
+	return errno_log(ENOENT, mac_type);
+
+found:
+	if (--filter->refcnt[i] > 0) {
+		LOG(INFO,
+		    "%s: %s mac " ETH_ADDR_FMT " still filtered (refs=%u)",
+		    iface->name,
+		    mac_type,
+		    ETH_ADDR_SPLIT(mac),
+		    filter->refcnt[i]);
+		return 0;
+	}
+
+	LOG(NOTICE,
+	    "%s: removing %s " ETH_ADDR_FMT " mac filter",
+	    iface->name,
+	    mac_type,
+	    ETH_ADDR_SPLIT(mac));
+
+	if (i + 1 < filter->count) {
+		// shift other addresses and ref counts left
+		memmove(&filter->mac[i],
+			&filter->mac[i + 1],
+			(filter->count - i - 1) * sizeof(filter->mac[i]));
+		memmove(&filter->refcnt[i],
+			&filter->refcnt[i + 1],
+			(filter->count - i - 1) * sizeof(filter->refcnt[i]));
+	}
+	filter->count--;
+
+	if (filter->flags & MAC_FILTER_F_ALL) {
+		if (filter->count > 0 && filter->flags & MAC_FILTER_F_UNSUPP)
+			return 0;
+		if (filter->count > filter->hw_limit && filter->flags & MAC_FILTER_F_NOSPC)
+			return 0;
+		filter->flags = 0;
+		filter->hw_limit = 0;
+		if (multicast)
+			ret = rte_eth_allmulticast_disable(port->port_id);
+		else
+			ret = rte_eth_promiscuous_disable(port->port_id);
+		if (ret < 0)
+			LOG(WARNING,
+			    "%s: %s disable: %s",
+			    iface->name,
+			    multicast ? "allmulti" : "promisc",
+			    rte_strerror(-ret));
+	}
+
+	if (multicast)
+		ret = rte_eth_dev_set_mc_addr_list(port->port_id, filter->mac, filter->count);
+	else
+		ret = rte_eth_dev_mac_addr_remove(port->port_id, (struct rte_ether_addr *)mac);
+
+	if (ret < 0)
+		LOG(WARNING, "%s: %s: %s", iface->name, mac_type, rte_strerror(-ret));
+
 	return 0;
 }
 
@@ -457,7 +630,9 @@ static struct iface_type iface_type_port = {
 	.init = iface_port_init,
 	.reconfig = iface_port_reconfig,
 	.fini = iface_port_fini,
-	.get_eth_addr = iface_port_get_eth_addr,
+	.get_eth_addr = port_mac_get,
+	.add_eth_addr = port_mac_add,
+	.del_eth_addr = port_mac_del,
 	.to_api = port_to_api,
 };
 

api/gr_bitops.h
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 
+#define GR_BIT8(n) (UINT8_C(1) << (n))
 #define GR_BIT16(n) (UINT16_C(1) << (n))
 #define GR_BIT32(n) (UINT32_C(1) << (n))
 #define GR_BIT64(n) (UINT64_C(1) << (n))

modules/infra/control/gr_iface.h
@@ -37,7 +37,8 @@ typedef int (*iface_reconfig_t)(
 	const void *api_info
 );
 typedef int (*iface_fini_t)(struct iface *);
-typedef int (*iface_get_eth_addr_t)(const struct iface *, struct rte_ether_addr *);
+typedef int (*iface_eth_addr_get_t)(const struct iface *, struct rte_ether_addr *);
+typedef int (*iface_eth_addr_filter_t)(struct iface *, const struct rte_ether_addr *);
 typedef void (*iface_to_api_t)(void *api_info, const struct iface *);
 
 struct iface_type {
@@ -46,7 +47,9 @@ struct iface_type {
 	iface_init_t init;
 	iface_reconfig_t reconfig;
 	iface_fini_t fini;
-	iface_get_eth_addr_t get_eth_addr;
+	iface_eth_addr_get_t get_eth_addr;
+	iface_eth_addr_filter_t add_eth_addr;
+	iface_eth_addr_filter_t del_eth_addr;
 	iface_to_api_t to_api;
 	const char *name;
 	STAILQ_ENTRY(iface_type) next;
@@ -76,6 +79,8 @@ struct iface *iface_from_id(uint16_t ifid);
 void iface_add_subinterface(struct iface *parent, const struct iface *sub);
 void iface_del_subinterface(struct iface *parent, const struct iface *sub);
 int iface_get_eth_addr(uint16_t ifid, struct rte_ether_addr *);
+int iface_add_eth_addr(uint16_t ifid, struct rte_ether_addr *);
+int iface_del_eth_addr(uint16_t ifid, struct rte_ether_addr *);
 uint16_t ifaces_count(uint16_t type_id);
 struct iface *iface_next(uint16_t type_id, const struct iface *prev);
 

modules/infra/control/gr_port.h
@@ -4,13 +4,29 @@
 #ifndef _GR_INFRA_PORT
 #define _GR_INFRA_PORT
 
+#include <gr_bitops.h>
 #include <gr_iface.h>
 
+#include <rte_ethdev.h>
 #include <rte_ether.h>
 #include <rte_mempool.h>
 
 #include <stdint.h>
 #include <sys/queue.h>
+
+enum {
+	MAC_FILTER_F_UNSUPP = GR_BIT8(0),
+	MAC_FILTER_F_NOSPC = GR_BIT8(1),
+	MAC_FILTER_F_ALL = GR_BIT8(2),
+};
+
+struct mac_filter {
+	uint8_t flags;
+	uint8_t count;
+	uint8_t hw_limit;
+	uint16_t refcnt[RTE_ETH_NUM_RECEIVE_MAC_ADDR];
+	struct rte_ether_addr mac[RTE_ETH_NUM_RECEIVE_MAC_ADDR];
+};
 
 struct __rte_aligned(alignof(void *)) iface_info_port {
 	uint16_t port_id;
@@ -23,6 +39,8 @@ struct __rte_aligned(alignof(void *)) iface_info_port {
 	struct rte_mempool *pool;
 	char *devargs;
 	uint32_t pool_size;
+	struct mac_filter ucast_filter;
+	struct mac_filter mcast_filter;
 };
 
 uint32_t port_get_rxq_buffer_us(uint16_t port_id, uint16_t rxq_id);
@@ -35,5 +53,4 @@ int iface_port_reconfig(
 	const void *api_info
 );
 const struct iface *port_get_iface(uint16_t port_id);
-
 #endif

modules/infra/control/iface.c
@@ -215,6 +215,34 @@ void iface_del_subinterface(struct iface *parent, const struct iface *sub) {
 	}
 }
 
+int iface_add_eth_addr(uint16_t ifid, struct rte_ether_addr *mac) {
+	struct iface *iface = iface_from_id(ifid);
+	struct iface_type *type;
+
+	if (iface == NULL)
+		return -1;
+
+	type = iface_type_get(iface->type_id);
+	if (type->add_eth_addr == NULL)
+		return errno_set(EOPNOTSUPP);
+
+	return type->add_eth_addr(iface, mac);
+}
+
+int iface_del_eth_addr(uint16_t ifid, struct rte_ether_addr *mac) {
+	struct iface *iface = iface_from_id(ifid);
+	struct iface_type *type;
+
+	if (iface == NULL)
+		return -1;
+
+	type = iface_type_get(iface->type_id);
+	if (type->del_eth_addr == NULL)
+		return errno_set(EOPNOTSUPP);
+
+	return type->del_eth_addr(iface, mac);
+}
+
 int iface_destroy(uint16_t ifid) {
 	struct iface *iface = iface_from_id(ifid);
 	struct iface_type *type;

modules/infra/control/vlan.c
@@ -47,20 +47,6 @@ static int get_parent_port_id(uint16_t parent_id, uint16_t *port_id) {
 	return 0;
 }
 
-static bool need_mac_filter(uint16_t port_id, struct rte_ether_addr *mac) {
-	struct rte_ether_addr parent_mac;
-	int ret;
-
-	if ((ret = rte_eth_macaddr_get(port_id, &parent_mac)) < 0) {
-		errno_log(-ret, "rte_eth_dev_vlan_filter");
-		return false;
-	}
-	if (memcmp(mac, &parent_mac, sizeof(*mac)) == 0)
-		return false;
-
-	return true;
-}
-
 static int iface_vlan_reconfig(
 	struct iface *iface,
 	uint64_t set_attrs,
@@ -76,6 +62,7 @@ static int iface_vlan_reconfig(
 	struct iface *cur_parent = iface_from_id(cur->parent_id);
 	struct iface *next_parent = iface_from_id(next->parent_id);
 	uint16_t cur_port_id, next_port_id;
+	struct iface_type *parent_type;
 	int ret;
 
 	if (get_parent_port_id(cur->parent_id, &cur_port_id) < 0)
@@ -83,11 +70,14 @@ static int iface_vlan_reconfig(
 	if (get_parent_port_id(next->parent_id, &next_port_id) < 0)
 		return -1;
 
+	parent_type = iface_type_get(next_parent->type_id);
+
 	if (set_attrs & (GR_VLAN_SET_PARENT | GR_VLAN_SET_VLAN)) {
 		if (rte_hash_lookup(vlan_hash, &next_key) >= 0)
 			return errno_set(EADDRINUSE);
 
-		if (next->parent_id != cur->parent_id || next->vlan_id != cur->vlan_id) {
+		if (set_attrs != IFACE_SET_ALL) {
+			// reconfig, *not initial config*
 			rte_hash_del_key(vlan_hash, &cur_key);
 			iface_del_subinterface(cur_parent, iface);
 			// remove previous vlan filter (ignore errors)
@@ -109,23 +99,14 @@ static int iface_vlan_reconfig(
 	}
 
 	if (set_attrs & GR_VLAN_SET_MAC) {
-		struct rte_ether_addr next_mac;
-		memcpy(&next_mac, &next->mac, sizeof(next_mac));
-
-		if (need_mac_filter(cur_port_id, &cur->mac)) {
-			if ((ret = rte_eth_dev_mac_addr_remove(cur_port_id, &cur->mac)) < 0)
-				errno_log(-ret, "rte_eth_dev_mac_addr_remove");
+		if (set_attrs != IFACE_SET_ALL) {
+			// reconfig, *not initial config*
+			// remove previous mac filter (ignore errors)
+			parent_type->del_eth_addr(cur_parent, &cur->mac);
 		}
-		if (need_mac_filter(next_port_id, &next_mac)) {
-			if ((ret = rte_eth_dev_mac_addr_add(next_port_id, &next_mac, 0)) < 0) {
-				errno_log(-ret, "rte_eth_dev_mac_addr_add");
-				if (ret != ENOTSUP)
-					return ret;
-				if ((ret = rte_eth_promiscuous_enable(next_port_id)) < 0)
-					return errno_log(-ret, "rte_eth_promiscuous_enable");
-			}
-		}
-		rte_ether_addr_copy(&next_mac, &cur->mac);
+		if ((ret = parent_type->add_eth_addr(next_parent, &next->mac)) < 0)
+			return ret;
+		rte_ether_addr_copy(&next->mac, &cur->mac);
 	}
 
 	if (set_attrs & GR_IFACE_SET_FLAGS)
@@ -141,11 +122,14 @@ static int iface_vlan_reconfig(
 static int iface_vlan_fini(struct iface *iface) {
 	struct iface_info_vlan *vlan = (struct iface_info_vlan *)iface->info;
 	struct iface *parent = iface_from_id(vlan->parent_id);
+	struct iface_type *parent_type;
 	int ret, status = 0;
 	uint16_t port_id;
 
 	if (get_parent_port_id(vlan->parent_id, &port_id) < 0)
 		return -1;
+
+	parent_type = iface_type_get(parent->type_id);
 
 	rte_hash_del_key(vlan_hash, &(struct vlan_key) {vlan->parent_id, vlan->vlan_id});
 
@@ -154,12 +138,9 @@ static int iface_vlan_fini(struct iface *iface) {
 	if (status == 0 && ret < 0)
 		status = ret;
 
-	if (need_mac_filter(port_id, &vlan->mac)) {
-		if ((ret = rte_eth_dev_mac_addr_remove(port_id, &vlan->mac)) < 0)
-			errno_log(-ret, "rte_eth_dev_mac_addr_remove");
-		if (status == 0 && ret < 0)
-			status = ret;
-	}
+	ret = parent_type->del_eth_addr(parent, &vlan->mac);
+	if (status == 0 && ret < 0)
+		status = ret;
 
 	iface_del_subinterface(parent, iface);
 

commit c83c67ed494dccb0de08cd60c0527c77a9206c04

port: ignore ENOTSUP in port_mac_del
Some port types (net_null, net_tap) do not support MAC address filter
removal or promiscuous mode control. When a bond is destroyed, its
members' MAC filters are cleaned up, triggering these warnings on such
ports:

  NOTICE: port_mac_del: p1: promisc disable: Operation not supported
  WARN: port_mac_del: p1: Operation not supported

ENOTSUP is expected for these drivers and not worth logging.

Fixes: 789392fe5b3e ("iface: implement mac address filtering")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/port.c
@@ -773,7 +773,7 @@ found:
 		port->filter.flags = 0;
 		port->filter.hw_limit = 0;
 		ret = rte_eth_promiscuous_disable(port->port_id);
-		if (ret < 0)
+		if (ret < 0 && ret != -ENOTSUP)
 			LOG(NOTICE, "%s: promisc disable: %s", iface->name, rte_strerror(-ret));
 		else
 			iface->state &= ~GR_IFACE_S_PROMISC_FIXED;
@@ -789,7 +789,7 @@ found:
 		ret = rte_eth_dev_mac_addr_remove(port->port_id, (struct rte_ether_addr *)mac);
 	}
 
-	if (ret < 0)
+	if (ret < 0 && ret != -ENOTSUP)
 		LOG(WARNING, "%s: %s", iface->name, rte_strerror(-ret));
 
 	return 0;