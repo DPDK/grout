DPDK/grout

commit 5870ab0daeddcc53cb7b6115cb69e5233a7fa940

ip,ip6: send GARP and NA on MAC address change
When an interface MAC address changes, neighbors must be notified so
they can update their caches without waiting for stale entries to
expire.

Introduce GR_EVENT_IFACE_MAC_CHANGE which is pushed from
iface_set_eth_addr() on success. Have bond and port reconfig handlers
call the generic iface_set_eth_addr() wrapper instead of their
type-specific callbacks. Implement set_eth_addr for VLAN interfaces.

Subscribe IPv4 and IPv6 address handlers to this event to trigger
gratuitous ARP and unsolicited neighbor advertisements respectively.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Anthony Harivel <aharivel@redhat.com>

modules/ip6/control/address.c
@@ -478,6 +478,7 @@ static void ip6_iface_event_handler(uint32_t event, const void *obj) {
 		ip6_iface_addrs_flush(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_UP:
+	case GR_EVENT_IFACE_MAC_CHANGE:
 		addrs = &iface_addrs[iface->id];
 		gr_vec_foreach (nh, addrs->nh) {
 			if (nh6_advertise(nh, NULL) < 0)
@@ -543,6 +544,7 @@ static struct gr_event_subscription iface_event_subscription = {
 		GR_EVENT_IFACE_POST_RECONFIG,
 		GR_EVENT_IFACE_PRE_REMOVE,
 		GR_EVENT_IFACE_STATUS_UP,
+		GR_EVENT_IFACE_MAC_CHANGE,
 	},
 };
 

modules/ip/control/address.c
@@ -336,8 +336,8 @@ static struct gr_event_subscription iface_pre_rm_subscription = {
 };
 static struct gr_event_subscription iface_up_subscription = {
 	.callback = iface_up_cb,
-	.ev_count = 1,
-	.ev_types = {GR_EVENT_IFACE_STATUS_UP},
+	.ev_count = 2,
+	.ev_types = {GR_EVENT_IFACE_STATUS_UP, GR_EVENT_IFACE_MAC_CHANGE},
 };
 static struct gr_event_serializer iface_addr_serializer = {
 	.size = sizeof(struct gr_ip4_ifaddr),

frr/zebra_dplane_grout.c
@@ -248,6 +248,7 @@ static void dplane_grout_connect(struct event *) {
 		{.type = GR_EVENT_IFACE_STATUS_UP, .suppress_self_events = true},
 		{.type = GR_EVENT_IFACE_STATUS_DOWN, .suppress_self_events = true},
 		{.type = GR_EVENT_IFACE_POST_RECONFIG, .suppress_self_events = true},
+		{.type = GR_EVENT_IFACE_MAC_CHANGE, .suppress_self_events = true},
 		{.type = GR_EVENT_IFACE_REMOVE, .suppress_self_events = true},
 		{.type = GR_EVENT_IP_ADDR_ADD, .suppress_self_events = false},
 		{.type = GR_EVENT_IP6_ADDR_ADD, .suppress_self_events = false},
@@ -396,6 +397,8 @@ static const char *gr_evt_to_str(uint32_t e) {
 		return TOSTRING(GR_EVENT_IFACE_STATUS_DOWN);
 	case GR_EVENT_IFACE_POST_RECONFIG:
 		return TOSTRING(GR_EVENT_IFACE_POST_RECONFIG);
+	case GR_EVENT_IFACE_MAC_CHANGE:
+		return TOSTRING(GR_EVENT_IFACE_MAC_CHANGE);
 	case GR_EVENT_IP_ADDR_ADD:
 		return TOSTRING(GR_EVENT_IP_ADDR_ADD);
 	case GR_EVENT_IP_ADDR_DEL:
@@ -446,6 +449,7 @@ static void dplane_read_notifications(struct event *event) {
 	case GR_EVENT_IFACE_STATUS_UP:
 	case GR_EVENT_IFACE_STATUS_DOWN:
 	case GR_EVENT_IFACE_POST_RECONFIG:
+	case GR_EVENT_IFACE_MAC_CHANGE:
 		new = true;
 		// fallthrough
 	case GR_EVENT_IFACE_REMOVE:

modules/infra/api/gr_infra.h
@@ -221,6 +221,7 @@ typedef enum {
 	GR_EVENT_IFACE_POST_RECONFIG = EVENT_TYPE(GR_INFRA_MODULE, 0x0005),
 	GR_EVENT_IFACE_STATUS_UP = EVENT_TYPE(GR_INFRA_MODULE, 0x0006),
 	GR_EVENT_IFACE_STATUS_DOWN = EVENT_TYPE(GR_INFRA_MODULE, 0x0007),
+	GR_EVENT_IFACE_MAC_CHANGE = EVENT_TYPE(GR_INFRA_MODULE, 0x0008),
 } gr_event_iface_t;
 
 // interface management ///////////////////////////////////////////////////////

modules/infra/api/iface.c
@@ -162,7 +162,7 @@ static int iface_event_serialize(const void *obj, void **buf) {
 
 static struct gr_event_serializer iface_serializer = {
 	.callback = iface_event_serialize,
-	.ev_count = 7,
+	.ev_count = 8,
 	.ev_types = {
 		GR_EVENT_IFACE_ADD,
 		GR_EVENT_IFACE_POST_ADD,
@@ -171,6 +171,7 @@ static struct gr_event_serializer iface_serializer = {
 		GR_EVENT_IFACE_POST_RECONFIG,
 		GR_EVENT_IFACE_STATUS_UP,
 		GR_EVENT_IFACE_STATUS_DOWN,
+		GR_EVENT_IFACE_MAC_CHANGE,
 	},
 };
 

modules/infra/cli/iface.c
@@ -618,6 +618,9 @@ static void iface_event_print(uint32_t event, const void *obj) {
 	case GR_EVENT_IFACE_POST_RECONFIG:
 		action = "reconf";
 		break;
+	case GR_EVENT_IFACE_MAC_CHANGE:
+		action = "mac change";
+		break;
 	default:
 		action = "?";
 		break;
@@ -633,7 +636,7 @@ static void iface_event_print(uint32_t event, const void *obj) {
 
 static struct cli_event_printer printer = {
 	.print = iface_event_print,
-	.ev_count = 7,
+	.ev_count = 8,
 	.ev_types = {
 		GR_EVENT_IFACE_ADD,
 		GR_EVENT_IFACE_POST_ADD,
@@ -642,6 +645,7 @@ static struct cli_event_printer printer = {
 		GR_EVENT_IFACE_STATUS_UP,
 		GR_EVENT_IFACE_STATUS_DOWN,
 		GR_EVENT_IFACE_POST_RECONFIG,
+		GR_EVENT_IFACE_MAC_CHANGE,
 	},
 };
 

modules/infra/control/iface.c
@@ -463,6 +463,7 @@ int iface_del_subinterface(struct iface *parent, struct iface *sub) {
 
 int iface_set_eth_addr(struct iface *iface, const struct rte_ether_addr *mac) {
 	const struct iface_type *type;
+	int ret;
 
 	if (iface == NULL)
 		return errno_set(EINVAL);
@@ -472,7 +473,11 @@ int iface_set_eth_addr(struct iface *iface, const struct rte_ether_addr *mac) {
 	if (type->set_eth_addr == NULL)
 		return errno_set(EOPNOTSUPP);
 
-	return type->set_eth_addr(iface, mac);
+	ret = type->set_eth_addr(iface, mac);
+	if (ret == 0)
+		gr_event_push(GR_EVENT_IFACE_MAC_CHANGE, iface);
+
+	return ret;
 }
 
 int iface_add_eth_addr(struct iface *iface, const struct rte_ether_addr *mac) {
@@ -693,6 +698,11 @@ static void iface_event(uint32_t event, const void *obj) {
 			gr_event_push(event, s);
 		}
 		break;
+	case GR_EVENT_IFACE_MAC_CHANGE:
+		str = "MAC_CHANGE";
+		gr_vec_foreach (struct iface *s, iface->subinterfaces)
+			gr_event_push(event, s);
+		break;
 	default:
 		str = "?";
 		break;
@@ -702,7 +712,7 @@ static void iface_event(uint32_t event, const void *obj) {
 
 static struct gr_event_subscription iface_event_handler = {
 	.callback = iface_event,
-	.ev_count = 7,
+	.ev_count = 8,
 	.ev_types = {
 		GR_EVENT_IFACE_ADD,
 		GR_EVENT_IFACE_POST_ADD,
@@ -711,6 +721,7 @@ static struct gr_event_subscription iface_event_handler = {
 		GR_EVENT_IFACE_POST_RECONFIG,
 		GR_EVENT_IFACE_STATUS_UP,
 		GR_EVENT_IFACE_STATUS_DOWN,
+		GR_EVENT_IFACE_MAC_CHANGE,
 	},
 };
 

modules/infra/control/port.c
@@ -354,7 +354,7 @@ static int iface_port_reconfig(
 			return ret;
 	}
 
-	if (set_attrs & GR_PORT_SET_MAC && (ret = port_mac_set(iface, &api->mac)) < 0)
+	if (set_attrs & GR_PORT_SET_MAC && (ret = iface_set_eth_addr(iface, &api->mac)) < 0)
 		return ret;
 
 	if (!p->started && (ret = rte_eth_dev_start(p->port_id)) < 0)

commit 1b122d82985b0ee61ca32a49c964bea7daf4ad34

ip6: fix event subscription count for MAC_CHANGE
The iface_event_subscription listed 5 event types but ev_count was set
to 4. GR_EVENT_IFACE_MAC_CHANGE was silently not subscribed, preventing
unsolicited neighbor advertisements on MAC address changes.

Fixes: 5870ab0daedd ("ip,ip6: send GARP and NA on MAC address change")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/address.c
@@ -543,7 +543,7 @@ static struct gr_module addr6_module = {
 
 static struct gr_event_subscription iface_event_subscription = {
 	.callback = ip6_iface_event_handler,
-	.ev_count = 4,
+	.ev_count = 5,
 	.ev_types = {
 		GR_EVENT_IFACE_POST_ADD,
 		GR_EVENT_IFACE_POST_RECONFIG,