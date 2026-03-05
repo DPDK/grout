DPDK/grout

commit 14aa49098411d32c746e49becead57b491ba1d48

ip,ip6: flush addresses on interface mode change
When an interface leaves VRF mode (e.g. reconfigured as cross-connect),
any IPv4 and IPv6 addresses previously configured on it become invalid.
Likewise, when an interface moves to a different VRF, its addresses
belong to the old VRF and need to be removed.

Subscribe to GR_EVENT_IFACE_POST_RECONFIG in both IPv4 and IPv6 address
modules. On reconfiguration, flush all addresses when the interface is
no longer in VRF mode or has moved to a different VRF. For IPv6, also
reinitialize link-local and well-known multicast addresses when entering
VRF mode or changing VRFs.

Extend the IPv6 add/del smoke test to exercise VRF reassignment and
cross-connect mode transitions.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/address.c
@@ -417,40 +417,65 @@ static const struct rte_ipv6_addr well_known_mcast_addrs[] = {
 	GR_IPV6_ADDR_MLDV2,
 };
 
-static void ip6_iface_event_handler(uint32_t event, const void *obj) {
-	const struct nexthop_info_l3 *l3;
-	const struct iface *iface = obj;
+static void ip6_iface_llocal_init(const struct iface *iface) {
 	struct rte_ipv6_addr link_local;
 	struct rte_ether_addr mac;
+	unsigned i;
+
+	if (iface_get_eth_addr(iface, &mac) == 0) {
+		rte_ipv6_llocal_from_ethernet(&link_local, &mac);
+		if (iface6_addr_add(iface, &link_local, 64) < 0)
+			errno_log(errno, "iface_addr_add");
+	}
+	for (i = 0; i < ARRAY_DIM(well_known_mcast_addrs); i++) {
+		if (mcast6_addr_add(iface, &well_known_mcast_addrs[i]) < 0)
+			LOG(INFO, "%s: mcast_addr_add: %s", iface->name, strerror(errno));
+	}
+}
+
+static void ip6_iface_addrs_flush(const struct iface *iface) {
+	const struct nexthop_info_l3 *l3;
 	const struct nexthop *nh;
 	struct hoplist *addrs;
-	unsigned i;
+
+	addrs = &iface_addrs[iface->id];
+	while (gr_vec_len(addrs->nh) > 0) {
+		nh = addrs->nh[gr_vec_len(addrs->nh) - 1];
+		l3 = nexthop_info_l3(nh);
+		iface6_addr_del(iface, &l3->ipv6, l3->prefixlen);
+	}
+	addrs = &iface_mcast_addrs[iface->id];
+	while (gr_vec_len(addrs->nh) > 0) {
+		nh = addrs->nh[gr_vec_len(addrs->nh) - 1];
+		l3 = nexthop_info_l3(nh);
+		mcast6_addr_del(iface, &l3->ipv6);
+	}
+}
+
+static void ip6_iface_event_handler(uint32_t event, const void *obj) {
+	const struct iface *iface = obj;
+	const struct nexthop *nh;
+	struct hoplist *addrs;
 
 	switch (event) {
 	case GR_EVENT_IFACE_POST_ADD:
-		if (iface_get_eth_addr(iface, &mac) == 0) {
-			rte_ipv6_llocal_from_ethernet(&link_local, &mac);
-			if (iface6_addr_add(iface, &link_local, 64) < 0)
-				errno_log(errno, "iface_addr_add");
-		}
-		for (i = 0; i < ARRAY_DIM(well_known_mcast_addrs); i++) {
-			if (mcast6_addr_add(iface, &well_known_mcast_addrs[i]) < 0)
-				LOG(INFO, "%s: mcast_addr_add: %s", iface->name, strerror(errno));
+		ip6_iface_llocal_init(iface);
+		break;
+	case GR_EVENT_IFACE_POST_RECONFIG:
+		if (iface->mode != GR_IFACE_MODE_VRF) {
+			// changing mode from VRF -> !VRF
+			ip6_iface_addrs_flush(iface);
+		} else if (gr_vec_len(iface_addrs[iface->id].nh) == 0) {
+			// changing mode from !VRF -> VRF
+			ip6_iface_llocal_init(iface);
+		} else if (iface_addrs[iface->id].nh[0]->vrf_id != iface->vrf_id) {
+			// changing to a different VRF
+			ip6_iface_addrs_flush(iface);
+			ip6_iface_llocal_init(iface);
 		}
 		break;
 	case GR_EVENT_IFACE_PRE_REMOVE:
-		addrs = &iface_addrs[iface->id];
-		while (gr_vec_len(addrs->nh) > 0) {
-			nh = addrs->nh[gr_vec_len(addrs->nh) - 1];
-			l3 = nexthop_info_l3(nh);
-			iface6_addr_del(iface, &l3->ipv6, l3->prefixlen);
-		}
-		addrs = &iface_mcast_addrs[iface->id];
-		while (gr_vec_len(addrs->nh) > 0) {
-			nh = addrs->nh[gr_vec_len(addrs->nh) - 1];
-			l3 = nexthop_info_l3(nh);
-			mcast6_addr_del(iface, &l3->ipv6);
-		}
+		ip6_iface_addrs_flush(iface);
 		break;
 	case GR_EVENT_IFACE_STATUS_UP:
 		addrs = &iface_addrs[iface->id];
@@ -512,9 +537,10 @@ static struct gr_module addr6_module = {
 
 static struct gr_event_subscription iface_event_subscription = {
 	.callback = ip6_iface_event_handler,
-	.ev_count = 3,
+	.ev_count = 4,
 	.ev_types = {
 		GR_EVENT_IFACE_POST_ADD,
+		GR_EVENT_IFACE_POST_RECONFIG,
 		GR_EVENT_IFACE_PRE_REMOVE,
 		GR_EVENT_IFACE_STATUS_UP,
 	},

modules/ip/control/address.c
@@ -257,15 +257,22 @@ static struct api_out addr_list(const void *request, struct api_ctx *ctx) {
 	return api_out(0, 0, NULL);
 }
 
-static void iface_pre_remove_cb(uint32_t /*event*/, const void *obj) {
+static void iface_event_cb(uint32_t event, const void *obj) {
 	const struct iface *iface = obj;
 	struct nexthop_info_l3 *l3;
 	struct hoplist *ifaddrs;
 
 	ifaddrs = addr4_get_all(iface->id);
-	if (ifaddrs == NULL)
+	if (ifaddrs == NULL || gr_vec_len(ifaddrs->nh) == 0)
 		return;
 
+	if (event == GR_EVENT_IFACE_POST_RECONFIG) {
+		if (iface->mode == GR_IFACE_MODE_VRF && iface->vrf_id == ifaddrs->nh[0]->vrf_id)
+			return;
+	}
+
+	// interface is either being deleted, mode changed to !VRF, or changed to another VRF
+	// delete all configured addresses
 	while (gr_vec_len(ifaddrs->nh) > 0) {
 		l3 = nexthop_info_l3(ifaddrs->nh[gr_vec_len(ifaddrs->nh) - 1]);
 		addr4_delete(iface->id, l3->ipv4, l3->prefixlen);
@@ -323,9 +330,9 @@ static struct gr_module addr_module = {
 };
 
 static struct gr_event_subscription iface_pre_rm_subscription = {
-	.callback = iface_pre_remove_cb,
-	.ev_count = 1,
-	.ev_types = {GR_EVENT_IFACE_PRE_REMOVE},
+	.callback = iface_event_cb,
+	.ev_count = 2,
+	.ev_types = {GR_EVENT_IFACE_POST_RECONFIG, GR_EVENT_IFACE_PRE_REMOVE},
 };
 static struct gr_event_subscription iface_up_subscription = {
 	.callback = iface_up_cb,

smoke/ip6_add_del_test.sh
@@ -14,3 +14,14 @@ grcli address del 2001::1/64 iface p0
 grcli address show
 grcli address add 2001::1/64 iface p0
 grcli address show
+
+grcli interface add vrf foo
+grcli interface set port p0 vrf foo
+grcli address show
+grcli nexthop show internal
+
+grcli interface add port p1 devargs net_tap1,iface=x-p1
+grcli interface set port p0 domain p1
+grcli interface set port p1 domain p0
+
+grcli nexthop show internal

commit 9e639b60d93460bd7f0bce1e15de9b026aee43c9

ip6: skip IPv6 auto-configuration on non-VRF interfaces
ip6_iface_llocal_init was called on POST_ADD for every new interface
regardless of its mode. On bond/bridge member ports, iface6_addr_add
rejects the address with EMEDIUMTYPE, and all multicast group joins
also fail:

  ERR: ip6_iface_llocal_init: iface_addr_add: Wrong medium type (124)
  INFO: mcast6_addr_add: p0: joining multicast group ff01::1
  INFO: ip6_iface_llocal_init: p0: mcast_addr_add: Machine is not on the network

Skip ip6_iface_llocal_init entirely for non-VRF interfaces.

Fixes: 14aa49098411 ("ip,ip6: flush addresses on interface mode change")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/address.c
@@ -461,7 +461,8 @@ static void ip6_iface_event_handler(uint32_t event, const void *obj) {
 
 	switch (event) {
 	case GR_EVENT_IFACE_POST_ADD:
-		ip6_iface_llocal_init(iface);
+		if (iface->mode == GR_IFACE_MODE_VRF)
+			ip6_iface_llocal_init(iface);
 		break;
 	case GR_EVENT_IFACE_POST_RECONFIG:
 		if (iface->mode != GR_IFACE_MODE_VRF) {