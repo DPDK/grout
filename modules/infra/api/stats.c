DPDK/grout

commit d43077e5fb5a0184c86ecc581152c239dab32b74

worker: use pthread conditions to unblock threads
Instead of polling an atomic boolean value with micro sleeps, use
pthread condition objects that are designed exactly for signaling
between threads.

This allows the kernel to properly schedule waiting threads until they
need to be woken up.

This will be leveraged in future commits to keep worker threads alive
all the time even when they don't have any RXQ to poll.

Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/infra/control/gr_worker.h
@@ -40,7 +40,6 @@ struct worker_stats {
 };
 
 struct worker {
-	atomic_bool started; // dataplane: wo, ctlplane: ro
 	atomic_bool shutdown; // dataplane: ro, ctlplane: wo
 	atomic_uint next_config; // dataplane: ro, ctlplane: rw
 	atomic_uint cur_config; // dataplane: wo, ctlplane: ro
@@ -57,6 +56,9 @@ struct worker {
 	unsigned lcore_id;
 	pid_t tid;
 
+	pthread_mutex_t lock;
+	pthread_cond_t ready;
+
 	// private for control plane only
 	pthread_t thread;
 	struct queue_map *rxqs;
@@ -69,5 +71,7 @@ extern struct workers workers;
 
 int worker_rxq_assign(uint16_t port_id, uint16_t rxq_id, uint16_t cpu_id);
 int worker_queue_distribute(const cpu_set_t *affinity, struct iface_info_port **ports);
+void worker_wait_ready(struct worker *);
+void worker_signal_ready(struct worker *);
 
 #endif

modules/infra/control/graph.c
@@ -239,6 +239,7 @@ int worker_graph_reload(struct worker *worker) {
 
 	// wait for datapath worker to pickup the config update
 	atomic_store(&worker->next_config, next);
+	worker_signal_ready(worker);
 	while (atomic_load(&worker->cur_config) != next)
 		usleep(500);
 

modules/infra/control/meson.build
@@ -22,6 +22,8 @@ tests += [
       '-Wl,--wrap=numa_available',
       '-Wl,--wrap=numa_node_of_cpu',
       '-Wl,--wrap=pthread_cancel',
+      '-Wl,--wrap=pthread_cond_wait',
+      '-Wl,--wrap=pthread_cond_timedwait',
       '-Wl,--wrap=pthread_create',
       '-Wl,--wrap=pthread_join',
       '-Wl,--wrap=rte_dev_name',

modules/infra/control/worker.c
@@ -37,43 +37,54 @@ int worker_create(unsigned cpu_id) {
 	struct worker *worker = rte_zmalloc(__func__, sizeof(*worker), 0);
 	pthread_attr_t attr;
 	cpu_set_t cpuset;
-	int ret;
+	int ret = ENOMEM;
 
 	if (worker == NULL)
-		return errno_log(ENOMEM, "rte_zmalloc");
+		goto end;
 
 	worker->cpu_id = cpu_id;
 	worker->lcore_id = LCORE_ID_ANY;
-
-	if (!!(ret = pthread_attr_init(&attr))) {
-		rte_free(worker);
-		return errno_log(ret, "pthread_attr_init");
-	}
+	pthread_mutex_init(&worker->lock, NULL);
+	pthread_mutex_lock(&worker->lock);
+	pthread_cond_init(&worker->ready, NULL);
 
 	CPU_ZERO(&cpuset);
 	CPU_SET(cpu_id, &cpuset);
-	if (!!(ret = pthread_attr_setaffinity_np(&attr, sizeof(cpuset), &cpuset))) {
-		rte_free(worker);
-		pthread_attr_destroy(&attr);
-		return errno_log(ret, "pthread_attr_setaffinity_np");
-	}
+	pthread_attr_init(&attr);
+	if (!!(ret = pthread_attr_setaffinity_np(&attr, sizeof(cpuset), &cpuset)))
+		goto end;
 
-	if (!!(ret = pthread_create(&worker->thread, &attr, gr_datapath_loop, worker))) {
-		pthread_cancel(worker->thread);
-		pthread_attr_destroy(&attr);
-		rte_free(worker);
-		return errno_log(ret, "pthread_create");
-	}
+	if (!!(ret = pthread_create(&worker->thread, &attr, gr_datapath_loop, worker)))
+		goto end;
 
 	STAILQ_INSERT_TAIL(&workers, worker, next);
 
 	// wait until thread has initialized lcore_id
-	while (!atomic_load(&worker->started))
-		usleep(500);
+	struct timespec timeout;
+	clock_gettime(CLOCK_REALTIME, &timeout);
+	timeout.tv_sec += 1;
+	do
+		ret = pthread_cond_timedwait(&worker->ready, &worker->lock, &timeout);
+	while (ret == EAGAIN || ret == EINTR);
 
+end:
 	pthread_attr_destroy(&attr);
-	LOG(INFO, "worker %u started", worker->cpu_id);
-	return 0;
+	if (worker != NULL)
+		pthread_mutex_unlock(&worker->lock);
+
+	if (ret == 0) {
+		LOG(INFO, "worker %u started", cpu_id);
+	} else {
+		if (worker != NULL) {
+			pthread_cancel(worker->thread);
+			pthread_cond_destroy(&worker->ready);
+			pthread_mutex_destroy(&worker->lock);
+			rte_free(worker);
+		}
+		LOG(ERR, "worker %u start failed: %s", cpu_id, strerror(ret));
+	}
+
+	return errno_set(ret);
 }
 
 int worker_destroy(unsigned cpu_id) {
@@ -85,6 +96,7 @@ int worker_destroy(unsigned cpu_id) {
 	STAILQ_REMOVE(&workers, worker, worker, next);
 
 	atomic_store(&worker->shutdown, true);
+	worker_signal_ready(worker);
 	pthread_join(worker->thread, NULL);
 	worker_graph_free(worker);
 	gr_vec_free(worker->rxqs);
@@ -93,6 +105,21 @@ int worker_destroy(unsigned cpu_id) {
 
 	LOG(INFO, "worker %d destroyed", cpu_id);
 	return 0;
+}
+
+void worker_wait_ready(struct worker *w) {
+	int ret;
+	pthread_mutex_lock(&w->lock);
+	do
+		ret = pthread_cond_wait(&w->ready, &w->lock);
+	while (ret == EAGAIN || ret == EINTR);
+	pthread_mutex_unlock(&w->lock);
+}
+
+void worker_signal_ready(struct worker *w) {
+	pthread_mutex_lock(&w->lock);
+	pthread_cond_signal(&w->ready);
+	pthread_mutex_unlock(&w->lock);
 }
 
 unsigned worker_count(void) {

modules/infra/datapath/main_loop.c
@@ -149,7 +149,7 @@ void *gr_datapath_loop(void *priv) {
 	static_assert(atomic_is_lock_free(&w->shutdown));
 	static_assert(atomic_is_lock_free(&w->cur_config));
 	static_assert(atomic_is_lock_free(&w->stats_reset));
-	atomic_store(&w->started, true);
+	worker_signal_ready(w);
 
 reconfig:
 	if (atomic_load(&w->shutdown))
@@ -160,7 +160,7 @@ reconfig:
 	atomic_store(&w->cur_config, cur);
 
 	if (graph == NULL) {
-		usleep(1000);
+		worker_wait_ready(w);
 		goto reconfig;
 	}
 

modules/infra/control/worker_test.c
@@ -20,11 +20,11 @@
 #include <rte_ethdev.h>
 
 static struct iface *ifaces[] = {NULL, NULL, NULL};
-static struct worker w1 = {.cpu_id = 1, .started = true};
-static struct worker w2 = {.cpu_id = 2, .started = true};
-static struct worker w3 = {.cpu_id = 3, .started = true};
-static struct worker w4 = {.cpu_id = 4, .started = true};
-static struct worker w5 = {.cpu_id = 5, .started = true};
+static struct worker w1 = {.cpu_id = 1};
+static struct worker w2 = {.cpu_id = 2};
+static struct worker w3 = {.cpu_id = 3};
+static struct worker w4 = {.cpu_id = 4};
+static struct worker w5 = {.cpu_id = 5};
 static struct rte_eth_dev_info dev_info = {.driver_name = "net_null", .nb_rx_queues = 2};
 
 // mocked types/functions
@@ -107,6 +107,11 @@ mock_func(
 );
 mock_func(int, __wrap_pthread_cancel(pthread_t));
 mock_func(int, __wrap_pthread_create(pthread_t *, const pthread_attr_t *, void *(void *), void *));
+mock_func(int, __wrap_pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *));
+mock_func(
+	int,
+	__wrap_pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, struct timespec *)
+);
 mock_func(int, __wrap_pthread_join(pthread_t *, const pthread_attr_t *, void *(void *), void *));
 mock_func(void *, __wrap_rte_zmalloc(char *, size_t, unsigned));
 
@@ -197,6 +202,8 @@ static void common_mocks(void) {
 	will_return_maybe(worker_graph_reload_all, 0);
 	will_return_maybe(__wrap_numa_available, 0);
 	will_return_maybe(__wrap_numa_node_of_cpu, 0);
+	will_return_maybe(__wrap_pthread_cond_wait, 0);
+	will_return_maybe(__wrap_pthread_cond_timedwait, 0);
 	will_return_maybe(__wrap_pthread_create, 0);
 	will_return_maybe(__wrap_pthread_join, 0);
 	will_return_maybe(__wrap_rte_dev_name, "");

commit 34360ecfada3c83297ac400b9e197fe2d023ea2c

worker: fix lost wakeup race on graph reload
When the control thread calls worker_graph_reload(), it stores the new
config index and signals the worker via pthread_cond_signal(). If the
worker has not yet reached pthread_cond_wait() at that point, the signal
is lost. Both threads then deadlock: the control thread polls
cur_config forever, and the worker blocks in pthread_cond_wait()
indefinitely.

Fix this by adding a boolean predicate to the wakeup condition, checked
under the mutex before waiting. The signaling side sets it to true
before calling pthread_cond_signal(), and the waiting side loops until
it becomes true. This is the standard pattern to avoid lost wakeups
with pthread conditions.

Also rename the API to worker_wakeup/worker_wait_wakeup, group the
mutex, condition and predicate into a struct, and drop the now unused
pthread_mutex_init/pthread_cond_init calls since zero-initialization
is equivalent to PTHREAD_MUTEX_INITIALIZER/PTHREAD_COND_INITIALIZER.

Fixes: d43077e5fb5a ("worker: use pthread conditions to unblock threads")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/api/stats.c
@@ -154,7 +154,7 @@ static struct api_out stats_reset(const void * /*request*/, struct api_ctx *) {
 
 	STAILQ_FOREACH (worker, &workers, next) {
 		atomic_store(&worker->stats_reset, true);
-		worker_signal_ready(worker);
+		worker_wakeup(worker);
 	}
 
 	iface = NULL;

modules/infra/control/gr_worker.h
@@ -62,8 +62,11 @@ struct worker {
 	unsigned lcore_id;
 	pid_t tid;
 
-	pthread_mutex_t lock;
-	pthread_cond_t ready;
+	struct {
+		pthread_mutex_t lock;
+		pthread_cond_t cond;
+		bool set;
+	} wakeup;
 
 	// private for control plane only
 	pthread_t thread;
@@ -77,6 +80,6 @@ extern struct workers workers;
 
 int worker_rxq_assign(uint16_t port_id, uint16_t rxq_id, uint16_t cpu_id);
 int worker_queue_distribute(const cpu_set_t *affinity, gr_vec struct iface_info_port **ports);
-void worker_wait_ready(struct worker *);
-void worker_signal_ready(struct worker *);
+void worker_wait_wakeup(struct worker *);
+void worker_wakeup(struct worker *);
 gr_vec struct gr_infra_stat *worker_dump_stats(uint16_t cpu_id);

modules/infra/control/graph.c
@@ -257,7 +257,7 @@ int worker_graph_reload(struct worker *worker, gr_vec struct iface_info_port **p
 
 	// wait for datapath worker to pickup the config update
 	atomic_store(&worker->next_config, next);
-	worker_signal_ready(worker);
+	worker_wakeup(worker);
 	for (unsigned i = 0; atomic_load(&worker->cur_config) != next; i++) {
 		if (i >= 10000) // 10000 * 500us -> 5s
 			return errno_log(ETIMEDOUT, "worker not responding to graph reload");

modules/infra/control/meson.build
@@ -27,7 +27,7 @@ tests += [
       '-Wl,--wrap=numa_available',
       '-Wl,--wrap=numa_node_of_cpu',
       '-Wl,--wrap=pthread_cancel',
-      '-Wl,--wrap=pthread_cond_timedwait',
+      '-Wl,--wrap=pthread_cond_signal',
       '-Wl,--wrap=pthread_cond_wait',
       '-Wl,--wrap=pthread_create',
       '-Wl,--wrap=pthread_join',

modules/infra/control/worker.c
@@ -43,8 +43,8 @@ int worker_create(unsigned cpu_id) {
 
 	worker->cpu_id = cpu_id;
 	worker->lcore_id = LCORE_ID_ANY;
-	pthread_mutex_init(&worker->lock, NULL);
-	pthread_cond_init(&worker->ready, NULL);
+	pthread_mutex_init(&worker->wakeup.lock, NULL);
+	pthread_cond_init(&worker->wakeup.cond, NULL);
 
 	CPU_ZERO(&cpuset);
 	CPU_SET(cpu_id, &cpuset);
@@ -74,8 +74,8 @@ end:
 	} else {
 		if (worker != NULL) {
 			pthread_cancel(worker->thread);
-			pthread_cond_destroy(&worker->ready);
-			pthread_mutex_destroy(&worker->lock);
+			pthread_cond_destroy(&worker->wakeup.cond);
+			pthread_mutex_destroy(&worker->wakeup.lock);
 			rte_free(worker);
 		}
 		LOG(ERR, "worker %u start failed: %s", cpu_id, strerror(ret));
@@ -93,8 +93,10 @@ int worker_destroy(unsigned cpu_id) {
 	STAILQ_REMOVE(&workers, worker, worker, next);
 
 	atomic_store(&worker->shutdown, true);
-	worker_signal_ready(worker);
+	worker_wakeup(worker);
 	pthread_join(worker->thread, NULL);
+	pthread_cond_destroy(&worker->wakeup.cond);
+	pthread_mutex_destroy(&worker->wakeup.lock);
 	worker_graph_free(worker);
 	gr_vec_free(worker->rxqs);
 	gr_vec_free(worker->txqs);
@@ -104,19 +106,19 @@ int worker_destroy(unsigned cpu_id) {
 	return 0;
 }
 
-void worker_wait_ready(struct worker *w) {
-	int ret;
-	pthread_mutex_lock(&w->lock);
-	do
-		ret = pthread_cond_wait(&w->ready, &w->lock);
-	while (ret == EAGAIN || ret == EINTR);
-	pthread_mutex_unlock(&w->lock);
+void worker_wait_wakeup(struct worker *w) {
+	pthread_mutex_lock(&w->wakeup.lock);
+	while (!w->wakeup.set)
+		pthread_cond_wait(&w->wakeup.cond, &w->wakeup.lock);
+	w->wakeup.set = false;
+	pthread_mutex_unlock(&w->wakeup.lock);
 }
 
-void worker_signal_ready(struct worker *w) {
-	pthread_mutex_lock(&w->lock);
-	pthread_cond_signal(&w->ready);
-	pthread_mutex_unlock(&w->lock);
+void worker_wakeup(struct worker *w) {
+	pthread_mutex_lock(&w->wakeup.lock);
+	w->wakeup.set = true;
+	pthread_cond_signal(&w->wakeup.cond);
+	pthread_mutex_unlock(&w->wakeup.lock);
 }
 
 unsigned worker_count(void) {

modules/infra/datapath/main_loop.c
@@ -233,7 +233,7 @@ reconfig:
 	atomic_store(&w->cur_config, cur);
 
 	if (graph == NULL) {
-		worker_wait_ready(w);
+		worker_wait_wakeup(w);
 		if (ctx.w_stats != NULL && atomic_exchange(&w->stats_reset, false))
 			stats_reset(ctx.w_stats);
 		goto reconfig;

modules/infra/control/worker_test.c
@@ -147,10 +147,7 @@ mock_func(
 	}
 );
 mock_func(int, __wrap_pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *));
-mock_func(
-	int,
-	__wrap_pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, struct timespec *)
-);
+mock_func(int, __wrap_pthread_cond_signal(pthread_cond_t *));
 mock_func(int, __wrap_pthread_join(pthread_t *, const pthread_attr_t *, void *(void *), void *));
 mock_func(void *, __wrap_rte_zmalloc(char *, size_t, unsigned));
 
@@ -249,7 +246,7 @@ static void common_mocks(void) {
 	will_return_maybe(__wrap_numa_available, 0);
 	will_return_maybe(__wrap_numa_node_of_cpu, 0);
 	will_return_maybe(__wrap_pthread_cond_wait, 0);
-	will_return_maybe(__wrap_pthread_cond_timedwait, 0);
+	will_return_maybe(__wrap_pthread_cond_signal, 0);
 	will_return_maybe(__wrap_pthread_create, 0);
 	will_return_maybe(__wrap_pthread_join, 0);
 	will_return_maybe(__wrap_rte_dev_name, "");