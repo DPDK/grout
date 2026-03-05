DPDK/grout

commit 668067a7e01b9eea0a9a4cfa229f8cb0d000cc45

nexthop: reject nexthops on non-existing VRFs
Validate vrf_id in get_vrf_iface() and make nexthop_update() fail when
the referenced VRF does not exist. This prevents creating nexthops that
are bound to missing VRFs.

Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/nexthop.c
@@ -390,6 +390,9 @@ int nexthop_update(struct nexthop *nh, const struct gr_nexthop_base *base, const
 			goto err;
 		}
 		nh->vrf_id = iface->vrf_id;
+	} else if (get_vrf_iface(nh->vrf_id) == NULL) {
+		ret = -errno;
+		goto err;
 	}
 
 	// Import type-specific info using callback

modules/infra/control/vrf.c
@@ -24,6 +24,11 @@ struct vrf_info {
 static struct vrf_info vrfs[GR_MAX_VRFS];
 
 struct iface *get_vrf_iface(uint16_t vrf_id) {
+	if (vrf_id >= GR_MAX_VRFS)
+		return errno_set_null(EOVERFLOW);
+	if (vrfs[vrf_id].iface == NULL)
+		return errno_set_null(ENONET);
+
 	return vrfs[vrf_id].iface;
 }
 

commit 01f965b7eed285a0cc46be8d44a8d5e7e15a7f5a

nexthop: allow internal nexthops without VRF
Commit 668067a7e01b ("nexthop: reject nexthops on non-existing VRFs")
made nexthop_update() reject all nexthops with an unresolvable VRF.
This broke SRv6 tunnel source nexthops which are created internally
with vrf_id=GR_VRF_ID_UNDEF since they are not bound to any VRF.

Skip the VRF validation when vrf_id is GR_VRF_ID_UNDEF. To prevent
API clients from abusing this, reject nexthop add requests that have
both vrf_id and iface_id undefined.

Fixes: 668067a7e01b ("nexthop: reject nexthops on non-existing VRFs")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/api/nexthop.c
@@ -42,6 +42,10 @@ static struct api_out nh_add(const void *request, struct api_ctx *) {
 	struct nexthop *nh;
 	int ret = 0;
 
+	if (req->nh.base.type != GR_NH_T_GROUP && req->nh.base.vrf_id == GR_VRF_ID_UNDEF
+	    && req->nh.base.iface_id == GR_IFACE_ID_UNDEF)
+		return api_out(EINVAL, 0, NULL);
+
 	nh = nexthop_lookup(&req->nh.base, req->nh.info);
 
 	if (nh == NULL) {

modules/infra/control/nexthop.c
@@ -360,7 +360,7 @@ int nexthop_update(struct nexthop *nh, const struct gr_nexthop_base *base, const
 			goto err;
 		}
 		nh->vrf_id = iface->vrf_id;
-	} else if (get_vrf_iface(nh->vrf_id) == NULL) {
+	} else if (nh->vrf_id != GR_VRF_ID_UNDEF && get_vrf_iface(nh->vrf_id) == NULL) {
 		ret = -errno;
 		goto err;
 	}

smoke/config_test.sh
@@ -85,6 +85,12 @@ grcli address show iface p3 | grep -qF '10.3.0.1/24' && fail "p3 should have no 
 grcli address show iface p3 | grep -qF '2348::1/24' && fail "p3 should have no user IPv6 after flush"
 grcli address show iface p3 | grep -qF 'fe80::' || fail "p3 should still have link-local after flush"
 
+# Test SRv6 tunsrc set/clear (internal nexthop with no VRF)
+grcli tunsrc set fd00::1 || fail "tunsrc set should succeed"
+grcli tunsrc show | grep -qF 'fd00::1' || fail "tunsrc addr should be fd00::1"
+grcli tunsrc clear || fail "tunsrc clear should succeed"
+grcli tunsrc show | grep -qF '::' || fail "tunsrc addr should be unspec after clear"
+
 grcli nexthop del 42
 grcli nexthop del 666
 grcli nexthop del 123456