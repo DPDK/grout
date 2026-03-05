DPDK/grout

commit ff5b30efcf7b099c8dc1b4d018ec1a9fb59c94ce

bond: use domain relationship for port attachment
Instead of specifying members during bond creation and tracking them via
a bond_iface_id field in port info, use the generic domain mechanism
introduced earlier. Ports are now attached to bonds using the domain
attribute, and bond membership is tracked through the interface mode.

Add GR_IFACE_MODE_BOND to indicate that an interface is part of a bond.
Implement attach_domain and detach_domain callbacks for the bond
interface type to handle member addition and removal.

Remove the bond_iface_id field from struct gr_iface_info_port and the
GR_BOND_SET_MEMBERS attribute since members are now managed through the
domain interface mechanism. The CLI syntax changes accordingly: bonds
are created without member specification, and ports specify their domain
when created or reconfigured.

The event handler for bond member status updates now checks interface
mode instead of bond_iface_id. Member detachment on interface removal
is handled automatically by the detach_domain callback.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Anthony Harivel <aharivel@redhat.com>

modules/infra/control/bond.c
@@ -69,16 +69,28 @@ static int bond_mac_del(struct iface *iface, const struct rte_ether_addr *mac) {
 
 static int bond_mac_set(struct iface *iface, const struct rte_ether_addr *mac) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
+	struct rte_ether_addr e;
 	int ret;
+
+	if (rte_is_zero_ether_addr(mac)) {
+		if (bond->primary_member >= bond->n_members)
+			return 0;
+		if (iface_get_eth_addr(bond->members[bond->primary_member].iface, &e) < 0)
+			return -errno;
+	} else if (rte_is_same_ether_addr(mac, &bond->mac)) {
+		return 0;
+	} else {
+		e = *mac;
+	}
 
 	if (!rte_is_zero_ether_addr(&bond->mac)) {
 		if ((ret = bond_all_member_del_mac(bond, &bond->mac)) < 0)
 			return ret;
 	}
-	if ((ret = bond_all_member_add_mac(bond, mac)) < 0)
+	if ((ret = bond_all_member_add_mac(bond, &e)) < 0)
 		return ret;
 
-	bond->mac = *mac;
+	bond->mac = e;
 
 	return 0;
 }
@@ -141,65 +153,86 @@ static int bond_promisc_set(struct iface *iface, bool enabled) {
 	return bond_all_members_set_flag(iface, GR_IFACE_F_PROMISC, enabled, iface_set_promisc);
 }
 
-static int bond_init_new_members(const struct iface *iface, const struct gr_iface_info_bond *new) {
+static int bond_attach_member(struct iface *iface, struct iface *member) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
-	struct iface_info_port *port;
 
-	for (uint8_t i = 0; i < new->n_members; i++) {
-		struct iface *member = iface_from_id(new->members[i].iface_id);
-		if (member == NULL)
-			return errno_set(errno);
+	if (member->type != GR_IFACE_TYPE_PORT)
+		return errno_set(EMEDIUMTYPE);
 
-		if (member->type != GR_IFACE_TYPE_PORT)
-			return errno_set(EMEDIUMTYPE);
-
-		for (uint8_t j = 0; j < bond->n_members; j++) {
-			if (bond->members[j].iface->id == member->id)
-				goto skip;
-		}
-
-		LOG(DEBUG, "adding %s to bond %s", member->name, iface->name);
-		port = iface_info_port(member);
-		port->bond_iface_id = iface->id;
-skip:;
+	for (unsigned i = 0; i < bond->n_members; i++) {
+		if (bond->members[i].iface == member)
+			return 0; // already a member
 	}
+
+	if (bond->n_members == ARRAY_DIM(bond->members))
+		return errno_set(EUSERS);
+
+	if (rte_is_zero_ether_addr(&bond->mac))
+		iface_get_eth_addr(member, &bond->mac);
+	else if (iface_add_eth_addr(member, &bond->mac) < 0)
+		return errno_log(errno, "iface_add_eth_addr(member)");
+
+	gr_vec_foreach_ref (struct rte_ether_addr *mac, bond->extra_macs) {
+		if (iface_add_eth_addr(member, mac) < 0)
+			return errno_log(errno, "iface_add_eth_addr(member)");
+	}
+
+	if (iface->flags & GR_IFACE_F_PROMISC && iface_set_promisc(member, true) < 0)
+		return errno_log(errno, "iface_set_promisc(member)");
+
+	if (bond->n_members == 0)
+		bond->primary_member = 0;
+	struct bond_member *m = &bond->members[bond->n_members];
+	memset(m, 0, sizeof(*m));
+	m->iface = member;
+	bond->n_members++;
+
+	bond_update_active_members(iface);
+
+	member->mode = GR_IFACE_MODE_BOND;
+	member->domain_id = iface->id;
 
 	return 0;
 }
 
-static void bond_fini_old_members(const struct iface *iface, const struct gr_iface_info_bond *new) {
+static int bond_detach_member(struct iface *iface, struct iface *member) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
-	struct iface_info_port *port;
 
-	for (uint8_t i = 0; i < bond->n_members; i++) {
-		struct iface *member = bond->members[i].iface;
+	LOG(DEBUG, "removing %s from bond %s", member->name, iface->name);
 
-		for (uint8_t j = 0; j < new->n_members; j++) {
-			if (new->members[j].iface_id == member->id)
-				goto skip;
-		}
-
-		LOG(DEBUG, "removing %s from bond %s", member->name, iface->name);
-		gr_vec_foreach_ref (struct rte_ether_addr *mac, bond->extra_macs) {
-			if (iface_del_eth_addr(member, mac) < 0 && errno != ENOENT) {
+	for (unsigned i = 0; i < bond->n_members; i++) {
+		if (bond->members[i].iface == member) {
+			unsigned last = bond->n_members - 1;
+			if (i < last) {
+				if (bond->primary_member == last)
+					bond->primary_member = i;
+				bond->members[i] = bond->members[last];
+			}
+			bond->n_members--;
+			if (bond->primary_member >= bond->n_members)
+				bond->primary_member--;
+			gr_vec_foreach_ref (struct rte_ether_addr *mac, bond->extra_macs) {
+				if (iface_del_eth_addr(member, mac) < 0 && errno != ENOENT) {
+					LOG(WARNING,
+					    "failed to unconfigure mac address on member %s: %s",
+					    member->name,
+					    strerror(errno));
+				}
+			}
+			if (iface_del_eth_addr(member, &bond->mac) < 0 && errno != ENOENT) {
 				LOG(WARNING,
 				    "failed to unconfigure mac address on member %s: %s",
 				    member->name,
 				    strerror(errno));
 			}
+			member->mode = GR_IFACE_MODE_VRF;
+			member->domain_id = GR_IFACE_ID_UNDEF;
+			bond_update_active_members(iface);
+			break;
 		}
-		if (iface_del_eth_addr(member, &bond->mac) < 0 && errno != ENOENT) {
-			LOG(WARNING,
-			    "failed to unconfigure mac address on member %s: %s",
-			    member->name,
-			    strerror(errno));
-		}
-
-		port = iface_info_port(member);
-		port->bond_iface_id = GR_IFACE_ID_UNDEF;
-		memset(&bond->members[i], 0, sizeof(bond->members[i]));
-skip:;
 	}
+
+	return 0;
 }
 
 void bond_update_active_members(struct iface *iface) {
@@ -329,60 +362,34 @@ static int bond_reconfig(
 		bond->algo = api->algo ?: GR_BOND_ALGO_RSS;
 
 	if (set_attrs & GR_BOND_SET_PRIMARY) {
-		uint8_t n_members = (set_attrs & GR_BOND_SET_MEMBERS) ?
-			api->n_members :
-			bond->n_members;
-
-		if (api->primary_member >= n_members)
+		if (api->primary_member >= bond->n_members)
 			return errno_set(ERANGE);
 
 		bond->primary_member = api->primary_member;
-	}
-
-	if (set_attrs & GR_BOND_SET_MEMBERS) {
-		if (api->n_members > ARRAY_DIM(bond->members))
-			return errno_set(ERANGE);
-
-		if (bond_init_new_members(iface, api) < 0)
-			return errno_set(errno);
-
-		bond_fini_old_members(iface, api);
-
-		for (uint8_t i = 0; i < api->n_members; i++)
-			bond->members[i].iface = iface_from_id(api->members[i].iface_id);
-		bond->n_members = api->n_members;
-	}
-
-	if (set_attrs & (GR_BOND_SET_MAC | GR_BOND_SET_MEMBERS | GR_BOND_SET_PRIMARY)) {
-		struct rte_ether_addr mac;
-		if (rte_is_zero_ether_addr(&api->mac)) {
-			const struct iface *primary = bond->members[bond->primary_member].iface;
-			if (iface_get_eth_addr(primary, &mac) < 0)
-				return errno_set(errno);
-		} else {
-			mac = api->mac;
-		}
-		if (bond_mac_set(iface, &mac) < 0)
-			return errno_set(errno);
-		bond->mac = mac;
-	}
-
-	if (set_attrs & (GR_BOND_SET_MEMBERS | GR_BOND_SET_PRIMARY))
 		bond_update_active_members(iface);
+	}
+
+	if (set_attrs & (GR_BOND_SET_MAC | GR_BOND_SET_PRIMARY)) {
+		if (iface_set_eth_addr(iface, &api->mac) < 0)
+			return -errno;
+	}
 
 	return 0;
 }
 
 static int bond_init(struct iface *iface, const void *api_info) {
 	struct gr_iface conf = {.base = iface->base};
-	return bond_reconfig(iface, IFACE_SET_ALL, &conf, api_info);
+	return bond_reconfig(iface, IFACE_SET_ALL & ~GR_BOND_SET_PRIMARY, &conf, api_info);
 }
 
 static int bond_fini(struct iface *iface) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
-	struct gr_iface_info_bond zero = {.n_members = 0};
-	bond_fini_old_members(iface, &zero);
+
+	while (bond->n_members > 0)
+		bond_detach_member(iface, bond->members[bond->n_members - 1].iface);
+
 	gr_vec_free(bond->extra_macs);
+
 	return 0;
 }
 
@@ -416,6 +423,8 @@ static struct iface_type iface_type_bond = {
 	.init = bond_init,
 	.reconfig = bond_reconfig,
 	.fini = bond_fini,
+	.attach_domain = bond_attach_member,
+	.detach_domain = bond_detach_member,
 	.set_up_down = bond_up_down,
 	.set_eth_addr = bond_mac_set,
 	.get_eth_addr = bond_mac_get,
@@ -426,59 +435,24 @@ static struct iface_type iface_type_bond = {
 	.to_api = bond_to_api,
 };
 
-static void bond_event(uint32_t event, const void *obj) {
-	const struct iface_info_port *port;
+static void bond_event(uint32_t /*event*/, const void *obj) {
 	const struct iface *iface = obj;
-	struct iface *b;
+	struct iface *bond;
 
-	if (iface->type != GR_IFACE_TYPE_PORT)
+	if (iface->mode != GR_IFACE_MODE_BOND)
 		return;
 
-	port = iface_info_port(iface);
-	if (port->bond_iface_id == GR_IFACE_ID_UNDEF)
+	bond = iface_from_id(iface->domain_id);
+	if (bond == NULL)
 		return;
 
-	b = iface_from_id(port->bond_iface_id);
-	if (b == NULL)
-		return;
-
-	if (event == GR_EVENT_IFACE_PRE_REMOVE) {
-		// Remove port from bond
-		struct iface_info_bond *bond = iface_info_bond(b);
-		const struct iface *primary;
-
-		assert(bond->n_members > 0);
-		assert(bond->primary_member < bond->n_members);
-		primary = bond->members[bond->primary_member].iface;
-
-		for (uint8_t i = 0; i < bond->n_members; i++) {
-			if (bond->members[i].iface == iface && i != bond->n_members - 1) {
-				// Replace the deleted member with the last one in the list
-				bond->members[i] = bond->members[bond->n_members - 1];
-				break;
-			}
-		}
-
-		bond->n_members--;
-
-		// Update the primary member index after deletion
-		bond->primary_member = 0;
-		for (uint8_t i = 0; i < bond->n_members; i++) {
-			if (bond->members[i].iface == primary) {
-				bond->primary_member = i;
-				break;
-			}
-		}
-	}
-
-	bond_update_active_members(b);
+	bond_update_active_members(bond);
 }
 
 static struct gr_event_subscription bond_event_handler = {
 	.callback = bond_event,
-	.ev_count = 3,
+	.ev_count = 2,
 	.ev_types = {
-		GR_EVENT_IFACE_PRE_REMOVE,
 		GR_EVENT_IFACE_STATUS_UP,
 		GR_EVENT_IFACE_STATUS_DOWN,
 	},

modules/infra/cli/bond.c
@@ -145,27 +145,6 @@ static uint64_t parse_bond_args(
 		set_attrs |= GR_BOND_SET_ALGO;
 	}
 
-	if (arg_str(p, "MEMBER") != NULL) {
-		const struct ec_pnode *m = NULL;
-		bond->n_members = 0;
-		while ((m = ec_pnode_find_next(p, m, "MEMBER", true)) != NULL) {
-			if (bond->n_members >= ARRAY_DIM(bond->members)) {
-				errno = EUSERS;
-				goto err;
-			}
-			const struct ec_strvec *v = ec_pnode_get_strvec(m);
-			assert(v != NULL);
-			assert(ec_strvec_len(v) == 1);
-			struct gr_iface *member = iface_from_name(c, ec_strvec_val(v, 0));
-			if (member == NULL) {
-				goto err;
-			}
-			bond->members[bond->n_members++].iface_id = member->id;
-			free(member);
-		}
-		set_attrs |= GR_BOND_SET_MEMBERS;
-	}
-
 	if ((str = arg_str(p, "PRIMARY")) != NULL) {
 		if (bond->mode != GR_BOND_MODE_ACTIVE_BACKUP) {
 			errno = EPROTOTYPE;
@@ -254,7 +233,7 @@ out:
 	return ret;
 }
 
-#define BOND_ATTRS_CMD IFACE_ATTRS_CMD ",((primary PRIMARY)|(balance ALGO)),(mac MAC)"
+#define BOND_ATTRS_CMD IFACE_ATTRS_CMD ",(balance ALGO),(mac MAC)"
 #define BOND_ATTRS_ARGS                                                                            \
 	IFACE_ATTRS_ARGS,                                                                          \
 		with_help(                                                                         \
@@ -266,10 +245,6 @@ out:
 				),                                                                 \
 				with_help("LACP mode.", ec_node_str("", "lacp"))                   \
 			)                                                                          \
-		),                                                                                 \
-		with_help(                                                                         \
-			"Primary member.",                                                         \
-			ec_node_dyn("PRIMARY", complete_iface_names, INT2PTR(GR_IFACE_TYPE_PORT))  \
 		),                                                                                 \
 		with_help(                                                                         \
 			"Balancing algorithm.",                                                    \
@@ -292,21 +267,17 @@ static int ctx_init(struct ec_node *root) {
 
 	ret = CLI_COMMAND(
 		INTERFACE_ADD_CTX(root),
-		"bond NAME mode MODE (member MEMBER)+ [" BOND_ATTRS_CMD "]",
+		"bond NAME mode MODE [" BOND_ATTRS_CMD "]",
 		bond_add,
 		"Create a new bond interface.",
 		with_help("Interface name.", ec_node("any", "NAME")),
-		with_help(
-			"Member port interface.",
-			ec_node_dyn("MEMBER", complete_iface_names, INT2PTR(GR_IFACE_TYPE_PORT))
-		),
 		BOND_ATTRS_ARGS
 	);
 	if (ret < 0)
 		return ret;
 	ret = CLI_COMMAND(
 		INTERFACE_SET_CTX(root),
-		"bond NAME (name NEW_NAME),(member MEMBER)+,(mode MODE)," BOND_ATTRS_CMD,
+		"bond NAME (name NEW_NAME),(primary PRIMARY),(mode MODE)," BOND_ATTRS_CMD,
 		bond_set,
 		"Modify bond parameters.",
 		with_help(
@@ -314,10 +285,6 @@ static int ctx_init(struct ec_node *root) {
 			ec_node_dyn("NAME", complete_iface_names, INT2PTR(GR_IFACE_TYPE_BOND))
 		),
 		with_help("New interface name.", ec_node("any", "NEW_NAME")),
-		with_help(
-			"Member port interface.",
-			ec_node_dyn("MEMBER", complete_iface_names, INT2PTR(GR_IFACE_TYPE_PORT))
-		),
 		with_help(
 			"Primary member.",
 			ec_node_dyn("PRIMARY", complete_iface_names, INT2PTR(GR_IFACE_TYPE_PORT))

frr/if_grout.c
@@ -67,10 +67,6 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		txqlen = gr_port->base.txq_size;
 		mac = &gr_port->base.mac;
 		link_type = ZEBRA_LLT_ETHER;
-		if (gr_port->bond_iface_id != GR_IFACE_ID_UNDEF) {
-			bond_ifindex = ifindex_grout_to_frr(gr_port->bond_iface_id);
-			slave_type = ZEBRA_IF_SLAVE_BOND;
-		}
 		break;
 	case GR_IFACE_TYPE_BOND:
 		gr_bond = (const struct gr_iface_info_bond *)&gr_if->info;
@@ -111,6 +107,31 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		dplane_ctx_set_status(ctx, ZEBRA_DPLANE_REQUEST_QUEUED);
 		dplane_ctx_set_ifp_mtu(ctx, gr_if->base.mtu);
 
+		switch (gr_if->mode) {
+		case GR_IFACE_MODE_VRF:
+			if (gr_if->base.vrf_id != 0) {
+				dplane_ctx_set_ifp_table_id(
+					ctx, ifindex_grout_to_frr(gr_if->base.vrf_id)
+				);
+				// In Linux, vrf_id equals the interface index; in Grout we model
+				// a VRF with its gr‑vrf interface.
+				// The gr‑vrf’s ifindex is guaranteed to match vrf_id
+				dplane_ctx_set_ifp_vrf_id(
+					ctx, ifindex_grout_to_frr(gr_if->base.vrf_id)
+				);
+			} else {
+				dplane_ctx_set_ifp_table_id(ctx, 0);
+				dplane_ctx_set_ifp_vrf_id(ctx, 0);
+			}
+			break;
+		case GR_IFACE_MODE_BOND:
+			bond_ifindex = ifindex_grout_to_frr(gr_if->domain_id);
+			slave_type = ZEBRA_IF_SLAVE_BOND;
+			break;
+		default:
+			break;
+		}
+
 		// no bridge support in grout
 		dplane_ctx_set_ifp_bridge_ifindex(ctx, IFINDEX_INTERNAL);
 		dplane_ctx_set_ifp_master_ifindex(ctx, IFINDEX_INTERNAL);
@@ -120,18 +141,6 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		dplane_ctx_set_ifp_zltype(ctx, link_type);
 		dplane_ctx_set_ifp_flags(ctx, gr_if_flags_to_netlink(gr_if, link_type));
 		dplane_ctx_set_ifp_protodown_set(ctx, false);
-
-		if (gr_if->base.vrf_id != 0) {
-			dplane_ctx_set_ifp_table_id(ctx, ifindex_grout_to_frr(gr_if->base.vrf_id));
-
-			// In Linux, vrf_id equals the interface index; in Grout we model a VRF
-			// with its gr‑vrf interface
-			// The gr‑vrf’s ifindex is guaranteed to match vrf_id
-			dplane_ctx_set_ifp_vrf_id(ctx, ifindex_grout_to_frr(gr_if->base.vrf_id));
-		} else {
-			dplane_ctx_set_ifp_table_id(ctx, 0);
-			dplane_ctx_set_ifp_vrf_id(ctx, 0);
-		}
 
 		if (mac)
 			dplane_ctx_set_ifp_hw_addr(

modules/infra/api/gr_infra.h
@@ -51,6 +51,7 @@ typedef enum : uint16_t {
 typedef enum : uint8_t {
 	GR_IFACE_MODE_VRF = 0,
 	GR_IFACE_MODE_XC,
+	GR_IFACE_MODE_BOND,
 	GR_IFACE_MODE_COUNT
 } gr_iface_mode_t;
 
@@ -95,7 +96,6 @@ struct __gr_iface_info_port_base {
 	uint16_t n_txq;
 	uint16_t rxq_size;
 	uint16_t txq_size;
-	uint16_t bond_iface_id;
 	struct rte_ether_addr mac;
 };
 
@@ -158,10 +158,9 @@ static inline const char *gr_bond_algo_name(gr_bond_algo_t algo) {
 
 // Bond reconfiguration attribute flags.
 #define GR_BOND_SET_MODE GR_BIT64(32)
-#define GR_BOND_SET_MEMBERS GR_BIT64(33)
-#define GR_BOND_SET_PRIMARY GR_BIT64(34)
-#define GR_BOND_SET_MAC GR_BIT64(35)
-#define GR_BOND_SET_ALGO GR_BIT64(36)
+#define GR_BOND_SET_PRIMARY GR_BIT64(33)
+#define GR_BOND_SET_MAC GR_BIT64(34)
+#define GR_BOND_SET_ALGO GR_BIT64(35)
 
 struct gr_bond_member {
 	uint16_t iface_id; // Must be a port interface.
@@ -449,6 +448,8 @@ static inline const char *gr_iface_mode_name(gr_iface_mode_t mode) {
 		return "VRF";
 	case GR_IFACE_MODE_XC:
 		return "XC";
+	case GR_IFACE_MODE_BOND:
+		return "bond";
 	case GR_IFACE_MODE_COUNT:
 		break;
 	}

modules/infra/cli/port.c
@@ -15,20 +15,12 @@
 #include <string.h>
 #include <sys/queue.h>
 
-static void port_show(struct gr_api_client *c, const struct gr_iface *iface) {
+static void port_show(struct gr_api_client *, const struct gr_iface *iface) {
 	const struct gr_iface_info_port *port = (const struct gr_iface_info_port *)iface->info;
 
 	printf("devargs: %s\n", port->devargs);
 	printf("driver:  %s\n", port->driver_name);
 	printf("mac: " ETH_F "\n", &port->mac);
-	if (port->bond_iface_id != GR_IFACE_ID_UNDEF) {
-		struct gr_iface *bond = iface_from_id(c, port->bond_iface_id);
-		if (bond != NULL)
-			printf("bond: %s\n", bond->name);
-		else
-			printf("bond: %u\n", port->bond_iface_id);
-		free(bond);
-	}
 	printf("n_rxq: %u\n", port->n_rxq);
 	printf("n_txq: %u\n", port->n_txq);
 	printf("rxq_size: %u\n", port->rxq_size);
@@ -36,20 +28,9 @@ static void port_show(struct gr_api_client *c, const struct gr_iface *iface) {
 }
 
 static void
-port_list_info(struct gr_api_client *c, const struct gr_iface *iface, char *buf, size_t len) {
+port_list_info(struct gr_api_client *, const struct gr_iface *iface, char *buf, size_t len) {
 	const struct gr_iface_info_port *port = (const struct gr_iface_info_port *)iface->info;
-	struct gr_iface *bond = NULL;
-	size_t n = 0;
-
-	SAFE_BUF(snprintf, len, "devargs=%s mac=" ETH_F, port->devargs, &port->mac);
-	if (port->bond_iface_id != GR_IFACE_ID_UNDEF) {
-		if ((bond = iface_from_id(c, port->bond_iface_id)) != NULL)
-			SAFE_BUF(snprintf, len, " bond=%s", bond->name);
-		else
-			SAFE_BUF(snprintf, len, " bond=%u", port->bond_iface_id);
-	}
-err:
-	free(bond);
+	snprintf(buf, len, "devargs=%s mac=" ETH_F, port->devargs, &port->mac);
 }
 
 static struct cli_iface_type port_type = {

modules/infra/control/lacp.c
@@ -26,27 +26,27 @@
 static struct event *lacp_timer;
 
 void lacp_input_cb(void *obj, uintptr_t, const struct control_queue_drain *drain) {
-	const struct iface_info_port *port;
-	const struct iface *port_iface;
 	struct iface_info_bond *bond;
 	struct rte_mbuf *mbuf = obj;
 	const struct lacp_pdu *pdu;
 	struct bond_member *member;
+	const struct iface *iface;
 	struct iface *bond_iface;
 
-	port_iface = mbuf_data(mbuf)->iface;
+	iface = mbuf_data(mbuf)->iface;
 
 	// Check if packet references deleted interface.
-	if (drain && drain->event == GR_EVENT_IFACE_REMOVE && port_iface == drain->obj)
+	if (drain && drain->event == GR_EVENT_IFACE_REMOVE && iface == drain->obj)
 		goto out;
-	if (port_iface->type != GR_IFACE_TYPE_PORT) {
-		LOG(DEBUG, "interface %s is not a port", port_iface->name);
+	if (iface->mode != GR_IFACE_MODE_BOND) {
+		LOG(DEBUG, "interface %s is not part of a bond", iface->name);
 		goto out;
 	}
-	port = iface_info_port(port_iface);
-	bond_iface = iface_from_id(port->bond_iface_id);
+	bond_iface = iface_from_id(iface->domain_id);
+	if (drain && drain->event == GR_EVENT_IFACE_REMOVE && bond_iface == drain->obj)
+		goto out;
 	if (bond_iface == NULL) {
-		LOG(DEBUG, "bond %u has disappeared", port->bond_iface_id);
+		LOG(DEBUG, "bond %u has disappeared", iface->domain_id);
 		goto out;
 	}
 	bond = iface_info_bond(bond_iface);
@@ -57,13 +57,13 @@ void lacp_input_cb(void *obj, uintptr_t, const struct control_queue_drain *drain
 
 	member = NULL;
 	for (uint8_t i = 0; i < bond->n_members; i++) {
-		if (bond->members[i].iface == port_iface) {
+		if (bond->members[i].iface == iface) {
 			member = &bond->members[i];
 			break;
 		}
 	}
 	if (member == NULL) {
-		LOG(DEBUG, "port %s is not part of bond %s", port_iface->name, bond_iface->name);
+		LOG(DEBUG, "port %s is not part of bond %s", iface->name, bond_iface->name);
 		goto out;
 	}
 

modules/infra/datapath/lacp_input.c
@@ -23,7 +23,7 @@ enum {
 
 static uint16_t
 lacp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
-	struct eth_input_mbuf_data *eth_data;
+	const struct iface *iface;
 	struct lacp_pdu *lacp;
 	struct rte_mbuf *mbuf;
 	rte_edge_t edge;
@@ -43,12 +43,8 @@ lacp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 		}
 
 		// Check that the RX interface is a port member of a bond
-		eth_data = eth_input_mbuf_data(mbuf);
-		if (eth_data->iface->type != GR_IFACE_TYPE_PORT) {
-			edge = NO_BOND;
-			goto next;
-		}
-		if (iface_info_port(eth_data->iface)->bond_iface_id == GR_IFACE_ID_UNDEF) {
+		iface = mbuf_data(mbuf)->iface;
+		if (iface->mode != GR_IFACE_MODE_BOND || iface->domain_id == GR_IFACE_ID_UNDEF) {
 			edge = NO_BOND;
 			goto next;
 		}

modules/infra/datapath/port_rx.c
@@ -50,8 +50,8 @@ rx_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t
 	port = iface_info_port(ctx->iface);
 	if (!port->started)
 		return 0;
-	if (port->bond_iface_id != GR_IFACE_ID_UNDEF) {
-		iface = iface_from_id(port->bond_iface_id);
+	if (ctx->iface->mode == GR_IFACE_MODE_BOND) {
+		iface = iface_from_id(ctx->iface->domain_id);
 		if (iface == NULL)
 			return 0;
 		const struct iface_info_bond *bond = iface_info_bond(iface);

commit cf9aad41f7e3f4201204b87136eeb3a297c7ff5e

bond: use bond_mac_set when attaching members
The inline MAC logic in bond_attach_member only inherited the member
MAC on the very first member and did not propagate it to other members.
It was also inconsistent with bond_mac_set's behavior.

Call bond_mac_set with a zero MAC after adding the member. This reuses
the existing logic to inherit the primary member's MAC and propagate
it to all members.

Note: this means that if a MAC was explicitly configured on the bond
before adding members, it will be overwritten by the primary member's
MAC. This should be addressed separately by tracking whether the MAC
was explicitly set.

Fixes: ff5b30efcf7b ("bond: use domain relationship for port attachment")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/bond.c
@@ -160,6 +160,7 @@ static int bond_promisc_set(struct iface *iface, bool enabled) {
 
 static int bond_attach_member(struct iface *iface, struct iface *member) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
+	static const struct rte_ether_addr zero = {0};
 	int ret;
 
 	if (member->type != GR_IFACE_TYPE_PORT)
@@ -172,11 +173,6 @@ static int bond_attach_member(struct iface *iface, struct iface *member) {
 
 	if (bond->n_members == ARRAY_DIM(bond->members))
 		return errno_set(EUSERS);
-
-	if (rte_is_zero_ether_addr(&bond->mac))
-		iface_get_eth_addr(member, &bond->mac);
-	else if (iface_add_eth_addr(member, &bond->mac) < 0)
-		return errno_log(errno, "iface_add_eth_addr(member)");
 
 	gr_vec_foreach_ref (struct rte_ether_addr *mac, bond->extra_macs) {
 		if (iface_add_eth_addr(member, mac) < 0)
@@ -192,6 +188,11 @@ static int bond_attach_member(struct iface *iface, struct iface *member) {
 	memset(m, 0, sizeof(*m));
 	m->iface = member;
 	bond->n_members++;
+
+	if (bond_mac_set(iface, &zero) < 0) {
+		bond->n_members--;
+		return errno_log(errno, "bond_mac_set");
+	}
 
 	bond_update_active_members(iface);
 