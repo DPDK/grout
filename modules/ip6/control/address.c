DPDK/grout

commit c561b28b70f6af46b18abac33dc687da50442714

infra: sync iface addresses to per-VRF gr-loopX so kernel can bind
When an address is configured on an interface in Grout, automatically
add the same address to the corresponding per-VRF loopback (gr-loopX) as
a host address (/32 or /128).

This mirrors Grout-managed local addresses into the kernel namespace,
allowing Linux applications to bind()/connect() using those addresses
(per VRF) without manual IP setup.

The change wires the add/del paths so the loopback is kept in sync with
Grout’s interface addresses.

Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/address.c
@@ -10,6 +10,7 @@
 #include <gr_log.h>
 #include <gr_module.h>
 #include <gr_net_types.h>
+#include <gr_netlink.h>
 #include <gr_queue.h>
 #include <gr_rcu.h>
 #include <gr_vec.h>
@@ -193,6 +194,7 @@ static int mcast6_addr_del(const struct iface *iface, const struct rte_ipv6_addr
 static int
 iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8_t prefixlen) {
 	struct rte_ipv6_addr solicited_node;
+	const struct iface *vrf_iface;
 	struct hoplist *addrs;
 	struct nexthop *nh;
 	int ret;
@@ -243,6 +245,10 @@ iface6_addr_add(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 	if (ret < 0)
 		return errno_set(-ret);
 
+	vrf_iface = get_vrf_iface(iface->vrf_id);
+	if (vrf_iface && netlink_add_addr6(vrf_iface->name, ip) < 0)
+		LOG(WARNING, "add addr " IP6_F " on linux has failed (%s)", ip, strerror(errno));
+
 	// gr_vec_add may realloc() and free the old vector
 	// Duplicate the whole vector and append to the clone.
 	gr_vec struct nexthop **nhs_copy = NULL;
@@ -287,6 +293,7 @@ static struct api_out addr6_add(const void *request, struct api_ctx *) {
 static int
 iface6_addr_del(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8_t prefixlen) {
 	struct rte_ipv6_addr solicited_node;
+	const struct iface *vrf_iface;
 	struct nexthop *nh = NULL;
 	struct hoplist *addrs;
 	unsigned i = 0;
@@ -325,6 +332,10 @@ iface6_addr_del(const struct iface *iface, const struct rte_ipv6_addr *ip, uint8
 		if (errno != EOPNOTSUPP && errno != ENOENT)
 			return errno_set(errno);
 	}
+
+	vrf_iface = get_vrf_iface(iface->vrf_id);
+	if (vrf_iface && netlink_del_addr6(vrf_iface->name, ip) < 0)
+		LOG(WARNING, "delete addr " IP6_F " on linux has failed (%s)", ip, strerror(errno));
 
 	return 0;
 }
@@ -477,6 +488,7 @@ static struct gr_event_subscription iface_event_subscription = {
 		GR_EVENT_IFACE_STATUS_UP,
 	},
 };
+
 static struct gr_event_serializer iface_addr_serializer = {
 	.size = sizeof(struct gr_ip6_ifaddr),
 	.ev_count = 2,

modules/ip/control/address.c
@@ -10,6 +10,7 @@
 #include <gr_log.h>
 #include <gr_module.h>
 #include <gr_net_types.h>
+#include <gr_netlink.h>
 #include <gr_queue.h>
 #include <gr_rcu.h>
 #include <gr_vec.h>
@@ -64,8 +65,8 @@ struct nexthop *addr4_get_preferred(uint16_t iface_id, ip4_addr_t dst) {
 
 static struct api_out addr_add(const void *request, struct api_ctx *) {
 	const struct gr_ip4_addr_add_req *req = request;
+	const struct iface *vrf_iface, *iface;
 	struct hoplist *ifaddrs;
-	const struct iface *iface;
 	struct nexthop *nh;
 	int ret;
 
@@ -124,6 +125,13 @@ static struct api_out addr_add(const void *request, struct api_ctx *) {
 		gr_vec_free(nhs_old);
 	}
 
+	vrf_iface = get_vrf_iface(iface->vrf_id);
+	if (vrf_iface && netlink_add_addr4(vrf_iface->name, req->addr.addr.ip) < 0)
+		LOG(WARNING,
+		    "add addr " IP4_F " on linux has failed (%s)",
+		    &req->addr.addr.ip,
+		    strerror(errno));
+
 	gr_event_push(GR_EVENT_IP_ADDR_ADD, &req->addr);
 
 	return api_out(0, 0, NULL);
@@ -131,6 +139,7 @@ static struct api_out addr_add(const void *request, struct api_ctx *) {
 
 static struct api_out addr_del(const void *request, struct api_ctx *) {
 	const struct gr_ip4_addr_del_req *req = request;
+	const struct iface *iface;
 	struct hoplist *addrs;
 	struct nexthop *nh;
 	unsigned i = 0;
@@ -157,6 +166,17 @@ static struct api_out addr_del(const void *request, struct api_ctx *) {
 	rib4_cleanup(nh);
 
 	gr_vec_del(addrs->nh, i);
+
+	iface = iface_from_id(req->addr.iface_id);
+	if (iface) {
+		const struct iface *vrf_iface = get_vrf_iface(iface->vrf_id);
+
+		if (vrf_iface && netlink_del_addr4(vrf_iface->name, req->addr.addr.ip) < 0)
+			LOG(WARNING,
+			    "delete addr " IP4_F " on linux has failed (%s)",
+			    &req->addr.addr.ip,
+			    strerror(errno));
+	}
 
 	return api_out(0, 0, NULL);
 }

modules/infra/control/gr_netlink.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <gr_net_types.h>
+
 #include <stdbool.h>
 
 int netlink_link_set_admin_state(const char *ifname, bool up);
@@ -11,3 +13,7 @@ int netlink_link_add_vrf(const char *vrf_name, uint32_t table_id);
 int netlink_link_del_iface(const char *ifname);
 int netlink_add_route(const char *ifname, uint32_t table);
 int netlink_del_route(const char *ifname, uint32_t table);
+int netlink_add_addr4(const char *ifname, ip4_addr_t ip);
+int netlink_del_addr4(const char *ifname, ip4_addr_t ip);
+int netlink_add_addr6(const char *ifname, const struct rte_ipv6_addr *ip);
+int netlink_del_addr6(const char *ifname, const struct rte_ipv6_addr *ip);

modules/infra/control/netlink.c
@@ -200,6 +200,59 @@ int netlink_del_route(const char *ifname, uint32_t table) {
 	return netlink_add_del_route(ifname, table, false);
 }
 
+static int netlink_add_del_addr(const char *ifname, const void *addr, size_t addr_len, bool add) {
+	char buf[NLMSG_SPACE(
+		sizeof(struct ifaddrmsg) + 2 * NLA_SPACE(sizeof(struct rte_ipv6_addr))
+		+ // IFA_LOCAL + IFA_ADDRESS
+		NLA_SPACE(IFNAMSIZ) // IFA_LABEL
+	)];
+	bool is_ipv4 = (addr_len == sizeof(ip4_addr_t));
+	struct nlmsghdr *nlh;
+	struct ifaddrmsg *ifa;
+	uint32_t ifindex;
+
+	ifindex = if_nametoindex(ifname);
+	if (!ifindex)
+		return errno_set(ENODEV);
+
+	memset(buf, 0, sizeof(buf));
+	nlh = mnl_nlmsg_put_header(buf);
+	nlh->nlmsg_type = add ? RTM_NEWADDR : RTM_DELADDR;
+	nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK;
+	if (add)
+		nlh->nlmsg_flags |= NLM_F_CREATE | NLM_F_EXCL;
+
+	ifa = mnl_nlmsg_put_extra_header(nlh, sizeof(*ifa));
+	ifa->ifa_family = is_ipv4 ? AF_INET : AF_INET6;
+	ifa->ifa_prefixlen = is_ipv4 ? 32 : 128;
+	ifa->ifa_scope = RT_SCOPE_UNIVERSE;
+	ifa->ifa_index = ifindex;
+
+	if (is_ipv4)
+		mnl_attr_put(nlh, IFA_LOCAL, addr_len, addr);
+
+	mnl_attr_put(nlh, IFA_ADDRESS, addr_len, addr);
+	mnl_attr_put_strz(nlh, IFA_LABEL, ifname);
+
+	return netlink_send_req(nlh);
+}
+
+int netlink_add_addr4(const char *ifname, ip4_addr_t ip) {
+	return netlink_add_del_addr(ifname, &ip, sizeof(ip), true);
+}
+
+int netlink_del_addr4(const char *ifname, ip4_addr_t ip) {
+	return netlink_add_del_addr(ifname, &ip, sizeof(ip), false);
+}
+
+int netlink_add_addr6(const char *ifname, const struct rte_ipv6_addr *ip) {
+	return netlink_add_del_addr(ifname, ip, sizeof(*ip), true);
+}
+
+int netlink_del_addr6(const char *ifname, const struct rte_ipv6_addr *ip) {
+	return netlink_add_del_addr(ifname, ip, sizeof(*ip), false);
+}
+
 static void netlink_init(struct event_base *) {
 	nl_sock = mnl_socket_open(NETLINK_ROUTE);
 	if (!nl_sock)

commit 87d4e8e5581247c59ea393878151c1d60ade9271

ip6: tolerate EADDRNOTAVAIL on netlink address deletion
When a port's peer TAP interface is destroyed (e.g. network namespace
cleanup), the link goes down and the kernel removes addresses from the
cp interface. When grout later deletes the interface, ip6_iface_addrs_flush
tries to remove the already-gone address via netlink:

  WARN: iface6_addr_del: delete addr fe80::... on linux has failed (Cannot assign requested address)

This is an unavoidable race between kernel cleanup and grout teardown.
The delete is idempotent, so EADDRNOTAVAIL is not an error worth
reporting.

Fixes: c561b28b70f6 ("infra: sync iface addresses to per-VRF gr-loopX so kernel can bind")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/ip6/control/address.c
@@ -326,7 +326,7 @@ int addr6_delete(uint16_t iface_id, const struct rte_ipv6_addr *ip, uint8_t pref
 			return errno_set(errno);
 	}
 
-	if (netlink_del_addr6(iface->cp_id, ip) < 0)
+	if (netlink_del_addr6(iface->cp_id, ip) < 0 && errno != EADDRNOTAVAIL)
 		LOG(WARNING, "delete addr " IP6_F " on linux has failed (%s)", ip, strerror(errno));
 
 	return 0;