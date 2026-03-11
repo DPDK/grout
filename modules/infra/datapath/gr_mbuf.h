ca8d639c364b9afc4e91e13606512cc0f2602b17
Robin Jarry <rjarry@redhat.com>
2026-03-11 11:19:52 +0100

    mbuf: remove intermediate hidden structure for traces
    The GR_MBUF_PRIV_DATA_TYPE macro generates a hidden __type_name wrapper
    struct to place the trace list head before the actual data fields. This
    makes mbuf_data size computation and pahole output confusing. There is
    no real reason to hide the trace head from the data structure. Move it
    directly into the generated struct and return rte_mbuf_to_priv(m)
    without going through an intermediate container.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: David Marchand <david.marchand@redhat.com>

--- modules/infra/datapath/gr_mbuf.h
+++ modules/infra/datapath/gr_mbuf.h
@@ -27,17 +27,13 @@ STAILQ_HEAD(gr_trace_head, gr_trace_item);
 
 #define GR_MBUF_PRIV_DATA_TYPE(type_name, fields)                                                  \
 	struct type_name {                                                                         \
+		struct gr_trace_head traces;                                                       \
 		const struct iface *iface;                                                         \
 		struct fields;                                                                     \
 	};                                                                                         \
-	struct __##type_name {                                                                     \
-		struct gr_trace_head traces;                                                       \
-		struct type_name data;                                                             \
-	};                                                                                         \
 	static inline struct type_name *type_name(struct rte_mbuf *m) {                            \
-		static_assert(sizeof(struct __##type_name) <= GR_MBUF_PRIV_MAX_SIZE);              \
-		struct __##type_name *priv = rte_mbuf_to_priv(m);                                  \
-		return &priv->data;                                                                \
+		static_assert(sizeof(struct type_name) <= GR_MBUF_PRIV_MAX_SIZE);                  \
+		return rte_mbuf_to_priv(m);                                                        \
 	}
 
 GR_MBUF_PRIV_DATA_TYPE(mbuf_data, {});
@@ -45,7 +41,7 @@ GR_MBUF_PRIV_DATA_TYPE(queue_mbuf_data, { struct rte_mbuf *next; });
 
 // Get the head of trace items from an mbuf.
 static inline struct gr_trace_head *gr_mbuf_traces(struct rte_mbuf *m) {
-	return rte_mbuf_to_priv(m);
+	return &mbuf_data(m)->traces;
 }
 
 // Return true the mbuf already contains trace items.
