DPDK/grout

commit 12496c5b2cf27ed4946f4adada125bd0775687ba

l2: add bridge datapath nodes
Add bridge_input and bridge_flood datapath nodes.

bridge_input receives packets from member interfaces via
GR_IFACE_MODE_BRIDGE. It learns source MAC addresses into the FDB
(unless GR_BRIDGE_F_NO_LEARN is set), then looks up the destination.

Known unicast destinations are forwarded to the learned output
interface. Unknown unicast, broadcast and multicast are sent to
bridge_flood. Hairpin packets (destination is the source interface)
are dropped. When the destination is the bridge interface itself,
packets are sent to iface_input for local processing.

bridge_flood replicates each packet to all bridge members except the
ingress interface, and to the bridge interface itself. The first
output reuses the original mbuf, subsequent ones are cloned.

When GR_BRIDGE_F_NO_FLOOD is set, the packet is dropped instead.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/l2/datapath/bridge_flood.c
@@ -1,0 +1,126 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#include <gr_graph.h>
+#include <gr_iface.h>
+#include <gr_infra.h>
+#include <gr_l2_control.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+#include <gr_port.h>
+#include <gr_trace.h>
+#include <gr_worker.h>
+
+#include <rte_ether.h>
+#include <rte_malloc.h>
+#include <rte_mbuf.h>
+
+enum edges {
+	OUTPUT = 0,
+	INPUT,
+	DROP,
+	EDGE_COUNT
+};
+
+static inline struct rte_mbuf *
+clone_packet(struct rte_mbuf *m, uint16_t clone_count, const struct iface *output_iface) {
+	struct rte_mbuf *clone;
+
+	// Copy packet for each output port (except the first one)
+	if (clone_count == 0) {
+		clone = m;
+	} else {
+		clone = gr_mbuf_copy(m, UINT32_MAX, sizeof(struct mbuf_data));
+		if (clone == NULL) {
+			// TODO: add xstat
+			return NULL;
+		}
+	}
+
+	mbuf_data(clone)->iface = output_iface;
+
+	return clone;
+}
+
+static uint16_t bridge_flood_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	const struct iface *br, *member, *iface;
+	const struct iface_info_bridge *bridge;
+	struct rte_mbuf *m, *clone;
+	uint16_t flood_count;
+	uint16_t sent = 0;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		flood_count = 0;
+
+		if (gr_mbuf_is_traced(m))
+			gr_mbuf_trace_add(m, node, 0);
+
+		iface = mbuf_data(m)->iface;
+		assert(iface != NULL);
+
+		br = iface_from_id(iface->domain_id);
+		if (br == NULL || br->type != GR_IFACE_TYPE_BRIDGE)
+			goto next;
+
+		bridge = iface_info_bridge(br);
+
+		for (uint16_t j = 0; j < bridge->n_members; j++) {
+			member = bridge->members[j];
+
+			if (member == iface)
+				continue; // Never flood back to source
+
+			if (!(member->flags & GR_IFACE_F_UP))
+				continue; // Skip down interfaces
+
+			clone = clone_packet(m, flood_count, member);
+			if (clone == NULL)
+				continue;
+
+			rte_node_enqueue_x1(graph, node, OUTPUT, clone);
+			flood_count++;
+		}
+		if (iface != br && (br->flags & GR_IFACE_F_UP)) {
+			// also flood to bridge interface
+			clone = clone_packet(m, flood_count, br);
+			if (clone != NULL) {
+				rte_node_enqueue_x1(graph, node, INPUT, clone);
+				flood_count++;
+			}
+		}
+next:
+		if (flood_count == 0) {
+			// If no flooding occurred, drop the original packet
+			rte_node_enqueue_x1(graph, node, DROP, m);
+		}
+		sent += flood_count;
+	}
+
+	return sent;
+}
+
+static struct rte_node_register node = {
+	.name = "bridge_flood",
+	.process = bridge_flood_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[OUTPUT] = "iface_output",
+		[INPUT] = "iface_input",
+		[DROP] = "bridge_flood_drop",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.type = GR_NODE_T_L2,
+};
+
+GR_NODE_REGISTER(info);
+
+GR_DROP_REGISTER(bridge_flood_drop);

modules/l2/datapath/bridge_input.c
@@ -1,0 +1,150 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#include <gr_eth.h>
+#include <gr_iface.h>
+#include <gr_l2_control.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+#include <gr_rxtx.h>
+#include <gr_trace.h>
+
+#include <rte_ether.h>
+
+enum edges {
+	OUTPUT = 0,
+	INPUT,
+	FLOOD,
+	BRIDGE_INVAL,
+	HAIRPIN,
+	OUT_IFACE_INVAL,
+	FLOOD_DISABLED,
+	EDGE_COUNT
+};
+
+struct bridge_input_trace {
+	uint16_t iface_id;
+	uint16_t bridge_id;
+};
+
+static uint16_t bridge_input_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	const struct iface *bridge, *iface;
+	const struct iface_info_bridge *br;
+	const struct gr_fdb_entry *fdb;
+	struct iface_mbuf_data *d;
+	struct rte_ether_hdr *eth;
+	struct rte_mbuf *m;
+	rte_edge_t edge;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		m = objs[i];
+		d = iface_mbuf_data(m);
+		eth = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+		fdb = NULL;
+
+		if (gr_mbuf_is_traced(m)) {
+			struct bridge_input_trace *t = gr_mbuf_trace_add(m, node, sizeof(*t));
+			t->iface_id = d->iface->id;
+			t->bridge_id = d->iface->domain_id;
+		}
+
+		bridge = iface_from_id(d->iface->domain_id);
+		if (bridge == NULL || bridge->type != GR_IFACE_TYPE_BRIDGE) {
+			edge = BRIDGE_INVAL;
+			goto next;
+		}
+		br = iface_info_bridge(bridge);
+
+		if (rte_is_unicast_ether_addr(&eth->src_addr)
+		    && !(br->flags & GR_BRIDGE_F_NO_LEARN))
+			fdb_learn(bridge->id, d->iface->id, &eth->src_addr, d->vlan_id);
+
+		if (rte_is_unicast_ether_addr(&eth->dst_addr)) {
+			fdb = fdb_lookup(bridge->id, &eth->dst_addr, d->vlan_id);
+			if (fdb == NULL) {
+				// Unknown unicast
+				edge = FLOOD;
+				goto next;
+			}
+			if (fdb->iface_id == d->iface->id) {
+				// Don't forward back to source interface
+				edge = HAIRPIN;
+				goto next;
+			}
+			iface = iface_from_id(fdb->iface_id);
+			if (iface == NULL) {
+				edge = OUT_IFACE_INVAL;
+				goto next;
+			}
+			// Direct output to learned interface
+			d->iface = iface;
+			if (iface->type == GR_IFACE_TYPE_BRIDGE) {
+				edge = INPUT;
+			} else {
+				edge = OUTPUT;
+			}
+		} else {
+			// Broadcast, multicast
+			edge = FLOOD;
+		}
+next:
+		if (edge == FLOOD && (br->flags & GR_BRIDGE_F_NO_FLOOD))
+			edge = FLOOD_DISABLED;
+
+		rte_node_enqueue_x1(graph, node, edge, m);
+	}
+
+	return nb_objs;
+}
+
+static int bridge_input_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
+	const struct bridge_input_trace *t = data;
+	const struct iface *iface = iface_from_id(t->iface_id);
+	const struct iface *bridge = iface_from_id(t->bridge_id);
+	return snprintf(
+		buf,
+		len,
+		"iface=%s bridge=%s",
+		iface ? iface->name : "[deleted]",
+		bridge ? bridge->name : "[deleted]"
+	);
+}
+
+static void bridge_input_register(void) {
+	iface_input_mode_register(GR_IFACE_MODE_BRIDGE, "bridge_input");
+	iface_output_type_register(GR_IFACE_TYPE_BRIDGE, "bridge_input");
+}
+
+static struct rte_node_register node = {
+	.name = "bridge_input",
+	.process = bridge_input_process,
+	.nb_edges = EDGE_COUNT,
+	.next_nodes = {
+		[OUTPUT] = "iface_output",
+		[INPUT] = "iface_input",
+		[FLOOD] = "bridge_flood",
+		[BRIDGE_INVAL] = "bridge_input_invalid_domain",
+		[HAIRPIN] = "bridge_input_hairpin",
+		[OUT_IFACE_INVAL] = "bridge_input_invalid_output",
+		[FLOOD_DISABLED] = "bridge_input_flood_disabled",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.type = GR_NODE_T_L2,
+	.register_callback = bridge_input_register,
+	.trace_format = bridge_input_trace_format,
+};
+
+GR_NODE_REGISTER(info);
+
+GR_DROP_REGISTER(bridge_input_invalid_domain);
+GR_DROP_REGISTER(bridge_input_hairpin);
+GR_DROP_REGISTER(bridge_input_invalid_output);
+GR_DROP_REGISTER(bridge_input_flood_disabled);

modules/l2/datapath/meson.build
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2026 Robin Jarry
 
-subdir('api')
-subdir('cli')
-subdir('control')
-subdir('datapath')
+src += files(
+  'bridge_flood.c',
+  'bridge_input.c',
+)

commit 4c5cc6ba7a91b8c08784d7984c352fa15b59db1d

bridge,vxlan: fix incomplete mbuf priv data copy in flood
gr_mbuf_copy() takes a priv_len argument that callers are expected to
set to the right private data size. The bridge and vxlan flood nodes are
passing sizeof(struct mbuf_data) which does not include the vlan_id
field from iface_mbuf_data. Copies then inherit whatever is already
present in the new mbuf priv area. This can cause random VLANs to
magically appear on TX on regular ports in a bridge. Example:

Original packet has no VLAN:
    [rx p0] 02:e3:ad:74:50:9b > 33:33:00:00:00:16 / IPv6...

First copy gets a fresh mbuf:
    [tx p1] 02:e3:ad:74:50:9b > 33:33:00:00:00:16 / IPv6...

Second copy is less lucky:
    [tx p2] 02:e3:ad:74:50:9b > 33:33:00:00:00:16 / VLAN id=33022 / IPv6...
                                                    ^^^^^^^^^^^^^

Remove the priv_len parameter and always copy GR_MBUF_PRIV_MAX_SIZE (1
cache line) bytes so that callers cannot get this wrong again.

Fixes: 12496c5b2cf2 ("l2: add bridge datapath nodes")
Fixes: 3e529150d5ec ("l2: implement VXLAN datapath nodes")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

modules/infra/datapath/gr_mbuf.h
@@ -70,10 +70,10 @@ void gr_mbuf_trace_copy(struct rte_mbuf *dst, struct rte_mbuf *src);
 void gr_mbuf_trace_finish(struct rte_mbuf *m);
 
 // Deep copy of an mbuf: duplicates mbuf, copies mbuf priv data and traces
-static inline struct rte_mbuf *gr_mbuf_copy(struct rte_mbuf *m, size_t data_len, size_t priv_len) {
+static inline struct rte_mbuf *gr_mbuf_copy(struct rte_mbuf *m, size_t data_len) {
 	struct rte_mbuf *copy = rte_pktmbuf_copy(m, m->pool, 0, data_len);
 	if (copy) {
-		memcpy(mbuf_data(copy), mbuf_data(m), priv_len);
+		memcpy(mbuf_data(copy), mbuf_data(m), GR_MBUF_PRIV_MAX_SIZE);
 		if (gr_mbuf_is_traced(m))
 			gr_mbuf_trace_copy(copy, m);
 	}

modules/ip/datapath/ip_fragment.c
@@ -100,9 +100,7 @@ ip_fragment_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 		// Create remaining fragments
 		for (i = 1; i < num_frags; i++) {
 			// Create new fragment, copying the original IPv4 header.
-			frag_mbuf = gr_mbuf_copy(
-				mbuf, ip_hdr_len, sizeof(struct ip_output_mbuf_data)
-			);
+			frag_mbuf = gr_mbuf_copy(mbuf, ip_hdr_len);
 			if (unlikely(frag_mbuf == NULL)) {
 				break;
 			}

modules/l2/datapath/bridge_flood.c
@@ -31,7 +31,7 @@ copy_packet(struct rte_mbuf *m, uint16_t copy_count, const struct iface *output_
 	if (copy_count == 0) {
 		copy = m;
 	} else {
-		copy = gr_mbuf_copy(m, UINT32_MAX, sizeof(struct mbuf_data));
+		copy = gr_mbuf_copy(m, UINT32_MAX);
 		if (copy == NULL) {
 			// TODO: add xstat
 			return NULL;

modules/l2/datapath/vxlan_flood.c
@@ -33,7 +33,7 @@ vxlan_flood_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 			if (flood_count == 0) {
 				copy = m;
 			} else {
-				copy = gr_mbuf_copy(m, UINT32_MAX, sizeof(struct mbuf_data));
+				copy = gr_mbuf_copy(m, UINT32_MAX);
 				if (copy == NULL)
 					continue;
 			}