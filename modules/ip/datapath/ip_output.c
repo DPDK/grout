DPDK/grout

commit 09d4d5e7088f8ae5d8cc33adfceaab6a22061421

ip: enforce mbuf packet_type
Make sure that mbuf packet_type has RTE_PTYPE_L3_IPV4 flag in ip_output.
This is required by the following nodes.

Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/ip/datapath/ip_output.c
@@ -72,6 +72,8 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 			goto next;
 		}
 
+		mbuf->packet_type |= RTE_PTYPE_L3_IPV4;
+
 		edge = nh_type_edges[nh->type];
 		if (edge != ETH_OUTPUT)
 			goto next;

commit c8f943dd10f7996069993101a70033c402b88cec

ip,ip6: clear stale L3 packet type in output nodes
Packets can change address family during processing. For example,
srv6_output encapsulates an IPv4 packet inside an IPv6 header and
forwards it to ip6_output. At that point the mbuf still carries
RTE_PTYPE_L3_IPV4 from ip_output, and ip6_output adds
RTE_PTYPE_L3_IPV6 with |=, leaving both flags set.

Clear the opposite address family L3 flag in each output node to
ensure packet_type is always unambiguous.

In the next commits, we will add the possibility of cross-family routes
(e.g. IPv4 routes with an IPv6 nexthop).

This means an IPv4 packet may be held in queue waiting for the
resolution of an IPv6 nexthop. And it requires the IPv4 and IPv6 nexthop
functions to dispatch packets based on their packet type.

Fixes: 09d4d5e7088f ("ip: enforce mbuf packet_type")
Fixes: 16b3065495c2 ("ip6: enforce mbuf packet_type")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/datapath/ip_output.c
@@ -78,6 +78,7 @@ ip_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, u
 		}
 
 		mbuf->packet_type |= RTE_PTYPE_L3_IPV4;
+		mbuf->packet_type &= ~RTE_PTYPE_L3_IPV6;
 
 		edge = nh_type_edges[nh->type];
 		if (edge != ETH_OUTPUT)

modules/ip6/datapath/ip6_output.c
@@ -73,6 +73,7 @@ ip6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 		}
 
 		mbuf->packet_type |= RTE_PTYPE_L3_IPV6;
+		mbuf->packet_type &= ~RTE_PTYPE_L3_IPV4;
 
 		edge = nh_type_edges[nh->type];
 		if (edge != ETH_OUTPUT)