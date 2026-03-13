DPDK/grout

commit 67d0bc3fbdef2ae77e178196b6bf5188c5834889

loopback: fix packet type of packet received from linux
The loopback code was checking `data[0] == 6` to decide whether a packet
was IPv6, but the IP version is stored in the upper 4 bits of the first
byte.

Use `(data[0] & 0xf0) == 0x60` to check the version field and correctly
set `mbuf->packet_type` to either `RTE_PTYPE_L3_IPV6` or `RTE_PTYPE_L3_IPV4`.

Fixes: cdfe4353139d ("loopback: rebuild ip/ip6 header")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Acked-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/loopback.c
@@ -138,7 +138,7 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 	// We can't call rte_net_get_ptype directly as we do not have an ethernet frame.
 	// An option would be to prepend/adjust every buffer, but let's set directly
 	// the information we need instead.
-	mbuf->packet_type = data[0] == 6 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
+	mbuf->packet_type = (data[0] & 0xf0) == 0x60 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
 
 	// required by ip(6)_input
 	e = eth_input_mbuf_data(mbuf);

commit 8d44c92b3f43c394f21cde9ce15a06173d607a27

loopback: fix packet type detection
The packet_type is determined by reading data[0] and checking if it
looks like an IPv6 version nibble. However, data points to the start
of the buffer which includes the struct tun_pi header (4 bytes). The
first byte is tun_pi.flags which is always 0, so the check always
falls through to RTE_PTYPE_L3_IPV4 even for IPv6 packets.

Since ip6_output sets packet_type with |= RTE_PTYPE_L3_IPV6, loopback
originated IPv6 packets end up with both RTE_PTYPE_L3_IPV4 and
RTE_PTYPE_L3_IPV6 flags set, which is incorrect.

Use tun_pi.proto instead of guessing the version from the first byte.

This fix is required for the next commit where ip_hold/ip6_hold will
dispatch resubmit callbacks based on packet_type.

Fixes: 67d0bc3fbdef ("loopback: fix packet type of packet received from linux")
Fixes: cdfe4353139d ("loopback: rebuild ip/ip6 header")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/loopback.c
@@ -110,6 +110,7 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 	struct eth_input_mbuf_data *e;
 	struct iface_stats *stats;
 	struct rte_mbuf *mbuf;
+	struct tun_pi *pi;
 	size_t read_len;
 	size_t len;
 	char *data;
@@ -150,7 +151,18 @@ static void iface_loopback_poll(evutil_socket_t, short reason, void *ev_iface) {
 	// We can't call rte_net_get_ptype directly as we do not have an ethernet frame.
 	// An option would be to prepend/adjust every buffer, but let's set directly
 	// the information we need instead.
-	mbuf->packet_type = (data[0] & 0xf0) == 0x60 ? RTE_PTYPE_L3_IPV6 : RTE_PTYPE_L3_IPV4;
+	pi = (struct tun_pi *)data;
+	switch (pi->proto) {
+	case RTE_BE16(RTE_ETHER_TYPE_IPV4):
+		mbuf->packet_type = RTE_PTYPE_L3_IPV4;
+		break;
+	case RTE_BE16(RTE_ETHER_TYPE_IPV6):
+		mbuf->packet_type = RTE_PTYPE_L3_IPV6;
+		break;
+	default:
+		LOG(ERR, "unknown proto: %#x", rte_be_to_cpu_16(pi->proto));
+		goto err;
+	}
 
 	// required by ip(6)_input
 	e = eth_input_mbuf_data(mbuf);