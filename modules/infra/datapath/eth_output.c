DPDK/grout

commit 6e56b0c49bc4d2e8478d47116b6ce25c8a49033d

iface_output: split out of eth_output
Separate interface-level output handling from Ethernet-specific
processing. The eth_output node was doing both Ethernet header
construction and interface type dispatching, which are distinct
responsibilities.

The new iface_output node handles:

* Interface up/down state checks
* TX packet and byte statistics
* Dispatching to the appropriate next node based on interface type
* Interface tracing

This allows eth_output to focus solely on Ethernet concerns: source and
destination MAC addresses, VLAN header insertion, and ethertype
handling.

Update the control plane to post outgoing packets to iface_output
instead of port_output. The bond active-member selection logic in
ctlplane.c is no longer needed since iface_output now handles interface
type dispatching.

This causes around 4% drop in IPv4 forwarding performance due to the
additional node traversal overhead.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Anthony Harivel <aharivel@redhat.com>

modules/infra/datapath/eth_output.c
@@ -15,56 +15,48 @@
 #include <stdint.h>
 
 enum {
-	INVAL = 0,
+	OUTPUT = 0,
 	NO_HEADROOM,
 	NO_MAC,
-	IFACE_DOWN,
+	NO_PARENT,
 	NB_EDGES,
 };
-
-static rte_edge_t iface_type_edges[GR_IFACE_TYPE_COUNT] = {INVAL};
-
-void eth_output_register_interface_type(gr_iface_type_t type, const char *next_node) {
-	LOG(DEBUG, "eth_output: iface_type=%s -> %s", gr_iface_type_name(type), next_node);
-	if (type == GR_IFACE_TYPE_UNDEF || type >= ARRAY_DIM(iface_type_edges))
-		ABORT("invalid iface type=%u", type);
-	if (iface_type_edges[type] != INVAL)
-		ABORT("next node already registered for iface type=%s", gr_iface_type_name(type));
-	iface_type_edges[type] = gr_node_attach_parent("eth_output", next_node);
-}
 
 static uint16_t
 eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
 	struct eth_output_mbuf_data *priv;
 	struct rte_ether_addr src_mac;
 	const struct iface *iface;
-	struct rte_vlan_hdr *vlan;
 	struct rte_ether_hdr *eth;
 	struct rte_mbuf *mbuf;
+	uint16_t vlan_id;
 	rte_edge_t edge;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
 		priv = eth_output_mbuf_data(mbuf);
 		iface = priv->iface;
-		vlan = NULL;
+		vlan_id = 0;
 
 		if (priv->iface->type == GR_IFACE_TYPE_VLAN) {
+			struct iface_stats *stats = iface_get_stats(
+				rte_lcore_id(), priv->iface->id
+			);
+			stats->tx_packets += 1;
+			stats->tx_bytes += rte_pktmbuf_pkt_len(mbuf);
+
 			const struct iface_info_vlan *sub = iface_info_vlan(priv->iface);
 			priv->iface = iface_from_id(sub->parent_id);
 			if (priv->iface == NULL) {
-				edge = INVAL;
+				edge = NO_PARENT;
 				goto next;
 			}
-			if (!(priv->iface->flags & GR_IFACE_F_UP)) {
-				edge = IFACE_DOWN;
-				goto next;
-			}
-			vlan = gr_mbuf_prepend(mbuf, vlan);
+			struct rte_vlan_hdr *vlan = gr_mbuf_prepend(mbuf, vlan);
 			if (unlikely(vlan == NULL)) {
 				edge = NO_HEADROOM;
 				goto next;
 			}
+			vlan_id = sub->vlan_id;
 			vlan->vlan_tci = rte_cpu_to_be_16(sub->vlan_id);
 			vlan->eth_proto = priv->ether_type;
 			priv->ether_type = RTE_BE16(RTE_ETHER_TYPE_VLAN);
@@ -73,10 +65,6 @@ eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 			edge = NO_MAC;
 			goto next;
 		}
-
-		edge = iface_type_edges[priv->iface->type];
-		if (edge == INVAL)
-			goto next;
 
 		eth = gr_mbuf_prepend(mbuf, eth);
 		if (unlikely(eth == NULL)) {
@@ -87,19 +75,16 @@ eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 		eth->src_addr = src_mac;
 		eth->ether_type = priv->ether_type;
 
-		struct iface_stats *stats = iface_get_stats(rte_lcore_id(), iface->id);
-		stats->tx_packets += 1;
-		stats->tx_bytes += rte_pktmbuf_pkt_len(mbuf);
-
+		edge = OUTPUT;
+next:
 		if (gr_mbuf_is_traced(mbuf)) {
 			struct eth_trace_data *t = gr_mbuf_trace_add(mbuf, node, sizeof(*t));
-			t->eth.dst_addr = eth->dst_addr;
-			t->eth.src_addr = eth->src_addr;
-			t->eth.ether_type = vlan ? vlan->eth_proto : eth->ether_type;
-			t->vlan_id = rte_be_to_cpu_16(vlan ? vlan->vlan_tci : 0);
-			t->iface_id = priv->iface->id;
+			t->eth.dst_addr = priv->dst;
+			t->eth.src_addr = src_mac;
+			t->eth.ether_type = priv->ether_type;
+			t->vlan_id = vlan_id;
+			t->iface_id = iface->id;
 		}
-next:
 		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 
@@ -113,10 +98,10 @@ static struct rte_node_register node = {
 
 	.nb_edges = NB_EDGES,
 	.next_nodes = {
-		[INVAL] = "eth_output_inval",
+		[OUTPUT] = "iface_output",
 		[NO_HEADROOM] = "error_no_headroom",
 		[NO_MAC] = "eth_output_no_mac",
-		[IFACE_DOWN] = "iface_input_admin_down",
+		[NO_PARENT] = "eth_output_vlan_no_parent",
 	},
 };
 
@@ -128,5 +113,5 @@ static struct gr_node_info info = {
 
 GR_NODE_REGISTER(info);
 
-GR_DROP_REGISTER(eth_output_inval);
 GR_DROP_REGISTER(eth_output_no_mac);
+GR_DROP_REGISTER(eth_output_vlan_no_parent);

modules/infra/control/ctlplane.c
@@ -3,7 +3,6 @@
 
 #include "ctlplane.h"
 
-#include <gr_bond.h>
 #include <gr_config.h>
 #include <gr_control_input.h>
 #include <gr_control_queue.h>
@@ -16,7 +15,6 @@
 #include <gr_module.h>
 #include <gr_netlink.h>
 #include <gr_nh_control.h>
-#include <gr_port.h>
 #include <gr_vlan.h>
 
 #include <event2/event.h>
@@ -40,7 +38,7 @@
 static struct rte_mempool *cp_pool;
 static struct event_base *ev_base;
 
-static control_input_t port_output_id;
+static control_input_t iface_output;
 
 static void finalize_fd(struct event *ev, void * /*priv*/) {
 	int fd = event_get_fd(ev);
@@ -197,38 +195,18 @@ static void iface_cp_poll(evutil_socket_t, short reason, void *ev_iface) {
 		eth->dst_addr = dst;
 		eth->ether_type = RTE_BE16(RTE_ETHER_TYPE_VLAN);
 
+		stats = iface_get_stats(rte_lcore_id(), iface->id);
+		stats->tx_packets += 1;
+		stats->tx_bytes += rte_pktmbuf_pkt_len(mbuf);
+
 		const uint32_t parent_id = iface_info_vlan(iface)->parent_id;
 		iface = iface_from_id(parent_id);
 		if (iface == NULL) {
 			LOG(ERR, "iface_from_id: no iface for id %u", parent_id);
 			goto err;
 		}
-	} else if (iface->type == GR_IFACE_TYPE_BOND) {
-		// For bond interface, find either the active interface
-		// or the first active member (for LACP)
-		struct iface_info_bond *b = iface_info_bond(iface);
-		struct iface *child_iface = NULL;
-		if (b->mode == GR_BOND_MODE_ACTIVE_BACKUP) {
-			if (b->n_members == 0 || b->active_member >= b->n_members)
-				goto err;
-			child_iface = (struct iface *)b->members[b->active_member].iface;
-		} else if (b->mode == GR_BOND_MODE_LACP) {
-			for (uint8_t i = 0; i < b->n_members; i++) {
-				struct bond_member *member = &b->members[i];
-				if (member->active) {
-					child_iface = (struct iface *)member->iface;
-					break;
-				}
-			}
-		} else {
-			ABORT("unknown bond mode");
-		}
-		if (child_iface == NULL) {
-			LOG(ERR, "No active member in bond %s", iface->name);
-			goto err;
-		}
-		iface = child_iface;
 	}
+
 	mbuf_data(mbuf)->iface = iface;
 
 	stats = iface_get_stats(rte_lcore_id(), iface->id);
@@ -238,7 +216,7 @@ static void iface_cp_poll(evutil_socket_t, short reason, void *ev_iface) {
 	if (gr_config.log_packets)
 		trace_log_packet(mbuf, "cp rx", iface->name);
 
-	post_to_stack(port_output_id, mbuf);
+	post_to_stack(iface_output, mbuf);
 	return;
 
 err:
@@ -457,7 +435,7 @@ static void cp_module_init(struct event_base *base) {
 	if (!cp_pool)
 		ABORT("pktmbuf_pool returned NULL");
 	ev_base = base;
-	port_output_id = gr_control_input_register_handler("port_output", true);
+	iface_output = gr_control_input_register_handler("iface_output", true);
 }
 
 static void cp_module_fini(struct event_base *) {

modules/infra/datapath/bond_output.c
@@ -2,11 +2,11 @@
 // Copyright (c) 2025 Robin Jarry
 
 #include <gr_bond.h>
-#include <gr_eth.h>
 #include <gr_graph.h>
 #include <gr_iface.h>
 #include <gr_log.h>
 #include <gr_mbuf.h>
+#include <gr_rxtx.h>
 
 #include <rte_ether.h>
 #include <rte_ip4.h>
@@ -248,7 +248,7 @@ static struct rte_node_register bond_output_node = {
 };
 
 static void bond_output_register(void) {
-	eth_output_register_interface_type(GR_IFACE_TYPE_BOND, "bond_output");
+	iface_output_type_register(GR_IFACE_TYPE_BOND, "bond_output");
 }
 
 static struct gr_node_info info = {

modules/infra/datapath/gr_eth.h
@@ -36,8 +36,6 @@ struct eth_trace_data {
 
 int eth_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);
 
-void eth_output_register_interface_type(gr_iface_type_t, const char *next_node);
-
 int lacp_send_pdu(const struct iface *, const struct lacp_pdu *);
 
 #define GR_IPPROTO_EIGRP 88

modules/infra/datapath/gr_rxtx.h
@@ -41,3 +41,5 @@ struct port_output_edges {
 int rxtx_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/);
 
 void iface_input_mode_register(gr_iface_mode_t, const char *next_node);
+
+void iface_output_type_register(gr_iface_type_t, const char *next_node);

modules/infra/datapath/iface_output.c
@@ -1,0 +1,99 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 Robin Jarry
+
+#include <gr_graph.h>
+#include <gr_iface.h>
+#include <gr_log.h>
+#include <gr_mbuf.h>
+#include <gr_rxtx.h>
+#include <gr_trace.h>
+
+#include <stdint.h>
+
+enum {
+	INVAL = 0,
+	IFACE_DOWN,
+	NB_EDGES,
+};
+
+static rte_edge_t iface_type_edges[GR_IFACE_TYPE_COUNT] = {INVAL};
+
+void iface_output_type_register(gr_iface_type_t type, const char *next_node) {
+	LOG(DEBUG, "iface_output: iface_type=%s -> %s", gr_iface_type_name(type), next_node);
+	if (type == GR_IFACE_TYPE_UNDEF || type >= ARRAY_DIM(iface_type_edges))
+		ABORT("invalid iface type=%u", type);
+	if (iface_type_edges[type] != INVAL)
+		ABORT("next node already registered for iface type=%s", gr_iface_type_name(type));
+	iface_type_edges[type] = gr_node_attach_parent("iface_output", next_node);
+}
+
+static uint16_t iface_output_process(
+	struct rte_graph *graph,
+	struct rte_node *node,
+	void **objs,
+	uint16_t nb_objs
+) {
+	const struct iface *iface;
+	struct iface_stats *stats;
+	struct rte_mbuf *m;
+	rte_edge_t edge;
+
+	for (uint16_t i = 0; i < nb_objs; i++) {
+		m = objs[i];
+
+		iface = mbuf_data(m)->iface;
+		if (iface == NULL || iface->type >= ARRAY_DIM(iface_type_edges)) {
+			edge = INVAL;
+			goto next;
+		}
+
+		if (!(iface->flags & GR_IFACE_F_UP)) {
+			edge = IFACE_DOWN;
+			goto next;
+		}
+
+		stats = iface_get_stats(rte_lcore_id(), iface->id);
+		stats->tx_packets += 1;
+		stats->tx_bytes += rte_pktmbuf_pkt_len(m);
+
+		edge = iface_type_edges[iface->type];
+
+next:
+		if (gr_mbuf_is_traced(m)) {
+			uint16_t *iface_id = gr_mbuf_trace_add(m, node, sizeof(*iface_id));
+			*iface_id = iface ? iface->id : 0;
+		}
+		rte_node_enqueue_x1(graph, node, edge, m);
+	}
+
+	return nb_objs;
+}
+
+static int iface_trace_format(char *buf, size_t len, const void *data, size_t /*data_len*/) {
+	const uint16_t *iface_id = data;
+	const struct iface *iface = iface_from_id(*iface_id);
+	return snprintf(buf, len, "iface=%s", iface ? iface->name : "[deleted]");
+}
+
+static struct rte_node_register node = {
+	.name = "iface_output",
+
+	.process = iface_output_process,
+
+	.nb_edges = NB_EDGES,
+	.next_nodes = {
+		[INVAL] = "iface_output_inval_type",
+		[IFACE_DOWN] = "iface_output_admin_down",
+	},
+};
+
+static struct gr_node_info info = {
+	.node = &node,
+	.type = GR_NODE_T_L1,
+	.trace_format = iface_trace_format,
+};
+
+GR_NODE_REGISTER(info);
+
+GR_DROP_REGISTER(iface_output_inval_type);
+GR_DROP_REGISTER(iface_output_admin_down);

modules/infra/datapath/meson.build
@@ -8,6 +8,7 @@ src += files(
   'drop.c',
   'eth_input.c',
   'eth_output.c',
+  'iface_output.c',
   'l1_xconnect.c',
   'l2_redirect.c',
   'lacp_input.c',

commit d4b63050d540463600e3bc7798f181c2f63ba036

eth_output: fix corrupted trace
Writing vlan_id=0 in the mbuf_data overwrites the start of
eth_output_mbuf_data->dst. This causes partially truncated traces where
the destination ethernet address starts with zeroes.

--------- 15:28:50.562920770 cpu 0 ---------
port_rx-p0q0: RX_L4_CKSUM_UNKNOWN RX_IP_CKSUM_GOOD RX_OUTER_L4_CKSUM_UNKNOWN TX_L4_NO_CKSUM
iface_input: iface=p0 mode=VRF
eth_input: 02:e3:ad:74:50:9b > 96:57:00:fb:bf:73 type=IP(0x0800)
ip_input: 16.0.0.1 > 16.1.0.1 ttl=64 proto=ICMP(1)
ip_forward:
ip_output: 16.0.0.1 > 16.1.0.1 ttl=63 proto=ICMP(1)
eth_output: 1e:58:43:26:80:7e > 00:00:27:14:8a:4f type=IP(0x0800)
                                ^^^^^
iface_output: iface=p1
port_output:
port_tx-p1q0: RX_L4_CKSUM_UNKNOWN RX_IP_CKSUM_GOOD RX_OUTER_L4_CKSUM_UNKNOWN TX_L4_NO_CKSUM

Zero the vlan_id *after* inserting the traces.

Fixes: 6e56b0c49bc4 ("iface_output: split out of eth_output")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

modules/infra/datapath/eth_output.c
@@ -51,8 +51,6 @@ eth_output_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 		eth->src_addr = src_mac;
 		eth->ether_type = priv->ether_type;
 
-		iface_mbuf_data(mbuf)->vlan_id = 0;
-
 		edge = OUTPUT;
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
@@ -61,6 +59,7 @@ next:
 			t->src_addr = src_mac;
 			t->ether_type = priv->ether_type;
 		}
+		iface_mbuf_data(mbuf)->vlan_id = 0;
 		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 