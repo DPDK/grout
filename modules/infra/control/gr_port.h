DPDK/grout

commit ec4cbdc7f18fa796fc78019718761ad015238285

port: always enable allmulti and simplify MAC filtering
IPv6 requires receiving multicast traffic for neighbor discovery and
other essential protocols. Additionally, we will soon need to receive
all multicast ethernet packets to allow Linux daemons running in network
namespaces to subscribe to multicast groups independently.

Rather than track multicast addresses in hardware filters (which are
subject to the same capacity limits as unicast filtering), unconditionally
enable allmulti mode on all ports.

Simplify the MAC filter structure to only handle unicast addresses.
Merge the separate ucast_filter and mcast_filter into a single filter
array. Combine each MAC address with its reference count in a port_mac
structure rather than maintaining parallel arrays.

Remove the MAC_FILTER_F_ALL flag since only promiscuous mode (for
unicast overflow) needs to be tracked. Replace GR_IFACE_S_ALLMULTI_FIXED
with GR_IFACE_S_ALLMULTI as it is no longer a temporary state that can
be disabled.

Remove allmulti from user-configurable interface flags since it is now
always enabled for all ports.

Update IPv6 neighbor discovery to no longer install multicast MAC
filters when joining solicited-node multicast groups, as allmulti mode
already ensures their reception.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/infra/control/gr_port.h
@@ -13,18 +13,14 @@
 #include <stdint.h>
 #include <sys/queue.h>
 
-typedef enum : uint8_t {
+typedef enum {
 	MAC_FILTER_F_UNSUPP = GR_BIT8(0),
 	MAC_FILTER_F_NOSPC = GR_BIT8(1),
-	MAC_FILTER_F_ALL = GR_BIT8(2),
 } mac_filter_flags_t;
 
-struct mac_filter {
-	mac_filter_flags_t flags;
-	uint8_t count;
-	uint8_t hw_limit;
-	uint16_t refcnt[RTE_ETH_NUM_RECEIVE_MAC_ADDR];
-	struct rte_ether_addr mac[RTE_ETH_NUM_RECEIVE_MAC_ADDR];
+struct port_mac {
+	uint16_t refcnt;
+	struct rte_ether_addr mac;
 };
 
 GR_IFACE_INFO(GR_IFACE_TYPE_PORT, iface_info_port, {
@@ -35,8 +31,12 @@ GR_IFACE_INFO(GR_IFACE_TYPE_PORT, iface_info_port, {
 	struct rte_mempool *pool;
 	char *devargs;
 	uint32_t pool_size;
-	struct mac_filter ucast_filter;
-	struct mac_filter mcast_filter;
+	struct {
+		mac_filter_flags_t flags;
+		unsigned hw_limit;
+		unsigned count;
+		struct port_mac macs[RTE_ETH_NUM_RECEIVE_MAC_ADDR];
+	} filter;
 });
 
 uint32_t port_get_rxq_buffer_us(uint16_t port_id, uint16_t rxq_id);

frr/if_grout.c
@@ -29,7 +29,7 @@ static uint64_t gr_if_flags_to_netlink(struct gr_iface *gr_if, enum zebra_link_t
 		frr_if_flags |= IFF_UP;
 	if (gr_if->base.flags & GR_IFACE_F_PROMISC)
 		frr_if_flags |= IFF_PROMISC;
-	if (gr_if->base.flags & GR_IFACE_F_ALLMULTI)
+	if (gr_if->base.state & GR_IFACE_S_ALLMULTI)
 		frr_if_flags |= IFF_ALLMULTI;
 	if (gr_if->base.state & GR_IFACE_S_RUNNING)
 		frr_if_flags |= IFF_RUNNING | IFF_LOWER_UP;

modules/infra/api/gr_infra.h
@@ -27,17 +27,16 @@ typedef enum : uint8_t {
 typedef enum : uint16_t {
 	GR_IFACE_F_UP = GR_BIT16(0),
 	GR_IFACE_F_PROMISC = GR_BIT16(1),
-	GR_IFACE_F_ALLMULTI = GR_BIT16(2),
-	GR_IFACE_F_PACKET_TRACE = GR_BIT16(3),
-	GR_IFACE_F_SNAT_STATIC = GR_BIT16(4),
-	GR_IFACE_F_SNAT_DYNAMIC = GR_BIT16(5),
+	GR_IFACE_F_PACKET_TRACE = GR_BIT16(2),
+	GR_IFACE_F_SNAT_STATIC = GR_BIT16(3),
+	GR_IFACE_F_SNAT_DYNAMIC = GR_BIT16(4),
 } gr_iface_flags_t;
 
 // Interface state flags
 typedef enum : uint16_t {
 	GR_IFACE_S_RUNNING = GR_BIT16(0),
 	GR_IFACE_S_PROMISC_FIXED = GR_BIT16(1),
-	GR_IFACE_S_ALLMULTI_FIXED = GR_BIT16(2),
+	GR_IFACE_S_ALLMULTI = GR_BIT16(2),
 } gr_iface_state_t;
 
 // Interface reconfig attributes

modules/infra/cli/gr_cli_iface.h
@@ -51,12 +51,11 @@ int complete_iface_names(
 #define INTERFACE_SET_CTX(root)                                                                    \
 	CLI_CONTEXT(root, INTERFACE_ARG, CTX_ARG("set", "Modify an existing interface."))
 
-#define IFACE_ATTRS_CMD "(up|down),(promisc PROMISC),(allmulti ALLMULTI),(mtu MTU),(vrf VRF)"
+#define IFACE_ATTRS_CMD "(up|down),(promisc PROMISC),(mtu MTU),(vrf VRF)"
 
 #define IFACE_ATTRS_ARGS                                                                           \
 	with_help("Set the interface UP.", ec_node_str("up", "up")),                               \
 		with_help("Enable/disable promiscuous mode.", ec_node_re("PROMISC", "on|off")),    \
-		with_help("Enable/disable all-multicast mode.", ec_node_re("ALLMULTI", "on|off")), \
 		with_help("Set the interface DOWN.", ec_node_str("down", "down")),                 \
 		with_help(                                                                         \
 			"Maximum transmission unit size.",                                         \

modules/infra/cli/iface.c
@@ -128,9 +128,7 @@ static ssize_t iface_flags_format(char *buf, size_t len, const struct gr_iface *
 		SAFE_BUF(snprintf, len, " promisc(fixed)");
 	else if (iface->flags & GR_IFACE_F_PROMISC)
 		SAFE_BUF(snprintf, len, " promisc");
-	if (iface->state & GR_IFACE_S_ALLMULTI_FIXED)
-		SAFE_BUF(snprintf, len, " allmulti(fixed)");
-	else if (iface->flags & GR_IFACE_F_ALLMULTI)
+	if (iface->state & GR_IFACE_S_ALLMULTI)
 		SAFE_BUF(snprintf, len, " allmulti");
 	if (iface->flags & GR_IFACE_F_PACKET_TRACE)
 		SAFE_BUF(snprintf, len, " tracing");
@@ -149,7 +147,7 @@ uint64_t parse_iface_args(
 	size_t info_size,
 	bool update
 ) {
-	const char *name, *promisc, *allmulti;
+	const char *name, *promisc;
 	uint64_t set_attrs = 0;
 
 	name = arg_str(p, "NAME");
@@ -184,15 +182,6 @@ uint64_t parse_iface_args(
 		set_attrs |= GR_IFACE_SET_FLAGS;
 	} else if (promisc != NULL && strcmp(promisc, "off") == 0) {
 		iface->flags &= ~GR_IFACE_F_PROMISC;
-		set_attrs |= GR_IFACE_SET_FLAGS;
-	}
-
-	allmulti = arg_str(p, "ALLMULTI");
-	if (allmulti != NULL && strcmp(allmulti, "on") == 0) {
-		iface->flags |= GR_IFACE_F_ALLMULTI;
-		set_attrs |= GR_IFACE_SET_FLAGS;
-	} else if (allmulti != NULL && strcmp(allmulti, "off") == 0) {
-		iface->flags &= ~GR_IFACE_F_ALLMULTI;
 		set_attrs |= GR_IFACE_SET_FLAGS;
 	}
 

modules/infra/control/bond.c
@@ -140,10 +140,6 @@ static int bond_promisc_set(struct iface *iface, bool enabled) {
 	return bond_all_members_set_flag(iface, GR_IFACE_F_PROMISC, enabled, iface_set_promisc);
 }
 
-static int bond_allmulti_set(struct iface *iface, bool enabled) {
-	return bond_all_members_set_flag(iface, GR_IFACE_F_ALLMULTI, enabled, iface_set_allmulti);
-}
-
 static int bond_vlan_add(struct iface *iface, uint16_t vlan_id) {
 	struct iface_info_bond *bond = iface_info_bond(iface);
 	const struct iface *member;
@@ -369,7 +365,6 @@ static struct iface_type iface_type_bond = {
 	.del_eth_addr = bond_mac_del,
 	.set_mtu = bond_mtu_set,
 	.set_promisc = bond_promisc_set,
-	.set_allmulti = bond_allmulti_set,
 	.add_vlan = bond_vlan_add,
 	.del_vlan = bond_vlan_del,
 	.to_api = bond_to_api,

modules/infra/control/gr_iface.h
@@ -49,7 +49,6 @@ struct iface_type {
 	int (*set_up_down)(struct iface *, bool up);
 	int (*set_mtu)(struct iface *, uint16_t mtu);
 	int (*set_promisc)(struct iface *, bool enabled);
-	int (*set_allmulti)(struct iface *, bool enabled);
 	int (*add_vlan)(struct iface *, uint16_t vlan_id);
 	int (*del_vlan)(struct iface *, uint16_t vlan_id);
 	void (*to_api)(void *api_info, const struct iface *);
@@ -77,7 +76,6 @@ int iface_del_eth_addr(uint16_t ifid, const struct rte_ether_addr *);
 int iface_set_mtu(uint16_t ifid, uint16_t mtu);
 int iface_set_up_down(uint16_t ifid, bool up);
 int iface_set_promisc(uint16_t ifid, bool enabled);
-int iface_set_allmulti(uint16_t ifid, bool enabled);
 int iface_add_vlan(uint16_t ifid, uint16_t vlan_id);
 int iface_del_vlan(uint16_t ifid, uint16_t vlan_id);
 uint16_t ifaces_count(gr_iface_type_t type_id);

modules/infra/control/iface.c
@@ -121,9 +121,6 @@ struct iface *iface_create(const struct gr_iface *conf, const void *api_info) {
 	if (type->set_promisc != NULL
 	    && type->set_promisc(iface, iface->flags & GR_IFACE_F_PROMISC) < 0)
 		goto fail;
-	if (type->set_allmulti != NULL
-	    && type->set_allmulti(iface, iface->flags & GR_IFACE_F_ALLMULTI) < 0)
-		goto fail;
 	if (type->set_up_down != NULL && type->set_up_down(iface, iface->flags & GR_IFACE_F_UP) < 0)
 		goto fail;
 
@@ -210,8 +207,6 @@ int iface_reconfig(
 
 	if (set_attrs & GR_IFACE_SET_FLAGS) {
 		if ((ret = iface_set_promisc(iface->id, conf->flags & GR_IFACE_F_PROMISC)) < 0)
-			return ret;
-		if ((ret = iface_set_allmulti(iface->id, conf->flags & GR_IFACE_F_ALLMULTI)) < 0)
 			return ret;
 		if ((ret = iface_set_up_down(iface->id, conf->flags & GR_IFACE_F_UP)) < 0)
 			return ret;
@@ -387,24 +382,6 @@ int iface_set_promisc(uint16_t ifid, bool enabled) {
 	assert(type != NULL);
 	if (type->set_promisc != NULL)
 		return type->set_promisc(iface, enabled);
-
-	if (enabled)
-		return errno_set(EOPNOTSUPP);
-
-	return 0;
-}
-
-int iface_set_allmulti(uint16_t ifid, bool enabled) {
-	struct iface *iface = iface_from_id(ifid);
-	const struct iface_type *type;
-
-	if (iface == NULL)
-		return -errno;
-
-	type = iface_type_get(iface->type);
-	assert(type != NULL);
-	if (type->set_allmulti != NULL)
-		return type->set_allmulti(iface, enabled);
 
 	if (enabled)
 		return errno_set(EOPNOTSUPP);

commit 75d6c7b0cb28297093d1699733bd99f78e5ac76f

modules: remove some zombie symbols
port_get_rxq_buffer_us and port_allmulti_set declarations were left behind
after dropping their implementation.

Fixes: ec4cbdc7f18f ("port: always enable allmulti and simplify MAC filtering")
Fixes: f371a5e987df ("port: use interrupt thread to monitor link status and speed")
Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/infra/control/gr_port.h
@@ -44,5 +44,4 @@ GR_IFACE_INFO(GR_IFACE_TYPE_PORT, iface_info_port, {
 	} filter;
 });
 
-uint32_t port_get_rxq_buffer_us(uint16_t port_id, uint16_t rxq_id);
 const struct iface *port_get_iface(uint16_t port_id);

modules/infra/control/port_priv.h
@@ -10,4 +10,3 @@
 int port_mac_add(struct iface *, const struct rte_ether_addr *);
 int port_mac_del(struct iface *, const struct rte_ether_addr *);
 int port_promisc_set(struct iface *, bool enabled);
-int port_allmulti_set(struct iface *, bool enabled);