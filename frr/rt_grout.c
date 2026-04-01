c5313db2d254e383e900103679c130d062e151d5
Robin Jarry <rjarry@redhat.com>
2026-04-01 19:43:38 +0200

    frr: sync resolved nexthops as neighbor entries to zebra
    When a grout nexthop event arrives for an L3 nexthop with a
    resolved MAC, push a DPLANE_OP_NEIGH_IP_INSTALL to zebra. On
    deletion, push DPLANE_OP_NEIGH_IP_DELETE. Use the IP-specific
    dplane ops (not DPLANE_OP_NEIGH_INSTALL which is the MAC/FDB path)
    so zebra routes the notification through
    zebra_vxlan_handle_kernel_neigh_update and populates the EVPN
    arp-cache for Type-2 MAC+IP advertisement via BGP.
    
    The notification is called before the nh_id check in
    grout_nexthop_change so that nexthops with nh_id=0 (auto-allocated
    by the ARP/NDP control plane) still produce neighbor entries in
    zebra even though they are not registered as zebra nexthop groups.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- frr/rt_grout.c
+++ frr/rt_grout.c
@@ -943,6 +943,60 @@ void grout_nexthop_group_add(struct gr_nexthop *gr_nh, bool startup) {
 	);
 }
 
+static void grout_neigh_notify(bool new, struct gr_nexthop *gr_nh) {
+	const struct gr_nexthop_info_l3 *l3;
+	static const struct ethaddr zero_mac = {};
+	struct zebra_dplane_ctx *ctx;
+	struct ethaddr mac;
+	struct ipaddr ip;
+
+	if (gr_nh->type != GR_NH_T_L3)
+		return;
+
+	l3 = (const struct gr_nexthop_info_l3 *)gr_nh->info;
+
+	if (l3->af != GR_AF_IP4 && l3->af != GR_AF_IP6)
+		return;
+	if (l3->flags & (GR_NH_F_LOCAL | GR_NH_F_LINK | GR_NH_F_MCAST | GR_NH_F_REMOTE))
+		return;
+	if (new && memcmp(&l3->mac, &zero_mac, sizeof(zero_mac)) == 0)
+		return;
+
+	memset(&ip, 0, sizeof(ip));
+	if (l3->af == GR_AF_IP4) {
+		ip.ipa_type = IPADDR_V4;
+		memcpy(&ip.ipaddr_v4, &l3->ipv4, sizeof(ip.ipaddr_v4));
+	} else {
+		ip.ipa_type = IPADDR_V6;
+		memcpy(&ip.ipaddr_v6, &l3->ipv6, sizeof(ip.ipaddr_v6));
+	}
+	memcpy(&mac, &l3->mac, sizeof(mac));
+
+	gr_log_debug(
+		"%s neigh iface=%u %pIA %pEA", new ? "add" : "del", gr_nh->iface_id, &ip, &mac
+	);
+
+	ctx = dplane_ctx_alloc();
+	dplane_ctx_set_op(ctx, new ? DPLANE_OP_NEIGH_IP_INSTALL : DPLANE_OP_NEIGH_IP_DELETE);
+	dplane_ctx_set_ns_id(ctx, GROUT_NS);
+	dplane_ctx_set_ifindex(ctx, ifindex_grout_to_frr(gr_nh->iface_id));
+	dplane_ctx_neigh_set_ipaddr(ctx, &ip);
+	dplane_ctx_neigh_set_mac(ctx, &mac);
+	dplane_ctx_neigh_set_ndm_state(ctx, NUD_REACHABLE);
+	dplane_ctx_neigh_set_ndm_family(ctx, l3->af == GR_AF_IP4 ? AF_INET : AF_INET6);
+	dplane_ctx_neigh_set_l2_len(ctx, ETH_ALEN);
+#if CURRENT_FRR_VERSION >= MAKE_FRRVERSION(10, 6, 0)
+	dplane_ctx_neigh_set_is_own(ctx, false);
+#else
+	dplane_ctx_neigh_set_is_ext(ctx, false);
+#endif
+	dplane_ctx_neigh_set_is_router(ctx, false);
+	dplane_ctx_neigh_set_dp_static(ctx, false);
+	dplane_ctx_neigh_set_local_inactive(ctx, false);
+
+	dplane_provider_enqueue_to_zebra(ctx);
+}
+
 void grout_nexthop_change(bool new, struct gr_nexthop *gr_nh, bool startup) {
 	struct nexthop *nh = NULL;
 	afi_t afi = AFI_UNSPEC;
@@ -954,6 +1008,8 @@ void grout_nexthop_change(bool new, struct gr_nexthop *gr_nh, bool startup) {
 		gr_nh->nh_id,
 		gr_nh_origin_name(gr_nh->origin)
 	);
+
+	grout_neigh_notify(new, gr_nh);
 
 	if (gr_nh->origin == GR_NH_ORIGIN_LEARN) {
 		gr_log_debug("dynamic neighbor learn, skip");
