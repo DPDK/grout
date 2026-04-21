DPDK/grout

commit 90050832ff300f88d76c11028ab8ecd81a718313

api: build message registry in client library
Override GR_REQ/GR_REQ_STREAM/GR_EVENT in gr_api_client_impl.h to build
a linked list of all known API messages at program startup via
constructors. This gives clients a runtime registry mapping request
codes to names and expected payload sizes.

Since GR_REQ and friends are guarded by #ifndef, the overrides must be
defined before any API header is included. And since the macros expand
at inclusion time, gr_api_client_impl.h must explicitly include every
module API header so that all messages are registered. The previously
added grout.h umbrella header serves that purpose.

The gr_api_message_name() function replaces the hand-maintained switch
statement in the FRR plugin for converting codes to strings.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

cli/main.c
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2023 Robin Jarry
 
+// clang-format: off
+#include <gr_api_client_impl.h>
+// clang-format: on
+
 #include "cli.h"
 #include "complete.h"
 #include "display.h"
@@ -9,8 +13,6 @@
 #include "interact.h"
 #include "pager.h"
 #include "tty.h"
-
-#include <gr_api_client_impl.h>
 
 #include <ecoli.h>
 

frr/zebra_dplane_grout.c
@@ -2,12 +2,16 @@
 // Copyright (c) 2024 Christophe Fontaine, Red Hat
 // Copyright (c) 2025 Maxime Leroy, Free Mobile
 
+#include <lib/queue.h>
+
+// clang-format: off
+#include <gr_api_client_impl.h>
+// clang-format: on
+
 #include "if_grout.h"
 #include "if_map.h"
 #include "log_grout.h"
 #include "rt_grout.h"
-
-#include <gr_api_client_impl.h>
 
 #include <lib/bitfield.h>
 #include <lib/frr_pthread.h>
@@ -403,64 +407,6 @@ static void zebra_grout_connect(struct event *) {
 	gr_log_notice("connected, monitoring route/nexthop events");
 }
 
-static const char *gr_req_type_to_str(uint32_t e) {
-	static __thread char buf[64];
-
-	switch (e) {
-	case GR_IP4_ADDR_ADD:
-		return TOSTRING(GR_IP4_ADDR_ADD);
-	case GR_IP4_ADDR_DEL:
-		return TOSTRING(GR_IP4_ADDR_DEL);
-	case GR_IP6_ADDR_ADD:
-		return TOSTRING(GR_IP6_ADDR_ADD);
-	case GR_IP6_ADDR_DEL:
-		return TOSTRING(GR_IP6_ADDR_DEL);
-	case GR_IP4_ROUTE_ADD:
-		return TOSTRING(GR_IP4_ROUTE_ADD);
-	case GR_IP4_ROUTE_DEL:
-		return TOSTRING(GR_IP4_ROUTE_DEL);
-	case GR_IP6_ROUTE_ADD:
-		return TOSTRING(GR_IP6_ROUTE_ADD);
-	case GR_IP6_ROUTE_DEL:
-		return TOSTRING(GR_IP6_ROUTE_DEL);
-	case GR_NH_ADD:
-		return TOSTRING(GR_NH_ADD);
-	case GR_NH_DEL:
-		return TOSTRING(GR_NH_DEL);
-	case GR_IFACE_GET:
-		return TOSTRING(GR_IFACE_GET);
-	case GR_IFACE_LIST:
-		return TOSTRING(GR_IFACE_LIST);
-	case GR_IP4_ADDR_LIST:
-		return TOSTRING(GR_IP4_ADDR_LIST);
-	case GR_IP6_ADDR_LIST:
-		return TOSTRING(GR_IP6_ADDR_LIST);
-	case GR_NH_LIST:
-		return TOSTRING(GR_NH_LIST);
-	case GR_IP4_ROUTE_LIST:
-		return TOSTRING(GR_IP4_ROUTE_LIST);
-	case GR_IP6_ROUTE_LIST:
-		return TOSTRING(GR_IP6_ROUTE_LIST);
-	case GR_SRV6_TUNSRC_SET:
-		return TOSTRING(GR_SRV6_TUNSRC_SET);
-	case GR_FDB_ADD:
-		return TOSTRING(GR_FDB_ADD);
-	case GR_FDB_DEL:
-		return TOSTRING(GR_FDB_DEL);
-	case GR_FDB_LIST:
-		return TOSTRING(GR_FDB_LIST);
-	case GR_FLOOD_ADD:
-		return TOSTRING(GR_FLOOD_ADD);
-	case GR_FLOOD_DEL:
-		return TOSTRING(GR_FLOOD_DEL);
-	case GR_FLOOD_LIST:
-		return TOSTRING(GR_FLOOD_LIST);
-	default:
-		snprintf(buf, sizeof(buf), "0x%x", e);
-		return buf;
-	}
-}
-
 int grout_client_send_recv(uint32_t req_type, size_t tx_len, const void *tx_data, void **rx_data) {
 	int ret;
 
@@ -469,11 +415,11 @@ int grout_client_send_recv(uint32_t req_type, size_t tx_len, const void *tx_data
 
 	ret = gr_api_client_send_recv(grout_ctx.client, req_type, tx_len, tx_data, rx_data);
 	if (ret == 0) {
-		gr_log_debug("%s: success", gr_req_type_to_str(req_type));
+		gr_log_debug("%s: success", gr_api_message_name(req_type));
 		return 0;
 	}
 
-	gr_log_err("%s: %s", gr_req_type_to_str(req_type), strerror(errno));
+	gr_log_err("%s: %s", gr_api_message_name(req_type), strerror(errno));
 
 	if (errno == ECONNRESET || errno == EPIPE || errno == ENOTCONN) {
 		gr_api_client_disconnect(grout_ctx.client);
@@ -484,62 +430,6 @@ int grout_client_send_recv(uint32_t req_type, size_t tx_len, const void *tx_data
 	}
 
 	return ret;
-}
-
-static const char *gr_evt_to_str(uint32_t e) {
-	static __thread char buf[64];
-
-	switch (e) {
-	case GR_EVENT_IFACE_ADD:
-		return TOSTRING(GR_EVENT_IFACE_ADD);
-	case GR_EVENT_IFACE_POST_ADD:
-		return TOSTRING(GR_EVENT_IFACE_POST_ADD);
-	case GR_EVENT_IFACE_REMOVE:
-		return TOSTRING(GR_EVENT_IFACE_REMOVE);
-	case GR_EVENT_IFACE_STATUS_UP:
-		return TOSTRING(GR_EVENT_IFACE_STATUS_UP);
-	case GR_EVENT_IFACE_STATUS_DOWN:
-		return TOSTRING(GR_EVENT_IFACE_STATUS_DOWN);
-	case GR_EVENT_IFACE_POST_RECONFIG:
-		return TOSTRING(GR_EVENT_IFACE_POST_RECONFIG);
-	case GR_EVENT_IFACE_MAC_CHANGE:
-		return TOSTRING(GR_EVENT_IFACE_MAC_CHANGE);
-	case GR_EVENT_IP_ADDR_ADD:
-		return TOSTRING(GR_EVENT_IP_ADDR_ADD);
-	case GR_EVENT_IP_ADDR_DEL:
-		return TOSTRING(GR_EVENT_IP_ADDR_DEL);
-	case GR_EVENT_IP6_ADDR_ADD:
-		return TOSTRING(GR_EVENT_IP6_ADDR_ADD);
-	case GR_EVENT_IP6_ADDR_DEL:
-		return TOSTRING(GR_EVENT_IP6_ADDR_DEL);
-	case GR_EVENT_IP_ROUTE_ADD:
-		return TOSTRING(GR_EVENT_IP_ROUTE_ADD);
-	case GR_EVENT_IP_ROUTE_DEL:
-		return TOSTRING(GR_EVENT_IP_ROUTE_DEL);
-	case GR_EVENT_IP6_ROUTE_ADD:
-		return TOSTRING(GR_EVENT_IP6_ROUTE_ADD);
-	case GR_EVENT_IP6_ROUTE_DEL:
-		return TOSTRING(GR_EVENT_IP6_ROUTE_DEL);
-	case GR_EVENT_NEXTHOP_NEW:
-		return TOSTRING(GR_EVENT_NEXTHOP_NEW);
-	case GR_EVENT_NEXTHOP_UPDATE:
-		return TOSTRING(GR_EVENT_NEXTHOP_UPDATE);
-	case GR_EVENT_NEXTHOP_DELETE:
-		return TOSTRING(GR_EVENT_NEXTHOP_DELETE);
-	case GR_EVENT_FDB_ADD:
-		return TOSTRING(GR_EVENT_FDB_ADD);
-	case GR_EVENT_FDB_UPDATE:
-		return TOSTRING(GR_EVENT_FDB_UPDATE);
-	case GR_EVENT_FDB_DEL:
-		return TOSTRING(GR_EVENT_FDB_DEL);
-	case GR_EVENT_FLOOD_ADD:
-		return TOSTRING(GR_EVENT_FLOOD_ADD);
-	case GR_EVENT_FLOOD_DEL:
-		return TOSTRING(GR_EVENT_FLOOD_DEL);
-	default:
-		snprintf(buf, sizeof(buf), "event 0x%x", e);
-		return buf;
-	}
 }
 
 static void dplane_read_notifications(struct event *event) {
@@ -558,7 +448,7 @@ static void dplane_read_notifications(struct event *event) {
 		return;
 	}
 
-	gr_log_debug("%s", gr_evt_to_str(gr_e->ev_type));
+	gr_log_debug("%s", gr_api_message_name(gr_e->ev_type));
 
 	switch (gr_e->ev_type) {
 	case GR_EVENT_IFACE_ADD:
@@ -617,7 +507,7 @@ static void zebra_read_notifications(struct event *event) {
 		return;
 	}
 
-	gr_log_debug("%s", gr_evt_to_str(gr_e->ev_type));
+	gr_log_debug("%s", gr_api_message_name(gr_e->ev_type));
 
 	switch (gr_e->ev_type) {
 	case GR_EVENT_IP_ROUTE_ADD:

smoke/fib_inject.c
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2026 Robin Jarry
 
+// clang-format: off
 #include <gr_api_client_impl.h>
+// clang-format: on
 
 #include <getopt.h>
 #include <stdio.h>

api/gr_api.h
@@ -129,6 +129,8 @@ int __gr_api_client_stream_drain(struct gr_api_client *, uint32_t for_id);
 		     });                                                                           \
 		     free(__ptr), __ptr = NULL)
 
+const char *gr_api_message_name(uint32_t type);
+
 #ifndef GR_REQ
 #define GR_REQ(code, req, resp)                                                                    \
 	enum {                                                                                     \

api/gr_api_client_impl.h
@@ -5,6 +5,76 @@
 
 #pragma once
 
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+
+#ifdef GR_REQ
+#error "gr_api_client_impl.h must be included *first*, before any other gr_*.h headers"
+#endif
+
+struct api_message {
+	uint32_t type;
+	const char *name;
+	size_t payload_min_size;
+	bool stream;
+	STAILQ_ENTRY(api_message) next;
+};
+
+static STAILQ_HEAD(, api_message) messages = STAILQ_HEAD_INITIALIZER(messages);
+
+static const struct api_message *get_message(uint32_t type) {
+	struct api_message *m;
+
+	STAILQ_FOREACH (m, &messages, next) {
+		if (m->type == type)
+			return m;
+	}
+
+	errno = EIDRM;
+	return NULL;
+}
+
+static void register_message(struct api_message *m) {
+	const struct api_message *existing = get_message(m->type);
+
+	if (existing != NULL) {
+		fprintf(stderr,
+			"fatal: message %s has duplicate type id %#08x with message %s",
+			m->name,
+			m->type,
+			existing->name);
+		abort();
+	}
+	STAILQ_INSERT_TAIL(&messages, m, next);
+}
+
+#define GR_REQ(r, req, resp)                                                                       \
+	static struct api_message r##_msg = {                                                      \
+		.type = r,                                                                         \
+		.name = #r,                                                                        \
+		.payload_min_size = sizeof(resp),                                                  \
+	};                                                                                         \
+	static void __attribute__((constructor, used)) r##_init(void) {                            \
+		register_message(&r##_msg);                                                        \
+	}
+
+#define GR_REQ_STREAM(r, req, resp)                                                                \
+	static struct api_message r##_msg = {                                                      \
+		.type = r,                                                                         \
+		.name = #r,                                                                        \
+		.payload_min_size = sizeof(resp),                                                  \
+		.stream = true,                                                                    \
+	};                                                                                         \
+	static void __attribute__((constructor, used)) r##_init(void) {                            \
+		register_message(&r##_msg);                                                        \
+	}
+
+#define GR_EVENT(e, obj) GR_REQ(e, , obj)
+
 #include <grout.h>
 
 #include <assert.h>
@@ -14,10 +84,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/queue.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+
+const char *gr_api_message_name(uint32_t type) {
+	static __thread char buf[64];
+	const struct api_message *m = get_message(type);
+	if (m == NULL) {
+		snprintf(buf, sizeof(buf), "0x%08x?", type);
+		return buf;
+	}
+	return m->name;
+}
 
 struct response {
 	struct gr_api_response header;

commit ea2c793c35b06596eb027f970372f1342c988fd8

clang-format: fix on/off comments
The correct syntax is `// clang-format off` and `// clang-format on`
without a separating colon.

Fixes: 90050832ff30 ("api: build message registry in client library")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Maxime Leroy <maxime@leroys.fr>

cli/main.c
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2023 Robin Jarry
 
-// clang-format: off
+// clang-format off
 #include <gr_api_client_impl.h>
-// clang-format: on
+// clang-format on
 
 #include "cli.h"
 #include "complete.h"

frr/zebra_dplane_grout.c
@@ -4,9 +4,9 @@
 
 #include <lib/queue.h>
 
-// clang-format: off
+// clang-format off
 #include <gr_api_client_impl.h>
-// clang-format: on
+// clang-format on
 
 #include "if_grout.h"
 #include "if_map.h"

smoke/fib_inject.c
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2026 Robin Jarry
 
-// clang-format: off
+// clang-format off
 #include <gr_api_client_impl.h>
-// clang-format: on
+// clang-format on
 
 #include <getopt.h>
 #include <stdio.h>