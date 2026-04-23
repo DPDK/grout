DPDK/grout

commit 8de36764e80e32b063f38117279cf83be9d5641b

api: generate a single grout.h umbrella header
Add a build-time generated grout.h that includes all public gr_*.h API
headers. Use it in gr_api_client_impl.h to replace the individual gr_*.h
includes. This prepares for the next commit which overrides
GR_REQ/GR_REQ_STREAM/GR_EVENT macros in gr_api_client_impl.h and needs
all API headers included before the macros are expanded.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

devtools/gen_api_header.sh
@@ -1,0 +1,13 @@
+#!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2026 Robin Jarry
+
+echo "// SPDX-License-Identifier: BSD-3-Clause"
+echo "// Copyright (c) $(date +Y) Red Hat"
+echo
+echo "#pragma once"
+echo
+
+for header in "$@"; do
+	echo "#include <$(basename $header)>"
+done | grep -v gr_api_client_impl.h | sort -u

.clang-format
@@ -92,9 +92,9 @@ IncludeBlocks: Regroup
 IncludeCategories:
   - Regex:           '^".*'
     Priority:        -1
-  - Regex:           '^<(gr_|gr\.)'
+  - Regex:           '^<gr_'
     Priority:        100
-  - Regex:           '^<(rte_|event|ecoli|numa.h)'
+  - Regex:           '^<(rte_|event|ecoli|numa.h|grout.h)'
     Priority:        500
   - Regex:           '^<cmocka'
     Priority:        100000

api/gr_api_client_impl.h
@@ -5,10 +5,7 @@
 
 #pragma once
 
-#include <gr_api.h>
-#include <gr_errno.h>
-#include <gr_macro.h>
-#include <gr_version.h>
+#include <grout.h>
 
 #include <assert.h>
 #include <getopt.h>

api/meson.build
@@ -18,6 +18,17 @@ api_headers += configure_file(
   }),
 )
 
+grout_header = custom_target(
+  'grout.h',
+  output: 'grout.h',
+  command: files('../devtools/gen_api_header.sh') + api_headers,
+  depend_files: api_headers,
+  install: true,
+  capture: true,
+  install_dir: get_option('includedir') / 'grout',
+  install_tag: 'man',
+)
+
 api_src = files(
   'printf.c',
   'string.c',

cli/main.c
@@ -10,10 +10,7 @@
 #include "pager.h"
 #include "tty.h"
 
-#include <gr_api.h>
 #include <gr_api_client_impl.h>
-#include <gr_clock.h>
-#include <gr_version.h>
 
 #include <ecoli.h>
 

debian/grout-headers.install
@@ -1,2 +1,2 @@
 /usr/share/pkgconfig/grout.pc
-/usr/include/grout/gr_*.h
+/usr/include/grout/*.h

frr/meson.build
@@ -25,7 +25,7 @@ frr_plugin = shared_module(
     'if_map.c',
     'rt_grout.c',
     'zebra_dplane_grout.c',
-  ),
+  ) + grout_header,
   name_prefix: '',
   dependencies: [frr_dep],
   include_directories: api_inc + include_directories('.'),

frr/zebra_dplane_grout.c
@@ -8,9 +8,6 @@
 #include "rt_grout.h"
 
 #include <gr_api_client_impl.h>
-#include <gr_errno.h>
-#include <gr_l2.h>
-#include <gr_srv6.h>
 
 #include <lib/bitfield.h>
 #include <lib/frr_pthread.h>

commit b0c4290005f3e4647d9a96f3350ee8c8b793a333

gen_api_header: fix copyright year
The date format needs a % character.

Fixes: 8de36764e80e ("api: generate a single grout.h umbrella header")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

devtools/gen_api_header.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2026 Robin Jarry
 
 echo "// SPDX-License-Identifier: BSD-3-Clause"
-echo "// Copyright (c) $(date +Y) Red Hat"
+echo "// Copyright (c) $(date +%Y) Red Hat"
 echo
 echo "#pragma once"
 echo