DPDK/grout

commit 393b14bf69784b1b7d29e47fed1e348cb3a9bd82

dhcp: forward to control plane interface when disabled
When DHCP is not enabled on an interface, forward incoming DHCP packets
to the control plane interface instead of processing and dropping them
with an error log message. This allows userspace applications listening
on UDP port 68 to receive the packets.

Since the dhcp_input node receives packets after IP header processing,
reconstruct the IP and Ethernet headers before sending to the control
plane interface via iface_cp_tx.

Move the iface_cp_tx declaration from the private ctlplane.h header to
the public gr_rxtx.h so it can be used by the DHCP datapath node. Delete
ctlplane.h since it is no longer needed.

Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/dhcp/datapath/dhcp_input.c
@@ -10,6 +10,7 @@
 #include <gr_log.h>
 #include <gr_macro.h>
 #include <gr_mbuf.h>
+#include <gr_rxtx.h>
 #include <gr_trace.h>
 
 #include <rte_byteorder.h>
@@ -25,17 +26,19 @@ struct dhcp_input_trace_data {
 
 enum {
 	CONTROL = 0,
+	NO_HEADROOM,
 	EDGE_COUNT,
 };
 
 static uint16_t
 dhcp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, uint16_t nb_objs) {
+	const struct iface *iface;
 	struct rte_mbuf *mbuf;
+	rte_edge_t edge;
 
 	for (uint16_t i = 0; i < nb_objs; i++) {
 		mbuf = objs[i];
-
-		control_output_set_cb(mbuf, dhcp_input_cb, 0);
+		iface = mbuf_data(mbuf)->iface;
 
 		if (gr_mbuf_is_traced(mbuf)) {
 			struct dhcp_input_trace_data *t;
@@ -79,7 +82,35 @@ dhcp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 			}
 		}
 
-		rte_node_enqueue_x1(graph, node, CONTROL, mbuf);
+		if (dhcp_enabled(iface->id)) {
+			control_output_set_cb(mbuf, dhcp_input_cb, 0);
+		} else {
+			struct ip_local_mbuf_data *d = ip_local_mbuf_data(mbuf);
+
+			// reconstruct the IP and Ethernet headers before sending to control plane
+			// interface
+			struct rte_ipv4_hdr *ip = gr_mbuf_prepend(mbuf, ip);
+			if (ip == NULL) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			ip_set_fields(ip, d);
+
+			struct rte_ether_hdr *eth = gr_mbuf_prepend(mbuf, eth);
+			if (eth == NULL) {
+				edge = NO_HEADROOM;
+				goto next;
+			}
+			memset(&eth->src_addr, 0, sizeof(eth->src_addr));
+			iface_get_eth_addr(iface, &eth->dst_addr);
+			eth->ether_type = RTE_BE16(RTE_ETHER_TYPE_IPV4);
+
+			control_output_set_cb(mbuf, iface_cp_tx, 0);
+		}
+
+		edge = CONTROL;
+next:
+		rte_node_enqueue_x1(graph, node, edge, mbuf);
 	}
 
 	return nb_objs;
@@ -132,6 +163,7 @@ static struct rte_node_register node = {
 	.nb_edges = EDGE_COUNT,
 	.next_nodes = {
 		[CONTROL] = "control_output",
+		[NO_HEADROOM] = "error_no_headroom",
 	},
 };
 

modules/dhcp/control/client.c
@@ -29,6 +29,12 @@ static struct dhcp_client *dhcp_clients[MAX_IFACES];
 static control_input_t dhcp_output;
 static struct rte_mempool *dhcp_mp;
 
+bool dhcp_enabled(uint16_t iface_id) {
+	if (iface_id < MAX_IFACES)
+		return dhcp_clients[iface_id] != NULL;
+	return false;
+}
+
 static int dhcp_configure_interface(struct dhcp_client *client) {
 	const struct iface *iface;
 	int ret;

modules/dhcp/control/client.h
@@ -107,6 +107,8 @@ struct dhcp_packet {
 #define BOOTREQUEST 1
 #define BOOTREPLY 2
 
+bool dhcp_enabled(uint16_t iface_id);
+
 void dhcp_input_cb(void *obj, uintptr_t priv, const struct control_queue_drain *);
 
 struct rte_mempool *dhcp_get_mempool(void);

modules/infra/control/ctlplane.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2025 Christophe Fontaine
 
-#include "ctlplane.h"
-
 #include <gr_config.h>
 #include <gr_control_input.h>
 #include <gr_control_queue.h>
@@ -15,6 +13,7 @@
 #include <gr_module.h>
 #include <gr_netlink.h>
 #include <gr_nh_control.h>
+#include <gr_rxtx.h>
 #include <gr_vlan.h>
 
 #include <event2/event.h>

modules/infra/control/ctlplane.h
@@ -1,9 +1,0 @@
-// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2025 Christophe Fontaine
-
-#pragma once
-
-#include <gr_control_queue.h>
-#include <gr_infra.h>
-
-void iface_cp_tx(void *obj, uintptr_t priv, const struct control_queue_drain *);

modules/infra/datapath/gr_rxtx.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <gr_control_queue.h>
 #include <gr_graph.h>
 #include <gr_infra.h>
 
@@ -43,3 +44,5 @@ int rxtx_trace_format(char *buf, size_t len, const void *data, size_t /*data_len
 void iface_input_mode_register(gr_iface_mode_t, const char *next_node);
 
 void iface_output_type_register(gr_iface_type_t, const char *next_node);
+
+void iface_cp_tx(void *obj, uintptr_t priv, const struct control_queue_drain *);

modules/infra/datapath/l2_redirect.c
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2025 Christophe Fontaine
 
-#include "ctlplane.h"
-
 #include <gr_control_output.h>
 #include <gr_graph.h>
 #include <gr_iface.h>
 #include <gr_infra.h>
 #include <gr_loopback.h>
+#include <gr_rxtx.h>
 #include <gr_snap.h>
 #include <gr_trace.h>
 

commit 9fb925309ee0c88ab6bf239f630c97b88c31af56

ctlplane: fix confusing vlan header in traces
When receiving an OSPF packet, the transmitted trace on the control
plane interface sometimes shows a VLAN header which wasn't present on
reception:

TRACE: [rx p0] 02:e3:ad:74:50:9b > 01:00:5e:00:00:05 / IP 172.16.0.2 > 224.0.0.5 ttl=1 proto=OSPF(89), (pkt_len=78)
TRACE: [cp tx p0] 52:54:00:00:00:01 > 01:00:5e:00:00:05 / VLAN id=4268 / IP 172.16.0.2 > 224.0.0.5 ttl=1 proto=OSPF(89), (pkt_len=78)

There is no VLAN in the packet and the packet lengths are equal. Nothing
was inserted. The trace function just displays a garbage
iface_mbuf_data->vlan_id value which wasn't reset.

Reset vlan_id before sending the packets to iface_cp_tx() like it is
done in l4_loopback_output.

Fixes: 393b14bf6978 ("dhcp: forward to control plane interface when disabled")
Fixes: 5b1141226a55 ("infra: redirect ospf packets to the cp interface")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

modules/dhcp/datapath/dhcp_input.c
@@ -102,6 +102,10 @@ dhcp_input_process(struct rte_graph *graph, struct rte_node *node, void **objs, 
 			iface_get_eth_addr(iface, &eth->dst_addr);
 			eth->ether_type = RTE_BE16(RTE_ETHER_TYPE_IPV4);
 
+			// No VLAN header is written to the TAP. Clear the residual
+			// mbuf metadata so traces reflect the actual frame bytes.
+			iface_mbuf_data(mbuf)->vlan_id = 0;
+
 			control_output_set_cb(mbuf, iface_cp_tx, 0);
 		}
 

modules/l4/ospf_redirect.c
@@ -6,6 +6,7 @@
 #include "ip4_datapath.h"
 #include "ip6_datapath.h"
 #include "port.h"
+#include "rxtx.h"
 
 #include <gr_infra.h>
 
@@ -110,6 +111,10 @@ static uint16_t ospf_redirect_process(
 		eth->ether_type = ether_type;
 		eth->src_addr = src;
 		eth->dst_addr = dst;
+
+		// No VLAN header is written to the TAP. Clear the residual
+		// mbuf metadata so traces reflect the actual frame bytes.
+		iface_mbuf_data(mbuf)->vlan_id = 0;
 next:
 		if (gr_mbuf_is_traced(mbuf)) {
 			gr_mbuf_trace_add(mbuf, node, 0);