DPDK/grout

commit b468374cf98d66624762c6c9472a9ae35168eb7a

frr: add L3VPN support
Handle EVPN type-5 (IP prefix) routes with symmetric IRB over VXLAN.

Present VRF-mode VXLAN interfaces to FRR as bridge slaves of the VRF
interface. FRR requires an SVI derived from a bridge master to bring the
L3VNI up and compute the Router MAC for type-5 routes. The VRF MAC (set
in a previous commit) serves as the Router MAC.

Handle DPLANE_OP_NEIGH_INSTALL/DELETE to cache remote router MACs
delivered by FRR before the corresponding nexthop install arrives. When
grout_add_nexthop processes an L3 nexthop in a VRF with an L3VNI, it
redirects the interface from VRF to VXLAN and applies the cached RMAC.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Maxime Leroy <maxime@leroys.fr>

frr/if_grout.c
@@ -3,6 +3,7 @@
 
 #include "if_grout.h"
 #include "if_map.h"
+#include "l3vni_map.h"
 #include "log_grout.h"
 #include "zebra_dplane_grout.h"
 
@@ -154,6 +155,16 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 				dplane_ctx_set_ifp_table_id(
 					ctx, vrf_grout_to_frr(gr_if->base.vrf_id)
 				);
+
+			// For VXLAN in VRF mode, present it as a bridge slave
+			// of the VRF interface. FRR requires an SVI (derived
+			// from the bridge master) to bring the L3VNI up and
+			// compute the Router MAC for EVPN type-5 routes.
+			if (zif_type == ZEBRA_IF_VXLAN) {
+				bridge_ifindex = ifindex_grout_to_frr(gr_if->base.vrf_id);
+				slave_type = ZEBRA_IF_SLAVE_BRIDGE;
+				l3vni_set(gr_if->base.vrf_id, gr_if->id);
+			}
 			break;
 		case GR_IFACE_MODE_BOND:
 			bond_ifindex = ifindex_grout_to_frr(gr_if->domain_id);
@@ -204,6 +215,8 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 	} else {
 		dplane_ctx_set_op(ctx, DPLANE_OP_INTF_DELETE);
 		dplane_ctx_set_status(ctx, ZEBRA_DPLANE_REQUEST_QUEUED);
+		if (gr_vxlan != NULL && gr_if->mode == GR_IFACE_MODE_VRF)
+			l3vni_del(gr_if->base.vrf_id);
 		remove_mapping_by_grout_ifindex(gr_if->id);
 	}
 

frr/rt_grout.c
@@ -2,6 +2,7 @@
 // Copyright (c) 2025 Maxime Leroy, Free Mobile
 
 #include "if_map.h"
+#include "l3vni_map.h"
 #include "log_grout.h"
 #include "rt_grout.h"
 
@@ -658,7 +659,10 @@ grout_add_nexthop(uint32_t nh_id, gr_nh_origin_t origin, const struct nexthop *n
 	struct gr_nexthop_info_srv6 *sr6;
 	struct gr_nh_add_req *req = NULL;
 	struct gr_nexthop_info_l3 *l3;
+	const struct ethaddr *rmac;
 	size_t len = sizeof(*req);
+	uint16_t vxlan_iface_id;
+	struct ipaddr vtep;
 	gr_nh_type_t type;
 
 	switch (nh->type) {
@@ -706,18 +710,41 @@ grout_add_nexthop(uint32_t nh_id, gr_nh_origin_t origin, const struct nexthop *n
 
 	switch (type) {
 	case GR_NH_T_L3:
+		// For L3 nexthops in VRFs with an L3VNI, redirect the iface from
+		// the VRF (SVI in FRR's model) to the VXLAN interface. Grout
+		// routes packets directly through the VXLAN tunnel.
+		vxlan_iface_id = l3vni_get_vxlan(req->nh.vrf_id);
+		if (vxlan_iface_id != GR_IFACE_ID_UNDEF)
+			req->nh.iface_id = vxlan_iface_id;
+
 		switch (nh->type) {
 		case NEXTHOP_TYPE_IPV4:
 		case NEXTHOP_TYPE_IPV4_IFINDEX:
 			l3 = (struct gr_nexthop_info_l3 *)req->nh.info;
 			l3->af = GR_AF_IP4;
 			memcpy(&l3->ipv4, &nh->gate.ipv4, sizeof(l3->ipv4));
+			// Apply cached RMAC from EVPN NEIGH install if available.
+			vtep.ipa_type = IPADDR_V4;
+			vtep.ipaddr_v4 = nh->gate.ipv4;
+			rmac = l3vni_rmac_get(req->nh.vrf_id, &vtep);
+			if (rmac != NULL) {
+				memcpy(&l3->mac, rmac, sizeof(l3->mac));
+				l3->flags |= GR_NH_F_REMOTE;
+			}
 			break;
 		case NEXTHOP_TYPE_IPV6:
 		case NEXTHOP_TYPE_IPV6_IFINDEX:
 			l3 = (struct gr_nexthop_info_l3 *)req->nh.info;
 			l3->af = GR_AF_IP6;
 			memcpy(&l3->ipv6, &nh->gate.ipv6, sizeof(l3->ipv6));
+			// Apply cached RMAC from EVPN NEIGH install if available.
+			vtep.ipa_type = IPADDR_V6;
+			vtep.ipaddr_v6 = nh->gate.ipv6;
+			rmac = l3vni_rmac_get(req->nh.vrf_id, &vtep);
+			if (rmac != NULL) {
+				memcpy(&l3->mac, rmac, sizeof(l3->mac));
+				l3->flags |= GR_NH_F_REMOTE;
+			}
 			break;
 		case NEXTHOP_TYPE_IFINDEX:
 			l3 = (struct gr_nexthop_info_l3 *)req->nh.info;
@@ -1010,6 +1037,32 @@ enum zebra_dplane_result grout_macfdb_update_ctx(struct zebra_dplane_ctx *ctx) {
 	free(req);
 
 	return ret == 0 ? ZEBRA_DPLANE_REQUEST_SUCCESS : ZEBRA_DPLANE_REQUEST_FAILURE;
+}
+
+enum zebra_dplane_result grout_neigh_update_ctx(struct zebra_dplane_ctx *ctx) {
+	const struct ipaddr *addr = dplane_ctx_neigh_get_ipaddr(ctx);
+	bool add = dplane_ctx_get_op(ctx) != DPLANE_OP_NEIGH_DELETE;
+	uint16_t vrf_id = vrf_frr_to_grout(dplane_ctx_get_vrf(ctx));
+
+	if (addr->ipa_type != IPADDR_V4) {
+		gr_log_debug("only IPv4 VTEP addresses supported, skip");
+		return ZEBRA_DPLANE_REQUEST_SUCCESS;
+	}
+
+	// Cache the RMAC for later use by grout_add_nexthop. We cannot
+	// create a separate nexthop here because grout's L3 nexthop hash
+	// keys on (vrf, addr) without iface_id, so it would collide with
+	// the route nexthop that FRR installs right after.
+	if (add) {
+		const struct ethaddr *mac = dplane_ctx_neigh_get_mac(ctx);
+		gr_log_debug("cache rmac vrf=%u %pIA %pEA", vrf_id, addr, mac);
+		l3vni_rmac_set(vrf_id, addr, mac);
+	} else {
+		gr_log_debug("uncache rmac vrf=%u %pIA", vrf_id, addr);
+		l3vni_rmac_del(vrf_id, addr);
+	}
+
+	return ZEBRA_DPLANE_REQUEST_SUCCESS;
 }
 
 enum zebra_dplane_result grout_vxlan_flood_update_ctx(struct zebra_dplane_ctx *ctx) {

frr/rt_grout.h
@@ -18,4 +18,5 @@ void grout_nexthop_change(bool new, struct gr_nexthop *gr_nh, bool startup);
 void grout_macfdb_change(const struct gr_fdb_entry *fdb, bool new);
 enum zebra_dplane_result grout_macfdb_update_ctx(struct zebra_dplane_ctx *ctx);
 
+enum zebra_dplane_result grout_neigh_update_ctx(struct zebra_dplane_ctx *ctx);
 enum zebra_dplane_result grout_vxlan_flood_update_ctx(struct zebra_dplane_ctx *ctx);

frr/zebra_dplane_grout.c
@@ -949,6 +949,11 @@ static enum zebra_dplane_result zd_grout_process_update(struct zebra_dplane_ctx 
 	case DPLANE_OP_MAC_DELETE:
 		return grout_macfdb_update_ctx(ctx);
 
+	case DPLANE_OP_NEIGH_INSTALL:
+	case DPLANE_OP_NEIGH_UPDATE:
+	case DPLANE_OP_NEIGH_DELETE:
+		return grout_neigh_update_ctx(ctx);
+
 	case DPLANE_OP_VTEP_ADD:
 	case DPLANE_OP_VTEP_DELETE:
 		return grout_vxlan_flood_update_ctx(ctx);

commit 5d6cce7b6a9c27ee897fb65fdaef30967e19e5ab

frr: fix L3VNI mapping cleanup on VXLAN mode change
The L3VNI forward table maintains a single VRF-to-VXLAN mapping for
EVPN type-5 nexthop redirection. When a VXLAN interface leaves VRF
mode (e.g. moved to a bridge as an L2 VNI), the mapping is never
removed. Type-5 nexthops then resolve to a VXLAN interface that is
no longer used for L3 routing.

On interface deletion, l3vni_del() uses gr_if->base.vrf_id to find
the entry to remove. However, this field reflects the current API
state which becomes zero when the interface is in bridge mode, not
the VRF that was originally registered in the forward table.

Add l3vni_del_iface() which scans the forward table by interface ID
instead of VRF ID and use it when a VXLAN enters bond or bridge
mode, and on interface deletion regardless of the current mode.

Fixes: b468374cf98d ("frr: add L3VPN support")
Reported-by: Andrea Panattoni <apanatto@redhat.com>
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Andrea Panattoni <apanatto@redhat.com>

frr/if_grout.c
@@ -177,10 +177,14 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 		case GR_IFACE_MODE_BOND:
 			bond_ifindex = ifindex_grout_to_frr(gr_if->domain_id);
 			slave_type = ZEBRA_IF_SLAVE_BOND;
+			if (zif_type == ZEBRA_IF_VXLAN)
+				l3vni_del_iface(gr_if->id);
 			break;
 		case GR_IFACE_MODE_BRIDGE:
 			bridge_ifindex = ifindex_grout_to_frr(gr_if->domain_id);
 			slave_type = ZEBRA_IF_SLAVE_BRIDGE;
+			if (zif_type == ZEBRA_IF_VXLAN)
+				l3vni_del_iface(gr_if->id);
 			break;
 		default:
 			break;
@@ -223,8 +227,8 @@ void grout_link_change(struct gr_iface *gr_if, bool new, bool startup) {
 	} else {
 		dplane_ctx_set_op(ctx, DPLANE_OP_INTF_DELETE);
 		dplane_ctx_set_status(ctx, ZEBRA_DPLANE_REQUEST_QUEUED);
-		if (gr_vxlan != NULL && gr_if->mode == GR_IFACE_MODE_VRF)
-			l3vni_del(gr_if->base.vrf_id);
+		if (gr_vxlan != NULL)
+			l3vni_del_iface(gr_if->id);
 		remove_mapping_by_grout_ifindex(gr_if->id);
 	}
 

frr/l3vni_map.c
@@ -58,6 +58,18 @@ void l3vni_del(uint16_t vrf_id) {
 	}
 }
 
+void l3vni_del_iface(uint16_t iface_id) {
+	struct l3vni_entry *e;
+
+	frr_each_safe(l3vni_hash, &l3vni_entries, e) {
+		if (e->vxlan_iface_id == iface_id) {
+			l3vni_hash_del(&l3vni_entries, e);
+			XFREE(MTYPE_GROUT_MEM, e);
+			return;
+		}
+	}
+}
+
 uint16_t l3vni_get_vxlan(uint16_t vrf_id) {
 	struct l3vni_entry key = {.vrf_id = vrf_id};
 	struct l3vni_entry *e = l3vni_hash_find(&l3vni_entries, &key);

frr/l3vni_map.h
@@ -32,6 +32,9 @@ void l3vni_set(uint16_t vrf_id, uint16_t vxlan_iface_id);
 // Remove mapping for vrf_id.
 void l3vni_del(uint16_t vrf_id);
 
+// Remove mapping for a specific VXLAN iface, if any.
+void l3vni_del_iface(uint16_t iface_id);
+
 // Return vxlan iface id for vrf_id, or GR_IFACE_ID_UNDEF.
 uint16_t l3vni_get_vxlan(uint16_t vrf_id);
 