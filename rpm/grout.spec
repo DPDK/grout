DPDK/grout

commit a6643d4302f5175f9b97dbaeb092b82c9c6284fc

frr: add systemd dropin to rpm/deb installed files
This was forgotten in the previous commit.

Fixes: 8ceb41a4d51a ("frr: add systemd drop-in for frr.service")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

rpm/grout.spec
@@ -161,6 +161,7 @@ rm -rf %{buildroot}%{_sysconfdir} %{buildroot}%{_unitdir}
 %doc README.md
 %license licenses/GPL-2.0-or-later.txt
 %attr(755, root, root) %{_libdir}/frr/modules/dplane_grout.so
+%attr(644, root, root) %{_unitdir}/frr.service.d/grout.conf
 %if %{with docs}
 %attr(644, root, root) %{_mandir}/man7/grout-frr.7*
 %endif

debian/grout-frr.install
@@ -1,2 +1,3 @@
 /usr/share/man/man7/grout-frr.7
 /usr/lib/*/frr/modules/dplane_grout.so
+/usr/lib/systemd/system/frr.service.d/grout.conf

commit cd66b6d535e52b86daf4ddd781419940fdcc74d9

rpm: fix build without systemd
Fix the following error when building --without=systemd:

Processing files: grout-frr-0.16.1-20260720120706.local.el10.x86_64
error: File not found: /BUILDROOT/.../usr/lib/systemd/system/frr.service.d/grout.conf

Fixes: a6643d4302f5 ("frr: add systemd dropin to rpm/deb installed files")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Andrea Panattoni <apanatto@redhat.com>

rpm/grout.spec
@@ -161,7 +161,9 @@ rm -rf %{buildroot}%{_sysconfdir} %{buildroot}%{_unitdir}
 %doc README.md
 %license licenses/GPL-2.0-or-later.txt
 %attr(755, root, root) %{_libdir}/frr/modules/dplane_grout.so
+%if %{with systemd}
 %attr(644, root, root) %{_unitdir}/frr.service.d/grout.conf
+%endif
 %if %{with docs}
 %attr(644, root, root) %{_mandir}/man7/grout-frr.7*
 %endif