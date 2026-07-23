DPDK/grout

commit 10be5b5883ce3dbd7e1a1e7313fabd0e054b21c8

l2: push external mac addresses in hardware
In setups where grout physical port is a VF, traffic sent by other
VFs reach grout only if the destination address is multicast or a mac
address for which a rx filter was declared in hw.

Make use of control path pushed FDB entries to populate hardware and
let the packets flow.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/l2/control/fdb.c
@@ -332,6 +332,67 @@ static struct api_out fdb_config_set(const void *request, struct api_ctx *) {
 	return api_out(0, 0, NULL);
 }
 
+static void push_mac_to_hw(struct iface *iface, const struct rte_ether_addr *mac, bool add) {
+	int ret;
+
+	if (add)
+		ret = iface_add_eth_addr(iface, mac);
+	else
+		ret = iface_del_eth_addr(iface, mac);
+	if (ret < 0) {
+		LOG(DEBUG,
+		    "failed to %s mac " ETH_F " to %s: %s",
+		    add ? "add" : "del",
+		    mac,
+		    iface->name,
+		    strerror(errno));
+	}
+}
+
+static void fdb_event_cb(uint32_t event, const void *obj) {
+	const struct iface_info_bridge *bridge_info;
+	const struct gr_fdb_entry *fdb = obj;
+	const struct iface *bridge;
+
+	bridge = iface_from_id(fdb->bridge_id);
+	if (bridge == NULL) {
+		LOG(ERR, "unknown bridge %u", fdb->bridge_id);
+		return;
+	}
+
+	if ((fdb->flags & GR_FDB_F_EXTERN) == 0)
+		return;
+
+	bridge_info = iface_info_bridge(bridge);
+	for (unsigned i = 0; i < bridge_info->n_members; i++) {
+		struct iface *member = bridge_info->members[i];
+
+		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
+		assert(fdb->vlan_id == 0);
+		// FIXME: ideally, we should ask for adding the mac in hw only when getting the
+		// *first* transition from !EXTERN to EXTERN to handle both learning mac on the
+		// wire and from FRR.
+		push_mac_to_hw(member, &fdb->mac, event != GR_EVENT_FDB_DEL);
+	}
+}
+
+void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add) {
+	struct gr_fdb_entry *fdb;
+	uint32_t next = 0;
+	const void *key;
+	void *data;
+
+	while (rte_hash_iterate(fdb_hash, &key, &data, &next) >= 0) {
+		if (!fdb_match(data, GR_FDB_F_EXTERN, bridge->id, GR_IFACE_ID_UNDEF, NULL))
+			continue;
+
+		fdb = data;
+		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
+		assert(fdb->vlan_id == 0);
+		push_mac_to_hw(member, &fdb->mac, add);
+	}
+}
+
 static void fdb_ageing_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
 	const struct iface *bridge;
 	struct gr_fdb_entry *fdb;
@@ -409,6 +470,8 @@ RTE_INIT(init) {
 	api_handler(GR_FDB_LIST, fdb_list);
 	api_handler(GR_FDB_CONFIG_GET, fdb_config_get);
 	api_handler(GR_FDB_CONFIG_SET, fdb_config_set);
+	event_subscribe(GR_EVENT_FDB_ADD, fdb_event_cb);
+	event_subscribe(GR_EVENT_FDB_DEL, fdb_event_cb);
 	event_serializer(GR_EVENT_FDB_ADD, NULL);
 	event_serializer(GR_EVENT_FDB_DEL, NULL);
 	event_serializer(GR_EVENT_FDB_UPDATE, NULL);

modules/l2/control/bridge.c
@@ -58,6 +58,8 @@ static int bridge_attach_member(struct iface *bridge, struct iface *member) {
 	member->vrf_id = GR_VRF_ID_UNDEF;
 	member->mode = GR_IFACE_MODE_BRIDGE;
 
+	fdb_sync_hardware(bridge, member, true);
+
 	return 0;
 }
 
@@ -67,6 +69,9 @@ static int bridge_detach_member(struct iface *bridge, struct iface *member) {
 	for (unsigned i = 0; i < br->n_members; i++) {
 		if (br->members[i] == member) {
 			unsigned last = br->n_members - 1;
+
+			fdb_sync_hardware(bridge, member, false);
+
 			if (i < last)
 				br->members[i] = br->members[last];
 			br->n_members--;
@@ -86,6 +91,7 @@ static int bridge_fini(struct iface *iface) {
 
 	for (unsigned i = 0; i < bridge->n_members; i++) {
 		struct iface *member = bridge->members[i];
+		fdb_sync_hardware(iface, member, false);
 		iface_set_promisc(member, false);
 		member->vrf_id = vrf_default_get_or_create();
 		if (member->vrf_id != GR_VRF_ID_UNDEF)

modules/l2/control/l2.h
@@ -39,6 +39,9 @@ void fdb_learn(
 // Delete all FDB entries referencing the provided interface.
 void fdb_purge_iface(uint16_t iface_id);
 
+// Push or remove mac addresses from HW Rx filters.
+void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add);
+
 // Delete all FDB entries referencing the provided bridge.
 void fdb_purge_bridge(uint16_t bridge_id);
 

commit fcec2b3eaebc353f815ca075c0d53cebe267d909

l2: do not push locally learnt mac to hardware
Avoid pushing back mac addresses coming from the same iface.
This is unneeded and may confuse some NICs.

With this approach, pushing mac to hardware becomes way simpler:
a decapsulated packet mac address coming from a VXLAN iface simply has
to be pushed to all members of the bridge, when it is first learnt.

Similarly, a mac address learnt by external means (FRR) can be pushed to
all but the originating interface.

If we had a setup with a bridge with two member ifaces backed by two
separate NICs, a mac address learnt from an interface belonging to the
first NIC will be pushed to the interface backed by the second NIC.

As a consequence of this approach, we don't need to listen to FDB updates.
The GR_FDB_F_HW flag becomes unneeded since only add/del ops matter.

Fixes: 10be5b5883ce ("l2: push external mac addresses in hardware")
Fixes: 3f6447663762 ("fdb: fix missing mac in hw when an entry is updated")
Fixes: a4c609a5a5ac ("l2: program all fdb entries in bridge ports")
Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

api/gr_api.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 
 // Must be bumped when making non-backward compatible changes in API headers
-#define GR_API_VERSION 4
+#define GR_API_VERSION 5
 
 // API request header.
 struct gr_api_request {

modules/l2/api/gr_l2.h
@@ -65,7 +65,6 @@ typedef enum : uint8_t {
 	GR_FDB_F_STATIC = GR_BIT8(0), // User-configured, never aged out.
 	GR_FDB_F_LEARN = GR_BIT8(1), // Learned via local bridge.
 	GR_FDB_F_EXTERN = GR_BIT8(2), // Programmed by external control plane.
-	GR_FDB_F_HW = GR_BIT8(3), // Mac address programmed in hardware filter.
 } gr_fdb_flags_t;
 
 // Forwarding database entry associating a MAC+VLAN to a bridge member interface.

modules/l2/cli/fdb.c
@@ -85,8 +85,6 @@ static size_t fdb_format_flags(char *buf, size_t len, gr_fdb_flags_t flags) {
 		SAFE_BUF(snprintf, len, "%sstatic", n ? " " : "");
 	if (flags & GR_FDB_F_EXTERN)
 		SAFE_BUF(snprintf, len, "%sextern", n ? " " : "");
-	if (flags & GR_FDB_F_HW)
-		SAFE_BUF(snprintf, len, "%shw", n ? " " : "");
 err:
 	return n;
 }
@@ -115,8 +113,6 @@ static cmd_status_t fdb_show(struct gr_api_client *c, const struct ec_pnode *p) 
 		req.flags |= GR_FDB_F_LEARN;
 	if (arg_str(p, "extern") != NULL)
 		req.flags |= GR_FDB_F_EXTERN;
-	if (arg_str(p, "hw") != NULL)
-		req.flags |= GR_FDB_F_HW;
 
 	struct gr_table *table = gr_table_new();
 	gr_table_column(table, "BRIDGE", GR_DISP_LEFT); // 0

modules/l2/control/fdb.c
@@ -256,8 +256,6 @@ static inline bool fdb_match(
 		return false;
 	if ((flags & GR_FDB_F_EXTERN) && !(e->flags & GR_FDB_F_EXTERN))
 		return false;
-	if ((flags & GR_FDB_F_HW) && !(e->flags & GR_FDB_F_HW))
-		return false;
 	if (bridge_id != GR_IFACE_ID_UNDEF && e->bridge_id != bridge_id)
 		return false;
 	if (iface_id != GR_IFACE_ID_UNDEF && e->iface_id != iface_id)
@@ -354,8 +352,7 @@ static void push_mac_to_hw(struct iface *iface, const struct rte_ether_addr *mac
 
 static void fdb_event_cb(uint32_t event, const void *obj) {
 	const struct iface_info_bridge *bridge_info;
-	struct gr_fdb_entry *fdb = (void *)obj;
-	bool add = event != GR_EVENT_FDB_DEL;
+	const struct gr_fdb_entry *fdb = obj;
 	const struct iface *bridge;
 
 	bridge = iface_from_id(fdb->bridge_id);
@@ -364,24 +361,18 @@ static void fdb_event_cb(uint32_t event, const void *obj) {
 		return;
 	}
 
-	if (add && (fdb->flags & GR_FDB_F_HW))
-		return;
-	if (!add && !(fdb->flags & GR_FDB_F_HW))
-		return;
-
 	bridge_info = iface_info_bridge(bridge);
 	for (unsigned i = 0; i < bridge_info->n_members; i++) {
 		struct iface *member = bridge_info->members[i];
 
+		// skip the interface where the MAC was learned
+		if (member->id == fdb->iface_id)
+			continue;
+
 		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
 		assert(fdb->vlan_id == 0);
-		push_mac_to_hw(member, &fdb->mac, add);
+		push_mac_to_hw(member, &fdb->mac, event != GR_EVENT_FDB_DEL);
 	}
-
-	if (add)
-		fdb->flags |= GR_FDB_F_HW;
-	else
-		fdb->flags &= ~GR_FDB_F_HW;
 }
 
 void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add) {
@@ -394,6 +385,9 @@ void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool ad
 		fdb = data;
 
 		if (fdb->bridge_id != bridge->id)
+			continue;
+		// skip the interface where the MAC was learned
+		if (member->id == fdb->iface_id)
 			continue;
 
 		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
@@ -480,7 +474,6 @@ RTE_INIT(init) {
 	api_handler(GR_FDB_CONFIG_GET, fdb_config_get);
 	api_handler(GR_FDB_CONFIG_SET, fdb_config_set);
 	event_subscribe(GR_EVENT_FDB_ADD, fdb_event_cb);
-	event_subscribe(GR_EVENT_FDB_UPDATE, fdb_event_cb);
 	event_subscribe(GR_EVENT_FDB_DEL, fdb_event_cb);
 	event_serializer(GR_EVENT_FDB_ADD, NULL);
 	event_serializer(GR_EVENT_FDB_DEL, NULL);