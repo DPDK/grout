DPDK/grout

commit 0a399004797ee4f8dade5f18bbab37dacbf1ba5a

ip: add ping and traceroute commands
Add API requests to send ICMP echo packets and receive replies. Use the
newly added mechanism to register callbacks based on ICMP message types.

Store up to 1024 packets in a linked list so that the clients can fetch
them based on the returned ICMP identifier.

The cli command outputs are similar to standard linux tools:

 grout# ping 172.16.0.2
 reply from 172.16.0.2: icmp_seq=0 ttl=64 time=0.191 ms
 reply from 172.16.0.2: icmp_seq=1 ttl=64 time=0.412 ms
 reply from 172.16.0.2: icmp_seq=2 ttl=64 time=0.318 ms
 reply from 172.16.0.2: icmp_seq=3 ttl=64 time=0.316 ms

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Signed-off-by: Robin Jarry <robin@jarry.cc>

modules/ip/control/icmp.c
@@ -1,0 +1,227 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr_api.h>
+#include <gr_control.h>
+#include <gr_control_input.h>
+#include <gr_control_output.h>
+#include <gr_ip4.h>
+#include <gr_ip4_control.h>
+#include <gr_ip4_datapath.h>
+#include <gr_log.h>
+#include <gr_queue.h>
+
+#include <rte_atomic.h>
+#include <rte_icmp.h>
+#include <rte_ip.h>
+#include <rte_ring.h>
+#include <stb_ds.h>
+
+#include <time.h>
+
+struct icmp_queue_item {
+	struct rte_mbuf *mbuf;
+	STAILQ_ENTRY(icmp_queue_item) next;
+};
+
+static STAILQ_HEAD(, icmp_queue_item) icmp_queue = STAILQ_HEAD_INITIALIZER(icmp_queue);
+static struct rte_mempool *pool;
+
+static void icmp_queue_pop(struct icmp_queue_item *i, bool free_mbuf) {
+	STAILQ_REMOVE(&icmp_queue, i, icmp_queue_item, next);
+	if (free_mbuf)
+		rte_pktmbuf_free(i->mbuf);
+	rte_mempool_put(pool, i);
+}
+
+// Callback invoked by control plane for each ICMP packet received for a local address.
+// The packet is added at the end of a linked list.
+static void icmp_input_cb(struct rte_mbuf *m) {
+	struct icmp_queue_item *i;
+	void *data;
+
+	while (rte_mempool_get(pool, &data) < 0)
+		icmp_queue_pop(STAILQ_FIRST(&icmp_queue), true);
+
+	i = data;
+	i->mbuf = m;
+	STAILQ_INSERT_TAIL(&icmp_queue, i, next);
+}
+
+// Search for the oldest ICMP response matching the given identifier.
+// If found, the packet is removed from the queue.
+static struct rte_mbuf *get_icmp_response(uint16_t id) {
+	struct icmp_queue_item *i, *tmp;
+	struct rte_mbuf *mbuf = NULL;
+
+	STAILQ_FOREACH_SAFE (i, &icmp_queue, next, tmp) {
+		struct rte_icmp_hdr *icmp = rte_pktmbuf_mtod(i->mbuf, struct rte_icmp_hdr *);
+
+		if (icmp->icmp_type != RTE_ICMP_TYPE_ECHO_REPLY) {
+			// RFC 792: Destination Unreachable or Time Exceeded
+			// The icmp_seq_nb and icmp_ident fields are unused.
+			// Jump to the next header which contains the original IP header
+			struct rte_ipv4_hdr *ip = PAYLOAD(icmp);
+
+			if (ip->next_proto_id != IPPROTO_ICMP) {
+				// should not happen, but let's be safe.
+				icmp_queue_pop(i, true);
+				continue;
+			}
+
+			// Skip the original IP header to find the original ICMP payload
+			icmp = PAYLOAD(ip);
+
+			if (icmp->icmp_type != RTE_ICMP_TYPE_ECHO_REQUEST) {
+				// should not happen, but let's be safe.
+				icmp_queue_pop(i, true);
+				continue;
+			}
+		}
+
+		if (rte_be_to_cpu_16(icmp->icmp_ident) == id) {
+			mbuf = i->mbuf;
+			icmp_queue_pop(i, false);
+			break;
+		}
+	}
+
+	return mbuf;
+}
+
+// Global id which is used to differentiate between api clients
+static rte_atomic16_t icmp_ident = RTE_ATOMIC16_INIT(0);
+
+static struct api_out icmp_send(const void *request, void **response) {
+	const struct gr_ip4_icmp_send_req *req = request;
+	struct gr_ip4_icmp_send_resp *resp = NULL;
+	struct nexthop *nh;
+	int ret;
+
+	if ((resp = calloc(1, sizeof(*resp))) == NULL)
+		return api_out(ENOMEM, 0);
+
+	if ((nh = ip4_route_lookup(req->vrf, req->addr)) == NULL) {
+		ret = -errno;
+		goto fail;
+	}
+
+	resp->id = rte_atomic16_add_return(&icmp_ident, 1);
+	ret = icmp_local_send(req->vrf, req->addr, nh, resp->id, req->seq_num, req->ttl);
+	if (ret < 0)
+		goto fail;
+
+	*response = resp;
+
+	return api_out(0, sizeof(*resp));
+fail:
+	free(resp);
+	return api_out(-ret, 0);
+}
+
+static struct api_out icmp_recv(const void *request, void **response) {
+	const struct gr_ip4_icmp_recv_req *icmp_req = request;
+	struct gr_ip4_icmp_recv_resp *resp = NULL;
+	struct rte_icmp_hdr *icmp;
+	struct rte_ipv4_hdr *ip;
+	clock_t *timestamp;
+	struct rte_mbuf *m;
+	size_t len = 0;
+	int ret = 0;
+
+	m = get_icmp_response(icmp_req->id);
+	if (m == NULL)
+		return api_out(0, 0);
+
+	if ((resp = calloc(1, sizeof(*resp))) == NULL) {
+		ret = ENOMEM;
+		goto out;
+	}
+
+	// Ugly, there is no guarantee that the outer packet is actually IPv4
+	ip = rte_pktmbuf_mtod_offset(m, struct rte_ipv4_hdr *, -sizeof(*ip));
+
+	icmp = rte_pktmbuf_mtod(m, struct rte_icmp_hdr *);
+	resp->src_addr = ip->src_addr;
+	resp->ttl = ip->time_to_live;
+	resp->type = icmp->icmp_type;
+	resp->code = icmp->icmp_code;
+
+	if (icmp->icmp_type != RTE_ICMP_TYPE_ECHO_REPLY) {
+		// RFC 792: Destination Unreachable or Time Exceeded
+		// The icmp_seq_nb and icmp_ident fields are unused.
+		// Jump to the next header which contains the original IP header
+		ip = PAYLOAD(icmp);
+		// Skip the original IP header to find the original ICMP payload
+		icmp = PAYLOAD(ip);
+	}
+
+	// icmp either points to an echo request or reply (checked in get_icmp_response())
+	resp->ident = rte_be_to_cpu_16(icmp->icmp_ident);
+	resp->seq_num = rte_be_to_cpu_16(icmp->icmp_seq_nb);
+	timestamp = PAYLOAD(icmp);
+	resp->response_time = control_output_mbuf_data(m)->timestamp - *timestamp;
+
+	*response = resp;
+	len = sizeof(*resp);
+out:
+	rte_pktmbuf_free(m);
+	return api_out(ret, len);
+}
+
+static struct gr_api_handler ip4_icmp_send_handler = {
+	.name = "icmp send",
+	.request_type = GR_IP4_ICMP_SEND,
+	.callback = icmp_send,
+};
+
+static struct gr_api_handler ip4_icmp_recv_handler = {
+	.name = "icmp recv",
+	.request_type = GR_IP4_ICMP_RECV,
+	.callback = icmp_recv,
+};
+
+#define ICMP_LOCAL_QUEUE_SIZE 1024
+
+static void icmp_init(struct event_base *) {
+	pool = rte_mempool_create(
+		"icmp_queue", // name
+		ICMP_LOCAL_QUEUE_SIZE,
+		sizeof(struct icmp_queue_item),
+		0, // cache size
+		0, // priv size
+		NULL, // mp_init
+		NULL, // mp_init_arg
+		NULL, // obj_init
+		NULL, // obj_init_arg
+		SOCKET_ID_ANY,
+		0 // flags
+	);
+	if (pool == NULL)
+		ABORT("rte_mempool_create(icmp_queue) failed");
+}
+
+static void icmp_fini(struct event_base *) {
+	if (pool != NULL) {
+		struct icmp_queue_item *i, *tmp;
+		STAILQ_FOREACH_SAFE (i, &icmp_queue, next, tmp)
+			icmp_queue_pop(i, true);
+		rte_mempool_free(pool);
+		pool = NULL;
+	}
+}
+
+static struct gr_module icmp_module = {
+	.name = "icmp",
+	.init = icmp_init,
+	.fini = icmp_fini,
+};
+
+RTE_INIT(icmp_module_init) {
+	gr_register_module(&icmp_module);
+	gr_register_api_handler(&ip4_icmp_send_handler);
+	gr_register_api_handler(&ip4_icmp_recv_handler);
+	icmp_input_register_callback(RTE_ICMP_TYPE_DEST_UNREACHABLE, icmp_input_cb);
+	icmp_input_register_callback(RTE_ICMP_TYPE_TTL_EXCEEDED, icmp_input_cb);
+	icmp_input_register_callback(RTE_ICMP_TYPE_ECHO_REPLY, icmp_input_cb);
+}

modules/ip/cli/icmp.c
@@ -1,0 +1,219 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024 Christophe Fontaine
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_ip4.h>
+#include <gr_net_types.h>
+
+#include <ecoli.h>
+#include <rte_icmp.h>
+#include <rte_ip.h>
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/queue.h>
+#include <unistd.h>
+
+static const char *icmp_dest_unreachable[] = {
+	[RTE_ICMP_CODE_UNREACH_NET] = "net unreachable",
+	[RTE_ICMP_CODE_UNREACH_HOST] = "host unreachable",
+	[RTE_ICMP_CODE_UNREACH_PROTO] = "protocol unreachable",
+	[RTE_ICMP_CODE_UNREACH_PORT] = "port unreachable",
+	[RTE_ICMP_CODE_UNREACH_FRAG] = "fragmentation needed and DF set",
+	[RTE_ICMP_CODE_UNREACH_SRC] = "source route failed",
+};
+
+static bool stop;
+
+static void sighandler(int) {
+	stop = true;
+}
+
+static cmd_status_t icmp_send(
+	const struct gr_api_client *c,
+	struct gr_ip4_icmp_send_req *req,
+	uint16_t msdelay,
+	uint16_t count,
+	bool mode_traceroute
+) {
+	struct gr_ip4_icmp_recv_resp *reply_resp;
+	struct gr_ip4_icmp_recv_req reply_req;
+	struct gr_ip4_icmp_send_resp *start_resp;
+	int timeout, ret, errors;
+	void *resp_ptr = NULL;
+
+	stop = false;
+	errors = 0;
+	errno = 0;
+
+	for (int i = mode_traceroute; i < count && stop == false; i++) {
+		req->ttl = mode_traceroute ? i : 64;
+		req->seq_num = i;
+
+		ret = gr_api_client_send_recv(c, GR_IP4_ICMP_SEND, sizeof(*req), req, &resp_ptr);
+		if (ret < 0)
+			return CMD_ERROR;
+
+		start_resp = resp_ptr;
+		reply_req.id = start_resp->id;
+		free(resp_ptr);
+		resp_ptr = NULL;
+
+		timeout = 50;
+		do {
+			usleep(10000);
+			ret = gr_api_client_send_recv(
+				c, GR_IP4_ICMP_RECV, sizeof(reply_req), &reply_req, &resp_ptr
+			);
+			if (resp_ptr != NULL)
+				break;
+		} while (ret == 0 && --timeout > 0);
+
+		if (ret < 0)
+			return CMD_ERROR;
+
+		reply_resp = resp_ptr;
+		if (reply_resp == NULL || timeout == 0) {
+			errors++;
+			errno = ETIMEDOUT;
+			if (mode_traceroute)
+				printf("%2d  timeout\n", i);
+			else
+				printf("timeout: icmp_seq=%d\n", i);
+		} else {
+			switch (reply_resp->type) {
+			case RTE_ICMP_TYPE_ECHO_REPLY:
+				if (mode_traceroute) {
+					printf("%2d  " IP4_ADDR_FMT " time=%.3f ms\n",
+					       i,
+					       IP4_ADDR_SPLIT(&reply_resp->src_addr),
+					       reply_resp->response_time / 1000.);
+					stop = true;
+					errors = 0;
+					errno = 0;
+				} else {
+					printf("reply from " IP4_ADDR_FMT ": icmp_seq=%d ttl=%d "
+					       "time=%.3f ms\n",
+					       IP4_ADDR_SPLIT(&reply_resp->src_addr),
+					       reply_resp->seq_num,
+					       reply_resp->ttl,
+					       reply_resp->response_time / 1000.);
+				}
+				break;
+			case RTE_ICMP_TYPE_DEST_UNREACHABLE:
+				errors++;
+				errno = EHOSTUNREACH;
+				printf("reply from " IP4_ADDR_FMT ": icmp_seq=%d ttl=%d: %s\n",
+				       IP4_ADDR_SPLIT(&reply_resp->src_addr),
+				       reply_resp->seq_num,
+				       reply_resp->ttl,
+				       icmp_dest_unreachable[reply_resp->code]);
+				break;
+			case RTE_ICMP_TYPE_TTL_EXCEEDED:
+				errors++;
+				errno = ETIMEDOUT;
+				printf("%2d  " IP4_ADDR_FMT " time=%.3f ms\n",
+				       i,
+				       IP4_ADDR_SPLIT(&reply_resp->src_addr),
+				       reply_resp->response_time / 1000.);
+				break;
+			}
+			free(resp_ptr);
+			resp_ptr = NULL;
+		}
+		usleep(msdelay * 1000);
+	}
+
+	if (!stop && errors > 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t ping(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip4_icmp_send_req req = {.seq_num = 0, .vrf = 0};
+	cmd_status_t ret = CMD_ERROR;
+	uint16_t count = UINT16_MAX;
+	uint16_t msdelay = 1000;
+
+	if (inet_pton(AF_INET, arg_str(p, "IP"), &req.addr) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if ((ret = arg_u16(p, "VRF", &req.vrf)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+	if ((ret = arg_u16(p, "COUNT", &count)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+	if ((ret = arg_u16(p, "DELAY", &msdelay)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+
+	sighandler_t prev_handler = signal(SIGINT, sighandler);
+	if (prev_handler == SIG_ERR)
+		return CMD_ERROR;
+
+	ret = icmp_send(c, &req, msdelay, count, false);
+
+	signal(SIGINT, prev_handler);
+
+	return ret;
+}
+
+static cmd_status_t traceroute(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip4_icmp_send_req req = {.seq_num = 0, .vrf = 0};
+	cmd_status_t ret = CMD_SUCCESS;
+
+	if (inet_pton(AF_INET, arg_str(p, "IP"), &req.addr) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if ((ret = arg_u16(p, "VRF", &req.vrf)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+
+	sighandler_t prev_handler = signal(SIGINT, sighandler);
+	if (prev_handler == SIG_ERR)
+		return CMD_ERROR;
+
+	ret = icmp_send(c, &req, 0, 255, true);
+
+	signal(SIGINT, prev_handler);
+
+	return ret;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(
+			root, CTX_ARG("ping", "Send IPv4 ICMP echo requests and wait for replies.")
+		),
+		"IP [vrf VRF] [count COUNT] [delay DELAY]",
+		ping,
+		"Send IPv4 ICMP echo requests and wait for replies.",
+		with_help("IPv4 destination address.", ec_node_re("IP", IPV4_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10)),
+		with_help("Number of packets to send.", ec_node_uint("COUNT", 1, UINT16_MAX, 10)),
+		with_help("Delay in ms between icmp echo.", ec_node_uint("DELAY", 0, 10000, 10))
+	);
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ARG("traceroute", "Discover IPv4 intermediate gateways.")),
+		"IP [vrf VRF]",
+		traceroute,
+		"Discover IPv4 intermediate gateways.",
+		with_help("IPv4 destination address.", ec_node_re("IP", IPV4_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+
+	return ret;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "ping",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

modules/ip/api/gr_ip4.h
@@ -176,4 +176,35 @@ struct gr_ip4_addr_list_resp {
 	struct gr_ip4_ifaddr addrs[/* n_addrs */];
 };
 
+// icmp ////////////////////////////////////////////////////////////////////////
+
+#define GR_IP4_ICMP_SEND REQUEST_TYPE(GR_IP4_MODULE, 0x0024)
+
+struct gr_ip4_icmp_send_req {
+	ip4_addr_t addr;
+	uint16_t vrf;
+	uint16_t seq_num;
+	uint8_t ttl;
+};
+
+struct gr_ip4_icmp_send_resp {
+	uint16_t id;
+};
+
+#define GR_IP4_ICMP_RECV REQUEST_TYPE(GR_IP4_MODULE, 0x0025)
+
+struct gr_ip4_icmp_recv_req {
+	uint16_t id;
+};
+
+struct gr_ip4_icmp_recv_resp {
+	uint8_t type;
+	uint8_t code;
+	uint8_t ttl;
+	uint16_t ident;
+	uint16_t seq_num;
+	ip4_addr_t src_addr;
+	clock_t response_time;
+};
+
 #endif

modules/ip/cli/meson.build
@@ -5,4 +5,5 @@ cli_src += files(
   'address.c',
   'nexthop.c',
   'route.c',
+  'icmp.c',
 )

modules/ip/control/meson.build
@@ -3,6 +3,7 @@
 
 src += files(
   'address.c',
+  'icmp.c',
   'nexthop.c',
   'route.c',
 )

commit 811270c44d59890c53fef448e5cbbc0889f149b6

icmp: account for IP options in embedded headers
ICMP error messages (Destination Unreachable, TTL Exceeded) contain the
original IP header followed by 8 bytes of the original payload. The code
uses PAYLOAD(ip) to skip past the embedded IP header, which assumes a
fixed 20-byte header. If the original packet had IP options, this lands
in the middle of the options instead of at the inner ICMP header.

Use rte_ipv4_hdr_len() to compute the actual header length from the IHL
field.

Fixes: 0a399004797e ("ip: add ping and traceroute commands")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip/control/icmp.c
@@ -64,8 +64,9 @@ get_icmp_response(uint16_t ident, uint16_t seq_num, gr_clock_ns_t *timestamp) {
 				continue;
 			}
 
-			// Skip the original IP header to find the original ICMP payload
-			icmp = PAYLOAD(ip);
+			// Skip the original IP header (may have options) to
+			// find the original ICMP payload.
+			icmp = RTE_PTR_ADD(ip, rte_ipv4_hdr_len(ip));
 
 			if (icmp->icmp_type != RTE_ICMP_TYPE_ECHO_REQUEST) {
 				// should not happen, but let's be safe.
@@ -132,8 +133,9 @@ static struct api_out icmp_recv(const void *request, struct api_ctx *) {
 		// The icmp_seq_nb and icmp_ident fields are unused.
 		// Jump to the next header which contains the original IP header
 		struct rte_ipv4_hdr *ip = PAYLOAD(icmp);
-		// Skip the original IP header to find the original ICMP payload
-		icmp = PAYLOAD(ip);
+		// Skip the original IP header (may have options) to
+		// find the original ICMP payload.
+		icmp = RTE_PTR_ADD(ip, rte_ipv4_hdr_len(ip));
 	}
 
 	// icmp either points to an echo request or reply (checked in get_icmp_response())