DPDK/grout

commit 71a80f63665eec0e40facc41ed9dee4540f8f0e2

ip6: add ping and traceroute command
Add "ping <ip6>" and "traceroute <ip6>" from cli.
Architecture is copied from existing ip4 ping/traceroute command.

Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

modules/ip6/control/icmp6.c
@@ -1,0 +1,201 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Olivier Gournet
+
+#include <gr_api.h>
+#include <gr_control_input.h>
+#include <gr_control_output.h>
+#include <gr_icmp6.h>
+#include <gr_ip6.h>
+#include <gr_ip6_control.h>
+#include <gr_ip6_datapath.h>
+#include <gr_log.h>
+#include <gr_module.h>
+#include <gr_queue.h>
+
+#include <rte_atomic.h>
+#include <rte_ip.h>
+#include <rte_ring.h>
+
+#include <time.h>
+
+struct icmp_queue_item {
+	struct rte_mbuf *mbuf;
+	STAILQ_ENTRY(icmp_queue_item) next;
+};
+
+static STAILQ_HEAD(, icmp_queue_item) icmp_queue = STAILQ_HEAD_INITIALIZER(icmp_queue);
+static struct rte_mempool *pool;
+
+static void icmp6_queue_pop(struct icmp_queue_item *i, bool free_mbuf) {
+	STAILQ_REMOVE(&icmp_queue, i, icmp_queue_item, next);
+	if (free_mbuf)
+		rte_pktmbuf_free(i->mbuf);
+	rte_mempool_put(pool, i);
+}
+
+// called from dataplane context
+static void icmp6_input_cb(struct rte_mbuf *m) {
+	struct icmp_queue_item *i;
+	void *data;
+
+	while (rte_mempool_get(pool, &data) < 0)
+		icmp6_queue_pop(STAILQ_FIRST(&icmp_queue), true);
+
+	i = data;
+	i->mbuf = m;
+	STAILQ_INSERT_TAIL(&icmp_queue, i, next);
+}
+
+static struct rte_mbuf *
+get_icmp6_echo_reply(uint16_t id, uint16_t seq_num, struct icmp6 **out_icmp6) {
+	struct icmp_queue_item *i, *tmp;
+	struct rte_mbuf *mbuf;
+	struct icmp6 *icmp6;
+	struct icmp6_echo_reply *icmp6_echo;
+	struct rte_ipv6_hdr *ip6;
+
+	STAILQ_FOREACH_SAFE (i, &icmp_queue, next, tmp) {
+		mbuf = i->mbuf;
+
+		if (rte_pktmbuf_pkt_len(mbuf) < GR_ICMP6_HDR_LEN + sizeof(clock_t))
+			goto free_and_skip;
+
+		icmp6 = rte_pktmbuf_mtod(mbuf, struct icmp6 *);
+		icmp6_echo = PAYLOAD(icmp6);
+
+		// icmpv6 error packet: find embedded origin ipv6 packet, and use
+		// it if it's our original echo request
+		if (icmp6->type != ICMP6_TYPE_ECHO_REPLY) {
+			ip6 = PAYLOAD(icmp6_echo);
+			icmp6 = PAYLOAD(ip6);
+			icmp6_echo = PAYLOAD(icmp6);
+			if (rte_pktmbuf_pkt_len(mbuf
+			    ) < GR_ICMP6_HDR_LEN + sizeof(*ip6) + GR_ICMP6_HDR_LEN + sizeof(clock_t)
+			    || ip6->proto != IPPROTO_ICMPV6
+			    || icmp6->type != ICMP6_TYPE_ECHO_REQUEST)
+				goto free_and_skip;
+		}
+
+		if (rte_be_to_cpu_16(icmp6_echo->ident) == id
+		    && rte_be_to_cpu_16(icmp6_echo->seqnum) == seq_num) {
+			icmp6_queue_pop(i, false);
+			*out_icmp6 = icmp6;
+			return mbuf;
+		}
+
+free_and_skip:
+		icmp6_queue_pop(i, true);
+	}
+
+	return NULL;
+}
+
+static struct api_out icmp6_send(const void *request, void ** /* response */) {
+	const struct gr_ip6_icmp_send_req *req = request;
+	struct nexthop *nh;
+	int ret;
+
+	// XXX won't be able to ping linklocal addresses.
+	if ((nh = ip6_route_lookup(req->vrf, GR_IFACE_ID_UNDEF, &req->addr)) == NULL)
+		return api_out(errno, 0);
+
+	ret = icmp6_local_send(&req->addr, nh, req->id, req->seq_num, req->ttl);
+	return api_out(ret, 0);
+}
+
+static struct api_out icmp6_recv(const void *request, void **response) {
+	const struct gr_ip6_icmp_recv_req *recvreq = request;
+	struct gr_ip6_icmp_recv_resp *resp;
+	struct control_output_mbuf_data *d_ctl;
+	struct ip6_local_mbuf_data *d_ip6;
+	struct icmp6 *icmp6;
+	struct icmp6_echo_reply *icmp6_echo;
+	clock_t *timestamp;
+	struct rte_mbuf *m;
+	int ret = 0;
+
+	m = get_icmp6_echo_reply(recvreq->id, recvreq->seq_num, &icmp6);
+	if (m == NULL)
+		return api_out(0, 0);
+
+	d_ctl = control_output_mbuf_data(m);
+	d_ip6 = (struct ip6_local_mbuf_data *)d_ctl->cb_data;
+	icmp6_echo = PAYLOAD(icmp6);
+	timestamp = PAYLOAD(icmp6_echo);
+
+	if ((resp = calloc(1, sizeof(*resp))) == NULL)
+		return api_out(ENOMEM, 0);
+	resp->src_addr = d_ip6->src;
+	resp->ttl = d_ip6->hop_limit;
+	resp->ident = rte_be_to_cpu_16(icmp6_echo->ident);
+	resp->seq_num = rte_be_to_cpu_16(icmp6_echo->seqnum);
+	resp->response_time = d_ctl->timestamp - *timestamp;
+	icmp6 = rte_pktmbuf_mtod(m, struct icmp6 *);
+	resp->type = icmp6->type;
+	resp->code = icmp6->code;
+
+	rte_pktmbuf_free(m);
+
+	*response = resp;
+
+	return api_out(ret, sizeof(*resp));
+}
+
+static struct gr_api_handler ip6_icmp_send_handler = {
+	.name = "icmp6 send",
+	.request_type = GR_IP6_ICMP6_SEND,
+	.callback = icmp6_send,
+};
+
+static struct gr_api_handler ip6_icmp_recv_handler = {
+	.name = "icmp6 recv",
+	.request_type = GR_IP6_ICMP6_RECV,
+	.callback = icmp6_recv,
+};
+
+#define ICMP6_LOCAL_QUEUE_SIZE 1024
+
+static void icmp_init(struct event_base *) {
+	pool = rte_mempool_create(
+		"icmp6_queue",
+		ICMP6_LOCAL_QUEUE_SIZE,
+		sizeof(struct icmp_queue_item),
+		0, // cache size
+		0, // priv size
+		NULL, // mp_init
+		NULL, // mp_init_arg
+		NULL, // obj_init
+		NULL, // obj_init_arg
+		SOCKET_ID_ANY,
+		0 // flags
+	);
+	if (pool == NULL)
+		ABORT("rte_mempool_create(icmp6_queue) failed");
+}
+
+static void icmp_fini(struct event_base *) {
+	if (pool != NULL) {
+		struct icmp_queue_item *i, *tmp;
+		STAILQ_FOREACH_SAFE (i, &icmp_queue, next, tmp)
+			icmp6_queue_pop(i, true);
+		rte_mempool_free(pool);
+		pool = NULL;
+	}
+}
+
+static struct gr_module icmp6_module = {
+	.name = "icmp6",
+	.init = icmp_init,
+	.fini = icmp_fini,
+};
+
+RTE_INIT(icmp_module_init) {
+	gr_register_module(&icmp6_module);
+	gr_register_api_handler(&ip6_icmp_send_handler);
+	gr_register_api_handler(&ip6_icmp_recv_handler);
+	icmp6_input_register_callback(ICMP6_TYPE_ECHO_REPLY, icmp6_input_cb);
+	icmp6_input_register_callback(ICMP6_ERR_DEST_UNREACH, icmp6_input_cb);
+	icmp6_input_register_callback(ICMP6_ERR_TTL_EXCEEDED, icmp6_input_cb);
+	icmp6_input_register_callback(ICMP6_ERR_PKT_TOO_BIG, icmp6_input_cb);
+	icmp6_input_register_callback(ICMP6_ERR_PARAM_PROBLEM, icmp6_input_cb);
+}

modules/ip6/cli/icmp6.c
@@ -1,0 +1,229 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Olivier Gournet
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_ip6.h>
+#include <gr_net_types.h>
+
+#include <ecoli.h>
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/queue.h>
+#include <unistd.h>
+
+static bool stop;
+
+static void sighandler(int) {
+	stop = true;
+}
+
+static cmd_status_t icmp_send(
+	const struct gr_api_client *c,
+	struct gr_ip6_icmp_send_req *req,
+	uint16_t msdelay,
+	uint16_t count,
+	bool mode_traceroute
+) {
+	struct gr_ip6_icmp_recv_resp *reply_resp;
+	struct gr_ip6_icmp_recv_req reply_req;
+	int i, timeout, ret, errors;
+	void *resp_ptr = NULL;
+	uint16_t ping_id;
+	const char *errdesc;
+
+	stop = false;
+	errors = 0;
+	errno = 0;
+	ping_id = random();
+
+	for (i = !!mode_traceroute; i < count && stop == false; i++) {
+		req->id = ping_id;
+		req->seq_num = i;
+		req->ttl = mode_traceroute ? i : 64;
+
+		ret = gr_api_client_send_recv(c, GR_IP6_ICMP6_SEND, sizeof(*req), req, &resp_ptr);
+		if (ret < 0)
+			return CMD_ERROR;
+		free(resp_ptr);
+		resp_ptr = NULL;
+
+		reply_req.id = ping_id;
+		reply_req.seq_num = i;
+		timeout = 50;
+		do {
+			usleep(10000);
+			ret = gr_api_client_send_recv(
+				c, GR_IP6_ICMP6_RECV, sizeof(reply_req), &reply_req, &resp_ptr
+			);
+			if (resp_ptr != NULL)
+				break;
+		} while (ret == 0 && --timeout > 0);
+
+		if (ret < 0)
+			return CMD_ERROR;
+
+		reply_resp = resp_ptr;
+		if (reply_resp == NULL || timeout == 0) {
+			errors++;
+			errno = ETIMEDOUT;
+			if (mode_traceroute)
+				printf("%2d  timeout\n", i);
+			else
+				printf("timeout: icmp_seq=%d\n", i);
+		} else {
+			switch (reply_resp->type) {
+			case ICMP6_TYPE_ECHO_REPLY:
+				if (!mode_traceroute) {
+					printf("reply from " IP6_F ": icmp_seq=%d ttl=%d "
+					       "time=%.3f ms\n",
+					       &reply_resp->src_addr,
+					       reply_resp->seq_num,
+					       reply_resp->ttl,
+					       reply_resp->response_time / 1000.);
+				} else {
+					printf("%2d  " IP6_F " time=%.3f ms\n",
+					       i,
+					       &reply_resp->src_addr,
+					       reply_resp->response_time / 1000.);
+					stop = true;
+					errors = 0;
+				}
+				errno = 0;
+				break;
+			case ICMP6_TYPE_ERR_DEST_UNREACH:
+				errno = EHOSTUNREACH;
+				errdesc = " destination unreachable";
+				break;
+			case ICMP6_TYPE_ERR_PKT_TOO_BIG:
+				errno = EINVAL;
+				errdesc = " packet too big";
+				break;
+			case ICMP6_TYPE_ERR_TTL_EXCEEDED:
+				errno = ETIMEDOUT;
+				errdesc = " ttl exceeded";
+				break;
+			case ICMP6_TYPE_ERR_PARAM_PROBLEM:
+			default:
+				errno = EINVAL;
+				errdesc = " parameter problem";
+				break;
+			}
+			if (errno && !mode_traceroute) {
+				errors++;
+				printf("reply from " IP6_F ": icmp_seq=%d ttl=%d:%s\n",
+				       &reply_resp->src_addr,
+				       reply_resp->seq_num,
+				       reply_resp->ttl,
+				       errdesc);
+			} else if (errno && mode_traceroute) {
+				errors++;
+				if (errno == ETIMEDOUT)
+					errdesc = "";
+				printf("%2d  " IP6_F " time=%.3f ms%s\n",
+				       i,
+				       &reply_resp->src_addr,
+				       reply_resp->response_time / 1000.,
+				       errdesc);
+			}
+			free(resp_ptr);
+			resp_ptr = NULL;
+		}
+		usleep(msdelay * 1000);
+	}
+
+	if (!stop && errors > 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t ping(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_icmp_send_req req = {.seq_num = 0, .vrf = 0};
+	cmd_status_t ret = CMD_ERROR;
+	uint16_t count = UINT16_MAX;
+	uint16_t msdelay = 1000;
+
+	if (inet_pton(AF_INET6, arg_str(p, "DEST"), &req.addr) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if ((ret = arg_u16(p, "VRF", &req.vrf)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+	if ((ret = arg_u16(p, "COUNT", &count)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+	if ((ret = arg_u16(p, "DELAY", &msdelay)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+
+	sighandler_t prev_handler = signal(SIGINT, sighandler);
+	if (prev_handler == SIG_ERR)
+		return CMD_ERROR;
+
+	ret = icmp_send(c, &req, msdelay, count, false);
+
+	signal(SIGINT, prev_handler);
+
+	return ret;
+}
+
+static cmd_status_t traceroute(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_ip6_icmp_send_req req = {.seq_num = 0, .vrf = 0};
+	cmd_status_t ret = CMD_SUCCESS;
+
+	if (inet_pton(AF_INET6, arg_str(p, "DEST"), &req.addr) != 1) {
+		errno = EINVAL;
+		return CMD_ERROR;
+	}
+	if ((ret = arg_u16(p, "VRF", &req.vrf)) < 0 && ret != ENOENT)
+		return CMD_ERROR;
+
+	sighandler_t prev_handler = signal(SIGINT, sighandler);
+	if (prev_handler == SIG_ERR)
+		return CMD_ERROR;
+
+	ret = icmp_send(c, &req, 0, 255, true);
+
+	signal(SIGINT, prev_handler);
+
+	return ret;
+}
+
+static int ctx_init(struct ec_node *root) {
+	int ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(
+			root, CTX_ARG("ping", "Send ICMPv6 echo requests and wait for replies.")
+		),
+		"DEST [vrf VRF] [count COUNT] [delay DELAY]",
+		ping,
+		"Send ICMPv6 echo requests and wait for replies.",
+		with_help("IPv6 destination address.", ec_node_re("DEST", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10)),
+		with_help("Number of packets to send.", ec_node_uint("COUNT", 1, UINT16_MAX, 10)),
+		with_help("Delay in ms between icmp6 echo.", ec_node_uint("DELAY", 0, 10000, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ARG("traceroute", "Discover IPv6 intermediate gateways.")),
+		"DEST [vrf VRF]",
+		traceroute,
+		"Discover IPv6 intermediate gateways.",
+		with_help("IPv6 destination address.", ec_node_re("DEST", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+
+	return ret;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "ping6",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

api/gr_net_compat.h
@@ -59,4 +59,12 @@ struct __attribute__((aligned(2))) rte_ether_addr {
 #define RTE_ICMP_CODE_REDIRECT_TOS_NET 2
 #define RTE_ICMP_CODE_REDIRECT_TOS_HOST 3
 
+/* ICMPv6 packet types */
+#define ICMP6_TYPE_ERR_DEST_UNREACH 1
+#define ICMP6_TYPE_ERR_PKT_TOO_BIG 2
+#define ICMP6_TYPE_ERR_TTL_EXCEEDED 3
+#define ICMP6_TYPE_ERR_PARAM_PROBLEM 4
+#define ICMP6_TYPE_ECHO_REQUEST 128
+#define ICMP6_TYPE_ECHO_REPLY 129
+
 #endif

modules/infra/datapath/gr_control_output.h
@@ -18,6 +18,7 @@ typedef void (*control_output_cb_t)(struct rte_mbuf *);
 GR_MBUF_PRIV_DATA_TYPE(control_output_mbuf_data, {
 	control_output_cb_t callback;
 	clock_t timestamp;
+	uint8_t cb_data[GR_MBUF_PRIV_MAX_SIZE - 6 * sizeof(size_t)];
 });
 
 // Enqueue a packet from data plane to a control plane ring.

modules/infra/datapath/gr_icmp6.h
@@ -31,6 +31,8 @@ typedef enum {
 	_ICMP6_TYPE_MAX = UINT8_C(0xff),
 } __rte_packed icmp6_type_t;
 
+#define GR_ICMP6_HDR_LEN 8
+
 // ICMP6 preamble
 struct icmp6 {
 	icmp6_type_t type;

modules/ip6/api/gr_ip6.h
@@ -133,6 +133,7 @@ struct gr_ip6_addr_list_resp {
 };
 
 // router advertisement ////////////////////////////////////////////////////////
+
 #define GR_IP6_IFACE_RA_SET REQUEST_TYPE(GR_IP6_MODULE, 0x0030)
 struct gr_ip6_ra_set_req {
 	uint16_t iface_id;
@@ -166,4 +167,36 @@ struct gr_ip6_ra_show_resp {
 	uint16_t n_ras;
 	struct gr_ip6_ra_conf ras[];
 };
+
+// icmpv6 ////////////////////////////////////////////////////////////////////////
+
+#define GR_IP6_ICMP6_SEND REQUEST_TYPE(GR_IP6_MODULE, 0x0041)
+
+struct gr_ip6_icmp_send_req {
+	struct rte_ipv6_addr addr;
+	uint16_t vrf;
+	uint16_t id;
+	uint16_t seq_num;
+	uint8_t ttl;
+};
+
+/* struct gr_ip6_icmp_send_resp { } */
+
+#define GR_IP6_ICMP6_RECV REQUEST_TYPE(GR_IP6_MODULE, 0x0042)
+
+struct gr_ip6_icmp_recv_req {
+	uint16_t id;
+	uint16_t seq_num;
+};
+
+struct gr_ip6_icmp_recv_resp {
+	uint8_t type;
+	uint8_t code;
+	uint8_t ttl;
+	uint16_t ident;
+	uint16_t seq_num;
+	struct rte_ipv6_addr src_addr;
+	clock_t response_time;
+};
+
 #endif

modules/ip6/cli/meson.build
@@ -6,4 +6,5 @@ cli_src += files(
   'nexthop.c',
   'route.c',
   'router_advert.c',
+  'icmp6.c',
 )

commit eabea1afee5bcb7cbb5d7163f9ba4ea8ef259b47

icmp6: do not discard non-matching packets from the queue
The free_and_skip label at the end of the loop in get_icmp6_echo_reply
causes all non-matching packets to be freed from the queue. This means
that when looking for a specific ident/seq_num pair, valid responses for
other concurrent pings are destroyed.

Only free invalid packets (too short, wrong protocol, wrong type) and
keep non-matching valid packets in the queue for subsequent lookups.

Fixes: 71a80f63665e ("ip6: add ping and traceroute command")
Signed-off-by: Robin Jarry <rjarry@redhat.com>

modules/ip6/control/icmp6.c
@@ -87,6 +87,7 @@ static struct rte_mbuf *get_icmp6_echo_reply(
 			return mbuf;
 		}
 
+		continue;
 free_and_skip:
 		icmp6_queue_pop(i, true);
 	}