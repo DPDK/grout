DPDK/grout

commit af07a70f9ee6b78bbce5b482bccdb98096d641a3

infra: sync linux control plane interfaces
Synchronize iface status and speed from Grout to the linux tap interface.
This allows Zebra to pickup the new speed when a link is reconfigured.

Signed-off-by: Christophe Fontaine <cfontain@redhat.com>
Reviewed-by: Maxime Leroy <maxime@leroys.fr>

modules/infra/control/ctlplane.c
@@ -241,7 +241,6 @@ static void cp_create(struct iface *iface) {
 	struct rte_ether_addr mac;
 	struct ifreq ifr;
 	int ioctl_sock;
-	int carrier;
 	int flags;
 
 	memset(&ifr, 0, sizeof(struct ifreq));
@@ -280,15 +279,9 @@ static void cp_create(struct iface *iface) {
 		goto err;
 	}
 
-	ifr.ifr_flags |= IFF_UP | IFF_NOARP;
+	ifr.ifr_flags |= IFF_NOARP;
 	if (ioctl(ioctl_sock, SIOCSIFFLAGS, &ifr) < 0) {
 		LOG(ERR, "ioctl(SIOCSIFFLAGS): %s", strerror(errno));
-		goto err;
-	}
-
-	carrier = 1;
-	if (ioctl(iface->cp_fd, TUNSETCARRIER, &carrier) < 0) {
-		LOG(ERR, "ioctl(TUNSETCARRIER): %s", strerror(errno));
 		goto err;
 	}
 
@@ -339,6 +332,54 @@ static void cp_delete(struct iface *iface) {
 		event_free_finalize(0, iface->cp_ev, finalize_fd);
 }
 
+static void cp_set_carrier(struct iface *iface) {
+	int carrier = iface->flags & GR_IFACE_S_RUNNING ? 1 : 0;
+	if (ioctl(iface->cp_fd, TUNSETCARRIER, &carrier) < 0) {
+		LOG(ERR, "ioctl(TUNSETCARRIER): %s", strerror(errno));
+	}
+}
+
+static void cp_set_speed(struct iface *iface) {
+	struct ethtool_link_settings *els;
+	struct ifreq ifr = {0};
+	char buf[512] = {0};
+	int ioctl_sock;
+
+	memccpy(ifr.ifr_name, iface->name, 0, IFNAMSIZ);
+	els = (struct ethtool_link_settings *)buf;
+
+	if ((ioctl_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		LOG(ERR, "socket(SOCK_DGRAM): %s", strerror(errno));
+		goto err;
+	}
+
+	ifr.ifr_data = (caddr_t)els;
+	els->cmd = ETHTOOL_GLINKSETTINGS;
+	if (ioctl(ioctl_sock, SIOCETHTOOL, &ifr) < 0) {
+		LOG(ERR, "ETHTOOL_GLINKSETTINGS: %s", strerror(errno));
+		goto err;
+	}
+	els->link_mode_masks_nwords = -els->link_mode_masks_nwords;
+	if (ioctl(ioctl_sock, SIOCETHTOOL, &ifr) < 0) {
+		LOG(ERR, "ETHTOOL_GLINKSETTINGS: %s", strerror(errno));
+		goto err;
+	}
+
+	els->speed = iface->speed;
+	els->duplex = DUPLEX_FULL;
+	els->autoneg = AUTONEG_DISABLE;
+	els->cmd = ETHTOOL_SLINKSETTINGS;
+
+	if (ioctl(ioctl_sock, SIOCETHTOOL, &ifr) < 0) {
+		LOG(ERR, "ETHTOOL_SLINKSETTINGS: %s", strerror(errno));
+		goto err;
+	}
+
+err:
+	if (ioctl_sock > 0)
+		close(ioctl_sock);
+}
+
 static void iface_event(uint32_t event, const void *obj) {
 	struct iface *iface = (struct iface *)obj;
 
@@ -358,15 +399,26 @@ static void iface_event(uint32_t event, const void *obj) {
 	case GR_EVENT_IFACE_PRE_REMOVE:
 		cp_delete(iface);
 		break;
+	case GR_EVENT_IFACE_STATUS_UP:
+		cp_set_speed(iface);
+		cp_set_carrier(iface);
+		netlink_link_set_admin_state(iface->name, true);
+		break;
+	case GR_EVENT_IFACE_STATUS_DOWN:
+		cp_set_carrier(iface);
+		netlink_link_set_admin_state(iface->name, false);
+		break;
 	}
 }
 
 static struct gr_event_subscription iface_event_handler = {
 	.callback = iface_event,
-	.ev_count = 2,
+	.ev_count = 4,
 	.ev_types = {
 		GR_EVENT_IFACE_ADD,
 		GR_EVENT_IFACE_PRE_REMOVE,
+		GR_EVENT_IFACE_STATUS_UP,
+		GR_EVENT_IFACE_STATUS_DOWN,
 	},
 };
 

commit 7c564c664e58b2515340dab085eb2ef8715d9b4d

ctlplane: skip speed config when TAP is not yet created
When a VLAN interface is created and its parent is already running,
iface_vlan_reconfig pushes a STATUS_UP event during type->init which
runs before IFACE_ADD is fired from iface_add. This causes cp_set_speed
to run before cp_create has created the TAP representor, resulting in
a spurious error in the logs:

  ERROR: cp_set_speed: ETHTOOL_GLINKSETTINGS: No such device

Guard cp_set_speed with a cp_id check like cp_update already does. The
second STATUS_UP event pushed by iface_set_up_down after IFACE_ADD will
set the speed correctly.

Fixes: af07a70f9ee6 ("infra: sync linux control plane interfaces")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: David Marchand <david.marchand@redhat.com>

modules/infra/control/ctlplane.c
@@ -371,6 +371,9 @@ static void cp_set_speed(struct iface *iface) {
 	char buf[512] = {0};
 	int ioctl_sock;
 
+	if (iface->cp_id == 0)
+		return;
+
 	gr_strcpy(ifr.ifr_name, IFNAMSIZ, iface->name);
 	els = (struct ethtool_link_settings *)buf;
 