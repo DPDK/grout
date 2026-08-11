DPDK/grout

commit 10be5b5883ce3dbd7e1a1e7313fabd0e054b21c8

l2: push external mac addresses in hardware
In setups where grout physical port is a VF, traffic sent by other
VFs reach grout only if the destination address is multicast or a mac
address for which a rx filter was declared in hw.

Make use of control path pushed FDB entries to populate hardware and
let the packets flow.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Robin Jarry <rjarry@redhat.com>

modules/l2/control/bridge.c
@@ -58,6 +58,8 @@ static int bridge_attach_member(struct iface *bridge, struct iface *member) {
 	member->vrf_id = GR_VRF_ID_UNDEF;
 	member->mode = GR_IFACE_MODE_BRIDGE;
 
+	fdb_sync_hardware(bridge, member, true);
+
 	return 0;
 }
 
@@ -67,6 +69,9 @@ static int bridge_detach_member(struct iface *bridge, struct iface *member) {
 	for (unsigned i = 0; i < br->n_members; i++) {
 		if (br->members[i] == member) {
 			unsigned last = br->n_members - 1;
+
+			fdb_sync_hardware(bridge, member, false);
+
 			if (i < last)
 				br->members[i] = br->members[last];
 			br->n_members--;
@@ -86,6 +91,7 @@ static int bridge_fini(struct iface *iface) {
 
 	for (unsigned i = 0; i < bridge->n_members; i++) {
 		struct iface *member = bridge->members[i];
+		fdb_sync_hardware(iface, member, false);
 		iface_set_promisc(member, false);
 		member->vrf_id = vrf_default_get_or_create();
 		if (member->vrf_id != GR_VRF_ID_UNDEF)

modules/l2/control/fdb.c
@@ -332,6 +332,67 @@ static struct api_out fdb_config_set(const void *request, struct api_ctx *) {
 	return api_out(0, 0, NULL);
 }
 
+static void push_mac_to_hw(struct iface *iface, const struct rte_ether_addr *mac, bool add) {
+	int ret;
+
+	if (add)
+		ret = iface_add_eth_addr(iface, mac);
+	else
+		ret = iface_del_eth_addr(iface, mac);
+	if (ret < 0) {
+		LOG(DEBUG,
+		    "failed to %s mac " ETH_F " to %s: %s",
+		    add ? "add" : "del",
+		    mac,
+		    iface->name,
+		    strerror(errno));
+	}
+}
+
+static void fdb_event_cb(uint32_t event, const void *obj) {
+	const struct iface_info_bridge *bridge_info;
+	const struct gr_fdb_entry *fdb = obj;
+	const struct iface *bridge;
+
+	bridge = iface_from_id(fdb->bridge_id);
+	if (bridge == NULL) {
+		LOG(ERR, "unknown bridge %u", fdb->bridge_id);
+		return;
+	}
+
+	if ((fdb->flags & GR_FDB_F_EXTERN) == 0)
+		return;
+
+	bridge_info = iface_info_bridge(bridge);
+	for (unsigned i = 0; i < bridge_info->n_members; i++) {
+		struct iface *member = bridge_info->members[i];
+
+		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
+		assert(fdb->vlan_id == 0);
+		// FIXME: ideally, we should ask for adding the mac in hw only when getting the
+		// *first* transition from !EXTERN to EXTERN to handle both learning mac on the
+		// wire and from FRR.
+		push_mac_to_hw(member, &fdb->mac, event != GR_EVENT_FDB_DEL);
+	}
+}
+
+void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add) {
+	struct gr_fdb_entry *fdb;
+	uint32_t next = 0;
+	const void *key;
+	void *data;
+
+	while (rte_hash_iterate(fdb_hash, &key, &data, &next) >= 0) {
+		if (!fdb_match(data, GR_FDB_F_EXTERN, bridge->id, GR_IFACE_ID_UNDEF, NULL))
+			continue;
+
+		fdb = data;
+		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
+		assert(fdb->vlan_id == 0);
+		push_mac_to_hw(member, &fdb->mac, add);
+	}
+}
+
 static void fdb_ageing_cb(evutil_socket_t, short /*what*/, void * /*priv*/) {
 	const struct iface *bridge;
 	struct gr_fdb_entry *fdb;
@@ -409,6 +470,8 @@ RTE_INIT(init) {
 	api_handler(GR_FDB_LIST, fdb_list);
 	api_handler(GR_FDB_CONFIG_GET, fdb_config_get);
 	api_handler(GR_FDB_CONFIG_SET, fdb_config_set);
+	event_subscribe(GR_EVENT_FDB_ADD, fdb_event_cb);
+	event_subscribe(GR_EVENT_FDB_DEL, fdb_event_cb);
 	event_serializer(GR_EVENT_FDB_ADD, NULL);
 	event_serializer(GR_EVENT_FDB_DEL, NULL);
 	event_serializer(GR_EVENT_FDB_UPDATE, NULL);

modules/l2/control/l2.h
@@ -39,6 +39,9 @@ void fdb_learn(
 // Delete all FDB entries referencing the provided interface.
 void fdb_purge_iface(uint16_t iface_id);
 
+// Push or remove mac addresses from HW Rx filters.
+void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add);
+
 // Delete all FDB entries referencing the provided bridge.
 void fdb_purge_bridge(uint16_t bridge_id);
 

commit 1c7a4f040cf711de0530faf352345dc0221d8140

fdb: purge entries and remove HW filters on iface deletion
Subscribe to GR_EVENT_IFACE_REMOVE to delete all FDB entries when
a bridge or member interface is removed.

The previous fdb_purge_bridge() call in bridge_fini() was ineffective:
iface_destroy() clears ifaces[iface->id] before calling fini(), so
iface_from_id() could not resolve the bridge anymore and the deferred
RCU free callback (fdb_event_cb) silently failed to remove HW filters.

Move FDB cleanup to a GR_EVENT_IFACE_REMOVE subscriber where we still
have the iface pointer from the event. Remove HW MAC filters inline
before calling rte_hash_del_key() since the deferred RCU free path
through fdb_event_cb cannot look up the iface either. Make fdb_event_cb
tolerate a NULL bridge lookup since the HW cleanup is already done at
that point.

Fixes: 10be5b5883ce ("l2: push external mac addresses in hardware")
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/l2/control/bridge.c
@@ -139,8 +139,6 @@ static int bridge_fini(struct iface *iface) {
 		event_push(GR_EVENT_IFACE_POST_RECONFIG, member);
 	}
 
-	fdb_purge_bridge(iface->id);
-
 	return 0;
 }
 

modules/l2/control/fdb.c
@@ -173,20 +173,6 @@ void fdb_purge_iface(uint16_t iface_id) {
 	}
 }
 
-void fdb_purge_bridge(uint16_t bridge_id) {
-	struct fdb_entry *fdb;
-	uint32_t next = 0;
-	const void *key;
-	void *data;
-
-	while (rte_hash_iterate(fdb_hash, &key, &data, &next) >= 0) {
-		fdb = data;
-		if (fdb->bridge_id == bridge_id) {
-			rte_hash_del_key(fdb_hash, key);
-		}
-	}
-}
-
 static struct api_out fdb_add(const void *request, struct api_ctx *) {
 	const struct gr_fdb_add_req *req = request;
 	const struct iface *iface;
@@ -368,11 +354,11 @@ static void fdb_event_cb(uint32_t event, const void *obj) {
 	const struct iface *bridge;
 	struct iface *member;
 
+	// The bridge may have been removed already. HW filters were cleaned up
+	// in fdb_iface_del_cb before the RCU deferred free triggered this event.
 	bridge = iface_from_id(fdb->bridge_id);
-	if (bridge == NULL) {
-		LOG(ERR, "unknown bridge %u", fdb->bridge_id);
+	if (bridge == NULL)
 		return;
-	}
 
 	// we have no clear idea what to do with a vlan_id if one got pushed by FRR
 	assert(fdb->vlan_id == 0);
@@ -420,6 +406,41 @@ void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool ad
 		// we have no clear idea what to do with a vlan_id if one got pushed by FRR
 		assert(fdb->vlan_id == 0);
 		push_mac_to_hw(member, &fdb->mac, add);
+	}
+}
+
+// Remove HW filters before deleting FDB entries. rte_hash_del_key() triggers
+// a deferred RCU free (fdb_free_entry -> fdb_event_cb) which would need
+// iface_from_id() to find the bridge/member, but at this point the iface is
+// already removed from the global array by iface_destroy().
+static void fdb_remove_hw_filters(const struct iface *bridge, const struct fdb_entry *fdb) {
+	const struct iface_info_bridge *br = iface_info_bridge(bridge);
+	for (unsigned i = 0; i < br->n_members; i++) {
+		if (br->members[i]->id != fdb->iface_id)
+			push_mac_to_hw(br->members[i], &fdb->mac, false);
+	}
+}
+
+static void fdb_iface_del_cb(uint32_t /*event*/, const void *obj) {
+	const struct iface *iface = obj;
+	const struct iface *bridge;
+	struct fdb_entry *fdb;
+	uint32_t next = 0;
+	const void *key;
+	void *data;
+
+	while (rte_hash_iterate(fdb_hash, &key, &data, &next) >= 0) {
+		fdb = data;
+
+		if (iface->type == GR_IFACE_TYPE_BRIDGE && fdb->bridge_id == iface->id) {
+			fdb_remove_hw_filters(iface, fdb);
+			rte_hash_del_key(fdb_hash, key);
+		} else if (fdb->iface_id == iface->id) {
+			bridge = iface_from_id(fdb->bridge_id);
+			if (bridge != NULL)
+				fdb_remove_hw_filters(bridge, fdb);
+			rte_hash_del_key(fdb_hash, key);
+		}
 	}
 }
 
@@ -503,6 +524,7 @@ RTE_INIT(init) {
 	event_subscribe(GR_EVENT_FDB_ADD, fdb_event_cb);
 	event_subscribe(GR_EVENT_FDB_DEL, fdb_event_cb);
 	event_subscribe(GR_EVENT_FDB_UPDATE, fdb_event_cb);
+	event_subscribe(GR_EVENT_IFACE_REMOVE, fdb_iface_del_cb);
 	event_serializer(GR_EVENT_FDB_ADD, NULL);
 	event_serializer(GR_EVENT_FDB_DEL, NULL);
 	event_serializer(GR_EVENT_FDB_UPDATE, NULL);

modules/l2/control/l2.h
@@ -42,9 +42,6 @@ void fdb_purge_iface(uint16_t iface_id);
 // Push or remove mac addresses from HW Rx filters.
 void fdb_sync_hardware(const struct iface *bridge, struct iface *member, bool add);
 
-// Delete all FDB entries referencing the provided bridge.
-void fdb_purge_bridge(uint16_t bridge_id);
-
 struct vxlan_template_ipv4 {
 	struct rte_ipv4_hdr ip;
 	struct rte_udp_hdr udp;