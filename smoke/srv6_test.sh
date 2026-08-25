DPDK/grout

commit 7d88a1443587946750806257faf73ae234989e87

srv6: add srv6 module
Add Segment Routing (SR) over IPv6 (SRv6). SR over MPLS is not addressed.

Endpoints, SR policies and steering rules can be configured as static rules
through api/cli. Integration with BGP/IGP is future work.

This patch addresses part of the following RFC:

RFC8402: Segment Routing Architecture
	https://www.rfc-editor.org/rfc/rfc8402

RFC8754: IPv6 Segment Routing Header (SRH)
	https://www.rfc-editor.org/rfc/rfc8754

RFC8986: Segment Routing over IPv6 (SRv6) Network Programming
	https://www.rfc-editor.org/rfc/rfc8986

Headend node implements H.Encaps and H.Encaps.Red.
Local node implements End, End.T (with PSP, USD flavors), End.DT6,
End.DT4 and End.DT46.

Signed-off-by: Olivier Gournet <gournet.olivier@gmail.com>

smoke/srv6_test.sh
@@ -1,0 +1,77 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2025 Olivier Gournet
+
+. $(dirname $0)/_init.sh
+
+p0=${run_id}0
+p1=${run_id}1
+
+ip netns del $p0 2> /dev/null || true
+ip netns del $p1 2> /dev/null || true
+ip netns add $p0
+ip netns add $p1
+
+# setup ports and connected
+grcli add interface port $p0 devargs net_tap0,iface=$p0 mac d2:f0:0c:ba:a5:10
+grcli add interface port $p1 devargs net_tap1,iface=$p1 mac d2:f0:0c:ba:a5:11
+grcli add ip6 address fd00:101::1/64 iface $p0
+grcli add ip6 address fd00:102::1/64 iface $p1
+grcli add ip address 192.168.61.1/24 iface $p0
+grcli add ip address 192.168.62.1/24 iface $p1
+ip link set $p0 netns $p0
+ip -n $p0 link set $p0 address d2:ad:ca:fe:b4:10
+ip -n $p0 link set lo up
+ip -n $p0 link set $p0 up
+ip -n $p0 addr add fd00:101::2/64 dev $p0
+ip -n $p0 addr add 192.168.61.2/24 dev $p0
+ip link set $p1 netns $p1
+ip -n $p1 link set $p1 address d2:ad:ca:fe:b4:11
+ip -n $p1 link set lo up
+ip -n $p1 link set $p1 up
+ip -n $p1 addr add fd00:102::2/64 dev $p1
+ip -n $p1 addr add 192.168.62.2/24 dev $p1
+
+sleep 3
+
+#
+# network layout:
+#  (client) p0(netns) <--> p0 <grout> p1 <--->  p1(netns) (public: 192.168.60.1/24 on p0)
+#       ipv4 ---------------|        srv6        |-- ipv4
+#
+# test case:
+#   - (1) send ipv4 ping from p0
+#   - (2) grout encap in srv6, send to sid fd00:202::2
+#   - (3) linux p1 decap it
+#   - (4) reply to ping
+#   - (5) linux p1 reencap in srv6, send to grout sid fd00:202::100,
+#   - (6) grout decap it, reply back in ipv4 to p0
+#
+
+# only linux's p1 will see srv6
+ip netns exec $p1 sysctl -w net.ipv6.conf.$p1.seg6_enabled=1
+ip netns exec $p1 sysctl -w net.ipv6.conf.$p1.forwarding=1
+
+# (1) send ipv4 to grout
+ip -n $p0 route add default via 192.168.61.1 dev $p0
+
+# (2)
+grcli add sr policy bsid fd00:202::200 seglist fd00:202::2
+grcli add sr steer 192.168.0.0/16 bsid fd00:202::200
+grcli add ip6 route fd00:202::/64 via fd00:102::2
+
+# (3)
+ip -n $p1 -6 route add fd00:202::2 encap seg6local action End.DX4 nh4 192.168.60.1 count dev $p1
+
+# (4) 192.168.60.0/24 is our 'public' network
+ip -n $p1 addr add 192.168.60.1/24 dev $p1
+
+# (5)
+ip -n $p1 route add 192.168.61.0/24 encap seg6 mode encap segs fd00:202::100 dev $p1
+ip -n $p1 -6 route add fd00:202::/64 via fd00:102::1 dev $p1
+
+# (6)
+grcli add sr localsid fd00:202::100 behavior end.dt4
+
+# test
+ip netns exec $p0 ping -c 3 192.168.60.1

modules/infra/api/gr_nexthop.h
@@ -26,6 +26,8 @@ typedef enum : uint16_t {
 typedef enum : uint8_t {
 	GR_NH_IPV4 = 1,
 	GR_NH_IPV6,
+	GR_NH_SR6_IPV4,
+	GR_NH_SR6_IPV6,
 	GR_NH_TYPE_COUNT
 } gr_nh_type_t;
 
@@ -82,8 +84,10 @@ static inline const char *gr_nh_flag_name(const gr_nh_flags_t flag) {
 static inline uint8_t nh_af(const struct gr_nexthop *nh) {
 	switch (nh->type) {
 	case GR_NH_IPV4:
+	case GR_NH_SR6_IPV4:
 		return AF_INET;
 	case GR_NH_IPV6:
+	case GR_NH_SR6_IPV6:
 		return AF_INET6;
 	case GR_NH_TYPE_COUNT:
 		break;

modules/infra/control/nexthop.c
@@ -48,9 +48,11 @@ nexthop_new(gr_nh_type_t type, uint16_t vrf_id, uint16_t iface_id, const void *a
 	nh->type = type;
 	switch (type) {
 	case GR_NH_IPV4:
+	case GR_NH_SR6_IPV4:
 		nh->ipv4 = *(ip4_addr_t *)addr;
 		break;
 	case GR_NH_IPV6:
+	case GR_NH_SR6_IPV6:
 		nh->ipv6 = *(struct rte_ipv6_addr *)addr;
 		break;
 	default:
@@ -101,10 +103,12 @@ static void nh_lookup_cb(struct nexthop *nh, void *priv) {
 
 	switch (filter->type) {
 	case GR_NH_IPV4:
+	case GR_NH_SR6_IPV4:
 		if (nh->ipv4 == *(ip4_addr_t *)filter->addr)
 			filter->nh = nh;
 		break;
 	case GR_NH_IPV6:
+	case GR_NH_SR6_IPV6:
 		if (rte_ipv6_addr_eq(&nh->ipv6, filter->addr)) {
 			bool is_linklocal = rte_ipv6_addr_is_linklocal(&nh->ipv6);
 			if (!is_linklocal || nh->iface_id == filter->iface_id)

modules/meson.build
@@ -6,3 +6,4 @@ subdir('ip')
 subdir('ip6')
 subdir('ipip')
 subdir('l4')
+subdir('srv6')

modules/srv6/cli.c
@@ -1,0 +1,572 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025 Olivier Gournet
+
+#include <gr_api.h>
+#include <gr_cli.h>
+#include <gr_net_types.h>
+#include <gr_srv6.h>
+#include <gr_table.h>
+
+#include <ecoli.h>
+#include <libsmartcols.h>
+
+#include <errno.h>
+
+// sr policy ////////////////////////////////////////////////////////////////
+
+static cmd_status_t srv6_policy_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_policy_add_req *req;
+	const struct ec_strvec *v;
+	const struct ec_pnode *n;
+	const char *str;
+	size_t len;
+	int ret, i;
+
+	// get SEGLIST sequence node. it is the parent of the first SEGLIST node.
+	n = ec_pnode_find(p, "SEGLIST");
+	if (n == NULL || (n = ec_pnode_get_parent(n)) == NULL || ec_pnode_len(n) < 1)
+		return CMD_ERROR;
+	if (ec_pnode_len(n) > GR_SRV6_POLICY_SEGLIST_COUNT_MAX)
+		return CMD_ERROR;
+	len = sizeof(*req) + sizeof(req->p.seglist[0]) * ec_pnode_len(n);
+	if ((req = calloc(1, len)) == NULL)
+		return CMD_ERROR;
+	req->p.n_seglist = ec_pnode_len(n);
+
+	// parse SEGLIST list.
+	for (n = ec_pnode_get_first_child(n), i = 0; n != NULL; n = ec_pnode_next(n), i++) {
+		v = ec_pnode_get_strvec(n);
+		str = ec_strvec_val(v, 0);
+		if (inet_pton(AF_INET6, str, &req->p.seglist[i]) != 1) {
+			free(req);
+			return CMD_ERROR;
+		}
+	}
+
+	if (arg_ip6(p, "BSID", &req->p.bsid) < 0) {
+		free(req);
+		return CMD_ERROR;
+	}
+
+	req->p.weight = 1;
+	if (arg_u16(p, "WEIGHT", &req->p.weight) < 0 && errno != ENOENT) {
+		free(req);
+		return CMD_ERROR;
+	}
+
+	req->p.encap_behavior = SR_H_ENCAPS;
+	if (ec_pnode_find(p, "h.encaps.red") != NULL)
+		req->p.encap_behavior = SR_H_ENCAPS_RED;
+
+	ret = gr_api_client_send_recv(c, GR_SRV6_POLICY_ADD, len, req, NULL);
+	free(req);
+
+	return ret < 0 ? CMD_ERROR : CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_policy_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_policy_del_req req = {};
+
+	if (arg_ip6(p, "BSID", &req.bsid) < 0)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_POLICY_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t
+srv6_policy_show(const struct gr_api_client *c, const struct ec_pnode * /* p */) {
+	struct libscols_table *table = scols_new_table();
+	struct gr_srv6_policy_list_resp *resp;
+	void *ptr, *resp_ptr = NULL;
+	struct libscols_line *line;
+	struct gr_srv6_policy *d;
+	uint32_t j, cur, n;
+	char buf[80];
+	int i, ret;
+
+	ret = gr_api_client_send_recv(c, GR_SRV6_POLICY_LIST, 0, NULL, &resp_ptr);
+	if (ret < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+
+	scols_table_new_column(table, "bsid", 0, 0);
+	scols_table_new_column(table, "encap", 0, 0);
+	scols_table_new_column(table, "weight", 0, 0);
+	scols_table_new_column(table, "segment list", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	ptr = resp->policy;
+	for (i = 0; i < resp->n_policy; i++) {
+		line = scols_table_new_line(table, NULL);
+		d = ptr;
+
+		scols_line_sprintf(line, 0, IP6_F, &d->bsid);
+		scols_line_sprintf(
+			line,
+			1,
+			"%s",
+			d->encap_behavior == SR_H_ENCAPS_RED ? "h.encaps.red" : "h.encap"
+		);
+		scols_line_sprintf(line, 2, "%d", d->weight);
+
+		cur = 0;
+		buf[0] = 0;
+		for (j = 0; j < d->n_seglist; j++) {
+			n = snprintf(buf + cur, sizeof(buf) - cur - 20, IP6_F " ", &d->seglist[j]);
+			if (n > sizeof(buf) - cur - 20) {
+				sprintf(buf + sizeof(buf) - 21, "...");
+				if (j + 1 < d->n_seglist)
+					snprintf(
+						buf + sizeof(buf) - 18,
+						18,
+						" (%d more)",
+						d->n_seglist - j - 1
+					);
+				break;
+			}
+			cur += n;
+		}
+		scols_line_set_data(line, 3, buf);
+
+		ptr += sizeof(*d) + d->n_seglist * sizeof(d->seglist[0]);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+// steer rules ////////////////////////////////////////////////////////////////
+
+static cmd_status_t srv6_steer_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_steer_add_req req = {};
+
+	if (arg_ip6(p, "BSID", &req.bsid) < 0)
+		return CMD_ERROR;
+
+	if (arg_ip6_net(p, "DEST6", &req.l3.dest6, true) >= 0)
+		req.l3.is_dest6 = true;
+	else if (arg_ip4_net(p, "DEST4", &req.l3.dest4, true) >= 0)
+		req.l3.is_dest6 = false;
+	else
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.l3.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_STEER_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_steer_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_steer_del_req req = {};
+
+	if (arg_ip6(p, "BSID", &req.bsid) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (arg_ip6_net(p, "DEST6", &req.l3.dest6, true) >= 0)
+		req.l3.is_dest6 = true;
+	else if (arg_ip4_net(p, "DEST4", &req.l3.dest4, true) >= 0)
+		req.l3.is_dest6 = false;
+	else
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.l3.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_STEER_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_steer_show(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_steer_list_req req = {.vrf_id = UINT16_MAX};
+	struct libscols_table *table = scols_new_table();
+	struct gr_srv6_steer_list_resp *resp;
+	struct gr_srv6_steer_entry *e;
+	struct libscols_line *line;
+	void *ptr, *resp_ptr = NULL;
+	int i, j, ret;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	ret = gr_api_client_send_recv(c, GR_SRV6_STEER_LIST, sizeof(req), &req, &resp_ptr);
+	if (ret < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+
+	scols_table_new_column(table, "vrf", 0, 0);
+	scols_table_new_column(table, "match", 0, 0);
+	scols_table_new_column(table, "bsid", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	ptr = resp->steer;
+	for (i = 0; i < resp->n_steer; i++) {
+		line = scols_table_new_line(table, NULL);
+		e = ptr;
+
+		scols_line_sprintf(line, 0, "%u", e->l3.vrf_id);
+		if (e->l3.is_dest6)
+			scols_line_sprintf(
+				line, 1, IP6_F "/%hhu", &e->l3.dest6.ip, e->l3.dest6.prefixlen
+			);
+		else
+			scols_line_sprintf(
+				line, 1, IP4_F "/%hhu", &e->l3.dest4.ip, e->l3.dest6.prefixlen
+			);
+
+		scols_line_sprintf(line, 2, IP6_F, &e->bsid[0]);
+		for (j = 1; j < e->n_bsid; j++) {
+			line = scols_table_new_line(table, NULL);
+			scols_line_sprintf(line, 2, IP6_F, &e->bsid[j]);
+		}
+
+		ptr += sizeof(*e) + e->n_bsid * sizeof(e->bsid[0]);
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+// localsid /////////////////////////////////////////////////////////////
+
+static const char *behavior_str[SR_BEHAVIOR_MAX] = {
+	[SR_BEHAVIOR_END] = "end",
+	[SR_BEHAVIOR_END_T] = "end.t",
+	[SR_BEHAVIOR_END_DT6] = "end.dt6",
+	[SR_BEHAVIOR_END_DT4] = "end.dt4",
+	[SR_BEHAVIOR_END_DT46] = "end.dt46",
+};
+
+static const char *behavior_to_str(gr_srv6_behavior_t b) {
+	return behavior_str[b];
+}
+
+static gr_srv6_behavior_t str_to_behavior(const char *str) {
+	int i;
+
+	if (str == NULL)
+		return SR_BEHAVIOR_MAX;
+	for (i = 0; i < SR_BEHAVIOR_MAX; i++) {
+		if (behavior_str[i] != NULL && !strcmp(str, behavior_str[i]))
+			return i;
+	}
+	return SR_BEHAVIOR_MAX;
+}
+
+static cmd_status_t srv6_localsid_add(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_localsid_add_req req = {.l.out_vrf_id = UINT16_MAX};
+	const struct ec_pnode *n;
+	const struct ec_strvec *v;
+	const char *str;
+	uint32_t i;
+
+	if (arg_ip6(p, "SID", &req.l.lsid) < 0)
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.l.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	n = ec_pnode_find(p, "FLAVORS");
+	if (n != NULL) {
+		v = ec_pnode_get_strvec(n);
+		for (i = 0; i < ec_strvec_len(v); i++) {
+			str = ec_strvec_val(v, i);
+			if (!strcmp(str, "psp"))
+				req.l.flags |= GR_SR_FL_FLAVOR_PSP;
+			if (!strcmp(str, "usd"))
+				req.l.flags |= GR_SR_FL_FLAVOR_USD;
+		}
+	}
+
+	n = ec_pnode_find(p, "BEHAVIOR");
+	if (n == NULL || ec_pnode_len(n) < 1)
+		return CMD_ERROR;
+	req.l.behavior = str_to_behavior(ec_strvec_val(ec_pnode_get_strvec(n), 0));
+	if (req.l.behavior == SR_BEHAVIOR_MAX)
+		return CMD_ERROR;
+
+	if (arg_u16(n, "TABLE", &req.l.out_vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_LOCALSID_ADD, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_localsid_del(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_localsid_del_req req = {.vrf_id = 0};
+
+	if (arg_ip6(p, "SID", &req.lsid) < 0)
+		return CMD_ERROR;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	if (gr_api_client_send_recv(c, GR_SRV6_LOCALSID_DEL, sizeof(req), &req, NULL) < 0)
+		return CMD_ERROR;
+
+	return CMD_SUCCESS;
+}
+
+static cmd_status_t srv6_localsid_show(const struct gr_api_client *c, const struct ec_pnode *p) {
+	struct gr_srv6_localsid_list_req req = {.vrf_id = UINT16_MAX};
+	struct libscols_table *table = scols_new_table();
+	struct gr_srv6_localsid_list_resp *resp;
+	struct gr_srv6_localsid *lsid;
+	struct libscols_line *line;
+	char vrf_buf[100];
+	void *resp_ptr = NULL;
+	int ret, i;
+
+	if (arg_u16(p, "VRF", &req.vrf_id) < 0 && errno != ENOENT)
+		return CMD_ERROR;
+
+	ret = gr_api_client_send_recv(c, GR_SRV6_LOCALSID_LIST, sizeof(req), &req, &resp_ptr);
+	if (ret < 0)
+		return CMD_ERROR;
+
+	resp = resp_ptr;
+
+	scols_table_new_column(table, "vrf", 0, 0);
+	scols_table_new_column(table, "lsid", 0, 0);
+	scols_table_new_column(table, "behavior", 0, 0);
+	scols_table_new_column(table, "args", 0, 0);
+	scols_table_set_column_separator(table, "  ");
+
+	for (i = 0; i < resp->n_lsid; i++) {
+		line = scols_table_new_line(table, NULL);
+		lsid = &resp->lsid[i];
+
+		vrf_buf[0] = 0;
+		if (lsid->out_vrf_id < UINT16_MAX)
+			sprintf(vrf_buf, "out_vrf=%d", lsid->out_vrf_id);
+
+		scols_line_sprintf(line, 0, "%u", lsid->vrf_id);
+		scols_line_sprintf(line, 1, IP6_F, &lsid->lsid);
+		scols_line_sprintf(line, 2, "%s", behavior_to_str(lsid->behavior));
+		switch (lsid->behavior) {
+		case SR_BEHAVIOR_END:
+			scols_line_sprintf(
+				line, 3, "flavor=0x%02x", lsid->flags & GR_SR_FL_FLAVOR_MASK
+			);
+			break;
+		case SR_BEHAVIOR_END_T:
+			scols_line_sprintf(
+				line,
+				3,
+				"flavor=0x%02x %s",
+				lsid->flags & GR_SR_FL_FLAVOR_MASK,
+				vrf_buf
+			);
+			break;
+		case SR_BEHAVIOR_END_DT6:
+		case SR_BEHAVIOR_END_DT4:
+		case SR_BEHAVIOR_END_DT46:
+			scols_line_sprintf(line, 3, "%s", vrf_buf);
+			break;
+		default:
+			break;
+		}
+	}
+
+	scols_print_table(table);
+	scols_unref_table(table);
+	free(resp_ptr);
+
+	return CMD_SUCCESS;
+}
+
+static int ctx_init(struct ec_node *root) {
+	struct ec_node *beh_node, *flavor_node;
+	int ret;
+
+	// policy commands
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ADD, CTX_ARG("sr", "Create srv6 stack elements.")),
+		"policy bsid BSID seglist SEGLIST+ [encap (h.encaps|h.encaps.red)] [weight WEIGHT]",
+		srv6_policy_add,
+		"Add SR policy rule.",
+		with_help("Policy weight.", ec_node_uint("WEIGHT", 1, UINT16_MAX, 10)),
+		with_help("Encaps.", ec_node_str("h.encaps", "h.encaps")),
+		with_help("Encaps Reduced.", ec_node_str("h.encaps.red", "h.encaps.red")),
+		with_help("Binding SID.", ec_node_re("BSID", IPV6_RE)),
+		with_help("Next SID to visit.", ec_node_re("SEGLIST", IPV6_RE))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_DEL, CTX_ARG("sr", "Delete srv6 stack elements.")),
+		"policy bsid BSID",
+		srv6_policy_del,
+		"Delete SR policy rule.",
+		with_help("Binding SID.", ec_node_re("BSID", IPV6_RE))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("sr", "Show srv6 stack elements.")),
+		"policy",
+		srv6_policy_show,
+		"View all SR policy rules"
+	);
+	if (ret < 0)
+		return ret;
+
+	// steer commands
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ADD, CTX_ARG("sr", "Create srv6 stack elements.")),
+		"steer DEST4|DEST6 bsid BSID [vrf VRF]",
+		srv6_steer_add,
+		"Add rule to encap traffic from ipv4/ipv6 prefix (tunnel entry).",
+		with_help("Ipv4 destination prefix to steer", ec_node_re("DEST4", IPV4_NET_RE)),
+		with_help("Ipv6 destination prefix to steer", ec_node_re("DEST6", IPV6_NET_RE)),
+		with_help("Binding SID.", ec_node_re("BSID", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_DEL, CTX_ARG("sr", "Delete srv6 stack elements.")),
+		"steer DEST4|DEST6 [vrf VRF]",
+		srv6_steer_del,
+		"Delete an encap rule.",
+		with_help("Ipv4 destination prefix to steer", ec_node_re("DEST4", IPV4_NET_RE)),
+		with_help("Ipv6 destination prefix to steer", ec_node_re("DEST6", IPV6_NET_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("sr", "Show srv6 stack elements.")),
+		"steer [vrf VRF]",
+		srv6_steer_show,
+		"View all encap rules",
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	// localsid commands
+	flavor_node = EC_NODE_CMD(
+		"FLAVORS",
+		"FLAVOR+",
+		EC_NODE_OR(
+			"FLAVOR",
+			EC_NODE_CMD(
+				EC_NO_ID,
+				"psp",
+				with_help(
+					"Penultimate Segment Pop of the SRH",
+					ec_node_str("psp", "psp")
+				)
+			),
+			EC_NODE_CMD(
+				EC_NO_ID,
+				"usd",
+				with_help(
+					"Ultimate Segment Decapsulation of the SRH",
+					ec_node_str("usd", "usd")
+				)
+			)
+		)
+	);
+	beh_node = EC_NODE_OR(
+		"BEHAVIOR",
+		EC_NODE_CMD(
+			EC_NO_ID,
+			"end [flavor FLAVORS]",
+			with_help("Transit endpoint.", ec_node_str("end", "end")),
+			with_help("Endpoint flavor(s).", ec_node_clone(flavor_node))
+		),
+		EC_NODE_CMD(
+			EC_NO_ID,
+			"end.t [flavor FLAVORS] table TABLE",
+			with_help(
+				"L3 routing domain ID.",
+				ec_node_uint("TABLE", 0, UINT16_MAX - 1, 10)
+			),
+			with_help(
+				"Transit endpoint with specific IPv6 table lookup.",
+				ec_node_str("end.t", "end.t")
+			),
+			with_help("Endpoint flavor(s).", flavor_node)
+		),
+		EC_NODE_CMD(
+			EC_NO_ID,
+			"(end.dt4|end.dt6|end.dt46) [table TABLE]",
+			with_help(
+				"L3 routing domain ID.",
+				ec_node_uint("TABLE", 0, UINT16_MAX - 1, 10)
+			),
+			with_help(
+				"Endpoint with decapsulation and specific IPv4 table lookup.",
+				ec_node_str("end.dt4", "end.dt4")
+			),
+			with_help(
+				"Endpoint with decapsulation and specific IPv6 table lookup.",
+				ec_node_str("end.dt6", "end.dt6")
+			),
+			with_help(
+				"Endpoint with decapsulation and specific IPv4/IPv6 table lookup.",
+				ec_node_str("end.dt46", "end.dt46")
+			)
+		)
+	);
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_ADD, CTX_ARG("sr", "Create srv6 stack elements.")),
+		"localsid SID behavior BEHAVIOR [vrf VRF]",
+		srv6_localsid_add,
+		"Create a new local endpoint.",
+		with_help("Local SID.", ec_node_re("SID", IPV6_RE)),
+		with_help("Node behavior", beh_node),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_DEL, CTX_ARG("sr", "Delete srv6 stack elements.")),
+		"localsid SID [vrf VRF]",
+		srv6_localsid_del,
+		"Delete a srv6 endpoint.",
+		with_help("Local SID.", ec_node_re("SID", IPV6_RE)),
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+	ret = CLI_COMMAND(
+		CLI_CONTEXT(root, CTX_SHOW, CTX_ARG("sr", "Show srv6 stack elements.")),
+		"localsid [vrf VRF]",
+		srv6_localsid_show,
+		"View all localsid",
+		with_help("L3 routing domain ID.", ec_node_uint("VRF", 0, UINT16_MAX - 1, 10))
+	);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static struct gr_cli_context ctx = {
+	.name = "srv6",
+	.init = ctx_init,
+};
+
+static void __attribute__((constructor, used)) init(void) {
+	register_context(&ctx);
+}

commit f2bdb58a011532ea3c37453cac0135b702af14b0

srv6: fix reduced encapsulation SRH length
In h.encaps.red mode the first segment is only carried in the outer
destination address, so the segment list holds one entry less. The SRH
was still sized and indexed for the full list, which left the last slot
untouched: 16 bytes of stale mbuf headroom, usually fragments of a
previously handled packet, were sent on the wire.

The extra slot also made last_entry point one entry too far.

Fixes: 7d88a1443587 ("srv6: add srv6 module")
Signed-off-by: Maxime Leroy <maxime@leroys.fr>
Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

modules/srv6/datapath/srv6_output.c
@@ -92,7 +92,7 @@ srv6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 		optlen = 0;
 		reduc = d->encap == SR_H_ENCAPS_RED ? 1 : 0;
 		if (d->n_seglist > reduc)
-			optlen += sizeof(*srh) + (d->n_seglist * sizeof(d->seglist[0]));
+			optlen += sizeof(*srh) + ((d->n_seglist - reduc) * sizeof(d->seglist[0]));
 
 		outer_ip6 = gr_mbuf_prepend(m, outer_ip6, optlen);
 		if (unlikely(outer_ip6 == NULL)) {
@@ -111,7 +111,7 @@ srv6_output_process(struct rte_graph *graph, struct rte_node *node, void **objs,
 			srh->segments_left = d->n_seglist - 1;
 			// flags aliases the whole word: last_entry, flag and tag
 			srh->flags = 0;
-			srh->last_entry = d->n_seglist - 1;
+			srh->last_entry = d->n_seglist - 1 - reduc;
 
 			segments = PAYLOAD(srh);
 			for (k = reduc; k < d->n_seglist; k++)

smoke/srv6_test.sh
@@ -156,3 +156,39 @@ seglist="fd00:202:a00:: fd00:202:b00:: fd00:202:c00::"
 grcli nexthop add srv6 seglist $seglist id 44
 grcli route add 192.168.0.0/16 via id 44
 ip netns exec n0 ping -i0.01 -c3 -n 192.168.60.1
+
+# h.encaps.red leaves the first segment out of the SRH, it is already carried
+# in the outer destination address. Only two entries remain: hdr_len=4,
+# last_entry=1 and segments_left=2, which RFC 8986 4.1 allows to exceed
+# last_entry by one.
+grcli nexthop add srv6 seglist $seglist encap h.encaps.red id 45
+grcli route add 192.168.0.0/16 via id 45
+
+# Start the captures before the traffic. ping -i0.2 -c5 is done emitting after
+# 0.8s, which tcpdump may not beat to opening its capture. The second capture
+# is only there to report what was really sent when the first one fails.
+ip netns exec n1 timeout 5 tcpdump -c1 -pnnli x-p1 \
+	'ip6 dst fd00:202:a00:: and ip6[41] = 4 and ip6[43] = 2 and ip6[44] = 1' \
+	>$tmp/srh_reduced 2>&1 &
+tcpdump_pid=$!
+ip netns exec n1 timeout 5 tcpdump -c1 -pnnvli x-p1 ip6 dst fd00:202:a00:: \
+	>$tmp/srh_any 2>&1 &
+tcpdump_any_pid=$!
+for _ in $(seq 50); do
+	if grep -q "listening on" $tmp/srh_reduced && grep -q "listening on" $tmp/srh_any; then
+		break
+	fi
+	sleep 0.1
+done
+
+ip netns exec n0 ping -i0.2 -c5 -n 192.168.60.1 >/dev/null 2>&1 &
+ping_pid=$!
+
+if ! wait $tcpdump_pid; then
+	wait $tcpdump_any_pid || true
+	wait $ping_pid || true
+	cat $tmp/srh_any
+	fail "h.encaps.red did not produce a reduced SRH"
+fi
+wait $tcpdump_any_pid || true
+wait $ping_pid || fail "h.encaps.red did not forward"