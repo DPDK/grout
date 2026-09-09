1dbd4a3bc163d3efa52beca5cf366c3f8a1ac91b
Robin Jarry <rjarry@redhat.com>
2026-09-09 19:00:07 +0200

    frr: implement neighbor read operation
    FRR PR 21206 also routes EVPN neighbor reads through the new
    DPLANE_OP_NEIGH_READ operation instead of reading the kernel neighbor
    table directly via netlink. Without a handler, grout leaves zebra
    unable to reconcile its EVPN neighbor state, so replay the grout L3
    nexthops as neighbor install notifications, filtered by the interface
    and IP address carried in the request.
    
    Reuse grout_neigh_notify(), which already selects the relevant L3
    nexthops and enqueues them to zebra.
    
    Link: https://github.com/FRRouting/frr/pull/21206
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Reviewed-by: Christophe Fontaine <cfontain@redhat.com>

--- frr/zebra_dplane_grout.c
+++ frr/zebra_dplane_grout.c
@@ -976,6 +976,9 @@ static enum zebra_dplane_result zd_grout_process_update(struct zebra_dplane_ctx 
 	case DPLANE_OP_FDB_READ:
 		return grout_fdb_read_ctx(ctx);
 
+	case DPLANE_OP_NEIGH_READ:
+		return grout_neigh_read_ctx(ctx);
+
 	case DPLANE_OP_INTF_SPEED_GET:
 		// Workaround: return fail INTF_SPEED_GET to stop zebra from repeatedly polling.
 		// Speed is already provided via INTF_INSTALL/UPDATE.
